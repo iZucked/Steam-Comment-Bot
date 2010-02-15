de43ae420e80256e95885dac427748a8b519e673
Simon Goodall <unknown@kiln.example.com>
2010-02-15 14:08:36 +0000

    Added some more methods

--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/acme/optimiser/lso/IMove.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/acme/optimiser/lso/IMove.java
@@ -2,12 +2,40 @@ package com.acme.optimiser.lso;
 
 import java.util.Collection;
 
-import com.acme.optimiser.ISequence;
+import com.acme.optimiser.IModifiableSequences;
+import com.acme.optimiser.IResource;
 import com.acme.optimiser.ISequences;
 
-public interface IMove {
-	
-	Collection<ISequence> getAffectedSequences();
-	
-	void apply(ISequences sequences);
+/**
+ * Interface defining a Move as used in the Local Search Optimiser.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public interface IMove<T> {
+
+	/**
+	 * Returns the collection of {@link IResource}s this move affects.
+	 * 
+	 * @return
+	 */
+	Collection<IResource> getAffectedResources();
+
+	/**
+	 * Apply the move to the {@link IResources} listed in
+	 * {@link #getAffectedResources()}.
+	 * 
+	 * @param sequences
+	 */
+	void apply(IModifiableSequences<T> sequences);
+
+	/**
+	 * Validate the move parameters are valid for the given {@link ISequences}
+	 * object.
+	 * 
+	 * @return True on valid parameters.
+	 */
+	boolean validate(ISequences<T> sequences);
 }
