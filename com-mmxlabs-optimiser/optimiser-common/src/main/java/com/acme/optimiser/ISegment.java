fa5e1a9ee25161829b4c9b57ddf31c7692100817
Simon Goodall <unknown@kiln.example.com>
2010-02-18 09:16:51 +0000

    Doc

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/ISegment.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/ISegment.java
@@ -9,14 +9,40 @@ package com.acme.optimiser;
  */
 public interface ISegment<T> extends Iterable<T> {
 
+	/**
+	 * Return size of segment
+	 * 
+	 * @return
+	 */
 	int size();
 
+	/**
+	 * Return an element within the segment.
+	 * 
+	 * @param index
+	 * @return
+	 */
 	T get(int index);
 
+	/**
+	 * Return segment start index in originating sequence
+	 * 
+	 * @return
+	 */
 	int getSequenceStart();
 
+	/**
+	 * Return segment end index in originating sequence
+	 * 
+	 * @return
+	 */
 	int getSequenceEnd();
 
+	/**
+	 * Return originating {@link ISequence} instance.
+	 * 
+	 * @return
+	 */
 	ISequence<T> getSequence();
 
 }
