79e87fe6b9c45c508c325bd1017801955fcc4d71
Simon Goodall <simon@simongoodall.co.uk>
2010-02-26 14:01:47 +0000

    Make LSO abstract and move main optimisation loop to a sub-class (such as DefaultLocalSearchOptimiser).

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/acme/optimiser/lso/impl/DefaultLocalSearchOptimiser.java
@@ -0,0 +1,87 @@
+package com.acme.optimiser.lso.impl;
+
+import java.util.Collection;
+
+import com.acme.optimiser.IOptimisationContext;
+import com.acme.optimiser.ISolution;
+import com.acme.optimiser.impl.ModifiableSequences;
+import com.acme.optimiser.lso.IMove;
+
+/**
+ * A sub-class of {@link LocalSearchOptimiser} implementing a default main loop.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public class DefaultLocalSearchOptimiser<T> extends LocalSearchOptimiser<T> {
+	@Override
+	public void optimise(final IOptimisationContext optimiserContext,
+			final Collection<ISolution> initialSolutions,
+			final Object archiverCallback) {
+
+		// Setup the optimisation process
+
+		int numberOfMovesTried = 0;
+		int numberOfMovesAccepted = 0;
+
+		final ModifiableSequences<T> currentRawSequences;
+
+		// Create a copy of the current sequences
+		final ModifiableSequences<T> potentialRawSequences = new ModifiableSequences<T>(
+				currentRawSequences.getResources());
+		updateSequences(currentRawSequences, potentialRawSequences,
+				currentRawSequences.getResources());
+
+		// Perform the optimisation
+		for (int iter = 0; iter < getNumberOfIterations(); ++iter) {
+
+			++numberOfMovesTried;
+
+			// Generate a new move
+			final IMove<T> move = getMoveGenerator().generateMove();
+
+			// Make sure the generator was able to generate a move
+			if (move == null) {
+				continue;
+			}
+
+			// Test move is valid against data.
+			if (!move.validate(potentialRawSequences)) {
+				continue;
+			}
+
+			// Update potential sequences
+			move.apply(potentialRawSequences);
+
+			// Apply sequence manipulators
+			// ... TODO ...
+			// ISequences<T> fullSequenes =
+			// manipulator.manipulate(potentialRawSquences);
+
+			// Apply hard constraint checkers
+			// ... TODO ...
+
+			if (evaluateSequences(potentialRawSequences)) {
+				// Success update state for new sequences
+
+				updateSequences(potentialRawSequences, currentRawSequences,
+						move.getAffectedResources());
+
+				// TODO: Update current fitness state
+
+				++numberOfMovesAccepted;
+			} else {
+				// Failed, reset state for old sequences
+
+				updateSequences(currentRawSequences, potentialRawSequences,
+						move.getAffectedResources());
+			}
+
+		}
+
+		// Finalise optimisation process
+
+	}
+}
--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/acme/optimiser/lso/impl/LocalSearchOptimiser.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/acme/optimiser/lso/impl/LocalSearchOptimiser.java
@@ -10,96 +10,31 @@ import com.acme.optimiser.IResource;
 import com.acme.optimiser.ISequence;
 import com.acme.optimiser.ISequences;
 import com.acme.optimiser.ISolution;
-import com.acme.optimiser.impl.ModifiableSequences;
-import com.acme.optimiser.lso.IMove;
 import com.acme.optimiser.lso.IMoveGenerator;
 
 /**
- * Main class implementing a Local Search Optimiser.
+ * Main class implementing a Local Search Optimiser. While the actual
+ * optimisation loop is left to sub-classes, this class provides the bulk of the
+ * implementation to simplify such loops.
  * 
  * @author Simon Goodall
  * 
  * @param <T>
  *            Sequence Element Type
  */
-public class LocalSearchOptimiser<T> implements IOptimiser<T> {
+public abstract class LocalSearchOptimiser<T> implements IOptimiser<T> {
 
 	private IMoveGenerator<T> moveGenerator;
 
 	private int numberOfIterations;
 
+	/**
+	 * Sub-classes of {@link LocalSearchOptimiser} should implement this method
+	 * to perform the actual optimisation.
+	 */
 	@Override
-	public void optimise(final IOptimisationContext optimiserContext,
-			final Collection<ISolution> initialSolutions,
-			final Object archiverCallback) {
-
-		// TODO: Some or all of this method should be abstract to allow custom
-		// loops to be provided. Appropriate accessors should be generated for
-		// internal data.
-
-		// Setup the optimisation process
-
-		int numberOfMovesTried = 0;
-		int numberOfMovesAccepted = 0;
-
-		final ModifiableSequences<T> currentRawSequences;
-
-		// Create a copy of the current sequences
-		final ModifiableSequences<T> potentialRawSequences = new ModifiableSequences<T>(
-				currentRawSequences.getResources());
-		updateSequences(currentRawSequences, potentialRawSequences,
-				currentRawSequences.getResources());
-
-		// Perform the optimisation
-		for (int iter = 0; iter < getNumberOfIterations(); ++iter) {
-
-			++numberOfMovesTried;
-
-			// Generate a new move
-			final IMove<T> move = getMoveGenerator().generateMove();
-
-			// Make sure the generator was able to generate a move
-			if (move == null) {
-				continue;
-			}
-
-			// Test move is valid against data.
-			if (!move.validate(potentialRawSequences)) {
-				continue;
-			}
-
-			// Update potential sequences
-			move.apply(potentialRawSequences);
-
-			// Apply sequence manipulators
-			// ... TODO ...
-			// ISequences<T> fullSequenes =
-			// manipulator.manipulate(potentialRawSquences);
-
-			// Apply hard constraint checkers
-			// ... TODO ...
-
-			if (evaluateSequences(potentialRawSequences)) {
-				// Success update state for new sequences
-
-				updateSequences(potentialRawSequences, currentRawSequences,
-						move.getAffectedResources());
-
-				// TODO: Update current fitness state
-
-				++numberOfMovesAccepted;
-			} else {
-				// Failed, reset state for old sequences
-
-				updateSequences(currentRawSequences, potentialRawSequences,
-						move.getAffectedResources());
-			}
-
-		}
-
-		// Finalise optimisation process
-
-	}
+	public abstract void optimise(IOptimisationContext optimiserContext,
+			Collection<ISolution> initialSolutions, Object archiverCallback);
 
 	/**
 	 * Evaluate the current sequences and return whether or not they are
