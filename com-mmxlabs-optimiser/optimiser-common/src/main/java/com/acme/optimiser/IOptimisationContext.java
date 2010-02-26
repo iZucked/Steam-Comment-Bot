447a0075f82e04422adfba403c242cf801e7baeb
Simon Goodall <simon@simongoodall.co.uk>
2010-02-26 16:44:26 +0000

    Add template parameter to interface Add method to return the initial state

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/IOptimisationContext.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/IOptimisationContext.java
@@ -1,5 +1,23 @@
 package com.acme.optimiser;
 
-public interface IOptimisationContext {
+/**
+ * Interface defining an optimisation context. This tes together static
+ * optimisation data, initial state, parameters and fitness components that will
+ * compose an optimisation run.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public interface IOptimisationContext<T> {
+
+	/**
+	 * Returns the initial sequences state - i.e. the starting point of the
+	 * optimisation process.
+	 * 
+	 * @return
+	 */
+	ISequences<T> getInitialSequences();
 
 }
