c36f84294a1849abfc5fc01e1c5492f8ee833338
Simon Goodall <simon@simongoodall.co.uk>
2010-03-03 11:41:20 +0000

    Created a registery implementation and added methods to the interface

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/fitness/IFitnessFunctionRegistery.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/acme/optimiser/fitness/IFitnessFunctionRegistery.java
@@ -1,5 +1,50 @@
 package com.acme.optimiser.fitness;
 
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * The {@link IFitnessFunctionRegistery} is a store for
+ * {@link IFitnessFunctionFactory} instances. The Registery is to be used to
+ * create new instances of fitness functions for use in optimisations.
+ * 
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public interface IFitnessFunctionRegistery {
 
+	/**
+	 * Registers a {@link IFitnessFunctionFactory} instance.
+	 * 
+	 * @param name
+	 * @param factory
+	 */
+	void registerFitnessFunction(String name, IFitnessFunctionFactory factory);
+
+	/**
+	 * Return a {@link Collection} of the registered
+	 * {@link IFitnessFunctionFactory} instances.
+	 * 
+	 * @return
+	 */
+
+	Collection<IFitnessFunctionFactory> getFitnessFunctionFactories();
+
+	/**
+	 * Returns a {@link Collection} of the registered
+	 * {@link IFitnessFunctionFactory} names.
+	 * 
+	 * @return
+	 */
+	Collection<String> getFitnessFunctionFactoryNames();
+
+	/**
+	 * Returns a {@link List} of {@link IFitnessFunctionFactory} instances
+	 * ordered by the given list of names.
+	 * 
+	 * @param names
+	 * @return
+	 */
+	List<IFitnessFunctionFactory> getFitnessFunctionFactories(List<String> names);
 }
