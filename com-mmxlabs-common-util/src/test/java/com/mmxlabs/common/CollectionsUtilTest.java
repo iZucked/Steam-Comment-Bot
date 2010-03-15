e516ec600961bf903eb6ed3b9664e99238f15419
Simon Goodall <simon@simongoodall.co.uk>
2010-03-15 07:29:54 +0000

    Created a new "common utility" project.
    BugzId: 10

---
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/CollectionsUtilTest.java
@@ -0,0 +1,50 @@
+package com.mmxlabs.common;
+
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CollectionsUtilTest {
+
+	@Test
+	public void makeArrayListTest() {
+
+		// TODO: We should test the actual object instances rather than rely
+		// upon equals as different instances could be equal.
+
+		final List<Integer> list = CollectionsUtil.makeArrayList(5, 4, 3, 2, 1);
+
+		Assert.assertEquals(5, list.size());
+
+		Assert.assertEquals(Integer.valueOf(5), list.get(0));
+		Assert.assertEquals(Integer.valueOf(4), list.get(1));
+		Assert.assertEquals(Integer.valueOf(3), list.get(2));
+		Assert.assertEquals(Integer.valueOf(2), list.get(3));
+		Assert.assertEquals(Integer.valueOf(1), list.get(4));
+	}
+
+	@Test
+	public void makeHashMapTest() {
+
+		// TODO: We should test the actual object instances rather than rely
+		// upon equals as different instances could be equal.
+
+		final Map<String, String> map = CollectionsUtil.makeHashMap("key1",
+				"value1", "key2", "value2");
+
+		Assert.assertEquals(2, map.size());
+
+		Assert.assertTrue(map.containsKey("key1"));
+		Assert.assertTrue(map.containsKey("key2"));
+
+		Assert.assertTrue(map.containsValue("value1"));
+		Assert.assertTrue(map.containsValue("value2"));
+
+		Assert.assertEquals("value1", map.get("key1"));
+		Assert.assertEquals("value2", map.get("key2"));
+	}
+
+}
---
+++ scripts/lingo-postrelease.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+echo $RELEASE_VERSION
+echo $DEVELOPMENT_VERSION
+
+
+## Check Variables
+
+if [ -z "$RELEASE_VERSION" ]; then
+        echo "Release version is not set"
+        exit 1
+fi
+LEN1=`expr match "$RELEASE_VERSION" '^[0-9]\+\.[0-9]\+\.[0-9]\+\.RELEASE$'`
+if [ ! $LEN1 -gt 0 ]; then
+        echo "Release version does not match expected pattern:  '^[0-9]\+\.[0-9]\+\.[0-9]\+\.RELEASE$'";
+        exit 1
+fi
+
+if [ -z "$DEVELOPMENT_VERSION" ]; then
+        echo "Development version is not set"
+        exit 1
+fi
+LEN2=`expr match "$DEVELOPMENT_VERSION" '^[0-9]\+\.[0-9]\+\.[0-9]\+-SNAPSHOT$'`
+if [ ! $LEN2 -gt 0 ]; then
+        echo "Development version does not match expected pattern:  '^[0-9]\+\.[0-9]\+\.[0-9]\+-SNAPSHOT$'";
+        exit 1
+fi
+
+## Set devel version number
+for repo in com*/; do
+        pushd $repo
+        mvn -Dtycho.mode=maven -DnewVersion=$DEVELOPMENT_VERSION org.eclipse.tycho:tycho-versions-plugin:0.18.0:set-version
+        popd
+done
+mvn -Dtycho.mode=maven -DnewVersion=$DEVELOPMENT_VERSION org.eclipse.tycho:tycho-versions-plugin:0.18.0:set-version
+git commit -a -m "Bump version to $DEVELOPMENT_VERSION in prep for next development iteration"
+
+SHORTVERSION=`echo $DEVELOPMENT_VERSION | sed -e "s/-SNAPSHOT//"`
+echo "projectRoot ." > update-refs-dev.tiger
+echo "setting osgi.release.qualifier RELEASE" >> update-refs-dev.tiger
+for project in `cat known-projects.txt`; do
+        echo "updateReferences $project $SHORTVERSION" >> update-refs-dev.tiger
+done
+java -jar libs/com.inventage.tools.versiontiger-1.3.0-SNAPSHOT-cli.jar update-refs-dev.tiger
+
+for repo in com*/; do
+        pushd $repo
+        #Update refs does bad things to pom parent version
+        git checkout -- pom.xml
+        git checkout -- */pom.xml
+        git checkout -- */category.xml
+        git checkout -- */feature.xml
+        git checkout -- */*.project
+        popd
+done
+
+
+## Update commit
+git commit -a --amend --no-edit
+git branch build/$DEVELOPMENT_VERSION
+
+## Generate bundles
+git bundle create changes.git TMP_RELEASE_BASE..build/$DEVELOPMENT_VERSION TMP_RELEASE_BASE..${RELEASE_VERSION} --tags --branches
