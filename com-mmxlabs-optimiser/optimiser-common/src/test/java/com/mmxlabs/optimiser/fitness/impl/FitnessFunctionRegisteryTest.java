52769b2bc03a7cc433d1fa00731acaedf43ec49b
Simon Goodall <unknown@kiln.example.com>
2010-03-17 19:36:49 +0000

    Fix up tests after fitness changes

--- com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/fitness/impl/FitnessFunctionRegisteryTest.java
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/fitness/impl/FitnessFunctionRegisteryTest.java
@@ -1,137 +1,156 @@
 package com.mmxlabs.optimiser.fitness.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import junit.framework.Assert;
 
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
 
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.optimiser.fitness.IFitnessCore;
 import com.mmxlabs.optimiser.fitness.IFitnessCoreFactory;
 
 public class FitnessFunctionRegisteryTest {
 
-	Mockery context = new JUnit4Mockery();
-	
 	@Test
 	public void testGetFitnessFunctionFactories() {
-		
+
 		FitnessFunctionRegistry registery = new FitnessFunctionRegistry();
-		
-		IFitnessCoreFactory factory1 = context.mock(IFitnessCoreFactory.class, "factory1");
-		IFitnessCoreFactory factory2 = context.mock(IFitnessCoreFactory.class, "factory2");
-		IFitnessCoreFactory factory3 = context.mock(IFitnessCoreFactory.class, "factory3");
-		
-		registery.registerFitnessFunction("factory1", factory1);
-		registery.registerFitnessFunction("factory2", factory2);
-		registery.registerFitnessFunction("factory3", factory3);
-		
-		Collection<IFitnessCoreFactory> fitnessFunctionFactories = registery.getFitnessFunctionFactories();
+
+		IFitnessCoreFactory factory1 = new MockFitnessCoreFactory("factory1",
+				CollectionsUtil.makeArrayList("component1"));
+		IFitnessCoreFactory factory2 = new MockFitnessCoreFactory("factory2",
+				CollectionsUtil.makeArrayList("component2"));
+		IFitnessCoreFactory factory3 = new MockFitnessCoreFactory("factory3",
+				CollectionsUtil.makeArrayList("component3"));
+
+		registery.registerFitnessCoreFactory(factory1);
+		registery.registerFitnessCoreFactory(factory2);
+		registery.registerFitnessCoreFactory(factory3);
+
+		Collection<IFitnessCoreFactory> fitnessFunctionFactories = registery
+				.getFitnessCoreFactories();
+
 		Assert.assertEquals(3, fitnessFunctionFactories.size());
-		
+
 		Assert.assertTrue(fitnessFunctionFactories.contains(factory1));
 		Assert.assertTrue(fitnessFunctionFactories.contains(factory2));
 		Assert.assertTrue(fitnessFunctionFactories.contains(factory3));
-
-
-		context.assertIsSatisfied();
-		
 	}
-
-	
 
 	@Test
 	public void testGetFitnessFunctionFactories_Str() {
-		
-		FitnessFunctionRegistry registery = new FitnessFunctionRegistry();
-		
-		IFitnessCoreFactory factory1 = context.mock(IFitnessCoreFactory.class, "factory1");
-		IFitnessCoreFactory factory2 = context.mock(IFitnessCoreFactory.class, "factory2");
-		IFitnessCoreFactory factory3 = context.mock(IFitnessCoreFactory.class, "factory3");
-		
-		registery.registerFitnessFunction("factory1", factory1);
-		registery.registerFitnessFunction("factory2", factory2);
-		registery.registerFitnessFunction("factory3", factory3);
-		
-		List<String> names = new ArrayList<String>(2);
-		names.add("factory3");
-		names.add("factory2");
-		
-		List<IFitnessCoreFactory> fitnessFunctionFactories = registery.getFitnessFunctionFactories(names);
-		Assert.assertEquals(2, fitnessFunctionFactories.size());
-		
-		Assert.assertEquals(factory3, fitnessFunctionFactories.get(0));
-		Assert.assertEquals(factory2, fitnessFunctionFactories.get(1));
-		
 
-		context.assertIsSatisfied();
+		FitnessFunctionRegistry registery = new FitnessFunctionRegistry();
+
+		IFitnessCoreFactory factory1 = new MockFitnessCoreFactory("factory1",
+				CollectionsUtil.makeArrayList("component1"));
+		IFitnessCoreFactory factory2 = new MockFitnessCoreFactory("factory2",
+				CollectionsUtil.makeArrayList("component2"));
+		IFitnessCoreFactory factory3 = new MockFitnessCoreFactory("factory3",
+				CollectionsUtil.makeArrayList("component3"));
+
+		registery.registerFitnessCoreFactory(factory1);
+		registery.registerFitnessCoreFactory(factory2);
+		registery.registerFitnessCoreFactory(factory3);
+
+		List<String> names = CollectionsUtil.makeArrayList("component3",
+				"component2");
+
+		Collection<IFitnessCoreFactory> fitnessFunctionFactories = registery
+				.getFitnessCoreFactories(names);
+		Assert.assertEquals(2, fitnessFunctionFactories.size());
+
+		Assert.assertTrue(fitnessFunctionFactories.contains(factory3));
+		Assert.assertTrue(fitnessFunctionFactories.contains(factory2));
 	}
-	
-	
+
 	@Test
 	public void testGetFitnessFunctionFactories_Str2() {
 
-		
 		FitnessFunctionRegistry registery = new FitnessFunctionRegistry();
-		
-		IFitnessCoreFactory factory1 = context.mock(IFitnessCoreFactory.class, "factory1");
-		IFitnessCoreFactory factory2 = context.mock(IFitnessCoreFactory.class, "factory2");
-		IFitnessCoreFactory factory3 = context.mock(IFitnessCoreFactory.class, "factory3");
-		
-		registery.registerFitnessFunction("factory1", factory1);
-		registery.registerFitnessFunction("factory2", factory2);
-		registery.registerFitnessFunction("factory3", factory3);
-		
-		List<String> names = new ArrayList<String>(2);
-		names.add("factory3");
-		names.add("factory3");
-		
-		List<IFitnessCoreFactory> fitnessFunctionFactories = registery.getFitnessFunctionFactories(names);
-		Assert.assertEquals(2, fitnessFunctionFactories.size());
-		
-		Assert.assertEquals(factory3, fitnessFunctionFactories.get(0));
-		Assert.assertEquals(factory3, fitnessFunctionFactories.get(1));
-		
-		Assert.fail("Ambiguous API - Duplicate entries?");
 
-		context.assertIsSatisfied();
-	
+		IFitnessCoreFactory factory1 = new MockFitnessCoreFactory("factory1",
+				CollectionsUtil.makeArrayList("component1"));
+		IFitnessCoreFactory factory2 = new MockFitnessCoreFactory("factory2",
+				CollectionsUtil.makeArrayList("component2"));
+		IFitnessCoreFactory factory3 = new MockFitnessCoreFactory("factory3",
+				CollectionsUtil.makeArrayList("component3"));
+
+		registery.registerFitnessCoreFactory(factory1);
+		registery.registerFitnessCoreFactory(factory2);
+		registery.registerFitnessCoreFactory(factory3);
+
+		List<String> names = CollectionsUtil.makeArrayList("component3",
+				"component3");
+
+		Collection<IFitnessCoreFactory> fitnessFunctionFactories = registery
+				.getFitnessCoreFactories(names);
+		Assert.assertEquals(1, fitnessFunctionFactories.size());
+
+		Assert.assertTrue(fitnessFunctionFactories.contains(factory3));
 	}
-	
 
-	
 	@Test
 	public void testGetFitnessFunctionFactories_Str3() {
 
-		
 		FitnessFunctionRegistry registery = new FitnessFunctionRegistry();
-		
-		IFitnessCoreFactory factory1 = context.mock(IFitnessCoreFactory.class, "factory1");
-		IFitnessCoreFactory factory2 = context.mock(IFitnessCoreFactory.class, "factory2");
-		IFitnessCoreFactory factory3 = context.mock(IFitnessCoreFactory.class, "factory3");
-		
-		registery.registerFitnessFunction("factory1", factory1);
-		registery.registerFitnessFunction("factory2", factory2);
-		registery.registerFitnessFunction("factory3", factory3);
-		
-		List<String> names = new ArrayList<String>(2);
-		names.add("factory3");
-		names.add("unknown");
-		names.add("factory1");
-		
-		List<IFitnessCoreFactory> fitnessFunctionFactories = registery.getFitnessFunctionFactories(names);
-		Assert.assertEquals(3, fitnessFunctionFactories.size());
-		
-		Assert.assertEquals(factory3, fitnessFunctionFactories.get(0));
-		Assert.assertNull(fitnessFunctionFactories.get(1));
-		Assert.assertEquals(factory1, fitnessFunctionFactories.get(2));
-		
-		Assert.fail("Ambiguous API - Duplicate entries?");
-		
-		context.assertIsSatisfied();
+
+		IFitnessCoreFactory factory1 = new MockFitnessCoreFactory("factory1",
+				CollectionsUtil.makeArrayList("component1"));
+		IFitnessCoreFactory factory2 = new MockFitnessCoreFactory("factory2",
+				CollectionsUtil.makeArrayList("component2"));
+		IFitnessCoreFactory factory3 = new MockFitnessCoreFactory("factory3",
+				CollectionsUtil.makeArrayList("component3"));
+
+		registery.registerFitnessCoreFactory(factory1);
+		registery.registerFitnessCoreFactory(factory2);
+		registery.registerFitnessCoreFactory(factory3);
+
+		List<String> names = CollectionsUtil.makeArrayList("component3",
+				"unknown", "component1");
+
+		Collection<IFitnessCoreFactory> fitnessFunctionFactories = registery
+				.getFitnessCoreFactories(names);
+
+		Assert.assertEquals(2, fitnessFunctionFactories.size());
+
+		Assert.assertTrue(fitnessFunctionFactories.contains(factory1));
+		Assert.assertTrue(fitnessFunctionFactories.contains(factory3));
 	}
-	
+
+	/**
+	 * Mock implementation of an {@link IFitnessCoreFactory} which just return
+	 * the core name and a {@link Collection} of component names.
+	 * 
+	 * @author Simon Goodall
+	 * 
+	 */
+	private static class MockFitnessCoreFactory implements IFitnessCoreFactory {
+
+		private final String coreName;
+		private final Collection<String> componentNames;
+
+		public MockFitnessCoreFactory(String coreName,
+				Collection<String> componentNames) {
+			this.coreName = coreName;
+			this.componentNames = componentNames;
+		}
+
+		@Override
+		public Collection<String> getFitnessComponentNames() {
+			return componentNames;
+		}
+
+		@Override
+		public String getFitnessCoreName() {
+			return coreName;
+		}
+
+		@Override
+		public <T> IFitnessCore<T> instantiate() {
+			throw new UnsupportedOperationException("Not implemented");
+		}
+	}
 }
