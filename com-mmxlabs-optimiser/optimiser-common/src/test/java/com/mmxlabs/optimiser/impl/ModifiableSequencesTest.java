a39053e7017ae770b357f7bc1233a80dc0c92bfd
Simon Goodall <unknown@kiln.example.com>
2010-03-23 11:59:45 +0000

    Create some missing stub test cases

---
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/impl/ModifiableSequencesTest.java
@@ -0,0 +1,54 @@
+package com.mmxlabs.optimiser.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ModifiableSequencesTest {
+
+	@Test
+	public void testModifiableSequencesListOfIResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifiableSequencesListOfIResourceMapOfIResourceIModifiableSequenceOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifiableSequencesISequencesOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetModifiableSequenceIResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetModifiableSequenceInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResources() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequenceIResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequenceInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequences() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/impl/UnmodifiableSequencesWrapperTest.java
@@ -0,0 +1,39 @@
+package com.mmxlabs.optimiser.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnmodifiableSequencesWrapperTest {
+
+	@Test
+	public void testUnmodifiableSequencesWrapper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResources() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequenceIResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequenceInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequences() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSequences() {
+		fail("Not yet implemented");
+	}
+
+}
