18f35022ca766b7190d1f1354b475254e2884cfe
Simon Goodall <unknown@kiln.example.com>
2010-03-24 08:26:54 +0000

    Added an implementation of IFitnessEvaluator to handle the LSO case

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/LinearSimulatedAnnealingFitnessEvaluator.java
@@ -0,0 +1,193 @@
+package com.mmxlabs.optimiser.lso.impl;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.fitness.IFitnessComponent;
+import com.mmxlabs.optimiser.fitness.IFitnessEvaluator;
+import com.mmxlabs.optimiser.fitness.IFitnessHelper;
+import com.mmxlabs.optimiser.impl.Sequences;
+
+/**
+ * A {@link IFitnessEvaluator} implementation to apply simulated annealing to
+ * the optimisation process and use a linear combination of weights to
+ * {@link IFitnessComponent} values. This implementation only tracks a single
+ * best state.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public final class LinearSimulatedAnnealingFitnessEvaluator<T> implements
+		IFitnessEvaluator<T> {
+
+	private IFitnessHelper<T> fitnessHelper = null;
+
+	private List<IFitnessComponent<T>> fitnessComponents = null;
+
+	private double coolingOrders = Double.MAX_VALUE;
+
+	private double temperature = Double.MAX_VALUE;
+
+	private Map<String, Double> fitnessComponentWeights = null;
+
+	private ISequences<T> bestSequences = null;
+
+	private double bestFitness = Double.MAX_VALUE;
+
+	private double currentFitness = Double.MAX_VALUE;
+
+	private double currentThreshold = Double.MAX_VALUE;
+
+	private Random random = null;
+
+	@Override
+	public boolean checkSequences(final ISequences<T> sequences) {
+
+		// TODO: What about affected resources?
+
+		double totalFitness = evaluteSequences(sequences);
+
+		// Calculate fitness delta
+		double delta = totalFitness - currentFitness;
+
+		// If fitness change is within the threshold, then accept the change
+		if (delta < currentThreshold) {
+
+			currentFitness = totalFitness;
+
+			// If this is the best state seen so far, then record it.
+			if (currentFitness < bestFitness) {
+
+				bestSequences = new Sequences<T>(sequences);
+			}
+
+			step();
+
+			return true;
+		} else {
+
+			step();
+
+			return false;
+		}
+	}
+
+	private double evaluteSequences(final ISequences<T> sequences) {
+		// Evaluates the current sequences
+		fitnessHelper.evaluateSequencesFromComponents(sequences,
+				fitnessComponents);
+
+		// Sum up total fitness, combining raw values with weights
+		double totalFitness = 0.0;
+		for (final IFitnessComponent<T> component : fitnessComponents) {
+			final long rawFitness = component.getFitness();
+			final double weight = fitnessComponentWeights.get(component
+					.getName());
+			final double fitness = weight * (double) rawFitness;
+			totalFitness += fitness;
+		}
+		return totalFitness;
+	}
+
+	@Override
+	public void init(final ISequences<T> initialSequences) {
+		if (fitnessComponents == null) {
+			throw new IllegalStateException("No fitness components set");
+		}
+
+		if (fitnessHelper == null) {
+			throw new IllegalStateException("No fitness helper set");
+		}
+
+		if (getFitnessComponentWeights() == null) {
+			throw new IllegalStateException("No fitness component weights set");
+		}
+
+		if (random == null) {
+			throw new IllegalStateException("No random number generator set");
+		}
+
+		if (initialSequences == null) {
+			throw new IllegalArgumentException(
+					"Initial sequences cannot be null");
+		}
+
+		double totalFitness = evaluteSequences(initialSequences);
+		bestFitness = totalFitness;
+		currentFitness = totalFitness;
+		bestSequences = new Sequences<T>(initialSequences);
+
+		// TODO: Init initial threshold.
+
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Step the evaluator to the next threshold value.
+	 */
+	private void step() {
+
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public void setFitnessComponents(
+			final List<IFitnessComponent<T>> fitnessComponents) {
+		this.fitnessComponents = fitnessComponents;
+	}
+
+	@Override
+	public List<IFitnessComponent<T>> getFitnessComponents() {
+		return fitnessComponents;
+	}
+
+	@Override
+	public IFitnessHelper<T> getFitnessHelper() {
+		return fitnessHelper;
+	}
+
+	@Override
+	public void setFitnessHelper(final IFitnessHelper<T> fitnessHelper) {
+		this.fitnessHelper = fitnessHelper;
+	}
+
+	@Override
+	public Collection<ISequences<T>> getBestSequences() {
+		return Collections.singletonList(bestSequences);
+	}
+
+	public void setTemperature(final double temperature) {
+		this.temperature = temperature;
+	}
+
+	public double getTemperature() {
+		return temperature;
+	}
+
+	public void setFitnessComponentWeights(
+			final Map<String, Double> fitnessComponentWeights) {
+		this.fitnessComponentWeights = fitnessComponentWeights;
+	}
+
+	public Map<String, Double> getFitnessComponentWeights() {
+		return fitnessComponentWeights;
+	}
+
+	public void setCoolingOrders(final double coolingOrders) {
+		this.coolingOrders = coolingOrders;
+	}
+
+	public double getCoolingOrders() {
+		return coolingOrders;
+	}
+
+	public double getBestFitness() {
+		return bestFitness;
+	}
+}
