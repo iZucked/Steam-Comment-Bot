f1e44abfd489024b3b4e5bc2aa206119eb83271a
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-01 11:34:27 -0100

    Add stub move body

--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move3over2.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move3over2.java
@@ -1,7 +1,36 @@
 package com.mmxlabs.optimiser.lso.impl;
 
-public class Move3over2 {
+import java.util.Collection;
 
-	
-	
+import com.mmxlabs.optimiser.IModifiableSequences;
+import com.mmxlabs.optimiser.IResource;
+import com.mmxlabs.optimiser.ISequence;
+import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.lso.IMove;
+
+/**
+ * The {@link Move3over2} move type moves a segment from one {@link ISequence}
+ * into another {@link ISequence}.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public class Move3over2<T> implements IMove<T> {
+
+	@Override
+	public void apply(final IModifiableSequences<T> sequences) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public Collection<IResource> getAffectedResources() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public boolean validate(final ISequences<T> sequences) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move4over1.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move4over1.java
@@ -1,5 +1,36 @@
 package com.mmxlabs.optimiser.lso.impl;
 
-public class Move4over1 {
+import java.util.Collection;
 
+import com.mmxlabs.optimiser.IModifiableSequences;
+import com.mmxlabs.optimiser.IResource;
+import com.mmxlabs.optimiser.ISequence;
+import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.lso.IMove;
+
+/**
+ * The {@link Move4over1} move type allows two non-overlapping segments within a
+ * single {@link ISequence} to be swapped.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
+public class Move4over1<T> implements IMove<T> {
+
+	@Override
+	public void apply(final IModifiableSequences<T> sequences) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public Collection<IResource> getAffectedResources() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public boolean validate(final ISequences<T> sequences) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
