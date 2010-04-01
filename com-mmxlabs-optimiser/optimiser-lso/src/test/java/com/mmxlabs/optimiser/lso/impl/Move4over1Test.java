bc225e3199bd3330e7f4c0d7836f56a697572641
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-01 12:25:40 -0100

    Add some stub test cases

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/Move4over1Test.java
@@ -0,0 +1,24 @@
+package com.mmxlabs.optimiser.lso.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Move4over1Test {
+
+	@Test
+	public void testApply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAffectedResources() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidate() {
+		fail("Not yet implemented");
+	}
+
+}
