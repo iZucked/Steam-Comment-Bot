f846a4b7265efd374c6b329b8135be23ef2cfea5
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-08 06:56:45 -0100

    Generate Move3over3 and Move4over1 objects

--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/RandomMoveGenerator.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/RandomMoveGenerator.java
@@ -42,9 +42,16 @@ public final class RandomMoveGenerator<T> implements IMoveGenerator<T> {
 
 		final IMove<T> move;
 		switch (moveType) {
-		case Move_4over2:
-			move = createMove4opt2();
+		case Move_4over1:
+			move = createMove4over1();
 			break;
+		case Move_4over2:
+			move = createMove4over2();
+			break;
+		case Move_3over2:
+			move = createMove3over2();
+			break;
+		// TODO: Add other move types here
 		default:
 			move = null;
 		}
@@ -61,7 +68,6 @@ public final class RandomMoveGenerator<T> implements IMoveGenerator<T> {
 		if (sequences == null) {
 			throw new IllegalStateException("Sequences is not set");
 		}
-
 	}
 
 	public void setRandom(final Random random) {
@@ -114,11 +120,50 @@ public final class RandomMoveGenerator<T> implements IMoveGenerator<T> {
 	}
 
 	/**
+	 * Generate a 4over1 move
+	 * 
+	 * @return
+	 */
+	private IMove<T> createMove4over1() {
+
+		final List<IResource> resources = sequences.getResources();
+
+		final int numResources = resources.size();
+
+		if (numResources < 1) {
+			return null;
+		}
+
+		final int resource = random.nextInt(numResources);
+
+		final ISequence<T> sequence = sequences.getSequence(resource);
+
+		final int[] segmentBreakPoints= new int[4];
+		generateSortedBreakPoints(sequence, segmentBreakPoints);
+
+
+		// Create new move
+		final Move4over1<T> move = new Move4over1<T>();
+
+		// Set resources
+		move.setResource(resources.get(resource));
+
+		// Set break points
+		move.setSegment1Start(segmentBreakPoints[0]);
+		move.setSegment1End(segmentBreakPoints[1]);
+		move.setSegment2Start(segmentBreakPoints[2]);
+		move.setSegment2End(segmentBreakPoints[3]);
+
+		return move;
+	}
+
+	
+	/**
 	 * Generate a 4over2 move
 	 * 
 	 * @return
 	 */
-	private IMove<T> createMove4opt2() {
+	private IMove<T> createMove4over2() {
 
 		final List<IResource> resources = sequences.getResources();
 
@@ -161,4 +206,52 @@ public final class RandomMoveGenerator<T> implements IMoveGenerator<T> {
 
 		return move;
 	}
+
+	/**
+	 * Generate a {@link Move3over2} move type
+	 * 
+	 * @return
+	 */
+	private IMove<T> createMove3over2() {
+
+		final List<IResource> resources = sequences.getResources();
+
+		final int numResources = resources.size();
+
+		if (numResources == 1) {
+			return null;
+		}
+
+		final int resource1 = random.nextInt(numResources);
+		int resource2 = random.nextInt(numResources);
+
+		// Ensure different resources
+		while (resource2 == resource1) {
+			resource2 = random.nextInt(numResources);
+		}
+
+		final ISequence<T> sequence1 = sequences.getSequence(resource1);
+		final ISequence<T> sequence2 = sequences.getSequence(resource2);
+
+		final int[] resource1StartEnd = new int[2];
+		generateSortedBreakPoints(sequence1, resource1StartEnd);
+
+		final int resource2Potision = generateBreakPoint(sequence2);
+
+		// Create new move
+		final Move3over2<T> move = new Move3over2<T>();
+
+		// Set resources
+		move.setResource1(resources.get(resource1));
+		move.setResource2(resources.get(resource2));
+
+		// Set break points
+		move.setResource1Start(resource1StartEnd[0]);
+		move.setResource1End(resource1StartEnd[1]);
+
+		move.setResource2Position(resource2Potision);
+
+		return move;
+	}
+
 }
