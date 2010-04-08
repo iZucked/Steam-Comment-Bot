6fcdfe9f3cd75c2cd6b2e1f869ca5790f68c6a4b
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-08 06:59:18 -0100

    Implement a basic test case

--- com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/DefaultLocalSearchOptimiserTest.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/DefaultLocalSearchOptimiserTest.java
@@ -1,14 +1,114 @@
 package com.mmxlabs.optimiser.lso.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.junit.Test;
+
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.optimiser.IConstraintChecker;
+import com.mmxlabs.optimiser.IModifiableSequence;
+import com.mmxlabs.optimiser.IModifiableSequences;
+import com.mmxlabs.optimiser.IOptimisationContext;
+import com.mmxlabs.optimiser.IResource;
+import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.fitness.IFitnessComponent;
+import com.mmxlabs.optimiser.fitness.IFitnessCore;
+import com.mmxlabs.optimiser.fitness.impl.FitnessHelper;
+import com.mmxlabs.optimiser.impl.ModifiableSequences;
+import com.mmxlabs.optimiser.impl.NullSequencesManipulator;
+import com.mmxlabs.optimiser.lso.fitness.impl.SortingFitnessFactory;
 
 public class DefaultLocalSearchOptimiserTest {
 
 	@Test
 	public void testOptimise() {
+
+		// Init rng
+		Random random = new Random(1);
+
+		final int numberOfIterations = 100;
+		final double temperature = 5.0;
+
+		final SortingFitnessFactory factory = new SortingFitnessFactory();
+		final IFitnessCore<Integer> core = factory.instantiate();
+
+		final List<IFitnessComponent<Integer>> fitnessComponents = new ArrayList<IFitnessComponent<Integer>>(
+				1);
+		fitnessComponents.addAll(core.getFitnessComponents());
+
+		final FitnessHelper<Integer> fitnessHelper = new FitnessHelper<Integer>();
+		final LinearSimulatedAnnealingFitnessEvaluator<Integer> fitnessEvaluator = new LinearSimulatedAnnealingFitnessEvaluator<Integer>();
+		fitnessEvaluator.setFitnessHelper(fitnessHelper);
+
+		fitnessEvaluator.setTemperature(temperature);
+		fitnessEvaluator.setNumberOfIterations(numberOfIterations);
+		fitnessEvaluator.setFitnessComponents(fitnessComponents);
+
+		final Map<String, Double> weightsMap = CollectionsUtil.makeHashMap(
+				factory.getFitnessComponentNames().iterator().next(), 1.0);
+
+		fitnessEvaluator.setFitnessComponentWeights(weightsMap);
+
+		// Init part of LSO -- doesn't seem right as init also verifies things
+		// have been set correctly which would be best placed here. Perhaps we
+		// need init() and strart/begin(seq) methods
+		// fitnessEvaluator.init(initialSequences);
+
+		final RandomMoveGenerator<Integer> moveGenerator = new RandomMoveGenerator<Integer>();
+		moveGenerator.setRandom(random);
+
+		final DefaultLocalSearchOptimiser<Integer> lso = new DefaultLocalSearchOptimiser<Integer>();
+
+		final List<IConstraintChecker<Integer>> constraintCheckers = Collections
+				.emptyList();
+		lso.setConstraintCheckers(constraintCheckers);
+		lso.setNumberOfIterations(numberOfIterations);
+		lso.setSequenceManipulator(new NullSequencesManipulator<Integer>());
+		lso.setMoveGenerator(moveGenerator);
+		lso.setFitnessEvaluator(fitnessEvaluator);
+
+		IResource r1 = OptimiserTestUtil.makeResource();
+		IResource r2 = OptimiserTestUtil.makeResource();
+
+		final Map<IResource, IModifiableSequence<Integer>> map = CollectionsUtil
+				.makeHashMap(r1, OptimiserTestUtil.makeSequence(1, 3, 2, 4),
+						r2, OptimiserTestUtil.makeSequence(5, 8, 7, 6));
+
+		final IModifiableSequences<Integer> sequences = new ModifiableSequences<Integer>(
+				CollectionsUtil.makeArrayList(r1, r2), map);
+
+		// fail("THis is wrong iu think");
+		moveGenerator.setSequences(sequences);
+		final IOptimisationContext<Integer> context = new IOptimisationContext<Integer>() {
+
+			@Override
+			public ISequences<Integer> getInitialSequences() {
+				return sequences;
+			}
+
+			@Override
+			public List<IFitnessComponent<Integer>> getFitnessComponents() {
+				return fitnessComponents;
+			}
+		};
+
+		lso.optimise(context, null, null);
+
+		System.out.println(fitnessEvaluator.getBestFitness());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testOptimiseInit1() {
 		fail("Not yet implemented");
+
+		// Test all init failure combinations
+
 	}
 
 }
