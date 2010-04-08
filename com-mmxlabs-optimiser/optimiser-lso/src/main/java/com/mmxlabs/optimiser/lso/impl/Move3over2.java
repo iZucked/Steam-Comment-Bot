0d2b952cfd3d4dd53885cc372b9b5fc529424232
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-08 06:53:42 -0100

    Implemented Move3over2

--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move3over2.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/Move3over2.java
@@ -1,9 +1,13 @@
 package com.mmxlabs.optimiser.lso.impl;
 
 import java.util.Collection;
+import java.util.Map;
 
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.optimiser.IModifiableSequence;
 import com.mmxlabs.optimiser.IModifiableSequences;
 import com.mmxlabs.optimiser.IResource;
+import com.mmxlabs.optimiser.ISegment;
 import com.mmxlabs.optimiser.ISequence;
 import com.mmxlabs.optimiser.ISequences;
 import com.mmxlabs.optimiser.lso.IMove;
@@ -19,18 +23,133 @@ import com.mmxlabs.optimiser.lso.IMove;
  */
 public class Move3over2<T> implements IMove<T> {
 
+	private IResource resource1;
+
+	private IResource resource2;
+
+	private int resource1Start, resource1End;
+
+	private int resource2Position;
+
 	@Override
 	public void apply(final IModifiableSequences<T> sequences) {
-		throw new UnsupportedOperationException("Not yet implemented");
+
+		// Get sequences
+		final IModifiableSequence<T> sequence1 = sequences
+				.getModifiableSequence(resource1);
+		final IModifiableSequence<T> sequence2 = sequences
+				.getModifiableSequence(resource2);
+
+		// Get the moving segments
+		final ISegment<T> segment1 = sequence1.getSegment(resource1Start,
+				resource1End);
+
+		// Insert the segments in the other sequence
+		sequence2.insert(resource2Position, segment1);
+
+		// Remove segments from original sequences
+		sequence1.remove(segment1);
 	}
 
 	@Override
 	public Collection<IResource> getAffectedResources() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		return CollectionsUtil.makeArrayList(resource1, resource2);
 	}
 
 	@Override
 	public boolean validate(final ISequences<T> sequences) {
-		throw new UnsupportedOperationException("Not yet implemented");
+
+		// Validate basic parameters
+		if (resource1 == null) {
+			return false;
+		}
+
+		if (resource2 == null) {
+			return false;
+		}
+
+		if (resource1Start < 0) {
+			return false;
+		}
+		if (resource1End < resource1Start) {
+			return false;
+		}
+
+		if (resource2Position < 0) {
+			return false;
+		}
+
+		// Validate parameters against sequences object
+		final Map<IResource, ISequence<T>> sequenceMap = sequences
+				.getSequences();
+
+		// Make sure resources exist
+		if (sequenceMap.containsKey(resource1) == false) {
+			return false;
+		}
+		if (sequenceMap.containsKey(resource2) == false) {
+			return false;
+		}
+
+		// Make sure end is within range. This implies start would also be
+		// within range due to previous checks.
+		final ISequence<T> sequence1 = sequenceMap.get(resource1);
+		if (resource1End > sequence1.size()) {
+			return false;
+		}
+
+		final ISequence<T> sequence2 = sequenceMap.get(resource2);
+		if (resource2Position > sequence2.size()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public IResource getResource1() {
+		return resource1;
+	}
+
+	public void setResource1(final IResource resource1) {
+		this.resource1 = resource1;
+	}
+
+	public IResource getResource2() {
+		return resource2;
+	}
+
+	public void setResource2(final IResource resource2) {
+		this.resource2 = resource2;
+	}
+
+	public int getResource1Start() {
+		return resource1Start;
+	}
+
+	public void setResource1Start(final int resource1Start) {
+		this.resource1Start = resource1Start;
+	}
+
+	public int getResource1End() {
+		return resource1End;
+	}
+
+	public void setResource1End(final int resource1End) {
+		this.resource1End = resource1End;
+	}
+
+	public int getResource2Position() {
+		return resource2Position;
+	}
+
+	public void setResource2Position(final int resource2Position) {
+		this.resource2Position = resource2Position;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("r1 (%s) [%d -> %d], r2 (%s) [%d]",
+				getResource1(), getResource1Start(), getResource1End(),
+				getResource2(), getResource2Position());
 	}
 }
