490cb132329c2b0f3ec041144dab5e10cc756223
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-09 05:55:31 -0100

    Add missing RunWith annotation

--- com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/impl/NullSequencesManipulatorTest.java
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/impl/NullSequencesManipulatorTest.java
@@ -2,12 +2,14 @@ package com.mmxlabs.optimiser.impl;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.mmxlabs.optimiser.IModifiableSequences;
 
-
+@RunWith(JMock.class)
 public class NullSequencesManipulatorTest {
 
 	Mockery context = new JUnit4Mockery();
