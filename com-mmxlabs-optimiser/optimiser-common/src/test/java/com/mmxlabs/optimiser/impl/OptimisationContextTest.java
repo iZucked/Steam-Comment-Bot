947661be7cf5adf7a9bceb1e2e68449646642be2
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-14 06:12:13 -0100

    Added stub tests

---
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/impl/OptimisationContextTest.java
@@ -0,0 +1,24 @@
+package com.mmxlabs.optimiser.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OptimisationContextTest {
+
+	@Test
+	public void testOptimisationContext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFitnessComponents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInitialSequences() {
+		fail("Not yet implemented");
+	}
+
+}
