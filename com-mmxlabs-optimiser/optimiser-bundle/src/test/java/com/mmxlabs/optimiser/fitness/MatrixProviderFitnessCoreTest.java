d9e0df33083e59f488537af9efd302456a109978
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-14 16:11:05 -0100

    Adding stub tests

---
+++ com-mmxlabs-optimiser/optimiser-bundle/src/test/java/com/mmxlabs/optimiser/fitness/MatrixProviderFitnessCoreTest.java
@@ -0,0 +1,64 @@
+package com.mmxlabs.optimiser.fitness;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatrixProviderFitnessCoreTest {
+
+	@Test
+	public void testMatrixProviderFitnessCore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccepted() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvaluateISequencesOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvaluateISequencesOfTCollectionOfIResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFitnessComponents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScaleFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetScaleFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMatrix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNewFitness() {
+		fail("Not yet implemented");
+	}
+
+}
