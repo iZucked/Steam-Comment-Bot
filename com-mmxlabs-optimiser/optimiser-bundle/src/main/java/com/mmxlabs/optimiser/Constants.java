999e5812cb491740d9bc9fbc940aadf365cf390b
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-14 15:35:48 -0100

    Added a Constants class to store commonly used constants

---
+++ com-mmxlabs-optimiser/optimiser-bundle/src/main/java/com/mmxlabs/optimiser/Constants.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.optimiser;
+
+/**
+ * A (temporary?) class to store commonly used optimiser related constants. E.g.
+ * keys for different data providers.
+ * 
+ * @author Simon Goodall
+ * @noinstantiate
+ */
+public final class Constants {
+
+	public static final String DATA_PROVIDER_KEY_distance_matrix = "matrix-distance";
+}
