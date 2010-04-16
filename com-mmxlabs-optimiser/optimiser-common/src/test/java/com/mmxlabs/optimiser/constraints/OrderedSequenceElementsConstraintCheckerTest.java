8816ff7c23b8662a6723ebabfe81a369225733f3
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-16 06:16:21 -0100

    Constraint checker to enforce predefined sequences

---
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/constraints/OrderedSequenceElementsConstraintCheckerTest.java
@@ -0,0 +1,29 @@
+package com.mmxlabs.optimiser.constraints;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderedSequenceElementsConstraintCheckerTest {
+
+	@Test
+	public void testCheckConstraintsISequencesOfT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckConstraintsISequencesOfTListOfString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProvider() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProvider() {
+		fail("Not yet implemented");
+	}
+
+}
