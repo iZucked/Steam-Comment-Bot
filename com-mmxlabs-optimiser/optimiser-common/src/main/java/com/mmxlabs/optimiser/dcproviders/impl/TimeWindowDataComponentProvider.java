f4184b488214c799e9768ebc126449ce0089abca
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-21 16:43:32 -0100

    Split into provider and editor interface

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/dcproviders/impl/TimeWindowDataComponentProvider.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/dcproviders/impl/TimeWindowDataComponentProvider.java
@@ -5,28 +5,28 @@ import java.util.List;
 import java.util.Map;
 
 import com.mmxlabs.optimiser.components.ITimeWindow;
-import com.mmxlabs.optimiser.dcproviders.IElementTimeWindowDataComponentProvider;
+import com.mmxlabs.optimiser.dcproviders.ITimeWindowDataComponentProviderEditor;
 
 /**
- * Implementation of {@link IElementTimeWindowDataComponentProvider}.
+ * Implementation of {@link ITimeWindowDataComponentEditor}
  * 
  * @author Simon Goodall
  * 
  */
-public final class ElementTimeWindowDataComponentProvider implements
-		IElementTimeWindowDataComponentProvider {
+public final class TimeWindowDataComponentProvider implements
+		ITimeWindowDataComponentProviderEditor {
 
 	private final String name;
 
 	private final Map<Object, List<ITimeWindow>> timeWindowsMap;
 
-	public ElementTimeWindowDataComponentProvider(final String name) {
+	public TimeWindowDataComponentProvider(final String name) {
 		this.name = name;
 		this.timeWindowsMap = new HashMap<Object, List<ITimeWindow>>();
 	}
 
 	@Override
-	public List<ITimeWindow> getTimeWindows(Object element) {
+	public List<ITimeWindow> getTimeWindows(final Object element) {
 		if (timeWindowsMap.containsKey(element)) {
 			return timeWindowsMap.get(element);
 		}
@@ -34,12 +34,18 @@ public final class ElementTimeWindowDataComponentProvider implements
 	}
 
 	@Override
-	public void setTimeWindows(Object element, List<ITimeWindow> timeWindows) {
+	public void setTimeWindows(final Object element,
+			final List<ITimeWindow> timeWindows) {
 		this.timeWindowsMap.put(element, timeWindows);
 	}
 
 	@Override
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public void dispose() {
+		timeWindowsMap.clear();
 	}
 }
