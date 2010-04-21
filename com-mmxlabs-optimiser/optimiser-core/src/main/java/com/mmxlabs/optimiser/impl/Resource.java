76c39f62bd4f2c620d6042a6a91ffb994b47702f
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-21 16:31:18 -0100

    Added default IResource implementation

---
+++ com-mmxlabs-optimiser/optimiser-core/src/main/java/com/mmxlabs/optimiser/impl/Resource.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.optimiser.impl;
+
+import com.mmxlabs.optimiser.IResource;
+
+/**
+ * Default implementation of a {@link IResource}
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class Resource implements IResource {
+
+}
