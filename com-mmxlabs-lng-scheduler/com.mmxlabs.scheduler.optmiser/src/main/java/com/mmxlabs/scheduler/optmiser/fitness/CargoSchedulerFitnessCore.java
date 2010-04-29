b18d4d2639e45dd31edd6ea1811c65570079f660
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-29 16:21:06 -0100

    Added some stub implementation

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/fitness/CargoSchedulerFitnessCore.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/fitness/CargoSchedulerFitnessCore.java
@@ -1,6 +1,8 @@
 package com.mmxlabs.scheduler.optmiser.fitness;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import com.mmxlabs.optimiser.IResource;
 import com.mmxlabs.optimiser.ISequence;
@@ -9,6 +11,7 @@ import com.mmxlabs.optimiser.fitness.IFitnessComponent;
 import com.mmxlabs.optimiser.fitness.IFitnessCore;
 import com.mmxlabs.optimiser.scenario.IOptimisationData;
 import com.mmxlabs.optimiser.scenario.common.IMatrixProvider;
+import com.mmxlabs.scheduler.optmiser.builder.SchedulerConstants;
 import com.mmxlabs.scheduler.optmiser.components.IPort;
 import com.mmxlabs.scheduler.optmiser.providers.IPortProvider;
 
@@ -18,54 +21,67 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 
 	private IMatrixProvider<IPort, Number> portDistances;
 
+	private List<IFitnessComponent<T>> components;
+
+	public CargoSchedulerFitnessCore() {
+		components = new ArrayList<IFitnessComponent<T>>(1);
+		components
+				.add(new DistanceComponent<T>(
+						CargoSchedulerFitnessCoreFactory.DISTANCE_COMPONENT_NAME,
+						this));
+	}
+
 	@Override
-	public void accepted(ISequences<T> sequences,
-			Collection<IResource> affectedResources) {
+	public void accepted(final ISequences<T> sequences,
+			final Collection<IResource> affectedResources) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void evaluate(ISequences<T> sequences) {
+	public void evaluate(final ISequences<T> sequences) {
 
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void evaluate(ISequences<T> sequences,
-			Collection<IResource> affectedResources) {
+	public void evaluate(final ISequences<T> sequences,
+			final Collection<IResource> affectedResources) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public Collection<IFitnessComponent<T>> getFitnessComponents() {
-		// TODO Auto-generated method stub
-		return null;
+		return components;
 	}
 
 	@Override
-	public void init(IOptimisationData<T> data) {
+	public void init(final IOptimisationData<T> data) {
 
-		portProvider = data.getDataComponentProvider("port-provider",
-				IPortProvider.class);
-		portDistances = data.getDataComponentProvider("port-distances",
+		portProvider = data.getDataComponentProvider(
+				SchedulerConstants.DCP_portProvider, IPortProvider.class);
+		portDistances = data.getDataComponentProvider(
+				SchedulerConstants.DCP_portDistanceProvider,
 				IMatrixProvider.class);
+
+		// init data structures
 	}
 
-	private long evaluateSequence(IResource resource, ISequence<T> sequence) {
+	private long evaluateSequence(final IResource resource,
+			final ISequence<T> sequence) {
 		long distance = 0;
 
 		T prevElement = null;
 
-		for (T element : sequence) {
+		for (final T element : sequence) {
 
 			if (prevElement != null) {
-				IPort from = portProvider.getPortForElement(prevElement);
-				IPort to = portProvider.getPortForElement(element);
+				final IPort from = portProvider.getPortForElement(prevElement);
+				final IPort to = portProvider.getPortForElement(element);
 
-				Number dist = portDistances.get(from, to);
+				final Number dist = portDistances.get(from, to);
 				if (dist != null) {
 					distance += dist.longValue();
 				} else {
@@ -77,5 +93,10 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 		}
 
 		return distance;
+	}
+
+	public long getDistanceFitness() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
