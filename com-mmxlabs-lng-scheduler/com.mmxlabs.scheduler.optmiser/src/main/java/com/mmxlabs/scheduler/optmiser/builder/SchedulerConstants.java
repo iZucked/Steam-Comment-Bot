dcf0648ecd0f6ce5b08c79b9abe19cc4f3e35954
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-04-29 16:02:48 -0100

    Added new constant for vessels

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/builder/SchedulerConstants.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/builder/SchedulerConstants.java
@@ -2,6 +2,7 @@ package com.mmxlabs.scheduler.optmiser.builder;
 
 public class SchedulerConstants {
 
+	public static final String DCP_vesselProvider = "provider-vessels";
 	public static final String DCP_portProvider = "provider-ports";
 	public static final String DCP_sequenceElementProvider = "provider-sequence-elements";
 	public static final String DCP_portDistanceProvider = "provider-port-distances";
