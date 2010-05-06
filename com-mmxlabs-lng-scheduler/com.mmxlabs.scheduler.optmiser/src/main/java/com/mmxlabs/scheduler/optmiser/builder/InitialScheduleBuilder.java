d857c454756adb494e2d43ae8cff91d4647be410
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-05-06 05:40:43 -0100

    Make method public

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/builder/InitialScheduleBuilder.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/builder/InitialScheduleBuilder.java
@@ -5,9 +5,10 @@ import com.mmxlabs.optimiser.scenario.IOptimisationData;
 
 public class InitialScheduleBuilder {
 
-	<T> ISequences<T> generateRandomSchedule(IOptimisationData<T> data) {
+	public <T> ISequences<T> generateRandomSchedule(IOptimisationData<T> data) {
 		
 		// Extract list of cargoes
+		
 		
 		// randomly assign cargos to resources
 		
