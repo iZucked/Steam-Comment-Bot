7bcadc54beadccd8ec449cd6e601fbc5f2ffc7a0
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-05-14 06:04:02 -0100

    Hook up a scheduler

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/fitness/CargoSchedulerFitnessCore.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optmiser/fitness/CargoSchedulerFitnessCore.java
@@ -7,12 +7,16 @@ import java.util.List;
 import com.mmxlabs.optimiser.IResource;
 import com.mmxlabs.optimiser.ISequence;
 import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.components.ITimeWindow;
+import com.mmxlabs.optimiser.dcproviders.IElementDurationProvider;
+import com.mmxlabs.optimiser.dcproviders.ITimeWindowDataComponentProvider;
 import com.mmxlabs.optimiser.fitness.IFitnessComponent;
 import com.mmxlabs.optimiser.fitness.IFitnessCore;
 import com.mmxlabs.optimiser.scenario.IOptimisationData;
 import com.mmxlabs.optimiser.scenario.common.IMatrixProvider;
 import com.mmxlabs.scheduler.optmiser.SchedulerConstants;
 import com.mmxlabs.scheduler.optmiser.components.IPort;
+import com.mmxlabs.scheduler.optmiser.fitness.impl.SimpleSequenceScheduler;
 import com.mmxlabs.scheduler.optmiser.providers.IPortProvider;
 
 public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
@@ -21,7 +25,11 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 
 	private IMatrixProvider<IPort, Number> portDistances;
 
-	private List<IFitnessComponent<T>> components;
+	private ITimeWindowDataComponentProvider timeWindowProvider;
+	
+	private final List<IFitnessComponent<T>> components;
+
+	private IOptimisationData<T> data;
 
 	public CargoSchedulerFitnessCore() {
 		components = new ArrayList<IFitnessComponent<T>>(2);
@@ -47,6 +55,15 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 
 		// TODO Auto-generated method stub
 
+		final ISequenceScheduler scheduler = createSequenceScheduler();
+		for (final IResource resource : sequences.getResources()) {
+			final ISequence sequence = sequences.getSequence(resource);
+
+			scheduler.schedule(resource, sequence);
+
+			evaluateSequence(resource, sequence, scheduler);
+		}
+
 	}
 
 	@Override
@@ -54,6 +71,14 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 			final Collection<IResource> affectedResources) {
 		// TODO Auto-generated method stub
 
+		final ISequenceScheduler scheduler = createSequenceScheduler();
+		for (final IResource resource : affectedResources) {
+			final ISequence sequence = sequences.getSequence(resource);
+
+			scheduler.schedule(resource, sequence);
+
+			evaluateSequence(resource, sequence, scheduler);
+		}
 	}
 
 	@Override
@@ -64,6 +89,8 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 	@Override
 	public void init(final IOptimisationData<T> data) {
 
+		this.data = data;
+
 		portProvider = data.getDataComponentProvider(
 				SchedulerConstants.DCP_portProvider, IPortProvider.class);
 		portDistances = data.getDataComponentProvider(
@@ -73,30 +100,59 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 		// init data structures
 	}
 
-	private long evaluateSequence(final IResource resource,
-			final ISequence<T> sequence) {
-		long distance = 0;
+	private long evaluateSequenceDistance(final IResource resource,
+			final ISequence<T> sequence, final ISequenceScheduler<T> scheduler) {
 
-		T prevElement = null;
+		long distance = 0;
 
 		for (final T element : sequence) {
 
-			if (prevElement != null) {
-				final IPort from = portProvider.getPortForElement(prevElement);
-				final IPort to = portProvider.getPortForElement(element);
+			IJourneyElement e = scheduler.getAdditionalInformation(element,
+					SchedulerConstants.AI_journeyInfo, IJourneyElement.class);
+			if (e != null) {
+				distance += e.getDistance();
+			}
 
-				final Number dist = portDistances.get(from, to);
-				if (dist != null) {
-					distance += dist.longValue();
-				} else {
-					return Long.MAX_VALUE;
+		}
+		return distance;
+	}
+
+	private long evaluateSequenceLateness(final IResource resource,
+			final ISequence<T> sequence, final ISequenceScheduler<T> scheduler) {
+
+		long lateness = 0;
+
+		for (final T element : sequence) {
+
+			
+			IVisitElement e = scheduler.getAdditionalInformation(element,
+					SchedulerConstants.AI_visitInfo, IVisitElement.class);
+			assert e != null;
+			if (e != null) {
+				int arrival= e.getStartTime();
+				List<ITimeWindow> tws = timeWindowProvider.getTimeWindows(element);
+				for (ITimeWindow tw : tws) {
+					if (arrival > tw.getEnd()) {
+						lateness += arrival - tw.getEnd();
+					}
 				}
 			}
 
-			prevElement = element;
 		}
+		return lateness;
+	}
 
-		return distance;
+	
+	private void evaluateSequence(final IResource resource,
+			final ISequence<T> sequence, final ISequenceScheduler<T> scheduler) {
+
+		// TODO: Move these into the components themselves
+		long distanceFitness = evaluateSequenceDistance(resource, sequence, scheduler);
+		long latenessFitness = evaluateSequenceLateness(resource, sequence, scheduler);
+		
+		// TODO: Do something with this data
+		
+		
 	}
 
 	public long getDistanceFitness() {
@@ -107,5 +163,33 @@ public class CargoSchedulerFitnessCore<T> implements IFitnessCore<T> {
 	public long getLatenessFitness() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	ISequenceScheduler createSequenceScheduler() {
+		final SimpleSequenceScheduler scheduler = new SimpleSequenceScheduler();
+
+		scheduler.setDistanceProvider(data.getDataComponentProvider(
+				SchedulerConstants.DCP_portDistanceProvider,
+				IMatrixProvider.class));
+		scheduler.setDurationsProvider(data.getDataComponentProvider(
+				SchedulerConstants.DCP_elementDurationsProvider,
+				IElementDurationProvider.class));
+		scheduler.setPortProvider(data.getDataComponentProvider(
+				SchedulerConstants.DCP_portProvider, IPortProvider.class));
+		scheduler.setTimeWindowProvider(data.getDataComponentProvider(
+				SchedulerConstants.DCP_timeWindowProvider,
+				ITimeWindowDataComponentProvider.class));
+
+		scheduler.init();
+
+		return scheduler;
+	}
+
+	@Override
+	public void dispose() {
+
+		this.data = null;
+		this.portDistances = null;
+		this.portProvider = null;
 	}
 }
