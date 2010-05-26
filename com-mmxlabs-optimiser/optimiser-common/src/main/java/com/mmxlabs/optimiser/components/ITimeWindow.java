9ff2322ef6850b16539bcfb7911f752f7382d6d6
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-05-26 06:54:57 -0100

    Doc

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/components/ITimeWindow.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/components/ITimeWindow.java
@@ -1,8 +1,16 @@
 package com.mmxlabs.optimiser.components;
 
+/**
+ * Interface defining a window of time bounded by {@link #getStart()} and
+ * {@link #getEnd()}.
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public interface ITimeWindow {
 
 	int getStart();
 
+	// TODO: Should the end be inclusive or exclusive?
 	int getEnd();
 }
