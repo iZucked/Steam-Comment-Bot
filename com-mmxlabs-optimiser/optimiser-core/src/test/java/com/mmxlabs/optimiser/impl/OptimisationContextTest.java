7e0cc6e7dc843831a4cf938cd6c72dcc39281642
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-02 16:08:58 -0100

    Implement test

--- com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/impl/OptimisationContextTest.java
+++ com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/impl/OptimisationContextTest.java
@@ -1,24 +1,58 @@
 package com.mmxlabs.optimiser.impl;
 
-import static org.junit.Assert.*;
+import java.util.Collections;
+import java.util.List;
 
+import junit.framework.Assert;
+
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.mmxlabs.optimiser.ISequences;
+import com.mmxlabs.optimiser.constraints.IConstraintChecker;
+import com.mmxlabs.optimiser.constraints.IConstraintCheckerRegistry;
+import com.mmxlabs.optimiser.fitness.IFitnessComponent;
+import com.mmxlabs.optimiser.fitness.IFitnessFunctionRegistry;
+import com.mmxlabs.optimiser.scenario.IOptimisationData;
+
+@RunWith(JMock.class)
 public class OptimisationContextTest {
 
+	Mockery context = new JUnit4Mockery();
+
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testOptimisationContext() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testGetFitnessComponents() {
-		fail("Not yet implemented");
-	}
+		final IOptimisationData optimisationData = context
+				.mock(IOptimisationData.class);
+		final ISequences initialSequences = context.mock(ISequences.class);
 
-	@Test
-	public void testGetInitialSequences() {
-		fail("Not yet implemented");
-	}
+		final List<IFitnessComponent> fitnessComponents = Collections
+				.emptyList();
+		final IFitnessFunctionRegistry fitnessFunctionRegistry = context
+				.mock(IFitnessFunctionRegistry.class);
+		final List<IConstraintChecker> constraintCheckers = Collections
+				.emptyList();
+		final IConstraintCheckerRegistry constraintCheckerRegistry = context
+				.mock(IConstraintCheckerRegistry.class);
 
+		final OptimisationContext optContext = new OptimisationContext(
+				optimisationData, initialSequences, fitnessComponents,
+				fitnessFunctionRegistry, constraintCheckers,
+				constraintCheckerRegistry);
+
+		Assert.assertSame(optimisationData, optContext.getOptimisationData());
+		Assert.assertSame(initialSequences, optContext.getInitialSequences());
+		Assert.assertSame(fitnessComponents, optContext.getFitnessComponents());
+		Assert.assertSame(fitnessFunctionRegistry, optContext
+				.getFitnessFunctionRegistry());
+		Assert.assertSame(constraintCheckers, optContext
+				.getConstraintCheckers());
+		Assert.assertSame(constraintCheckerRegistry, optContext
+				.getConstraintCheckerRegistry());
+	}
 }
