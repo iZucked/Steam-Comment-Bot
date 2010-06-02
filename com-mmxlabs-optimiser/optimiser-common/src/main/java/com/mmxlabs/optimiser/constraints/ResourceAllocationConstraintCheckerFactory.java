30e728499ed6777e34934cf63c6fe2c3ced4299d
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-02 16:23:45 -0100

    Add factory to create ResourceAllocationConstraintCheckers

---
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/constraints/ResourceAllocationConstraintCheckerFactory.java
@@ -0,0 +1,31 @@
+package com.mmxlabs.optimiser.constraints;
+
+/**
+ * {@link IConstraintCheckerFactory} implementation to construct
+ * {@link ResourceAllocationConstraintChecker} instances.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class ResourceAllocationConstraintCheckerFactory implements
+		IConstraintCheckerFactory {
+
+	public static final String NAME = "ResourceAllocationConstraintChecker";
+
+	private final String key;
+
+	public ResourceAllocationConstraintCheckerFactory(final String key) {
+		this.key = key;
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public <T> IConstraintChecker<T> instantiate() {
+		return new ResourceAllocationConstraintChecker<T>(NAME, key);
+	}
+
+}
