6319784ae9eb330b0e0860da64892953ffa0456a
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-03 15:56:44 -0100

    Implement tests

--- com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/dcproviders/impl/ResourceAllocationConstraintProviderTest.java
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/dcproviders/impl/ResourceAllocationConstraintProviderTest.java
@@ -1,29 +1,60 @@
 package com.mmxlabs.optimiser.dcproviders.impl;
 
-import static org.junit.Assert.*;
+import java.util.Collection;
+import java.util.Collections;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.mmxlabs.optimiser.IResource;
 
 public class ResourceAllocationConstraintProviderTest {
 
 	@Test
 	public void testResourceAllocationConstraintProvider() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
+		final String name = "name";
+		final ResourceAllocationConstraintProvider provider = new ResourceAllocationConstraintProvider(
+				name);
+		Assert.assertSame(name, provider.getName());
 	}
 
 	@Test
 	public void testGetAllowedResources() {
-		fail("Not yet implemented");
+		final ResourceAllocationConstraintProvider provider = new ResourceAllocationConstraintProvider(
+				"name");
+
+		final Object obj1 = new Object();
+
+		Assert.assertNull(provider.getAllowedResources(obj1));
+
+		final Collection<IResource> resources = Collections.emptyList();
+
+		provider.setAllowedResources(obj1, resources);
+
+		Assert.assertSame(resources, provider.getAllowedResources(obj1));
+
 	}
 
 	@Test
-	public void testSetAllowedResources() {
-		fail("Not yet implemented");
+	public void testDispose() {
+
+		final ResourceAllocationConstraintProvider provider = new ResourceAllocationConstraintProvider(
+				"name");
+		final Object obj1 = new Object();
+
+		Assert.assertNull(provider.getAllowedResources(obj1));
+
+		final Collection<IResource> resources = Collections.emptyList();
+
+		provider.setAllowedResources(obj1, resources);
+
+		Assert.assertSame(resources, provider.getAllowedResources(obj1));
+
+		provider.dispose();
+
+		Assert.assertNull(provider.getAllowedResources(obj1));
+
 	}
 
 }
