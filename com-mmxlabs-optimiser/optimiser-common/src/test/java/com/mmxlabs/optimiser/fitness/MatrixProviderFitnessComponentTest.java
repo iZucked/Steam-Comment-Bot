7d1c58586d9d6f9aa9d8d9135e803f24912f7148
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-04 16:42:03 -0100

    Implement tests

--- com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/fitness/MatrixProviderFitnessComponentTest.java
+++ com-mmxlabs-optimiser/optimiser-common/src/test/java/com/mmxlabs/optimiser/fitness/MatrixProviderFitnessComponentTest.java
@@ -3,25 +3,20 @@ package com.mmxlabs.optimiser.fitness;
 import static org.junit.Assert.fail;
 import junit.framework.Assert;
 
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
-import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(JMock.class)
 class MatrixProviderFitnessComponentTest {
 
-	
-	Mockery context = new JUnit4Mockery();
 	@Test
 	public void testMatrixProviderFitnessComponent() {
-		
+
 		String name = "name";
 		String key = "key";
-		MatrixProviderFitnessCore<Object>core = new MatrixProviderFitnessCore<Object>(name, key );
-		MatrixProviderFitnessComponent<Object> component = new MatrixProviderFitnessComponent<Object>(name, core);
-		
+		MatrixProviderFitnessCore<Object> core = new MatrixProviderFitnessCore<Object>(
+				name, key);
+		MatrixProviderFitnessComponent<Object> component = new MatrixProviderFitnessComponent<Object>(
+				name, core);
+
 		Assert.assertSame(name, component.getName());
 		Assert.assertSame(core, component.getFitnessCore());
 	}
@@ -29,5 +24,6 @@ class MatrixProviderFitnessComponentTest {
 	@Test
 	public void testGetFitness() {
 		fail("Not yet implemented");
+		// TODO: Ideally jmock would be used to ensure we call the right method on the core, but it cannot instantiate our class
 	}
 }
