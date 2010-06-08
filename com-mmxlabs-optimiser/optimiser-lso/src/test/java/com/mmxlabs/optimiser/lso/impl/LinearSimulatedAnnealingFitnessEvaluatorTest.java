080ba18aa034b8d9bcf11a7dcbfa02f4c399262e
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-08 06:50:59 -0100

    Implement basic tests

--- com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/LinearSimulatedAnnealingFitnessEvaluatorTest.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/LinearSimulatedAnnealingFitnessEvaluatorTest.java
@@ -1,63 +1,191 @@
 package com.mmxlabs.optimiser.lso.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.mmxlabs.optimiser.fitness.IFitnessComponent;
+import com.mmxlabs.optimiser.fitness.IFitnessHelper;
+
+@RunWith(JMock.class)
 public class LinearSimulatedAnnealingFitnessEvaluatorTest {
 
+	Mockery context = new JUnit4Mockery();
+
 	@Test
-	public void testCheckSequences() {
-		fail("Not yet implemented");
+	public void testGetSetFitnessComponents() {
+
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+		Assert.assertNull(evaluator.getFitnessComponents());
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+		Assert.assertSame(fitnessComponents, evaluator.getFitnessComponents());
+	}
+
+	@Test
+	public void testGetSetFitnessHelper() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+		Assert.assertNull(evaluator.getFitnessHelper());
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+		Assert.assertSame(fitnessHelper, evaluator.getFitnessHelper());
+	}
+
+	@Test
+	public void testGetSetTemperature() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+		Assert.assertEquals(Double.MAX_VALUE, evaluator.getTemperature(), 0.0);
+		evaluator.setTemperature(10);
+		Assert.assertEquals(10.0, evaluator.getTemperature(), 0.0);
+
+	}
+
+	@Test
+	public void testGetSetFitnessComponentWeights() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+		Assert.assertNull(evaluator.getFitnessComponentWeights());
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+		Assert.assertSame(fitnessWeights, evaluator
+				.getFitnessComponentWeights());
+	}
+
+	@Test
+	public void testGetSetNumberOfIterations() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+		Assert.assertEquals(Integer.MAX_VALUE, evaluator
+				.getNumberOfIterations());
+		evaluator.setNumberOfIterations(10);
+		Assert.assertEquals(10, evaluator.getNumberOfIterations());
+
 	}
 
 	@Test
 	public void testInit() {
-		fail("Not yet implemented");
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+
+		evaluator.setNumberOfIterations(10);
+		evaluator.setTemperature(10);
+
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+
+		evaluator.init();
+
 	}
 
-	@Test
-	public void testSetFitnessComponents() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalStateException.class)
+	public void testInit2() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+
+		evaluator.setNumberOfIterations(10);
+		evaluator.setTemperature(10);
+
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+
+		evaluator.init();
+
 	}
 
-	@Test
-	public void testGetFitnessComponents() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalStateException.class)
+	public void testInit3() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+
+		evaluator.setNumberOfIterations(10);
+		evaluator.setTemperature(10);
+
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+
+		evaluator.init();
+
 	}
 
-	@Test
-	public void testGetFitnessHelper() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalStateException.class)
+	public void testInit4() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+
+		evaluator.setTemperature(10);
+
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+
+		evaluator.init();
+
 	}
 
-	@Test
-	public void testSetFitnessHelper() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalStateException.class)
+	public void testInit5() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+
+		evaluator.setNumberOfIterations(10);
+
+		@SuppressWarnings("unchecked")
+		IFitnessHelper<Object> fitnessHelper = context.mock(IFitnessHelper.class);
+		evaluator.setFitnessHelper(fitnessHelper);
+
+		evaluator.init();
+
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testInit6() {
+		LinearSimulatedAnnealingFitnessEvaluator<Object> evaluator = new LinearSimulatedAnnealingFitnessEvaluator<Object>();
+
+		List<IFitnessComponent<Object>> fitnessComponents = Collections.emptyList();
+		evaluator.setFitnessComponents(fitnessComponents);
+
+		Map<String, Double> fitnessWeights = Collections.emptyMap();
+		evaluator.setFitnessComponentWeights(fitnessWeights);
+
+		evaluator.setNumberOfIterations(10);
+		evaluator.setTemperature(10);
+
+		evaluator.init();
+
 	}
 
 	@Test
 	public void testGetBestSequences() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetTemperature() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTemperature() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetFitnessComponentWeights() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFitnessComponentWeights() {
 		fail("Not yet implemented");
 	}
 
@@ -67,17 +195,12 @@ public class LinearSimulatedAnnealingFitnessEvaluatorTest {
 	}
 
 	@Test
-	public void testSetNumberOfIterations() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetNumberOfIterations() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetCurrentSequences() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckSequences() {
 		fail("Not yet implemented");
 	}
 
