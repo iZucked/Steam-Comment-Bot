a4d1aa75977865a534c171cbf33daeea3659dbf9
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-08 16:19:18 -0100

    Move the thresholding of LSO into separate classes

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/IThresholder.java
@@ -0,0 +1,30 @@
+package com.mmxlabs.optimiser.lso;
+
+import com.mmxlabs.optimiser.fitness.IFitnessEvaluator;
+
+/**
+ * {@link IThresholder} implementations are used by {@link IFitnessEvaluator}s
+ * to determine whether a single delta value is acceptable.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public interface IThresholder {
+
+	/**
+	 * Prepare {@link IThresholder} for use.
+	 */
+	void init();
+
+	/**
+	 * Return true if the given delta value is acceptable.
+	 * @param delta
+	 * @return
+	 */
+	boolean accept(long delta);
+
+	/**
+	 * Step {@link IThresholder} for the next iteration.
+	 */
+	void step();
+}
---
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/StepThresholder.java
@@ -0,0 +1,66 @@
+package com.mmxlabs.optimiser.lso.impl;
+
+import com.mmxlabs.optimiser.lso.IThresholder;
+
+/**
+ * Simple {@link IThresholder} implementation which reduces an initial threshold
+ * by a fixed amount each step.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class StepThresholder implements IThresholder {
+
+	private int stepSize;
+
+	private long initialThreshold;
+
+	private long currentThreshold;
+
+	public StepThresholder() {
+
+	}
+
+	public StepThresholder(int stepSize, long initialThreshold) {
+		this.stepSize = stepSize;
+		this.initialThreshold = initialThreshold;
+		this.currentThreshold = initialThreshold;
+	}
+
+	public final int getStepSize() {
+		return stepSize;
+	}
+
+	public final void setStepSize(int stepSize) {
+		this.stepSize = stepSize;
+	}
+
+	public final long getInitialThreshold() {
+		return initialThreshold;
+	}
+
+	public final void setInitialThreshold(long initialThreshold) {
+		this.initialThreshold = initialThreshold;
+	}
+
+	public final long getCurrentThreshold() {
+		return currentThreshold;
+	}
+
+	@Override
+	public boolean accept(long delta) {
+
+		return (delta < currentThreshold);
+	}
+
+	@Override
+	public void init() {
+		currentThreshold = initialThreshold;
+	}
+
+	@Override
+	public void step() {
+		currentThreshold -= stepSize;
+	}
+
+}
