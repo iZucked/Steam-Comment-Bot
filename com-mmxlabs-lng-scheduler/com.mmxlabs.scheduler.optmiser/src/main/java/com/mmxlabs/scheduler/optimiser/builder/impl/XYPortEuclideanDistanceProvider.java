1ac6b1a54dee8fc99237753edca09396eec5a754
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-10 15:51:53 -0100

    Doc

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/builder/impl/XYPortEuclideanDistanceProvider.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/builder/impl/XYPortEuclideanDistanceProvider.java
@@ -3,12 +3,20 @@ package com.mmxlabs.scheduler.optimiser.builder.impl;
 import com.mmxlabs.scheduler.optimiser.builder.IXYPortDistanceProvider;
 import com.mmxlabs.scheduler.optimiser.components.IXYPort;
 
-public final class XYPortEuclideanDistanceProvider implements IXYPortDistanceProvider {
+/**
+ * {@link IXYPortDistanceProvider} implementation calculating the Euclidean
+ * distance between the X/Y co-ordinates of two ports.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class XYPortEuclideanDistanceProvider implements
+		IXYPortDistanceProvider {
 
-	public double getDistance(IXYPort from, IXYPort to) {
+	public double getDistance(final IXYPort from, final IXYPort to) {
 
-		float diffX = from.getX() - to.getX();
-		float diffY = from.getY() - to.getY();
+		final float diffX = from.getX() - to.getX();
+		final float diffY = from.getY() - to.getY();
 
 		return Math.sqrt((diffX * diffX) + (diffY * diffY));
 	}
