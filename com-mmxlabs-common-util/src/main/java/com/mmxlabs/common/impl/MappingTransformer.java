119b2404ebfdc9a3d426ca8d76ec92985834c517
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-16 06:05:08 -0100

    ITransformer implementation using a HashMap as the backing implementation

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/impl/MappingTransformer.java
@@ -0,0 +1,38 @@
+package com.mmxlabs.common.impl;
+
+import java.util.Map;
+
+import com.mmxlabs.common.ITransformer;
+
+/**
+ * {@link ITransformer} implementation which uses a {@link Map} to transform a
+ * known set of types.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class MappingTransformer<T, U> implements ITransformer<T, U> {
+
+	private final Map<T, U> mapping;
+
+	public MappingTransformer(final Map<T, U> mapping) {
+		this.mapping = mapping;
+	}
+
+	@Override
+	public U transform(T t) {
+		if (mapping.containsKey(t)) {
+			return mapping.get(t);
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the {@link Map} used in this {@link MappingTransformer}.
+	 * 
+	 * @return
+	 */
+	public Map<T, U> getMapping() {
+		return mapping;
+	}
+}
