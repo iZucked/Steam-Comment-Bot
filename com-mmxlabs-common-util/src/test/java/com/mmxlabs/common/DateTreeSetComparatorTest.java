24cd0ca19775b3b351c26a092246440238ef590c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-16 06:06:00 -0100

    Implement tests

--- com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/DateTreeSetComparatorTest.java
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/DateTreeSetComparatorTest.java
@@ -1,24 +1,60 @@
 package com.mmxlabs.common;
 
-import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DateTreeSetComparatorTest {
 
 	@Test
 	public void testDateTreeSetComparator() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testCompareObjectObject() {
-		fail("Not yet implemented");
-	}
+		final Date d1 = Calendar.getInstance().getTime();
 
-	@Test
-	public void testCompareDateDate() {
-		fail("Not yet implemented");
-	}
+		final Object obj1 = new Object();
+		final Object obj2 = new Object();
 
+		final Date d2 = Calendar.getInstance().getTime();
+
+		final Map<Object, Date> datemap = new HashMap<Object, Date>();
+		datemap.put(obj1, d1);
+		datemap.put(obj2, d2);
+
+		final ITransformer<Object, Date> transformer = new ITransformer<Object, Date>() {
+
+			@Override
+			public Date transform(final Object t) {
+				return datemap.get(t);
+			}
+		};
+
+		final DateTreeSetComparator<Object> cmp = new DateTreeSetComparator<Object>(
+				transformer);
+
+		//
+		Assert.assertEquals(cmp.compare(d1, d2), cmp.compare(obj1, obj2));
+		Assert.assertEquals(cmp.compare(d1, obj2), cmp.compare(obj1, d2));
+		Assert.assertEquals(cmp.compare(d2, d1), cmp.compare(obj2, obj1));
+		Assert.assertEquals(cmp.compare(d2, obj1), cmp.compare(obj2, d1));
+		
+		
+		//
+		Assert.assertEquals(cmp.compare(d1, d2), -cmp.compare(d2, d1));
+		Assert.assertEquals(cmp.compare(d1, obj1), -cmp.compare(obj1, d1));
+		Assert.assertEquals(cmp.compare(d1, obj2), -cmp.compare(obj2, d1));
+		//....
+		
+		// Test equality
+		Assert.assertEquals(0, cmp.compare(d1, d1));
+		Assert.assertEquals(0, cmp.compare(d2, d2));
+		Assert.assertEquals(0, cmp.compare(obj1, obj1));
+		Assert.assertEquals(0, cmp.compare(obj2, obj2));
+		
+		Assert.assertEquals(0, cmp.compare(d1, obj1));
+		Assert.assertEquals(0, cmp.compare(d2, obj2));
+	}
 }
