30711653f78e5506b978913251048a18de82f33e
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-16 05:47:09 -0100

    Implement tests

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/providers/impl/HashMapVesselEditorTest.java
@@ -0,0 +1,61 @@
+package com.mmxlabs.scheduler.optimiser.providers.impl;
+
+import static org.junit.Assert.fail;
+
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.mmxlabs.optimiser.IResource;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+
+@RunWith(JMock.class)
+public class HashMapVesselEditorTest {
+
+	Mockery context = new JUnit4Mockery();
+
+	@Test
+	public void testHashMapVesselEditor() {
+		final String name = "name";
+		final HashMapVesselEditor editor = new HashMapVesselEditor(name);
+		Assert.assertSame(name, editor.getName());
+
+		final IResource resource1 = context.mock(IResource.class, "resource-1");
+		final IVessel vessel1 = context.mock(IVessel.class, "vessel-1");
+
+		Assert.assertNull(editor.getResource(vessel1));
+		Assert.assertNull(editor.getVessel(resource1));
+
+		editor.setVesselResource(resource1, vessel1);
+
+		Assert.assertSame(vessel1, editor.getVessel(resource1));
+		Assert.assertSame(resource1, editor.getResource(vessel1));
+	}
+
+	@Test
+	public void testDispose() {
+		final String name = "name";
+		final HashMapVesselEditor editor = new HashMapVesselEditor(name);
+
+		final IResource resource1 = context.mock(IResource.class, "resource-1");
+		final IVessel vessel1 = context.mock(IVessel.class, "vessel-1");
+
+		Assert.assertNull(editor.getResource(vessel1));
+		Assert.assertNull(editor.getVessel(resource1));
+
+		editor.setVesselResource(resource1, vessel1);
+
+		Assert.assertSame(vessel1, editor.getVessel(resource1));
+		Assert.assertSame(resource1, editor.getResource(vessel1));
+
+		editor.dispose();
+
+		Assert.assertNull(editor.getResource(vessel1));
+		Assert.assertNull(editor.getVessel(resource1));
+
+	}
+
+}
