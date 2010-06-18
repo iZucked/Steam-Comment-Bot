243ab1e5bc6fce525d8e0954ee2d61cbde895f17
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-18 07:06:26 -0100

    Consumption rate calculator implementation which generates a lookup table for speed/rate pairs

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/LookupTableConsumptionRateCalculator.java
@@ -0,0 +1,57 @@
+package com.mmxlabs.scheduler.optimiser.components.impl;
+
+import com.mmxlabs.scheduler.optimiser.components.IConsumptionRateCalculator;
+
+/**
+ * {@link IConsumptionRateCalculator} implementation which takes an existing
+ * {@link IConsumptionRateCalculator} and a min/max bounds to generate a lookup
+ * table of speed to rate pairs. Note if the range between min and max bounds is
+ * large, then the lookup table will also be large.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class LookupTableConsumptionRateCalculator implements
+		IConsumptionRateCalculator {
+
+	private final int minSpeed;
+
+	private final int maxSpeed;
+
+	private long[] table;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param minSpeed
+	 *            Inclusive min speed
+	 * @param maxSpeed
+	 *            Inclusive max speed
+	 * @param calc
+	 */
+	public LookupTableConsumptionRateCalculator(final int minSpeed,
+			final int maxSpeed, final IConsumptionRateCalculator calc) {
+		this.minSpeed = minSpeed;
+		this.maxSpeed = maxSpeed;
+
+		init(calc);
+	}
+
+	@Override
+	public long getRate(final int speed) {
+		return table[speed - minSpeed];
+	}
+
+	private void init(final IConsumptionRateCalculator calc) {
+		final int s = maxSpeed - minSpeed + 1;
+
+		table = new long[s];
+
+		for (int i = 0; i < table.length; ++i) {
+
+			final int speed = minSpeed + i;
+			final long rate = calc.getRate(speed);
+			table[i] = rate;
+		}
+	}
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/components/impl/LookupTableConsumptionRateCalculatorTest.java
@@ -0,0 +1,102 @@
+package com.mmxlabs.scheduler.optimiser.components.impl;
+
+import junit.framework.Assert;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.mmxlabs.scheduler.optimiser.components.IConsumptionRateCalculator;
+
+@RunWith(JMock.class)
+public class LookupTableConsumptionRateCalculatorTest {
+
+	Mockery context = new JUnit4Mockery();
+
+	@Test
+	public void testLookupTableConsumptionRateCalculator() {
+		final IConsumptionRateCalculator calc = context
+				.mock(IConsumptionRateCalculator.class);
+
+		context.setDefaultResultForType(long.class, 100l);
+
+		context.checking(new Expectations() {
+			{
+				one(calc).getRate(5);
+				one(calc).getRate(6);
+				one(calc).getRate(7);
+				one(calc).getRate(8);
+				one(calc).getRate(9);
+				one(calc).getRate(10);
+			}
+		});
+
+		final LookupTableConsumptionRateCalculator lookupCalc = new LookupTableConsumptionRateCalculator(
+				5, 10, calc);
+
+		Assert.assertEquals(100l,lookupCalc.getRate(5));
+		Assert.assertEquals(100l,lookupCalc.getRate(6));
+		Assert.assertEquals(100l,lookupCalc.getRate(7));
+		Assert.assertEquals(100l,lookupCalc.getRate(8));
+		Assert.assertEquals(100l,lookupCalc.getRate(9));
+		Assert.assertEquals(100l,lookupCalc.getRate(10));
+		
+		context.assertIsSatisfied();
+
+	}
+
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void testLookupTableConsumptionRateCalculator2() {
+		final IConsumptionRateCalculator calc = context
+				.mock(IConsumptionRateCalculator.class);
+
+		context.setDefaultResultForType(long.class, 100l);
+
+		context.checking(new Expectations() {
+			{
+				one(calc).getRate(5);
+				one(calc).getRate(6);
+				one(calc).getRate(7);
+				one(calc).getRate(8);
+				one(calc).getRate(9);
+				one(calc).getRate(10);
+			}
+		});
+
+		final LookupTableConsumptionRateCalculator lookupCalc = new LookupTableConsumptionRateCalculator(
+				5, 10, calc);
+
+		context.assertIsSatisfied();
+		lookupCalc.getRate(4);
+		
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void testLookupTableConsumptionRateCalculator3() {
+		final IConsumptionRateCalculator calc = context
+				.mock(IConsumptionRateCalculator.class);
+
+		context.setDefaultResultForType(long.class, 100l);
+
+		context.checking(new Expectations() {
+			{
+				one(calc).getRate(5);
+				one(calc).getRate(6);
+				one(calc).getRate(7);
+				one(calc).getRate(8);
+				one(calc).getRate(9);
+				one(calc).getRate(10);
+			}
+		});
+
+		final LookupTableConsumptionRateCalculator lookupCalc = new LookupTableConsumptionRateCalculator(
+				5, 10, calc);
+
+		context.assertIsSatisfied();
+		lookupCalc.getRate(11);
+		
+	}
+}
