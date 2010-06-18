b284d98b382e57c29394e4bb153d08306960d4a8
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-06-18 06:59:41 -0100

    Implement a HashMap based Caching consumption rate calculator

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/CachingConsumptionRateCalculator.java
@@ -0,0 +1,46 @@
+package com.mmxlabs.scheduler.optimiser.components.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mmxlabs.scheduler.optimiser.components.IConsumptionRateCalculator;
+
+/**
+ * Implementation of {@link IConsumptionRateCalculator} which wraps another
+ * {@link IConsumptionRateCalculator} instance and caches the result in a
+ * {@link HashMap} for future lookups.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class CachingConsumptionRateCalculator implements
+		IConsumptionRateCalculator {
+
+	private final IConsumptionRateCalculator calc;
+
+	/**
+	 * Cache of speed to rate pairs.
+	 */
+	private final Map<Integer, Long> cache = new HashMap<Integer, Long>();
+
+	public CachingConsumptionRateCalculator(IConsumptionRateCalculator calc) {
+		this.calc = calc;
+	}
+
+	@Override
+	public long getRate(int speed) {
+
+		// Check cache first,
+		if (cache.containsKey(speed)) {
+			return cache.get(speed);
+		}
+
+		// Fallback to real calculator
+		long rate = calc.getRate(speed);
+
+		// Cache the value
+		cache.put(speed, rate);
+
+		return rate;
+	}
+}
