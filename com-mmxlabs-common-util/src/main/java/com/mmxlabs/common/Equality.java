d969783cb33de4ed7d4180020a15525c364af9ee
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-02 05:28:10 -0100

    Add util class to hepl with equality tests

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Equality.java
@@ -0,0 +1,37 @@
+package com.mmxlabs.common;
+
+/**
+ * Utility class to help with equality checks.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class Equality {
+
+	/**
+	 * Compare two object using their {@link #equals(Object)} method. However
+	 * check for <code>null</code>. Returns true if both object are null or
+	 * {@link #equals(Object)} return true.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static boolean isEqual(final Object a, final Object b) {
+
+		if (a == null && b == null) {
+			return true;
+		}
+
+		if (a == null && b != null) {
+			return false;
+		}
+
+		if (a != null && b == null) {
+			return false;
+		}
+
+		// Null check over, fall back to proper equals method
+		return a.equals(b);
+	}
+}
---
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/EqualityTest.java
@@ -0,0 +1,23 @@
+package com.mmxlabs.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EqualityTest {
+
+	@Test
+	public void testIsEqual() {
+
+		final Object a = new Object();
+		final Object b = new Object();
+
+		Assert.assertTrue(Equality.isEqual(null, null));
+		Assert.assertFalse(Equality.isEqual(a, null));
+		Assert.assertFalse(Equality.isEqual(null, a));
+		Assert.assertFalse(Equality.isEqual(a, b));
+
+		Assert.assertTrue(Equality.isEqual(a, a));
+		Assert.assertTrue(Equality.isEqual(b, b));
+	}
+
+}
