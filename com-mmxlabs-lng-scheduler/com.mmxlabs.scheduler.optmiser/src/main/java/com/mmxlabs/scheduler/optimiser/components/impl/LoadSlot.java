47bd44300c31b0a145671d4b460f05bce08d9f3c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-02 06:08:19 -0100

    Implement equals method

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/LoadSlot.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/LoadSlot.java
@@ -1,6 +1,8 @@
 package com.mmxlabs.scheduler.optimiser.components.impl;
 
+import com.mmxlabs.optimiser.components.ITimeWindow;
 import com.mmxlabs.scheduler.optimiser.components.ILoadSlot;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
 
 /**
  * Default implementation of {@link ILoadSlot}.
@@ -15,6 +17,19 @@ public final class LoadSlot extends PortSlot implements ILoadSlot {
 	private long maxLoadVolume;
 
 	private long purchasePrice;
+
+	public LoadSlot() {
+
+	}
+
+	public LoadSlot(final String id, final IPort port,
+			final ITimeWindow timwWindow, final long minLoadVolume,
+			final long maxLoadVolume, final long purchasePrice) {
+		super(id, port, timwWindow);
+		this.minLoadVolume = minLoadVolume;
+		this.maxLoadVolume = maxLoadVolume;
+		this.purchasePrice = purchasePrice;
+	}
 
 	@Override
 	public long getMinLoadVolume() {
@@ -42,4 +57,26 @@ public final class LoadSlot extends PortSlot implements ILoadSlot {
 	public void setPurchasePrice(final long purchasePrice) {
 		this.purchasePrice = purchasePrice;
 	}
+
+	@Override
+	public boolean equals(final Object obj) {
+
+		if (obj instanceof LoadSlot) {
+			final LoadSlot slot = (LoadSlot) obj;
+
+			if (minLoadVolume != slot.minLoadVolume) {
+				return false;
+			}
+			if (maxLoadVolume != slot.maxLoadVolume) {
+				return false;
+			}
+			if (purchasePrice != slot.purchasePrice) {
+				return false;
+			}
+
+			return super.equals(obj);
+		}
+		return false;
+	}
+
 }
--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/components/impl/LoadSlotTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/components/impl/LoadSlotTest.java
@@ -1,9 +1,40 @@
 package com.mmxlabs.scheduler.optimiser.components.impl;
 
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.mmxlabs.optimiser.components.ITimeWindow;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+
+@RunWith(JMock.class)
 public class LoadSlotTest {
+
+	Mockery context = new JUnit4Mockery();
+
+	@Test
+	public void testLoadSlot() {
+		final String id = "id";
+		final IPort port = context.mock(IPort.class);
+		final ITimeWindow tw = context.mock(ITimeWindow.class);
+		final long minLoadVolume = 10l;
+		final long maxLoadVolume = 20l;
+		final long purchasePrice = 30l;
+
+		final LoadSlot slot = new LoadSlot(id, port, tw, minLoadVolume,
+				maxLoadVolume, purchasePrice);
+		Assert.assertSame(id, slot.getId());
+		Assert.assertSame(port, slot.getPort());
+		Assert.assertSame(tw, slot.getTimeWindow());
+
+		Assert.assertEquals(minLoadVolume, slot.getMinLoadVolume());
+		Assert.assertEquals(maxLoadVolume, slot.getMaxLoadVolume());
+		Assert.assertEquals(purchasePrice, slot.getPurchasePrice());
+
+	}
 
 	@Test
 	public void testGetSetMinLoadVolume() {
@@ -23,7 +54,6 @@ public class LoadSlotTest {
 		Assert.assertEquals(value, slot.getMaxLoadVolume());
 	}
 
-
 	@Test
 	public void testGetSetPurchasePrice() {
 		final long value = 10;
@@ -32,4 +62,48 @@ public class LoadSlotTest {
 		slot.setPurchasePrice(value);
 		Assert.assertEquals(value, slot.getPurchasePrice());
 	}
+
+	@Test
+	public void testEquals() {
+	
+		final String id1 = "id1";
+		final String id2 = "id2";
+		final IPort port1 = context.mock(IPort.class, "port1");
+		final IPort port2 = context.mock(IPort.class, "port2");
+		final ITimeWindow tw1 = context.mock(ITimeWindow.class, "tw1");
+		final ITimeWindow tw2 = context.mock(ITimeWindow.class, "tw2");
+		
+		
+		final LoadSlot slot1 = new LoadSlot(id1, port1, tw1, 10l, 20l, 30l);
+		final LoadSlot slot2 = new LoadSlot(id1, port1, tw1, 10l, 20l, 30l);
+		
+		final LoadSlot slot3 = new LoadSlot(id2, port1, tw1, 10l, 20l, 30l);
+		final LoadSlot slot4 = new LoadSlot(id1, port2, tw1, 10l, 20l, 30l);
+		final LoadSlot slot5 = new LoadSlot(id1, port1, tw2, 10l, 20l, 30l);
+		final LoadSlot slot6 = new LoadSlot(id1, port1, tw1, 210l, 20l, 30l);
+		final LoadSlot slot7 = new LoadSlot(id1, port1, tw1, 10l, 220l, 30l);
+		final LoadSlot slot8 = new LoadSlot(id1, port1, tw1, 10l, 20l, 230l);
+	
+		
+		Assert.assertTrue(slot1.equals(slot1));
+		Assert.assertTrue(slot1.equals(slot2));
+		Assert.assertTrue(slot2.equals(slot1));
+		
+		Assert.assertFalse(slot1.equals(slot3));
+		Assert.assertFalse(slot1.equals(slot4));
+		Assert.assertFalse(slot1.equals(slot5));
+		Assert.assertFalse(slot1.equals(slot6));
+		Assert.assertFalse(slot1.equals(slot7));
+		Assert.assertFalse(slot1.equals(slot8));
+	
+		Assert.assertFalse(slot3.equals(slot1));
+		Assert.assertFalse(slot4.equals(slot1));
+		Assert.assertFalse(slot5.equals(slot1));
+		Assert.assertFalse(slot6.equals(slot1));
+		Assert.assertFalse(slot7.equals(slot1));
+		Assert.assertFalse(slot8.equals(slot1));
+		
+		Assert.assertFalse(slot1.equals(new Object()));
+	}
+	
 }
