c35707f99daefb3380ce265ad46a1f0f4a48a690
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-02 05:45:28 -0100

    Implement equals method

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/XYPort.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/XYPort.java
@@ -1,5 +1,6 @@
 package com.mmxlabs.scheduler.optimiser.components.impl;
 
+import com.mmxlabs.common.Equality;
 import com.mmxlabs.scheduler.optimiser.components.IXYPort;
 
 /**
@@ -15,6 +16,16 @@ public final class XYPort implements IXYPort {
 	private float x;
 
 	private float y;
+
+	public XYPort() {
+		
+	}
+	
+	public XYPort(final String name, final float x, final float y) {
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
 
 	@Override
 	public String getName() {
@@ -41,5 +52,27 @@ public final class XYPort implements IXYPort {
 
 	public final void setY(final float y) {
 		this.y = y;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+
+		if (obj instanceof XYPort) {
+			final XYPort p = (XYPort) obj;
+
+			if (x != p.x) {
+				return false;
+			}
+			if (y != p.y) {
+				return false;
+			}
+			if (!Equality.isEqual(name, p.name)) {
+				return false;
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 }
--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/components/impl/XYPortTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/components/impl/XYPortTest.java
@@ -6,6 +6,18 @@ import org.junit.Test;
 public class XYPortTest {
 
 	@Test
+	public void testXYPort() {
+		final String name = "name";
+		final float x = 1.0f;
+		final float y = 2.0f;
+		
+		final XYPort port = new XYPort(name, x, y);
+		Assert.assertSame(name, port.getName());
+		Assert.assertEquals(x, port.getX(), 0.0f);
+		Assert.assertEquals(y, port.getY(), 0.0f);
+	}
+	
+	@Test
 	public void testGetSetName() {
 		final XYPort port = new XYPort();
 		Assert.assertNull(port.getName());
@@ -30,4 +42,24 @@ public class XYPortTest {
 		Assert.assertEquals(10.0f, port.getY(), 0.0);
 	}
 
+	@Test
+	public void testEquals() {
+		
+		XYPort port1 = new XYPort("name", 1.0f, 2.0f);
+		XYPort port2 = new XYPort("name", 1.0f, 2.0f);
+		
+		XYPort port3 = new XYPort("name2", 1.0f, 2.0f);
+		XYPort port4 = new XYPort("name", 2.0f, 2.0f);
+		XYPort port5 = new XYPort("name", 1.0f, 1.0f);
+		
+		Assert.assertTrue(port1.equals(port1));
+		Assert.assertTrue(port1.equals(port2));
+		Assert.assertTrue(port2.equals(port1));
+		
+		Assert.assertFalse(port1.equals(port3));
+		Assert.assertFalse(port1.equals(port4));
+		Assert.assertFalse(port1.equals(port5));
+		
+		Assert.assertFalse(port1.equals(new Object()));
+	}
 }
