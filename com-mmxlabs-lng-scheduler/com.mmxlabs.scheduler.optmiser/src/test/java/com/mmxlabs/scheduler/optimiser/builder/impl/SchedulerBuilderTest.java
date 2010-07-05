edf82351bbff4ab7aee42e37b842063f14812c53
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-05 16:12:35 -0100

    Added stub tests

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/builder/impl/SchedulerBuilderTest.java
@@ -0,0 +1,84 @@
+package com.mmxlabs.scheduler.optimiser.builder.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SchedulerBuilderTest {
+
+	@Test
+	public void testSchedulerBuilder() {
+		
+		/**
+		 * How to test builder? -- No access to state until we get the finished product?
+		 */
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateLoadSlot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateDischargeSlot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateCargo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreatePortString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreatePortStringFloatFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateTimeWindow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateVessel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPortToPortDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetElementDurations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOptimisationData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateVesselClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVesselClassStateParamaters() {
+		fail("Not yet implemented");
+	}
+
+}
