9366220d375ec905611e4dfbd2da9b131a31f897
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-05 16:10:40 -0100

    Added stub tests

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.mmxlabs.scheduler.optimiser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testSpeedFromDistanceTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeFromSpeedDistance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQuantityFromRateTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeFromRateQuantity() {
+		fail("Not yet implemented");
+	}
+}
