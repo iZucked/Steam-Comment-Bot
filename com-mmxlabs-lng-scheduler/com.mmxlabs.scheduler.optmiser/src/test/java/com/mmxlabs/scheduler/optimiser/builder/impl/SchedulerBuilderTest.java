7285cc0b3a314ceea01338ffcd8b7ac65777d197
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-05 16:25:13 -0100

    Start implementing tests

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/builder/impl/SchedulerBuilderTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/builder/impl/SchedulerBuilderTest.java
@@ -1,18 +1,34 @@
 package com.mmxlabs.scheduler.optimiser.builder.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import junit.framework.Assert;
 
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.mmxlabs.optimiser.components.ITimeWindow;
+import com.mmxlabs.scheduler.optimiser.components.IDischargeSlot;
+import com.mmxlabs.scheduler.optimiser.components.ILoadSlot;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IVesselClass;
+
+@RunWith(JMock.class)
 public class SchedulerBuilderTest {
+
+	Mockery context = new JUnit4Mockery();
 
 	@Test
 	public void testSchedulerBuilder() {
-		
+
 		/**
-		 * How to test builder? -- No access to state until we get the finished product?
+		 * How to test builder? -- No access to state until we get the finished
+		 * product? Perhaps a second constructor passing in DCP objects and
+		 * ensure they are called correctly.
 		 */
-		
+
 		fail("Not yet implemented");
 	}
 
@@ -21,14 +37,89 @@ public class SchedulerBuilderTest {
 		fail("Not yet implemented");
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateLoadSlot2() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = context.mock(IPort.class);
+		ITimeWindow window = builder.createTimeWindow(0, 0);
+
+		builder.createLoadSlot("id", port, window, 0, 0, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateLoadSlot3() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = builder.createPort("port");
+		ITimeWindow window = context.mock(ITimeWindow.class);
+
+		builder.createLoadSlot("id", port, window, 0, 0, 0);
+	}
+
 	@Test
 	public void testCreateDischargeSlot() {
 		fail("Not yet implemented");
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateDischargeSlot2() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = context.mock(IPort.class);
+		ITimeWindow window = builder.createTimeWindow(0, 0);
+
+		builder.createDischargeSlot("id", port, window, 0, 0, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateDischargeSlot3() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = builder.createPort("port");
+		ITimeWindow window = context.mock(ITimeWindow.class);
+
+		builder.createDischargeSlot("id", port, window, 0, 0, 0);
+	}
+
 	@Test
 	public void testCreateCargo() {
 		fail("Not yet implemented");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateCargo2() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = builder.createPort("port");
+		ITimeWindow window = builder.createTimeWindow(0, 0);
+
+		ILoadSlot loadSlot = context.mock(ILoadSlot.class);
+
+		IDischargeSlot dischargeSlot = builder.createDischargeSlot("id", port,
+				window, 0, 0, 0);
+
+		builder.createCargo("id", loadSlot, dischargeSlot);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreateCargo3() {
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		IPort port = builder.createPort("port");
+		ITimeWindow window = builder.createTimeWindow(0, 0);
+
+		ILoadSlot loadSlot = builder
+				.createLoadSlot("id", port, window, 0, 0, 0);
+		IDischargeSlot dischargeSlot = context.mock(IDischargeSlot.class);
+
+		builder.createCargo("id", loadSlot, dischargeSlot);
 	}
 
 	@Test
@@ -43,7 +134,12 @@ public class SchedulerBuilderTest {
 
 	@Test
 	public void testCreateTimeWindow() {
-		fail("Not yet implemented");
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+		ITimeWindow window = builder.createTimeWindow(10, 20);
+
+		Assert.assertEquals(10, window.getStart());
+		Assert.assertEquals(20, window.getEnd());
 	}
 
 	@Test
@@ -79,6 +175,15 @@ public class SchedulerBuilderTest {
 	@Test
 	public void testSetVesselClassStateParamaters() {
 		fail("Not yet implemented");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetVesselClassStateParamaters2() {
+		IVesselClass vc = context.mock(IVesselClass.class);
+
+		SchedulerBuilder builder = new SchedulerBuilder();
+
+		builder.setVesselClassStateParamaters(vc, null, 0, 0, 0, null, 0);
 	}
 
 }
