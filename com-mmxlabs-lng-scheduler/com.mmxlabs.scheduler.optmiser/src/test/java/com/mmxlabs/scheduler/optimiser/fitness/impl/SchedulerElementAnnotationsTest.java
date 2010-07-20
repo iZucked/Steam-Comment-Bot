09b61baa61b75692f612c43d9dd66df2b67912f1
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-20 19:00:36 -0100

    Add stub tests

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/fitness/impl/SchedulerElementAnnotationsTest.java
@@ -0,0 +1,24 @@
+package com.mmxlabs.scheduler.optimiser.fitness.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SchedulerElementAnnotationsTest {
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsKey() {
+		fail("Not yet implemented");
+	}
+
+}
