f8c94d193c1aa8ef74548e24f6f62ec9b173411c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-20 18:57:27 -0100

    Add stub test cases

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optmiser/src/test/java/com/mmxlabs/scheduler/optimiser/fitness/impl/AnnotatedSequenceTest.java
@@ -0,0 +1,34 @@
+package com.mmxlabs.scheduler.optimiser.fitness.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnnotatedSequenceTest {
+
+	@Test
+	public void testAnnotatedSequence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAnnotation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAnnotation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasAnnotation() {
+		fail("Not yet implemented");
+	}
+
+}
