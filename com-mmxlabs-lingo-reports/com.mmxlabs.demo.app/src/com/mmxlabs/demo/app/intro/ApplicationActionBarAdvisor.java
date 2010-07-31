e2edc8e63a827c389fd6a7b868c893ab8040f756
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-31 12:34:15 -0100

    Add additional menu options

--- com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/ApplicationActionBarAdvisor.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/ApplicationActionBarAdvisor.java
@@ -1,11 +1,13 @@
 package com.mmxlabs.demo.app.intro;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.actions.ContributionItemFactory;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
@@ -13,6 +15,10 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
     private IWorkbenchAction introAction;
     
+    private IWorkbenchAction quitAction;
+
+	private IContributionItem showViewMenu;
+	
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
 	}
@@ -21,10 +27,32 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	protected void makeActions(IWorkbenchWindow window) {
 		introAction = ActionFactory.INTRO.create(window);
 		register(introAction);
+		
+		quitAction = ActionFactory.QUIT.create(window);
+		register(quitAction);
+		
+		
+		showViewMenu = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
+		
 	}
 
 	@Override
 	protected void fillMenuBar(IMenuManager menuBar) {
+		
+		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
+		menuBar.add(fileMenu);
+		
+		fileMenu.add(quitAction);
+		
+		MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW);
+		menuBar.add(windowMenu);
+		
+		{
+			MenuManager showViewMenuMgr = new MenuManager("&Show View", "showView"); //
+            
+            showViewMenuMgr.add(showViewMenu);
+            windowMenu.add(showViewMenuMgr);
+		}
 		
 		MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
 		menuBar.add(helpMenu);
@@ -32,5 +60,4 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 		// Help
 		helpMenu.add(introAction);
 	}
-
 }
