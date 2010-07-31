5b0926c87014d1903b647b53c48e62a5ebf2507b
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-07-31 12:25:19 -0100

    Create action to pack the table columns

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.reports/src/com/mmxlabs/demo/reports/views/actions/PackTableColumnsAction.java
@@ -0,0 +1,26 @@
+package com.mmxlabs.demo.reports.views.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.TableColumn;
+
+public class PackTableColumnsAction extends Action {
+
+	private TableViewer viewer;
+
+	public PackTableColumnsAction(TableViewer viewer) {
+		super("Pack Columns");
+		this.viewer = viewer;
+	}
+
+	@Override
+	public void run() {
+
+		if (!viewer.getControl().isDisposed()) {
+			TableColumn[] columns = viewer.getTable().getColumns();
+			for (TableColumn c : columns) {
+				c.pack();
+			}
+		}
+	}
+}
