169e8613f967e394876b6a9b28b3f88e0cbd4e1c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-08-18 16:04:50 -0100

    Doc

--- com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/common/fitness/MatrixProviderFitnessComponent.java
+++ com-mmxlabs-optimiser/optimiser-common/src/main/java/com/mmxlabs/optimiser/common/fitness/MatrixProviderFitnessComponent.java
@@ -3,7 +3,15 @@ package com.mmxlabs.optimiser.common.fitness;
 import com.mmxlabs.optimiser.core.fitness.IFitnessComponent;
 import com.mmxlabs.optimiser.core.fitness.IFitnessCore;
 
-
+/**
+ * Simple {@link IFitnessComponent} implementation for a
+ * {@link MatrixProviderFitnessCore}.
+ * 
+ * @author Simon Goodall
+ * 
+ * @param <T>
+ *            Sequence element type
+ */
 public final class MatrixProviderFitnessComponent<T> implements
 		IFitnessComponent<T> {
 
