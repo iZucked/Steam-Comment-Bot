aecc6c03adbc45eacd5ad61a8e4ca2d4c52efbfd
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-08-18 18:58:30 -0100

    Add additional methods for unit conversion

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/Calculator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/Calculator.java
@@ -1,5 +1,6 @@
 package com.mmxlabs.scheduler.optimiser;
 
+
 /**
  * Class to handle integer based maths. Assume, unless otherwise noted each
  * input has been converted into a int/long by multiplying against the
@@ -14,12 +15,12 @@ public class Calculator {
 
 	public static int speedFromDistanceTime(long distance, int time) {
 
-		return (int)((distance * ScaleFactor) / time);
+		return (int) ((distance * ScaleFactor) / time);
 	}
 
 	public static int getTimeFromSpeedDistance(int speed, long distance) {
 
-		return (int)((distance * ScaleFactor) / speed);
+		return (int) ((distance * ScaleFactor) / speed);
 	}
 
 	public static long quantityFromRateTime(long rate, int time) {
@@ -28,11 +29,27 @@ public class Calculator {
 	}
 
 	public static int getTimeFromRateQuantity(long rate, long quantity) {
-		
-		return (int)(quantity / rate);
+
+		return (int) (quantity / rate);
 	}
 
 	public static long costFromConsumption(long consumption, int unitPrice) {
 		return (consumption * unitPrice) / ScaleFactor;
+	}
+
+	public static long convertM3ToMMBTu(long m3, int factor) {
+		return (m3 * factor) / ScaleFactor;
+	}
+
+	public static long convertMMBTuToM3(long mmbtu, int factor) {
+		return (mmbtu * ScaleFactor) / factor;
+	}
+
+	public static long convertM3ToMT(long m3, int factor) {
+		return (m3 * factor) / ScaleFactor;
+	}
+
+	public static long convertMTToM3(long mt, int factor) {
+		return (mt * ScaleFactor) / factor;
 	}
 }
