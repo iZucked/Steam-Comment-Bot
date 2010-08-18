e9edef12e60064eecd495c889d41afec0b1342ec
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-08-18 18:58:55 -0100

    Create Enum representing units of measure for Fuels

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/voyage/FuelUnit.java
@@ -0,0 +1,23 @@
+package com.mmxlabs.scheduler.optimiser.voyage;
+
+/**
+ * Units of Measure for Fuel / Gas etc
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public enum FuelUnit {
+
+	/**
+	 * Metres Cubed
+	 */
+	M3,
+	/**
+	 * Metric Tonnes
+	 */
+	MT,
+	/**
+	 * Million Million British Thermal Units.
+	 */
+	MMBTu,
+}
