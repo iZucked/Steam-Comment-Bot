5c7734318a49447637b97e657d121e2c9f4c40a8
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-21 10:41:56 -0100

    Added an Association class for tracking associated pairs of objects. It's used for transforming Scenarios into optimisation jobs, to track which ports etc. in the model correspond to which IPorts.

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Association.java
@@ -0,0 +1,32 @@
+package com.mmxlabs.common;
+
+import java.util.HashMap;
+
+/**
+ * A two-way association from one type to another. Internally maintains two hashmaps for lookup in both directions.
+ * @author hinton
+ *
+ * @param <A>
+ * @param <B>
+ */
+public class Association<A, B> {
+	protected HashMap<A, B> forwards;
+	protected HashMap<B, A>reverse;
+	public Association() {
+		forwards = new HashMap<A, B>();
+		reverse = new HashMap<B, A>();
+	}
+	
+	public void add(A a, B b) {
+		forwards.put(a, b);
+		reverse.put(b, a);
+	}
+	
+	public B lookup(A a) {
+		return forwards.get(a);
+	}
+	
+	public A reverseLookup(B b) {
+		return reverse.get(b);
+	}
+}
