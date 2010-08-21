7caa7c1b0dc75d059a12567e1fb508107fefa658
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-21 11:23:20 -0100

    Class for converting a scenario to an optimisation data instance

---
+++ com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/LNGScenarioTransformer.java
@@ -0,0 +1,272 @@
+package com.mmxlabs.jobcontroller.emf;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.TreeMap;
+
+import javax.management.timer.Timer;
+
+import scenario.Scenario;
+import scenario.cargo.Cargo;
+import scenario.cargo.DischargeSlot;
+import scenario.cargo.LoadSlot;
+import scenario.fleet.FuelConsumptionLine;
+import scenario.fleet.Vessel;
+import scenario.fleet.VesselClass;
+import scenario.fleet.VesselStateAttributes;
+import scenario.port.DistanceLine;
+import scenario.port.Port;
+
+import com.mmxlabs.common.Association;
+import com.mmxlabs.optimiser.common.components.ITimeWindow;
+import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
+import com.mmxlabs.scheduler.optimiser.Calculator;
+import com.mmxlabs.scheduler.optimiser.builder.impl.SchedulerBuilder;
+import com.mmxlabs.scheduler.optimiser.components.IDischargeSlot;
+import com.mmxlabs.scheduler.optimiser.components.ILoadSlot;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.ISequenceElement;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+import com.mmxlabs.scheduler.optimiser.components.IVesselClass;
+import com.mmxlabs.scheduler.optimiser.components.impl.InterpolatingConsumptionRateCalculator;
+
+/**
+ * Wrapper for an EMF LNG Scheduling {@link scenario.Scenario}, providing utility methods to convert it into
+ * an optimization job. Typical usage is to construct an LNGScenarioTransformer with a given Scenario, and then call
+ * the {@link createOptimisationData} method
+ * 
+ * @author hinton
+ *
+ */
+public class LNGScenarioTransformer {
+	private Scenario scenario;
+	private TimeZone timezone;
+
+	/**
+	 * Create a transformer for the given scenario; the class holds a reference, so changes
+	 * made to the scenario after construction will be reflected in calls to the various helper methods.
+	 * 
+	 * @param scenario
+	 */
+	public LNGScenarioTransformer(Scenario scenario) {
+		this.scenario = scenario;
+		this.timezone = TimeZone.getTimeZone("UTC");
+	}
+
+	/**
+	 * Instantiates and returns an {@link IOptimisationData} isomorphic to the contained scenario.
+	 * @return
+	 * @throws IncompleteScenarioException 
+	 */
+	public IOptimisationData<ISequenceElement> createOptimisationData() throws IncompleteScenarioException {
+		SchedulerBuilder builder = new SchedulerBuilder();
+		/**
+		 * Bidirectionally maps EMF {@link Port} Models to {@link IPort}s in the builder.
+		 */
+		Association<Port, IPort> portAssociation = new Association<Port, IPort>();
+		/**
+		 * Lists all the {@link IPort}s created for this scenario 
+		 */
+		List<IPort> allPorts = new ArrayList<IPort>();
+		/**
+		 * A reverse-lookup for the {@link allPorts} array.
+		 */
+		Map<IPort, Integer> portIndices = new HashMap<IPort, Integer>();
+		/*
+		 * Construct ports for each port in the scenario port model, and keep them in a
+		 * two-way lookup table (the two-way lookup is needed to do things like setting distances later).
+		 */
+		for (Port ePort : scenario.getPortModel().getPorts()) {
+			IPort port = builder.createPort(ePort.getName());
+			portAssociation.add(ePort, port);
+			portIndices.put(port, allPorts.size());
+			allPorts.add(port);
+		}
+
+		buildDistances(builder, portAssociation, allPorts, portIndices);
+		buildFleet(builder, portAssociation);
+		buildCargoes(builder, portAssociation);
+		return builder.getOptimisationData();
+	}
+	/**
+	 * Extract the cargoes from the scenario and add them to the given builder.
+	 * @param builder current builder. should already have ports/distances/vessels built
+	 */
+	private void buildCargoes(SchedulerBuilder builder, Association<Port, IPort> ports) {
+		/*
+		 * Find the earliest date, to convert from absolute date and time to offset hours 
+		 */
+		Date earliest = null;
+		for (Cargo eCargo: scenario.getCargoModel().getCargoes()) {
+			final Date loadDate = eCargo.getLoadSlot().getWindowStart();
+			if (earliest == null || earliest.after(loadDate)) {
+				earliest = loadDate;
+			}
+		}
+		for (Cargo eCargo: scenario.getCargoModel().getCargoes()) {
+			//not escargot.
+			LoadSlot loadSlot = eCargo.getLoadSlot();
+			DischargeSlot dischargeSlot = eCargo.getDischargeSlot();
+			int loadStart = convertTime(earliest, loadSlot.getWindowStart());
+			int dischargeStart = convertTime(earliest, dischargeSlot.getWindowStart());
+			
+			//TODO check units again
+			ITimeWindow loadWindow = builder.createTimeWindow(loadStart, loadStart + loadSlot.getWindowDuration());
+			ITimeWindow dischargeWindow = builder.createTimeWindow(dischargeStart, dischargeStart + dischargeSlot.getWindowDuration());
+			
+			ILoadSlot load = builder.createLoadSlot(loadSlot.getId(), 
+					ports.lookup(loadSlot.getPort()), 
+					loadWindow, 
+					loadSlot.getMinQuantity(), 
+					loadSlot.getMaxQuantity(), 
+					loadSlot.getUnitPrice());
+			
+			IDischargeSlot discharge = builder.createDischargeSlot(dischargeSlot.getId(), 
+					ports.lookup(dischargeSlot.getPort()), 
+					dischargeWindow, 
+					dischargeSlot.getMinQuantity(),
+					dischargeSlot.getMaxQuantity(), 
+					dischargeSlot.getUnitPrice());
+			
+			builder.createCargo(eCargo.getId(), load, discharge);
+		}
+	}
+
+	/**
+	 * Convert a date into relative hours; returns the number of hours between windowStart and earliest.
+	 * @param earliest
+	 * @param windowStart
+	 * @return number of hours between earliest and windowStart
+	 */
+	private int convertTime(Date earliest, Date windowStart) {
+		//I am using two calendars, because the java date objects are all
+		//deprecated; however, timezones should not be a problem because
+		//every Date in the EMF representation is in UTC.
+		Calendar a = Calendar.getInstance(timezone);
+		a.setTime(earliest);
+		Calendar b = Calendar.getInstance(timezone);
+		b.setTime(windowStart);
+		long difference = b.getTimeInMillis() - a.getTimeInMillis();
+		return (int)(difference / Timer.ONE_HOUR);
+	}
+
+	/**
+	 * Create the distance matrix for the given builder.
+	 * @param builder the builder we are working with
+	 * @param portAssociation an association between ports in the EMF model and IPorts in the builder
+	 * @param allPorts the list of all IPorts constructed so far
+	 * @param portIndices a reverse-lookup for the ports in allPorts
+	 * @throws IncompleteScenarioException
+	 */
+	private void buildDistances(SchedulerBuilder builder,
+			Association<Port, IPort> portAssociation, List<IPort> allPorts,
+			Map<IPort, Integer> portIndices) throws IncompleteScenarioException {
+		boolean[][] modeledEdges = new boolean[allPorts.size()][allPorts.size()];
+
+		/*
+		 * Now fill out the distances from the distance model.
+		 */
+		for (DistanceLine dl : scenario.getDistanceModel().getDistances()) {
+			IPort from, to;
+			from = portAssociation.lookup(dl.getFromPort());
+			to = portAssociation.lookup(dl.getToPort());
+
+			modeledEdges[portIndices.get(from)][portIndices.get(to)] = true;
+
+			builder.setPortToPortDistance(from, to, Calculator.scale(dl.getDistance()));
+		}
+		StringBuilder missingEdges = new StringBuilder();
+		/*
+		 * Check whether any entries were missing from the distance matrix
+		 */
+		for (int i = 0; i<modeledEdges.length; i++) {
+			for (int j = 0; j<modeledEdges.length; j++) {
+				if (modeledEdges[i][j] == false) {
+					if (missingEdges.length() > 0) missingEdges.append(", ");
+					missingEdges.append("from " + allPorts.get(i).getName() + " to " + allPorts.get(j).getName());
+				}
+			}
+		}
+
+		if (missingEdges.length() > 0) {
+			throw new IncompleteScenarioException("Missing edges in distance model : " + missingEdges.toString());
+		}
+	}
+
+	/**
+	 * Construct the fleet model for the scenario
+	 * @param builder a builder which has had its ports and distances instantiated
+	 * @param portAssociation the Port <-> IPort association to connect EMF Ports with builder IPorts
+	 */
+	private void buildFleet(SchedulerBuilder builder,
+			Association<Port, IPort> portAssociation) {
+
+		/*
+		 * Build the fleet model - first we must create the vessel classes from the model
+		 */
+		Association<VesselClass, IVesselClass> vesselClassAssociation = new Association<VesselClass, IVesselClass>();
+
+		for (VesselClass eVc : scenario.getFleetModel().getVesselClasses()) {
+			//TODO missing min heel capacity (penultimate parameter)
+			//TODO check whether these values need converting
+			IVesselClass vc = builder.createVesselClass(eVc.getName(), 
+					eVc.getMinSpeed(), eVc.getMaxSpeed(), 
+					eVc.getCapacity(), 
+					eVc.getMinHeelVolume(), //TODO figure out what this ought to be, really
+					eVc.getBaseFuelUnitPrice());
+			vesselClassAssociation.add(eVc, vc);
+
+			/*
+			 * Set state-specific attributes
+			 */
+			buildVesselStateAttributes(builder, vc, com.mmxlabs.scheduler.optimiser.components.VesselState.Laden,eVc.getLadenAttributes());
+			buildVesselStateAttributes(builder, vc, com.mmxlabs.scheduler.optimiser.components.VesselState.Ballast,eVc.getBallastAttributes());
+		}
+
+		/*
+		 * Now create each vessel
+		 */
+		for (Vessel eV : scenario.getFleetModel().getFleet()) {
+			IVessel v = builder.createVessel(eV.getName(), 
+					vesselClassAssociation.lookup(eV.getClass_()), 
+					portAssociation.lookup(eV.getStartPort()),
+					portAssociation.lookup(eV.getEndPort()));
+		}
+
+	}
+
+	/**
+	 * Tell the builder to set up the given vessel state from the EMF fleet model
+	 * @param builder the builder which is currently in use
+	 * @param vc the {@link IVesselClass} which the builder has constructed whose attributes we are setting 
+	 * @param laden the {@link com.mmxlabs.scheduler.optimiser.components.VesselState} we are setting attributes for
+	 * @param ladenAttributes the {@link VesselStateAttributes} from the EMF model
+	 */
+	private void buildVesselStateAttributes(SchedulerBuilder builder,
+			IVesselClass vc,
+			com.mmxlabs.scheduler.optimiser.components.VesselState state,
+			VesselStateAttributes attrs) {
+
+		//create consumption rate calculator for the curve
+		TreeMap<Integer, Long> keypoints = new TreeMap<Integer, Long>();
+
+		//TODO these units may need converting.
+		for (FuelConsumptionLine line : attrs.getFuelConsumptionCurve()) {
+			keypoints.put(line.getSpeed(), (long) line.getConsumption());
+		}
+
+		InterpolatingConsumptionRateCalculator consumptionCalculator = new InterpolatingConsumptionRateCalculator(keypoints);
+
+		//TODO the NBO speed is fixed but needs checking inside this call.
+		builder.setVesselClassStateParamaters(vc, state, 
+				attrs.getNboRate(), 
+				attrs.getIdleNBORate(), 
+				attrs.getIdleConsumptionRate(), 
+				consumptionCalculator);
+	}
+}
