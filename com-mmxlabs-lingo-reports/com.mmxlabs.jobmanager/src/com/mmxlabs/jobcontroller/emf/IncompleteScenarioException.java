450336ec80db678b101a344fb3bf25db20c99339
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-21 11:23:46 -0100

    Supports LNGScenarioTransformer

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/IncompleteScenarioException.java
@@ -0,0 +1,9 @@
+package com.mmxlabs.jobcontroller.emf;
+
+public class IncompleteScenarioException extends Exception {
+
+	public IncompleteScenarioException(String string) {
+		super(string);
+	}
+
+}
