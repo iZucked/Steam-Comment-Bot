da59a47ff46ab19941353456d2262a21025cab2c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-08-23 05:38:47 -0100

    Create enum to specify the type of vessel. Is is a fleet, time or spot charter?

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/VesselInstanceType.java
@@ -0,0 +1,35 @@
+package com.mmxlabs.scheduler.optimiser.components;
+
+/**
+ * The {@link VesselInstanceType} indicates what the {@link IVessel} instance
+ * is. For example, is it a core fleet vessel, or is it an instance created to
+ * handle spot charters?
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public enum VesselInstanceType {
+
+	/**
+	 * Unknown type
+	 */
+	UNKNOWN,
+
+	/**
+	 * Core fleet vessel. Implies vessel is always available for use.
+	 */
+	FLEET,
+
+	/**
+	 * Long term charter vessel. May have a specific start or end time and port,
+	 * depending on the overlap of contracted period with the optimisation
+	 * period.
+	 */
+	TIME_CHARTER,
+
+	/**
+	 * Short term charter to deal with one or two cargoes. Start and end time
+	 * and port needs to be determined by optimiser.
+	 */
+	SPOT_CHARTER
+}
