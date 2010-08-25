607f5058b62401c7ff73e977b5fea635cf19e42a
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-25 16:58:37 -0100

    New random scenario generation code

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/wizards/RandomScenarioUtils.java
@@ -0,0 +1,198 @@
+package com.mmxlabs.demo.app.wizards;
+
+import java.util.Date;
+import java.util.Random;
+
+import javax.management.timer.Timer;
+
+import org.eclipse.emf.common.util.EList;
+
+import scenario.Scenario;
+import scenario.cargo.Cargo;
+import scenario.cargo.CargoFactory;
+import scenario.cargo.CargoPackage;
+import scenario.cargo.Slot;
+import scenario.fleet.FleetFactory;
+import scenario.fleet.FleetPackage;
+import scenario.fleet.FuelConsumptionLine;
+import scenario.fleet.PortAndTime;
+import scenario.fleet.Vessel;
+import scenario.fleet.VesselClass;
+import scenario.fleet.VesselInstanceType;
+import scenario.fleet.VesselState;
+import scenario.fleet.VesselStateAttributes;
+import scenario.port.DistanceLine;
+import scenario.port.Port;
+
+public class RandomScenarioUtils {
+	FleetFactory ff = FleetPackage.eINSTANCE.getFleetFactory();
+	private Random random;
+	public RandomScenarioUtils(Random random) {
+		this.random = random;
+	}
+	public RandomScenarioUtils() {
+		this(new Random(1));
+	}
+	
+	void addStandardFleet(Scenario scenario) {
+		//generate the standard fleet
+		VesselClass class1 = addVesselClass(scenario, "STEAM-138", 12, 20, 138000, 200);
+		VesselClass class2 = addVesselClass(scenario, "STEAM-145", 12, 20, 145000, 200);
+		VesselClass class3 = addVesselClass(scenario, "DFDE-177", 12, 20, 177000, 200);
+		VesselClass class4 = addVesselClass(scenario, "STEAM-126", 12, 19, 138000, 200); //TODO units in the model; should it be a float?
+		
+		float[][] steam = new float[][] {{12, 12}, {20, 20}};
+		float[][] dfde = new float[][] {{12, 8}, {20, 16}};
+		
+		//create class parameters; currently model uses containment for curves, so we need to do duplicates
+		class1.setLadenAttributes(createVesselStateAttributes(VesselState.LADEN, 138/24.0f, 118/24.0f, 10/24.0f, steam));
+		class1.setBallastAttributes(createVesselStateAttributes(VesselState.BALLAST, 138/24.0f, 118/24.0f, 10/24.0f, steam));
+		
+		class2.setLadenAttributes(createVesselStateAttributes(VesselState.LADEN, 145/24.0f, 125/24.0f, 10/24.0f, steam));
+		class2.setBallastAttributes(createVesselStateAttributes(VesselState.BALLAST, 145/24.0f, 125/24.0f, 10/24.0f, steam));
+		
+		class3.setLadenAttributes(createVesselStateAttributes(VesselState.LADEN, 177/24.0f, 157/24.0f, 10/24.0f, dfde));
+		class3.setBallastAttributes(createVesselStateAttributes(VesselState.BALLAST, 177/24.0f, 157/24.0f, 10/24.0f, dfde));
+		
+		class4.setLadenAttributes(createVesselStateAttributes(VesselState.LADEN, 126/24.0f, 106/24.0f, 10/24.0f, steam));
+		class4.setBallastAttributes(createVesselStateAttributes(VesselState.BALLAST, 125/24.0f, 106/24.0f, 10/24.0f, steam));
+		
+		//create vessels in each class
+		
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Rita Andrea", class1));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Jane Elizabeth", class2));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Lydon Volney", class2));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Shirley Elizabeth", class2));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Heather Sally", class2));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Alison Victoria", class2));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Nile Eagle", class2));
+		
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Julia Louise", class3));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Becki Anne", class3));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Mickie Harper", class3));
+		randomiseAvailability(scenario, addVessel(scenario, "Methane Unknown", class3));
+		
+		addVessel(scenario, "Hilli", class4);
+		addVessel(scenario, "Gimi", class4);
+		addVessel(scenario, "Khannur", class4);
+		addVessel(scenario, "Golar Freeze", class4);
+		addVessel(scenario, "Methane Princes", class1);
+		
+		addVessel(scenario, "Extra-Charter-1", class1).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-2", class2).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-3", class3).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-4", class4).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-5", class1).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-6", class2).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-7", class3).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-8", class4).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-9", class1).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-10", class2).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-11", class3).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+		addVessel(scenario, "Extra-Charter-12", class4).setInstanceType(VesselInstanceType.SPOT_CHARTER);
+	}
+	
+	private void randomiseAvailability(Scenario scenario, Vessel addVessel) {
+		//Add a random start constraint.
+		PortAndTime start = ff.createPortAndTime();
+		EList<Port> ports = scenario.getPortModel().getPorts();
+		start.setPort(ports.get(random.nextInt(ports.size())));
+	}
+	
+	private Vessel addVessel(Scenario scenario, String string, VesselClass class1) {
+		Vessel v = ff.createVessel();
+		
+		v.setName(string);
+		v.setClass(class1);
+		
+		//TODO start and end constraints?
+		
+		scenario.getFleetModel().getFleet().add(v);
+		
+		return v;
+	}
+	private VesselStateAttributes createVesselStateAttributes(VesselState state,
+			float nbo, float idlenbo, float idleconsumption, float[][] curve) {
+		VesselStateAttributes vsa = ff.createVesselStateAttributes();
+		
+		vsa.setVesselState(state);
+		vsa.setIdleConsumptionRate(idleconsumption);
+		vsa.setIdleNBORate(nbo);
+		vsa.setIdleNBORate(idlenbo);
+		
+		for (float[] point : curve) {
+			FuelConsumptionLine line = ff.createFuelConsumptionLine();
+			line.setConsumption(point[0]);
+			line.setSpeed(point[1]);
+			vsa.getFuelConsumptionCurve().add(line);
+		}
+		
+		return vsa;
+	}
+	VesselClass addVesselClass(Scenario scenario, String name, int minSpeed, int maxSpeed, long capacity, int baseFuelUnitPrice) {
+		VesselClass vc = ff.createVesselClass();
+		
+		vc.setName(name);
+		vc.setMinSpeed(minSpeed);
+		vc.setMaxSpeed(maxSpeed);
+		vc.setCapacity(capacity);
+		vc.setBaseFuelUnitPrice(baseFuelUnitPrice);
+		vc.setDailyCharterPrice(40000);
+		
+		scenario.getFleetModel().getVesselClasses().add(vc);
+		
+		return vc;
+	}
+	
+	void createRandomCargoes(Scenario scenario, int cargoCount) {
+		//TODO this is not how it was before; load and discharge ports are more random; there will be less contention.
+		CargoFactory cf = CargoPackage.eINSTANCE.getCargoFactory();
+		EList<DistanceLine> lines = scenario.getDistanceModel().getDistances();
+		
+		long now = (new Date()).getTime();
+		
+		for (int i = 0; i<cargoCount; i++) {
+			//create a random cargo
+			Cargo c = cf.createCargo();
+			Slot load = cf.createSlot();
+			Slot discharge = cf.createSlot();
+			
+			//invalid distance lines shouldn't be in the model
+			final DistanceLine dl = lines.get(random.nextInt(lines.size()));
+			load.setPort(dl.getFromPort());
+			discharge.setPort(dl.getToPort());
+			
+			load.setId("load-" + i);
+			discharge.setId("discharge-"+i);
+			
+			final int minTime = (int) Math.ceil(dl.getDistance()/20/24);
+			final int maxTime = (int) Math.ceil(dl.getDistance()/12/24);
+			
+			final int start = random.nextInt(365);
+			final int end = 1+start+minTime + random.nextInt(maxTime - minTime + 15);
+			
+			Date startDate = new Date(now + start * Timer.ONE_DAY);
+			Date endDate = new Date(now + end * Timer.ONE_DAY);
+			
+			load.setWindowStart(startDate);
+			discharge.setWindowStart(endDate);
+			
+			load.setWindowDuration(1);
+			discharge.setWindowDuration(1);
+
+			discharge.setUnitPrice(170+random.nextInt(100));
+			load.setUnitPrice(discharge.getUnitPrice() - 20);
+			
+			load.setMinQuantity(0);
+			load.setMaxQuantity(200000);
+			
+			discharge.setMinQuantity(0);
+			discharge.setMaxQuantity(200000);
+			
+			c.setId("cargo-" + i);
+			c.setLoadSlot(load);
+			c.setDischargeSlot(discharge);
+			scenario.getCargoModel().getCargoes().add(c);
+		}
+	}
+}
---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/wizards/RandomScenarioWizard.java
@@ -0,0 +1,240 @@
+package com.mmxlabs.demo.app.wizards;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+import scenario.Scenario;
+import scenario.ScenarioPackage;
+import scenario.cargo.CargoPackage;
+import scenario.fleet.FleetPackage;
+import scenario.port.DistanceLine;
+import scenario.port.DistanceModel;
+import scenario.port.Port;
+import scenario.port.PortFactory;
+import scenario.port.PortPackage;
+import scenario.presentation.LngEditorAdvisor;
+
+import com.mmxlabs.demo.app.util.DistanceImporter;
+
+public class RandomScenarioWizard extends Wizard implements INewWizard {
+
+	private IWorkbench workbench;
+	private DetailsPage details;
+
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.workbench = workbench;
+		this.details = new DetailsPage("Details");
+	}
+
+
+	@Override
+	public void addPages() {
+		super.addPages();
+		addPage(details);
+	}
+
+
+
+	@Override
+	public boolean performFinish() {
+		final URI fileURI = details.getFileURI();
+
+		/*
+		 * Have to create scenario first, because otherwise it won't be registered with the resourcesetimpl
+		 * when we try to save.
+		 */
+		Scenario scenario = ScenarioPackage.eINSTANCE.getScenarioFactory().createScenario();
+		/*
+		 * Now load distance matrix and add to scenario
+		 */
+
+
+		try {
+			DistanceImporter di = new DistanceImporter(details.getMatrixURI().toFileString());
+			final PortFactory pf = PortPackage.eINSTANCE.getPortFactory();
+			scenario.setPortModel(pf.createPortModel());
+			//TODO put this somewhere more sensible
+			for (String s : di.getKeys()) {
+				Port port = pf.createPort();
+				port.setName(s);
+				scenario.getPortModel().getPorts().add(port);
+			}
+
+			//add useful default models
+			scenario.setFleetModel(FleetPackage.eINSTANCE.getFleetFactory().createFleetModel());
+			scenario.setCargoModel(CargoPackage.eINSTANCE.getCargoFactory().createCargoModel());
+
+			scenario.setDistanceModel(pf.createDistanceModel());
+			final DistanceModel dm = scenario.getDistanceModel();
+
+			for (Port a : scenario.getPortModel().getPorts()) {
+				for (Port b : scenario.getPortModel().getPorts()) {
+					final int distance = di.getDistance(a.getName(), b.getName());
+					if (!(a.equals(b)) && distance != Integer.MAX_VALUE) {
+						final DistanceLine dl = pf.createDistanceLine();
+						dl.setFromPort(a);
+						dl.setToPort(b);
+						dl.setDistance(distance);
+						dm.getDistances().add(dl);
+					}
+				}
+			}
+
+			//hack
+			RandomScenarioUtils utils = new RandomScenarioUtils();
+
+			if (details.shouldCreateVesselClasses()) {
+				utils.addStandardFleet(scenario);
+				utils.createRandomCargoes(scenario, details.getCargoCount());
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			return false;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return false;
+		}
+
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.createResource(fileURI);
+
+		resource.getContents().add(scenario);
+
+		try {
+			resource.save(new HashMap<Object, Object>());
+			System.err.println("Created " + fileURI.path());
+		} catch (IOException e) {
+			return false;
+		}
+
+		return LngEditorAdvisor.openEditor(workbench, fileURI);
+	}
+
+	class DetailsPage extends WizardPage {
+		protected DetailsPage(String pageName) {
+			super(pageName);
+		}
+
+		public URI getFileURI() {
+			String s = fileField.getText();
+
+			if (!s.endsWith(".scenario")) {
+				s = s + ".scenario";
+			}
+
+			return URI.createFileURI(s);
+		}
+
+		public URI getMatrixURI() {
+			return URI.createFileURI(matrixField.getText());
+		}
+
+		public boolean shouldCreateVesselClasses() {
+			return vesselClasses.getSelection();
+		}
+
+		public int getCargoCount() {
+			return cargoSpinner.getSelection();
+		}
+
+		protected Text fileField;
+		protected Text matrixField;
+		private Spinner cargoSpinner;
+		protected Button vesselClasses;
+
+		@Override
+		public void createControl(Composite parent) {
+			Composite myContents = new Composite(parent, SWT.NONE);
+			GridLayout layout = new GridLayout(1, false);
+			myContents.setLayout(layout);
+
+			fileField = makeElement(myContents,"Scenario file to create", "scenario", false);
+			matrixField = makeElement(myContents, "Distance matrix to import", "csv", true);
+
+			Label label = new Label(myContents, SWT.NONE);
+			label.setText("Other model features");
+			Composite row = new Composite(myContents, SWT.NONE);
+			row.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+			row.setLayout(new GridLayout(3, false));
+
+			vesselClasses = new Button(row, SWT.CHECK);
+
+			vesselClasses.setText("Add default Fleet Model");
+
+			label = new Label(row, SWT.NONE);
+			label.setText("Number of random cargo slots:");
+			cargoSpinner = new Spinner(row, SWT.NONE);
+
+			setControl(myContents);
+		}
+
+		public Text makeElement(Composite contents, String caption, String extension, boolean open) {
+			Label label = new Label(contents, SWT.NONE);
+			label.setText(caption);
+			return makeFilePicker(contents, new String[] {"*." + extension}, open);
+		}
+
+		public Text makeFilePicker(Composite container, final String[] extensions, final boolean open) {
+			Composite row = new Composite(container, SWT.NONE);
+
+			row.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+
+			row.setLayout(new GridLayout(2, false));
+
+			final Text t = new Text(row, SWT.BORDER);
+			t.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+			Button picker = new Button(row, SWT.PUSH);
+			picker.setLayoutData(new GridData(SWT.END, SWT.CENTER, false, false));
+
+			picker.setText("Choose");
+
+			picker.addSelectionListener(
+
+					new SelectionListener() {
+
+						@Override
+						public void widgetSelected(SelectionEvent e) {
+							FileDialog fd = 
+								new FileDialog(getShell(), open ? SWT.OPEN : SWT.SAVE);
+							fd.setFilterExtensions(extensions);
+							String sd = fd.open();
+							t.setText(sd);
+						}
+
+						@Override
+						public void widgetDefaultSelected(SelectionEvent e) {
+						}
+					}
+
+			);
+
+
+			return t;
+		}
+	}
+}
