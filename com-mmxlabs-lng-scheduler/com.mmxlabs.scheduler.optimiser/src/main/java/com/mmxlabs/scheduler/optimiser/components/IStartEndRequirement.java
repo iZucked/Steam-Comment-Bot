0554b055f68b7879b2c4b3a28f8d9cf82c7409da
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-25 13:13:18 -0100

    Add interface and implementation for start/end requirements

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/IStartEndRequirement.java
@@ -0,0 +1,30 @@
+package com.mmxlabs.scheduler.optimiser.components;
+
+/**
+ * A requirement that a vessel's journey start or end in a particular location at a particular time.
+ * 
+ * @author hinton
+ *
+ */
+public interface IStartEndRequirement {
+	/**
+	 * If the vessel must start/end at a given location this is true; otherwise false
+	 * @return
+	 */
+	public boolean hasPortRequirement();
+	/**
+	 * If and only if there is an earliest start time or latest arrival time this is true.
+	 * @return
+	 */
+	public boolean hasTimeRequirement();
+	/**
+	 * The earliest start time or latest arrival time for this vessel.
+	 * @return
+	 */
+	public int getTime();
+	/**
+	 * The location from which this vessel must start or to which it must return
+	 * @return
+	 */
+	public IPort getLocation();
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/StartEndRequirement.java
@@ -0,0 +1,42 @@
+package com.mmxlabs.scheduler.optimiser.components.impl;
+
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IStartEndRequirement;
+
+public class StartEndRequirement implements IStartEndRequirement {
+	IPort port;
+	boolean portIsSpecified;
+	int time;
+	boolean timeIsSpecified;
+	
+	public StartEndRequirement(IPort port, boolean portIsSpecified, int time,
+			boolean timeIsSpecified) {
+		super();
+		this.port = port;
+		this.portIsSpecified = portIsSpecified;
+		this.time = time;
+		this.timeIsSpecified = timeIsSpecified;
+	}
+
+	@Override
+	public boolean hasPortRequirement() {
+		return portIsSpecified;
+	}
+
+	@Override
+	public boolean hasTimeRequirement() {
+		return timeIsSpecified;
+	}
+
+	@Override
+	public int getTime() {
+		return time;
+	}
+
+	@Override
+	public IPort getLocation() {
+		return port;
+	}
+	
+	
+}
