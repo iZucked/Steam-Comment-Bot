f82e991b9926f57fcc6775c028ba6f9d35cb83a7
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-26 10:02:17 -0100

    Pair type. Handy for returning two associated things.

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Pair.java
@@ -0,0 +1,35 @@
+package com.mmxlabs.common;
+
+/**
+ * A class for holding a pair of heterogenous items
+ * @author hinton
+ *
+ * @param <A> the type of the first item
+ * @param <B> the type of the second item
+ */
+public class Pair<A, B> {
+	private A first;
+	private B second;
+	
+	public Pair() {
+		this(null, null);
+	}
+	
+	public Pair(A first, B second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+	public A getFirst() {
+		return first;
+	}
+	public void setFirst(A first) {
+		this.first = first;
+	}
+	public B getSecond() {
+		return second;
+	}
+	public void setSecond(B second) {
+		this.second = second;
+	}
+}
