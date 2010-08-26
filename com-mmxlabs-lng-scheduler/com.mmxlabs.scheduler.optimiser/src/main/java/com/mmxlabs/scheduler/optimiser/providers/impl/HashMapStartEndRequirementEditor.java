995e5a0f9e74f8534693d9ed7fa734ed3028db25
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-26 08:32:13 -0100

    Provide start/end elements as well as start/end requirements.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/providers/impl/HashMapStartEndRequirementEditor.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/providers/impl/HashMapStartEndRequirementEditor.java
@@ -6,11 +6,15 @@ import com.mmxlabs.optimiser.core.IResource;
 import com.mmxlabs.scheduler.optimiser.components.IStartEndRequirement;
 import com.mmxlabs.scheduler.optimiser.providers.IStartEndRequirementProviderEditor;
 
-public class HashMapStartEndRequirementEditor implements
-		IStartEndRequirementProviderEditor {
+public class HashMapStartEndRequirementEditor<T> implements
+		IStartEndRequirementProviderEditor<T> {
 
 	protected HashMap<IResource, IStartEndRequirement> startRequirements = new HashMap<IResource, IStartEndRequirement>();
 	protected HashMap<IResource, IStartEndRequirement> endRequirements = new HashMap<IResource, IStartEndRequirement>();
+	
+	protected HashMap<IResource, T> startElements = new HashMap<IResource, T>();
+	protected HashMap<IResource, T> endElements = new HashMap<IResource, T>();
+	
 	private final String name;
 	
 	public HashMapStartEndRequirementEditor(String name) {
@@ -36,6 +40,9 @@ public class HashMapStartEndRequirementEditor implements
 	public void dispose() {
 		startRequirements.clear();
 		endRequirements.clear();
+		
+		startElements.clear();
+		endElements.clear();
 	}
 
 	@Override
@@ -44,5 +51,21 @@ public class HashMapStartEndRequirementEditor implements
 			IStartEndRequirement endRequirement) {
 		startRequirements.put(resource, startRequirement);
 		endRequirements.put(resource, endRequirement);
+	}
+
+	@Override
+	public T getEndElement(IResource resource) {
+		return endElements.get(resource);
+	}
+
+	@Override
+	public T getStartElement(IResource resource) {
+		return startElements.get(resource);
+	}
+	
+	@Override
+	public void setStartEndElements(IResource resource, T startElement, T endElement) {
+		startElements.put(resource, startElement);
+		endElements.put(resource, endElement);
 	}
 }
