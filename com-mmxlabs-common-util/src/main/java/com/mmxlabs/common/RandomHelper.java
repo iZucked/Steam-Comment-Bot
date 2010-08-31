5bab7a687010d5726ad22936f8f3f4a1c65bf9d0
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-08-31 10:03:34 -0100

    A helper class for random number generation

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/RandomHelper.java
@@ -0,0 +1,61 @@
+package com.mmxlabs.common;
+
+import java.util.Random;
+
+/**
+ * A class which adds some convenience methods to a Random
+ * @author hinton
+ *
+ */
+public class RandomHelper extends Random {
+	private Random delegate;
+	public RandomHelper(Random delegate) {
+		this.delegate = delegate;
+	}
+	
+	/**
+	 * Return an integer between from 0 to n-1 which is not equal to d, drawn from a uniform distribution.
+	 * @param n
+	 * @param d
+	 * @return
+	 */
+	public int nextDifferentInt(int n, int d) {
+		final int k = nextInt(n-1);
+		if (k >= d) {
+			return k + 1;
+		} else {
+			return k;
+		}
+	}
+	
+	public void setSeed(long seed) {
+		delegate.setSeed(seed);
+	}
+	public void nextBytes(byte[] bytes) {
+		delegate.nextBytes(bytes);
+	}
+	public int nextInt() {
+		return delegate.nextInt();
+	}
+	public int nextInt(int n) {
+		return delegate.nextInt(n);
+	}
+	public String toString() {
+		return delegate.toString();
+	}
+	public long nextLong() {
+		return delegate.nextLong();
+	}
+	public boolean nextBoolean() {
+		return delegate.nextBoolean();
+	}
+	public float nextFloat() {
+		return delegate.nextFloat();
+	}
+	public double nextDouble() {
+		return delegate.nextDouble();
+	}
+	public double nextGaussian() {
+		return delegate.nextGaussian();
+	}
+}
