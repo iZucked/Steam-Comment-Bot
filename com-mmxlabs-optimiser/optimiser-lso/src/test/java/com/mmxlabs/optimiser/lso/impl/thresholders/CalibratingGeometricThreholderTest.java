862c63d8d5c9d224992634de5b963da5a75805fa
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-06 13:43:24 -0100

    Thresholder tests

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/thresholders/CalibratingGeometricThreholderTest.java
@@ -0,0 +1,44 @@
+package com.mmxlabs.optimiser.lso.impl.thresholders;
+
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalibratingGeometricThreholderTest {
+	@Test
+	public void testCalibration() {
+		//check that the calibration is OK
+		Random random = new Random();
+		CalibratingGeometricThresholder cgt = new CalibratingGeometricThresholder(random, 20, 0.75, 0.95);
+		cgt.init();
+		
+		int base = 2200;
+		
+		for (int i = 0; i<19; i++) {
+			cgt.accept(i + base);
+			Assert.assertFalse("Calibrated at step " + i, cgt.isCalibrated());
+			cgt.step();
+		}
+		cgt.accept(119);
+		cgt.step();
+		Assert.assertTrue("Is calibrated after 20 steps", cgt.isCalibrated());
+		
+		//test the calibrated values
+		
+		double roughTemperature = (-(base + 10)) / Math.log(0.75);
+		
+		//ten percent error because it's not a massively accurate solution procedure.
+		Assert.assertEquals("Calibrated temperature should be around " + (int) roughTemperature, roughTemperature, cgt.getCalibratedTemperature(), roughTemperature/10);
+		//test the acceptance rate
+		
+		int accepts = 0;
+		final int tests = 1000000;
+		for (int i = 0; i<tests; i++) {
+			long delta = base + i % 20;
+			if (cgt.accept(delta)) accepts++;
+		}
+		
+		Assert.assertEquals("Acceptance rate should be around 75%", 0.75, accepts/(double)tests, 0.15);
+	}
+}
---
+++ com-mmxlabs-optimiser/optimiser-lso/src/test/java/com/mmxlabs/optimiser/lso/impl/thresholders/GeometricThreholderTest.java
@@ -0,0 +1,39 @@
+package com.mmxlabs.optimiser.lso.impl.thresholders;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class GeometricThreholderTest {
+	@Test
+	public void testAcceptanceCases() {
+		//can't mock classes, sadly. Perhaps we should use a random interface instead.
+		final double p = 0.76;
+		Random mockRandom = new Random() {
+			@Override
+			public double nextDouble() {
+				return p;
+			}
+		};
+		final double mlnHalf = -Math.log(p);
+		final double initialTemperature = 100;
+		final double alpha = 0.95;
+		GeometricThresholder t = new GeometricThresholder(mockRandom, 2, initialTemperature, alpha);
+		t.init();
+		
+		Assert.assertEquals(initialTemperature, t.getTemperature());
+
+		Assert.assertFalse(t.accept((long) (mlnHalf * initialTemperature) + 1));
+		t.step();
+		Assert.assertTrue(t.accept((long) (mlnHalf * initialTemperature) - 1));
+		t.step();
+		Assert.assertEquals(initialTemperature * alpha, t.getTemperature());
+		t.step();
+		t.step();
+		Assert.assertEquals(initialTemperature * alpha * alpha, t.getTemperature());
+		Assert.assertTrue(t.accept((long) (mlnHalf * initialTemperature * alpha * alpha)  - 1));
+		Assert.assertFalse(t.accept((long) (mlnHalf * initialTemperature * alpha * alpha) + 1 ));
+	}
+}
