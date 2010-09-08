6bc2642bfe2fdb7b97e7f9e14e0ca7bb62635bb7
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-08 12:14:31 -0100

    Fix travel time calculation and apply formatting rules

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/constraints/impl/TravelTimeConstraintChecker.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/constraints/impl/TravelTimeConstraintChecker.java
@@ -8,6 +8,7 @@ import com.mmxlabs.optimiser.core.ISequences;
 import com.mmxlabs.optimiser.core.constraints.IPairwiseConstraintChecker;
 import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
 import com.mmxlabs.optimiser.core.scenario.common.IMultiMatrixProvider;
+import com.mmxlabs.scheduler.optimiser.Calculator;
 import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
 import com.mmxlabs.scheduler.optimiser.components.IPort;
 import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
@@ -16,20 +17,21 @@ import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 
 /**
- * A constraint checker which tests whether the ports in a sequence can
- * be reached from one another presuming the vessel travels at its maximum
- * speed all the way and spends a minimum amount of time at each point.
+ * A constraint checker which tests whether the ports in a sequence can be
+ * reached from one another presuming the vessel travels at its maximum speed
+ * all the way and spends a minimum amount of time at each point.
  * 
  * Currently only actually checks the pairwise constraint, because it is only
  * interesting to pairwise constraint users, but it might be useful to implement
- * in general as a quick first-pass rejection of routes which contain a leg which cannot
- * be made in the available time.
+ * in general as a quick first-pass rejection of routes which contain a leg
+ * which cannot be made in the available time.
  * 
  * @author hinton
- *
+ * 
  * @param <T>
  */
-public class TravelTimeConstraintChecker<T> implements IPairwiseConstraintChecker<T> {
+public class TravelTimeConstraintChecker<T> implements
+		IPairwiseConstraintChecker<T> {
 
 	private final String name;
 	private IOptimisationData<T> data;
@@ -58,13 +60,21 @@ public class TravelTimeConstraintChecker<T> implements IPairwiseConstraintChecke
 		return true;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void setOptimisationData(IOptimisationData<T> optimisationData) {
 		this.data = optimisationData;
-		this.portSlotProvider = data.getDataComponentProvider(SchedulerConstants.DCP_portSlotsProvider, IPortSlotProvider.class);
-		this.vesselProvider = data.getDataComponentProvider(SchedulerConstants.DCP_vesselProvider, IVesselProvider.class);
-		this.elementDurationProvider = data.getDataComponentProvider(SchedulerConstants.DCP_elementDurationsProvider, IElementDurationProvider.class);
-		this.distanceProvider = data.getDataComponentProvider(SchedulerConstants.DCP_portDistanceProvider, IMultiMatrixProvider.class);
+		this.portSlotProvider = data.getDataComponentProvider(
+				SchedulerConstants.DCP_portSlotsProvider,
+				IPortSlotProvider.class);
+		this.vesselProvider = data.getDataComponentProvider(
+				SchedulerConstants.DCP_vesselProvider, IVesselProvider.class);
+		this.elementDurationProvider = data.getDataComponentProvider(
+				SchedulerConstants.DCP_elementDurationsProvider,
+				IElementDurationProvider.class);
+		this.distanceProvider = data.getDataComponentProvider(
+				SchedulerConstants.DCP_portDistanceProvider,
+				IMultiMatrixProvider.class);
 	}
 
 	@Override
@@ -80,18 +90,20 @@ public class TravelTimeConstraintChecker<T> implements IPairwiseConstraintChecke
 	public boolean checkPairwiseConstraint(T first, T second, IResource resource) {
 		final IPortSlot slot1 = portSlotProvider.getPortSlot(first);
 		final IPortSlot slot2 = portSlotProvider.getPortSlot(second);
-		
+
 		final IVessel vessel = vesselProvider.getVessel(resource);
-		
-		final int travelTime = distanceProvider.get(IMultiMatrixProvider.Default_Key)
-			.get(slot1.getPort(), slot2.getPort()) / vessel.getVesselClass().getMaxSpeed();
-		
-		final int earliestArrivalTime = slot1.getTimeWindow().getStart() + 
-			elementDurationProvider.getElementDuration(first, resource) +
-			travelTime;
-		
+
+		final int travelTime = Calculator.getTimeFromSpeedDistance(
+				vessel.getVesselClass().getMaxSpeed(),
+				distanceProvider.get(IMultiMatrixProvider.Default_Key).get(
+						slot1.getPort(), slot2.getPort()));
+
+		final int earliestArrivalTime = slot1.getTimeWindow().getStart()
+				+ elementDurationProvider.getElementDuration(first, resource)
+				+ travelTime;
+
 		final int latestAllowableTime = slot2.getTimeWindow().getEnd();
-		
+
 		return earliestArrivalTime < latestAllowableTime;
 	}
 
