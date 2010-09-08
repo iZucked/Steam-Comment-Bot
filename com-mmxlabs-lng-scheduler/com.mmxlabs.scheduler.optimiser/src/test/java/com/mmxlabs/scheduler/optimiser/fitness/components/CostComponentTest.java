1759b7e088c395b7cd0e86883bd880dd3511d953
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-09-08 06:16:18 -0100

    Start implementing tests

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/fitness/components/CostComponentTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/fitness/components/CostComponentTest.java
@@ -2,7 +2,14 @@ package com.mmxlabs.scheduler.optimiser.fitness.components;
 
 import static org.junit.Assert.fail;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.mmxlabs.scheduler.optimiser.fitness.CargoSchedulerFitnessCore;
+import com.mmxlabs.scheduler.optimiser.voyage.FuelComponent;
 
 public class CostComponentTest {
 
@@ -23,7 +30,14 @@ public class CostComponentTest {
 
 	@Test
 	public void testCostComponent() {
-		fail("Not yet implemented");
-	}
+		final String name = "name";
+		final CargoSchedulerFitnessCore<Object> core = new CargoSchedulerFitnessCore<Object>();
+		final List<FuelComponent> fuelComponents = Collections.emptyList();
+		final CostComponent<Object> c = new CostComponent<Object>(name, fuelComponents,
+				core);
 
+		Assert.assertSame(name, c.getName());
+		Assert.assertSame(core, c.getFitnessCore());
+		Assert.assertEquals(fuelComponents, c.getFuelComponents());
+	}
 }
