a6b1d13ff9c1919f480c7ba3284be32b3cde16ec
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-09 08:18:33 -0100

    A manipulator which delegates to several other manipulators

---
+++ com-mmxlabs-optimiser/optimiser-core/src/main/java/com/mmxlabs/optimiser/core/impl/ChainedSequencesManipulator.java
@@ -0,0 +1,41 @@
+package com.mmxlabs.optimiser.core.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mmxlabs.optimiser.core.IModifiableSequences;
+import com.mmxlabs.optimiser.core.ISequencesManipulator;
+
+/**
+ * A sequences manipulator which applies a sequence of other sequences
+ * manipulators. Use the {@code addDelegate(..)} method to add another
+ * manipulator to the list of delegates.
+ * 
+ * @author hinton
+ * 
+ * @param <T>
+ */
+public class ChainedSequencesManipulator<T> implements ISequencesManipulator<T> {
+	List<ISequencesManipulator<T>> delegates = new ArrayList<ISequencesManipulator<T>>();
+
+	public void addDelegate(ISequencesManipulator<T> delegate) {
+		delegates.add(delegate);
+	}
+
+	@Override
+	public void manipulate(IModifiableSequences<T> sequences) {
+		for (ISequencesManipulator<T> manipulator : delegates) {
+			manipulator.manipulate(sequences);
+		}
+	}
+
+	@Override
+	public void dispose() {
+		for (ISequencesManipulator<T> manipulator : delegates) {
+			manipulator.dispose();
+		}
+		delegates.clear();
+		delegates = null;
+	}
+
+}
