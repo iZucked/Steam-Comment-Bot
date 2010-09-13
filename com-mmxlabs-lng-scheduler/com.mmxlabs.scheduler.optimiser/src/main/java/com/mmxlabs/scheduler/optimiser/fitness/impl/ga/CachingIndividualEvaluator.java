cb0a897955c8fb2b3c69c06ac76e33e3bdcfe538
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-13 15:04:39 -0100

    Trivial caching individual evaluator. Seems to save around 1/2 to 2/3 of the evaluations overall. Could benefit from (a) reuse of allocated memory and (b) sharing among evaluations (perhaps)

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/CachingIndividualEvaluator.java
@@ -0,0 +1,67 @@
+package com.mmxlabs.scheduler.optimiser.fitness.impl.ga;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * A memoizing evaluator; wraps another evaluator but adds a cache to make reevaluation
+ * efficient.
+ * 
+ * @author hinton
+ *
+ * @param <T>
+ */
+public class CachingIndividualEvaluator<T> implements IIndividualEvaluator<T> {
+	final IIndividualEvaluator<T> delegate;
+	
+	final int tableSize;
+	byte [][] occupants;
+	long []   values;
+	int  []   hits;
+	int  []   misses;
+	
+	int totalHits;
+	int totalMisses;
+	
+	public CachingIndividualEvaluator(IIndividualEvaluator<T> delegate, int size) {
+		super();
+		this.delegate = delegate;
+		this.tableSize = size;
+		occupants = new byte[size][];
+		values = new long[size];
+		misses = new int[size];
+		hits = new int[size];
+		totalHits = totalMisses = 0;
+	}
+
+	@Override
+	public long evaluate(Individual individual) {
+		final int hash = individual.hashBytes() % tableSize;
+//		System.out.println(totalHits / (0.0 + totalHits + totalMisses));
+		if (Arrays.equals(individual.bytes, occupants[hash])) {
+			hits[hash]++;
+			totalHits++;
+			return values[hash];
+		} else {
+			totalMisses++;
+			final long value = delegate.evaluate(individual);
+			
+			if (misses[hash] > hits[hash]) {
+				//replace
+				misses[hash] = 0;
+				hits[hash] = 1;
+				values[hash] = value;
+				occupants[hash] = individual.bytes.clone(); //is this a terrible idea?
+			} else {
+				misses[hash]++;
+			}
+			
+			return value;
+		}
+	}
+
+	@Override
+	public void dispose() {
+		delegate.dispose();
+	}
+}
