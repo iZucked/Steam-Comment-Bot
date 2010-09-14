ca83c908ba46f4c923c961b1a796d3529adf87b3
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-14 09:25:28 -0100

    Partially completed end location sequences manipulator; this may still require significant adjustment, as the inserted element has no time window, port type and so on. See ticket [touch:6]

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/manipulators/EndLocationSequenceManipulator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/manipulators/EndLocationSequenceManipulator.java
@@ -3,19 +3,36 @@ package com.mmxlabs.scheduler.optimiser.manipulators;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.optimiser.common.components.impl.TimeWindow;
 import com.mmxlabs.optimiser.core.IModifiableSequence;
 import com.mmxlabs.optimiser.core.IModifiableSequences;
 import com.mmxlabs.optimiser.core.IResource;
 import com.mmxlabs.optimiser.core.ISequencesManipulator;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
+import com.mmxlabs.scheduler.optimiser.components.impl.PortSlot;
+import com.mmxlabs.scheduler.optimiser.providers.IPortProviderEditor;
+import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProviderEditor;
 import com.mmxlabs.scheduler.optimiser.providers.IPortTypeProvider;
 import com.mmxlabs.scheduler.optimiser.providers.PortType;
 
+
 /**
  * The {@link EndLocationSequenceManipulator} replaces the end location with
- * another location. The end location must be a pre-defined "virtual" sequence
- * element. Each {@link IResource} can applied only one {@link EndLocationRule}.
+ * another location. The replacement location is chosen using one of the possible
+ * {@link EndLocationSequenceManipulator.EndLocationRule} values. The class needs to 
+ * be given an implementation of {@link EndLocationSequenceManipulator.IElementFactory},
+ * which it uses to construct the dummy sequence elements to be "swapped out".
  * 
- * @author Simon Goodall
+ * This is slightly ugly, because it has to directly alter the IOptimisationData's 
+ * data component providers, but hopefully using the provided factory to create dummy elements
+ * will ensure sufficient insulation from any awful consequences.
+ * 
+ * Actually, this is more difficult than it looks; all the auxiliary data structures
+ * for an element need to be created and updated accordingly; 
+ * 
+ * @author Simon Goodall, significantly modified by Tom Hinton
  * 
  * @param <T>
  *            Sequence element type.
@@ -24,13 +41,24 @@ public final class EndLocationSequenceManipulator<T> implements
 		ISequencesManipulator<T> {
 
 	/**
+	 * An interface used to allow the {@link EndLocationSequenceManipulator}
+	 * to create new dummy sequence elements to swap out for genuine end elements.
+	 * 
+	 * @author hinton
+	 *
+	 * @param <T>
+	 */
+	public interface IElementFactory<T> {
+		public T createElement();
+	}
+	
+	/**
 	 * An enum of the different end location rules that can be applied.
 	 * 
 	 * @author Simon Goodall
 	 * 
 	 */
 	public static enum EndLocationRule {
-
 		/**
 		 * No special rules to apply.
 		 */
@@ -48,20 +76,29 @@ public final class EndLocationSequenceManipulator<T> implements
 	}
 
 	private IPortTypeProvider<T> portTypeProvider;
+	
+	/**
+	 * This is needed both to find out the location to which we are returning,
+	 * and to set that as the location associated with the new dummy sequence element.
+	 */
+	private IPortProviderEditor portProviderEditor;
 
-	private final Map<T, T> elementMap = new HashMap<T, T>();
+	private IPortSlotProviderEditor<T> portSlotProviderEditor;
+	
+	private final Map<IResource, EndLocationRule> ruleMap = 
+		new HashMap<IResource, EndLocationSequenceManipulator.EndLocationRule>();
 
-	private final Map<IResource, EndLocationRule> ruleMap = new HashMap<IResource, EndLocationSequenceManipulator.EndLocationRule>();
+	private IElementFactory<T> elementFactory = null;
 
-	private final T virtualLocation;
+	private final Map<IResource, Pair<T, PortSlot>> dummyElements = 
+		new HashMap<IResource, Pair<T, PortSlot>>();
 
-	public EndLocationSequenceManipulator(final T virtualLocation) {
-		this.virtualLocation = virtualLocation;
+	public EndLocationSequenceManipulator(final IElementFactory<T> elementFactory) {
+		this.elementFactory = elementFactory;
 	}
 
 	@Override
 	public void manipulate(final IModifiableSequences<T> sequences) {
-
 		assert portTypeProvider != null;
 		
 		// Loop through each sequence in turn and manipulate
@@ -75,12 +112,15 @@ public final class EndLocationSequenceManipulator<T> implements
 			final IModifiableSequence<T> sequence) {
 
 		final EndLocationRule rule = ruleMap.get(resource);
+		if (rule == null) return;
 		switch (rule) {
 		case RETURN_TO_FIRST_LOAD:
-			returnToFirstLoadPort(resource, sequence);
+			adjustLastElement(resource, sequence,
+					getFirstLoadElement(sequence));
 			break;
 		case RETURN_TO_LAST_LOAD:
-			returnToLastLoadPort(resource, sequence);
+			adjustLastElement(resource, sequence,
+					getLastLoadElement(sequence));
 			break;
 		default:
 			break;
@@ -88,23 +128,14 @@ public final class EndLocationSequenceManipulator<T> implements
 	}
 
 	/**
-	 * Apply the {@link EndLocationRule#RETURN_TO_LAST_LOAD} rule. Iterate
-	 * through the sequence looking for {@link PortType#Load} and record the
-	 * last one in the sequence. Use this element as the key for the end
-	 * location replacement.
+	 * Find and return the last element in the sequence with {@link PortType} PortType.Load
 	 * 
-	 * @param resource
 	 * @param sequence
 	 */
-	public void returnToLastLoadPort(final IResource resource,
-			final IModifiableSequence<T> sequence) {
-
-		// Check last element is the virtual loc
-		final T last = sequence.get(sequence.size() - 1);
-		if (last != virtualLocation) {
-			return;
-		}
-
+	public T getLastLoadElement(final IModifiableSequence<T> sequence) {
+		/*
+		 * Search for the last load port
+		 */
 		T t = null;
 		for (final T element : sequence) {
 			final PortType type = portTypeProvider.getPortType(element);
@@ -113,33 +144,43 @@ public final class EndLocationSequenceManipulator<T> implements
 				t = element;
 			}
 		}
-
-		if (elementMap.containsKey(t)) {
-			final T newEndLocation = elementMap.get(t);
-			sequence.set(sequence.size() - 1, newEndLocation);
-		} else {
-			// Log a warning?
-		}
+		return t;
+//		adjustLastElement(resource, sequence, t);
 	}
 
 	/**
-	 * Apply the {@link EndLocationRule#RETURN_TO_FIRST_LOAD} rule. Iterate
-	 * through the sequence looking for {@link PortType#Load} and record the
-	 * last one in the sequence. Use this element as the key for the end
-	 * location replacement.
+	 * Swap the dummy element in for the given sequence, and set its location to the
+	 * given port.
+	 * @param resource 
 	 * 
-	 * @param resource
+	 * @param sequence
+	 * @param location
+	 */
+	private final void adjustLastElement(final IResource resource, 
+			final IModifiableSequence<T> sequence, 
+			final T returnElement) {
+		if (returnElement == null) return;
+		/*
+		 * Look up the port we are returning to, and then set that as the port
+		 * for the dummy element.
+		 */
+		final IPort returnPort = portProviderEditor.getPortForElement(returnElement);
+		final Pair<T, PortSlot> dummyElement = dummyElements.get(resource);
+		portProviderEditor.setPortForElement(returnPort, dummyElement.getFirst());
+		dummyElement.getSecond().setPort(returnPort);
+		
+		/*
+		 * Replace the final sequence element with the dummy
+		 */
+		sequence.set(sequence.size()-1, dummyElement.getFirst());
+	}
+	
+	/**
+	 * Find and return the first element in the sequence with {@link PortType} PortType.Load
+	 * 
 	 * @param sequence
 	 */
-	public void returnToFirstLoadPort(final IResource resource,
-			final IModifiableSequence<T> sequence) {
-
-		// Check last element is the virtual loc
-		final T last = sequence.get(sequence.size() - 1);
-		if (last != virtualLocation) {
-			return;
-		}
-
+	public T getFirstLoadElement(final IModifiableSequence<T> sequence) {
 		T t = null;
 		for (final T element : sequence) {
 			final PortType type = portTypeProvider.getPortType(element);
@@ -149,13 +190,7 @@ public final class EndLocationSequenceManipulator<T> implements
 				break;
 			}
 		}
-
-		if (elementMap.containsKey(t)) {
-			final T newEndLocation = elementMap.get(t);
-			sequence.set(sequence.size() - 1, newEndLocation);
-		} else {
-			// Log a warning?
-		}
+		return t;
 	}
 	
 	public IPortTypeProvider<T> getPortTypeProvider() {
@@ -166,28 +201,21 @@ public final class EndLocationSequenceManipulator<T> implements
 		this.portTypeProvider = portTypeProvider;
 	}
 
-	/**
-	 * Specify that if the given sequence element is the element used to specify
-	 * the end location, then use the given end location as the new end location
-	 * 
-	 * @param element
-	 * @param endLocation
-	 */
-	public void setElementMapping(final T element, final T endLocation) {
-		elementMap.put(element, endLocation);
+	public IPortProviderEditor getPortProviderEditor() {
+		return portProviderEditor;
 	}
 
-	/**
-	 * Returns the end location mapped to this element.
-	 * 
-	 * @param element
-	 * @return
-	 */
-	public T getElementMapping(final T element) {
-		if (elementMap.containsKey(element)) {
-			return elementMap.get(element);
-		}
-		return null;
+	public void setPortProviderEditor(IPortProviderEditor portProviderEditor) {
+		this.portProviderEditor = portProviderEditor;
+	}
+
+	public IPortSlotProviderEditor<T> getPortSlotProviderEditor() {
+		return portSlotProviderEditor;
+	}
+
+	public void setPortSlotProviderEditor(
+			IPortSlotProviderEditor<T> portSlotProviderEditor) {
+		this.portSlotProviderEditor = portSlotProviderEditor;
 	}
 
 	/**
@@ -198,6 +226,17 @@ public final class EndLocationSequenceManipulator<T> implements
 	 */
 	public void setEndLocationRule(final IResource resource,
 			final EndLocationRule rule) {
+		if (ruleMap.containsKey(resource) == false) {
+			/* 
+			 * Set up a dummy element for this resource,
+			 * because we might need it
+			 */
+			T element = elementFactory.createElement();
+			PortSlot slot = new PortSlot();
+			slot.setId(resource.getName() + "-return");
+			portSlotProviderEditor.setPortSlot(element, slot);
+			dummyElements.put(resource, new Pair<T, PortSlot>(element, slot));
+		}
 		ruleMap.put(resource, rule);
 	}
 
@@ -215,13 +254,8 @@ public final class EndLocationSequenceManipulator<T> implements
 		return EndLocationRule.NONE;
 	}
 
-	public T getVirtualLocation() {
-		return virtualLocation;
-	}
-
 	@Override
 	public void dispose() {
-		elementMap.clear();
 		ruleMap.clear();
 	}
 }
