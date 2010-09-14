61220a9a96f9d02130f3aed0ae74ae73bb1a4233
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-14 09:00:58 -0100

    Make manipulator work using resources instead of vessel instance type; makes it a bit quicker. Resources are picked using the new SequencesManipulatorUtil (according to VIT, as before)

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/manipulators/StartLocationRemovingSequenceManipulator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/manipulators/StartLocationRemovingSequenceManipulator.java
@@ -1,6 +1,8 @@
 package com.mmxlabs.scheduler.optimiser.manipulators;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.mmxlabs.optimiser.core.IModifiableSequence;
 import com.mmxlabs.optimiser.core.IModifiableSequences;
@@ -12,7 +14,9 @@ import com.mmxlabs.scheduler.optimiser.components.VesselInstanceType;
 import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 
 /**
- * A sequence manipulator which removes the dummy start location from spot charter vessels
+ * A sequence manipulator for removing the dummy start location from spot charter vessels.
+ * Needs to be told which resources should have their start locations removed.
+ * 
  * @author hinton
  *
  * @param <T>
@@ -20,13 +24,11 @@ import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 public class StartLocationRemovingSequenceManipulator<T> implements
 		ISequencesManipulator<T> {
 	
-	private IVesselProvider vesselProvider;
+	private final Set<IResource> resourcesToManipulate = 
+		new HashSet<IResource>();
 	
 	@Override
 	public void manipulate(IModifiableSequences<T> sequences) {
-		if (vesselProvider == null) {
-			throw new RuntimeException("Vessel Provider has not been set in StartLocationRemovingSequenceManipulator");
-		}
 		// Loop through each sequence in turn and manipulate
 		for (final Map.Entry<IResource, IModifiableSequence<T>> entry : sequences
 				.getModifiableSequences().entrySet()) {
@@ -35,28 +37,32 @@ public class StartLocationRemovingSequenceManipulator<T> implements
 	}
 
 	private void manipulate(IResource resource, IModifiableSequence<T> sequence) {
-		if (vesselProvider.getVessel(resource).getVesselInstanceType().
-				equals(VesselInstanceType.SPOT_CHARTER)) {
-			// Remove start element
+		if (getShouldRemoveStartLocation(resource)) {
 			sequence.remove(0);
 		}
 	}
 
 	@Override
 	public void dispose() {
-		vesselProvider = null;
+		resourcesToManipulate.clear();
 	}
 
-	public IVesselProvider getVesselProvider() {
-		return vesselProvider;
+	/**
+	 * Set whether the given resource should have its start element removed (typically
+	 * this will be if it is a spot charter vessel).
+	 * 
+	 * @param resource
+	 * @param shouldRemoveStartLocation if true, start location will be removed, otherwise not.
+	 */
+	public void setShouldRemoveStartLocation(final IResource resource,
+			final boolean shouldRemoveStartLocation) {
+		if (shouldRemoveStartLocation)
+			resourcesToManipulate.add(resource);
+		else
+			resourcesToManipulate.remove(resource);
 	}
-
-	public void setVesselProvider(IVesselProvider vesselProvider) {
-		this.vesselProvider = vesselProvider;
-	}
-
-	public void setOptimisationData(IOptimisationData<T> data) {
-		setVesselProvider(data.getDataComponentProvider(
-				SchedulerConstants.DCP_vesselProvider, IVesselProvider.class));
+	
+	public boolean getShouldRemoveStartLocation(final IResource resource) {
+		return resourcesToManipulate.contains(resource);
 	}
 }
