8d3f98dc70c15a7b81ca97d8ab57ac3539fdd4f6
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-16 13:32:48 -0100

    equals and hashcode to support guava cache

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/Individual.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/Individual.java
@@ -1,5 +1,7 @@
 package com.mmxlabs.scheduler.optimiser.fitness.impl.ga;
 
+import java.util.Arrays;
+
 /**
  * Obsolete as just a byte array?
  * 
@@ -10,7 +12,7 @@ package com.mmxlabs.scheduler.optimiser.fitness.impl.ga;
  * @author Simon Goodall
  * 
  */
-public final class Individual {
+public final class Individual implements Cloneable {
 
 	public final byte[] bytes;
 
@@ -18,7 +20,36 @@ public final class Individual {
 		this.bytes = bytes;
 	}
 	
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(bytes);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Individual) {
+			final Individual i = (Individual) obj;
+			return Arrays.equals(i.bytes, bytes);
+		}
+		return false;
+	}
+
+	@Override
+	protected Object clone() {
+		return new Individual(bytes.clone());
+	}
+
 	public final int hashBytes() {
+		return externalHashBytes();
+	}
+	
+	public final int externalHashBytes() {
+		final int hash = Arrays.hashCode(bytes);
+		return  (hash ^ (hash>>31)) - (hash>>31);
+	}
+	
+	
+	public final int internalHashBytes() {
 		int hash = 9;
 		final int multiplier = 17;
 		for (int i = 0; i<bytes.length; i+=2) {
@@ -27,4 +58,5 @@ public final class Individual {
 		//make positive (handy trick)
 		return  (hash ^ (hash>>31)) - (hash>>31);
 	}
+	
 }
