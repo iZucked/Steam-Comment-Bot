d3828c898f5c483fa9c619adbf8d0bafce8fc5a0
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-16 13:32:33 -0100

    Use guava mapmapker instead of handmade cache

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/CachingIndividualEvaluator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/CachingIndividualEvaluator.java
@@ -1,6 +1,11 @@
 package com.mmxlabs.scheduler.optimiser.fitness.impl.ga;
 
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Function;
+import com.google.common.collect.MapMaker;
 
 /**
  * A memoizing evaluator; wraps another evaluator but adds a cache to make reevaluation
@@ -10,57 +15,80 @@ import java.util.Arrays;
  *
  * @param <T>
  */
-public class CachingIndividualEvaluator<T> implements IIndividualEvaluator<T> {
+public final class CachingIndividualEvaluator<T> implements IIndividualEvaluator<T> {
 	final IIndividualEvaluator<T> delegate;
 	
-	final int tableSize;
+
+	
+	private final ConcurrentMap<Individual, Long> cache = 
+		new MapMaker()
+			.concurrencyLevel(1)
+//			.softKeys()
+			.weakValues()
+			.expiration(1, TimeUnit.MINUTES)
+			.makeComputingMap(new Function<Individual, Long>() {
+				@Override
+				public Long apply(Individual arg) {
+					return delegate.evaluate(arg);
+				}
+			});
+		
+	
+	/*final int tableSize;
 	byte [][] occupants;
 	long []   values;
 	int  []   hits;
 	int  []   misses;
 	
 	int totalHits;
-	int totalMisses;
+	int totalMisses;*/
 	
 	public CachingIndividualEvaluator(IIndividualEvaluator<T> delegate, int size) {
 		super();
+		
+		
 		this.delegate = delegate;
-		this.tableSize = size;
+		/*this.tableSize = size;
 		occupants = new byte[size][];
 		values = new long[size];
 		misses = new int[size];
 		hits = new int[size];
 		totalHits = totalMisses = 0;
+		*/
 	}
 
 	@Override
 	public long evaluate(Individual individual) {
-		final int hash = individual.hashBytes() % tableSize;
-//		System.out.println(totalHits / (0.0 + totalHits + totalMisses));
-		if (Arrays.equals(individual.bytes, occupants[hash])) {
-			hits[hash]++;
-			totalHits++;
-			return values[hash];
-		} else {
-			totalMisses++;
-			final long value = delegate.evaluate(individual);
-			
-			if (misses[hash] > hits[hash]) {
-				//replace
-				misses[hash] = 0;
-				hits[hash] = 1;
-				values[hash] = value;
-				occupants[hash] = individual.bytes.clone(); //is this a terrible idea?
-			} else {
-				misses[hash]++;
-			}
-			
-			return value;
-		}
+		return cache.get(individual.clone());
+		
+		
+//		final int hash = individual.hashBytes() % tableSize;
+//		if (Arrays.equals(individual.bytes, occupants[hash])) {
+//			hits[hash]++;
+//			totalHits++;
+//			misses[hash] = 0;
+//			return values[hash];
+//		} else {
+//			totalMisses++;
+//			final long value = delegate.evaluate(individual);
+//			
+//			if (misses[hash] > 10) {
+//				//replace
+//				misses[hash] = 0;
+//				hits[hash] = 1;
+//				values[hash] = value;
+//				occupants[hash] = individual.bytes.clone(); //is this a terrible idea?
+//			} else {
+//				misses[hash]++;
+//			}
+//			
+//			return value;
+//		}
 	}
 
 	@Override
 	public void dispose() {
 		delegate.dispose();
+		cache.clear();
 	}
 }
