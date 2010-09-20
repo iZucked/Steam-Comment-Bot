1c35c90deffb7cdab6f133d836ba4a939c96066d
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-20 11:41:07 -0100

    Set up sequence manipulators

--- com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/impl/OptManagedJob.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/impl/OptManagedJob.java
@@ -40,8 +40,8 @@ import com.mmxlabs.scheduler.optimiser.providers.IPortProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IPortTypeProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
-import com.mmxlabs.scheduler.optimiser.voyage.IVoyagePlan;
 import com.mmxlabs.scheduler.optimiser.voyage.impl.LNGVoyageCalculator;
+import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlan;
 import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlanAnnotator;
 
 //TODO Generate a base class and provide some methods for job creation etc.
@@ -467,7 +467,7 @@ public class OptManagedJob implements IManagedJob {
 				final ISequence<ISequenceElement> sequence = entry.getValue();
 
 				// Schedule sequence
-				final List<IVoyagePlan> plans = scheduler
+				final List<VoyagePlan> plans = scheduler
 						.schedule(resource, sequence);
 
 				if (plans == null) {
