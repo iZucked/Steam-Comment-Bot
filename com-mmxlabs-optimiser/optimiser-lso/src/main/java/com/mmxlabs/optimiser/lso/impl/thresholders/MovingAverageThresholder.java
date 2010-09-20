20e9d3bf41bdd36716ca550c2cd2a2d113a17193
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-20 11:45:20 -0100

    Another self-calibrating thresholder, which might work better

---
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/impl/thresholders/MovingAverageThresholder.java
@@ -0,0 +1,61 @@
+package com.mmxlabs.optimiser.lso.impl.thresholders;
+
+import java.util.Random;
+
+import com.mmxlabs.optimiser.lso.IThresholder;
+
+public class MovingAverageThresholder implements IThresholder {
+	private long[] window;
+	private double sum = 0;
+	private int front = 0;
+	private double mlogAlpha;
+	final double alpha0;
+	final double cooling;
+	final private Random random;
+	int tick = 0;
+	int epoch = 0;
+	private final int epochLength;
+	
+	public MovingAverageThresholder(Random random, double initialAcceptance, double cooling, int epochLength, int window) {
+		this.random = random;
+		this.window = new long[window];
+		this.alpha0 = initialAcceptance;
+		this.cooling = cooling;
+		this.epochLength = epochLength;
+	}
+	
+	@Override
+	public void init() {
+		tick = 0;
+		epoch = 0;
+	}
+
+	@Override
+	public boolean accept(long delta) {
+		if (delta < 0) return true;
+		
+		sum -= window[front];
+		sum += delta;
+		window[front] = delta;
+		front = (front+1)%window.length;
+		
+		final double mean = sum/window.length;
+		
+		final double T = mlogAlpha/mean;
+		return random.nextDouble() < Math.exp(-delta/T);
+	}
+
+	@Override
+	public void step() {
+		if (tick == epochLength) {
+			tick = 0;
+			epoch++;
+		} else {
+			if (tick == 0) {
+				mlogAlpha = -Math.log(alpha0 * Math.pow(cooling, epoch));
+				System.err.println("Epoch " + epoch + ", alpha = " + alpha0 *Math.pow(cooling, epoch));
+			}
+			tick++;
+		}
+	}
+}
