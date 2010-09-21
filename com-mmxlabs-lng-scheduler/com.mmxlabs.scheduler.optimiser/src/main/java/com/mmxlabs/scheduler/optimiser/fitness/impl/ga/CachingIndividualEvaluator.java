6ed67eae0af0126c05ca918ae8189c09f3f32952
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-21 12:43:08 -0100

    Set initial cache size correctly, and add clear method, so that client classes can avoid instantiating a new cache every time.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/CachingIndividualEvaluator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/ga/CachingIndividualEvaluator.java
@@ -8,87 +8,47 @@ import com.google.common.base.Function;
 import com.google.common.collect.MapMaker;
 
 /**
- * A memoizing evaluator; wraps another evaluator but adds a cache to make reevaluation
- * efficient.
+ * A memoizing evaluator; wraps another evaluator but adds a cache to make
+ * reevaluation efficient.
  * 
  * @author hinton
- *
+ * 
  * @param <T>
  */
-public final class CachingIndividualEvaluator<T> implements IIndividualEvaluator<T> {
+public final class CachingIndividualEvaluator<T> implements
+		IIndividualEvaluator<T> {
 	final IIndividualEvaluator<T> delegate;
-	
 
-	
-	private final ConcurrentMap<Individual, Long> cache = 
-		new MapMaker()
-			.concurrencyLevel(1)
-//			.softKeys()
-			.weakValues()
-			.expiration(1, TimeUnit.MINUTES)
-			.makeComputingMap(new Function<Individual, Long>() {
-				@Override
-				public Long apply(Individual arg) {
-					return delegate.evaluate(arg);
-				}
-			});
-		
-	
-	/*final int tableSize;
-	byte [][] occupants;
-	long []   values;
-	int  []   hits;
-	int  []   misses;
-	
-	int totalHits;
-	int totalMisses;*/
-	
-	public CachingIndividualEvaluator(IIndividualEvaluator<T> delegate, int size) {
+	private final ConcurrentMap<Individual, Long> cache;
+
+	public CachingIndividualEvaluator(final IIndividualEvaluator<T> delegate,
+			final int size) {
 		super();
-		
-		
+
+		cache = new MapMaker().concurrencyLevel(1).weakValues()
+				.initialCapacity(size).expiration(1, TimeUnit.MINUTES)
+				.makeComputingMap(new Function<Individual, Long>() {
+					@Override
+					public Long apply(Individual arg) {
+						return delegate.evaluate(arg);
+					}
+				});
+
 		this.delegate = delegate;
-		/*this.tableSize = size;
-		occupants = new byte[size][];
-		values = new long[size];
-		misses = new int[size];
-		hits = new int[size];
-		totalHits = totalMisses = 0;
-		*/
 	}
 
 	@Override
 	public long evaluate(Individual individual) {
 		return cache.get(individual.clone());
-		
-		
-//		final int hash = individual.hashBytes() % tableSize;
-//		if (Arrays.equals(individual.bytes, occupants[hash])) {
-//			hits[hash]++;
-//			totalHits++;
-//			misses[hash] = 0;
-//			return values[hash];
-//		} else {
-//			totalMisses++;
-//			final long value = delegate.evaluate(individual);
-//			
-//			if (misses[hash] > 10) {
-//				//replace
-//				misses[hash] = 0;
-//				hits[hash] = 1;
-//				values[hash] = value;
-//				occupants[hash] = individual.bytes.clone(); //is this a terrible idea?
-//			} else {
-//				misses[hash]++;
-//			}
-//			
-//			return value;
-//		}
 	}
 
 	@Override
 	public void dispose() {
 		delegate.dispose();
+		cache.clear();
+	}
+
+	public void clearCache() {
 		cache.clear();
 	}
 }
