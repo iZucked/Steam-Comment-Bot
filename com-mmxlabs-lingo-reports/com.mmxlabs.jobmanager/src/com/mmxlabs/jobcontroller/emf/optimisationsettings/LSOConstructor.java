d984e7b99e2865571acfb0432e9dc4594a1d0af0
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-09-21 12:37:32 -0100

    Clean up imports

--- com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/optimisationsettings/LSOConstructor.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/optimisationsettings/LSOConstructor.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import scenario.optimiser.lso.LSOSettings;
 import scenario.optimiser.lso.MoveGeneratorSettings;
 import scenario.optimiser.lso.RandomMoveGeneratorSettings;
+import scenario.optimiser.lso.ThresholderSettings;
 
 import com.mmxlabs.optimiser.core.IOptimisationContext;
 import com.mmxlabs.optimiser.core.ISequencesManipulator;
@@ -17,15 +18,14 @@ import com.mmxlabs.optimiser.core.fitness.IFitnessComponent;
 import com.mmxlabs.optimiser.core.fitness.IFitnessEvaluator;
 import com.mmxlabs.optimiser.core.fitness.impl.FitnessComponentInstantiator;
 import com.mmxlabs.optimiser.core.fitness.impl.FitnessHelper;
-import com.mmxlabs.optimiser.core.impl.ChainedSequencesManipulator;
 import com.mmxlabs.optimiser.lso.IMoveGenerator;
 import com.mmxlabs.optimiser.lso.IThresholder;
 import com.mmxlabs.optimiser.lso.impl.DefaultLocalSearchOptimiser;
 import com.mmxlabs.optimiser.lso.impl.LinearFitnessCombiner;
 import com.mmxlabs.optimiser.lso.impl.LinearSimulatedAnnealingFitnessEvaluator;
 import com.mmxlabs.optimiser.lso.impl.LocalSearchOptimiser;
-import com.mmxlabs.optimiser.lso.impl.thresholders.CalibratingGeometricThresholder;
 import com.mmxlabs.optimiser.lso.impl.thresholders.InstrumentingThresholder;
+import com.mmxlabs.optimiser.lso.impl.thresholders.MovingAverageThresholder;
 import com.mmxlabs.optimiser.lso.movegenerators.impl.InstrumentingMoveGenerator;
 import com.mmxlabs.optimiser.lso.movegenerators.impl.Move2over2GeneratorUnit;
 import com.mmxlabs.optimiser.lso.movegenerators.impl.Move3over2GeneratorUnit;
@@ -33,13 +33,12 @@ import com.mmxlabs.optimiser.lso.movegenerators.impl.Move4over1GeneratorUnit;
 import com.mmxlabs.optimiser.lso.movegenerators.impl.Move4over2GeneratorUnit;
 import com.mmxlabs.optimiser.lso.movegenerators.impl.RandomMoveGenerator;
 import com.mmxlabs.scheduler.optimiser.lso.ConstrainedMoveGenerator;
-import com.mmxlabs.scheduler.optimiser.manipulators.EndLocationSequenceManipulator;
-import com.mmxlabs.scheduler.optimiser.manipulators.StartLocationRemovingSequenceManipulator;
 
 /**
  * A class for converting the settings in the EMF into optimisation jobs
+ * 
  * @author hinton
- *
+ * 
  */
 public class LSOConstructor {
 	private LSOSettings settings;
@@ -48,20 +47,23 @@ public class LSOConstructor {
 	public LSOConstructor(LSOSettings settings) {
 		this.settings = settings;
 	}
-	
+
 	/**
 	 * Build an LSO according to the LSO settings passed in
+	 * 
 	 * @param <T>
 	 * @param context
 	 * @return
 	 */
-	public <T> LocalSearchOptimiser<T> buildOptimiser(final IOptimisationContext<T> context, final ISequencesManipulator<T> manipulator) {
+	public <T> LocalSearchOptimiser<T> buildOptimiser(
+			final IOptimisationContext<T> context,
+			final ISequencesManipulator<T> manipulator) {
 		final ConstraintCheckerInstantiator constraintCheckerInstantiator = new ConstraintCheckerInstantiator();
 		final List<IConstraintChecker<T>> constraintCheckers = constraintCheckerInstantiator
 				.instantiateConstraintCheckers(
 						context.getConstraintCheckerRegistry(),
 						context.getConstraintCheckers());
-		
+
 		for (final IConstraintChecker<T> checker : constraintCheckers) {
 			checker.setOptimisationData(context.getOptimisationData());
 		}
@@ -70,65 +72,73 @@ public class LSOConstructor {
 		final List<IFitnessComponent<T>> fitnessComponents = fitnessComponentInstantiator
 				.instantiateFitnesses(context.getFitnessFunctionRegistry(),
 						context.getFitnessComponents());
-		
-		final IMoveGenerator<T> moveGenerator =
-			createMoveGenerator(context);
-		
-		
-		final InstrumentingMoveGenerator<T> instrumentingMoveGenerator =
-			instrumenting  ?
-			new InstrumentingMoveGenerator<T>(moveGenerator, 
-					false //profile moves (true) or just rate (false)
-					)
-			: null;
-		
-		final IFitnessEvaluator<T> fitnessEvaluator = 
-			createFitnessEvaluator(fitnessComponents, instrumentingMoveGenerator);
-		
+
+		final IMoveGenerator<T> moveGenerator = createMoveGenerator(context);
+
+		final InstrumentingMoveGenerator<T> instrumentingMoveGenerator = instrumenting ? new InstrumentingMoveGenerator<T>(
+				moveGenerator, false // profile moves (true) or just rate
+										// (false)
+		)
+				: null;
+
+		final IFitnessEvaluator<T> fitnessEvaluator = createFitnessEvaluator(
+				fitnessComponents, instrumentingMoveGenerator);
+
 		final DefaultLocalSearchOptimiser<T> lso = new DefaultLocalSearchOptimiser<T>();
 
 		lso.setNumberOfIterations(getNumberOfIterations());
 		lso.setSequenceManipulator(manipulator);
-		
-		lso.setMoveGenerator(instrumenting ? instrumentingMoveGenerator : moveGenerator);
+
+		lso.setMoveGenerator(instrumenting ? instrumentingMoveGenerator
+				: moveGenerator);
 		lso.setFitnessEvaluator(fitnessEvaluator);
 		lso.setConstraintCheckers(constraintCheckers);
 
-//		lso.setProgressMonitor(monitor);
+		// lso.setProgressMonitor(monitor);
 		lso.setReportInterval(Math.max(10, getNumberOfIterations() / 100));
 
-//		lso.init();
+		// lso.init();
 
 		return lso;
 	}
-	
-	private <T> IMoveGenerator<T> createMoveGenerator(IOptimisationContext<T> context) {
-		final MoveGeneratorSettings generatorSettings = settings.getMoveGeneratorSettings();
-		if (generatorSettings != null && generatorSettings instanceof RandomMoveGeneratorSettings) {
-			//Ideally this code should go in the EMF classes, to be honest.
+
+	private <T> IMoveGenerator<T> createMoveGenerator(
+			IOptimisationContext<T> context) {
+		final MoveGeneratorSettings generatorSettings = settings
+				.getMoveGeneratorSettings();
+		if (generatorSettings != null
+				&& generatorSettings instanceof RandomMoveGeneratorSettings) {
+			// Ideally this code should go in the EMF classes, to be honest.
 			final RandomMoveGeneratorSettings rmgs = (RandomMoveGeneratorSettings) generatorSettings;
 			final RandomMoveGenerator<T> moveGenerator = new RandomMoveGenerator<T>();
 			moveGenerator.setRandom(getRandom());
 			if (rmgs.isUsing2over2())
-				moveGenerator.addMoveGeneratorUnit(new Move2over2GeneratorUnit<T>());
+				moveGenerator
+						.addMoveGeneratorUnit(new Move2over2GeneratorUnit<T>());
 			if (rmgs.isUsing3over2())
-				moveGenerator.addMoveGeneratorUnit(new Move3over2GeneratorUnit<T>());
+				moveGenerator
+						.addMoveGeneratorUnit(new Move3over2GeneratorUnit<T>());
 			if (rmgs.isUsing4over2())
-				moveGenerator.addMoveGeneratorUnit(new Move4over2GeneratorUnit<T>());
+				moveGenerator
+						.addMoveGeneratorUnit(new Move4over2GeneratorUnit<T>());
 			if (rmgs.isUsing4over1())
-				moveGenerator.addMoveGeneratorUnit(new Move4over1GeneratorUnit<T>());
-			
+				moveGenerator
+						.addMoveGeneratorUnit(new Move4over1GeneratorUnit<T>());
+
 			return moveGenerator;
 		} else {
-			final ConstrainedMoveGenerator<T> cmg = new ConstrainedMoveGenerator<T>(context);
+			final ConstrainedMoveGenerator<T> cmg = new ConstrainedMoveGenerator<T>(
+					context);
 			cmg.setRandom(getRandom());
 			return cmg;
 		}
 	}
-	
-	private <T> IFitnessEvaluator<T> createFitnessEvaluator(List<IFitnessComponent<T>> fitnessComponents, InstrumentingMoveGenerator<T> img) {
-		//create a linear FE for now.
-		
+
+	private <T> IFitnessEvaluator<T> createFitnessEvaluator(
+			List<IFitnessComponent<T>> fitnessComponents,
+			InstrumentingMoveGenerator<T> img) {
+		// create a linear FE for now.
+
 		final FitnessHelper<T> fitnessHelper = new FitnessHelper<T>();
 
 		final LinearSimulatedAnnealingFitnessEvaluator<T> fitnessEvaluator = new LinearSimulatedAnnealingFitnessEvaluator<T>();
@@ -146,11 +156,9 @@ public class LSOConstructor {
 
 		fitnessEvaluator.setFitnessCombiner(combiner);
 
-		final IThresholder thresholder = 
-			instrumenting ? 
-			new InstrumentingThresholder( getThresholder(), img )
-			: getThresholder();
-			
+		final IThresholder thresholder = instrumenting ? new InstrumentingThresholder(
+				getThresholder(), img) : getThresholder();
+
 		fitnessEvaluator.setThresholder(thresholder);
 
 		fitnessEvaluator.init();
@@ -159,11 +167,9 @@ public class LSOConstructor {
 	}
 
 	private IThresholder getThresholder() {
-		//For now we are just going to generate a self-calibrating thresholder
-		return new CalibratingGeometricThresholder(getRandom(), 
-				settings.getThresholderSettings().getEpochLength(),
-				settings.getThresholderSettings().getInitialAcceptanceRate(), 
-				settings.getThresholderSettings().getAlpha());
+		// For now we are just going to generate a self-calibrating thresholder
+		final ThresholderSettings ts = settings.getThresholderSettings();
+		return new MovingAverageThresholder(getRandom(), ts.getInitialAcceptanceRate(), ts.getAlpha(), ts.getEpochLength(), 500);
 	}
 
 	private Random getRandom() {
