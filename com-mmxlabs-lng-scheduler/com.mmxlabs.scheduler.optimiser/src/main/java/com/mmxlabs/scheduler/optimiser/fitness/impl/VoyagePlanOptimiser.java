72d2f8fb534c5b8c3accea4aff89cff7d477f11c
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-09-23 05:48:07 -0100

    Fix typo

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/VoyagePlanOptimiser.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/VoyagePlanOptimiser.java
@@ -154,7 +154,7 @@ public final class VoyagePlanOptimiser<T> implements IVoyagePlanOptimiser<T> {
 	/**
 	 * Evaluates the current sequences from the current choice set. Optionally,
 	 * the last voyage can be further optimised to find the best arrival time
-	 * that miminises cost.
+	 * that minimises cost.
 	 * 
 	 * @param optimiseLastLeg
 	 */
