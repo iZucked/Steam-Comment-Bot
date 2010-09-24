8f653cc929ae2f5219edc1f340a0a90c65e1f76d
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-09-24 05:29:54 -0100

    Doc

--- com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/ILocalSearchOptimiser.java
+++ com-mmxlabs-optimiser/optimiser-lso/src/main/java/com/mmxlabs/optimiser/lso/ILocalSearchOptimiser.java
@@ -3,6 +3,7 @@ package com.mmxlabs.optimiser.lso;
 import java.util.List;
 
 import com.mmxlabs.optimiser.core.IOptimiser;
+import com.mmxlabs.optimiser.core.ISequences;
 import com.mmxlabs.optimiser.core.ISequencesManipulator;
 import com.mmxlabs.optimiser.core.constraints.IConstraintChecker;
 
@@ -16,11 +17,35 @@ import com.mmxlabs.optimiser.core.constraints.IConstraintChecker;
  */
 public interface ILocalSearchOptimiser<T> extends IOptimiser<T> {
 
+	/**
+	 * Returns the {@link IMoveGenerator} used by this
+	 * {@link ILocalSearchOptimiser} to generate a {@link IMove} each iteration.
+	 * 
+	 * @return
+	 */
 	IMoveGenerator<T> getMoveGenerator();
 
+	/**
+	 * Returns the number of iterations, moves to try, in this optimisation
+	 * 
+	 * @return
+	 */
 	int getNumberOfIterations();
 
+	/**
+	 * Returns the list of {@link IConstraintChecker} used to validate
+	 * {@link ISequences} before evaluating them in each iteration.
+	 * 
+	 * @return
+	 */
 	List<IConstraintChecker<T>> getConstraintCheckers();
 
+	/**
+	 * Returns the {@link ISequencesManipulator} used to transform
+	 * {@link ISequences} into a new {@link ISequences} object to validate and
+	 * evaluate each iteration.
+	 * 
+	 * @return
+	 */
 	ISequencesManipulator<T> getSequenceManipulator();
 }
