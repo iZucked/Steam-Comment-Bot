857142944ab014a88153362264c7a083db0a08fc
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-10-04 11:40:15 -0100

    Tidy up diagnostic information

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/caches/AbstractCache.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/caches/AbstractCache.java
@@ -3,13 +3,11 @@ package com.mmxlabs.common.caches;
 import com.mmxlabs.common.Pair;
 
 public abstract class AbstractCache<K, V> {
-	private static final int SAMPLE = 100000;
-	int queries = 0;
-	int hits = 0;
-	int memoryMisses = 0;
-	int valueMisses = 0;
-	int evictions = 0;
+	private static final int SAMPLE = 1000000;
 	private final String name;
+	
+	int hits = 0;
+	int queries = 0;
 	
 	public interface IKeyEvaluator<K, V> {
 		public Pair<K, V> evaluate(K key);
@@ -26,30 +24,23 @@ public abstract class AbstractCache<K, V> {
 	}
 	
 	public String toString() {
-		final StringBuilder sb = new StringBuilder();
-		sb.append("Cache " + name + " info: " + size() + " elements, ");
-		
-		sb.append(String.format(
-				"%.2f%% hits %.2f%% misses %.2f%% evictions, %.2f%% gc",
-				100 * (hits / (double)queries),
-				100 * (valueMisses/ (double)queries),
-				100 * (evictions / (double)queries),
-				100 * (memoryMisses / (double)queries)));
-		return sb.toString();
+		return String.format("%s cache: size %d, hit rate %.2f",
+				name, size() , 100*(hits/(double)queries));
 	}
 	
-	protected void resetCounters() {
-		queries = hits = memoryMisses = valueMisses = evictions = 0;
+	protected final void hit() {
+		hits++;
 	}
 	
-	protected final void report() {
-		if (hits == SAMPLE) {
-			System.err.println("Free memory: "+Runtime.getRuntime().freeMemory()/1024 +"K");
+	protected final void query() {
+		queries++;
+		if (queries == SAMPLE) {
 			System.err.println(this);
-			resetCounters();
+			queries = hits = 0;
 		}
 	}
 	
+		
 	public abstract void clear();
 	
 	public abstract V get(final K key);
--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/caches/LHMCache.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/caches/LHMCache.java
@@ -1,5 +1,6 @@
 package com.mmxlabs.common.caches;
 
+import java.lang.ref.Reference;
 import java.lang.ref.SoftReference;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
@@ -7,28 +8,32 @@ import java.util.Map.Entry;
 import com.mmxlabs.common.Pair;
 
 public final class LHMCache<K, V> extends AbstractCache<K, V> {
-	final private LinkedHashMap<K, SoftReference<V>> map;
+	final private LinkedHashMap<K, Reference<V>> map;
 
 	public LHMCache(final String name, final IKeyEvaluator<K, V> evaluator, final int intendedSize) {
 		super(name, evaluator);
-		map = new LinkedHashMap<K, SoftReference<V>> ((int)(intendedSize) + 20, 1f, true) {
+		
+		map = new LinkedHashMap<K, Reference<V>> (intendedSize+1, 1, true) {
 			private static final long serialVersionUID = 1L;
-
+			
 			@Override
 			protected boolean removeEldestEntry(
-					Entry<K, SoftReference<V>> eldest) {
+					Entry<K, Reference<V>> eldest) {
 				return size() > intendedSize;
 			}
 		};
 	}
 
 	public final V get(final K key) {
-		final SoftReference<V> ref = map.get(key);
+		query();
+		final Reference<V> ref = map.get(key);
 		V value = null;
 		if (ref == null || (value = ref.get()) == null) {
 			Pair<K, V> pair = evaluate(key);
 			map.put(pair.getFirst(), new SoftReference<V>(pair.getSecond()));
 			value = pair.getSecond();
+		} else {
+			hit();
 		}
 		return value;
 	}
