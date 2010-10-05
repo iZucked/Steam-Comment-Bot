873064db1ceea887cd51778634e9b7e544a10a77
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-10-05 08:38:00 -0100

    Not implemented yet

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/enumerator/EnumeratingSequenceScheduler.java
@@ -0,0 +1,12 @@
+package com.mmxlabs.scheduler.optimiser.fitness.impl.enumerator;
+
+/**
+ * A sequence scheduler which enumerates possible combinations of arrival times
+ * explicitly, rather than using the GA byte array decoding method. This should be subclassable
+ * into a random sequence scheduler as well, with reduced decoding overhead
+ * @author hinton
+ *
+ */
+public class EnumeratingSequenceScheduler {
+
+}
