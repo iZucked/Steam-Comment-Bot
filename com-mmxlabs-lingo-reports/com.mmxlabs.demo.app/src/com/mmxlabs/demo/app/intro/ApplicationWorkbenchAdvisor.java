a8757d85822ec28dda4bea5d9527d91c604c6439
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-10-08 15:38:38 -0100

    Update with hints from IDE workbench advisor

--- com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/ApplicationWorkbenchAdvisor.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/ApplicationWorkbenchAdvisor.java
@@ -1,31 +1,106 @@
 package com.mmxlabs.demo.app.intro;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
 
+import com.mmxlabs.demo.app.Activator;
+
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	private static final String PERSPECTIVE_ID = "com.mmxlabs.demo.app.perspective";
 
-    @Override
-	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
-    
-    @Override
-	public void initialize(IWorkbenchConfigurer configurer) {
-        super.initialize(configurer);
-        configurer.setSaveAndRestore(true);
-        
-        // Need to call this to make resource navigator work correctly.
-        IDE.registerAdapters();
-    }
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			final IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
+	public void initialize(final IWorkbenchConfigurer configurer) {
+		super.initialize(configurer);
+		configurer.setSaveAndRestore(true);
+	}
 
 	@Override
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
+	}
+
+	@Override
+	public void preStartup() {
+
+		Job.getJobManager().suspend();
+
+		// Need to call this to make resource navigator work correctly.
+		IDE.registerAdapters();
+	}
+
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
+
+	@Override
+	public void postStartup() {
+		// Resume background jobs after we startup
+		Job.getJobManager().resume();
+	}
+
+	@Override
+	public void postShutdown() {
+		disconnectFromWorkspace();
+	}
+
+	/**
+	 * Disconnect from the core workspace.
+	 */
+	public static void disconnectFromWorkspace() {
+		// save the workspace
+		try {
+			final ProgressMonitorDialog p = new ProgressMonitorDialog(null);
+
+			final IRunnableWithProgress runnable = new IRunnableWithProgress() {
+				@Override
+				public void run(final IProgressMonitor monitor) {
+					try {
+						ResourcesPlugin.getWorkspace().save(true, monitor);
+					} catch (final CoreException e) {
+						Activator
+								.getDefault()
+								.getLog()
+								.log(new Status(IStatus.ERROR,
+										Activator.PLUGIN_ID, e.getMessage(), e));
+					}
+				}
+			};
+
+			p.run(true, false, runnable);
+		} catch (final InvocationTargetException e) {
+			Activator
+					.getDefault()
+					.getLog()
+					.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e
+							.getMessage(), e));
+		} catch (final InterruptedException e) {
+			Activator
+					.getDefault()
+					.getLog()
+					.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e
+							.getMessage(), e));
+		}
 	}
 }
