9250523e3dc80489f5edd10b447e82e3026dfb66
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-10-12 08:54:13 -0100

    Fix tests for indexed resource

--- com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/core/impl/ResourceTest.java
+++ com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/core/impl/ResourceTest.java
@@ -3,26 +3,28 @@ package com.mmxlabs.optimiser.core.impl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.mmxlabs.common.indexedobjects.IIndexingContext;
+import com.mmxlabs.common.indexedobjects.SimpleIndexingContext;
 import com.mmxlabs.optimiser.core.impl.Resource;
 
 public class ResourceTest {
-
+	IIndexingContext index = new SimpleIndexingContext();
 	@Test
 	public void testResource() {
-		final Resource resource = new Resource();
+		final Resource resource = new Resource(index);
 		Assert.assertNull(resource.getName());
 	}
 
 	@Test
 	public void testResourceString() {
 		final String name = "name";
-		final Resource resource = new Resource(name);
+		final Resource resource = new Resource(index, name);
 		Assert.assertSame(name, resource.getName());
 	}
 
 	@Test
 	public void testGetSetName() {
-		final Resource resource = new Resource();
+		final Resource resource = new Resource(index);
 		Assert.assertNull(resource.getName());
 		final String name = "name";
 		resource.setName(name);
