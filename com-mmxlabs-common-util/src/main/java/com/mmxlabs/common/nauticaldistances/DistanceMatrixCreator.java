41479edac65a2adb1ea66b8bdefa3424f6160ad3
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-10-14 16:04:26 -0100

    Rather simple distance matrix generator (could stand to be improved quite a lot)

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/nauticaldistances/DistanceMatrixCreator.java
@@ -0,0 +1,173 @@
+package com.mmxlabs.common.nauticaldistances;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.common.options.InvalidArgumentException;
+import com.mmxlabs.common.options.InvalidOptionException;
+import com.mmxlabs.common.options.Option;
+import com.mmxlabs.common.options.Options;
+import com.mmxlabs.common.options.OptionsException;
+
+public class DistanceMatrixCreator {
+
+	private String imageFilePath;
+	private String outputFilePath;
+	private String coordinatesFilePath;
+	private double maxLatitude;
+	private double resolution;
+
+	public double getResolution() {
+		return resolution;
+	}
+
+	@Option(defaultValue = "0.5", help = "How many grid squares per pixel of input, roughly")
+	public void setResolution(double resolution) {
+		this.resolution = resolution;
+	}
+
+	/**
+	 * @param args
+	 * @throws InvalidArgumentException
+	 * @throws InvalidOptionException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+		final DistanceMatrixCreator me = new DistanceMatrixCreator();
+		final Options options = new Options();
+
+		try {
+			options.parseAndSet(args, me);
+		} catch (OptionsException e1) {
+			System.err.println("Error: " + e1.getMessage());
+			System.err.println(options.getHelp());
+			return;
+		}
+
+		try {
+			me.run();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+
+	}
+
+	private void run() throws IOException {
+		final BufferedImage image = ImageIO.read(new File(getImageFilePath()));
+
+		final boolean[][] landMatrix = new boolean[image.getWidth()][image
+				.getHeight()];
+		for (int i = 0; i < landMatrix.length; i++) {
+			for (int j = 0; j < landMatrix[i].length; j++) {
+				final int pixel = image.getRGB(i, j);
+				landMatrix[i][j] = isLand(pixel);
+			}
+		}
+		final int lonRes = (int) (getResolution() * landMatrix.length);
+		final int latRes = (int) (getResolution() * landMatrix[0].length * 90 / getMaxLatitude());
+		System.err.println("Map resolution: " + latRes + "x" + lonRes);
+		final NauticalDistanceCalculator calculator = new NauticalDistanceCalculator(
+				landMatrix, getMaxLatitude(), latRes, lonRes);
+
+		final BufferedReader portReader = new BufferedReader(new FileReader(
+				getCoordinatesFilePath()));
+		final List<Pair<String, Pair<Double, Double>>> ports = new ArrayList<Pair<String, Pair<Double, Double>>>();
+		String line;
+		while ((line = portReader.readLine()) != null) {
+			String[] parts = line.split(",");
+			ports.add(new Pair<String, Pair<Double, Double>>(parts[0],
+					new Pair<Double, Double>(Double.parseDouble(parts[1]),
+							Double.parseDouble(parts[2]))));
+		}
+
+		final int[][] matrix = new int[ports.size()][ports.size()];
+		final ArrayList<Pair<Double, Double>> otherPorts = new ArrayList<Pair<Double, Double>>();
+		for (int i = 0; i < matrix.length; i++) {
+			final Pair<Double, Double> startPort = ports.get(i).getSecond();
+			otherPorts.clear();
+			for (int j = 0; j < i; j++)
+				otherPorts.add(ports.get(j).getSecond());
+			System.err.println("Calculating distances from " + ports.get(i));
+			final List<Integer> distances = calculator.getMultipleDistances(
+					startPort, otherPorts);
+
+			for (int j = 0; j < i; j++) {
+				matrix[i][j] = matrix[j][i] = distances.get(j);
+			}
+		}
+
+		// write matrix out
+		final BufferedWriter distanceWriter = new BufferedWriter(
+				new FileWriter(getOutputFilePath()));
+		for (final Pair<String, Pair<Double, Double>> x : ports) {
+			distanceWriter.write(",");
+			distanceWriter.write(x.getFirst());
+		}
+		for (int i = 0; i < matrix.length; i++) {
+			distanceWriter.write("\n");
+			distanceWriter.write(ports.get(i).getFirst());
+			distanceWriter.write(",");
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (j != 0)
+					distanceWriter.write(",");
+				if (matrix[i][j] != Integer.MAX_VALUE)
+					distanceWriter.write(Integer.toString(matrix[i][j]));
+			}
+		}
+		distanceWriter.flush();
+		distanceWriter.close();
+	}
+
+	private boolean isLand(int pixel) {
+		return !(ColorModel.getRGBdefault().getRed(pixel) == 0
+				&& ColorModel.getRGBdefault().getGreen(pixel) == 0 && ColorModel
+				.getRGBdefault().getBlue(pixel) == 255);
+	}
+
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+
+	@Option(defaultValue = "earth.png", help = "Image to load (white = sea, other colours = land)")
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+
+	public String getOutputFilePath() {
+		return outputFilePath;
+	}
+
+	@Option(defaultValue = "distances.csv", help = "Distance matrix file name (output)")
+	public void setOutputFilePath(String outputFilePath) {
+		this.outputFilePath = outputFilePath;
+	}
+
+	public double getMaxLatitude() {
+		return maxLatitude;
+	}
+
+	@Option(defaultValue = "70", help = "Maximum/minimum latitude of image in degrees")
+	public void setMaxLatitude(double maxLatitude) {
+		this.maxLatitude = maxLatitude;
+	}
+
+	public String getCoordinatesFilePath() {
+		return coordinatesFilePath;
+	}
+
+	@Option(defaultValue = "ports.csv", help = "List of port coordinates")
+	public void setCoordinatesFilePath(String coordinatesFilePath) {
+		this.coordinatesFilePath = coordinatesFilePath;
+	}
+}
---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/nauticaldistances/NauticalDistanceCalculator.java
@@ -0,0 +1,341 @@
+package com.mmxlabs.common.nauticaldistances;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.mmxlabs.common.Pair;
+
+/**
+ * Calculator for port-port distances which uses a simple model of the sea as
+ * passable/impassable terrain and computes the shortest path through passable
+ * squares.
+ * 
+ * Assumes the matrix is a rough mercator projection, and the earth is
+ * normal-sized for an earth.
+ * 
+ * Estimates will be over by maybe 5% because of a lack of genuine diagonal lines
+ * and the low resolution of the terrain bitmap. A better solution would probably
+ * have to find all coastal points, determine which coastal points are great-circle
+ * reachable from one another, build a graph on that and then run dijkstra like this.
+ * 
+ * @author hinton
+ * 
+ */
+public class NauticalDistanceCalculator {
+	private final int longitudeResolution;
+	private final int latitudeResolution;
+	private static final double RAD_KNOTS = 3443.89849;
+	private final boolean[][] landMatrix;
+	/**
+	 * This matrix will be filled with the shortest path to each square from the
+	 * current start square 
+	 */
+	private final int[][] distances;
+	private Pair<Integer, Integer> startSquare = null;
+
+	public NauticalDistanceCalculator(final boolean[][] mercatorLandMatrix,
+			double verticalRange, int latitudeResolution, int longitudeResolution) {
+		this.landMatrix = new boolean[latitudeResolution][longitudeResolution];
+		this.latitudeResolution = latitudeResolution;
+		this.longitudeResolution = longitudeResolution;
+		verticalRange = d2r(verticalRange);
+		// populate land matrix using mercator projected image
+
+		final double maxProjectedLat = Math.log(Math.tan(verticalRange) + 1
+				/ Math.cos(verticalRange));
+
+		final int mapWidth = mercatorLandMatrix.length;
+		final int mapHeight = mercatorLandMatrix[0].length;
+
+		for (int i = 0; i < latitudeResolution; i++) {
+			for (int j = 0; j < longitudeResolution; j++) {
+				final double lat = (i / (double) latitudeResolution - 0.5) * Math.PI;
+				final double lon = (j / (double) longitudeResolution - 0.5) * Math.PI * 2;
+				if (Math.abs(lat) > verticalRange) {
+					this.landMatrix[i][j] = true; // land
+				} else {
+					// compute mercator position
+					final double px = ((lon / Math.PI) + 1) / 2;
+					// better not use a longitude higher than the maximum, or
+					// this will explode
+					final double py = (1 - ((Math.log(Math.tan(lat) + 1
+							/ Math.cos(lat))) / maxProjectedLat)) / 2;
+//					System.err
+//							.println(String.format(
+//									"%.2f %.2f => %.2f %.2f, %d %d %s",
+//									lat,
+//									lon,
+//									px,
+//									py,
+//									(int) Math.floor(px * mapWidth),
+//									(int) Math.floor(py * mapHeight),
+//									mercatorLandMatrix[(int) Math.floor(px
+//											* mapWidth)][(int) Math.floor(py
+//											* mapHeight)] ? "land" : "sea"));
+					this.landMatrix[i][j] = mercatorLandMatrix[(int) Math
+							.floor(px * mapWidth)][(int) Math.floor((1 - py)
+							* mapHeight - 1)];
+				}
+			}
+		}
+
+//		for (int i = 0; i < LAT_RES; i++) {
+//			System.err.print("|");
+//			for (int j = 0; j < LON_RES; j++) {
+//				System.err.print(landMatrix[i][j] ? "*" : " ");
+//			}
+//			System.err.println("|");
+//		}
+
+		this.distances = new int[latitudeResolution][longitudeResolution];
+	}
+
+	/**
+	 * move point to nearest water
+	 * @param point
+	 */
+	private void moveToSea(final Pair<Integer, Integer> point) {
+		if (landMatrix[point.getFirst()][point.getSecond()]) {
+			final int p1 = point.getFirst();
+			final int p2 = point.getSecond();
+			
+			double distance = Double.MAX_VALUE;
+			
+			for (int i = 0; i<latitudeResolution; i++) {
+				for (int j = 0; j<latitudeResolution; j++) {
+					if (!landMatrix[i][j]) {
+						double d2 = Math.pow(i-p1, 2) + Math.pow(j-p2, 2);
+						if (d2 < distance) {
+							distance = d2;
+							point.setBoth(i, j);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public List<Integer> getMultipleDistances(
+			final Pair<Double, Double> latlon,
+			final List<Pair<Double, Double>> others) {
+		final ArrayList<Integer> result = new ArrayList<Integer>();
+		final ArrayList<Pair<Integer, Integer>> ends = new ArrayList<Pair<Integer, Integer>>();
+
+		for (final Pair<Double, Double> p : others) {
+			final Pair<Integer, Integer> grid = gridLatLon(p);
+			moveToSea(grid);
+			ends.add(grid);
+		}
+
+		// transform the lat/lon into simple map coordinates
+		startSquare = gridLatLon(latlon);
+		moveToSea(startSquare);
+		System.err
+				.println("Start square for "
+						+ latlon
+						+ " is "
+						+ startSquare
+						+ " "
+						+ (landMatrix[startSquare.getFirst()][startSquare
+								.getSecond()] ? "(land)" : "(sea)"));
+
+//		for (int i = 0; i < LAT_RES; i++) {
+//			System.err.print("|");
+//			for (int j = 0; j < LON_RES; j++) {
+//				if (i == startSquare.getFirst() && j == startSquare.getSecond())
+//					System.err.print("*");
+//				else
+//					System.err.print(landMatrix[i][j] ? "." : " ");
+//			}
+//			System.err.println("|");
+//		}
+
+		// flood-fill shortest-path matrix with suitably computed distances
+		// 1. reset distances to maximum value
+		for (int[] row : distances) {
+			Arrays.fill(row, Integer.MAX_VALUE);
+		}
+		// 2. set point to be 0 from itself
+		distances[startSquare.getFirst()][startSquare.getSecond()] = 0;
+		// 3. set up marks; all land is already done
+		final boolean[][] marks = new boolean[latitudeResolution][longitudeResolution];
+		final int markCount = latitudeResolution * longitudeResolution;
+		int percent = -1;
+		int markProgress = 0;
+		Pair<Integer, Integer> here = new Pair<Integer, Integer>(startSquare);
+		int distanceToHere = distances[here.getFirst()][here.getSecond()];
+		while (distanceToHere != Integer.MAX_VALUE) {
+			boolean markedEveryEnd = true;
+			for (final Pair<Integer, Integer> end : ends) {
+				markedEveryEnd = markedEveryEnd
+						&& marks[end.getFirst()][end.getSecond()];
+			}
+			if (markedEveryEnd) {
+				break;
+			}
+
+			if (landMatrix[here.getFirst()][here.getSecond()] == false) {
+				for (final int[] n : neighbours(here)) {
+					if (marks[n[0]][n[1]])
+						continue; // marked entries are done
+					if (landMatrix[n[0]][n[1]])
+						continue; // nowhere is reachable by land
+
+					final int delta = distance(here, n);
+					if ((delta + distanceToHere) < distances[n[0]][n[1]]) {
+						distances[n[0]][n[1]] = delta + distanceToHere;
+					}
+				}
+			}
+			marks[here.getFirst()][here.getSecond()] = true;
+			markProgress++;
+//			System.err.println(markProgress + " of " + markCount);
+			int newpercent = 100*markProgress / markCount;
+			if (newpercent != percent) {
+				percent = newpercent;
+				System.err.println(percent + "% of sea marked");
+			}
+			// find next location; ugly and slow, but sufficient
+			distanceToHere = Integer.MAX_VALUE;
+			for (int i = 0; i < distances.length; i++) {
+				for (int j = 0; j < distances[i].length; j++) {
+					if (marks[i][j] == false) {
+						if (distances[i][j] <= distanceToHere) {
+							distanceToHere = distances[i][j];
+							here.setBoth(i, j);
+						}
+					}
+				}
+			}
+		}
+		
+		for (final Pair<Integer, Integer> end : ends) {
+			result.add(distances[end.getFirst()][end.getSecond()]);
+		}
+		
+		return result;
+	}
+
+	private Pair<Integer, Integer> gridLatLon(Pair<Double, Double> latlon) {
+		return new Pair<Integer, Integer>(
+				(int) Math.floor(((latlon.getFirst() + 90) / 180.0) * latitudeResolution),
+				(int) Math.floor(((latlon.getSecond() + 180) / 360.0) * longitudeResolution));
+	}
+
+	private double d2r(double degrees) {
+		return Math.PI * (degrees / 180);
+	}
+
+	/**
+	 * Compute great-circle distance in nautical miles. parameters are a lat/lon
+	 * index, and n, which has delta-values in elements 2 and 3.
+	 * 
+	 * @param here
+	 * @param n
+	 * @return
+	 */
+	private int distance(final Pair<Integer, Integer> here, final int[] delta) {
+		final double latH = (here.getFirst() / (double) latitudeResolution - 0.5)
+				* Math.PI / 2;
+		final double latT = ((here.getFirst() + delta[2]) / (double) latitudeResolution - 0.5)
+				* Math.PI / 2;
+		final double dLat = Math.PI * delta[2] / (double) latitudeResolution;
+		final double dLon = 2 * Math.PI * delta[3] / (double) longitudeResolution;
+		return (int) (RAD_KNOTS * 2 * Math.asin(Math.sqrt(Math.pow(
+				Math.sin(dLat / 2), 2)
+				+ Math.cos(latH)
+				* Math.cos(latT)
+				* Math.pow(Math.sin(dLon / 2), 2))));
+	}
+
+	/**
+	 * Return an iterable of the neighbours of the given square. Doesn't have to
+	 * be well behaved (i.e. may just return the same object again and again).
+	 * Doesn't work at the poles, but doesn't need to.
+	 * 
+	 * @param marks
+	 * @param here
+	 * @return
+	 */
+	private Iterable<int[]> neighbours(final Pair<Integer, Integer> here) {
+		return new Iterable<int[]>() {
+
+			@Override
+			public Iterator<int[]> iterator() {
+				return new Iterator<int[]>() {
+					final int[] n = new int[4];
+					int neighbour = 0;
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+
+					@Override
+					public int[] next() {
+						switch (neighbour) {
+						case 0:
+							n[2] = -1;
+							n[3] = 0;
+							break;
+						case 1:
+							n[2] = -1;
+							n[3] = +1;
+							break;
+						case 2:
+							n[2] = 0;
+							n[3] = +1;
+							break;
+						case 3:
+							n[2] = +1;
+							n[3] = +1;
+							break;
+						case 4:
+							n[2] = +1;
+							n[3] = 0;
+							break;
+						case 5:
+							n[2] = +1;
+							n[3] = -1;
+							break;
+						case 6:
+							n[2] = 0;
+							n[3] = -1;
+							break;
+						case 7:
+							n[2] = -1;
+							n[3] = -1;
+							break;
+						}
+						neighbour++;
+
+						n[0] = here.getFirst() + n[2];
+						n[1] = here.getSecond() + n[3];
+
+						// wrap
+						if (n[0] < 0) {
+							n[0] += latitudeResolution;
+						} else if (n[0] >= latitudeResolution) {
+							n[0] -= latitudeResolution;
+						}
+
+						if (n[1] < 0) {
+							n[1] += longitudeResolution;
+						} else if (n[1] >= longitudeResolution) {
+							n[1] -= longitudeResolution;
+						}
+
+						return n;
+					}
+
+					@Override
+					public boolean hasNext() {
+						return neighbour < 7;
+					}
+				};
+			}
+		};
+	}
+}
