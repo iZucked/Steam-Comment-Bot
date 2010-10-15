c80c363366b733d32decf83f4e19d7fd9b0f4279
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-10-15 06:04:30 -0100

    Add missing @Override tag

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/indexedobjects/impl/IndexedObject.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/indexedobjects/impl/IndexedObject.java
@@ -5,9 +5,12 @@ import com.mmxlabs.common.indexedobjects.IIndexingContext;
 
 public class IndexedObject implements IIndexedObject {
 	public final int index;
+
 	public IndexedObject(final IIndexingContext provider) {
 		index = provider.assignIndex(this);
 	}
+
+	@Override
 	public final int getIndex() {
 		return index;
 	}
