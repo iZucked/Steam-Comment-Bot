2ced51ac9a69a27d0a0545f7b07cd1ebe57ec644
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-10-19 15:30:50 -0100

    Add copyright headers

--- com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/IManagedJob.java
+++ com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/IManagedJob.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) Minimaxlabs, 2010
+ * All rights reserved.
+ */
+
 package com.mmxlabs.jobcontroller.core;
 
 import com.mmxlabs.optimiser.core.IAnnotatedSolution;
--- com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/JobManager.java
+++ com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/JobManager.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) Minimaxlabs, 2010
+ * All rights reserved.
+ */
+
 package com.mmxlabs.jobcontroller.core;
 
 import java.util.HashSet;
