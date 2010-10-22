1ef70d6590e8d3051723c85f2842f6e015585527
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-10-22 15:59:30 -0100

    Organise imports

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/options/Options.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/options/Options.java
@@ -5,7 +5,6 @@
 
 package com.mmxlabs.common.options;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
