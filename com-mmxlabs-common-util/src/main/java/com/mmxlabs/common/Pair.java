e236f70b9b6884bc88469aef106be5fd16ab9d5a
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-10-26 06:17:23 -0100

    Make class and methods final

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Pair.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Pair.java
@@ -7,49 +7,55 @@ package com.mmxlabs.common;
 
 /**
  * A class for holding a pair of heterogenous items
+ * 
  * @author hinton
- *
- * @param <A> the type of the first item
- * @param <B> the type of the second item
+ * 
+ * @param <A>
+ *            the type of the first item
+ * @param <B>
+ *            the type of the second item
  */
-public class Pair<A, B> {
+public final class Pair<A, B> {
 	private A first;
 	private B second;
-	
+
 	public Pair() {
 		this(null, null);
 	}
-	
+
 	public Pair(final A first, final B second) {
 		super();
 		this.first = first;
 		this.second = second;
 	}
-	
+
 	public Pair(final Pair<A, B> copy) {
 		setBoth(copy.getFirst(), copy.getSecond());
 	}
 
-	public A getFirst() {
+	public final A getFirst() {
 		return first;
 	}
-	public void setFirst(final A first) {
+
+	public final void setFirst(final A first) {
 		this.first = first;
 	}
-	public B getSecond() {
+
+	public final B getSecond() {
 		return second;
 	}
-	public void setSecond(final B second) {
+
+	public final void setSecond(final B second) {
 		this.second = second;
 	}
 
-	public void setBoth(final A i, final B j) {
+	public final void setBoth(final A i, final B j) {
 		setFirst(i);
 		setSecond(j);
 	}
 
 	@Override
-	public String toString() {
+	public final String toString() {
 		return "(" + first.toString() + ", " + second.toString() + ")";
 	}
 }
