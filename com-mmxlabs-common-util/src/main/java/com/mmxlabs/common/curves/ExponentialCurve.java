46df5a271f130a051fcaecb91ab394a60a48ae05
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-11-09 14:15:04 +0000

    Some simple classes and interfaces for representing curves

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/curves/ExponentialCurve.java
@@ -0,0 +1,62 @@
+/**
+ *
+ * Copyright (C) Minimax Labs Ltd., 2010 
+ * All rights reserved. 
+ * 
+ */
+package com.mmxlabs.common.curves;
+
+/**
+ * A curve which implements $y = Ae^kx + C$
+ * 
+ * @author hinton
+ *
+ */
+public final class ExponentialCurve implements ICurve {
+	private double A = 1;
+	private double k = -1;
+	private double C = 0;
+	
+	public ExponentialCurve() {
+		
+	}
+	
+	public ExponentialCurve(final double A, final double k, final double C) {
+		setA(A);
+		setK(k);
+		setC(C);
+	}
+	
+	public double getA() {
+		return A;
+	}
+
+	public void setA(double a) {
+		A = a;
+	}
+
+	public double getK() {
+		return k;
+	}
+
+	public void setK(double k) {
+		this.k = k;
+	}
+
+	public double getC() {
+		return C;
+	}
+
+	public void setC(double c) {
+		C = c;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see com.mmxlabs.common.curves.ICurve#getValueAtPoint(double)
+	 */
+	@Override
+	public double getValueAtPoint(final double point) {
+		return C + A*Math.exp(k * point);
+	}
+}
---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/curves/ICurve.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * Copyright (C) Minimax Labs Ltd., 2010 
+ * All rights reserved. 
+ * 
+ */
+package com.mmxlabs.common.curves;
+
+/**
+ * An interface for a curve, which uses double precision.
+ * 
+ * @author hinton
+ *
+ */
+public interface ICurve {
+	double getValueAtPoint(final double point);
+}
