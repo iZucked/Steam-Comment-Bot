592bb66691832bcbf32f78ac03f7da1d8ebc8981
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-11-24 16:53:57 +0000

    Add method for associating EMF entities with builder objects

--- com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/ModelEntityMap.java
+++ com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/emf/ModelEntityMap.java
@@ -12,11 +12,16 @@ import java.util.Map;
 import javax.management.timer.Timer;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import scenario.Scenario;
 
 /**
+ * Class which maps model entities to classes; the LNGScenarioTransformer
+ * should populate one of these, which maps every PortSlot to the URI of a
+ * Slot in the EMF, and similarly for vessels, vessel classes, etc etc.
  * @author hinton
  * 
  */
@@ -51,5 +56,9 @@ public class ModelEntityMap {
 				);
 		
 	}
-
+	
+	public void addModelObject(final EObject modelObject, final Object internalObject) {
+		final URI theURI = EcoreUtil.getURI(modelObject);
+		uriMap.put(internalObject, theURI);
+	}
 }
