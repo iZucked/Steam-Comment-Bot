4a1d37cc41b4e02717527512f1dece8a16499162
Tom Hinton <tom.hinton@minimaxlabs.com>
2010-12-01 14:41:36 +0000

    Pair keyed map; would be useful for annotations, but not actually using it right now

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/PairKeyedMap.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * Copyright (C) Minimax Labs Ltd., 2010 
+ * All rights reserved. 
+ * 
+ */
+package com.mmxlabs.common;
+
+import java.util.HashMap;
+
+/**
+ * An extension of HashMap<Pair<K1, K2>, V> with convenience methods
+ * for getting and setting.
+ * 
+ * @author hinton
+ *
+ */
+public class PairKeyedMap<K1, K2, V> extends HashMap<Pair<K1, K2>, V>{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public V get(final K1 k1, final K2 k2) {
+		return get(new Pair<K1, K2>(k1, k2));
+	}
+	
+	public V put(final K1 k1, final K2 k2, final V value) {
+		return put(new Pair<K1, K2>(k1, k2), value);
+	}
+}
