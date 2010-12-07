b207cf6c875943093d8c2f3b580677f87dc68dff
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-12-07 09:02:13 +0000

    Add a single constant value price curve implementation

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/curves/ConstantValueCurve.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Copyright (C) Minimax Labs Ltd., 2010 
+ * All rights reserved. 
+ * 
+ */
+package com.mmxlabs.common.curves;
+
+/**
+ * Implementation of {@link ICurve} which returns a single constant value
+ * regardless of the input value.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class ConstantValueCurve implements ICurve {
+
+	private final double value;
+
+	public ConstantValueCurve(final double value) {
+		this.value = value;
+	}
+
+	@Override
+	public final double getValueAtPoint(final double point) {
+		return value;
+	}
+}
