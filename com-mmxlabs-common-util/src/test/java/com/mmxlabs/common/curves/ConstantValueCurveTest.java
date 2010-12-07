951417c074506b4af7eb40162743026c9325d4aa
Simon Goodall <simon.goodall@minimaxlabs.com>
2010-12-07 09:04:17 +0000

    Added test case for constant curve

---
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/curves/ConstantValueCurveTest.java
@@ -0,0 +1,16 @@
+package com.mmxlabs.common.curves;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class ConstantValueCurveTest {
+
+	@Test
+	public void testGetValueAtPoint() {
+		final double v = 1.2345;
+		final ConstantValueCurve curve = new ConstantValueCurve(v);
+		Assert.assertEquals(v, curve.getValueAtPoint(-1000.0), 0);
+		Assert.assertEquals(v, curve.getValueAtPoint(1000.0), 0);
+		Assert.assertEquals(v, curve.getValueAtPoint(Double.MAX_VALUE), 0);
+	}
+}
