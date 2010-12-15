4f052d31b1b2871713ed63720f483931c8be0e6c
Tom Hinton <unknown@kiln.example.com>
2010-12-15 18:46:02 +0000

    Tidy ASH and add trace exporter

--- com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager.ui/src/com/mmxlabs/jobmanager/ui/handlers/AbstractScenarioHandler.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.jobmanager.ui/src/com/mmxlabs/jobmanager/ui/handlers/AbstractScenarioHandler.java
@@ -8,21 +8,12 @@ package com.mmxlabs.jobmanager.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import scenario.Scenario;
-import scenario.ScenarioPackage;
 
 public abstract class AbstractScenarioHandler extends AbstractHandler {
 
