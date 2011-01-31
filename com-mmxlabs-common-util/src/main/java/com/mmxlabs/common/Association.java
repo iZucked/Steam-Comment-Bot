bd5472b106c0aa0d03a0d30d63d892c2db80cc7d
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-01-31 08:43:21 +0000

    Make things final

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Association.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Association.java
@@ -7,30 +7,34 @@ package com.mmxlabs.common;
 import java.util.HashMap;
 
 /**
- * A two-way association from one type to another. Internally maintains two hashmaps for lookup in both directions.
+ * A two-way association from one type to another. Internally maintains two
+ * hashmaps for lookup in both directions.
+ * 
  * @author hinton
- *
+ * 
  * @param <A>
  * @param <B>
  */
 public class Association<A, B> {
-	protected HashMap<A, B> forwards;
-	protected HashMap<B, A>reverse;
+	
+	protected final HashMap<A, B> forwards;
+	protected final HashMap<B, A> reverse;
+
 	public Association() {
 		forwards = new HashMap<A, B>();
 		reverse = new HashMap<B, A>();
 	}
-	
-	public void add(A a, B b) {
+
+	public void add(final A a, final B b) {
 		forwards.put(a, b);
 		reverse.put(b, a);
 	}
-	
-	public B lookup(A a) {
+
+	public B lookup(final A a) {
 		return forwards.get(a);
 	}
-	
-	public A reverseLookup(B b) {
+
+	public A reverseLookup(final B b) {
 		return reverse.get(b);
 	}
 }
--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Pair.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/Pair.java
@@ -68,7 +68,7 @@ public final class Pair<A, B> {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(final Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
