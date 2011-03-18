20014baf8116de30e9c331e09c0fceb96f91d511
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-18 08:59:11 +0000

    Implement copy

--- com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/CheckPointHandler.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/CheckPointHandler.java
@@ -1,23 +1,34 @@
 package com.mmxlabs.rcp.navigator.handlers;
 
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.QualifiedName;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import com.mmxlabs.jobcontroller.core.IJobManagerListener;
-import com.mmxlabs.jobcontroller.core.IManagedJobListener;
+import com.mmxlabs.rcp.navigator.scenario.ScenarioTreeNodeClass;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class CheckPointHandler extends AbstractHandler {
-	
+
 	/**
 	 * The constructor.
 	 */
@@ -28,17 +39,94 @@ public class CheckPointHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
 
-		
-		// Copy scenario into new workspace resource.
-		
-		// --> e.g. find root name Add on timestamp.
-		
-		// --> Refresh parent folder
-	
-		// Update button state?
-		
+		final ISelection selection = HandlerUtil
+				.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+		if (selection != null & selection instanceof IStructuredSelection) {
+			final IStructuredSelection strucSelection = (IStructuredSelection) selection;
+
+			final Iterator<?> itr = strucSelection.iterator();
+			while (itr.hasNext()) {
+				final Object obj = itr.next();
+
+				if (obj instanceof ScenarioTreeNodeClass) {
+
+					final WorkspaceJob job = new WorkspaceJob("Checkpoint") {
+
+						@Override
+						public IStatus runInWorkspace(final IProgressMonitor monitor)
+								throws CoreException {
+							
+							monitor.beginTask("Copy Scenario", 3);
+							try {
+								final ScenarioTreeNodeClass node = (ScenarioTreeNodeClass) obj;
+
+								final IContainer container = node.getContainer();
+								final IResource resource = node.getResource();
+
+								// Obtain a handle for the new folder
+								final IFolder folder = ((IFolder) container)
+										.getFolder(container.getName());
+								
+								// Create the new folder
+								folder.create(true, true,
+										new SubProgressMonitor(monitor, 1));
+								
+								// Copy the scenario file into new folder
+								resource.copy(
+										folder.getFullPath().append(
+												"/.scenario"), true,
+										new SubProgressMonitor(monitor, 1));
+
+								// Trigger refresh (although a workspace job might do this already?
+								container.refreshLocal(
+										IResource.DEPTH_INFINITE,
+										new SubProgressMonitor(monitor, 1));
+								
+							} finally {
+								monitor.done();
+							}
+							return Status.OK_STATUS;
+						}
+					};
+					// Run the job as soon as
+					job.schedule();
+
+				}
+			}
+		}
+
 		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+
+		// We could do some of this in plugin.xml - but not been able to
+		// configure it properly.
+		// Plugin.xml will make it enabled if the resource can be a Scenario.
+		// But need finer grained control depending on optimisation state.
+
+		if (!super.isEnabled()) {
+			return false;
+		}
+
+		final ISelection selection = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getSelectionService()
+				.getSelection();
+
+		if (selection != null & selection instanceof IStructuredSelection) {
+			final IStructuredSelection strucSelection = (IStructuredSelection) selection;
+
+			final Iterator<?> itr = strucSelection.iterator();
+			while (itr.hasNext()) {
+				final Object obj = itr.next();
+				return (obj instanceof ScenarioTreeNodeClass);
+			}
+		}
+
+		return false;
 	}
 }
