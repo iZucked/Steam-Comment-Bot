219030fab9100303c8919d488fa8d7eff08ef369
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-20 08:40:20 +0000

    Start check box hook up

--- com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/TheNavigator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/TheNavigator.java
@@ -1,6 +1,8 @@
 package com.mmxlabs.rcp.navigator;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
@@ -11,22 +13,25 @@ import org.eclipse.ui.navigator.CommonNavigator;
 import org.eclipse.ui.navigator.CommonViewer;
 
 import com.mmxlabs.jobcontoller.Activator;
-import com.mmxlabs.jobcontroller.core.JobManager;
+import com.mmxlabs.jobcontroller.core.IJobManager;
+import com.mmxlabs.jobcontroller.core.IJobManagerListener;
+import com.mmxlabs.jobcontroller.core.IManagedJob;
 import com.mmxlabs.rcp.navigator.scenario.ScenarioTreeNodeClass;
 
 public class TheNavigator extends CommonNavigator {
-	
-	
-	protected CommonViewer createCommonViewerObject(Composite aParent) {
+
+	@Override
+	protected CommonViewer createCommonViewerObject(final Composite aParent) {
 		return new CommonViewer(getViewSite().getId(), aParent, SWT.MULTI
 				| SWT.H_SCROLL | SWT.V_SCROLL | SWT.CHECK);
 	}
 
-	protected CommonViewer createCommonViewer(Composite aParent) {
+	@Override
+	protected CommonViewer createCommonViewer(final Composite aParent) {
 
-		CommonViewer aViewer = createCommonViewerObject(aParent);
+		final CommonViewer aViewer = createCommonViewerObject(aParent);
 
-		Tree treeTable = aViewer.getTree();
+		final Tree treeTable = aViewer.getTree();
 
 		treeTable.setHeaderVisible(true);
 		treeTable.setLinesVisible(true);
@@ -36,7 +41,7 @@ public class TheNavigator extends CommonNavigator {
 		// .setHelp(treeTable, ContextIds.VIEW_SERVERS);
 
 		// add columns
-		TreeColumn column = new TreeColumn(treeTable, SWT.SINGLE);
+		final TreeColumn column = new TreeColumn(treeTable, SWT.SINGLE);
 		column.setText("Resource");
 		column.setWidth(150);
 		// column.setWidth(cols[0]);
@@ -44,7 +49,7 @@ public class TheNavigator extends CommonNavigator {
 		treeTable.setSortColumn(column);
 		treeTable.setSortDirection(SWT.UP);
 
-		TreeColumn column2 = new TreeColumn(treeTable, SWT.SINGLE);
+		final TreeColumn column2 = new TreeColumn(treeTable, SWT.SINGLE);
 
 		column2.setText("Status");
 		column2.setWidth(150);
@@ -55,20 +60,79 @@ public class TheNavigator extends CommonNavigator {
 		aViewer.getNavigatorContentService().restoreState(memento);
 
 		treeTable.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event event) {
-				String string = event.detail == SWT.CHECK ? "Checked"
-						: "Selected";
-				System.out.println(event.item + " " + string);
-				System.out.println(((TreeItem)event.item).getChecked());
-				if (event.item.getData() instanceof ScenarioTreeNodeClass) {
-					ScenarioTreeNodeClass node = (ScenarioTreeNodeClass)event.item.getData();
-//					Activator.getDefault().getJobManager().setResourceSelection(node.getResource(), event.s)
+
+			@Override
+			public void handleEvent(final Event event) {
+				if (event.item instanceof Button
+						&& event.item.getData() instanceof ScenarioTreeNodeClass) {
+					final ScenarioTreeNodeClass node = (ScenarioTreeNodeClass) event.item
+							.getData();
+					final Button button = (Button) event.item;
+					
+					// Set selection status
+					Activator
+							.getDefault()
+							.getJobManager()
+							.setResourceSelection(node.getResource(),
+									button.getSelection());
 				}
-				// HOok into job manager
-				
 			}
 		});
 
+		IJobManagerListener jobManagerListener = new IJobManagerListener() {
+			
+			void checkItems(TreeItem item, IResource res, boolean check) {
+				
+				if (item.getData() == res) {
+					item.setChecked(check);
+				}
+				for (TreeItem i : item.getItems()) {
+					checkItems(i, res, check);
+				}
+			}
+			
+			@Override
+			public void jobSelected(IJobManager jobManager, IManagedJob job,
+					IResource resource) {
+				
+				// TODO: Update Check status of widget
+				
+				// TODO Auto-generated method stub
+				TreeItem[] items = TheNavigator.this.getCommonViewer().getTree().getItems();
+				for (TreeItem i : items) {
+					checkItems(i, resource, true);
+				}
+				TheNavigator.this.getCommonViewer().getTree().redraw();
+			}
+			
+			@Override
+			public void jobRemoved(IJobManager jobManager, IManagedJob job,
+					IResource resource) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void jobDeselected(IJobManager jobManager, IManagedJob job,
+					IResource resource) {
+				// TODO Auto-generated method stub
+				TreeItem[] items = TheNavigator.this.getCommonViewer().getTree().getItems();
+				for (TreeItem i : items) {
+					checkItems(i, resource, false);
+				}
+			}
+			
+			@Override
+			public void jobAdded(IJobManager jobManager, IManagedJob job,
+					IResource resource) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		Activator
+		.getDefault()
+		.getJobManager().addJobManagerListener(jobManagerListener);
+		
 		return aViewer;
 	}
 }
