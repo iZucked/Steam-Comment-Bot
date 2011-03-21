40b77e8f0af7b6df772b33052289b455cc02cb8a
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-21 21:58:44 +0000

    Report every 1%

--- com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/impl/LNGSchedulerJob.java
+++ com-mmxlabs-jobmanager/com.mmxlabs.jobmanager/src/com/mmxlabs/jobcontroller/core/impl/LNGSchedulerJob.java
@@ -26,7 +26,7 @@ import com.mmxlabs.scheduler.optimiser.components.ISequenceElement;
  * 
  */
 public class LNGSchedulerJob extends AbstractManagedJob {
-	private static final int REPORT_PERCENTAGE = 5;
+	private static final int REPORT_PERCENTAGE = 1;
 	private final Scenario scenario;
 	private int currentProgress = 0;
 
