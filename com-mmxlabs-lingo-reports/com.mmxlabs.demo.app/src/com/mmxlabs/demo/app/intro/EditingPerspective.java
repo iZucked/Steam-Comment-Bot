5870f2bee980fe488b383e6674dfcf37c297ce4d
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-23 22:47:44 +0000

    Remove job controller and move properties across

--- com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/EditingPerspective.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.app/src/com/mmxlabs/demo/app/intro/EditingPerspective.java
@@ -9,15 +9,13 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-import com.mmxlabs.jobcontroller.views.JobManagerView;
-
 public class EditingPerspective implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(final IPageLayout layout) {
 
 		final IFolderLayout navFolder = layout.createFolder("navFolder",
-				IPageLayout.LEFT, 0.2f, IPageLayout.ID_EDITOR_AREA);
+				IPageLayout.LEFT, 0.3f, IPageLayout.ID_EDITOR_AREA);
 		navFolder.addView("com.mmxlabs.rcp.navigator");
 		navFolder.addView(IPageLayout.ID_RES_NAV);
 
