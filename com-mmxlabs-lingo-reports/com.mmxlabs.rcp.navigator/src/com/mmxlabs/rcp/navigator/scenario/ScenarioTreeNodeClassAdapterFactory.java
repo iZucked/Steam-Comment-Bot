916230223891d245fc12e985a8cf8796b14605de
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-23 17:46:40 +0000

    When adapting, try and locate an existing job and get scenario/schedule from job rather than the on disk version.

--- com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/scenario/ScenarioTreeNodeClassAdapterFactory.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/scenario/ScenarioTreeNodeClassAdapterFactory.java
@@ -7,59 +7,78 @@ import scenario.Scenario;
 import scenario.schedule.Schedule;
 import scenario.schedule.ScheduleModel;
 
+import com.mmxlabs.jobcontoller.Activator;
+import com.mmxlabs.jobcontroller.core.IManagedJob;
+import com.mmxlabs.jobcontroller.core.impl.LNGSchedulerJob;
 import com.mmxlabs.rcp.navigator.ecore.EcoreContentProvider;
 
 /**
- * Needed to make Eclipse Core Expressions resolve the adapt method for arbitary
- * objects
+ * Needed to make Eclipse Core Expressions resolve the adapt method for
+ * arbitrary objects
  * 
  * @author Simon Goodall
  * 
  */
 public class ScenarioTreeNodeClassAdapterFactory implements IAdapterFactory {
 
-	EcoreContentProvider scp = new EcoreContentProvider();
+	private final EcoreContentProvider scp = new EcoreContentProvider();
 
 	@Override
-	public Object getAdapter(Object adaptableObject, Class adapterType) {
-
-		// if (adaptableObject instanceof ScenarioTreeNodeClass) {
-		// ScenarioTreeNodeClass node = (ScenarioTreeNodeClass) adaptableObject;
-		// if (Scenario.class.isAssignableFrom(adapterType)) {
-		// return node.getScenario();
-		// } else if (IResource.class.isAssignableFrom(adapterType)) {
-		// return node.getResource();
-		// }
-		// }
+	public Object getAdapter(final Object adaptableObject,
+			@SuppressWarnings("rawtypes") final Class adapterType) {
 
 		if (adaptableObject instanceof IResource) {
 
-			if (Scenario.class.isAssignableFrom(adapterType)) {
-				Scenario s = getScenario((IResource) adaptableObject);
-				return s;
-			} else if (Schedule.class.isAssignableFrom(adapterType)) {
-				Scenario s = getScenario((IResource) adaptableObject);
-				if (s != null) {
-					ScheduleModel scheduleModel = s.getScheduleModel();
-					if (scheduleModel != null) {
-						int size = scheduleModel.getSchedules().size();
-						if (size > 0) {
-							return scheduleModel.getSchedules().get(size - 1);
-						}
+			/**
+			 * Try obtaining in memory data from a running job before falling
+			 * back to loading the scenario from the resource. This allows the
+			 * current optimisation state to be shown.
+			 */
+			final IManagedJob job = Activator.getDefault().getJobManager()
+					.findJobForResource((IResource) adaptableObject);
+			if (job != null) {
+				final Scenario scenario = ((LNGSchedulerJob) job).getScenario();
+				if (scenario != null) {
+					if (Scenario.class.isAssignableFrom(adapterType)) {
+						return scenario;
+					} else if (Schedule.class.isAssignableFrom(adapterType)) {
+						return getSchedule(scenario);
 					}
 				}
+			}
+
+			// Fall back to directly loading from resource
+			if (Scenario.class.isAssignableFrom(adapterType)) {
+				final Scenario scenario = getScenario((IResource) adaptableObject);
+				return scenario;
+			} else if (Schedule.class.isAssignableFrom(adapterType)) {
+				final Scenario scenario = getScenario((IResource) adaptableObject);
+				return getSchedule(scenario);
 			}
 		}
 
 		return null;
 	}
 
-	Scenario getScenario(IResource resource) {
+	private Schedule getSchedule(final Scenario scenario) {
+		if (scenario != null) {
+			final ScheduleModel scheduleModel = scenario.getScheduleModel();
+			if (scheduleModel != null) {
+				final int size = scheduleModel.getSchedules().size();
+				if (size > 0) {
+					return scheduleModel.getSchedules().get(size - 1);
+				}
+			}
+		}
+		return null;
+	}
+
+	private Scenario getScenario(final IResource resource) {
 		final Object[] scenarioObjects = scp.getChildren(resource);
 		if (scenarioObjects != null) {
 			for (final Object o : scenarioObjects) {
 				if (o instanceof Scenario) {
-					Scenario scenario = (Scenario) o;
+					final Scenario scenario = (Scenario) o;
 					return scenario;
 				}
 			}
@@ -68,7 +87,7 @@ public class ScenarioTreeNodeClassAdapterFactory implements IAdapterFactory {
 	}
 
 	@Override
-	public Class[] getAdapterList() {
+	public Class<?>[] getAdapterList() {
 		return new Class[] { Scenario.class, IResource.class };
 	}
 }
