b8c89a87ccbd67f45258f07828a08e9680aadd3e
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-03-23 17:45:41 +0000

    Create a shared Handler implementation to add in job hooks to keep the commands enabled state in sync as job state changes

--- com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/PauseOptimisationHandler.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/PauseOptimisationHandler.java
@@ -2,7 +2,6 @@ package com.mmxlabs.rcp.navigator.handlers;
 
 import java.util.Iterator;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
@@ -10,8 +9,6 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
-
-import scenario.Scenario;
 
 import com.mmxlabs.jobcontoller.Activator;
 import com.mmxlabs.jobcontroller.core.IManagedJob;
@@ -23,12 +20,13 @@ import com.mmxlabs.jobcontroller.core.IManagedJob.JobState;
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
-public class PauseOptimisationHandler extends AbstractHandler {
+public class PauseOptimisationHandler extends AbstractOptimisationHandler {
 
 	/**
 	 * The constructor.
 	 */
 	public PauseOptimisationHandler() {
+
 	}
 
 	/**
@@ -38,28 +36,23 @@ public class PauseOptimisationHandler extends AbstractHandler {
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 
-		final String id = event.getCommand().getId();
-
 		final ISelection selection = HandlerUtil
 				.getActiveWorkbenchWindow(event).getActivePage().getSelection();
 		if (selection != null & selection instanceof IStructuredSelection) {
 			final IStructuredSelection strucSelection = (IStructuredSelection) selection;
 
-			if (id.equals("com.mmxlabs.rcp.navigator.commands.optimisation.pause")) {
+			final Iterator<?> itr = strucSelection.iterator();
+			while (itr.hasNext()) {
+				final Object obj = itr.next();
+				if (obj instanceof IResource) {
+					final IResource resource = (IResource) obj;
 
-				final Iterator<?> itr = strucSelection.iterator();
-				while (itr.hasNext()) {
-					final Object obj = itr.next();
-					if (obj instanceof IResource) {
-						IResource resource = (IResource)obj;
-						
-						IManagedJob job = Activator.getDefault().getJobManager().findJobForResource(resource);
+					final IManagedJob job = Activator.getDefault()
+							.getJobManager().findJobForResource(resource);
 
-
-						if (job != null) {
-							if (job.getJobState() == JobState.RUNNING) {
-								job.pause();
-							}
+					if (job != null) {
+						if (job.getJobState() == JobState.RUNNING) {
+							job.pause();
 						}
 					}
 				}
@@ -97,9 +90,10 @@ public class PauseOptimisationHandler extends AbstractHandler {
 			while (itr.hasNext()) {
 				final Object obj = itr.next();
 				if (obj instanceof IResource) {
-					IResource resource = (IResource)obj;
-					Scenario s = (Scenario)resource.getAdapter(Scenario.class);
-					IManagedJob job = Activator.getDefault().getJobManager().findJobForResource(resource);
+					final IResource resource = (IResource) obj;
+
+					final IManagedJob job = Activator.getDefault()
+							.getJobManager().findJobForResource(resource);
 
 					if (job == null) {
 						return false;
