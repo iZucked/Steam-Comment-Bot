f9aa6c48093ec75fa6c5bbba3e1cf52ae6675962
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-04-07 06:26:11 -0100

    Update license headers

--- com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/DateTreeSetTest.java
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/DateTreeSetTest.java
@@ -4,14 +4,49 @@
  */
 package com.mmxlabs.common;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.mmxlabs.common.impl.MappingTransformer;
 
 public class DateTreeSetTest {
 
 	@Test
 	public void testDateTreeSet() {
+		
+		final Object obj1 = new Object();
+		final Object obj2 = new Object();
+		final Object obj3 = new Object();
+		
+		final Date date1 = createDate(2011, 01, 01); 
+		final Date date2 = createDate(2011, 01, 01);
+		final Date date3 = createDate(2011, 01, 02);
+		
+		final Map<Object, Date> map = CollectionsUtil.makeHashMap(obj1, date1, obj2, date2, obj3, date3);
+		
+		final ITransformer<Object, Date> transformer = new MappingTransformer<Object, Date>(map);
+		
+		final DateTreeSet<Object> dts = new DateTreeSet<Object>(transformer);
+		
+		dts.add(obj1);
+		
+		Assert.assertTrue(dts.contains(obj1));
+		Assert.assertTrue(dts.contains(date1));
+		
+		Assert.assertFalse(dts.contains(obj2));
+		Assert.assertTrue(dts.contains(date2));
+		
+		Assert.assertFalse(dts.contains(obj3));
+		Assert.assertFalse(dts.contains(date3));
+	
+		Assert.assertEquals(1, dts.size());
+		
 		fail("Not yet implemented");
 	}
 
@@ -220,4 +255,15 @@ public class DateTreeSetTest {
 		fail("Not yet implemented");
 	}
 
+	
+	private Date createDate(final int year, final int month, final int day) {
+		
+		final Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, month);
+		c.set(Calendar.DAY_OF_MONTH, day);
+		return c.getTime();
+	}
 }
+
