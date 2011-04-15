92a2a4ed3a9fea4bab8f4ec75be7ee8b35b368cc
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-04-15 05:55:53 -0100

    Add actions to copy table and tree contents to the system clipboard

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/actions/CopyTableToClipboardAction.java
@@ -0,0 +1,96 @@
+package com.mmxlabs.rcp.common.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+import com.mmxlabs.rcp.common.Activator;
+
+/**
+ * Copies "rendered" table contents to the clipboard. This will maintain the
+ * column order, sort order and label provider output.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class CopyTableToClipboardAction extends Action {
+
+	private final Table table;
+
+	private final char separator;
+
+	public CopyTableToClipboardAction(final Table table) {
+		this(table, '\t');
+	}
+
+	public CopyTableToClipboardAction(final Table table, final char separator) {
+
+		super("Copy");
+
+		this.table = table;
+		this.separator = separator;
+
+		setText("Copy");
+		setDescription("Copies table data into the clipboard");
+		setToolTipText("Copies table data into the clipboard");
+		setImageDescriptor(Activator.getImageDescriptor("/icons/copy.gif"));
+	}
+
+	@Override
+	public void run() {
+
+		final StringBuffer sb = new StringBuffer();
+
+		// Note this may be zero if no columns have been defined. However an
+		// implicit column will be created in such cases
+		final int numColumns = table.getColumnCount();
+
+		// Header row
+		for (int i = 0; i < numColumns; ++i) {
+			final TableColumn tc = table.getColumn(i);
+			sb.append(tc.getText());
+			if ((i + 1) == numColumns) {
+				sb.append("\n");
+			} else {
+				sb.append(separator);
+			}
+		}
+
+		for (final TableItem item : table.getItems()) {
+			// Ensure at least 1 column to grab data
+			processTableItem(sb, Math.max(1, numColumns), item);
+		}
+
+		// Create a new clipboard instance
+		final Display display = Display.getDefault();
+		final Clipboard cb = new Clipboard(display);
+		try {
+			// Create the text transfer and set the contents
+			final TextTransfer textTransfer = TextTransfer.getInstance();
+			cb.setContents(new Object[] { sb.toString() },
+					new Transfer[] { textTransfer });
+		} finally {
+			// Clean up our local resources - system clipboard now has the data
+			cb.dispose();
+		}
+	}
+
+	private void processTableItem(final StringBuffer sb, final int numColumns,
+			final TableItem item) {
+		for (int i = 0; i < numColumns; ++i) {
+
+			sb.append(item.getText(i));
+			// Add EOL or separator char as appropriate
+			if ((i + 1) == numColumns) {
+				sb.append("\n");
+			} else {
+				sb.append(separator);
+			}
+		}
+	}
+}
