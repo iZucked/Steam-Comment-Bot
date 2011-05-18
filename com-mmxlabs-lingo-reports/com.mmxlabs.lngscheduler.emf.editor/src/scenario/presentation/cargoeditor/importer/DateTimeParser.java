dac83974dd6a114fcca84661e599c892b3282577
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-05-18 13:14:27 -0100

    parse more kinds of date and time

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/DateTimeParser.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/DateTimeParser.java
@@ -9,6 +9,9 @@ import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
 
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+
 /**
  * Attempts to parse a date/time automatically
  * 
@@ -26,9 +29,30 @@ public class DateTimeParser {
 		return INSTANCE;
 	}
 
+	/**
+	 * Attempt to parse the given string into a date using all known
+	 * date / date+time formats in the current locale.
+	 * @param dateString
+	 * @return
+	 */
 	public Date parseDate(final String dateString) {
 		final Locale locale = Locale.getDefault();
 
+		//try parsing as a date and time
+		for (int style = DateFormat.FULL; style <= DateFormat.SHORT; style++) {
+			for (int timeStyle = DateFormat.FULL; timeStyle <= DateFormat.SHORT; timeStyle++) {
+				DateFormat df = DateFormat.getDateTimeInstance(style, timeStyle, locale);
+				try {
+					Date result = df.parse(dateString);
+					// either return "true", or return the Date obtained Date object
+					return result;
+				} catch (ParseException ex) {
+					continue;
+				}
+			}
+		}
+		
+		//maybe it's just a date?
 		for (int style = DateFormat.FULL; style <= DateFormat.SHORT; style++) {
 			DateFormat df = DateFormat.getDateInstance(style, locale);
 			try {
@@ -40,6 +64,12 @@ public class DateTimeParser {
 			}
 		}
 
-		return null;
+		// last try - use ECore
+		try {
+			return (Date) EcoreFactory.eINSTANCE.createFromString(
+					EcorePackage.eINSTANCE.getEDate(), dateString);
+		} catch (final Exception ex) {
+			return null;
+		}
 	}
 }
