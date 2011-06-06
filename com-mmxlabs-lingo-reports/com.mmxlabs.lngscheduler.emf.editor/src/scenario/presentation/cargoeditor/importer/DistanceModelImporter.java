a489fad02c875bb6692d70836f20a5d7491f94c4
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-06 16:07:51 -0100

    Add export mode to distance model exporter.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/DistanceModelImporter.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/DistanceModelImporter.java
@@ -8,12 +8,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.TreeSet;
 
 import org.eclipse.emf.ecore.EObject;
 
+import scenario.port.Canal;
 import scenario.port.DistanceLine;
 import scenario.port.DistanceModel;
+import scenario.port.Port;
 import scenario.port.PortFactory;
 import scenario.port.PortPackage;
 
@@ -63,6 +70,48 @@ public class DistanceModelImporter extends EObjectImporter {
 		}
 
 		return Collections.singletonList((EObject) result);
+	}
+
+	@Override
+	public Map<String, Collection<Map<String, String>>> exportObjects(
+			final Collection<EObject> objects) {
+		final Map<String, Collection<Map<String, String>>> result = new HashMap<String, Collection<Map<String, String>>>();
+		for (final EObject model : objects) {
+			if (model instanceof DistanceModel) {
+				final EObject container = model.eContainer();
+				String outputName = "Default-Distances";
+				if (container instanceof Canal) {
+					outputName = ((Canal) container).getName() + "-Distances"; 
+				}
+				
+				final LinkedList<Map<String, String>> rows = new LinkedList<Map<String, String>>();
+				final DistanceModel dm = (DistanceModel) model;
+				final TreeSet<Port> ports = new TreeSet<Port>(new Comparator<Port>() {
+					@Override
+					public int compare(Port arg0, Port arg1) {
+						return arg0.getName().compareTo(arg1.getName());
+					}});
+				for (final DistanceLine dl : dm.getDistances()) {
+					ports.add(dl.getFromPort());
+					ports.add(dl.getToPort());
+				}
+				
+				for (final Port fromPort : ports) {
+					final LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+					for (final Port toPort : ports) {
+						map.put("From", fromPort.getName());
+						for (final DistanceLine dl : dm.getDistances()) {
+							if (dl.getFromPort() == fromPort && dl.getToPort() == toPort) {
+								map.put(toPort.getName(), dl.getDistance() + "");
+							}
+						}
+					}
+				}
+				
+				result.put(outputName, rows);
+			}
+		}
+		return result;
 	}
 
 	@Override
