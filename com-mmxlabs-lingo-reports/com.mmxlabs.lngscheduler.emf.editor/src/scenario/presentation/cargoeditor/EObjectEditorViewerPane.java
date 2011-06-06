0f9253d997ae63bed540bc9b9084f769041a5094
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-06-06 21:20:57 -0100

    Organise imports

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/EObjectEditorViewerPane.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/EObjectEditorViewerPane.java
@@ -5,7 +5,6 @@
 
 package scenario.presentation.cargoeditor;
 
-import java.lang.ref.Reference;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -34,7 +33,6 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.EFactory;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
