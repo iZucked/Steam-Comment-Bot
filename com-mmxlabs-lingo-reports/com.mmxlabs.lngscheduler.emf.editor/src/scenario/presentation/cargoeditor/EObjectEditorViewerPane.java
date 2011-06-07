a35bf3507385b3535b501e843de307bd29829ab2
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-07 09:00:52 -0100

    Add hack to prevent canals making everything slow

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/EObjectEditorViewerPane.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/EObjectEditorViewerPane.java
@@ -83,6 +83,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 
 import scenario.NamedObject;
+import scenario.port.Canal;
 import scenario.presentation.ScenarioEditor;
 import scenario.presentation.cargoeditor.handlers.AddAction;
 import scenario.presentation.cargoeditor.importer.ExportCSVAction;
@@ -287,6 +288,9 @@ public class EObjectEditorViewerPane extends ViewerPane {
 					return Display.getCurrent().getSystemColor(SWT.COLOR_RED);
 				}
 
+				//TODO hack because this is very slow and canals contain many elements
+				if (object instanceof Canal) return super.getBackground(element);
+				
 				final TreeIterator<EObject> iterator = object.eAllContents();
 				while (iterator.hasNext()) {
 					if (hasValidationError(iterator.next()))
@@ -652,8 +656,16 @@ public class EObjectEditorViewerPane extends ViewerPane {
 						// get a dangling reference
 
 						// now perform replace in the original container
-						cc.append(ReplaceCommand.create(editingDomain,
-								oldObject, Collections.singleton(newObject)));
+						final Command replace = ReplaceCommand.create(
+								editingDomain, oldObject.eContainer(), oldObject.eContainingFeature(), oldObject, 
+								Collections.singleton(newObject));
+						
+						if (replace.canExecute() == false) {
+							System.err
+									.println("Cannot execute replace command from "
+											+ oldObject + " to " + newObject);
+						}
+						cc.append(replace);
 					} else {
 						// just do the add
 						cc.append(AddCommand.create(editingDomain, container,
@@ -661,6 +673,10 @@ public class EObjectEditorViewerPane extends ViewerPane {
 					}
 				}
 
+				if (cc.canExecute() == false) {
+					System.err
+							.println("Warning: cannot evaluate import command");
+				}
 				editingDomain.getCommandStack().execute(cc);
 			}
 
@@ -698,14 +714,21 @@ public class EObjectEditorViewerPane extends ViewerPane {
 				}
 				if (cc.getCommandList().isEmpty())
 					return null;
-				else
+				else {
+					if (cc.canExecute() == false) {
+						System.err.println("Cannot execute ref fixer from "
+								+ oldObject + " to " + newObject);
+					}
 					return cc;
+				}
+
 			}
 
 			/**
 			 * Create a command which updates references to oldObject so that
 			 * they point to newObject instead. Returns null if there are no
-			 * references to update (because an empty compoundCommand is not executable.
+			 * references to update (because an empty compoundCommand is not
+			 * executable.
 			 * 
 			 * @param oldObject
 			 * @param newObject
@@ -756,10 +779,14 @@ public class EObjectEditorViewerPane extends ViewerPane {
 							setting.getEObject(),
 							setting.getEStructuralFeature(), newObject));
 				}
-				
+
 				if (cc.getCommandList().isEmpty())
 					return null;
-				
+
+				if (cc.canExecute() == false) {
+					System.err.println("Cannot fix references from "
+							+ oldObject + " to " + newObject);
+				}
 				return cc;
 			}
 
