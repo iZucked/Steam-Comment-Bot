9b6b720dbf28409cb80e59a6bf57876c2c8fdb78
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-09 07:45:02 -0100

    Don't schedule job twice, because that would be silly.

--- com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/StartOptimisationHandler.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.rcp.navigator/src/com/mmxlabs/rcp/navigator/handlers/StartOptimisationHandler.java
@@ -129,7 +129,7 @@ public class StartOptimisationHandler extends AbstractOptimisationHandler {
 		};
 
 		eclipseJob.setPriority(Job.SHORT);
-		eclipseJob.schedule();
+//		eclipseJob.schedule();
 		
 //		newJob.prepare();
 
