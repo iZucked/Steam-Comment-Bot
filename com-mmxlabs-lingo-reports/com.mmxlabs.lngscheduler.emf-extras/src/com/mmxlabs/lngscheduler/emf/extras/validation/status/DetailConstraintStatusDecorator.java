c4e0b114e751d58242218e69d01d5bedfd375a73
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-06-17 07:23:03 -0100

    Created a subclass of the IConstrainStatus interface to record the eobject and feature which the error message directly relates to. Added sample implementation to CargoVolumeConstraint.

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/validation/status/DetailConstraintStatusDecorator.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.lngscheduler.emf.extras.validation.status;
+
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+import org.eclipse.emf.validation.model.IModelConstraint;
+
+/**
+ * Implementation of {@link IDetailConstraintStatus} wrapping a
+ * {@link IConstraintStatus} instance and adding in the extra methods for the
+ * {@link IDetailConstraintStatus} interface
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class DetailConstraintStatusDecorator implements IDetailConstraintStatus {
+
+	private final IConstraintStatus status;
+	private final String feature;
+	private final EObject object;
+
+	public DetailConstraintStatusDecorator(final IConstraintStatus status,
+			final EObject object, final String feature) {
+		this.status = status;
+		this.feature = feature;
+		this.object = object;
+	}
+
+	@Override
+	public IModelConstraint getConstraint() {
+		return status.getConstraint();
+	}
+
+	@Override
+	public EObject getTarget() {
+		return status.getTarget();
+	}
+
+	@Override
+	public Set<EObject> getResultLocus() {
+		return status.getResultLocus();
+	}
+
+	@Override
+	public IStatus[] getChildren() {
+		return status.getChildren();
+	}
+
+	@Override
+	public int getCode() {
+		return status.getCode();
+	}
+
+	@Override
+	public Throwable getException() {
+		return status.getException();
+	}
+
+	@Override
+	public String getMessage() {
+		return status.getMessage();
+	}
+
+	@Override
+	public String getPlugin() {
+		return status.getPlugin();
+	}
+
+	@Override
+	public int getSeverity() {
+		return status.getSeverity();
+	}
+
+	@Override
+	public boolean isMultiStatus() {
+		return status.isMultiStatus();
+	}
+
+	@Override
+	public boolean isOK() {
+		return status.isOK();
+	}
+
+	@Override
+	public boolean matches(final int severityMask) {
+		return status.matches(severityMask);
+	}
+
+	@Override
+	public String getFeature() {
+		return feature;
+	}
+
+	@Override
+	public final EObject getObject() {
+		return object;
+	}
+
+}
---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/validation/status/IDetailConstraintStatus.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.lngscheduler.emf.extras.validation.status;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+/**
+ * Extended version of {@link IConstraintStatus} adding additional details about
+ * which {@link EStructuralFeature} of an {@link EObject} the status refers to.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public interface IDetailConstraintStatus extends IConstraintStatus {
+
+	/**
+	 * Returns the name of the feature as defined by
+	 * {@link EStructuralFeature#getName()} this status applies to.
+	 * 
+	 * @return
+	 */
+	String getFeature();
+
+	/**
+	 * The {@link EObject} instance {@link #getFeature()} relates to. This may
+	 * be different to {@link IConstraintStatus}{@link #getTarget()} if the
+	 * {@link AbstractModelConstraint} checks references.
+	 * 
+	 * @return
+	 */
+	EObject getObject();
+}
