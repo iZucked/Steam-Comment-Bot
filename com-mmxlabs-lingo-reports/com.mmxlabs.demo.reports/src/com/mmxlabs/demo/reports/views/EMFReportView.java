a48d7e268e01b79ca2e074cffe4cbad61ae0b32a
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-24 08:05:17 -0100

    note to self

--- com-mmxlabs-lingo-reports/com.mmxlabs.demo.reports/src/com/mmxlabs/demo/reports/views/EMFReportView.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.demo.reports/src/com/mmxlabs/demo/reports/views/EMFReportView.java
@@ -161,6 +161,10 @@ public abstract class EMFReportView extends ViewPart implements
 				}
 			}
 			if (object instanceof Schedule) {
+				
+				//TODO fix this, it's not correct nor is it reliable
+				// once a schedule has been replaced it's removed from container.
+				// bad
 				Scenario s = (Scenario)((Schedule)object).eContainer().eContainer();
 				return s.getName();
 			} else {
