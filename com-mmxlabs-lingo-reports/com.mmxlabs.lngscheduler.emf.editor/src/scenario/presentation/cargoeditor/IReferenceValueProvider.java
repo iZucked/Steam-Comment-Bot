25ab51d05c54fe85dfa04c339afc1391fd8fdc0a
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-29 14:22:18 -0100

    Add method to get notifiers for a given object.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/IReferenceValueProvider.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/IReferenceValueProvider.java
@@ -6,6 +6,7 @@ package scenario.presentation.cargoeditor;
 
 import java.util.List;
 
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
@@ -17,5 +18,9 @@ public interface IReferenceValueProvider {
 			final EStructuralFeature field);
 
 	public String getName(final EObject referer, final EReference feature,
+			final EObject referenceValue);
+
+	public Iterable<Pair<Notifier, List<Object>>> getNotifiers(
+			final EObject referer, final EReference feature,
 			final EObject referenceValue);
 }
