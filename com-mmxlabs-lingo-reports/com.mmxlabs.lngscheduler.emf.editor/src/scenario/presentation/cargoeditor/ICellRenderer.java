673fc2af1639642c2139fa952f873e89746767a5
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-06-29 14:20:54 -0100

    Add method to get un-contained notifiers for a cell

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/ICellRenderer.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/ICellRenderer.java
@@ -4,22 +4,44 @@
  */
 package scenario.presentation.cargoeditor;
 
+import java.util.List;
+
+import org.eclipse.emf.common.notify.Notifier;
+
+import com.mmxlabs.common.Pair;
+
 /**
+ * Implementors are used by {@link EObjectEditorViewerPane} to render (as text)
+ * and sort columns. Rendering and sorting are together because they are
+ * typically operations decided by the type of the thing being rendered.
+ * 
  * @author hinton
- *
+ * 
  */
 public interface ICellRenderer {
 	/**
 	 * Render the given object for viewing in a table cell.
+	 * 
 	 * @param object
 	 * @return string rep. of object
 	 */
 	String render(Object object);
-	
+
 	/**
 	 * Get a comparable representation of the object, for sorting
+	 * 
 	 * @param object
-	 * @return
+	 * @return comparable for sorting object by this cell.
 	 */
 	Comparable getComparable(Object object);
+
+	/**
+	 * Get any non-contained notifiers which should be listened to for
+	 * triggering a refresh on the given object. We could use a modified
+	 * EContentAdapter to track non-containment references, but that leads to
+	 * refreshing every table whenever anything is changed.
+	 * 
+	 * @return
+	 */
+	Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(Object object);
 }
