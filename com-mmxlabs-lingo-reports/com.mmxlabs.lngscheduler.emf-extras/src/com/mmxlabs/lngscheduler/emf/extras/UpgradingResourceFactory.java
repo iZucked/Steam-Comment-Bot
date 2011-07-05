d53342d5f49e9e12f6b4d4a241f2952ba8281c6d
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-07-05 08:32:36 -0100

    Use NullProgressMonitor

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/UpgradingResourceFactory.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/UpgradingResourceFactory.java
@@ -7,6 +7,7 @@ package com.mmxlabs.lngscheduler.emf.extras;
 import java.util.Collections;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.Resource.Factory;
@@ -71,56 +72,7 @@ public class UpgradingResourceFactory implements Factory {
 				try {
 					log.info("Migrating " + uri + " to version " + r);
 					migrator.migrateAndSave(Collections.singletonList(uri), r,
-							Integer.MAX_VALUE, new IProgressMonitor() {
-
-								@Override
-								public void worked(int work) {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public void subTask(String name) {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public void setTaskName(String name) {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public void setCanceled(boolean value) {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public boolean isCanceled() {
-									// TODO Auto-generated method stub
-									return false;
-								}
-
-								@Override
-								public void internalWorked(double work) {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public void done() {
-									// TODO Auto-generated method stub
-
-								}
-
-								@Override
-								public void beginTask(String name, int totalWork) {
-									// TODO Auto-generated method stub
-
-								}
-							});
+							Integer.MAX_VALUE, new NullProgressMonitor());
 				} catch (final MigrationException e) {
 					throw new RuntimeException(e);
 				}
