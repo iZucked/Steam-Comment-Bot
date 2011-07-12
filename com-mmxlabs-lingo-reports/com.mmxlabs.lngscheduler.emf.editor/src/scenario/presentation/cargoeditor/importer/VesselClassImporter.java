dad6b7b4e546fbc586f14e69c306a1925b3658bc
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-07-12 13:54:14 -0100

    Add canal costs to fleet file

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/VesselClassImporter.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/cargoeditor/importer/VesselClassImporter.java
@@ -5,14 +5,18 @@
 package scenario.presentation.cargoeditor.importer;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 
+import scenario.fleet.FleetPackage;
 import scenario.fleet.FuelConsumptionLine;
 import scenario.fleet.VesselClass;
+import scenario.fleet.VesselClassCost;
 import scenario.fleet.VesselStateAttributes;
 
 /**
@@ -22,6 +26,15 @@ import scenario.fleet.VesselStateAttributes;
  * 
  */
 public class VesselClassImporter extends EObjectImporter {
+
+	private static final String TRANSIT_FUEL = "transitFuel";
+
+	private static final String TRANSIT_TIME = "transitTime";
+
+	private static final String UNLADEN_COST = "unladenCost";
+
+	private static final String LADEN_COST = "ladenCost";
+
 	@Override
 	public Map<String, Collection<Map<String, String>>> exportObjects(
 			Collection<? extends EObject> objects) {
@@ -39,10 +52,10 @@ public class VesselClassImporter extends EObjectImporter {
 		}
 
 		result.put("Fuel Curve", lines);
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * @param vcName
 	 * @param ballastAttributes
@@ -62,4 +75,70 @@ public class VesselClassImporter extends EObjectImporter {
 
 		return out;
 	}
+
+	@Override
+	protected void flattenMultiContainment(EObject object, String prefix,
+			EReference reference, Map<String, String> output) {
+		if (reference == FleetPackage.eINSTANCE.getVesselClass_CanalCosts()) {
+			final EObjectImporter importer = EObjectImporterFactory
+					.getInstance().getImporter(
+							FleetPackage.eINSTANCE.getVesselClassCost());
+			
+			for (final VesselClassCost vcc : ((VesselClass) object)
+					.getCanalCosts()) {
+				final Map<String, String> map = importer.exportObject((EObject) vcc);
+				final String p2 = prefix + vcc.getCanal().getName() + SEPARATOR;
+				for (final Map.Entry<String, String> e : map.entrySet()) {
+					if (e.getKey().equalsIgnoreCase("canal")) continue;
+					output.put(p2 + e.getKey(), e.getValue());
+				}
+			}
+		} else {
+			super.flattenMultiContainment(object, prefix, reference, output);
+		}
+	}
+
+	@Override
+	protected void populateContainment(String prefix, EObject result,
+			EReference reference, Map<String, String> fields,
+			Collection<DeferredReference> deferredReferences,
+			NamedObjectRegistry registry) {
+		if (reference == FleetPackage.eINSTANCE.getVesselClass_CanalCosts()) {
+			final Map<String, Map<String, String>> canalCosts = new HashMap<String, Map<String, String>>();
+			for (final String fieldName : fields.keySet()) {
+				if (fieldName.startsWith(prefix + SEPARATOR + "canal")) {
+					final String fn2 = getCurrentReader().getCasedColumnName(
+							fieldName).substring(
+							(prefix + SEPARATOR + "canal").length());
+					final String canalName = fn2.substring(0,
+							fn2.indexOf(SEPARATOR));
+					final String rest = fn2
+							.substring(fn2.indexOf(SEPARATOR) + 1);
+					Map<String, String> vals = canalCosts.get(canalName);
+					if (vals == null) {
+						vals = new HashMap<String, String>();
+						canalCosts.put(canalName, vals);
+						vals.put("canal", canalName);
+					}
+					vals.put(rest.toLowerCase(), fields.get(fieldName));
+				}
+			}
+
+			final EObjectImporter importer = EObjectImporterFactory
+					.getInstance().getImporter(
+							FleetPackage.eINSTANCE.getVesselClassCost());
+
+			final VesselClass vc = (VesselClass) result;
+
+			for (final Map<String, String> canalCost : canalCosts.values()) {
+				vc.getCanalCosts().add(
+						(VesselClassCost) importer.importObject(canalCost,
+								deferredReferences, registry));
+			}
+		} else {
+			super.populateContainment(prefix, result, reference, fields,
+					deferredReferences, registry);
+		}
+	}
+
 }
