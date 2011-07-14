7429ca858fb16dd0faed32680d0f3c1167fcbc3f
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-07-14 14:53:30 -0100

    Add distance editor action to port editor viewer pane

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/distance_editor/DistanceEditorDialog.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/distance_editor/DistanceEditorDialog.java
@@ -4,20 +4,40 @@
  */
 package scenario.presentation.distance_editor;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.PlatformUI;
+
+import com.mmxlabs.common.Pair;
 
 import scenario.port.DistanceModel;
+import scenario.port.PortPackage;
 import scenario.presentation.ScenarioEditor;
+import scenario.presentation.cargoeditor.importer.CSVReader;
+import scenario.presentation.cargoeditor.importer.DeferredReference;
+import scenario.presentation.cargoeditor.importer.EObjectImporter;
+import scenario.presentation.cargoeditor.importer.EObjectImporterFactory;
+import scenario.presentation.cargoeditor.importer.NamedObjectRegistry;
 
 /**
  * A dialog for containing a {@link DistanceLineViewer}. It's easier to have it
@@ -28,6 +48,7 @@ import scenario.presentation.ScenarioEditor;
  * 
  */
 public class DistanceEditorDialog extends Dialog {
+	private static final int IMPORT_CSV = 0x10;
 	private DistanceLineViewer viewer;
 	private DistanceModel distanceModel;
 	private ScenarioEditor part;
@@ -44,7 +65,8 @@ public class DistanceEditorDialog extends Dialog {
 	protected Control createDialogArea(final Composite parent) {
 		final Composite c = (Composite) super.createDialogArea(parent);
 
-		this.viewer = new DistanceLineViewer(parent, SWT.FULL_SELECTION | SWT.BORDER);
+		this.viewer = new DistanceLineViewer(parent, SWT.FULL_SELECTION
+				| SWT.BORDER);
 
 		viewer.init(part);
 
@@ -53,26 +75,31 @@ public class DistanceEditorDialog extends Dialog {
 		table.setLayout(layout);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-		
+
 		table.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
+
 		viewer.setInput(distanceModel);
 
 		viewer.refresh();
-		
+
 		return c;
 	}
-	
-	
 
 	@Override
 	public void create() {
 		super.create();
-		getShell().setSize(1024,768);
+		getShell().setSize(800, 600);
 		getShell().setText("Distance Matrix Editor");
-		
-		//center shell
-		
+
+		// center shell
+		// center in parent window
+		final Rectangle shellBounds = getParentShell().getBounds();
+		final Point dialogSize = getShell().getSize();
+
+		getShell().setLocation(
+				shellBounds.x + (shellBounds.width - dialogSize.x) / 2,
+				shellBounds.y + (shellBounds.height - dialogSize.y) / 2);
+
 		final TableColumn[] columns = viewer.getTable().getColumns();
 		for (final TableColumn c : columns) {
 			c.pack();
@@ -82,7 +109,7 @@ public class DistanceEditorDialog extends Dialog {
 	public DistanceModel getResult() {
 		return distanceModel;
 	}
-	
+
 	public int open(final ScenarioEditor part, final DistanceModel dm) {
 		this.part = part;
 		this.distanceModel = EcoreUtil.copy(dm);
@@ -92,7 +119,63 @@ public class DistanceEditorDialog extends Dialog {
 
 	@Override
 	protected boolean isResizable() {
-		return false;
+		return true;
 	}
-	
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		super.createButtonsForButtonBar(parent);
+
+		createButton(parent, IMPORT_CSV, "Import CSV", false);
+	}
+
+	@Override
+	protected void buttonPressed(int buttonId) {
+		if (buttonId == IMPORT_CSV) {
+			final FileDialog openDialog = new FileDialog(PlatformUI
+					.getWorkbench().getActiveWorkbenchWindow().getShell(),
+					SWT.OPEN);
+
+			openDialog.setFilterExtensions(new String[] { "*.csv" });
+			openDialog
+					.setText("Choose a CSV file from which to import a distance matrix");
+
+			final String inputFileName = openDialog.open();
+			if (inputFileName == null)
+				return;
+
+			final EObjectImporter importer = EObjectImporterFactory
+					.getInstance().getImporter(
+							PortPackage.eINSTANCE.getDistanceModel());
+			final List<DeferredReference> deferments = new ArrayList<DeferredReference>();
+
+			final NamedObjectRegistry registry = new NamedObjectRegistry();
+
+			registry.addEObjects(part.getScenario());
+
+			CSVReader reader;
+			try {
+				reader = new CSVReader(inputFileName);
+				final Collection<EObject> importedObjects = importer
+						.importObjects(reader, deferments, registry);
+
+				// there should be a single distance model here
+				final Map<Pair<EClass, String>, EObject> m = registry
+						.getContents();
+				for (final DeferredReference dr : deferments) {
+					dr.setRegistry(m);
+					dr.run();
+				}
+				final DistanceModel dm = (DistanceModel) importedObjects
+						.iterator().next();
+				this.distanceModel = dm;
+				viewer.setInput(this.distanceModel);
+				viewer.refresh();
+			} catch (IOException e) {
+			}
+		} else {
+			super.buttonPressed(buttonId);
+		}
+	}
+
 }
