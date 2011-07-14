0a49d7c781b020ae412da4fa4ff215c0ed24163c
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-07-14 09:03:35 -0100

    Add editing support to distance model viewer

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/distance_editor/DistanceLineViewer.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/distance_editor/DistanceLineViewer.java
@@ -11,10 +11,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.command.AddCommand;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.ICellEditorValidator;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -23,6 +32,8 @@ import org.eclipse.swt.widgets.Table;
 import scenario.port.DistanceLine;
 import scenario.port.DistanceModel;
 import scenario.port.Port;
+import scenario.port.PortFactory;
+import scenario.port.PortPackage;
 import scenario.presentation.ScenarioEditor;
 
 import com.mmxlabs.common.Pair;
@@ -84,56 +95,139 @@ public class DistanceLineViewer extends TableViewer {
 					}
 
 					final List<Pair<Port, Map<Port, DistanceLine>>> output = new ArrayList<Pair<Port, Map<Port, DistanceLine>>>();
-					for (final Map.Entry<Port, Map<Port, DistanceLine>> entry : values.entrySet()) {
-						output.add(new Pair<Port, Map<Port,DistanceLine>>(entry.getKey(), entry.getValue()));
+					for (final Map.Entry<Port, Map<Port, DistanceLine>> entry : values
+							.entrySet()) {
+						output.add(new Pair<Port, Map<Port, DistanceLine>>(
+								entry.getKey(), entry.getValue()));
 					}
-					
-					Collections.sort(output, new Comparator<Pair<Port, Map<Port, DistanceLine>>>() {
-						@Override
-						public int compare(
-								Pair<Port, Map<Port, DistanceLine>> o1,
-								Pair<Port, Map<Port, DistanceLine>> o2) {
-							return o1.getFirst().getName().compareTo(o2.getFirst().getName());
-						}
-					});
-					
+
+					Collections
+							.sort(output,
+									new Comparator<Pair<Port, Map<Port, DistanceLine>>>() {
+										@Override
+										public int compare(
+												Pair<Port, Map<Port, DistanceLine>> o1,
+												Pair<Port, Map<Port, DistanceLine>> o2) {
+											return o1
+													.getFirst()
+													.getName()
+													.compareTo(
+															o2.getFirst()
+																	.getName());
+										}
+									});
+
 					return output.toArray();
 				}
 				return new Object[0];
 			}
 		});
-		
-		final TableViewerColumn fromColumn = new TableViewerColumn(this, SWT.NONE);
+
+		final TableViewerColumn fromColumn = new TableViewerColumn(this,
+				SWT.NONE);
 		fromColumn.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
-				return ((Pair<Port, Map<Port, DistanceLine>>) element).getFirst().getName();
+				return ((Pair<Port, Map<Port, DistanceLine>>) element)
+						.getFirst().getName();
 			}
 		});
-		
+
 		final ArrayList<Port> ports = new ArrayList<Port>();
 		ports.addAll(part.getScenario().getPortModel().getPorts());
 		Collections.sort(ports, new Comparator<Port>() {
 			@Override
 			public int compare(Port o1, Port o2) {
 				return o1.getName().compareTo(o2.getName());
-			}});
-		
+			}
+		});
+
 		for (final Port p : ports) {
-			final TableViewerColumn toColumn = new TableViewerColumn(this, SWT.NONE);
+
+			final TableViewerColumn toColumn = new TableViewerColumn(this,
+					SWT.NONE);
 			toColumn.setLabelProvider(new ColumnLabelProvider() {
 				@Override
 				public String getText(Object element) {
 					final Pair<Port, Map<Port, DistanceLine>> e = (Pair<Port, Map<Port, DistanceLine>>) element;
 					final DistanceLine dl = e.getSecond().get(p);
 					if (dl != null) {
-						return ""+dl.getDistance();
+						return "" + dl.getDistance();
 					} else {
 						return "";
 					}
-				}});
-			
-			// TODO add editing support here
+				}
+			});
+
+			toColumn.setEditingSupport(new EditingSupport(this) {
+				@Override
+				protected void setValue(Object element, Object value) {
+					final Pair<Port, Map<Port, DistanceLine>> e = (Pair<Port, Map<Port, DistanceLine>>) element;
+					final String stringValue = value.toString();
+					final Command command;
+					DistanceLine dl = e.getSecond().get(p);
+
+					if (stringValue.isEmpty()) {
+						if (dl == null)
+							return;
+						command = RemoveCommand.create(part.getEditingDomain(),
+								dl);
+					} else {
+						if (dl == null) {
+							dl = PortFactory.eINSTANCE.createDistanceLine();
+							dl.setFromPort(e.getFirst());
+							dl.setToPort(p);
+							dl.setDistance(Integer.parseInt(stringValue));
+							e.getSecond().put(p, dl);
+							command = AddCommand.create(
+									part.getEditingDomain(), getInput(),
+									PortPackage.eINSTANCE
+											.getDistanceModel_Distances(), dl);
+						} else {
+							command = SetCommand.create(
+									part.getEditingDomain(), dl,
+									PortPackage.eINSTANCE
+											.getDistanceLine_Distance(),
+									Integer.parseInt(stringValue));
+						}
+					}
+					part.getEditingDomain().getCommandStack().execute(command);
+				}
+
+				@Override
+				protected Object getValue(Object element) {
+					final Pair<Port, Map<Port, DistanceLine>> e = (Pair<Port, Map<Port, DistanceLine>>) element;
+					final DistanceLine dl = e.getSecond().get(p);
+					if (dl == null) return "";
+					return Integer.toString(dl.getDistance());
+				}
+
+				@Override
+				protected CellEditor getCellEditor(Object element) {
+					final TextCellEditor tce = new TextCellEditor();
+					
+					tce.setValidator(new ICellEditorValidator() {
+						@Override
+						public String isValid(Object value) {
+							final String s = value.toString();
+							if (s.isEmpty()) return null;
+							try {
+								Integer.parseInt(s);
+								return null;
+							} catch (NumberFormatException nfe) {
+								return s + " is not an integer";
+							}
+						}
+					});
+					
+					return tce;
+				}
+
+				@Override
+				protected boolean canEdit(Object element) {
+					return true;
+				}
+			});
 		}
 	}
 }
