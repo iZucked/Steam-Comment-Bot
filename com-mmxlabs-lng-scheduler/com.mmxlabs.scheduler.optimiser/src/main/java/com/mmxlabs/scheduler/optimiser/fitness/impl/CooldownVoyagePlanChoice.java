8cac8fafd9487872eb27fd5e206d1294774fa4df
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-08-17 11:49:53 -0100

    BugzID: 28
    Add IVoyagePlanChoice for cooldown options

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/impl/CooldownVoyagePlanChoice.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ */
+package com.mmxlabs.scheduler.optimiser.fitness.impl;
+
+import com.mmxlabs.common.Equality;
+import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyageOptions;
+
+/**
+ * Sets whether cooldown is used for a given voyage.
+ * 
+ * @author hinton
+ * 
+ */
+public class CooldownVoyagePlanChoice implements IVoyagePlanChoice {
+	private int choice = 0;
+	private final VoyageOptions options;
+
+	public CooldownVoyagePlanChoice(final VoyageOptions options) {
+		this.options = options;
+	}
+
+	@Override
+	public int numChoices() {
+		return 2;
+	}
+
+	@Override
+	public boolean apply(int choice) {
+		final boolean cooldown = choice == 0;
+		options.setAllowCooldown(cooldown);
+
+		if (!cooldown) {
+			// if we are not allowing a cooldown, we have to be using NBO for
+			// travel.
+			return options.useNBOForTravel();
+		}
+
+		return true;
+	}
+
+	@Override
+	public final boolean reset() {
+		for (int i = 0; i < numChoices(); i++) {
+			if (apply(i)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean nextChoice() {
+		while (true) {
+			if (choice + 1 == numChoices()) {
+				return true;
+			}
+			if (apply(choice + 1)) {
+				return false;
+			}
+		}
+	}
+
+	@Override
+	public final boolean equals(final Object obj) {
+
+		if (obj instanceof CooldownVoyagePlanChoice) {
+
+			final CooldownVoyagePlanChoice other = (CooldownVoyagePlanChoice) obj;
+
+			if (!Equality.isEqual(options, other.options)) {
+				return false;
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+}
