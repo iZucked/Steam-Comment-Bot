8f5213157603055a0ce298beb28bc98e2d7dec77
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-08-20 10:01:48 -0100

    Changed 'import junit.framework' to 'org.junit'.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/CalculatorTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/CalculatorTest.java
@@ -5,8 +5,8 @@
 package com.mmxlabs.scheduler.optimiser;
 
 import static org.junit.Assert.fail;
-import junit.framework.Assert;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
