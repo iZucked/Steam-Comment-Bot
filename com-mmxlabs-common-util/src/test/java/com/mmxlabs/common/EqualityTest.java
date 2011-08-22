4d2af3b53e163a43fad6d56b26f9bbe19a6d777d
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-08-22 12:15:54 -0100

    Added tests to shallowEquals.

--- com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/EqualityTest.java
+++ com-mmxlabs-common-util/src/test/java/com/mmxlabs/common/EqualityTest.java
@@ -24,4 +24,61 @@ public class EqualityTest {
 		Assert.assertTrue(Equality.isEqual(b, b));
 	}
 
+	@Test
+	public void testShallowEquals() {
+
+		// test unequal lengths
+		final Object[] twoElementArray = { 0, 1 };
+		final Object[] fourElementArray = { 0, 1, 2, 3 };
+
+		Assert.assertFalse(Equality.shallowEquals(twoElementArray,
+				fourElementArray));
+
+		// Test null args are handled correctly.
+		Assert.assertFalse(Equality.shallowEquals(twoElementArray, null));
+		Assert.assertFalse(Equality.shallowEquals(null, twoElementArray));
+		Assert.assertTrue(Equality.shallowEquals(null, null));
+
+		// Testing the same list returns true
+		Assert.assertTrue(Equality.shallowEquals(twoElementArray,
+				twoElementArray));
+		Assert.assertTrue(Equality.shallowEquals(fourElementArray,
+				fourElementArray));
+
+		// Test two different lists for equality.
+		final Object[] fourElementArrayAgain = { 0, 1, 2, 3 };
+		final Object[] fourElementArrayDifferent = { 1, 1, 2, 3 };
+
+		Assert.assertTrue(Equality.shallowEquals(fourElementArray,
+				fourElementArrayAgain));
+		Assert.assertFalse(Equality.shallowEquals(fourElementArray,
+				fourElementArrayDifferent));
+	}
+
+	/**
+	 * shallowEquals should fail when testing objects that are different, even
+	 * if they have identical content. This is because it tests using == rather
+	 * than the "correct" .equals method.
+	 */
+	@Test
+	public void testShallowEqualsStringEquality() {
+
+		String a1 = new String("a");
+		String b1 = new String("b");
+		String c1 = new String("c");
+
+		String a2 = new String("a");
+		String b2 = new String("b");
+		String c2 = new String("c");
+
+		String[] array1 = { a1, b1, c1 };
+		String[] array2 = { a2, b2, c2 };
+
+		// Methods that use .equals will work.
+		Assert.assertArrayEquals(array1, array2);
+		// Methods that test equality using == will not return true.
+		Assert.assertFalse(Equality.shallowEquals(array1, array2));
+
+	}
+
 }
