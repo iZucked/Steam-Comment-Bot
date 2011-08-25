a487697bc1e669d8589fdcebb7ba541a20d8526c
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-08-25 13:05:37 -0100

    BugzID: 99
    Added constraint for port / vessel class compatibility. Does not flag the vessel class field in editors, because validation is not triggered on vessel instances when the class is modified. Will still get picked up in a global validation.

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/validation/StartEndRequirementPortConstraint.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.lngscheduler.emf.extras.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import scenario.fleet.FleetPackage;
+import scenario.fleet.PortAndTime;
+import scenario.fleet.Vessel;
+import scenario.port.Port;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.lngscheduler.emf.extras.validation.context.ValidationSupport;
+import com.mmxlabs.lngscheduler.emf.extras.validation.status.DetailConstraintStatusDecorator;
+
+/**
+ * Check that start/end requirement matches port constraint
+ * 
+ * @author hinton
+ * 
+ */
+public class StartEndRequirementPortConstraint extends AbstractModelConstraint {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse.emf.validation.IValidationContext)
+	 */
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+		if (target instanceof PortAndTime) {
+			final PortAndTime pat = (PortAndTime) target;
+			if (pat.isSetPort()) {
+				final Pair<EObject, EReference> container = ValidationSupport.getInstance().getContainer(pat);
+				if (container.getFirst() instanceof Vessel) {
+					final Vessel vessel = (Vessel) container.getFirst();
+					final Port port = pat.getPort();
+					if (vessel.getClass_().getInaccessiblePorts().contains(port)) {
+						final String startOrEnd = (container.getSecond() == FleetPackage.eINSTANCE.getVessel_StartRequirement() ? "start" : "end");
+						final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(vessel.getName(), startOrEnd, vessel.getClass_()
+								.getName(),
+								port.getName()));
+
+						dcsd.addEObjectAndFeature(pat, FleetPackage.eINSTANCE.getPortAndTime_Port());
+						dcsd.addEObjectAndFeature(vessel.getClass_(), FleetPackage.eINSTANCE.getVesselClass_InaccessiblePorts());
+						return dcsd;
+					}
+				}
+			}
+		}
+		return ctx.createSuccessStatus();
+	}
+}
