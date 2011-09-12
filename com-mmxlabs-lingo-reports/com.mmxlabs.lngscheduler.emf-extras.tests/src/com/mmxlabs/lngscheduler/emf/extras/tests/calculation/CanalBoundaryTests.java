5c0e7932594446b21e8bbce7c4b77b6dd846d653
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-09-12 07:57:49 -0100

    BugzID: 184 Class for testing canal boundaries.

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras.tests/src/com/mmxlabs/lngscheduler/emf/extras/tests/calculation/CanalBoundaryTests.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.lngscheduler.emf.extras.tests.calculation;
+
+/**
+ * @author Adam Semenenko
+ *
+ */
+public class CanalBoundaryTests {
+
+}
