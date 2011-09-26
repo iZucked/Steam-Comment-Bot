c574b9cd04d2b83222eef2ba9a3bdd43ca1e41ea
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-09-26 10:30:25 -0100

    BugzID: 203 Class for min/max speed tests.

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras.tests/src/com/mmxlabs/lngscheduler/emf/extras/tests/calculation/MinMaxSpeedTests.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.lngscheduler.emf.extras.tests.calculation;
+
+/**
+ * <a href="">Case 203: Test for min/max speed conditions</a>
+ * <p>
+ * Create some tests which check that the min/max speeds are hit under the correct conditions: <br>
+ * - max speed when available time is too small (can cause lateness)<br>
+ * - min speed when available time is long enough and on base fuel mode<br>
+ * - min NBO speed when available time is long enough and on NBO<br>
+ * mode
+ * 
+ * @author Adam Semenenko
+ * 
+ */
+public class MinMaxSpeedTests {
+
+}
