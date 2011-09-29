47c29308834c43062db6518221ccf1410c01b4f3
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-09-29 08:59:05 -0100

    BugzID: 223 Make class final with private constructor. Add more conversion methods.

--- com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/TimeUnitConvert.java
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/TimeUnitConvert.java
@@ -8,11 +8,33 @@ package com.mmxlabs.common;
  * Class for methods that convert measurements that are per one time unit to another unit of time.
  * 
  * @author Adam Semenenko
- *
+ * 
  */
-public class TimeUnitConvert {
+public final class TimeUnitConvert {
 
+	private TimeUnitConvert() {
+	}
 
+	/**
+	 * Convert a unit that is measured per second to per minute (multiply unitPerSecond by 60).
+	 * 
+	 * @param unitPerSecond
+	 *            The measurement per Second to convert to per minute.
+	 * @return The measurement per minute.
+	 */
+	public static int convertPerSecondToPerMinute(int unitPerSecond) {
+		return unitPerSecond * 60;
+	}
+	/**
+	 * Convert a unit that is measured per minute to per hours (multiply unitPerMinute by 60).
+	 * 
+	 * @param unitPerMinute
+	 *            The measurement per minute to convert to per hour.
+	 * @return The measurement per hour.
+	 */
+	public static int convertPerMinuteToPerHour(int unitPerMinute) {
+		return unitPerMinute * 60;
+	}
 	/**
 	 * Convert a unit that is measured per hour to per day (multiply unitPerHour by 24).
 	 * 
