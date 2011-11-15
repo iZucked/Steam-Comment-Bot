49b38572d2c12379dd3e44351e310bce964671a4
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2011-11-15 14:39:20 +0000

    Update methods, fix rates to per day, add assertions.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras.tests/src/com/mmxlabs/lngscheduler/emf/extras/tests/calculation/MultipleCargoFuelConsumptionTests.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras.tests/src/com/mmxlabs/lngscheduler/emf/extras/tests/calculation/MultipleCargoFuelConsumptionTests.java
@@ -8,13 +8,18 @@ import java.util.Date;
 
 import javax.management.timer.Timer;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import scenario.Scenario;
 import scenario.port.Port;
 import scenario.schedule.CargoAllocation;
 import scenario.schedule.Schedule;
+import scenario.schedule.events.FuelQuantity;
+import scenario.schedule.events.FuelType;
 
+import com.mmxlabs.common.TimeUnitConvert;
 import com.mmxlabs.demo.app.wizards.CustomScenarioCreator;
 
 /**
@@ -23,8 +28,12 @@ import com.mmxlabs.demo.app.wizards.CustomScenarioCreator;
  */
 public class MultipleCargoFuelConsumptionTests {
 
-	private static final int dischargePrice = 10;
+	private static final int dischargePrice = 1;
+	private static final int loadPrice = 1;
 
+	/**
+	 * One vessel must take two cargos from port A to port B. NBO should be cheaper always.
+	 */
 	@Test
 	public void twoCargoTest() {
 
@@ -37,11 +46,11 @@ public class MultipleCargoFuelConsumptionTests {
 
 		final String vesselClassName = "vc";
 		final int numOfVesselsToCreate = 1;
-		final float baseFuelUnitPrice = 10;
+		final float baseFuelUnitPrice = 10.01f;
 		final int speed = 10;
 		final int capacity = 1000000;
-		final int consumption = 10;
-		final int NBORate = 10;
+		final int consumption = TimeUnitConvert.convertPerHourToPerDay(10);
+		final int NBORate = TimeUnitConvert.convertPerHourToPerDay(10);
 		final int pilotLightRate = 0;
 		final int minHeelVolume = 0;
 		csc.addVesselSimple(vesselClassName, numOfVesselsToCreate, baseFuelUnitPrice, speed, capacity, consumption, NBORate, pilotLightRate, minHeelVolume);
@@ -53,15 +62,57 @@ public class MultipleCargoFuelConsumptionTests {
 		final Date cargoBStart = new Date(cargoAStart.getTime() + Timer.ONE_HOUR * cargoADuration);
 		final int cargoBDuration = 10;
 
-		csc.addCargo("alpha", portA, portB, dischargePrice, 10, cargoAStart, cargoADuration);
-		csc.addCargo("beta", portA, portB, dischargePrice, 10, cargoBStart, cargoBDuration);
+		csc.addCargo("alpha", portA, portB, loadPrice, dischargePrice, 10, cargoAStart, cargoADuration);
+		csc.addCargo("beta", portA, portB, loadPrice, dischargePrice, 10, cargoBStart, cargoBDuration);
 
 		final Scenario scenario = csc.buildScenario();
 
 		// evaluate and get a schedule
 		final Schedule result = ScenarioTools.evaluate(scenario);
 
+
 		for (CargoAllocation ca : result.getCargoAllocations())
 			ScenarioTools.printScenario(ca.getName(), ca);
+		
+		// add assertions on results
+		for (CargoAllocation ca : result.getCargoAllocations()) {
+			// expect only NBO to be used always
+
+			// check the laden journey
+			for (final FuelQuantity fq : ca.getLadenLeg().getFuelUsage()) {
+				if (fq.getFuelType() == FuelType.FBO)
+					Assert.assertTrue("Laden leg never uses FBO", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.BASE_FUEL)
+					Assert.assertTrue("Laden leg never uses base fuel", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.NBO)
+					Assert.assertTrue("Laden leg uses NBO", fq.getQuantity() > 0);
+			}
+			for (final FuelQuantity fq : ca.getLadenIdle().getFuelUsage()) {
+				if (fq.getFuelType() == FuelType.FBO)
+					Assert.assertTrue("Laden idle never uses FBO", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.BASE_FUEL)
+					Assert.assertTrue("Laden idle never uses base fuel", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.NBO)
+					Assert.assertTrue("Laden idle uses NBO", fq.getQuantity() > 0);
+			}
+
+			// check the ballast journey
+			for (final FuelQuantity fq : ca.getBallastLeg().getFuelUsage()) {
+				if (fq.getFuelType() == FuelType.FBO)
+					Assert.assertTrue("Ballast leg never uses FBO", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.BASE_FUEL)
+					Assert.assertTrue("Ballast leg never uses base fuel", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.NBO)
+					Assert.assertTrue("Ballast leg uses NBO", fq.getQuantity() > 0);
+			}
+			for (final FuelQuantity fq : ca.getBallastIdle().getFuelUsage()) {
+				if (fq.getFuelType() == FuelType.FBO)
+					Assert.assertTrue("Ballast idle never uses FBO", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.BASE_FUEL)
+					Assert.assertTrue("Ballast idle never uses base fuel", fq.getQuantity() == 0);
+				if (fq.getFuelType() == FuelType.NBO)
+					Assert.assertTrue("Ballast idle uses NBO", fq.getQuantity() > 0);
+			}
+		}
 	}
 }
