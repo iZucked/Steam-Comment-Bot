3099597def69c5a42523e1f52ea7c9dca4a42c03
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-11-28 13:49:35 +0000

    Add indexed yes/no tracking thing (not quite a set)

---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/indexedobjects/IIndexBits.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.common.indexedobjects;
+
+/**
+ * A thing which maps a boolean to IIndexedObjects.
+ * 
+ * @author hinton
+ * 
+ */
+public interface IIndexBits<T extends IIndexedObject> {
+	/**
+	 * Returns true if set(element) has been called and clear(element) has not been called subsequently.
+	 * 
+	 * @param element
+	 * @return
+	 */
+	public boolean isSet(T element);
+
+	/**
+	 * Set that an element is marked
+	 * 
+	 * @param element
+	 */
+	public void set(T element);
+
+	/**
+	 * Clear the mark against an element
+	 * 
+	 * @param element
+	 */
+	public void clear(T element);
+}
---
+++ com-mmxlabs-common-util/src/main/java/com/mmxlabs/common/indexedobjects/impl/ArrayIndexBits.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.common.indexedobjects.impl;
+
+import java.util.BitSet;
+
+import com.mmxlabs.common.indexedobjects.IIndexBits;
+import com.mmxlabs.common.indexedobjects.IIndexedObject;
+
+/**
+ * Uses a BitSet to back
+ * 
+ * @author hinton
+ * 
+ */
+public class ArrayIndexBits<T extends IIndexedObject> implements IIndexBits<T> {
+	final BitSet bits = new BitSet();
+
+	@Override
+	public boolean isSet(T element) {
+		return bits.get(element.getIndex());
+	}
+
+
+	@Override
+	public void set(T element) {
+		bits.set(element.getIndex());
+	}
+
+
+	@Override
+	public void clear(T element) {
+		bits.clear(element.getIndex());
+	}
+}
