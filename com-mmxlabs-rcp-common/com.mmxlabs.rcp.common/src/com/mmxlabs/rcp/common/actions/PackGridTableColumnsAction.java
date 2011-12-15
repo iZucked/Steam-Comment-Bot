20a3fb7fbe4cbf5ac43dd3cee99b5e73fb749696
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-12-15 16:55:38 +0000

    add grid packing action

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/actions/PackGridTableColumnsAction.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+
+package com.mmxlabs.rcp.common.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
+import org.eclipse.nebula.widgets.grid.GridColumn;
+import org.eclipse.swt.widgets.TableColumn;
+
+import com.mmxlabs.rcp.common.Activator;
+
+public class PackGridTableColumnsAction extends Action {
+
+	private final GridTableViewer viewer;
+
+	public PackGridTableColumnsAction(final GridTableViewer viewer) {
+		super("Pack Columns");
+		setImageDescriptor(Activator.getImageDescriptor("/icons/pack.gif"));
+		this.viewer = viewer;
+	}
+
+	@Override
+	public void run() {
+
+		if (!viewer.getControl().isDisposed()) {
+			final GridColumn[] columns = viewer.getGrid().getColumns();
+			for (final GridColumn c : columns) {
+				c.pack();
+			}
+		}
+	}
+}
