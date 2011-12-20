44234491a3222c51cb6bd97bb652f5e3ad0e0f5a
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-12-20 16:00:02 +0000

    add fancy filter bar to viewer

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/model_editors/ScenarioObjectEditorViewerPane.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/model_editors/ScenarioObjectEditorViewerPane.java
@@ -9,21 +9,39 @@ import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.ui.CommonUIPlugin;
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ViewForm;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 
 import scenario.ScenarioPackage;
+import scenario.presentation.LngEditorPlugin;
 import scenario.presentation.ScenarioEditor;
 import scenario.presentation.cargoeditor.EObjectEditorViewerPane;
 import scenario.presentation.cargoeditor.handlers.AddAction;
@@ -121,15 +139,133 @@ public class ScenarioObjectEditorViewerPane extends EObjectEditorViewerPane {
 		};
 		return result;
 	}
+	
+	private Text filterText;
+	
+	public void createControl(Composite parent) 
+	  {
+	    if (getControl() == null)
+	    {
+	      container = parent;
 
+	      // Create view form.    
+	      //control = new ViewForm(parent, getStyle());
+	      control = new ViewForm(parent, SWT.NONE);
+	      control.addDisposeListener
+	        (new DisposeListener()
+	         {
+	           public void widgetDisposed(DisposeEvent event)
+	           {
+	             dispose();
+	           }
+	         });
+	      
+	      control.marginWidth = 0;
+	      control.marginHeight = 0;
+
+	      // Create a title bar.
+	      createTitleBar();
+
+	      final Composite inner = new Composite(control, SWT.NONE);
+		  
+	      filterText = new Text(inner, SWT.NONE);
+	      filterText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	      filterText.setVisible(false);
+	      ((GridData)filterText.getLayoutData()).exclude = true;
+	      final GridLayout layout = new GridLayout(1, false);
+	      layout.marginHeight = 0;
+	      layout.marginWidth = 0;
+	      inner.setLayout(layout);
+	      
+	      viewer = createViewer(inner);
+	      
+	      viewer.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
+	      
+	      control.setContent(inner);
+
+	      control.setTabList(new Control [] { inner });
+	      
+	      // When the pane or any child gains focus, notify the workbench.
+	      control.addListener(SWT.Activate, this);
+	      hookFocus(control);
+	      hookFocus(viewer.getControl());
+	    }
+	  }
+	
 	@Override
-	/**
-	 * Hook a key listener to the viewer to pick up return and display an editor.
-	 * 
-	 * TODO cache editors for efficiency.
-	 */
 	public EObjectTableViewer createViewer(final Composite parent) {
 		final EObjectTableViewer v = super.createViewer(parent);
+		
+		final ActionContributionItem filter = new ActionContributionItem(
+				new Action() {
+					{
+						setChecked(false);
+						setText("Filter table");
+						setImageDescriptor(LngEditorPlugin.Implementation
+								.imageDescriptorFromPlugin(LngEditorPlugin.getPlugin()
+										.getSymbolicName(), "/icons/filter.gif"));
+					}
+					@Override
+					public void run() {
+						filterText.setVisible(!filterText.getVisible());
+						((GridData) filterText.getLayoutData()).exclude = !filterText.getVisible();
+						filterText.getParent().layout(true);
+						if (filterText.isVisible() == false) {
+							v.setFilterString("");
+						} else {
+							filterText.setFocus();
+						}
+						setChecked(filterText.isVisible());
+					}
+					
+				}
+				);
+		
+		getToolBarManager().appendToGroup("filter", filter);
+		
+		filterText.addModifyListener(new ModifyListener() {
+			{
+				final ModifyListener self = this;
+				filterText.addDisposeListener(new DisposeListener() {
+					@Override
+					public void widgetDisposed(DisposeEvent e) {
+						filterText.removeModifyListener(self);
+					}
+				});
+			}
+			@Override
+			public void modifyText(ModifyEvent e) {
+				if (filterText.isVisible()) {
+					v.setFilterString(filterText.getText());
+				} else {
+					v.setFilterString("");
+				}
+			}
+		});
+		
+		filterText.addKeyListener(new KeyAdapter() {
+			{
+				final KeyAdapter self = this;
+				filterText.addDisposeListener(new DisposeListener() {
+					@Override
+					public void widgetDisposed(DisposeEvent e) {
+						filterText.removeKeyListener(self);
+					}
+				});
+			}
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.keyCode == SWT.ESC) {
+					if (filterText.getText().length() > 0) {
+						filterText.setText("");
+					} else {
+						// hide filter if esc is pressed twice
+						filter.getWidget().notifyListeners(SWT.Selection, new Event());
+					}
+				}
+			}
+		});
+		
 		v.getGrid().setCellSelectionEnabled(true);
 		v.getControl().addKeyListener(new KeyListener() {
 			@Override
@@ -172,6 +308,7 @@ public class ScenarioObjectEditorViewerPane extends EObjectEditorViewerPane {
 				}
 			}
 		});
+		
 		return v;
 	}
 }
