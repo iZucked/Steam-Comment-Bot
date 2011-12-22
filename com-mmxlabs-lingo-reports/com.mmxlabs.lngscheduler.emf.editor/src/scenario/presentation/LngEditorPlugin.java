51683b2eed766e50c7aee36b5f9a8430cbd3c368
Tom Hinton <tom.hinton@minimaxlabs.com>
2011-12-22 17:19:54 +0000

    add service tracker for EJM

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/LngEditorPlugin.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf.editor/src/scenario/presentation/LngEditorPlugin.java
@@ -7,6 +7,10 @@ package scenario.presentation;
 import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
 import org.eclipse.emf.common.util.ResourceLocator;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
 
 /**
  * This is the central singleton for the Lng editor plugin.
@@ -19,6 +23,9 @@ public final class LngEditorPlugin extends EMFPlugin {
 	public static final boolean DEBUG_UI_ENABLED;
 
 	private static final String DEBUG_PROPERTY_KEY = "com.mmxlabs.debug_ui";
+	
+	private ServiceTracker<IEclipseJobManager, IEclipseJobManager> jobManagerServiceTracker;
+
 	
 	static {
 		DEBUG_UI_ENABLED = System.getProperty(DEBUG_PROPERTY_KEY).equalsIgnoreCase("true");
@@ -81,6 +88,8 @@ public final class LngEditorPlugin extends EMFPlugin {
 	 * @generated
 	 */
 	public static class Implementation extends EclipseUIPlugin {
+		private ServiceTracker<IEclipseJobManager, IEclipseJobManager> jobManagerServiceTracker;
+		
 		/**
 		 * Creates an instance.
 		 * <!-- begin-user-doc -->
@@ -93,6 +102,25 @@ public final class LngEditorPlugin extends EMFPlugin {
 			// Remember the static instance.
 			//
 			plugin = this;
+		}
+
+		@Override
+		public void start(BundleContext context) throws Exception {
+			super.start(context);
+			jobManagerServiceTracker = new ServiceTracker<IEclipseJobManager, IEclipseJobManager>(context,
+					IEclipseJobManager.class.getName(), null);
+			jobManagerServiceTracker.open();
+		}
+
+		@Override
+		public void stop(BundleContext context) throws Exception {
+			jobManagerServiceTracker.close();
+			jobManagerServiceTracker = null;
+			super.stop(context);
+		}
+		
+		public IEclipseJobManager getJobManager() {
+			return jobManagerServiceTracker.getService();
 		}
 	}
 
