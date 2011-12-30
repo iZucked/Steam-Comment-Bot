a0cfd4f675cbb253d58c8b7908b6d5bbbf1cc7ac
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-12-30 16:15:24 +0000

    Add in the composed adapter factory

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceComposedAdapterFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.scenario.service.ui.navigator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
+import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
+
+import com.mmxlabs.scenario.service.model.provider.ScenarioServiceItemProviderAdapterFactory;
+
+/**
+ * Utility class to create a shared {@link ComposedAdapterFactory} instance with the {@link ScenarioServiceItemProviderAdapterFactory} the basic item provider adapter factories.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class ScenarioServiceComposedAdapterFactory {
+	private static ComposedAdapterFactory instance;
+
+	public final static ComposedAdapterFactory getAdapterFactory() {
+		if (instance == null) {
+			instance = createNewAdapterFactory();
+		}
+		return instance;
+	}
+
+	private static ComposedAdapterFactory createNewAdapterFactory() {
+		return new ComposedAdapterFactory(createFactoryList());
+	}
+
+	public final static List<AdapterFactory> createFactoryList() {
+		final List<AdapterFactory> factories = new ArrayList<AdapterFactory>();
+		factories.add(new ResourceItemProviderAdapterFactory());
+		factories.add(new ScenarioServiceItemProviderAdapterFactory());
+		factories.add(new ReflectiveItemProviderAdapterFactory());
+		return factories;
+	}
+}
