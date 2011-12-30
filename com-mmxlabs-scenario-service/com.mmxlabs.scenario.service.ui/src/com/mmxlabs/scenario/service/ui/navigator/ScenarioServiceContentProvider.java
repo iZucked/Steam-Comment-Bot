69cf8645e27ab1990592237bdd2fc8c79c1d5f0c
Simon Goodall <simon.goodall@minimaxlabs.com>
2011-12-30 16:10:27 +0000

    Use a composable adapter factory to add in reflective factories into content and label providers.

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
@@ -2,11 +2,9 @@ package com.mmxlabs.scenario.service.ui.navigator;
 
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 
-import com.mmxlabs.scenario.service.model.provider.ScenarioServiceItemProviderAdapterFactory;
-
 public class ScenarioServiceContentProvider extends AdapterFactoryContentProvider {
 
 	public ScenarioServiceContentProvider() {
-		super(new ScenarioServiceItemProviderAdapterFactory());
+		super(ScenarioServiceComposedAdapterFactory.getAdapterFactory());
 	}
 }
--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceLabelProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceLabelProvider.java
@@ -2,11 +2,9 @@ package com.mmxlabs.scenario.service.ui.navigator;
 
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 
-import com.mmxlabs.scenario.service.model.provider.ScenarioServiceItemProviderAdapterFactory;
-
 public class ScenarioServiceLabelProvider extends AdapterFactoryLabelProvider {
 
 	public ScenarioServiceLabelProvider() {
-		super(new ScenarioServiceItemProviderAdapterFactory());
+		super(ScenarioServiceComposedAdapterFactory.getAdapterFactory());
 	}
 }
