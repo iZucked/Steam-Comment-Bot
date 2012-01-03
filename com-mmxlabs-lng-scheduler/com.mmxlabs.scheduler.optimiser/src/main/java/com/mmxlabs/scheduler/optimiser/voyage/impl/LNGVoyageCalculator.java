a4d6bb4fd56257f7fc82841e59b6da7be6be4b3e
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-01-03 17:51:53 +0000

    Update copyright headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/voyage/impl/LNGVoyageCalculator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/voyage/impl/LNGVoyageCalculator.java
@@ -1,8 +1,7 @@
 /**
- * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
  * All rights reserved.
  */
-
 package com.mmxlabs.scheduler.optimiser.voyage.impl;
 
 import com.mmxlabs.scheduler.optimiser.Calculator;
