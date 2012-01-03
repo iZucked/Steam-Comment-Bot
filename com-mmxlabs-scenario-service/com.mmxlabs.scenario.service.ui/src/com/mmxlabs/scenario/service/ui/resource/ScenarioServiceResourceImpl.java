85dc821d34df7d24b0746e78229596e8482f1b02
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-01-03 17:31:49 +0000

    Update copyright headers

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/resource/ScenarioServiceResourceImpl.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/resource/ScenarioServiceResourceImpl.java
@@ -1,13 +1,25 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
 package com.mmxlabs.scenario.service.ui.resource;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.emf.query.conditions.eobjects.structuralfeatures.EObjectAttributeValueCondition;
+import org.eclipse.emf.query.statements.FROM;
+import org.eclipse.emf.query.statements.IQueryResult;
+import org.eclipse.emf.query.statements.SELECT;
+import org.eclipse.emf.query.statements.WHERE;
 
 import com.mmxlabs.scenario.service.IScenarioService;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.scenario.service.model.ScenarioServicePackage;
 
 public class ScenarioServiceResourceImpl extends ResourceImpl {
 
@@ -26,15 +38,36 @@ public class ScenarioServiceResourceImpl extends ResourceImpl {
 	@Override
 	public void save(final Map<?, ?> options) throws IOException {
 
-		throw new UnsupportedOperationException("Not yet implemented");
+//		scenarioService.save(uuid, options);
+		
+//		throw new UnsupportedOperationException("Not yet implemented");
 	}
 
 	@Override
 	public void load(final Map<?, ?> options) throws IOException {
 
-		final EObject scenario = scenarioService.getScenario(uri.path());
+		// Remove "/" 
+		final EObject scenario = scenarioService.getScenario(uri.path().substring(1));
 
 		this.getContents().clear();
 		this.getContents().add(scenario);
 	}
+	
+	@Override
+	protected void doUnload() {
+		// TODO Auto-generated method stub
+//		super.doUnload();
+	}
+	
+	public ScenarioInstance findInstance(final String uuid) {
+		final SELECT query = new SELECT(1, new FROM(scenarioService.getServiceModel()), new WHERE(new EObjectAttributeValueCondition(ScenarioServicePackage.eINSTANCE.getScenarioInstance_Uuid(), new org.eclipse.emf.query.conditions.strings.StringValue(uuid))));
+		final IQueryResult queryResult = query.execute();
+
+		final Iterator<EObject> itr = queryResult.iterator();
+		while (itr.hasNext()) {
+			return (ScenarioInstance) itr.next();
+		}
+		return null;
+	}
+
 }
