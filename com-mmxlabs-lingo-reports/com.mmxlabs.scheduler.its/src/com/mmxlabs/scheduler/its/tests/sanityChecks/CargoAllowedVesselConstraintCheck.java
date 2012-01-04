460edce0578c189be1608837794e90e22a97603d
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2012-01-04 15:10:20 +0000

    BugzID: 255 Rename constraint check classes.

--- com-mmxlabs-lingo-reports/com.mmxlabs.scheduler.its/src/com/mmxlabs/scheduler/its/tests/sanityChecks/CargoAllowedVesselConstraintCheck.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.scheduler.its/src/com/mmxlabs/scheduler/its/tests/sanityChecks/CargoAllowedVesselConstraintCheck.java
@@ -32,7 +32,7 @@ import com.mmxlabs.scheduler.its.tests.calculation.ScenarioTools;
  * @author Adam
  * 
  */
-public class CheckCargoAllowedVesselConstraintNotViolated {
+public class CargoAllowedVesselConstraintCheck {
 
 	private static final int dischargePrice = 1;
 	private CustomScenarioCreator csc = new CustomScenarioCreator(dischargePrice);
