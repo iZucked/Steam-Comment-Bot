681404e51520931882beae394ebae944614b5b6a
Adam Semenenko <adam.semenenko@minimaxlabs.com>
2012-01-04 15:29:41 +0000

    BugzID: 255 Rename test class.

--- com-mmxlabs-lingo-reports/com.mmxlabs.scheduler.its/src/com/mmxlabs/scheduler/its/tests/sanityChecks/VesselClassInaccessiblePortConstraintCheck.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.scheduler.its/src/com/mmxlabs/scheduler/its/tests/sanityChecks/VesselClassInaccessiblePortConstraintCheck.java
@@ -32,7 +32,7 @@ import com.mmxlabs.scheduler.its.tests.calculation.ScenarioTools;
  * @author Adam
  * 
  */
-public class VesselClassPortConstraintCheck {
+public class VesselClassInaccessiblePortConstraintCheck {
 
 	private static final int dischargePrice = 1;
 	private CustomScenarioCreator csc = new CustomScenarioCreator(dischargePrice);
