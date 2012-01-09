4636f734a30bda2c1e5193e6d1e64b3c79f8e44c
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-01-09 09:35:44 +0000

    BugzID: 297 (Add load / discharge capability flags to ports, for filtering in the UI) https://mmxlabs.fogbugz.com/default.asp?297
    Add "capabilities" list to Ports

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf/src/scenario/port/impl/PortFactoryImpl.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf/src/scenario/port/impl/PortFactoryImpl.java
@@ -5,6 +5,7 @@
 package scenario.port.impl;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EFactoryImpl;
@@ -80,6 +81,36 @@ public class PortFactoryImpl extends EFactoryImpl implements PortFactory {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
+	public Object createFromString(EDataType eDataType, String initialValue) {
+		switch (eDataType.getClassifierID()) {
+			case PortPackage.PORT_CAPABILITY:
+				return createPortCapabilityFromString(eDataType, initialValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String convertToString(EDataType eDataType, Object instanceValue) {
+		switch (eDataType.getClassifierID()) {
+			case PortPackage.PORT_CAPABILITY:
+				return convertPortCapabilityToString(eDataType, instanceValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	public PortModel createPortModel() {
 		PortModelImpl portModel = new PortModelImpl();
 		return portModel;
@@ -133,6 +164,26 @@ public class PortFactoryImpl extends EFactoryImpl implements PortFactory {
 	public CanalModel createCanalModel() {
 		CanalModelImpl canalModel = new CanalModelImpl();
 		return canalModel;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PortCapability createPortCapabilityFromString(EDataType eDataType, String initialValue) {
+		PortCapability result = PortCapability.get(initialValue);
+		if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+		return result;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertPortCapabilityToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null ? null : instanceValue.toString();
 	}
 
 	/**
