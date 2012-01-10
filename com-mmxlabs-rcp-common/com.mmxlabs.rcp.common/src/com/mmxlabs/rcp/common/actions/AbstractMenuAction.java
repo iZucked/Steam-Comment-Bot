f6146dcb2e284aa1c7319f43cba556ce913efbe4
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-01-10 14:53:42 +0000

    Handy action subclass for adding menus

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/actions/AbstractMenuAction.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.rcp.common.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+
+/**
+ * An abstract superclass for actions with a menu that provide their own menu.
+ * 
+ * @author hinton
+ *
+ */
+public abstract class AbstractMenuAction extends Action implements IMenuCreator {
+	private Menu lastMenu;
+
+	protected AbstractMenuAction(final String label) {
+		super(label, IAction.AS_DROP_DOWN_MENU);
+	}
+
+	@Override
+	public void dispose() {
+		if (lastMenu != null && lastMenu.isDisposed() == false) {
+			lastMenu.dispose();
+		}
+		lastMenu = null;
+	}
+
+	@Override
+	public IMenuCreator getMenuCreator() {
+		return this;
+	}
+
+	@Override
+	public Menu getMenu(final Control parent) {
+		if (lastMenu != null) {
+			lastMenu.dispose();
+		}
+		lastMenu = new Menu(parent);
+
+		populate(lastMenu);
+
+		return lastMenu;
+	}
+	
+	protected void addActionToMenu(final Action a, final Menu m) {
+		final ActionContributionItem aci = new ActionContributionItem(a);
+		aci.fill(m, -1);
+	}
+
+	/**
+	 * Subclasses should fill their menu with actions here.
+	 * 
+	 * @param menu the menu which is about to be displayed
+	 */
+	protected abstract void populate(final Menu menu);
+
+	@Override
+	public Menu getMenu(final Menu parent) {
+		if (lastMenu != null) {
+			lastMenu.dispose();
+		}
+		lastMenu = new Menu(parent);
+
+		populate(lastMenu);
+
+		return lastMenu;
+	}
+
+}
