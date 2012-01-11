7d1c8bddc9c208cf9235b1e9b2c8df864e774eef
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-01-11 18:14:02 +0000

    Add missing @Override tags

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/Activator.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/Activator.java
@@ -43,6 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -81,6 +82,7 @@ public class Activator extends AbstractUIPlugin {
 	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(final BundleContext context) throws Exception {
 		context.removeServiceListener(serviceListener);
 		services.clear();
