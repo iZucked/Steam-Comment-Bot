a3171f72fab9c6b3b77cb4a433f472979b12709f
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-01-25 18:02:18 +0000

    Add generated serial id

--- com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/IncompleteScenarioException.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lngscheduler.emf-extras/src/com/mmxlabs/lngscheduler/emf/extras/IncompleteScenarioException.java
@@ -6,6 +6,11 @@ package com.mmxlabs.lngscheduler.emf.extras;
 
 public class IncompleteScenarioException extends Exception {
 
+	/**
+	 *  Generated Serial - Re-generate should this class change.
+	 */
+	private static final long serialVersionUID = 8957217197214045120L;
+
 	public IncompleteScenarioException(final String string) {
 		super(string);
 	}
