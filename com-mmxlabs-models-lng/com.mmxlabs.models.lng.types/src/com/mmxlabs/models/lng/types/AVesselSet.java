a8bdebc8653b0674c8d731bf1098cc2e46ac62eb
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-06 10:20:34 +0000

    generate new model

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.types/src/com/mmxlabs/models/lng/types/AVesselSet.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.mmxlabs.models.lng.types;
+
+import com.mmxlabs.models.mmxcore.NamedObject;
+import com.mmxlabs.models.mmxcore.UUIDObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>AVessel Set</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see com.mmxlabs.models.lng.types.TypesPackage#getAVesselSet()
+ * @model
+ * @generated
+ */
+public interface AVesselSet extends UUIDObject, NamedObject {
+} // AVesselSet
