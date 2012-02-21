14fc417d7fca56acdcc5cbad00031f2480dc6775
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-21 11:04:25 +0000

    Add index value provider

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/valueproviders/IndexReferenceValueProviderFactory.java
@@ -0,0 +1,39 @@
+package com.mmxlabs.models.lng.pricing.ui.valueproviders;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcorePackage;
+
+import com.mmxlabs.models.lng.pricing.PricingModel;
+import com.mmxlabs.models.lng.pricing.PricingPackage;
+import com.mmxlabs.models.lng.types.TypesPackage;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.valueproviders.IReferenceValueProvider;
+import com.mmxlabs.models.ui.valueproviders.IReferenceValueProviderFactory;
+import com.mmxlabs.models.ui.valueproviders.SimpleReferenceValueProvider;
+
+public class IndexReferenceValueProviderFactory implements IReferenceValueProviderFactory {
+	@Override
+	public IReferenceValueProvider createReferenceValueProvider(final EClass owner,
+			final EReference reference, final MMXRootObject rootObject) {
+		final PricingModel model = rootObject.getSubModel(PricingModel.class);
+		final EClass referenceClass = reference.getEReferenceType();
+		
+		// TODO; there needs to be more cleverness involved here.
+		if (referenceClass == TypesPackage.eINSTANCE.getAIndex()) {
+			return new SimpleReferenceValueProvider(model, PricingPackage.eINSTANCE.getPricingModel_CommodityIndices());
+		} else if (referenceClass == PricingPackage.eINSTANCE.getIndex()) {
+			// look at generic data??
+			// or look at reference? blah.
+			final EClassifier typeArgument = reference.getEGenericType().getETypeArguments().get(0).getERawType();
+			if (typeArgument == EcorePackage.eINSTANCE.getEDoubleObject()) {
+				return new SimpleReferenceValueProvider(model, PricingPackage.eINSTANCE.getPricingModel_CommodityIndices());
+			} else if (typeArgument == EcorePackage.eINSTANCE.getEIntegerObject()) {
+				return new SimpleReferenceValueProvider(model, PricingPackage.eINSTANCE.getPricingModel_CharterIndices());
+			}
+		}
+		
+		return null;
+	}
+}
