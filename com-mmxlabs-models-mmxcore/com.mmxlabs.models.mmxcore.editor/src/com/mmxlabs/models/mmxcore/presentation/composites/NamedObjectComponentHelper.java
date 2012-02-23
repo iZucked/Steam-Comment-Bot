3485133eeeb62178097d47c2a62000797f0541b9
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-23 09:33:44 +0000

    Add component helper for named object (manually)

---
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore.editor/src/com/mmxlabs/models/mmxcore/presentation/composites/NamedObjectComponentHelper.java
@@ -0,0 +1,21 @@
+package com.mmxlabs.models.mmxcore.presentation.composites;
+
+import org.eclipse.emf.ecore.EClass;
+
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
+import com.mmxlabs.models.ui.ComponentHelperUtils;
+import com.mmxlabs.models.ui.IComponentHelper;
+import com.mmxlabs.models.ui.IInlineEditorContainer;
+
+public class NamedObjectComponentHelper implements IComponentHelper {
+	@Override
+	public void addEditorsToComposite(IInlineEditorContainer detailComposite) {
+		addEditorsToComposite(detailComposite, MMXCorePackage.eINSTANCE.getNamedObject());
+	}
+
+	@Override
+	public void addEditorsToComposite(IInlineEditorContainer detailComposite,
+			EClass displayedClass) {
+		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(displayedClass, MMXCorePackage.eINSTANCE.getNamedObject_Name()));
+	}
+}
