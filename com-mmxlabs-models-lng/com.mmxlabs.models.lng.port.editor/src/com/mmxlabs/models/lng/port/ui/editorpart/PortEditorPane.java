9577e0a1828e3fcba295b5e075613c45c96367e3
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-23 22:43:12 +0000

    use base class features

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editorpart/PortEditorPane.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editorpart/PortEditorPane.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 
 import com.mmxlabs.models.lng.port.PortCapability;
 import com.mmxlabs.models.lng.types.ui.tabular.ScenarioTableViewerPane;
-import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
-import com.mmxlabs.models.ui.tabular.BasicAttributeManipulator;
 
 public class PortEditorPane extends ScenarioTableViewerPane {
 
@@ -28,11 +24,12 @@ public class PortEditorPane extends ScenarioTableViewerPane {
 	public void init(List<EReference> path, AdapterFactory adapterFactory) {
 		super.init(path, adapterFactory);
 		
-		addTypicalColumn("Name", new BasicAttributeManipulator(MMXCorePackage.eINSTANCE.getNamedObject_Name(), jointModelEditorPart.getEditingDomain()));
+		addNameManipulator("Name");
 	
 		for (final PortCapability capability : PortCapability.values()) {
 			addTypicalColumn(capability.getName(), new CapabilityManipulator(capability, jointModelEditorPart.getEditingDomain()));
 		}
-		setTitle("Ports", PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_DEF_VIEW));
+		
+		defaultSetTitle("Ports");
 	}
 }
