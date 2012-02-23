8594f99bf85d4593a3844a49f6caa579b1a36e7f
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-23 15:20:59 +0000

    add cargo model importer

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/importer/CargoImporter.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.models.lng.cargo.importer;
+
+import org.eclipse.emf.ecore.EReference;
+
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.util.importer.impl.DefaultClassImporter;
+
+public class CargoImporter extends DefaultClassImporter {
+	@Override
+	protected boolean shouldFlattenReference(EReference reference) {
+		return super.shouldFlattenReference(reference) || reference == CargoPackage.eINSTANCE.getCargo_LoadSlot() || reference == CargoPackage.eINSTANCE.getCargo_DischargeSlot();
+	}
+}
