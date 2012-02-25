652a5353cd1c0a075242b824472d4784a99570d3
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-25 17:28:12 +0000

    Add import warning page for wizard, index editor, and update the pricing mdoel contribution to display the index page.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/editorpart/IndexEditorPane.java
@@ -0,0 +1,121 @@
+package com.mmxlabs.models.lng.pricing.ui.editorpart;
+
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPage;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.models.lng.pricing.DataIndex;
+import com.mmxlabs.models.lng.pricing.DerivedIndex;
+import com.mmxlabs.models.lng.pricing.PricingPackage;
+import com.mmxlabs.models.lng.types.ui.tabular.ScenarioTableViewerPane;
+import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
+import com.mmxlabs.models.ui.tabular.BasicAttributeManipulator;
+import com.mmxlabs.models.ui.tabular.DialogFeatureManipulator;
+import com.mmxlabs.models.ui.tabular.ICellManipulator;
+import com.mmxlabs.models.ui.tabular.ICellRenderer;
+import com.mmxlabs.models.ui.tabular.NonEditableColumn;
+
+public class IndexEditorPane extends ScenarioTableViewerPane {
+	public IndexEditorPane(final IWorkbenchPage page, final JointModelEditorPart part) {
+		super(page, part);
+	}
+
+	@Override
+	public void init(List<EReference> path, AdapterFactory adapterFactory) {
+		super.init(path, adapterFactory);
+		
+		addTypicalColumn("Type", new NonEditableColumn() {
+			@Override
+			public String render(Object object) {
+				if (object instanceof DerivedIndex) {
+					return "Expression";
+				} else {
+					return "Data";
+				}
+			}
+		});
+		addNameManipulator("Name");
+		
+		addTypicalColumn("Content", new IndexValueManipulator());
+		
+		defaultSetTitle("Indices");
+	}
+	
+	private class IndexValueManipulator implements ICellRenderer, ICellManipulator {
+		private BasicAttributeManipulator expressionManipulator = 
+				new BasicAttributeManipulator(
+						PricingPackage.eINSTANCE.getDerivedIndex_Expression(),
+						getEditingDomain());
+		
+		private BasicAttributeManipulator dataManipulator = 
+				new DialogFeatureManipulator(PricingPackage.eINSTANCE.getDataIndex_Points(), getEditingDomain()) {
+					
+					@Override
+					protected String renderValue(Object value) {
+						if (value == null) return "";
+						return ((DataIndex) value).getPoints().size() + " points";
+					}
+					
+					@Override
+					protected Object openDialogBox(Control cellEditorWindow, Object object) {
+						return null;
+					}
+				};
+		
+		private BasicAttributeManipulator pick(final Object object) {
+			if (object instanceof DataIndex) return dataManipulator;
+			else return expressionManipulator;
+		}
+		
+		@Override
+		public void setValue(Object object, Object value) {
+			pick(object).setValue(object, value);
+		}
+
+		@Override
+		public CellEditor getCellEditor(Composite parent, Object object) {
+			return pick(object).getCellEditor(parent, object);
+		}
+
+		@Override
+		public Object getValue(Object object) {
+			return pick(object).getValue(object);
+		}
+
+		@Override
+		public boolean canEdit(Object object) {
+			return pick(object).canEdit(object);
+		}
+
+		@Override
+		public String render(Object object) {
+			return pick(object).render(object);
+		}
+
+		@Override
+		public Comparable getComparable(Object object) {
+			return pick(object).getComparable(object);
+		}
+
+		@Override
+		public Object getFilterValue(Object object) {
+			return pick(object).getFilterValue(object);
+		}
+
+		@Override
+		public Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(
+				Object object) {
+			return pick(object).getExternalNotifiers(object);
+		}
+		
+	}
+}
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/editorpart/PricingModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/editorpart/PricingModelEditorContribution.java
@@ -1,23 +1,49 @@
 package com.mmxlabs.models.lng.pricing.ui.editorpart;
 
+import java.util.Collections;
+
 import org.eclipse.swt.widgets.Composite;
 
+import com.mmxlabs.models.lng.pricing.PricingPackage;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 import com.mmxlabs.models.ui.editorpart.IJointModelEditorContribution;
 import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
 
 public class PricingModelEditorContribution implements IJointModelEditorContribution {
+	private UUIDObject modelObject;
+	private JointModelEditorPart editorPart;
+
 	@Override
 	public void init(JointModelEditorPart editorPart, MMXRootObject rootObject,
 			UUIDObject modelObject) {
-		
+		this.editorPart = editorPart;
+		this.modelObject = modelObject;
 	}
 
 	@Override
 	public void addPages(final Composite parent) {
-		// pricing model editor; complicated!
-		// ideas on a postcard.
+		addIndexPage(parent);
+		addMarketPage(parent);
+		addPricingPage(parent);
+	}
+
+	private void addPricingPage(final Composite parent) {
+		
+	}
+
+	private void addMarketPage(final Composite parent) {
+		
+	}
+
+	private void addIndexPage(final Composite parent) {
+		final IndexEditorPane pane = new IndexEditorPane(editorPart.getSite().getPage(), editorPart);
+		pane.createControl(parent);
+		pane.init(Collections.singletonList(PricingPackage.eINSTANCE.getPricingModel_CommodityIndices()), editorPart.getAdapterFactory());
+		pane.getViewer().setInput(modelObject);
+		
+		int page = editorPart.addPage(pane.getControl());
+		editorPart.setPageText(page, "Indices");
 	}
 
 }
