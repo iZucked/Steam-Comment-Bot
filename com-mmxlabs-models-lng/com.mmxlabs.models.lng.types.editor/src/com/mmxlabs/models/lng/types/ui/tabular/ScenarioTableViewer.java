c16e29516ce3501ff866578620420b03362b4349
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-02-25 17:17:43 +0000

    keep hold of useful things for subclasses

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.types.editor/src/com/mmxlabs/models/lng/types/ui/tabular/ScenarioTableViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.types.editor/src/com/mmxlabs/models/lng/types/ui/tabular/ScenarioTableViewer.java
@@ -2,10 +2,20 @@ package com.mmxlabs.models.lng.types.ui.tabular;
 
 import org.eclipse.swt.widgets.Composite;
 
+import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
 import com.mmxlabs.models.ui.tabular.EObjectTableViewer;
 
 public class ScenarioTableViewer extends EObjectTableViewer  {
-	public ScenarioTableViewer(Composite parent, int style) {
+	private JointModelEditorPart editorPart;
+
+	public ScenarioTableViewer(final Composite parent, final int style, final JointModelEditorPart editorPart) {
 		super(parent, style);
+		this.editorPart = editorPart;
+	}
+
+	@Override
+	public void refresh() {
+		if (editorPart.isSaving()) return;
+		super.refresh();
 	}
 }
