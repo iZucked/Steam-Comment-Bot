09479fddb588473c29002803184544c62c15881b
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-02-28 09:45:21 +0000

    Remove unused imports

--- com-mmxlabs-models-mmxcore/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/FieldMap.java
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/FieldMap.java
@@ -3,7 +3,6 @@ package com.mmxlabs.models.util.importer;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
