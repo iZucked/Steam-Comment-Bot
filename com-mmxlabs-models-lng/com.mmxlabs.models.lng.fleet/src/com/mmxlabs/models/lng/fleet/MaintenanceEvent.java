a4c3e566371d264f67b0c6f238428abfc4cc32df
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-02-28 16:27:05 +0000

    Add a MaintenanceEvent

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet/src/com/mmxlabs/models/lng/fleet/MaintenanceEvent.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.mmxlabs.models.lng.fleet;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Maintenance Event</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see com.mmxlabs.models.lng.fleet.FleetPackage#getMaintenanceEvent()
+ * @model
+ * @generated
+ */
+public interface MaintenanceEvent extends VesselEvent {
+} // end of  MaintenanceEvent
+
+// finish type fixing
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet/src/com/mmxlabs/models/lng/fleet/impl/MaintenanceEventImpl.java
@@ -0,0 +1,46 @@
+
+
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.mmxlabs.models.lng.fleet.impl;
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.MaintenanceEvent;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Maintenance Event</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class MaintenanceEventImpl extends VesselEventImpl implements MaintenanceEvent {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected MaintenanceEventImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return FleetPackage.Literals.MAINTENANCE_EVENT;
+	}
+
+} // end of MaintenanceEventImpl
+
+// finish type fixing
