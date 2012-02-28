cbe18a0b0769d8d878a4661fcc71532c13cade18
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-02-28 16:45:39 +0000

    Fix up for API changes

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/ResourcelessModelEntityMap.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/ResourcelessModelEntityMap.java
@@ -9,38 +9,39 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 
-import scenario.Scenario;
+import com.mmxlabs.models.lng.transformer.EMFUtils;
+import com.mmxlabs.models.lng.transformer.ModelEntityMap;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 
 /**
  * A model entity map which doesn't use the resourceset.
+ * 
  * @author Tom Hinton
  * 
  */
 public class ResourcelessModelEntityMap extends ModelEntityMap {
 	private final Map<Object, Object> modelToOptimiser = new HashMap<Object, Object>();
 	private final Map<Object, Object> optimiserToModel = new HashMap<Object, Object>();
-	
-	
+
 	@Override
-	public <U> U getModelObject(Object internalObject, Class<? extends U> clz) {
+	public <U> U getModelObject(final Object internalObject, final Class<? extends U> clz) {
 		return clz.cast(optimiserToModel.get(internalObject));
 	}
 
 	@Override
-	public void addModelObject(EObject modelObject, Object internalObject) {
+	public void addModelObject(final EObject modelObject, final Object internalObject) {
 		modelToOptimiser.put(modelObject, internalObject);
 		optimiserToModel.put(internalObject, modelObject);
 	}
 
 	@Override
-	public <T> T getOptimiserObject(EObject modelObject, Class<? extends T> clz) {
+	public <T> T getOptimiserObject(final EObject modelObject, final Class<? extends T> clz) {
 		return clz.cast(modelToOptimiser.get(modelObject));
 	}
 
 	@Override
-	public void setScenario(Scenario scenario) {
-		earliestDate = EMFUtils.findEarliestAndLatestEvents(scenario)
-				.getFirst();
+	public void setScenario(final MMXRootObject scenario) {
+		earliestDate = EMFUtils.findEarliestAndLatestEvents(scenario).getFirst();
 	}
 
 	@Override
