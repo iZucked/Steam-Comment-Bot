d962964970bc058b21ba014d804177bdf6d1773b
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-03-02 10:52:08 +0000

    Various project/package renames

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.app/src/com/mmxlabs/demo/app/intro/EditingPerspective.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.demo.app.intro;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class EditingPerspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(final IPageLayout layout) {
+
+		final IFolderLayout navFolder = layout.createFolder("navFolder", IPageLayout.LEFT, 0.2f, IPageLayout.ID_EDITOR_AREA);
+		navFolder.addView("com.mmxlabs.rcp.navigator");
+
+		layout.addShowViewShortcut("com.mmxlabs.rcp.navigator");
+
+		layout.addShowViewShortcut("org.eclipse.pde.runtime.LogView");
+
+		layout.addPerspectiveShortcut("com.mmxlabs.demo.app.perspective.optimisation");
+	}
+}
