b439959d9662ebd4a8f7197f4991b581926bb255
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-03-02 14:48:15 +0000

    move demo to be manifest instead

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/models/lng/demo/importWizards/ImportCSVWizard.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.demo.importWizards;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class ImportCSVWizard extends Wizard implements IImportWizard {
+	
+	ImportCSVWizardPage mainPage;
+	private ImportCSVFilesPage filesPage;
+
+	public ImportCSVWizard() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	@Override
+	public boolean performFinish() {
+		IFile file = mainPage.createNewFile();
+        if (file == null)
+            return false;
+        return true;
+	}
+	 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		setWindowTitle("File Import Wizard"); //NON-NLS-1
+		setNeedsProgressMonitor(true);
+		filesPage = new ImportCSVFilesPage("CSV Files");
+		mainPage = new ImportCSVWizardPage("Import CSV",selection, filesPage); //NON-NLS-1
+	}
+	
+	/* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.IWizard#addPages()
+     */
+    @Override
+	public void addPages() {
+        super.addPages(); 
+        addPage(filesPage);
+        addPage(mainPage);
+    }
+}
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/models/lng/demo/manifest/Entry.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2011
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.demo.manifest;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Entry</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link com.mmxlabs.models.lng.demo.manifest.Entry#getSubModelKey <em>Sub Model Key</em>}</li>
+ *   <li>{@link com.mmxlabs.models.lng.demo.manifest.Entry#getRelativePath <em>Relative Path</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see com.mmxlabs.models.lng.demo.manifest.ManifestPackage#getEntry()
+ * @model
+ * @generated
+ */
+public interface Entry extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Sub Model Key</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Sub Model Key</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Sub Model Key</em>' attribute.
+	 * @see #setSubModelKey(String)
+	 * @see com.mmxlabs.models.lng.demo.manifest.ManifestPackage#getEntry_SubModelKey()
+	 * @model required="true"
+	 * @generated
+	 */
+	String getSubModelKey();
+
+	/**
+	 * Sets the value of the '{@link com.mmxlabs.models.lng.demo.manifest.Entry#getSubModelKey <em>Sub Model Key</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Sub Model Key</em>' attribute.
+	 * @see #getSubModelKey()
+	 * @generated
+	 */
+	void setSubModelKey(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Relative Path</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Relative Path</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Relative Path</em>' attribute.
+	 * @see #setRelativePath(String)
+	 * @see com.mmxlabs.models.lng.demo.manifest.ManifestPackage#getEntry_RelativePath()
+	 * @model required="true"
+	 * @generated
+	 */
+	String getRelativePath();
+
+	/**
+	 * Sets the value of the '{@link com.mmxlabs.models.lng.demo.manifest.Entry#getRelativePath <em>Relative Path</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Relative Path</em>' attribute.
+	 * @see #getRelativePath()
+	 * @generated
+	 */
+	void setRelativePath(String value);
+
+} // Entry
