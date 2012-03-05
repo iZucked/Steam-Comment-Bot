de106ffaa088e5ce40d342f5b58379d5c3016348
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-03-05 14:01:04 +0000

    Factor out fuel using methods

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/events/IFuelUsingEvent.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.events;
+
+import com.mmxlabs.scheduler.optimiser.voyage.FuelComponent;
+import com.mmxlabs.scheduler.optimiser.voyage.FuelUnit;
+
+/**
+ * A common interface for events that use fuel to implement
+ * 
+ * @author hinton
+ *
+ */
+public interface IFuelUsingEvent {
+	/**
+	 * Returns the fuel consumption per {@link FuelComponent} during this event.
+	 * 
+	 * @param fuel
+	 * @return
+	 */
+	long getFuelConsumption(FuelComponent fuel, FuelUnit unit);
+
+	/**
+	 * Returns the total cost of the fuel consumed per {@link FuelComponent} during this event. TODO: What about making this the unit cost?
+	 * 
+	 * @param fuel
+	 * @return
+	 */
+	long getFuelCost(FuelComponent fuel);
+}
--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/events/IIdleEvent.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/events/IIdleEvent.java
@@ -6,8 +6,6 @@ package com.mmxlabs.scheduler.optimiser.events;
 
 import com.mmxlabs.scheduler.optimiser.components.IPort;
 import com.mmxlabs.scheduler.optimiser.components.VesselState;
-import com.mmxlabs.scheduler.optimiser.voyage.FuelComponent;
-import com.mmxlabs.scheduler.optimiser.voyage.FuelUnit;
 
 /**
  * {@link IScheduledEvent} defining a period of idle time at a {@link IPort}
@@ -15,7 +13,7 @@ import com.mmxlabs.scheduler.optimiser.voyage.FuelUnit;
  * l@author Simon Goodall
  * 
  */
-public interface IIdleEvent extends IScheduledEvent {
+public interface IIdleEvent extends IScheduledEvent, IFuelUsingEvent {
 
 	/**
 	 * Returns the {@link IPort} at which this idle event occurs.
@@ -30,20 +28,4 @@ public interface IIdleEvent extends IScheduledEvent {
 	 * @return
 	 */
 	VesselState getVesselState();
-
-	/**
-	 * Returns the fuel consumption per {@link FuelComponent} during this event.
-	 * 
-	 * @param fuel
-	 * @return
-	 */
-	long getFuelConsumption(FuelComponent fuel, FuelUnit unit);
-
-	/**
-	 * Returns the total cost of the fuel consumed per {@link FuelComponent} during this event. TODO: What about making this the unit cost?
-	 * 
-	 * @param fuel
-	 * @return
-	 */
-	long getFuelCost(FuelComponent fuel);
 }
