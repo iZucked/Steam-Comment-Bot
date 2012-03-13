a36b17891487fdc2fa89b3706f3c9c9c39ed3a19
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-03-13 14:54:21 +0000

    Correct bundle ids

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.navigator/src/com/mmxlabs/rcp/navigator/Activator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.navigator/src/com/mmxlabs/rcp/navigator/Activator.java
@@ -19,7 +19,7 @@ import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.mmxlabs.rcp.navigator"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.mmxlabs.shiplingo.platform.navigator"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/scheduleview/Activator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/scheduleview/Activator.java
@@ -19,7 +19,7 @@ import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.mmxlabs.scheduleview"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.mmxlabs.shiplingo.platform.scheduleview"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
