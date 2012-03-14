b07d71457b995dbc283cfff72e2963f9cc95094c
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-03-14 17:54:48 +0000

    picker for shipping entity

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/editorpart/EntityEditorPane.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/editorpart/EntityEditorPane.java
@@ -4,14 +4,24 @@
  */
 package com.mmxlabs.models.lng.commercial.ui.editorpart;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPage;
 
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.models.lng.commercial.CommercialModel;
+import com.mmxlabs.models.lng.commercial.LegalEntity;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
+import com.mmxlabs.models.ui.tabular.ICellManipulator;
+import com.mmxlabs.models.ui.tabular.ICellRenderer;
 
 public class EntityEditorPane extends ScenarioTableViewerPane {
 	public EntityEditorPane(IWorkbenchPage page, JointModelEditorPart part) {
@@ -22,10 +32,62 @@ public class EntityEditorPane extends ScenarioTableViewerPane {
 	public void init(List<EReference> path, AdapterFactory adapterFactory) {
 		super.init(path, adapterFactory);
 		addNameManipulator("Name");
-		// TODO add shipping entity picker
+		
+		addTypicalColumn("Is Shipping", new ShippingEntityManipulator());
 		
 		defaultSetTitle("Entities");
 	}
 	
-	
+	class ShippingEntityManipulator implements ICellRenderer, ICellManipulator {
+		@Override
+		public void setValue(Object object, Object value) {
+			if (object instanceof LegalEntity) {
+				final LegalEntity settings = (LegalEntity) object;
+				if (settings.eContainer() instanceof CommercialModel) {
+					final CommercialModel cm = (CommercialModel) settings.eContainer();
+					cm.setShippingEntity(settings);
+				}
+			}
+		}
+
+		@Override
+		public CellEditor getCellEditor(Composite parent, Object object) {
+			return new ComboBoxCellEditor(parent, new String[] { "Y", "N" });
+		}
+
+		@Override
+		public Object getValue(Object object) {
+			if (object instanceof LegalEntity) {
+				final LegalEntity settings = (LegalEntity) object;
+				if (((CommercialModel) settings.eContainer()).getShippingEntity() == settings)
+					return 0;
+			}
+			return 1;
+		}
+
+		@Override
+		public boolean canEdit(Object object) {
+			return true;
+		}
+
+		@Override
+		public String render(Object object) {
+			return ((Integer) getValue(object)) == 0 ? "Y" : "N";
+		}
+
+		@Override
+		public Comparable getComparable(Object object) {
+			return render(object);
+		}
+
+		@Override
+		public Object getFilterValue(Object object) {
+			return render(object);
+		}
+
+		@Override
+		public Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(Object object) {
+			return Collections.emptySet();
+		}
+	}
 }
