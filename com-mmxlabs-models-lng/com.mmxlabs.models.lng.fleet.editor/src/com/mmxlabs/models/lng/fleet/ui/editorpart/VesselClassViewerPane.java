7c7e870d519eeedc4c446fc8b57de24bbef4dd43
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-03-14 12:40:23 +0000

    Add base fuel editor menu to VC pane toolbar

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/ui/editorpart/VesselClassViewerPane.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/ui/editorpart/VesselClassViewerPane.java
@@ -13,23 +13,34 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.edit.command.AddCommand;
+import org.eclipse.emf.edit.command.DeleteCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 
+import com.mmxlabs.models.lng.fleet.BaseFuel;
+import com.mmxlabs.models.lng.fleet.FleetFactory;
+import com.mmxlabs.models.lng.fleet.FleetModel;
 import com.mmxlabs.models.lng.fleet.FleetPackage;
 import com.mmxlabs.models.lng.fleet.VesselStateAttributes;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
+import com.mmxlabs.models.ui.editors.ICommandHandler;
 import com.mmxlabs.models.ui.editors.dialogs.DetailCompositeDialog;
 import com.mmxlabs.models.ui.tabular.BasicAttributeManipulator;
 import com.mmxlabs.models.ui.tabular.DialogFeatureManipulator;
 import com.mmxlabs.models.ui.tabular.MultipleReferenceManipulator;
 import com.mmxlabs.models.ui.tabular.NumericAttributeManipulator;
+import com.mmxlabs.rcp.common.actions.AbstractMenuAction;
 
 public class VesselClassViewerPane extends ScenarioTableViewerPane {
 	private JointModelEditorPart jointModelEditor;
@@ -43,6 +54,10 @@ public class VesselClassViewerPane extends ScenarioTableViewerPane {
 	public void init(List<EReference> path, AdapterFactory adapterFactory) {
 		super.init(path, adapterFactory);
 		final EditingDomain editingDomain = jointModelEditor.getEditingDomain();
+		
+		getToolBarManager().appendToGroup("edit", new BaseFuelEditorAction());
+		getToolBarManager().update(true);
+		
 		addTypicalColumn("Name", new BasicAttributeManipulator(
 				MMXCorePackage.eINSTANCE.getNamedObject_Name(), editingDomain));
 
@@ -71,6 +86,65 @@ public class VesselClassViewerPane extends ScenarioTableViewerPane {
 				.getImage(ISharedImages.IMG_DEF_VIEW));
 	}
 	
+	class BaseFuelEditorAction extends AbstractMenuAction {
+		public BaseFuelEditorAction() {
+			super("Base Fuels");
+		}
+		
+		@Override
+		protected void populate(final Menu menu) {
+			final FleetModel fleetModel = jointModelEditor.getRootObject().getSubModel(FleetModel.class);
+			boolean b = false;
+			for (final BaseFuel baseFuel : fleetModel.getBaseFuels()) {
+				b = true;
+				final Action editBase = new AbstractMenuAction(baseFuel.getName()) {					
+					@Override
+					protected void populate(Menu submenu) {
+						final Action edit = new Action("Edit...") {
+							public void run() {
+								final DetailCompositeDialog dcd = new DetailCompositeDialog(
+										jointModelEditor.getSite().getShell(), 
+										jointModelEditor.getDefaultCommandHandler());
+								dcd.open(jointModelEditor.getRootObject(), Collections.singletonList((EObject) baseFuel));
+							}
+						};
+						addActionToMenu(edit, submenu);
+						
+						final Action delete = new Action("Delete...") {
+							public void run() {
+								final ICommandHandler handler = jointModelEditor.getDefaultCommandHandler();
+								handler.handleCommand(DeleteCommand.create(handler.getEditingDomain(), Collections.singleton(baseFuel)), fleetModel, FleetPackage.eINSTANCE.getFleetModel_BaseFuels());
+							}
+						};
+						addActionToMenu(delete, submenu);
+					}
+				};
+				addActionToMenu(editBase, menu);
+			}
+			
+			if (b) {
+				new MenuItem(menu, SWT.SEPARATOR);
+			}
+			final Action newBase = new Action("Add base fuel...") {
+				@Override
+				public void run() {
+					final BaseFuel newFuel = FleetFactory.eINSTANCE.createBaseFuel();
+					final DetailCompositeDialog dcd = new DetailCompositeDialog(
+							jointModelEditor.getSite().getShell(), 
+							jointModelEditor.getDefaultCommandHandler());
+					if (dcd.open(jointModelEditor.getRootObject(), Collections.singletonList((EObject) newFuel)) == Window.OK) {
+						final ICommandHandler handler = jointModelEditor.getDefaultCommandHandler();
+						handler.handleCommand(
+								AddCommand.create(handler.getEditingDomain(), fleetModel,  FleetPackage.eINSTANCE.getFleetModel_BaseFuels(),
+										newFuel),
+										fleetModel, FleetPackage.eINSTANCE.getFleetModel_BaseFuels());
+					}
+				}
+			};
+			addActionToMenu(newBase, menu);
+		}
+	}
+	
 	class VSAManipulator extends DialogFeatureManipulator {
 
 		public VSAManipulator(EStructuralFeature field,
@@ -87,11 +161,6 @@ public class VesselClassViewerPane extends ScenarioTableViewerPane {
 			} else {
 				return null;
 			}
-			
-			
-//			final VesselStateAttributesDialog dlg = new VesselStateAttributesDialog(cellEditorWindow.getShell(), (SWT.DIALOG_TRIM & ~SWT.CLOSE) | SWT.APPLICATION_MODAL);
-
-//			return dlg.open((VesselStateAttributes) getValue(object), "Ballast");
 		}
 
 		@Override
