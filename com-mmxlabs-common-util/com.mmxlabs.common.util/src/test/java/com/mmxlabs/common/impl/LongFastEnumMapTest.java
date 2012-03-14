332abece5dae7e90478e58b70a04e7dcd87600cd
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-03-14 08:32:20 +0000

    Add test case for putAll

--- com-mmxlabs-common-util/com.mmxlabs.common.util/src/test/java/com/mmxlabs/common/impl/LongFastEnumMapTest.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/test/java/com/mmxlabs/common/impl/LongFastEnumMapTest.java
@@ -55,6 +55,23 @@ public class LongFastEnumMapTest {
 	}
 
 	@Test
+	public void testGetPutAll() {
+		final LongFastEnumMap<Enum1> map = new LongFastEnumMap<LongFastEnumMapTest.Enum1>(Enum1.values().length);
+		final LongFastEnumMap<Enum1> map2 = new LongFastEnumMap<LongFastEnumMapTest.Enum1>(Enum1.values().length);
+
+		map.put(Enum1.Enum1_A, 5l);
+		map2.put(Enum1.Enum1_B, 10l);
+
+		Assert.assertEquals(0l, map2.get(Enum1.Enum1_A));
+		Assert.assertEquals(10l, map2.get(Enum1.Enum1_B));
+
+		map2.putAll(map);
+
+		Assert.assertEquals(5l, map2.get(Enum1.Enum1_A));
+		Assert.assertEquals(0l, map2.get(Enum1.Enum1_B));
+	}
+
+	@Test
 	public void testEqualsObject() {
 		final LongFastEnumMap<Enum1> map1 = new LongFastEnumMap<LongFastEnumMapTest.Enum1>(1);
 
