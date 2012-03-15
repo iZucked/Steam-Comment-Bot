18958163bd48c293b45dc28d0a4e57324ad6e47e
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-03-15 15:55:48 +0000

    Special import behaviour for cargos

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
@@ -4,18 +4,28 @@
  */
 package com.mmxlabs.models.lng.cargo.ui.editorpart;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.action.Action;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 
+import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.ui.actions.RewireAction;
 import com.mmxlabs.models.lng.cargo.ui.actions.RotateSlotsAction;
+import com.mmxlabs.models.lng.ui.actions.SimpleImportAction;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.ui.dates.DateAttributeManipulator;
@@ -73,4 +83,29 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 		
 		setTitle("Cargos", PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_DEF_VIEW));
 	}
+
+	@Override
+	protected Action createImportAction() {
+		return new SimpleImportAction(part, getScenarioViewer()) {
+			@Override
+			protected Command mergeImports(EObject container, EReference containment, Collection<EObject> imports) {
+				final List<EObject> cargos = new ArrayList<EObject>();
+				final List<EObject> loads = new ArrayList<EObject>();
+				final List<EObject> discharges = new ArrayList<EObject>();
+				
+				for (final EObject o : imports) {
+					if (o instanceof Cargo) cargos.add(o);
+					else if (o instanceof LoadSlot) loads.add(o);
+					else if (o instanceof DischargeSlot) discharges.add(o);
+				}
+				
+				final CompoundCommand mergeAll = new CompoundCommand();
+				mergeAll.append(super.mergeImports(container, containment, cargos));
+				mergeAll.append(super.mergeImports(container, CargoPackage.eINSTANCE.getCargoModel_LoadSlots(), loads));
+				mergeAll.append(super.mergeImports(container, CargoPackage.eINSTANCE.getCargoModel_DischargeSlots(), discharges));
+				return mergeAll;
+			}
+		};
+	}
+	
 }
