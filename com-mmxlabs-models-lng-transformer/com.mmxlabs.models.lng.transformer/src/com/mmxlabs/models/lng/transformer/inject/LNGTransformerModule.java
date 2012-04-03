890985bca539bd0c6bafd1450a626bd94cc93f16
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-03 14:26:59 -0100

    Bind registries from osgi service - if platform is running. Another module will need to provide these objects outside of an osgi environment.

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/inject/LNGTransformerModule.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/inject/LNGTransformerModule.java
@@ -4,13 +4,20 @@
  */
 package com.mmxlabs.models.lng.transformer.inject;
 
+import static org.ops4j.peaberry.Peaberry.service;
+
 import javax.inject.Singleton;
+
+import org.eclipse.core.runtime.Platform;
 
 import com.google.inject.AbstractModule;
 import com.mmxlabs.models.lng.transformer.LNGScenarioTransformer;
 import com.mmxlabs.models.lng.transformer.ModelEntityMap;
 import com.mmxlabs.models.lng.transformer.ResourcelessModelEntityMap;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.optimiser.core.constraints.IConstraintCheckerRegistry;
+import com.mmxlabs.optimiser.core.evaluation.IEvaluationProcessRegistry;
+import com.mmxlabs.optimiser.core.fitness.IFitnessFunctionRegistry;
 
 /**
  * Main entry point to create {@link LNGScenarioTransformer}. This uses injection to populate the data structures.
@@ -32,5 +39,11 @@ public class LNGTransformerModule extends AbstractModule {
 		bind(LNGScenarioTransformer.class).in(Singleton.class);
 
 		bind(ModelEntityMap.class).to(ResourcelessModelEntityMap.class).in(Singleton.class);
+		
+		if (Platform.isRunning()) {
+			bind(IFitnessFunctionRegistry.class).toProvider(service(IFitnessFunctionRegistry.class).single());
+			bind(IConstraintCheckerRegistry.class).toProvider(service(IConstraintCheckerRegistry.class).single());
+			bind(IEvaluationProcessRegistry.class).toProvider(service(IEvaluationProcessRegistry.class).single());
+		}
 	}
 }
