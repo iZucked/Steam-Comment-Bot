f7a32c8e4670d84446e7ea6e9c4b71804f7fecc2
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-04-03 07:40:47 -0100

    Notify subclasses when they have missed a notification

--- com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXAdapterImpl.java
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXAdapterImpl.java
@@ -6,21 +6,32 @@ import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import com.mmxlabs.models.mmxcore.IMMXAdapter;
 
 public abstract class MMXAdapterImpl extends AdapterImpl implements IMMXAdapter {
+	private boolean missed = false;
 	@Override
 	public void notifyChanged(final Notification notification) {
 		if (enabled) reallyNotifyChanged(notification);
+		else missed = true;
 	}
 	
 	public abstract void reallyNotifyChanged(final Notification notification);
-
+	
+	protected void missedNotification() {
+		
+	}
+	
 	boolean enabled = true;
 	@Override
 	public void disable() {
+		missed = false;
 		enabled = false;
 	}
 
 	@Override
 	public void enable() {
+		if (missed) {
+			missedNotification();
+		}
+		missed = false;
 		enabled = true;
 	}
 }
--- com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXContentAdapter.java
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXContentAdapter.java
@@ -6,22 +6,33 @@ import org.eclipse.emf.ecore.util.EContentAdapter;
 import com.mmxlabs.models.mmxcore.IMMXAdapter;
 
 public abstract class MMXContentAdapter extends EContentAdapter implements IMMXAdapter {
+	private boolean missed = false;
 	@Override
 	public void notifyChanged(Notification notification) {
 		super.notifyChanged(notification);
 		if (enabled) reallyNotifyChanged(notification);
+		else missed = true;
 	}
 	
 	public abstract void reallyNotifyChanged(final Notification notification);
 
+	protected void missedNotification() {
+		
+	}
+	
 	boolean enabled = true;
 	@Override
 	public void disable() {
 		enabled = false;
+		missed = false;
 	}
 
 	@Override
 	public void enable() {
+		if (missed) {
+			missedNotification();
+		}
+		missed = false;
 		enabled = true;
 	}
 
