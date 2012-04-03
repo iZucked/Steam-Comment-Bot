2c653c445340fea399154dea5bc1b8cd16f5ff74
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-03 09:24:12 -0100

    Bind fitness and constraint registries with a set of factories if the platform is not running. If it is, they will be registered via a osgi service instead.

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.its/src/com/mmxlabs/shiplingo/platform/its/tests/ContractExtensionTestModule.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.its/src/com/mmxlabs/shiplingo/platform/its/tests/ContractExtensionTestModule.java
@@ -2,30 +2,85 @@ package com.mmxlabs.shiplingo.platform.its.tests;
 
 import java.util.Collections;
 
+import org.eclipse.core.runtime.Platform;
+import org.ops4j.peaberry.ServiceRegistry;
+import org.ops4j.peaberry.eclipse.EclipseRegistry;
 import org.ops4j.peaberry.util.TypeLiterals;
 
 import com.google.inject.AbstractModule;
+import com.mmxlabs.models.lng.transformer.OptimisationTransformer;
 import com.mmxlabs.models.lng.transformer.contracts.SimpleContractTransformer;
 import com.mmxlabs.models.lng.transformer.inject.extensions.ContractTransformer;
 import com.mmxlabs.models.lng.transformer.inject.extensions.ContractTransformerWrapper;
+import com.mmxlabs.optimiser.common.constraints.OrderedSequenceElementsConstraintCheckerFactory;
+import com.mmxlabs.optimiser.common.constraints.ResourceAllocationConstraintCheckerFactory;
+import com.mmxlabs.optimiser.core.constraints.IConstraintCheckerRegistry;
+import com.mmxlabs.optimiser.core.constraints.impl.ConstraintCheckerRegistry;
+import com.mmxlabs.optimiser.core.fitness.IFitnessFunctionRegistry;
+import com.mmxlabs.optimiser.core.fitness.impl.FitnessFunctionRegistry;
+import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
+import com.mmxlabs.scheduler.optimiser.constraints.impl.PortExclusionConstraintCheckerFactory;
+import com.mmxlabs.scheduler.optimiser.constraints.impl.PortTypeConstraintCheckerFactory;
+import com.mmxlabs.scheduler.optimiser.constraints.impl.TravelTimeConstraintCheckerFactory;
+import com.mmxlabs.scheduler.optimiser.fitness.CargoSchedulerFitnessCoreFactory;
 
 public class ContractExtensionTestModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+
+		bind(ServiceRegistry.class).to(EclipseRegistry.class);
+		
+		if (!Platform.isRunning()) {
+			bind(IFitnessFunctionRegistry.class).toInstance(createFitnessFunctionRegistry());
+			bind(IConstraintCheckerRegistry.class).toInstance(createConstraintCheckerRegistry());
+		}
+
 		final SimpleContractTransformer sct = new SimpleContractTransformer();
 
 		final ContractTransformer transformer = new ContractTransformerWrapper(sct, sct.getContractEClasses());
-//		bind(ContractTransformer.class).toInstance(transformer);
 		bind(TypeLiterals.iterable(ContractTransformer.class)).toInstance(Collections.singleton(transformer));
-//		bind(TypeLiterals.iterable(ContractTransformer.class)).to(service(transformer)());
 	}
-	
-//	@Provides
-//	public Iterable<ContractTransformer> getTT() {
-//		final SimpleContractTransformer sct = new SimpleContractTransformer();
-//	
-//		return Collections.<ContractTransformer>singleton(new ContractTransformerWrapper(sct, sct.getContractEClasses()));
-//	}
 
+	/**
+	 * Creates a fitness function registry used by this {@link OptimisationTransformer} instance.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public IFitnessFunctionRegistry createFitnessFunctionRegistry() {
+		FitnessFunctionRegistry fitnessFunctionRegistry = new FitnessFunctionRegistry();
+
+		final CargoSchedulerFitnessCoreFactory factory = new CargoSchedulerFitnessCoreFactory();
+
+		fitnessFunctionRegistry.registerFitnessCoreFactory(factory);
+		return fitnessFunctionRegistry;
+	}
+
+	/**
+	 * Creates a constraint checker registry used by this {@link OptimisationTransformer} instance.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public IConstraintCheckerRegistry createConstraintCheckerRegistry() {
+		ConstraintCheckerRegistry constraintCheckerRegistry = new ConstraintCheckerRegistry();
+		{
+			final OrderedSequenceElementsConstraintCheckerFactory constraintFactory = new OrderedSequenceElementsConstraintCheckerFactory(SchedulerConstants.DCP_orderedElementsProvider);
+			constraintCheckerRegistry.registerConstraintCheckerFactory(constraintFactory);
+		}
+		{
+			final ResourceAllocationConstraintCheckerFactory constraintFactory = new ResourceAllocationConstraintCheckerFactory(SchedulerConstants.DCP_resourceAllocationProvider);
+			constraintCheckerRegistry.registerConstraintCheckerFactory(constraintFactory);
+		}
+
+		constraintCheckerRegistry.registerConstraintCheckerFactory(new PortTypeConstraintCheckerFactory(SchedulerConstants.DCP_portTypeProvider, SchedulerConstants.DCP_vesselProvider));
+
+		constraintCheckerRegistry.registerConstraintCheckerFactory(new TravelTimeConstraintCheckerFactory());
+
+		constraintCheckerRegistry.registerConstraintCheckerFactory(new PortExclusionConstraintCheckerFactory(SchedulerConstants.DCP_portExclusionProvider, SchedulerConstants.DCP_vesselProvider,
+				SchedulerConstants.DCP_portProvider));
+
+		return constraintCheckerRegistry;
+	}
 }
