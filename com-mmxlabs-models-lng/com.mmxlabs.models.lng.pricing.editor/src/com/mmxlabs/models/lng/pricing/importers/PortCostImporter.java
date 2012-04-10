fd32bd6180fe6708e3fb966a132502ef27b13ec0
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-04-10 16:03:32 -0100

    fix importer for port costs a little

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/importers/PortCostImporter.java
@@ -0,0 +1,52 @@
+package com.mmxlabs.models.lng.pricing.importers;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+import com.mmxlabs.models.lng.pricing.PortCost;
+import com.mmxlabs.models.lng.pricing.PortCostEntry;
+import com.mmxlabs.models.lng.pricing.PricingFactory;
+import com.mmxlabs.models.lng.types.PortCapability;
+import com.mmxlabs.models.util.importer.IImportContext;
+import com.mmxlabs.models.util.importer.impl.DefaultClassImporter;
+
+public class PortCostImporter extends DefaultClassImporter {
+	@Override
+	public Collection<EObject> importObject(EClass eClass,
+			Map<String, String> row, IImportContext context) {
+		final PortCost cost = (PortCost) super.importObject(eClass, row, context).iterator().next();
+		
+		for (final PortCapability pc : PortCapability.values()) {
+			final PortCostEntry pce = PricingFactory.eINSTANCE.createPortCostEntry();
+			pce.setActivity(pc);
+
+			if (row.containsKey(pc.name().toLowerCase() + "cost")) {
+				try {
+					pce.setCost(Integer.parseInt(row.get(pc.name().toLowerCase() + "cost")));
+				} catch (NumberFormatException nfe) {
+					
+				}
+			}
+			
+			cost.getEntries().add(pce);
+		}
+		
+		return Collections.singleton((EObject)cost);
+	}
+
+	@Override
+	protected Map<String, String> exportObject(EObject object) {
+		final Map<String, String> result = super.exportObject(object);
+		
+		for (final PortCostEntry pce : ((PortCost) object).getEntries()) {
+			result.put(pce.getActivity().name() + "Cost", Integer.toString(pce.getCost()));
+		}
+		
+		return result;
+	}
+	
+}
