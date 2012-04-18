659e754b7a25e465ec187b81de29b7f0eed77982
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-04-18 14:03:18 -0100

    add editor contribution to set dirty bit on schedule

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule.editor/src/com/mmxlabs/models/lng/schedule/ui/editorpart/ScheduleModelEditorContribution.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.schedule.ui.editorpart;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.widgets.Composite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mmxlabs.models.lng.schedule.ScheduleModel;
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.mmxcore.UUIDObject;
+import com.mmxlabs.models.mmxcore.impl.MMXContentAdapter;
+import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
+import com.mmxlabs.models.ui.editorpart.JointModelEditorPart;
+
+/**
+ * This editor contribution doesn't display anything in the editor. It does add an adapter which updates the
+ * dirty state bit on the schedule model. This may in turn trigger re-evaluation, if live evaluation is on.
+ * 
+ * @author hinton
+ *
+ */
+public class ScheduleModelEditorContribution extends BaseJointModelEditorContribution<ScheduleModel> {
+	private static final Logger log = LoggerFactory.getLogger(ScheduleModelEditorContribution.class);
+	private MMXContentAdapter dirtyStateAdapter = new MMXContentAdapter() {
+		@Override
+		public void reallyNotifyChanged(final Notification notification) {
+			if (ScheduleModelEditorContribution.this.modelObject.isDirty()) {
+				return;
+			}
+			final int type = notification.getEventType();
+			switch (type) {
+			case Notification.RESOLVE:
+			case Notification.REMOVING_ADAPTER:
+				break;
+			default:
+				if (notification.getFeature() == MMXCorePackage.eINSTANCE.getNamedObject_Name()) return; // this feature is irrelevant
+				EObject target = (EObject) notification.getNotifier();
+				while (target != null) {
+					if (target == modelObject) return;
+					target = target.eContainer();
+				}
+				log.debug("Setting dirty bit on schedule model");
+				ScheduleModelEditorContribution.this.modelObject.setDirty(true);
+			}
+		}
+
+
+		@Override
+		public void disable() {
+			log.debug("Adapter disabled");
+			super.disable();
+		}
+
+
+		@Override
+		public void enable() {
+			log.debug("Adapter enabled");
+			super.enable();
+		}
+	};
+	
+	@Override
+	public void init(final JointModelEditorPart editorPart, final MMXRootObject rootObject, final UUIDObject modelObject) {
+		super.init(editorPart, rootObject, modelObject);
+		rootObject.eAdapters().add(dirtyStateAdapter);
+	}
+
+
+	@Override
+	public void dispose() {
+		rootObject.eAdapters().remove(dirtyStateAdapter);
+		super.dispose();
+	}
+
+
+
+	@Override
+	public void addPages(Composite parent) {
+		
+	}
+
+	@Override
+	public void setLocked(boolean locked) {
+		
+	}
+}
