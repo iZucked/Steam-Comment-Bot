7d4488518b8caa1e20231ef380db1dd1c726d17d
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-04-18 14:05:28 -0100

    Remove evaluation service, as it was not the right way to do it.

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.analytics.transformer/src/com/mmxlabs/models/lng/analytics/transformer/Activator.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.analytics.transformer/src/com/mmxlabs/models/lng/analytics/transformer/Activator.java
@@ -2,10 +2,6 @@ package com.mmxlabs.models.lng.analytics.transformer;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-
-import com.mmxlabs.models.lng.analytics.evaluation.IEvaluationService;
-import com.mmxlabs.models.lng.analytics.evaluation.impl.EvaluationService;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -18,7 +14,6 @@ public class Activator extends AbstractUIPlugin {
 	// The shared instance
 	private static Activator plugin;
 
-	private ServiceRegistration<IEvaluationService> evaluationServiceRegistration;
 	
 	/**
 	 * The constructor
@@ -30,12 +25,9 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		
-		evaluationServiceRegistration = context.registerService(IEvaluationService.class, new EvaluationService(), null);
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		evaluationServiceRegistration.unregister();
 		plugin = null;
 		super.stop(context);
 	}
