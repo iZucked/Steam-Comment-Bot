27cedc9e01b89922cb922028cafbdb33c30989ec
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-20 13:51:30 -0100

    Doc

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service/src/com/mmxlabs/scenario/service/ScenarioServiceRegistry.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service/src/com/mmxlabs/scenario/service/ScenarioServiceRegistry.java
@@ -5,12 +5,19 @@
 package com.mmxlabs.scenario.service;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.mmxlabs.scenario.service.model.ScenarioModel;
 import com.mmxlabs.scenario.service.model.ScenarioServiceFactory;
 
+/**
+ * Registry used to hold references to {@link IScenarioService} instances.
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public final class ScenarioServiceRegistry {
 
 	private final List<IScenarioService> scenarioServices = new LinkedList<IScenarioService>();
@@ -22,22 +29,45 @@ public final class ScenarioServiceRegistry {
 		scenarioModel = ScenarioServiceFactory.eINSTANCE.createScenarioModel();
 	}
 
+	/**
+	 * Returns the EMF logical model of the scenario service.
+	 * 
+	 * @return
+	 */
 	public ScenarioModel getScenarioModel() {
 		return scenarioModel;
 	}
 
+	/**
+	 * Returns an unmodifiable {@link Collection} of the registered {@link IScenarioService} instances.
+	 * 
+	 * @return
+	 */
 	public Collection<IScenarioService> getScenarioServices() {
-		return scenarioServices;
+		return Collections.unmodifiableCollection(scenarioServices);
 	}
 
+	/**
+	 * Register an {@link IScenarioService} instance.
+	 * 
+	 * @param scenarioService
+	 */
 	public void bindScenarioService(final IScenarioService scenarioService) {
+		// Add to local list
 		scenarioServices.add(scenarioService);
+		// Add to EMF model
 		scenarioModel.getScenarioServices().add(scenarioService.getServiceModel());
 	}
 
+	/**
+	 * Un-register an {@link IScenarioService} instance.
+	 * 
+	 * @param scenarioService
+	 */
 	public void unbindScenarioService(final IScenarioService scenarioService) {
+		// Remove local list
 		scenarioServices.remove(scenarioService);
+		// Update EMF model
 		scenarioModel.getScenarioServices().remove(scenarioService.getServiceModel());
 	}
-
 }
