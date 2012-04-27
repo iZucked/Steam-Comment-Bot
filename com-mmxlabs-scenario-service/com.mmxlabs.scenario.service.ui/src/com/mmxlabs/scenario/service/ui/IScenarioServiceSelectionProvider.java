c8e9848b7da415a1ab3af22ea496da55451d2047
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-27 11:01:34 -0100

    Add some interfaces for a selection provider

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/IScenarioServiceSelectionProvider.java
@@ -0,0 +1,25 @@
+package com.mmxlabs.scenario.service.ui;
+
+import java.util.Collection;
+
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+/**
+ * A selection provider interface for the scenario service.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public interface IScenarioServiceSelectionProvider {
+
+	/**
+	 * Returns the currently selected {@link ScenarioInstance}s.
+	 * 
+	 * @return
+	 */
+	Collection<ScenarioInstance> getSelection();
+
+	void addSelectionChangedListener(IScenarioServiceSelectionChangedListener listener);
+
+	void removeSelectionChangedListener(IScenarioServiceSelectionChangedListener listener);
+}
