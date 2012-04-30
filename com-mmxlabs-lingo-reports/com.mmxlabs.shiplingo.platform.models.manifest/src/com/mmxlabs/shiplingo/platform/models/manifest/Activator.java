5f3a22d2106f212f0c9e2dadab5a3a41da554c5a
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-30 16:31:43 -0100

    Correct bundle ID

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.models.manifest/src/com/mmxlabs/shiplingo/platform/models/manifest/Activator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.models.manifest/src/com/mmxlabs/shiplingo/platform/models/manifest/Activator.java
@@ -13,7 +13,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.mmxlabs.models.lng.demo"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.mmxlabs.shiplingo.platform.models.manifest"; //$NON-NLS-1$
 	
 	// The shared instance
 	private static Activator plugin;
