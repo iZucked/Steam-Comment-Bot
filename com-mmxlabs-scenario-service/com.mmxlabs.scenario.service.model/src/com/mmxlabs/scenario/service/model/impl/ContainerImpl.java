e238f983bb014ae59c295e7b09a1e793f4a7591d
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-04-30 15:39:55 -0100

    Combine com.mmxlabs.scenario.service and com.mmxlabs.scenario.service.model to avoid cyclic dependencies in planned future changes.

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.model/src/com/mmxlabs/scenario/service/model/impl/ContainerImpl.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.model/src/com/mmxlabs/scenario/service/model/impl/ContainerImpl.java
@@ -22,7 +22,6 @@ import org.eclipse.emf.ecore.impl.EObjectImpl;
 
 import org.eclipse.emf.ecore.util.EObjectContainmentWithInverseEList;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.ecore.util.EObjectContainmentEList;
 import org.eclipse.emf.ecore.util.InternalEList;
 
 /**
