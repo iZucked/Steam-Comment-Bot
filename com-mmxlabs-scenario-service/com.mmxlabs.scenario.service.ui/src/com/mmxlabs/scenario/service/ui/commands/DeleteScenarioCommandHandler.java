73c1ee1a5189ccd295d16f0d2149a25f8b9f3693
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-01 15:08:03 -0100

    fix delete command

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/DeleteScenarioCommandHandler.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/DeleteScenarioCommandHandler.java
@@ -9,18 +9,14 @@ import java.util.Iterator;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.mmxlabs.scenario.service.IScenarioService;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
 public class DeleteScenarioCommandHandler extends AbstractHandler {
@@ -42,29 +38,12 @@ public class DeleteScenarioCommandHandler extends AbstractHandler {
 				final Object element = iterator.next();
 				if (element instanceof ScenarioInstance) {
 					final ScenarioInstance model = (ScenarioInstance) element;
-
-					model.setLocked(!model.isLocked());
+					final IScenarioService service = model.getScenarioService();
+					service.delete(model);
 				}
 			}
 		}
 
 		return null;
-	}
-
-	public void openEditor(final IEditorInput editorInput) throws PartInitException {
-
-		final IEditorPart editorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findEditor(editorInput);
-		if (editorPart != null) {
-			// PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(editorInput, null, true);
-		} else {
-			// String contentTypeString = editorInput.getContentType();
-			final IContentType contentType = null;// contentTypeString == null ? null : Platform.getContentTypeManager().getContentType(contentTypeString);
-
-			final IEditorDescriptor descriptor = registry.getDefaultEditor(editorInput.getName(), contentType);
-
-			if (descriptor != null) {
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(editorInput, descriptor.getId());
-			}
-		}
 	}
 }
