f245554686213a126c0e87bebfd321c14655a3cb
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-02 16:01:29 -0100

    Fix mistake where empty folders are hidden

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
@@ -4,6 +4,7 @@
  */
 package com.mmxlabs.scenario.service.ui.navigator;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 
 public class ScenarioServiceContentProvider extends AdapterFactoryContentProvider {
@@ -17,9 +18,17 @@ public class ScenarioServiceContentProvider extends AdapterFactoryContentProvide
 		final Object[] elements = super.getElements(object);
 
 		// Skip root node if there is only one item
-		if (elements.length == 1) {
+		if (elements.length == 1 && super.getParent(object) == null) {
 			return getChildren(elements[0]);
 		}
 		return elements;
 	}
+
+	@Override
+	public void notifyChanged(Notification notification) {
+		// TODO Auto-generated method stub
+		super.notifyChanged(notification);
+	}
+	
+	
 }
