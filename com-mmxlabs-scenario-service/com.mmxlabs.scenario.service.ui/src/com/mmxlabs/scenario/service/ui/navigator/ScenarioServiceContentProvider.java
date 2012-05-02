73e431a11e535276f7a7e777679b2b29a1d6e6ab
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-05-02 18:59:15 -0100

    Add method to be able to filter out extra stuff from tree - i.e. show everything down the current hierarchy to a Container but not for example Metadata objects.

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceContentProvider.java
@@ -4,9 +4,22 @@
  */
 package com.mmxlabs.scenario.service.ui.navigator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 
+import com.mmxlabs.scenario.service.IScenarioService;
+import com.mmxlabs.scenario.service.ScenarioServiceRegistry;
+import com.mmxlabs.scenario.service.model.Container;
+import com.mmxlabs.scenario.service.model.ScenarioModel;
+
 public class ScenarioServiceContentProvider extends AdapterFactoryContentProvider {
+
+	/**
+	 * Flag indicating whether this content provider returns the whole tree, or only down to {@link Container} elements
+	 */
+	private boolean showOnlyContainers = false;
 
 	public ScenarioServiceContentProvider() {
 		super(ScenarioServiceComposedAdapterFactory.getAdapterFactory());
@@ -14,12 +27,73 @@ public class ScenarioServiceContentProvider extends AdapterFactoryContentProvide
 
 	@Override
 	public Object[] getElements(final Object object) {
-		final Object[] elements = super.getElements(object);
 
-		// Skip root node if there is only one item
+		return getChildren(object);
+	}
+
+	@Override
+	public Object[] getChildren(final Object object) {
+
+		final Object[] elements;
+		if (object instanceof ScenarioServiceRegistry) {
+			final ScenarioServiceRegistry scenarioServiceRegistry = (ScenarioServiceRegistry) object;
+			elements = scenarioServiceRegistry.getScenarioServices().toArray();
+		} else if (object instanceof IScenarioService) {
+			final IScenarioService scenarioService = (IScenarioService) object;
+			elements = scenarioService.getServiceModel().getElements().toArray();
+		} else {
+			elements = super.getChildren(object);
+		}
+
+		// Problem - if there is only one - but then add another, the parent in not in the tree!
+		// We need to record something so the notification can force a full refresh of the viewer.
+
+		// // Skip root node if there is only one item
 		if (elements.length == 1) {
 			return getChildren(elements[0]);
 		}
+
+		return filter(elements);
+	}
+
+	/**
+	 * Filter the input array of elements based on configuration flags and return the same or new array of objects.
+	 * 
+	 * @param elements
+	 * @return
+	 */
+	private Object[] filter(final Object[] elements) {
+
+		if (showOnlyContainers) {
+			// Allow Objects down to the Container
+			final List<Object> c = new ArrayList<Object>(elements.length);
+			for (final Object e : elements) {
+				if (e instanceof Container) {
+					c.add(e);
+				} else if (e instanceof ScenarioModel) {
+					c.add(e);
+				} else if (e instanceof IScenarioService) {
+					c.add(e);
+				} else if (e instanceof ScenarioServiceRegistry) {
+					c.add(e);
+				}
+			}
+			return c.toArray();
+		}
+
 		return elements;
+	}
+
+	@Override
+	public boolean hasChildren(final Object object) {
+		return getChildren(object).length > 0;
+	}
+
+	public boolean isShowOnlyContainers() {
+		return showOnlyContainers;
+	}
+
+	public void setShowOnlyContainers(final boolean showOnlyContainers) {
+		this.showOnlyContainers = showOnlyContainers;
 	}
 }
