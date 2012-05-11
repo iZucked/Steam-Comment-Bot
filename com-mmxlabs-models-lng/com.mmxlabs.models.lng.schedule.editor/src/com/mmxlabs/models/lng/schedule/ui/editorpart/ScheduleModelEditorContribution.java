ec10b3d13f6f952cac26e14e11516b42adea8595
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-11 07:49:23 -0100

    Fix dirty bit setting adapter to work with non-contained submodels

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule.editor/src/com/mmxlabs/models/lng/schedule/ui/editorpart/ScheduleModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule.editor/src/com/mmxlabs/models/lng/schedule/ui/editorpart/ScheduleModelEditorContribution.java
@@ -4,6 +4,10 @@
  */
 package com.mmxlabs.models.lng.schedule.ui.editorpart;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.widgets.Composite;
@@ -13,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import com.mmxlabs.models.lng.schedule.ScheduleModel;
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.mmxcore.MMXSubModel;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 import com.mmxlabs.models.mmxcore.impl.MMXContentAdapter;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
@@ -47,17 +52,23 @@ public class ScheduleModelEditorContribution extends BaseJointModelEditorContrib
 			}
 		}
 	};
+	private List<UUIDObject> adaptedObjects = new ArrayList<UUIDObject>();
 	
 	@Override
 	public void init(final JointModelEditorPart editorPart, final MMXRootObject rootObject, final UUIDObject modelObject) {
 		super.init(editorPart, rootObject, modelObject);
-		rootObject.eAdapters().add(dirtyStateAdapter);
+		for (final MMXSubModel subModel : rootObject.getSubModels()) {
+			subModel.getSubModelInstance().eAdapters().add(dirtyStateAdapter);
+			adaptedObjects.add(subModel.getSubModelInstance());
+		}
 	}
 
 
 	@Override
 	public void dispose() {
-		rootObject.eAdapters().remove(dirtyStateAdapter);
+		for (final UUIDObject sub : adaptedObjects) {
+			sub.eAdapters().removeAll(Collections.singleton(dirtyStateAdapter));
+		}
 		super.dispose();
 	}
 
