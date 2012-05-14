eeeba4cfcd6b5c6067276746a8d1c13c8b0eb3a2
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-05-14 19:16:39 -0100

    Fix up export CSV action and move import menu to manifest plugin

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/shiplingo/platform/models/manifest/exportWizards/ExportCSVWizard.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/shiplingo/platform/models/manifest/exportWizards/ExportCSVWizard.java
@@ -11,94 +11,108 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 
-import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IExportWizard;
 import org.eclipse.ui.IWorkbench;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.MMXSubModel;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 import com.mmxlabs.models.util.Activator;
 import com.mmxlabs.models.util.importer.ISubmodelImporter;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
 /**
  * Export the selected scenario to the filesystem somehow.
  * 
  * @author hinton
- *
+ * 
  */
 public class ExportCSVWizard extends Wizard implements IExportWizard {
+
+	private static final Logger log = LoggerFactory.getLogger(ExportCSVWizard.class);
+
 	private ExportCSVWizardPage exportPage;
 
 	@Override
 	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
 		setWindowTitle("CSV Export Wizard");
-		exportPage = new ExportCSVWizardPage();
+		exportPage = new ExportCSVWizardPage(selection);
 	}
 
 	@Override
 	public boolean performFinish() {
 		final File outputDirectory = exportPage.getOutputDirectory();
-		final List<IResource> l = exportPage.getSelectedResources();
-		
-		boolean createExportDirectories = l.size() > 1;
-		
-		for (final IResource resource : l) {
-			final MMXRootObject rootObject = (MMXRootObject) resource.getAdapter(MMXRootObject.class);
-			
+		final Collection<ScenarioInstance> instances = exportPage.getScenarioInstance();
+
+		final boolean createExportDirectories = instances.size() > 1;
+		for (final ScenarioInstance instance : instances) {
+			MMXRootObject rootObject = (MMXRootObject) instance.getInstance();
+
+			if (rootObject == null) {
+				try {
+					instance.getScenarioService().load(instance);
+				} catch (final IOException e) {
+					log.error(e.getMessage(), e);
+					throw new RuntimeException(e);
+				}
+				rootObject = (MMXRootObject) instance.getInstance();
+			}
+
 			if (rootObject != null) {
-				final File directory = createExportDirectories ? new File(outputDirectory, resource.getName()) : outputDirectory;
+				final File directory = createExportDirectories ? new File(outputDirectory, instance.getName()) : outputDirectory;
 				if (!directory.exists()) {
 					directory.mkdirs();
 				}
-				
+
 				// generate export files
 				for (final MMXSubModel subModel : rootObject.getSubModels()) {
 					final UUIDObject modelInstance = subModel.getSubModelInstance();
 					final ISubmodelImporter importer = Activator.getDefault().getImporterRegistry().getSubmodelImporter(modelInstance.eClass());
 					if (importer != null) {
-						final Map<String, Collection<Map<String, String>>> outputs = new HashMap<String, Collection<Map<String,String>>>();
+						final Map<String, Collection<Map<String, String>>> outputs = new HashMap<String, Collection<Map<String, String>>>();
 						importer.exportModel(modelInstance, outputs);
-						
+
 						for (final String key : outputs.keySet()) {
 							final Collection<Map<String, String>> rows = outputs.get(key);
 							final String friendlyName = importer.getRequiredInputs().get(key);
 							final File outputFile = new File(directory, friendlyName + ".csv");
-							
+
 							// export CSV for this file
-							
 							writeCSV(rows, outputFile);
 						}
 					}
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * @param rows
 	 * @param outputFile
 	 */
-	private void writeCSV(Collection<Map<String, String>> rows, File outputFile) {
-		if (rows.isEmpty()) return;
+	private void writeCSV(final Collection<Map<String, String>> rows, final File outputFile) {
+		if (rows.isEmpty())
+			return;
 		final LinkedHashSet<String> keys = new LinkedHashSet<String>();
 		for (final Map<String, String> row : rows) {
 			keys.addAll(row.keySet());
 		}
-		
+
 		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter(outputFile));
 			boolean firstKey = true;
 			for (final String key : keys) {
-				if (!firstKey) writer.write(",");
+				if (!firstKey)
+					writer.write(",");
 				firstKey = false;
 				writer.write(escape(key));
 			}
@@ -106,31 +120,33 @@ public class ExportCSVWizard extends Wizard implements IExportWizard {
 				writer.write("\n");
 				firstKey = true;
 				for (final String key : keys) {
-					if (!firstKey) writer.write(",");
+					if (!firstKey)
+						writer.write(",");
 					firstKey = false;
 					writer.write(escape(row.get(key)));
 				}
 			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 		} finally {
 			if (writer != null) {
 				try {
 					writer.close();
-				} catch (IOException e) {
+				} catch (final IOException e) {
 				}
 			}
 		}
 	}
 
 	private String escape(final String key) {
-		if (key == null) return "";
+		if (key == null)
+			return "";
 		final String sub = key.trim().replace("\"", "\\\"");
-		return sub.contains(",") ? "\""+sub+"\"" : sub;
+		return sub.contains(",") ? "\"" + sub + "\"" : sub;
 	}
 
 	@Override
 	public void addPages() {
-        super.addPages(); 
-        addPage(exportPage);
+		super.addPages();
+		addPage(exportPage);
 	}
 }
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/shiplingo/platform/models/manifest/exportWizards/ExportCSVWizardPage.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario/src/com/mmxlabs/shiplingo/platform/models/manifest/exportWizards/ExportCSVWizardPage.java
@@ -5,10 +5,13 @@
 package com.mmxlabs.shiplingo.platform.models.manifest.exportWizards;
 
 import java.io.File;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.eclipse.jface.preference.DirectoryFieldEditor;
-import org.eclipse.jface.preference.FileFieldEditor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -17,54 +20,32 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.ui.dialogs.WizardExportResourcesPage;
+import org.eclipse.swt.widgets.Listener;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import com.mmxlabs.scenario.service.ScenarioServiceRegistry;
+import com.mmxlabs.scenario.service.model.Container;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.scenario.service.ui.ScenarioServiceSelectionGroup;
+import com.mmxlabs.shiplingo.platform.models.manifest.internal.Activator;
 
 /**
  * @author hinton
- *
+ * 
  */
-public class ExportCSVWizardPage extends WizardExportResourcesPage {
+public class ExportCSVWizardPage extends WizardPage {
+
+	private ScenarioServiceSelectionGroup scenarioServiceSelectionGroup;
+
+	private final ISelection selection;
+
 	private DirectoryFieldEditor editor;
 
-	protected ExportCSVWizardPage() {
-		super("Export Scenario as CSV", null);
-	}
-
-	@Override
-	public void handleEvent(Event event) {
-		
-	}
-
-	@Override
-	protected void createDestinationGroup(Composite parent) {
-		final Group destination = new Group(parent, SWT.NONE);
-		destination.setText("Destination Directory");
-		GridLayout layout = new GridLayout();
-		destination.setLayout(layout);
-		destination.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL
-                | GridData.GRAB_HORIZONTAL));
-		destination.setFont(parent.getFont());
-		
-		final DirectoryFieldEditor editor = new DirectoryFieldEditor("destination-directory", "Export to directory:", destination);
-		
-		this.editor = editor;
-		editor.getTextControl(destination).addModifyListener(new ModifyListener() {			
-			@Override
-			public void modifyText(ModifyEvent e) {
-				ExportCSVWizardPage.this.updatePageCompletion();
-			}
-		});
-	}
-
-	@Override
-	protected void createOptionsGroup(Composite parent) {
-		// TODO Auto-generated method stub
-//		super.createOptionsGroup(parent);
-	}
-
-	@Override
-	public List getSelectedResources() {
-		return super.getSelectedResources();
+	protected ExportCSVWizardPage(ISelection selection) {
+		super("Export Scenario as CSV");
+		this.selection = selection;
 	}
 
 	@Override
@@ -78,4 +59,99 @@ public class ExportCSVWizardPage extends WizardExportResourcesPage {
 	public File getOutputDirectory() {
 		return new File(editor.getStringValue());
 	}
+
+	@Override
+	public void createControl(Composite parent) {
+
+		final Composite container = new Composite(parent, SWT.NULL);
+
+		final GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 2;
+		layout.verticalSpacing = 9;
+		final Listener listener = new Listener() {
+			public void handleEvent(final Event event) {
+
+				dialogChanged();
+
+			}
+		};
+
+		scenarioServiceSelectionGroup = new ScenarioServiceSelectionGroup(container, listener, SWT.BORDER);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.widthHint = 300;
+		gd.heightHint = 320;
+		gd.horizontalSpan = 2;
+		scenarioServiceSelectionGroup.setLayoutData(gd);
+
+		final Group destination = new Group(container, SWT.NONE);
+		destination.setText("Destination Directory");
+		GridLayout layout2 = new GridLayout();
+		destination.setLayout(layout2);
+		destination.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL));
+		destination.setFont(parent.getFont());
+
+		final DirectoryFieldEditor editor = new DirectoryFieldEditor("destination-directory", "Export to directory:", destination);
+
+		this.editor = editor;
+		editor.getTextControl(destination).addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		initialize();
+		dialogChanged();
+		setControl(container);
+	}
+
+	private void initialize() {
+		final Bundle bundle = Activator.getDefault().getBundle();
+		final BundleContext bundleContext = bundle.getBundleContext();
+		final ServiceReference<ScenarioServiceRegistry> serviceReference = bundleContext.getServiceReference(ScenarioServiceRegistry.class);
+
+		scenarioServiceSelectionGroup.setInput(bundleContext.getService(serviceReference));
+
+		if (selection != null && selection.isEmpty() == false && selection instanceof IStructuredSelection) {
+
+			final IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1) {
+				return;
+			}
+			final Object obj = ssel.getFirstElement();
+			if (obj instanceof Container) {
+				scenarioServiceSelectionGroup.setSelectedContainer((Container) obj);
+			}
+		}
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+	private void dialogChanged() {
+
+		Container c = scenarioServiceSelectionGroup.getSelectedContainer();
+		if (!(c instanceof ScenarioInstance)) {
+			updateStatus("A Scenario must be selected");
+			return;
+		}
+
+		// TODO: Create automatically?
+		if (!new File(editor.getStringValue()).exists()) {
+			updateStatus("Output diectory must exist");
+		}
+
+		updateStatus(null);
+	}
+
+	private void updateStatus(final String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public Collection<ScenarioInstance> getScenarioInstance() {
+		return Collections.singleton((ScenarioInstance) scenarioServiceSelectionGroup.getSelectedContainer());
+	}
+
 }
