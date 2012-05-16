8a7c4f26df423c9d7c3db58ef0fab4df2397ac16
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-16 11:42:50 -0100

    Hide ports from editor

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editorpart/PortModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editorpart/PortModelEditorContribution.java
@@ -4,44 +4,39 @@
  */
 package com.mmxlabs.models.lng.port.ui.editorpart;
 
-import java.util.Collections;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.widgets.Composite;
 
 import com.mmxlabs.models.lng.port.PortModel;
-import com.mmxlabs.models.lng.port.PortPackage;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
 
 public class PortModelEditorContribution extends BaseJointModelEditorContribution<PortModel> {
-	private PortEditorPane portViewerPane;
-	private PortGroupEditorPane groupViewerPane;
-	private int pageNumber = 1;
+//	private PortEditorPane portViewerPane;
+//	private PortGroupEditorPane groupViewerPane;
+//	private int pageNumber = 1;
 
 	@Override
 	public void addPages(Composite parent) {
-		final SashForm sash = new SashForm(parent, SWT.HORIZONTAL);
-		portViewerPane = new PortEditorPane(editorPart.getSite().getPage(), editorPart, editorPart);
-		portViewerPane.createControl(sash);
-		portViewerPane.init(Collections.singletonList(PortPackage.eINSTANCE.getPortModel_Ports()),
-				editorPart.getAdapterFactory());
-		
-		groupViewerPane = new PortGroupEditorPane(editorPart.getSite().getPage(), editorPart, editorPart);
-		groupViewerPane.createControl(sash);
-		groupViewerPane.init(Collections.singletonList(PortPackage.eINSTANCE.getPortModel_PortGroups()),
-				editorPart.getAdapterFactory());
-		
-		portViewerPane.getViewer().setInput(modelObject);
-		groupViewerPane.getViewer().setInput(modelObject);
-		
-		pageNumber = editorPart.addPage(sash);
-		editorPart.setPageText(pageNumber, "Ports");
+//		final SashForm sash = new SashForm(parent, SWT.HORIZONTAL);
+//		portViewerPane = new PortEditorPane(editorPart.getSite().getPage(), editorPart, editorPart);
+//		portViewerPane.createControl(sash);
+//		portViewerPane.init(Collections.singletonList(PortPackage.eINSTANCE.getPortModel_Ports()),
+//				editorPart.getAdapterFactory());
+//		
+//		groupViewerPane = new PortGroupEditorPane(editorPart.getSite().getPage(), editorPart, editorPart);
+//		groupViewerPane.createControl(sash);
+//		groupViewerPane.init(Collections.singletonList(PortPackage.eINSTANCE.getPortModel_PortGroups()),
+//				editorPart.getAdapterFactory());
+//		
+//		portViewerPane.getViewer().setInput(modelObject);
+//		groupViewerPane.getViewer().setInput(modelObject);
+//		
+//		pageNumber = editorPart.addPage(sash);
+//		editorPart.setPageText(pageNumber, "Ports");
 	}
 
 	@Override
 	public void setLocked(boolean locked) {
-		portViewerPane.setLocked(locked);
-		groupViewerPane.setLocked(locked);
+//		portViewerPane.setLocked(locked);
+//		groupViewerPane.setLocked(locked);
 	}
 }
