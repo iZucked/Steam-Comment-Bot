c16103862fd9eb5d102d6e921d960ed9e664ed1a
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-16 10:26:35 -0100

    Use job state properties to control enablement of handlers

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/shiplingo/platform/models/optimisation/propertytesters/JobStatePropertyTester.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/shiplingo/platform/models/optimisation/propertytesters/JobStatePropertyTester.java
@@ -3,6 +3,7 @@ package com.mmxlabs.shiplingo.platform.models.optimisation.propertytesters;
 import org.eclipse.core.expressions.PropertyTester;
 
 import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
+import com.mmxlabs.jobmanager.jobs.EJobState;
 import com.mmxlabs.jobmanager.jobs.IJobControl;
 import com.mmxlabs.jobmanager.jobs.IJobDescriptor;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
@@ -14,8 +15,54 @@ public class JobStatePropertyTester extends PropertyTester {
 
 	@Override
 	public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
-		final IEclipseJobManager jobManager = Activator.getDefault().getJobManager();
+		if ("jobState".equals(property)) {
+			return testJobState(receiver, expectedValue);
+		} else if ("canPause".equals(property)) {
+			final EJobState state = getJobState(receiver);
+			if (state == EJobState.RUNNING) return true;
+		} else if ("canPlay".equals(property)) {
+			final EJobState state = getJobState(receiver);
+			if (state == null) return true;
+			switch (state) {
+			case CANCELLED:
+			case COMPLETED:
+			case PAUSED:
+			case INITIALISED:
+			case CREATED:
+				return true;
+			default: return false;
+			}
+		} else if ("canTerminate".equals(property)) {
+			final EJobState state = getJobState(receiver);
+			if (state == null) return false;
+			switch (state) {
+			case RUNNING:
+			case PAUSED:
+				return true;
+			default: return false;
+			}
+		} else if ("hasActiveJob".equals(property)) {
+			final EJobState state = getJobState(receiver);
+			if (state == null) return false;
+			switch (state) {
+			case RUNNING:
+			case PAUSED:
+			case PAUSING:
+			case INITIALISED:
+			case INITIALISING:
+			case CANCELLING:
+			case RESUMING:
+				return true;
+			default: return false;
+			}
+		}
 		
+		
+		return false;
+	}
+
+	private EJobState getJobState(final Object receiver) {
+		final IEclipseJobManager jobManager = Activator.getDefault().getJobManager();
 		if (jobManager != null) {
 			if (receiver instanceof ScenarioInstance) {
 				final ScenarioInstance instance = (ScenarioInstance) receiver;
@@ -23,14 +70,22 @@ public class JobStatePropertyTester extends PropertyTester {
 				if (descriptor != null) {
 					final IJobControl control = jobManager.getControlForJob(descriptor);
 					if (control!= null) {
-						if (expectedValue != null) {
-							return expectedValue.toString().equalsIgnoreCase(control.getJobState().name());
-						}
+						return control.getJobState();
 					}
 				}
 			}
 		}
+		return null;
+	}
+	
+	private boolean testJobState(final Object receiver,
+			final Object expectedValue) {
+		final EJobState state = getJobState(receiver);
 		
+		if (expectedValue != null && state != null) {
+			return expectedValue.toString().equalsIgnoreCase(state.name());
+		}
+					
 		return false;
 	}
 }
