8be0cd546aef48165a483dcd259f162c66fe0109
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-16 08:51:43 -0100

    Actually store up the backlog of notifications when adapter is disabled

--- com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXAdapterImpl.java
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXAdapterImpl.java
@@ -4,38 +4,54 @@
  */
 package com.mmxlabs.models.mmxcore.impl;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 import com.mmxlabs.models.mmxcore.IMMXAdapter;
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
 
 public abstract class MMXAdapterImpl extends AdapterImpl implements IMMXAdapter {
-	private boolean missed = false;
+	private List<Notification> missedNotifications = new LinkedList<Notification>();
+	
+	protected final HashSet<EStructuralFeature> ignoredFeatures = new HashSet<EStructuralFeature>();
+	
+	public MMXAdapterImpl() {
+		ignoredFeatures.add(MMXCorePackage.eINSTANCE.getMMXObject_Proxies());
+		ignoredFeatures.addAll(MMXCorePackage.eINSTANCE.getMMXProxy().getEStructuralFeatures());
+	}
+	
 	@Override
 	public void notifyChanged(final Notification notification) {
+		if (ignoredFeatures.contains(notification.getFeature())) return;
 		if (enabled) reallyNotifyChanged(notification);
-		else missed = true;
+		else missedNotifications.add(notification);
 	}
 	
 	public abstract void reallyNotifyChanged(final Notification notification);
 	
-	protected void missedNotification() {
+	protected void missedNotifications(final List<Notification> missed) {
 		
 	}
 	
 	boolean enabled = true;
 	@Override
 	public void disable() {
-		missed = false;
+		missedNotifications.clear();
 		enabled = false;
 	}
 
 	@Override
 	public void enable() {
-		if (missed) {
-			missedNotification();
+		if (missedNotifications.isEmpty() == false) {
+			missedNotifications(Collections.unmodifiableList(missedNotifications));
+			missedNotifications.clear();
 		}
-		missed = false;
 		enabled = true;
 	}
 }
--- com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXContentAdapter.java
+++ com-mmxlabs-models-mmxcore/com.mmxlabs.models.mmxcore/src/com/mmxlabs/models/mmxcore/impl/MMXContentAdapter.java
@@ -4,23 +4,38 @@
  */
 package com.mmxlabs.models.mmxcore.impl;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 
 import com.mmxlabs.models.mmxcore.IMMXAdapter;
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
 
 public abstract class MMXContentAdapter extends EContentAdapter implements IMMXAdapter {
-	private boolean missed = false;
+	private List<Notification> missedNotifications = new LinkedList<Notification>();
+	protected final HashSet<EStructuralFeature> ignoredFeatures = new HashSet<EStructuralFeature>();
+	
+	public MMXContentAdapter() {
+		ignoredFeatures.add(MMXCorePackage.eINSTANCE.getMMXObject_Proxies());
+		ignoredFeatures.addAll(MMXCorePackage.eINSTANCE.getMMXProxy().getEStructuralFeatures());
+	}
+	
 	@Override
 	public void notifyChanged(Notification notification) {
 		super.notifyChanged(notification);
+		if (ignoredFeatures.contains(notification.getFeature())) return;
 		if (enabled) reallyNotifyChanged(notification);
-		else missed = true;
+		else missedNotifications.add(notification);
 	}
 	
 	public abstract void reallyNotifyChanged(final Notification notification);
 
-	protected void missedNotification() {
+	protected void missedNotifications(final List<Notification> missed) {
 		
 	}
 	
@@ -28,16 +43,15 @@ public abstract class MMXContentAdapter extends EContentAdapter implements IMMXA
 	@Override
 	public void disable() {
 		enabled = false;
-		missed = false;
+		missedNotifications.clear();
 	}
 
 	@Override
 	public void enable() {
-		if (missed) {
-			missedNotification();
+		if (missedNotifications.isEmpty() == false) {
+			missedNotifications(Collections.unmodifiableList(missedNotifications));
+			missedNotifications.clear();
 		}
-		missed = false;
 		enabled = true;
 	}
-
 }
