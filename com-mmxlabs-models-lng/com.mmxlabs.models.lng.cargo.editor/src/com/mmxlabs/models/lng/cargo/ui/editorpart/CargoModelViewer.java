61a364caa4f47c065d4932ea73989113e3276726
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-05-17 11:47:50 -0100

    Override notification processing method in EObjectTableViewer to allow slot -> cargo transitions.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
@@ -15,6 +15,8 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
@@ -28,6 +30,7 @@ import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.ui.actions.RewireAction;
 import com.mmxlabs.models.lng.cargo.ui.actions.RotateSlotsAction;
 import com.mmxlabs.models.lng.ui.actions.SimpleImportAction;
+import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewer;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.ui.dates.DateAttributeManipulator;
@@ -37,15 +40,15 @@ import com.mmxlabs.models.ui.tabular.SingleReferenceManipulator;
 import com.mmxlabs.models.ui.valueproviders.IReferenceValueProviderProvider;
 
 public class CargoModelViewer extends ScenarioTableViewerPane {
-	private IScenarioEditingLocation part;
-	
-	public CargoModelViewer(IWorkbenchPage page, IWorkbenchPart part, final IScenarioEditingLocation location, final IActionBars actionBars) {
+	private final IScenarioEditingLocation part;
+
+	public CargoModelViewer(final IWorkbenchPage page, final IWorkbenchPart part, final IScenarioEditingLocation location, final IActionBars actionBars) {
 		super(page, part, location, actionBars);
 		this.part = location;
 	}
-	
+
 	@Override
-	public void init(List<EReference> path, AdapterFactory adapterFactory) {
+	public void init(final List<EReference> path, final AdapterFactory adapterFactory) {
 		super.init(path, adapterFactory);
 		final RewireAction rewire = new RewireAction(getJointModelEditorPart());
 		final RotateSlotsAction rotate = new RotateSlotsAction(getJointModelEditorPart());
@@ -58,31 +61,21 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 		final CargoPackage pkg = CargoPackage.eINSTANCE;
 		final IReferenceValueProviderProvider provider = part.getReferenceValueProviderCache();
 		final EditingDomain editingDomain = part.getEditingDomain();
-		
-		addTypicalColumn(
-				"ID",
-				new BasicAttributeManipulator(mmx.getNamedObject_Name(), editingDomain));
-		
-		addTypicalColumn(
-				"Load Port",
-				new SingleReferenceManipulator(pkg.getSlot_Port(), provider, editingDomain), pkg.getCargo_LoadSlot());
-		
+
+		addTypicalColumn("ID", new BasicAttributeManipulator(mmx.getNamedObject_Name(), editingDomain));
+
+		addTypicalColumn("Load Port", new SingleReferenceManipulator(pkg.getSlot_Port(), provider, editingDomain), pkg.getCargo_LoadSlot());
+
 		addTypicalColumn("Load Date", new DateAttributeManipulator(pkg.getSlot_WindowStart(), editingDomain), pkg.getCargo_LoadSlot());
-		
-		addTypicalColumn(
-				"Load Contract",
-				new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_LoadSlot());
-		
-		addTypicalColumn(
-				"Discharge Port",
-				new SingleReferenceManipulator(pkg.getSlot_Port(), provider, editingDomain), pkg.getCargo_DischargeSlot());
-		
+
+		addTypicalColumn("Load Contract", new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_LoadSlot());
+
+		addTypicalColumn("Discharge Port", new SingleReferenceManipulator(pkg.getSlot_Port(), provider, editingDomain), pkg.getCargo_DischargeSlot());
+
 		addTypicalColumn("Discharge Date", new DateAttributeManipulator(pkg.getSlot_WindowStart(), editingDomain), pkg.getCargo_DischargeSlot());
-		
-		addTypicalColumn(
-				"Discharge Contract",
-				new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_DischargeSlot());
-		
+
+		addTypicalColumn("Discharge Contract", new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_DischargeSlot());
+
 		setTitle("Cargoes", PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_DEF_VIEW));
 	}
 
@@ -90,17 +83,20 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 	protected Action createImportAction() {
 		return new SimpleImportAction(part, getScenarioViewer()) {
 			@Override
-			protected Command mergeImports(EObject container, EReference containment, Collection<EObject> imports) {
+			protected Command mergeImports(final EObject container, final EReference containment, final Collection<EObject> imports) {
 				final List<EObject> cargoes = new ArrayList<EObject>();
 				final List<EObject> loads = new ArrayList<EObject>();
 				final List<EObject> discharges = new ArrayList<EObject>();
-				
+
 				for (final EObject o : imports) {
-					if (o instanceof Cargo) cargoes.add(o);
-					else if (o instanceof LoadSlot) loads.add(o);
-					else if (o instanceof DischargeSlot) discharges.add(o);
+					if (o instanceof Cargo)
+						cargoes.add(o);
+					else if (o instanceof LoadSlot)
+						loads.add(o);
+					else if (o instanceof DischargeSlot)
+						discharges.add(o);
 				}
-				
+
 				final CompoundCommand mergeAll = new CompoundCommand();
 				mergeAll.append(super.mergeImports(container, containment, cargoes));
 				mergeAll.append(super.mergeImports(container, CargoPackage.eINSTANCE.getCargoModel_LoadSlots(), loads));
@@ -109,5 +105,23 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 			}
 		};
 	}
-	
+
+	@Override
+	protected ScenarioTableViewer constructViewer(final Composite parent) {
+		return new ScenarioTableViewer(parent, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL, getJointModelEditorPart()) {
+
+			@Override
+			protected EObject getElementForNotificationTarget(final EObject source) {
+				if (source instanceof LoadSlot) {
+					return ((LoadSlot) source).getCargo();
+				} else if (source instanceof DischargeSlot) {
+					return ((DischargeSlot) source).getCargo();
+				}
+
+				return super.getElementForNotificationTarget(source);
+			}
+
+		};
+	}
+
 }
