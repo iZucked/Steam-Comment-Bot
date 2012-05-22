c618099cbeb7de3af6a47918a8aa196dd7ae0f8d
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-22 09:24:11 -0100

    Whoops - turns out we do want things from mmxcore

--- com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/impl/DefaultClassImporter.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/impl/DefaultClassImporter.java
@@ -376,7 +376,7 @@ public class DefaultClassImporter implements IClassImporter {
 	}
 
 	protected boolean shouldExportFeature(final EStructuralFeature feature) {
-		return feature.getEContainingClass().getEPackage() != MMXCorePackage.eINSTANCE;
+		return feature == MMXCorePackage.eINSTANCE.getMMXObject_Extensions() || feature == MMXCorePackage.eINSTANCE.getMMXObject_Proxies();
 	}
 
 	protected boolean shouldFlattenReference(final EReference reference) {
