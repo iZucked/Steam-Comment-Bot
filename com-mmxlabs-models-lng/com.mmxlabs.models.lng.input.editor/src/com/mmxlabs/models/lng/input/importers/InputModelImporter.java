ddc7babe3470e09ea55b020e52c1e14ca4c445ce
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-22 08:35:48 -0100

    Add input model importer

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/importers/InputModelImporter.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/importers/InputModelImporter.java
@@ -5,35 +5,46 @@
 package com.mmxlabs.models.lng.input.importers;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EObject;
+
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.models.lng.input.Assignment;
 import com.mmxlabs.models.lng.input.InputFactory;
 import com.mmxlabs.models.lng.input.InputModel;
+import com.mmxlabs.models.lng.input.InputPackage;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.UUIDObject;
+import com.mmxlabs.models.util.Activator;
 import com.mmxlabs.models.util.importer.CSVReader;
+import com.mmxlabs.models.util.importer.IClassImporter;
 import com.mmxlabs.models.util.importer.IImportContext;
 import com.mmxlabs.models.util.importer.ISubmodelImporter;
 
 public class InputModelImporter implements ISubmodelImporter {
-
+	
+	private static final String ASSIGNMENTS = "ASSIGNMENTS";
+	
+	private final IClassImporter importer = Activator.getDefault().getImporterRegistry().getClassImporter(InputPackage.eINSTANCE.getAssignment());
+	
 	@Override
 	public Map<String, String> getRequiredInputs() {
-		return Collections.emptyMap();
+		return CollectionsUtil.makeHashMap(ASSIGNMENTS, "Assignments");
 	}
 
 	@Override
-	public UUIDObject importModel(Map<String, CSVReader> inputs,
-			IImportContext context) {
+	public UUIDObject importModel(final Map<String, CSVReader> inputs, final IImportContext context) {
 		final InputModel input = InputFactory.eINSTANCE.createInputModel();
+		if (inputs.containsKey(ASSIGNMENTS)) {
+			final Collection<EObject> importObjects = importer.importObjects(InputPackage.eINSTANCE.getAssignment(), inputs.get(ASSIGNMENTS), context);
+			input.getAssignments().addAll((Collection<? extends Assignment>) importObjects);
+		}
 		return input;
 	}
 
 	@Override
-	public void exportModel(MMXRootObject root,
-			UUIDObject model, Map<String, Collection<Map<String, String>>> output) {
-		
+	public void exportModel(final MMXRootObject root, final UUIDObject model, final Map<String, Collection<Map<String, String>>> output) {
+		output.put(ASSIGNMENTS, importer.exportObjects(((InputModel) model).getAssignments(), root));
 	}
-
 }
