a5e55f02fc963bb9d29b68e5f214acd719cb6fe3
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-05-25 14:03:00 -0100

    Add a command to fork the current scenario. TODO: determine the naming scheme.
    BugzId: 497

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/ForkScenarioCommandHandler.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scenario.service.ui.commands;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.mmxlabs.scenario.service.IScenarioService;
+import com.mmxlabs.scenario.service.model.Container;
+import com.mmxlabs.scenario.service.model.Folder;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+/**
+ * @author Simon Goodall
+ * 
+ */
+public class ForkScenarioCommandHandler extends AbstractHandler {
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		final IWorkbenchPage activePage = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+
+		final ISelection selection = activePage.getSelection();
+		if (selection instanceof IStructuredSelection) {
+			final IStructuredSelection strucSelection = (IStructuredSelection) selection;
+			final Iterator<?> itr = strucSelection.iterator();
+			while (itr.hasNext()) {
+				final Object element = itr.next();
+
+				if (element instanceof ScenarioInstance) {
+					final ScenarioInstance instance = (ScenarioInstance) element;
+
+					final IScenarioService scenarioService = instance.getScenarioService();
+
+					try {
+						final ScenarioInstance fork = scenarioService.duplicate(instance, instance);
+						final DateFormat df = DateFormat.getDateTimeInstance();
+
+						final Set<String> existingNames = new HashSet<String>();
+						for (final Container c : instance.getElements()) {
+							if (c instanceof Folder) {
+								existingNames.add(((Folder) c).getName());
+							} else if (c instanceof ScenarioInstance) {
+								existingNames.add(((ScenarioInstance) c).getName());
+							}
+						}
+
+						final String namePrefix = "Fork-" +df.format(new Date());
+						String newName = namePrefix;
+						int counter = 1;
+						while (existingNames.contains(newName)) {
+							newName = namePrefix + " (" + counter++ + ")";
+						}
+
+						fork.setName(newName);
+					} catch (final IOException e) {
+						throw new ExecutionException("Unable to fork scenario", e);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+}
