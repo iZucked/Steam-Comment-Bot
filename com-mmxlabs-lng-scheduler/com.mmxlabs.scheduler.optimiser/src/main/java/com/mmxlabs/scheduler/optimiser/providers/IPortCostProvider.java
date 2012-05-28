7f266d3e63b2b96f374dbded79e880e2275af1d3
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-28 15:41:48 -0100

    Add port cost DCP

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/providers/IPortCostProvider.java
@@ -0,0 +1,25 @@
+package com.mmxlabs.scheduler.optimiser.providers;
+
+import com.mmxlabs.optimiser.core.scenario.IDataComponentProvider;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+
+/**
+ * DCP which gives you the cost of visiting a given port in a given vessel class at a given time
+ * to perform a given activity
+ * 
+ * @author hinton
+ *
+ */
+public interface IPortCostProvider extends IDataComponentProvider {
+	/**
+	 * Returns the cost (in scaled dollars) of visiting the given port in the given vessel
+	 * to do an activity of the given type
+	 * 
+	 * @param port
+	 * @param vessel
+	 * @param portType
+	 * @return
+	 */
+	public long getPortCost(final IPort port, final IVessel vessel, final PortType portType);
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/providers/impl/indexed/IndexedPortCostEditor.java
@@ -0,0 +1,67 @@
+package com.mmxlabs.scheduler.optimiser.providers.impl.indexed;
+
+import com.mmxlabs.common.impl.LongFastEnumMap;
+import com.mmxlabs.common.indexedobjects.IIndexMap;
+import com.mmxlabs.common.indexedobjects.impl.ArrayIndexMap;
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+import com.mmxlabs.scheduler.optimiser.providers.IPortCostProviderEditor;
+import com.mmxlabs.scheduler.optimiser.providers.PortType;
+
+public class IndexedPortCostEditor implements IPortCostProviderEditor {
+	final String name;
+	
+	class PortCosts {
+		private final IIndexMap<IVessel, LongFastEnumMap<PortType>> contents = 
+				new ArrayIndexMap<IVessel, LongFastEnumMap<PortType>>();
+		
+		public void setPortCost(final IVessel vessel, final PortType type, final long cost) {
+			LongFastEnumMap<PortType> x = contents.maybeGet(vessel);
+			if (x != null) {
+				x = new LongFastEnumMap<PortType>(PortType.values().length);
+				contents.set(vessel, x);
+			}
+			x.put(type, cost);
+		}
+		
+		public long getPortCost(final IVessel vessel, final PortType type) {
+			LongFastEnumMap<PortType> maybeGet = contents.maybeGet(vessel);
+			if (maybeGet == null) return 0;
+			return maybeGet.get(type);
+		}
+	}
+	
+	final IIndexMap<IPort, PortCosts> content = new ArrayIndexMap<IPort, PortCosts>();
+	
+	public IndexedPortCostEditor(final String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public long getPortCost(final IPort port, final IVessel vessel, final PortType portType) {
+		final PortCosts pc = content.maybeGet(port);
+		if (pc == null) return 0;
+		return pc.getPortCost(vessel, portType);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void dispose() {
+		content.clear();
+	}
+
+	@Override
+	public void setPortCost(final IPort port, final IVessel vessel, final PortType type, final long cost) {
+		PortCosts pc = content.maybeGet(port);
+		if (pc == null) {
+			pc = new PortCosts();
+			content.set(port, pc);
+		}
+		pc.setPortCost(vessel, type, cost);
+	}
+}
