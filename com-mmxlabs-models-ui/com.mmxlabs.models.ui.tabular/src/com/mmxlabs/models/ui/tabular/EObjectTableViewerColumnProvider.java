11c8e69137f6dd803b5c80d389a10cbdfe54444a
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-05-28 11:24:02 -0100

    Extract column viewer into top level class.

---
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/EObjectTableViewerColumnProvider.java
@@ -0,0 +1,109 @@
+package com.mmxlabs.models.ui.tabular;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+
+import com.mmxlabs.models.util.emfpath.EMFPath;
+
+public class EObjectTableViewerColumnProvider extends ColumnLabelProvider {
+	/**
+	 * 
+	 */
+	private final EObjectTableViewer eObjectTableViewer;
+	private final ICellRenderer renderer;
+	private final EMFPath path;
+	private final Color errorColour = new Color(Display.getDefault(), new RGB(255, 100, 100));
+	private final Color warningColour = new Color(Display.getDefault(), new RGB(255, 242, 0));
+	private final Color lockedColour = new Color(Display.getDefault(), new RGB(100, 100, 100));
+
+	public EObjectTableViewerColumnProvider(EObjectTableViewer eObjectTableViewer, ICellRenderer renderer, EMFPath path) {
+		this.eObjectTableViewer = eObjectTableViewer;
+		this.renderer = renderer;
+		this.path = path;
+	}
+
+	@Override
+	public Color getForeground(final Object element) {
+
+		if (this.eObjectTableViewer.lockedForEditing) {
+			return lockedColour;
+		}
+
+		if (this.eObjectTableViewer.delegateColourProvider != null) {
+			final Color colour = this.eObjectTableViewer.delegateColourProvider.getForeground(element);
+			if (colour != null) {
+				return colour;
+			}
+		}
+
+		return super.getForeground(element);
+	}
+
+	@Override
+	public Color getBackground(final Object element) {
+		if (this.eObjectTableViewer.validationErrors.containsKey(element)) {
+			final IStatus s = this.eObjectTableViewer.validationErrors.get(element);
+			if (s.getSeverity() == IStatus.ERROR) {
+				return errorColour;
+			} else if (s.getSeverity() == IStatus.WARNING) {
+				return warningColour;
+			}
+		}
+
+		if (this.eObjectTableViewer.delegateColourProvider != null) {
+			final Color colour = this.eObjectTableViewer.delegateColourProvider.getBackground(element);
+			if (colour != null) {
+				return colour;
+			}
+		}
+
+		return super.getBackground(element);
+	}
+
+	@Override
+	public String getText(final Object element) {
+		return renderer.render(path.get((EObject) element));
+	}
+
+	@Override
+	public String getToolTipText(final Object element) {
+		if (this.eObjectTableViewer.validationErrors.containsKey(element)) {
+			final IStatus s = this.eObjectTableViewer.validationErrors.get(element);
+			if (!s.isOK()) {
+				return getMessages(s);
+			}
+		}
+		return super.getToolTipText(element);
+	}
+
+	/**
+	 * Extract message hierarchy and construct the tool tip message.
+	 * 
+	 * @param status
+	 * @return
+	 */
+	private String getMessages(final IStatus status) {
+		if (status.isMultiStatus()) {
+			final StringBuilder sb = new StringBuilder();
+			for (final IStatus s : status.getChildren()) {
+				sb.append(getMessages(s));
+				sb.append("\n");
+			}
+			return sb.toString();
+		} else {
+			return status.getMessage();
+		}
+	}
+
+	@Override
+	public void dispose() {
+		errorColour.dispose();
+		warningColour.dispose();
+		lockedColour.dispose();
+		super.dispose();
+	}
+}
