b9cf5b4f472a8f8065c55834785096d3fa790a25
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-29 09:41:56 -0100

    Add command provider which re-inserts any event whose dates are changed.
    Note that this won't work with the dialog editor at the moment; need to tell the command provider command stack which providers to disable.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/commands/TimeSortingCommandProvider.java
@@ -0,0 +1,71 @@
+package com.mmxlabs.models.lng.input.commands;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.CommandParameter;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.LoadSlot;
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.VesselEvent;
+import com.mmxlabs.models.lng.input.Assignment;
+import com.mmxlabs.models.lng.input.InputModel;
+import com.mmxlabs.models.lng.input.editor.utils.AssignmentEditorHelper;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.mmxcore.UUIDObject;
+import com.mmxlabs.models.ui.commandservice.IModelCommandProvider;
+
+public class TimeSortingCommandProvider implements IModelCommandProvider {
+	@Override
+	public Command provideAdditionalCommand(EditingDomain editingDomain,MMXRootObject rootObject, Class<? extends Command> commandClass,CommandParameter parameter, Command input) {
+		if (commandClass == SetCommand.class) {
+			if (parameter.getEAttribute() == CargoPackage.eINSTANCE.getSlot_WindowStart()) {
+				final EObject o = parameter.getEOwner();
+				if (o instanceof LoadSlot) {
+					final Cargo c = ((LoadSlot) o).getCargo();
+					return fixTask(c, editingDomain, rootObject);
+				} else if (o instanceof DischargeSlot) {
+					final Cargo c = ((DischargeSlot) o).getCargo();
+					return fixTask(c, editingDomain, rootObject);
+				}
+			} else if (parameter.getEAttribute() == FleetPackage.eINSTANCE.getVesselEvent_StartAfter() 
+					|| parameter.getEAttribute() == FleetPackage.eINSTANCE.getVesselEvent_StartBy()
+					|| parameter.getEAttribute() == FleetPackage.eINSTANCE.getVesselEvent_DurationInDays()) {
+				final EObject o = parameter.getEOwner();
+				if (o instanceof VesselEvent) {
+					return fixTask((VesselEvent) o, editingDomain, rootObject);
+				}
+			}
+		}
+		return null;
+	}
+
+
+
+	private Command fixTask(UUIDObject o, EditingDomain editingDomain,
+			MMXRootObject rootObject) {
+		final InputModel input = rootObject.getSubModel(InputModel.class);
+		if (input != null) {
+			final Assignment a = AssignmentEditorHelper.getAssignmentForTask(input, o);
+			if (a != null) return AssignmentEditorHelper.taskReassigned(editingDomain, input, o, null, null, null, a);
+		}
+		return null;
+	}
+
+	@Override
+	public void startCommandProvision() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endCommandProvision() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
