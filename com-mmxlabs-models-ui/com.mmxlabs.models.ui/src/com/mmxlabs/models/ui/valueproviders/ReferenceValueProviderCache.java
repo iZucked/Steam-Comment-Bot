9f096fd00d2df5256a4bc6a38aa86720805c628f
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-05-30 20:28:56 -0100

    add log warning

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/valueproviders/ReferenceValueProviderCache.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/valueproviders/ReferenceValueProviderCache.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mmxlabs.common.Pair;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
@@ -27,6 +29,8 @@ public class ReferenceValueProviderCache implements IReferenceValueProviderProvi
 	private final MMXRootObject rootObject;
 	private final HashMap<Pair<EClass, EReference>, IReferenceValueProvider> cache = 
 			new HashMap<Pair<EClass, EReference>, IReferenceValueProvider>();
+	
+	private static final Logger log = LoggerFactory.getLogger(ReferenceValueProviderCache.class);
 	
 	public ReferenceValueProviderCache(final MMXRootObject root, final IReferenceValueProviderFactoryRegistry registry) {
 		this.registry = registry;
@@ -49,6 +53,9 @@ public class ReferenceValueProviderCache implements IReferenceValueProviderProvi
 		
 		final IReferenceValueProviderFactory factory = registry.getValueProviderFactory(owner, reference);
 		final IReferenceValueProvider provider = factory == null ? null : factory.createReferenceValueProvider(owner, reference, rootObject);
+		if (provider == null) {
+			log.warn("Could not find reference provider for " + owner.getName() + "." + reference.getName());
+		}
 		cache.put(p, provider);
 		
 		return provider;
