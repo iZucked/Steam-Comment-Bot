5be8ef3a6f877ead01688aa751be70d964a8169e
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-01 15:43:02 -0100

    Delete assignments when vessels are deleted

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/commands/AssignmentDeletingCommandProvider.java
@@ -0,0 +1,35 @@
+package com.mmxlabs.models.lng.input.commands;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+
+import com.mmxlabs.models.lng.input.Assignment;
+import com.mmxlabs.models.lng.input.InputModel;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.commandservice.BaseModelCommandProvider;
+
+public class AssignmentDeletingCommandProvider extends BaseModelCommandProvider {
+
+	@Override
+	protected Command handleDeletion(EditingDomain editingDomain,
+			MMXRootObject rootObject, Collection<Object> deleted) {
+		final InputModel im = rootObject.getSubModel(InputModel.class);
+		if (im != null) {
+			final CompoundCommand cc = new CompoundCommand();
+			boolean hasDeletedSomething = false;
+			for (final Assignment a : im.getAssignments()) {
+				if (deleted.containsAll(a.getVessels())) {
+					cc.append(DeleteCommand.create(editingDomain, a));
+					hasDeletedSomething = true;
+				}
+			}
+			if (hasDeletedSomething) return cc;
+		}
+		return null;
+	}
+	
+}
