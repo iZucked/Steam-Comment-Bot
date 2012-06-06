32cbbc90094deb7b9bb955a78fead4d8e583a28f
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-06 14:36:21 -0100

    Show other names editor

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/mmxcore/NamedObjectComponentHelper.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/mmxcore/NamedObjectComponentHelper.java
@@ -4,12 +4,16 @@
  */
 package com.mmxlabs.models.ui.mmxcore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EClass;
 
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
 import com.mmxlabs.models.ui.BaseComponentHelper;
 import com.mmxlabs.models.ui.ComponentHelperUtils;
 import com.mmxlabs.models.ui.IInlineEditorContainer;
+import com.mmxlabs.models.ui.editors.impl.TextInlineEditor;
 
 public class NamedObjectComponentHelper extends BaseComponentHelper {
 	@Override
@@ -21,5 +25,41 @@ public class NamedObjectComponentHelper extends BaseComponentHelper {
 	public void addEditorsToComposite(IInlineEditorContainer detailComposite,
 			EClass displayedClass) {
 		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(displayedClass, MMXCorePackage.eINSTANCE.getNamedObject_Name()));
+		detailComposite.addInlineEditor(new TextInlineEditor(MMXCorePackage.eINSTANCE.getNamedObject_OtherNames()) {
+			@Override
+			protected synchronized void doSetValue(Object value,
+					boolean forceCommandExecution) {
+				if (value == null) return;
+				final String valueString = "" + value;
+				final String[] values = valueString.split(",");
+				final List<String> valueList = new ArrayList<String>(values.length);
+				for (final String s : values) {
+					final String s2 = s.trim();
+					if (s2.isEmpty()) continue;
+					valueList.add(s2);
+				}
+				super.doSetValue(valueList, forceCommandExecution);
+			}
+
+			@Override
+			protected void updateValueDisplay(Object value) {
+				final StringBuilder sb = new StringBuilder();
+				
+				if (value instanceof List) {
+					boolean c = false;
+					for (final Object o : (List) value) {
+						if (c) {
+							sb.append(", ");
+						} else {
+							c = true;
+						}
+						sb.append(o);
+					}
+					super.updateValueDisplay(sb.toString());
+				} else {
+					super.updateValueDisplay(value);
+				}
+			}
+		});
 	}
 }
