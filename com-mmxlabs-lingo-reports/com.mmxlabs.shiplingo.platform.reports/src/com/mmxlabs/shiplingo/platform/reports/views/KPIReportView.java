1792c67436a86d48ca7b04fd2c1abe8309ed6163
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-07 08:23:49 -0100

    Add new KPI report

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.reports/src/com/mmxlabs/shiplingo/platform/reports/views/KPIReportView.java
@@ -0,0 +1,391 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.shiplingo.platform.reports.views;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.IFontProvider;
+import org.eclipse.jface.viewers.IOpenListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.OpenEvent;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.part.ViewPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mmxlabs.models.lng.schedule.Schedule;
+import com.mmxlabs.rcp.common.actions.CopyTableToClipboardAction;
+import com.mmxlabs.rcp.common.actions.PackTableColumnsAction;
+import com.mmxlabs.shiplingo.platform.reports.IScenarioViewerSynchronizerOutput;
+import com.mmxlabs.shiplingo.platform.reports.ScenarioViewerSynchronizer;
+import com.mmxlabs.shiplingo.platform.reports.ScheduleElementCollector;
+import com.mmxlabs.shiplingo.platform.reports.views.KPIContentProvider.RowData;
+
+public class KPIReportView extends ViewPart {
+
+	private static final Logger log = LoggerFactory.getLogger(KPIReportView.class);
+
+	private final ArrayList<Integer> sortColumns = new ArrayList<Integer>(4);
+
+	private boolean inverseSort = false;
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "com.mmxlabs.shiplingo.platform.reports.views.KPIReportView";
+
+	private TableViewer viewer;
+
+	private Action packColumnsAction;
+
+	private Action copyTableAction;
+
+	private ScenarioViewerSynchronizer jobManagerListener;
+
+	static class ViewLabelProvider extends CellLabelProvider implements ITableLabelProvider, IFontProvider {
+
+		private final Font boldFont;
+
+		public ViewLabelProvider() {
+			final Font systemFont = Display.getDefault().getSystemFont();
+			// Clone the font data
+			final FontData fd = new FontData(systemFont.getFontData()[0].toString());
+			// Set the bold bit.
+			fd.setStyle(fd.getStyle() | SWT.BOLD);
+			boldFont = new Font(Display.getDefault(), fd);
+		}
+
+		@Override
+		public void dispose() {
+			boldFont.dispose();
+			super.dispose();
+		}
+
+		@Override
+		public String getColumnText(final Object obj, final int index) {
+
+			if (obj instanceof RowData) {
+				final RowData d = (RowData) obj;
+				switch (index) {
+				case 0:
+					return d.scheduleName;
+				case 1:
+					return d.component;
+				case 2:
+					if (KPIContentProvider.TYPE_TIME.equals(d.type)) {
+						final long days = d.value / 24;
+						final long hours = d.value % 24;
+						return "" + days + "d, " + hours + "h";
+					} else if (KPIContentProvider.TYPE_COST.equals(d.type)) {
+						return String.format("$%,d", d.value);
+					} else {
+						return String.format("%,d", d.value);
+					}
+				}
+			}
+			return "";
+		}
+
+		@Override
+		public Image getColumnImage(final Object obj, final int index) {
+			return null;
+		}
+
+		@Override
+		public void update(final ViewerCell cell) {
+
+		}
+
+		@Override
+		public Font getFont(final Object element) {
+			if (element instanceof RowData) {
+				final RowData d = (RowData) element;
+				if ("Total Cost".equals(d.component)) {
+					return boldFont;
+				}
+			}
+
+			return null;
+		}
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public KPIReportView() {
+	}
+
+	private void addSortSelectionListener(final TableColumn column, final int value) {
+		column.addSelectionListener(new SelectionAdapter() {
+			{
+				final SelectionAdapter self = this;
+				column.addDisposeListener(new DisposeListener() {
+					@Override
+					public void widgetDisposed(final DisposeEvent e) {
+						column.removeSelectionListener(self);
+					}
+				});
+			}
+
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				setSortColumn(column, value);
+			}
+		});
+	}
+
+	protected void setSortColumn(final TableColumn column, final int value) {
+		if (sortColumns.get(0) == value) {
+			inverseSort = !inverseSort;
+		} else {
+			inverseSort = false;
+			sortColumns.remove((Object) value);
+			sortColumns.add(0, value);
+		}
+
+		viewer.getTable().setSortColumn(column);
+		viewer.getTable().setSortDirection(inverseSort ? SWT.DOWN : SWT.UP);
+
+		viewer.refresh();
+	}
+
+	/**
+	 * This is a callback that will allow us to create the viewer and initialize it.
+	 */
+	@Override
+	public void createPartControl(final Composite parent) {
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION) {
+			@Override
+			protected void inputChanged(final Object input, final Object oldInput) {
+				super.inputChanged(input, oldInput);
+
+				final boolean inputEmpty = (input == null) || ((input instanceof IScenarioViewerSynchronizerOutput) && ((IScenarioViewerSynchronizerOutput) input).getCollectedElements().isEmpty());
+				final boolean oldInputEmpty = (oldInput == null)
+						|| ((oldInput instanceof IScenarioViewerSynchronizerOutput) && ((IScenarioViewerSynchronizerOutput) oldInput).getCollectedElements().isEmpty());
+
+				if (inputEmpty != oldInputEmpty) {
+
+					if (packColumnsAction != null) {
+						packColumnsAction.run();
+					}
+				}
+			};
+		};
+		viewer.setContentProvider(new KPIContentProvider());
+		viewer.setInput(getViewSite());
+
+		final TableViewerColumn tvc0 = new TableViewerColumn(viewer, SWT.NONE);
+		tvc0.getColumn().setText("Schedule");
+		tvc0.getColumn().pack();
+		addSortSelectionListener(tvc0.getColumn(), 0);
+
+		final TableViewerColumn tvc1 = new TableViewerColumn(viewer, SWT.NONE);
+		tvc1.getColumn().setText("KPI");
+		tvc1.getColumn().pack();
+		addSortSelectionListener(tvc1.getColumn(), 1);
+
+		final TableViewerColumn tvc2 = new TableViewerColumn(viewer, SWT.NONE);
+		tvc2.getColumn().setText("Value");
+		tvc2.getColumn().pack();
+		addSortSelectionListener(tvc2.getColumn(), 2);
+
+		viewer.setLabelProvider(new ViewLabelProvider());
+
+		viewer.getTable().setLinesVisible(true);
+		viewer.getTable().setHeaderVisible(true);
+
+		sortColumns.add(0);
+		sortColumns.add(2);
+		sortColumns.add(3);
+		sortColumns.add(1);
+
+		viewer.getTable().setSortColumn(tvc0.getColumn());
+		viewer.getTable().setSortDirection(SWT.UP);
+
+		viewer.setComparator(new ViewerComparator() {
+			@Override
+			public int compare(final Viewer viewer, final Object e1, final Object e2) {
+				final RowData r1 = (RowData) e1;
+				final RowData r2 = (RowData) e2;
+
+				final int d = inverseSort ? -1 : 1;
+				int sort = 0;
+				final Iterator<Integer> iterator = sortColumns.iterator();
+				while (iterator.hasNext() && (sort == 0)) {
+					switch (iterator.next()) {
+					case 0:
+						sort = r1.scheduleName.compareTo(r2.scheduleName);
+						break;
+					case 1:
+						sort = r1.component.compareTo(r2.component);
+						break;
+					case 2:
+						sort = ((Long) r1.value).compareTo(r2.value);
+						break;
+					}
+				}
+
+				return d * sort;
+			}
+		});
+
+		// Create the help context id for the viewer's control
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "com.mmxlabs.demo.reports.KPIReportView");
+		makeActions();
+		hookContextMenu();
+		contributeToActionBars();
+
+		jobManagerListener = ScenarioViewerSynchronizer.registerView(viewer, new ScheduleElementCollector() {
+
+			@Override
+			protected Collection<? extends Object> collectElements(final Schedule schedule) {
+				return Collections.singleton(schedule);
+			}
+		});
+
+		viewer.addOpenListener(new IOpenListener() {
+
+			@Override
+			public void open(final OpenEvent event) {
+				if (event.getSelection() instanceof IStructuredSelection) {
+					final IStructuredSelection structuredSelection = (IStructuredSelection) event.getSelection();
+					if (structuredSelection.size() == 1) {
+						final Object obj = structuredSelection.getFirstElement();
+						if (obj instanceof RowData) {
+							final RowData rowData = (RowData) obj;
+							final String viewId = rowData.viewID;
+							if (viewId != null) {
+								try {
+									getSite().getPage().showView(viewId);
+								} catch (final PartInitException e) {
+									log.error(e.getMessage(), e);
+								}
+							}
+
+						}
+					}
+				}
+			}
+		});
+	}
+
+	private void hookContextMenu() {
+		final MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			@Override
+			public void menuAboutToShow(final IMenuManager manager) {
+				KPIReportView.this.fillContextMenu(manager);
+			}
+		});
+		final Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+	}
+
+	private void contributeToActionBars() {
+		final IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(final IMenuManager manager) {
+		manager.add(new Separator());
+	}
+
+	private void fillContextMenu(final IMenuManager manager) {
+		// Other plug-ins can contribute there actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+
+	private void fillLocalToolBar(final IToolBarManager manager) {
+		manager.add(new GroupMarker("pack"));
+		manager.add(new GroupMarker("additions"));
+		manager.add(new GroupMarker("edit"));
+		manager.add(new GroupMarker("copy"));
+		manager.add(new GroupMarker("importers"));
+		manager.add(new GroupMarker("exporters"));
+
+		manager.appendToGroup("pack", packColumnsAction);
+		manager.appendToGroup("copy", copyTableAction);
+	}
+
+	private void makeActions() {
+		packColumnsAction = new PackTableColumnsAction(viewer);
+		copyTableAction = new CopyTableToClipboardAction(viewer.getTable());
+		getViewSite().getActionBars().setGlobalActionHandler(ActionFactory.COPY.getId(), copyTableAction);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	@Override
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+
+	public void refresh() {
+		getSite().getShell().getDisplay().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!viewer.getControl().isDisposed()) {
+					viewer.refresh();
+				}
+			}
+		});
+	}
+
+	public void setInput(final Object input) {
+		getSite().getShell().getDisplay().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!viewer.getControl().isDisposed()) {
+					viewer.setInput(input);
+				}
+			}
+		});
+	}
+
+	@Override
+	public void dispose() {
+
+		ScenarioViewerSynchronizer.deregisterView(jobManagerListener);
+		super.dispose();
+	}
+}
