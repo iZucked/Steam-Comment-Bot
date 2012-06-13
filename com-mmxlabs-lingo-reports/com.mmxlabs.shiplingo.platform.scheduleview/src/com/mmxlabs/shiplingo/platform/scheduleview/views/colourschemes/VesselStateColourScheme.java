69764460450cd77a29dc5f4d08275d30a52f3bc5
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-13 19:21:00 -0100

    Implemented some bits to hightlight a border around locked cargoes. TODO: Get hold of the input model to make this work
    BugzId :564

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/shiplingo/platform/scheduleview/views/colourschemes/VesselStateColourScheme.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/shiplingo/platform/scheduleview/views/colourschemes/VesselStateColourScheme.java
@@ -4,12 +4,24 @@
  */
 package com.mmxlabs.shiplingo.platform.scheduleview.views.colourschemes;
 
+import java.util.Collection;
+
 import org.eclipse.nebula.widgets.ganttchart.ColorCache;
 import org.eclipse.swt.graphics.Color;
 
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.input.Assignment;
+import com.mmxlabs.models.lng.input.InputModel;
+import com.mmxlabs.models.lng.schedule.CargoAllocation;
+import com.mmxlabs.models.lng.schedule.Event;
 import com.mmxlabs.models.lng.schedule.Journey;
+import com.mmxlabs.models.lng.schedule.Sequence;
+import com.mmxlabs.models.lng.schedule.SlotAllocation;
 import com.mmxlabs.models.lng.schedule.SlotVisit;
 import com.mmxlabs.models.lng.schedule.VesselEventVisit;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.shiplingo.platform.reports.IScenarioViewerSynchronizerOutput;
 import com.mmxlabs.shiplingo.platform.scheduleview.views.IScheduleViewColourScheme;
 
 public class VesselStateColourScheme implements IScheduleViewColourScheme {
@@ -60,6 +72,46 @@ public class VesselStateColourScheme implements IScheduleViewColourScheme {
 
 	@Override
 	public Color getBorderColour(final Object element) {
+
+		if (element instanceof Event) {
+			final Event event = (Event) element;
+
+			// Stage 1: Find the cargo
+			final Sequence sequence = (Sequence) event.eContainer();
+			int index = sequence.getEvents().indexOf(event);
+			Cargo cargo = null;
+			while (cargo == null && index != -1) {
+				if (event instanceof SlotVisit) {
+					final SlotVisit slotVisit = ((SlotVisit) event);
+					final SlotAllocation slotAllocation = slotVisit.getSlotAllocation();
+					final CargoAllocation cargoAllocation = slotAllocation.getCargoAllocation();
+					cargo = cargoAllocation.getInputCargo();
+				}
+				--index;
+			}
+
+			// Stage 2: Find the input assignment
+			if (cargo != null) {
+
+				// TODO: Get the input model - no access to viewer here?
+				final Object input = null;// viewer.getInput();
+				if (input instanceof IScenarioViewerSynchronizerOutput) {
+					final IScenarioViewerSynchronizerOutput output = (IScenarioViewerSynchronizerOutput) input;
+
+					final Collection<Object> collectedElements = output.getCollectedElements();
+					if (collectedElements.size() > 1) {
+						final ScenarioInstance instance = output.getScenarioInstance(sequence.eContainer());
+						final MMXRootObject rootObject = (MMXRootObject) instance.getInstance();
+						final InputModel inputModel = rootObject.getSubModel(InputModel.class);
+						for (final Assignment assignment : inputModel.getAssignments()) {
+							if (assignment.getAssignedObjects().contains(cargo)) {
+								return ColorCache.getWhite();
+							}
+						}
+					}
+				}
+			}
+		}
 		return null;
 	}
 }
