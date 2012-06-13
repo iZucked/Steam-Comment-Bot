11aaa5c4a719e9e9393b63f938589cd18f740ed1
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-13 19:12:14 -0100

    Add editor actions to handle optimisation commands.
    BugzId: 504

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/shiplingo/platform/models/optimisation/navigator/handlers/editor/EvaluateOptimisationEditorActionDelegate.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.shiplingo.platform.models.optimisation.navigator.handlers.editor;
+
+public class EvaluateOptimisationEditorActionDelegate extends StartOptimisationEditorActionDelegate {
+	public EvaluateOptimisationEditorActionDelegate() {
+		super(false);
+	}
+}
