db70db52f785b6f4a12e1a90b155801cc6c7a793
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-13 10:40:41 -0100

    Suppress API warnings

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/AlternatingRowCellRenderer.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/AlternatingRowCellRenderer.java
@@ -25,6 +25,7 @@ import org.eclipse.swt.graphics.TextLayout;
  * @author chris.gross@us.ibm.com
  * @since 2.0.0
  */
+@SuppressWarnings("restriction")
 public class AlternatingRowCellRenderer extends GridCellRenderer {
 
 	int leftMargin = 4;
