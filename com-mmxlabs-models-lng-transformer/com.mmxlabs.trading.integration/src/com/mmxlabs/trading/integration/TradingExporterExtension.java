bbde5dba40522c287af6baa383fa506511c8a636
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-18 09:33:02 -0100

    Fix up for API changes TODO: Fix up export

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/TradingExporterExtension.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/TradingExporterExtension.java
@@ -8,23 +8,16 @@ import org.eclipse.emf.common.util.EList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import scenario.Scenario;
-import scenario.cargo.Slot;
-import scenario.contract.Entity;
-import scenario.fleet.VesselEvent;
-import scenario.schedule.BookedRevenue;
-import scenario.schedule.CargoAllocation;
-import scenario.schedule.CargoRevenue;
-import scenario.schedule.Schedule;
-import scenario.schedule.ScheduleFactory;
-import scenario.schedule.Sequence;
-import scenario.schedule.VesselEventRevenue;
-import scenario.schedule.events.ScheduledEvent;
-import scenario.schedule.events.VesselEventVisit;
-
-import com.mmxlabs.lngscheduler.emf.extras.ModelEntityMap;
-import com.mmxlabs.lngscheduler.emf.extras.export.DetailTreeExporter;
-import com.mmxlabs.lngscheduler.emf.extras.export.IExporterExtension;
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.schedule.AdditionalData;
+import com.mmxlabs.models.lng.schedule.CargoAllocation;
+import com.mmxlabs.models.lng.schedule.Schedule;
+import com.mmxlabs.models.lng.schedule.ScheduleFactory;
+import com.mmxlabs.models.lng.schedule.Sequence;
+import com.mmxlabs.models.lng.schedule.VesselEventVisit;
+import com.mmxlabs.models.lng.transformer.ModelEntityMap;
+import com.mmxlabs.models.lng.transformer.export.IExporterExtension;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.optimiser.core.IAnnotatedSolution;
 import com.mmxlabs.optimiser.core.ISequenceElement;
 import com.mmxlabs.scheduler.optimiser.Calculator;
@@ -32,10 +25,12 @@ import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
 import com.mmxlabs.scheduler.optimiser.components.ILoadSlot;
 import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
 import com.mmxlabs.scheduler.optimiser.components.IVesselEventPortSlot;
+import com.mmxlabs.scheduler.optimiser.components.impl.VesselEvent;
 import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProvider;
 import com.mmxlabs.trading.optimiser.TradingConstants;
 import com.mmxlabs.trading.optimiser.annotations.IProfitAndLossAnnotation;
 import com.mmxlabs.trading.optimiser.annotations.IProfitAndLossEntry;
+import com.mmxlabs.trading.optimiser.impl.Entity;
 
 /**
  * EMF export side for trading optimiser information. Model may need reworking, so this isn't exactly final.
@@ -45,14 +40,14 @@ import com.mmxlabs.trading.optimiser.annotations.IProfitAndLossEntry;
  */
 public class TradingExporterExtension implements IExporterExtension {
 	private final static Logger log = LoggerFactory.getLogger(TradingExporterExtension.class);
-	private Scenario scenario;
+	private MMXRootObject inputScenario;
 	private ModelEntityMap entities;
 	private IAnnotatedSolution annotatedSolution;
 	private Schedule outputSchedule;
 
 	@Override
-	public void startExporting(Scenario inputScenario, Schedule outputSchedule, ModelEntityMap entities, IAnnotatedSolution annotatedSolution) {
-		this.scenario = inputScenario;
+	public void startExporting(MMXRootObject inputScenario, Schedule outputSchedule, ModelEntityMap entities, IAnnotatedSolution annotatedSolution) {
+		this.inputScenario = inputScenario;
 		this.entities = entities;
 		this.annotatedSolution = annotatedSolution;
 		this.outputSchedule = outputSchedule;
@@ -60,79 +55,81 @@ public class TradingExporterExtension implements IExporterExtension {
 
 	@Override
 	public void finishExporting() {
-		final EList<BookedRevenue> revenues = outputSchedule.getRevenue();
-		final IPortSlotProvider slotProvider = annotatedSolution.getContext().getOptimisationData()
-				.getDataComponentProvider(SchedulerConstants.DCP_portSlotsProvider, IPortSlotProvider.class);
-		for (final ISequenceElement element : annotatedSolution.getContext().getOptimisationData().getSequenceElements()) {
-			final IProfitAndLossAnnotation profitAndLoss = annotatedSolution.getElementAnnotations().getAnnotation(element, TradingConstants.AI_profitAndLoss, IProfitAndLossAnnotation.class);
-			if (profitAndLoss != null) {
-				// emit p&l entry - depends on the type of slot associated with the element.
-				final IPortSlot slot = slotProvider.getPortSlot(element);
-
-				if (slot instanceof ILoadSlot) {
-					final Slot modelSlot = entities.getModelObject(slot, Slot.class);
-					CargoAllocation cargoAllocation = null;
-					for (final CargoAllocation allocation : outputSchedule.getCargoAllocations()) {
-						if (allocation.getLoadSlot() == modelSlot || allocation.getDischargeSlot() == slot) {
-							cargoAllocation = allocation;
-							break;
-						}
-					}
-					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
-						final CargoRevenue revenue = ScheduleFactory.eINSTANCE.createCargoRevenue();
-						revenue.setCargo(cargoAllocation);
-						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
-						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
-						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
-						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
-						revenues.add(revenue);
-						if (cargoAllocation.getLoadRevenue() == null) {
-							cargoAllocation.setLoadRevenue(revenue);
-						} else if (cargoAllocation.getShippingRevenue() == null) {
-							cargoAllocation.setShippingRevenue(revenue);
-						} else if (cargoAllocation.getDischargeRevenue() == null) {
-							cargoAllocation.setDischargeRevenue(revenue);
-						}
-					}
-				} else if (slot instanceof IVesselEventPortSlot) {
-					final VesselEvent modelEvent = entities.getModelObject(slot, VesselEvent.class);
-					VesselEventVisit visit = null;
-
-					for (final Sequence sequence : outputSchedule.getSequences()) {
-						for (final ScheduledEvent event : sequence.getEvents()) {
-							if (event instanceof VesselEventVisit) {
-								if (((VesselEventVisit) event).getVesselEvent() == modelEvent) {
-									visit = (VesselEventVisit) event;
-								}
-							}
-						}
-					}
-
-					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
-						final VesselEventRevenue revenue = ScheduleFactory.eINSTANCE.createVesselEventRevenue();
-						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
-						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
-						revenue.setVesselEventVisit(visit);
-						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
-						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
-						revenues.add(revenue);
-						visit.setRevenue(revenue);
-					}
-				} else {
-					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
-						final BookedRevenue revenue = ScheduleFactory.eINSTANCE.createBookedRevenue();
-						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
-						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
-						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
-						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
-						revenues.add(revenue);
-					}
-				}
-			}
-		}
+//		final EList<BookedRevenue> revenues = outputSchedule.getRevenue();
+//		final IPortSlotProvider slotProvider = annotatedSolution.getContext().getOptimisationData()
+//				.getDataComponentProvider(SchedulerConstants.DCP_portSlotsProvider, IPortSlotProvider.class);
+//		for (final ISequenceElement element : annotatedSolution.getContext().getOptimisationData().getSequenceElements()) {
+//			final IProfitAndLossAnnotation profitAndLoss = annotatedSolution.getElementAnnotations().getAnnotation(element, TradingConstants.AI_profitAndLoss, IProfitAndLossAnnotation.class);
+//			if (profitAndLoss != null) {
+//				// emit p&l entry - depends on the type of slot associated with the element.
+//				final IPortSlot slot = slotProvider.getPortSlot(element);
+//
+//				if (slot instanceof ILoadSlot) {
+//					final Slot modelSlot = entities.getModelObject(slot, Slot.class);
+//					CargoAllocation cargoAllocation = null;
+//					for (final CargoAllocation allocation : outputSchedule.getCargoAllocations()) {
+//						if (allocation.getLoadSlot() == modelSlot || allocation.getDischargeSlot() == slot) {
+//							cargoAllocation = allocation;
+//							break;
+//						}
+//					}
+//					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
+//						final CargoRevenue revenue = ScheduleFactory.eINSTANCE.createCargoRevenue();
+//						revenue.setCargo(cargoAllocation);
+//						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
+//						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
+//						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
+//						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
+//						revenues.add(revenue);
+//						if (cargoAllocation.getLoadRevenue() == null) {
+//							cargoAllocation.setLoadRevenue(revenue);
+//						} else if (cargoAllocation.getShippingRevenue() == null) {
+//							cargoAllocation.setShippingRevenue(revenue);
+//						} else if (cargoAllocation.getDischargeRevenue() == null) {
+//							cargoAllocation.setDischargeRevenue(revenue);
+//						}
+//					}
+//				} else if (slot instanceof IVesselEventPortSlot) {
+//					final VesselEvent modelEvent = entities.getModelObject(slot, VesselEvent.class);
+//					VesselEventVisit visit = null;
+//
+//					for (final Sequence sequence : outputSchedule.getSequences()) {
+//						for (final Event event : sequence.getEvents()) {
+//							if (event instanceof VesselEventVisit) {
+//								if (((VesselEventVisit) event).getVesselEvent() == modelEvent) {
+//									visit = (VesselEventVisit) event;
+//								}
+//							}
+//						}
+//					}
+//
+//					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
+//						final VesselEventRevenue revenue = ScheduleFactory.eINSTANCE.createVesselEventRevenue();
+//						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
+//						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
+//						revenue.setVesselEventVisit(visit);
+//						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
+//						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
+//						revenues.add(revenue);
+//						visit.setRevenue(revenue);
+//					}
+//				} else {
+//					for (final IProfitAndLossEntry entry : profitAndLoss.getEntries()) {
+//						AdditionalData additionalData = ScheduleFactory.eINSTANCE.createAdditionalData();
+//
+//						final BookedRevenue revenue = ScheduleFactory.eINSTANCE.createBookedRevenue();
+//						revenue.setEntity(entities.getModelObject(entry.getEntity(), Entity.class));
+//						revenue.setDate(entities.getDateFromHours(profitAndLoss.getBookingTime()));
+//						revenue.setValue((int) (entry.getFinalGroupValue() / Calculator.ScaleFactor));
+//						revenue.setDetails(DetailTreeExporter.exportDetail(entry.getDetails()));
+//						revenues.add(revenue);
+//					}
+//				}
+//			}
+//		}
 
 		// clear refs, just in case.
-		scenario = null;
+		inputScenario = null;
 		entities = null;
 		outputSchedule = null;
 		annotatedSolution = null;
--- com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/TradingTransformerExtension.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/TradingTransformerExtension.java
@@ -4,14 +4,14 @@
  */
 package com.mmxlabs.trading.integration;
 
-import scenario.Scenario;
-import scenario.cargo.Cargo;
-import scenario.contract.Entity;
-import scenario.contract.GroupEntity;
-
 import com.mmxlabs.common.curves.ConstantValueCurve;
-import com.mmxlabs.lngscheduler.emf.extras.ITransformerExtension;
-import com.mmxlabs.lngscheduler.emf.extras.ModelEntityMap;
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoModel;
+import com.mmxlabs.models.lng.commercial.CommercialModel;
+import com.mmxlabs.models.lng.commercial.LegalEntity;
+import com.mmxlabs.models.lng.transformer.ITransformerExtension;
+import com.mmxlabs.models.lng.transformer.ModelEntityMap;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.scheduler.optimiser.Calculator;
 import com.mmxlabs.scheduler.optimiser.builder.ISchedulerBuilder;
 import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
@@ -26,14 +26,13 @@ import com.mmxlabs.trading.optimiser.builder.TradingBuilderExtension;
  */
 public class TradingTransformerExtension implements ITransformerExtension {
 
-	private TradingBuilderExtension tradingBuilder;
-	private Scenario scenario;
+	private MMXRootObject rootObject;
 	private ModelEntityMap entities;
-
+	private TradingBuilderExtension tradingBuilder;
 
 	@Override
-	public void startTransforming(Scenario scenario, ModelEntityMap map, ISchedulerBuilder builder) {
-		this.scenario = scenario;
+	public void startTransforming(MMXRootObject rootObject, ModelEntityMap map, ISchedulerBuilder builder) {
+		this.rootObject = rootObject;
 		this.entities = map;
 		tradingBuilder = new TradingBuilderExtension();
 		builder.addBuilderExtension(tradingBuilder);
@@ -41,33 +40,42 @@ public class TradingTransformerExtension implements ITransformerExtension {
 
 	@Override
 	public void finishTransforming() {
-		for (final Entity e : scenario.getContractModel().getEntities()) {
+		CommercialModel commercialModel = rootObject.getSubModel(CommercialModel.class);
+		for (final LegalEntity e : commercialModel.getEntities()) {
 			final IEntity e2;
-			if (e instanceof GroupEntity) {
-				final GroupEntity ge = (GroupEntity) e;
-				e2 = tradingBuilder.createGroupEntity(ge.getName(), Calculator.scaleToInt(ge.getOwnership()), 
-						new ConstantValueCurve(Calculator.scaleToInt(ge.getTaxRate())), //TODO fix tax rates.
-						Calculator.scaleToInt(ge.getTransferOffset()));
-			} else {
+			// Tmp hack until model is updated
+			if (e.getName().equalsIgnoreCase("Third-parties")) {
 				e2 = tradingBuilder.createExternalEntity(e.getName());
+			} else {
+				e2 = tradingBuilder.createGroupEntity(e.getName(), Calculator.scaleToInt(1.0), new ConstantValueCurve(Calculator.scaleToInt(0.25)), // TODO fix tax rates.
+						Calculator.scaleToInt(0));
 			}
+
+			// if (e instanceof GroupEntity) {
+			// final GroupEntity ge = (GroupEntity) e;
+			// e2 = tradingBuilder.createGroupEntity(ge.getName(), Calculator.scaleToInt(ge.getOwnership()), new ConstantValueCurve(Calculator.scaleToInt(ge.getTaxRate())), // TODO fix tax rates.
+			// Calculator.scaleToInt(ge.getTransferOffset()));
+			// } else {
+			// e2 = tradingBuilder.createExternalEntity(e.getName());
+			// }
 			entities.addModelObject(e, e2);
 		}
-		
-		final GroupEntity shipping = scenario.getContractModel().getShippingEntity();
-		final IEntity shipping2 = tradingBuilder.createGroupEntity(shipping.getName(), Calculator.scaleToInt(shipping.getOwnership()),
-				new ConstantValueCurve(Calculator.scaleToInt(shipping.getTaxRate())), // TODO fix tax rates.
-				Calculator.scaleToInt(shipping.getTransferOffset()));
-		entities.addModelObject(shipping, shipping2);
 
-		tradingBuilder.setShippingEntity(shipping2);
+		final LegalEntity shipping = commercialModel.getShippingEntity();
+		// final IEntity shipping2 = tradingBuilder.createGroupEntity(shipping.getName(), Calculator.scaleToInt(shipping.getOwnership()),
+		// new ConstantValueCurve(Calculator.scaleToInt(shipping.getTaxRate())), // TODO fix tax rates.
+		// Calculator.scaleToInt(shipping.getTransferOffset()));
+		// entities.addModelObject(shipping, shipping2);
+
+		tradingBuilder.setShippingEntity(entities.getOptimiserObject(shipping, IEntity.class));
 
 		// set up contract association or slot association or whatever
-		for (final Cargo c : scenario.getCargoModel().getCargoes()) {
+		CargoModel cargoModel = rootObject.getSubModel(CargoModel.class);
+		for (final Cargo c : cargoModel.getCargoes()) {
 			final IPortSlot load = entities.getOptimiserObject(c.getLoadSlot(), IPortSlot.class);
 			final IPortSlot dis = entities.getOptimiserObject(c.getDischargeSlot(), IPortSlot.class);
-			final IEntity loadEntity = entities.getOptimiserObject(c.getLoadSlot().getSlotOrPortContract(scenario).getEntity(), IEntity.class);
-			final IEntity dischargeEntity = entities.getOptimiserObject(c.getDischargeSlot().getSlotOrPortContract(scenario).getEntity(), IEntity.class);
+			final IEntity loadEntity = entities.getOptimiserObject(c.getLoadSlot().getContract().getEntity(), IEntity.class);
+			final IEntity dischargeEntity = entities.getOptimiserObject(c.getDischargeSlot().getContract().getEntity(), IEntity.class);
 			tradingBuilder.setEntityForSlot(loadEntity, load);
 			tradingBuilder.setEntityForSlot(dischargeEntity, dis);
 		}
