e3b7f89e84f8d212efea5db2a4e1625d090cb0cf
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-18 12:55:39 -0100

    Added tests

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.trading.optimiser.tests/src/com/mmxlabs/trading/optimiser/annotations/impl/ProfitAndLossEntryTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.trading.optimiser.annotations.impl;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mmxlabs.common.detailtree.IDetailTree;
+import com.mmxlabs.trading.optimiser.IEntity;
+
+public class ProfitAndLossEntryTest {
+
+	@Test
+	public void test() {
+
+		final IEntity entity = mock(IEntity.class);
+		final long groupValue = 1234567890l;
+		final IDetailTree details = mock(IDetailTree.class);
+
+		final ProfitAndLossEntry entry = new ProfitAndLossEntry(entity, groupValue, details);
+
+		Assert.assertSame(entity, entry.getEntity());
+		Assert.assertEquals(groupValue, entry.getFinalGroupValue());
+		Assert.assertSame(details, entry.getDetails());
+	}
+
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.trading.optimiser.tests/src/com/mmxlabs/trading/optimiser/impl/SimpleEntityTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.trading.optimiser.impl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mmxlabs.common.curves.ICurve;
+
+public class SimpleEntityTest {
+
+
+	@Test
+	public void testGetDownstreamTransferPrice() {
+
+		final SimpleEntity entity = new SimpleEntity("name", 500, null, 2000);
+		// 1234 - (scaled)500 * 2000
+		Assert.assertEquals(1234 - 10000, entity.getDownstreamTransferPrice(1234, 5000));
+	}
+
+	@Test
+	public void testGetUpstreamTransferPrice() {
+
+		final SimpleEntity entity = new SimpleEntity("name", 500, null, 2000);
+		// 1234 + (scaled)500 * 2000
+		Assert.assertEquals(1234 + 10000, entity.getUpstreamTransferPrice(1234, 5000));
+	}
+
+	@Test
+	public void testGetTaxedProfit() {
+		final ICurve curve = mock(ICurve.class);
+
+		final SimpleEntity entity = new SimpleEntity("name", 500, curve, 2000);
+
+		final int time = 12345;
+
+		// 50 %
+		when(curve.getValueAtPoint(time)).thenReturn(500.0);
+		
+		// 50% * 50% * 10000
+		Assert.assertEquals(2500, entity.getTaxedProfit(10000, time));
+
+	
+		verifyNoMoreInteractions(curve);
+	}
+
+}
