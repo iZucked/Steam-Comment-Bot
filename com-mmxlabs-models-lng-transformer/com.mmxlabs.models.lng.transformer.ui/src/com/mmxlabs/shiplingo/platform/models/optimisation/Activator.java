c6bfcbda86b5d47c6c0d6b5ac0382dcaacdeae5e
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-18 07:51:35 -0100

    Add listeners to keep job selection status in sync with job property to show in taskbar

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/shiplingo/platform/models/optimisation/Activator.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/shiplingo/platform/models/optimisation/Activator.java
@@ -1,10 +1,20 @@
 package com.mmxlabs.shiplingo.platform.models.optimisation;
 
+import java.util.Collection;
+
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.progress.IProgressConstants2;
 import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
 
+import com.mmxlabs.jobmanager.eclipse.jobs.impl.AbstractEclipseJobControl;
 import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
+import com.mmxlabs.jobmanager.jobs.IJobControl;
+import com.mmxlabs.jobmanager.jobs.IJobDescriptor;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.scenario.service.ui.IScenarioServiceSelectionChangedListener;
+import com.mmxlabs.scenario.service.ui.IScenarioServiceSelectionProvider;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -18,6 +28,46 @@ public class Activator extends AbstractUIPlugin {
 	private static Activator plugin;
 
 	private ServiceTracker<IEclipseJobManager, IEclipseJobManager> jobManagerServiceTracker;
+	private ServiceTracker<IScenarioServiceSelectionProvider, IScenarioServiceSelectionProvider> scenarioServiceSelectionProviderTracker;
+
+	private final IScenarioServiceSelectionChangedListener scenarioServiceSelectionChangedListener = new IScenarioServiceSelectionChangedListener() {
+
+		@Override
+		public void selected(final IScenarioServiceSelectionProvider provider, final Collection<ScenarioInstance> selected) {
+
+			setJobProperty(selected, true);
+		}
+
+		@Override
+		public void deselected(final IScenarioServiceSelectionProvider provider, final Collection<ScenarioInstance> deselected) {
+			setJobProperty(deselected, false);
+		}
+
+		private void setJobProperty(final Collection<ScenarioInstance> selected, final boolean showInTaskbar) {
+			final IEclipseJobManager jobManager = jobManagerServiceTracker.getService();
+			if (jobManager != null) {
+
+				for (final ScenarioInstance instance : selected) {
+					final Object object = instance.getInstance();
+					if (object instanceof MMXRootObject) {
+						final MMXRootObject root = (MMXRootObject) object;
+
+						final String uuid = instance.getUuid();
+
+						final IJobDescriptor job = jobManager.findJobForResource(uuid);
+						if (job == null) {
+							continue;
+						}
+
+						final IJobControl control = jobManager.getControlForJob(job);
+						if (control instanceof AbstractEclipseJobControl) {
+							((AbstractEclipseJobControl) control).setJobProperty(IProgressConstants2.SHOW_IN_TASKBAR_ICON_PROPERTY, showInTaskbar);
+						}
+					}
+				}
+			}
+		}
+	};
 
 	/**
 	 * The constructor
@@ -27,6 +77,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
@@ -36,26 +87,47 @@ public class Activator extends AbstractUIPlugin {
 
 		jobManagerServiceTracker = new ServiceTracker<IEclipseJobManager, IEclipseJobManager>(context, IEclipseJobManager.class.getName(), null);
 		jobManagerServiceTracker.open();
+		scenarioServiceSelectionProviderTracker = new ServiceTracker<IScenarioServiceSelectionProvider, IScenarioServiceSelectionProvider>(context, IScenarioServiceSelectionProvider.class.getName(),
+				null);
+		scenarioServiceSelectionProviderTracker.open();
+
+		{
+			final IScenarioServiceSelectionProvider service = scenarioServiceSelectionProviderTracker.getService();
+			if (service != null) {
+				service.addSelectionChangedListener(scenarioServiceSelectionChangedListener);
+			}
+		}
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
 	public void stop(final BundleContext context) throws Exception {
 
+		{
+			final IScenarioServiceSelectionProvider service = scenarioServiceSelectionProviderTracker.getService();
+			if (service != null) {
+				service.removeSelectionChangedListener(scenarioServiceSelectionChangedListener);
+			}
+		}
+
 		// close the service tracker
 		jobManagerServiceTracker.close();
 		jobManagerServiceTracker = null;
 
-		plugin = null;		
+		scenarioServiceSelectionProviderTracker.close();
+		scenarioServiceSelectionProviderTracker = null;
+
+		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
