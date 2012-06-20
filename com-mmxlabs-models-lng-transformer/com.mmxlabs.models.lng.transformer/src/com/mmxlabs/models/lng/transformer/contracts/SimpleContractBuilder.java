de3b103002908d266b410bf0fcc7a44ad88b2d01
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-20 18:54:16 -0100

    Remove Netback and ProfitShare contract from this transformer

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/SimpleContractBuilder.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/SimpleContractBuilder.java
@@ -6,23 +6,14 @@ package com.mmxlabs.models.lng.transformer.contracts;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 import com.mmxlabs.common.Pair;
 import com.mmxlabs.common.curves.ICurve;
 import com.mmxlabs.optimiser.core.scenario.IDataComponentProvider;
 import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
-import com.mmxlabs.optimiser.core.scenario.common.IMultiMatrixProvider;
-import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
 import com.mmxlabs.scheduler.optimiser.builder.IBuilderExtension;
-import com.mmxlabs.scheduler.optimiser.components.IPort;
-import com.mmxlabs.scheduler.optimiser.contracts.ILoadPriceCalculator2;
 import com.mmxlabs.scheduler.optimiser.contracts.impl.FixedPriceContract;
 import com.mmxlabs.scheduler.optimiser.contracts.impl.MarketPriceContract;
-import com.mmxlabs.scheduler.optimiser.contracts.impl.NetbackContract;
-import com.mmxlabs.scheduler.optimiser.contracts.impl.ProfitSharingContract;
 
 /**
  * A builder for all the simple contract types. Needs a way to wire up DCPs after everything has finished.
@@ -31,7 +22,6 @@ import com.mmxlabs.scheduler.optimiser.contracts.impl.ProfitSharingContract;
  * 
  */
 public class SimpleContractBuilder implements IBuilderExtension {
-	private final List<NetbackContract> netbacks = new LinkedList<NetbackContract>();
 
 	FixedPriceContract createFixedPriceContract(final int pricePerMMBTU) {
 		return new FixedPriceContract(pricePerMMBTU);
@@ -41,20 +31,6 @@ public class SimpleContractBuilder implements IBuilderExtension {
 		return new MarketPriceContract(index, offset, multiplier);
 	}
 
-	ProfitSharingContract createProfitSharingContract(final ICurve actualMarket, final ICurve referenceMarket, final int margin, final int share, Set<IPort> baseMarketPorts) {
-		return new ProfitSharingContract(actualMarket, referenceMarket, margin, share, baseMarketPorts);
-	}
-
-	// ILoadPriceCalculator2 createNetbackContract(final int buyersMargin) {
-	// final NetbackContract result = new NetbackContract();
-	//
-	// result.setMarginScaled(buyersMargin);
-	//
-	// netbacks.add(result);
-	//
-	// return result;
-	// }
-
 	@Override
 	public Collection<Pair<String, IDataComponentProvider>> createDataComponentProviders(IOptimisationData optimisationData) {
 		return Collections.emptySet();
@@ -62,14 +38,9 @@ public class SimpleContractBuilder implements IBuilderExtension {
 
 	@Override
 	public void dispose() {
-		netbacks.clear();
 	}
 
 	@Override
 	public void finishBuilding(IOptimisationData optimisationData) {
-		final IMultiMatrixProvider<IPort, Integer> distanceProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_portDistanceProvider, IMultiMatrixProvider.class);
-		for (final NetbackContract netback : netbacks) {
-			netback.setDistanceProvider(distanceProvider);
-		}
 	}
 }
--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/SimpleContractTransformer.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/SimpleContractTransformer.java
@@ -6,8 +6,6 @@ package com.mmxlabs.models.lng.transformer.contracts;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.eclipse.emf.ecore.EClass;
 
@@ -17,19 +15,13 @@ import com.mmxlabs.models.lng.commercial.CommercialPackage;
 import com.mmxlabs.models.lng.commercial.Contract;
 import com.mmxlabs.models.lng.commercial.FixedPriceContract;
 import com.mmxlabs.models.lng.commercial.IndexPriceContract;
-import com.mmxlabs.models.lng.commercial.ProfitSharePurchaseContract;
 import com.mmxlabs.models.lng.commercial.PurchaseContract;
 import com.mmxlabs.models.lng.commercial.SalesContract;
 import com.mmxlabs.models.lng.transformer.ModelEntityMap;
-import com.mmxlabs.models.lng.types.APort;
-import com.mmxlabs.models.lng.types.APortSet;
-import com.mmxlabs.models.lng.types.util.SetUtils;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.scheduler.optimiser.Calculator;
 import com.mmxlabs.scheduler.optimiser.builder.ISchedulerBuilder;
-import com.mmxlabs.scheduler.optimiser.components.IPort;
 import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
-import com.mmxlabs.scheduler.optimiser.components.impl.AXPlusBCurve;
 import com.mmxlabs.scheduler.optimiser.contracts.ILoadPriceCalculator2;
 import com.mmxlabs.scheduler.optimiser.contracts.IShippingPriceCalculator;
 import com.mmxlabs.scheduler.optimiser.contracts.impl.SimpleContract;
@@ -45,8 +37,7 @@ public class SimpleContractTransformer implements IContractTransformer {
 	private SimpleContractBuilder contractBuilder;
 	private ModelEntityMap map;
 	private MMXRootObject rootObject;
-	private final Collection<EClass> handledClasses = Arrays.asList(CommercialPackage.eINSTANCE.getFixedPriceContract(), CommercialPackage.eINSTANCE.getIndexPriceContract(),
-			CommercialPackage.eINSTANCE.getProfitSharePurchaseContract(), CommercialPackage.eINSTANCE.getNetbackPurchaseContract());
+	private final Collection<EClass> handledClasses = Arrays.asList(CommercialPackage.eINSTANCE.getFixedPriceContract(), CommercialPackage.eINSTANCE.getIndexPriceContract());
 
 	@Override
 	public void startTransforming(final MMXRootObject rootObject, final ModelEntityMap map, final ISchedulerBuilder builder) {
@@ -63,29 +54,6 @@ public class SimpleContractTransformer implements IContractTransformer {
 		this.map = null;
 		this.builder = null;
 		this.contractBuilder = null;
-	}
-
-	private ILoadPriceCalculator2 instantiatePC(final Contract c) {
-		if (c instanceof ProfitSharePurchaseContract) {
-			final ProfitSharePurchaseContract contract = (ProfitSharePurchaseContract) c;
-			final Set<IPort> baseMarketPorts = new HashSet<IPort>();
-			for (APortSet portSet : contract.getBaseMarketPorts()) {
-				final Set<APort> ports = SetUtils.getPorts(portSet);
-				for (APort p : ports) {
-					baseMarketPorts.add(map.getOptimiserObject(p, IPort.class));
-				}
-			}
-			ICurve baseMarket = new AXPlusBCurve(Calculator.scaleToInt(contract.getBaseMarketMultiplier()), Calculator.scaleToInt(contract.getBaseMarketConstant()), map.getOptimiserObject(
-					contract.getBaseMarketIndex(), ICurve.class));
-			ICurve referenceMarket = new AXPlusBCurve(Calculator.scaleToInt(contract.getRefMarketMultiplier()), Calculator.scaleToInt(contract.getRefMarketConstant()), map.getOptimiserObject(
-					contract.getRefMarketIndex(), ICurve.class));
-
-			return contractBuilder.createProfitSharingContract(baseMarket, referenceMarket, Calculator.scaleToInt(contract.getMargin()), Calculator.scaleToInt(contract.getShare()), baseMarketPorts);
-			// } else if (c instanceof IndexPriceContract) {
-			// return contractBuilder.createNetbackContract(buyersMargin)map.getOptimiserObject(((IndexPriceContract) c).getIndex(), ICurve.class),
-			// Calculator.scaleToInt(((IndexPriceContract) c).getConstant()), Calculator.scaleToInt(((IndexPriceContract) c).getMultiplier()));
-		}
-		return instantiate(c);
 	}
 
 	private SimpleContract instantiate(final Contract c) {
@@ -105,7 +73,7 @@ public class SimpleContractTransformer implements IContractTransformer {
 
 	@Override
 	public ILoadPriceCalculator2 transformPurchaseContract(final PurchaseContract pc) {
-		return instantiatePC(pc);
+		return instantiate(pc);
 	}
 
 	@Override
