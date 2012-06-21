c33cf053fc471f1ba4d1bea3fce7a4b48c162433
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-21 13:27:38 -0100

    Build a real inline editor control for notional ballast params.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/inlineeditors/NotionalBallastParametersInlineEditorFactory.java
@@ -0,0 +1,14 @@
+package com.mmxlabs.models.lng.commercial.ui.inlineeditors;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com.mmxlabs.models.ui.editors.IInlineEditor;
+import com.mmxlabs.models.ui.editors.IInlineEditorFactory;
+
+public class NotionalBallastParametersInlineEditorFactory implements IInlineEditorFactory {
+	@Override
+	public IInlineEditor createEditor(final EClass owner, final EStructuralFeature feature) {
+		return new NotionalBallastParametersInlineEditor(feature);
+	}
+}
