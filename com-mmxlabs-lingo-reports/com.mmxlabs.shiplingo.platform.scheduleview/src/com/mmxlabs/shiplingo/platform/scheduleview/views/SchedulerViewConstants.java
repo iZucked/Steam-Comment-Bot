d9e342b94d32019237083341d70b5b50190d2318
proshun@minimaxlabs.com <unknown@kiln.example.com>
2012-06-23 21:09:02 -0100

    New Gantt colour and highlighting schemes (2)

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/shiplingo/platform/scheduleview/views/SchedulerViewConstants.java
@@ -0,0 +1,18 @@
+package com.mmxlabs.shiplingo.platform.scheduleview.views;
+
+/**
+ * Hierarchical key-value constants for Scheduler view settings, used in IMemento and PreferenceStore.
+ * 
+ * @author proshun
+ */
+public interface SchedulerViewConstants {
+
+	public static final String Show_ = "Show.";
+	public static final String Show_Canals = "Show.Canals";
+
+	public static final String Highlight_ = "Hi.";
+//	public static final String HighlightScheme = "SchedulerView.HighlightScheme";
+	public static final String Highlight_Riskies = "Hi.RiskyJourneys";
+	
+	public static final String SCHEDULER_VIEW_COLOUR_SCHEME = "SCHEDULER_VIEW_COLOUR_SCHEME";
+}
---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.scheduleview/src/com/mmxlabs/shiplingo/platform/scheduleview/views/colourschemes/CooldownHighlighter.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.shiplingo.platform.scheduleview.views.colourschemes;
+
+import static com.mmxlabs.shiplingo.platform.scheduleview.views.colourschemes.ColourSchemeUtil.Warning_Yellow;
+
+import org.eclipse.nebula.widgets.ganttchart.ColorCache;
+import org.eclipse.swt.graphics.Color;
+
+import com.mmxlabs.models.lng.schedule.Cooldown;
+
+public class CooldownHighlighter extends ColourScheme {
+
+	@Override
+	public String getName() {
+		return "Cooldown";
+	}
+
+	@Override
+	public Color getBackground(final Object element) {
+		if (element instanceof Cooldown) {
+			return ColorCache.getColor(Warning_Yellow);
+		}
+		return null;
+	}
+}
