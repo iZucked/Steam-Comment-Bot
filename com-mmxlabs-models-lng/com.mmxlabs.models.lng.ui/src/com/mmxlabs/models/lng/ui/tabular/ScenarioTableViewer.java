dce94a62f4e6858e079652e993f35b479464141a
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-25 09:21:03 -0100

    Claim validation lock from scenario instance when validating

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/tabular/ScenarioTableViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/tabular/ScenarioTableViewer.java
@@ -4,6 +4,7 @@ import org.eclipse.swt.widgets.Composite;
 
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 import com.mmxlabs.models.ui.tabular.EObjectTableViewer;
+import com.mmxlabs.scenario.service.model.ScenarioLock;
 
 public class ScenarioTableViewer extends EObjectTableViewer  {
 	private IScenarioEditingLocation editorPart;
@@ -11,5 +12,17 @@ public class ScenarioTableViewer extends EObjectTableViewer  {
 	public ScenarioTableViewer(final Composite parent, final int style, final IScenarioEditingLocation part) {
 		super(parent, style);
 		this.editorPart = part;
-	}	
+	}
+
+	@Override
+	protected boolean claimValidationLock() {
+		ScenarioLock lock = editorPart.getScenarioInstance().getLock(ScenarioLock.VALIDATION);
+		return lock.claim();
+	}
+
+	@Override
+	protected void releaseValidationLock() {
+		ScenarioLock lock = editorPart.getScenarioInstance().getLock(ScenarioLock.VALIDATION);
+		lock.release();
+	}
 }
