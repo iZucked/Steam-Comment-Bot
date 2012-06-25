9f0b7928750e8d6a89a0cc6cdf7fc53ecf2d0375
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-25 09:20:13 -0100

    org. imports

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/ValidationContentAdapter.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/ValidationContentAdapter.java
@@ -1,13 +1,10 @@
 package com.mmxlabs.models.ui.validation;
 
 import java.util.Collections;
-import java.util.EventObject;
 import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.common.command.CommandStack;
-import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EContentAdapter;
