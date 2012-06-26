d318f80b41ea93e7cf59b8119d8c503a6993f587
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-26 18:30:45 -0100

    Log when lock is missed

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.liveevaluator/src/com/mmxlabs/models/lng/analytics/ui/liveeval/LiveEvaluator.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.liveevaluator/src/com/mmxlabs/models/lng/analytics/ui/liveeval/LiveEvaluator.java
@@ -108,6 +108,7 @@ public class LiveEvaluator extends MMXAdapterImpl {
 								instance.getLock(ScenarioLock.EVALUATOR).release();
 							}
 						} else {
+							log.debug("Didn't get lock, spinning");
 							spinLock = true;
 						}
 					} else {
