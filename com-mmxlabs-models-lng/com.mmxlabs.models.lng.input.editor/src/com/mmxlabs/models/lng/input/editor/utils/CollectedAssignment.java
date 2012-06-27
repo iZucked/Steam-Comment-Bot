32762a6e4aeb18c883495ed0e4f44490f5377f41
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-27 08:40:29 -0100

    add better null checking

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/editor/utils/CollectedAssignment.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.input.editor/src/com/mmxlabs/models/lng/input/editor/utils/CollectedAssignment.java
@@ -36,18 +36,24 @@ public class CollectedAssignment {
 		Collections.sort(assignments, new Comparator<ElementAssignment>() {
 			@Override
 			public int compare(final ElementAssignment arg0, final ElementAssignment arg1) {
-				//TODO check for nulls.
-				if (arg0.getAssignedObject() == null) {
-					System.err.println("Dangling assignment!!");
-				}
-				
-				if (arg1.getAssignedObject() == null) {
-					System.err.println("Another dangling assignment!!");
-				}
 				final Date start0 = AssignmentEditorHelper.getStartDate(arg0.getAssignedObject());
 				final Date start1 = AssignmentEditorHelper.getStartDate(arg1.getAssignedObject());
 				final Date end0 = AssignmentEditorHelper.getEndDate(arg0.getAssignedObject());
 				final Date end1 = AssignmentEditorHelper.getEndDate(arg1.getAssignedObject());
+				
+				final boolean null0 = start0 == null || end0 == null;
+				final boolean null1 = start1 == null || end1 == null;
+				
+				if (null0) {
+					if (null1) {
+						return 0;
+					} else {
+						return -1;
+					}
+				} else if (null1) {
+					return 1;
+				}
+				
 				if (overlaps(start0, end0, start1, end1)) {
 					return ((Integer) arg0.getSequence()).compareTo(arg1.getSequence());
 				} else {
