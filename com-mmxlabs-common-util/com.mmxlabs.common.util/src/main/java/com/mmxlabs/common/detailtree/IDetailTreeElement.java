9ad4e77228fc6a315141b865a2c71c9db3688958
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-06-28 10:30:23 -0100

    Doc

--- com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/detailtree/IDetailTreeElement.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/detailtree/IDetailTreeElement.java
@@ -1,7 +1,20 @@
 package com.mmxlabs.common.detailtree;
 
+import com.mmxlabs.common.detailtree.impl.CurrencyDetailElement;
+
+/**
+ * An interface wrapping an object with the intention that implementing classes infer some kind of formatting information or hints. For example the {@link CurrencyDetailElement} infers the contained
+ * object is related to currency and should be formatted as such. This is intended for use with {@link IDetailTree} although there is no explicit link between this and the {@link IDetailTree}
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public interface IDetailTreeElement {
-	
+
+	/**
+	 * Return the wrapped object.
+	 * 
+	 * @return
+	 */
 	Object getObject();
 }
-
