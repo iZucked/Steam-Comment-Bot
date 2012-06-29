f3b9bb4046dd92905938a4876cec7e45e8a61396
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-29 09:15:38 -0100

    Add (disabled) code to allow menu contributions in toolbar. Can't find where to dispose contributions

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editorpart/JointModelEditorContributor.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editorpart/JointModelEditorContributor.java
@@ -8,5 +8,25 @@ import org.eclipse.emf.edit.ui.action.EditingDomainActionBarContributor;
 import org.eclipse.ui.IEditorActionBarContributor;
 
 public class JointModelEditorContributor extends EditingDomainActionBarContributor implements IEditorActionBarContributor {
-
+//	@Override
+//	public void contributeToToolBar(IToolBarManager toolBarManager) {
+//		if (toolBarManager instanceof ContributionManager) {
+//			final ContributionManager cm = (ContributionManager) toolBarManager;
+//			final String toolbarID = "toolbar:scneditor";
+//			final IMenuService menuService = (IMenuService) PlatformUI.getWorkbench().getService(IMenuService.class);
+//			if (menuService != null) {
+//				menuService.populateContributionManager(cm, toolbarID);
+////				
+////				viewer.getControl().addDisposeListener(new DisposeListener() {
+////					@Override
+////					public void widgetDisposed(final DisposeEvent e) {
+////						menuService.releaseContributions(cm);
+////					}
+////				});
+//			}
+//		}
+//		
+//	}
+	
+	
 }
