76382732555b8f4d8e747c028a10f1209bb6f918
Tom Hinton <tom.hinton@minimaxlabs.com>
2012-06-29 14:18:33 -0100

    Add filtered provider

---
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/valueproviders/FilteredReferenceValueProvider.java
@@ -0,0 +1,51 @@
+package com.mmxlabs.models.ui.valueproviders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com.mmxlabs.common.Pair;
+
+public abstract class FilteredReferenceValueProvider implements IReferenceValueProvider {
+	private final IReferenceValueProvider delegate;
+
+	public FilteredReferenceValueProvider(IReferenceValueProvider delegate) {
+		super();
+		this.delegate = delegate;
+	}
+
+	public List<Pair<String, EObject>> getAllowedValues(EObject target,EStructuralFeature field) {
+		List<Pair<String, EObject>> allowedValues = delegate.getAllowedValues(target, field);
+		final ArrayList<Pair<String, EObject>> filtered = new ArrayList<Pair<String, EObject>>(allowedValues.size());
+		
+		for (final Pair<String, EObject> p : allowedValues) {
+			if (isAllowedValue(target, field, p)) filtered.add(p);
+		}
+		
+		return filtered;
+	}
+
+	public String getName(EObject referer, EReference feature,
+			EObject referenceValue) {
+		return delegate.getName(referer, feature, referenceValue);
+	}
+
+	public Iterable<Pair<Notifier, List<Object>>> getNotifiers(EObject referer,
+			EReference feature, EObject referenceValue) {
+		return delegate.getNotifiers(referer, feature, referenceValue);
+	}
+
+	public boolean updateOnChangeToFeature(Object changedFeature) {
+		return delegate.updateOnChangeToFeature(changedFeature);
+	}
+
+	public void dispose() {
+		delegate.dispose();
+	}
+	
+	protected abstract boolean isAllowedValue(final EObject target, final EStructuralFeature field, final Pair<String, EObject> value);
+}
