3b35984584148b31885f536c2ebf0721eb348cae
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-09 13:59:36 -0100

    Forbif re-wiring for not shipped cargoes.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/CargoTypeConstraint.java
@@ -0,0 +1,54 @@
+package com.mmxlabs.models.lng.cargo.validation;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.CargoType;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+public class CargoTypeConstraint extends AbstractModelConstraint {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse .emf.validation.IValidationContext)
+	 */
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject object = ctx.getTarget();
+
+		final List<IStatus> failures = new LinkedList<IStatus>();
+		if (object instanceof Cargo) {
+			final Cargo cargo = (Cargo) object;
+			if (cargo.isAllowRewiring() && cargo.getCargoType() != CargoType.FLEET) {
+
+				final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Non-shipped cargo  " + cargo.getName()
+						+ " cannot be re-wired."));
+
+				failure.addEObjectAndFeature(cargo, CargoPackage.eINSTANCE.getCargo_AllowRewiring());
+
+				failures.add(failure);
+			}
+		}
+
+		if (failures.isEmpty()) {
+			return ctx.createSuccessStatus();
+		} else if (failures.size() == 1) {
+			return failures.get(0);
+		} else {
+			final MultiStatus multi = new MultiStatus(Activator.PLUGIN_ID, IStatus.ERROR, null, null);
+			for (final IStatus s : failures) {
+				multi.add(s);
+			}
+			return multi;
+		}
+	}
+}
