1c692128b9c1636be89953e657fda97521f008e1
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-09 10:10:58 -0100

    Implement hooks to handle Cargo validation statues.
    BugzId: 55

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelEditorContribution.java
@@ -6,11 +6,15 @@ package com.mmxlabs.models.lng.cargo.ui.editorpart;
 
 import java.util.Collections;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Composite;
 
+import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class CargoModelEditorContribution extends BaseJointModelEditorContribution<CargoModel> {
 	private int pageNumber = 0;
@@ -20,15 +24,39 @@ public class CargoModelEditorContribution extends BaseJointModelEditorContributi
 	public void addPages(final Composite parent) {
 		this.viewerPane = new CargoModelViewer(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
 		viewerPane.createControl(parent);
-		viewerPane.init(Collections.singletonList(CargoPackage.eINSTANCE.getCargoModel_Cargoes()),
-				editorPart.getAdapterFactory());
+		viewerPane.init(Collections.singletonList(CargoPackage.eINSTANCE.getCargoModel_Cargoes()), editorPart.getAdapterFactory());
 		viewerPane.getViewer().setInput(modelObject);
 		pageNumber = editorPart.addPage(viewerPane.getControl());
 		editorPart.setPageText(pageNumber, "Cargoes");
 	}
 
 	@Override
-	public void setLocked(boolean locked) {
+	public void setLocked(final boolean locked) {
 		viewerPane.setLocked(locked);
+	}
+
+	@Override
+	public boolean canHandle(final IStatus status) {
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			if (dcsd.getTarget() instanceof Cargo) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public void handle(final IStatus status) {
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			if (dcsd.getTarget() instanceof Cargo) {
+				final Cargo cargo = (Cargo) dcsd.getTarget();
+				editorPart.setActivePage(pageNumber);
+				viewerPane.getScenarioViewer().setSelection(new StructuredSelection(cargo), true);
+			}
+		}
 	}
 }
