90ab2f54a066fef5874d9ddc3bbd9ea0ccdf2f57
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-09 08:17:04 -0100

    Disable rewiring check box (and untick) when the vessel assignment is marked as locked.
    BugzId: 602

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CargoComponentHelper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CargoComponentHelper.java
@@ -5,25 +5,35 @@
 package com.mmxlabs.models.lng.cargo.presentation.composites;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.swt.SWT;
 
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.editor.editors.CargoTypeInlineEditor;
+import com.mmxlabs.models.lng.input.InputPackage;
 import com.mmxlabs.models.lng.types.TypesPackage;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.BaseComponentHelper;
 import com.mmxlabs.models.ui.ComponentHelperUtils;
 import com.mmxlabs.models.ui.IComponentHelper;
 import com.mmxlabs.models.ui.IInlineEditorContainer;
+import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
+import com.mmxlabs.models.ui.editors.ICommandHandler;
+import com.mmxlabs.models.ui.editors.IInlineEditor;
+import com.mmxlabs.models.ui.editors.impl.IInlineEditorEnablementWrapper;
 import com.mmxlabs.models.ui.registries.IComponentHelperRegistry;
 
 /**
  * A component helper for Cargo instances
- *
+ * 
  * @generated
  */
 public class CargoComponentHelper extends BaseComponentHelper {
@@ -31,7 +41,7 @@ public class CargoComponentHelper extends BaseComponentHelper {
 
 	/**
 	 * Construct a new instance, using the platform adapter manager
-	 *
+	 * 
 	 * @generated
 	 */
 	public CargoComponentHelper() {
@@ -40,32 +50,33 @@ public class CargoComponentHelper extends BaseComponentHelper {
 
 	/**
 	 * Construct a new instance of this helper
-	 *
+	 * 
 	 * @generated
 	 */
 	public CargoComponentHelper(IAdapterManager adapterManager) {
 		final IComponentHelperRegistry registry = com.mmxlabs.models.ui.Activator.getDefault().getComponentHelperRegistry();
 		superClassesHelpers.addAll(registry.getComponentHelpers(TypesPackage.Literals.ACARGO));
 	}
-	
+
 	/**
 	 * add editors to a composite, using Cargo as the supertype
-	 *
+	 * 
 	 * @generated
 	 */
-	 @Override
+	@Override
 	public void addEditorsToComposite(final IInlineEditorContainer detailComposite) {
-		addEditorsToComposite(detailComposite, CargoPackage.Literals.CARGO);	
+		addEditorsToComposite(detailComposite, CargoPackage.Literals.CARGO);
 	}
-	
+
 	/**
 	 * Create the editors for features on this class directly, and superclass' features.
-	 *
+	 * 
 	 * @generated NOT
 	 */
 	@Override
 	public void addEditorsToComposite(final IInlineEditorContainer detailComposite, final EClass topClass) {
-		for (final IComponentHelper helper : superClassesHelpers) helper.addEditorsToComposite(detailComposite, topClass);
+		for (final IComponentHelper helper : superClassesHelpers)
+			helper.addEditorsToComposite(detailComposite, topClass);
 
 		// This line is not auto-generated
 		add_cargoTypeEditor(detailComposite, topClass);
@@ -74,45 +85,82 @@ public class CargoComponentHelper extends BaseComponentHelper {
 		add_allowRewiringEditor(detailComposite, topClass);
 		add_allowedVesselsEditor(detailComposite, topClass);
 	}
-	
+
 	/**
 	 * Create the editor for the cargoType operation on Cargo
-	 *
+	 * 
 	 * @generated NO
 	 */
 	protected void add_cargoTypeEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
 		detailComposite.addInlineEditor(new CargoTypeInlineEditor(SWT.NONE));
 	}
-	
+
 	/**
 	 * Create the editor for the loadSlot feature on Cargo
-	 *
+	 * 
 	 * @generated NO
 	 */
 	protected void add_loadSlotEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
-//		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__LOAD_SLOT));
+		// detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__LOAD_SLOT));
 	}
+
 	/**
 	 * Create the editor for the dischargeSlot feature on Cargo
-	 *
+	 * 
 	 * @generated NO
 	 */
 	protected void add_dischargeSlotEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
-//		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__DISCHARGE_SLOT));
+		// detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__DISCHARGE_SLOT));
 	}
 
 	/**
 	 * Create the editor for the allowRewiring feature on Cargo
-	 *
-	 * @generated
+	 * 
+	 * @generated NOT
 	 */
 	protected void add_allowRewiringEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
-		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__ALLOW_REWIRING));
+
+		/**
+		 * @generated NOT
+		 */
+		class SlotInlineEditorWrapper extends IInlineEditorEnablementWrapper {
+			private IScenarioEditingLocation location;
+
+			public SlotInlineEditorWrapper(final IInlineEditor wrapped) {
+				super(wrapped);
+			}
+
+			@Override
+			public void display(IScenarioEditingLocation location, MMXRootObject scenario, EObject object, Collection<EObject> range) {
+				this.location = location;
+				super.display(location, scenario, object, range);
+			}
+
+			@Override
+			public void reallyNotifyChanged(final Notification notification) {
+				if (notification.getFeature() == InputPackage.eINSTANCE.getElementAssignment_Locked()) {
+					setEnabled(!notification.getNewBooleanValue());
+					if (notification.getNewBooleanValue()) {
+						ICommandHandler handler = location.getDefaultCommandHandler();
+						handler.handleCommand(SetCommand.create(location.getEditingDomain(), input, CargoPackage.eINSTANCE.getCargo_AllowRewiring(), false), input,
+								CargoPackage.eINSTANCE.getCargo_AllowRewiring());
+					}
+				}
+			}
+
+			@Override
+			public EObject getEditorTarget() {
+				return wrapped.getEditorTarget();
+			}
+		}
+		;
+
+		detailComposite.addInlineEditor(new SlotInlineEditorWrapper(ComponentHelperUtils.createDefaultEditor(topClass, CargoPackage.Literals.CARGO__ALLOW_REWIRING)));
 	}
 
 	/**
 	 * Create the editor for the allowedVessels feature on Cargo
-	 *
+	 * 
 	 * @generated
 	 */
 	protected void add_allowedVesselsEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
