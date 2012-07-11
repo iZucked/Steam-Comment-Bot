f10db6d872d0a05a029e75b41ea90961d4f6476a
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-11 06:48:45 -0100

    Update license headers

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/ComponentProviderModule.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/ComponentProviderModule.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
 package com.mmxlabs.trading.integration;
 
 import org.ops4j.peaberry.activation.util.PeaberryActivationModule;
