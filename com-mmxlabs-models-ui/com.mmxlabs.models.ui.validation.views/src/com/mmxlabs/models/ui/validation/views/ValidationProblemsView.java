d69afb60201593e07e6b8921e7c4a29185c7241c
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-11 06:43:02 -0100

    Update license headers

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation.views/src/com/mmxlabs/models/ui/validation/views/ValidationProblemsView.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation.views/src/com/mmxlabs/models/ui/validation/views/ValidationProblemsView.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
 package com.mmxlabs.models.ui.validation.views;
 
 import java.lang.reflect.InvocationTargetException;
--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/gui/IValidationStatusGoto.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/gui/IValidationStatusGoto.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
 package com.mmxlabs.models.ui.validation.gui;
 
 import org.eclipse.core.runtime.IStatus;
