3f29e6f398a270bd9ea3abca2a21af8cfa470efd
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-11 06:46:17 -0100

    Update license headers

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceNavigatorDecoratingLabelProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/navigator/ScenarioServiceNavigatorDecoratingLabelProvider.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
 package com.mmxlabs.scenario.service.ui.navigator;
 
 import org.eclipse.jface.preference.JFacePreferences;
