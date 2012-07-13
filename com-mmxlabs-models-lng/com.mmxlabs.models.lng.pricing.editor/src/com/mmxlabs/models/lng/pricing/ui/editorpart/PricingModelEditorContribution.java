40eda5385ee447bf5c7871b7298a408991e36de9
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-13 12:31:33 -0100

    Link up to validation status goto interface

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/editorpart/PricingModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/editorpart/PricingModelEditorContribution.java
@@ -7,25 +7,39 @@ package com.mmxlabs.models.lng.pricing.ui.editorpart;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.widgets.Composite;
 
+import com.mmxlabs.models.lng.pricing.CharterCostModel;
+import com.mmxlabs.models.lng.pricing.CooldownPrice;
+import com.mmxlabs.models.lng.pricing.FleetCostModel;
+import com.mmxlabs.models.lng.pricing.Index;
+import com.mmxlabs.models.lng.pricing.IndexPoint;
+import com.mmxlabs.models.lng.pricing.PortCost;
 import com.mmxlabs.models.lng.pricing.PricingModel;
 import com.mmxlabs.models.lng.pricing.PricingPackage;
+import com.mmxlabs.models.lng.pricing.RouteCost;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class PricingModelEditorContribution extends BaseJointModelEditorContribution<PricingModel> {
-	private CharterMarketPane charter;
+	private CharterMarketPane fleetCostPane;
 	private CanalCostsPane route;
 	private CooldownCostsPane cool;
 	private PortCostPane port;
+	private IndexPane charterIndicesPane;
+	private IndexPane commodityPane;
+
+	private final int indexPage = -1;
+	private final int costsPage = -1;
 
 	@Override
 	public void addPages(final Composite parent) {
 		addIndexPage(parent);
-		addMarketPage(parent);
 		addCostsPage(parent);
 	}
 
@@ -33,7 +47,7 @@ public class PricingModelEditorContribution extends BaseJointModelEditorContribu
 		final SashForm sash = new SashForm(parent, SWT.HORIZONTAL);
 		final SashForm sash2 = new SashForm(sash, SWT.VERTICAL);
 		final SashForm sash3 = new SashForm(sash, SWT.VERTICAL);
-		
+
 		route = new CanalCostsPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
 		route.createControl(sash2);
 		route.init(Arrays.asList(new EReference[] { PricingPackage.eINSTANCE.getPricingModel_RouteCosts() }), editorPart.getAdapterFactory());
@@ -41,19 +55,15 @@ public class PricingModelEditorContribution extends BaseJointModelEditorContribu
 
 		port = new PortCostPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
 		port.createControl(sash3);
-		port.init(Arrays.asList(new EReference[] {PricingPackage.eINSTANCE.getPricingModel_PortCosts()}), editorPart.getAdapterFactory());
+		port.init(Arrays.asList(new EReference[] { PricingPackage.eINSTANCE.getPricingModel_PortCosts() }), editorPart.getAdapterFactory());
 		port.getViewer().setInput(modelObject);
-		
+
 		cool = new CooldownCostsPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
 		cool.createControl(sash3);
-		cool.init(Arrays.asList(new EReference[] {PricingPackage.eINSTANCE.getPricingModel_CooldownPrices()}), editorPart.getAdapterFactory());
+		cool.init(Arrays.asList(new EReference[] { PricingPackage.eINSTANCE.getPricingModel_CooldownPrices() }), editorPart.getAdapterFactory());
 		cool.getViewer().setInput(modelObject);
-		
+
 		editorPart.setPageText(editorPart.addPage(sash), "Costs");
-	}
-
-	private void addMarketPage(final Composite parent) {
-
 	}
 
 	private void addIndexPage(final Composite parent) {
@@ -61,34 +71,116 @@ public class PricingModelEditorContribution extends BaseJointModelEditorContribu
 		final SashForm sash2 = new SashForm(sash, SWT.VERTICAL);
 		final SashForm sash3 = new SashForm(sash, SWT.VERTICAL);
 
-		final IndexPane pane = new IndexPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
-		pane.createControl(sash2);
-		pane.init(Collections.singletonList(PricingPackage.eINSTANCE.getPricingModel_CommodityIndices()), editorPart.getAdapterFactory());
-		pane.getViewer().setInput(modelObject);
-		pane.defaultSetTitle("Commodity Indices");
+		commodityPane = new IndexPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
+		commodityPane.createControl(sash2);
+		commodityPane.init(Collections.singletonList(PricingPackage.eINSTANCE.getPricingModel_CommodityIndices()), editorPart.getAdapterFactory());
+		commodityPane.getViewer().setInput(modelObject);
+		commodityPane.defaultSetTitle("Commodity Indices");
 
-		final IndexPane pane2 = new IndexPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
-		pane2.createControl(sash2);
-		pane2.init(Collections.singletonList(PricingPackage.eINSTANCE.getPricingModel_CharterIndices()), editorPart.getAdapterFactory());
-		pane2.getViewer().setInput(modelObject);
-		pane2.defaultSetTitle("Chartering Indices");
-		pane2.setUseIntegers(true);
+		charterIndicesPane = new IndexPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
+		charterIndicesPane.createControl(sash2);
+		charterIndicesPane.init(Collections.singletonList(PricingPackage.eINSTANCE.getPricingModel_CharterIndices()), editorPart.getAdapterFactory());
+		charterIndicesPane.getViewer().setInput(modelObject);
+		charterIndicesPane.defaultSetTitle("Chartering Indices");
+		charterIndicesPane.setUseIntegers(true);
 
-		charter = new CharterMarketPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
-		charter.createControl(sash3);
-		charter.init(Arrays.asList(new EReference[] { PricingPackage.eINSTANCE.getPricingModel_FleetCost(), PricingPackage.eINSTANCE.getFleetCostModel_CharterCosts() }),
+		fleetCostPane = new CharterMarketPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
+		fleetCostPane.createControl(sash3);
+		fleetCostPane.init(Arrays.asList(new EReference[] { PricingPackage.eINSTANCE.getPricingModel_FleetCost(), PricingPackage.eINSTANCE.getFleetCostModel_CharterCosts() }),
 				editorPart.getAdapterFactory());
-		charter.getViewer().setInput(modelObject);
+		fleetCostPane.getViewer().setInput(modelObject);
 
-		int page = editorPart.addPage(sash);
+		final int page = editorPart.addPage(sash);
 		editorPart.setPageText(page, "Markets");
 	}
 
 	@Override
-	public void setLocked(boolean locked) {
+	public void setLocked(final boolean locked) {
 		route.setLocked(locked);
-		charter.setLocked(locked);
+		fleetCostPane.setLocked(locked);
 		cool.setLocked(locked);
 		port.setLocked(locked);
+		commodityPane.setLocked(locked);
+		charterIndicesPane.setLocked(locked);
+	}
+
+	@Override
+	public boolean canHandle(final IStatus status) {
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+
+			final Object target = dcsd.getTarget();
+			if (target instanceof Index) {
+				return true;
+			}
+			if (target instanceof IndexPoint) {
+				return true;
+			}
+			if (target instanceof FleetCostModel) {
+				return true;
+			}
+			if (target instanceof RouteCost) {
+				return true;
+			}
+			if (target instanceof PortCost) {
+				return true;
+			}
+			if (target instanceof CooldownPrice) {
+				return true;
+			}
+			if (target instanceof CharterCostModel) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void handle(final IStatus status) {
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+
+			Object target = dcsd.getTarget();
+			// Be careful of Index as it can belong to various containers...
+			if (target instanceof IndexPoint<?>) {
+				target = ((IndexPoint<?>) target).eContainer();
+			}
+
+			if (target instanceof Index<?>) {
+				final Index<?> index = (Index<?>) target;
+				// get container reference
+
+				if (index.eContainingFeature() == PricingPackage.eINSTANCE.getPricingModel_CharterIndices()) {
+					editorPart.setActivePage(indexPage);
+					charterIndicesPane.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+					return;
+				}
+				if (index.eContainingFeature() == PricingPackage.eINSTANCE.getPricingModel_CommodityIndices()) {
+					editorPart.setActivePage(indexPage);
+					commodityPane.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+					return;
+				}
+			}
+			if (target instanceof CooldownPrice) {
+				editorPart.setActivePage(costsPage);
+				cool.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+				return;
+			}
+			if (target instanceof RouteCost) {
+				editorPart.setActivePage(costsPage);
+				route.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+				return;
+			}
+			if (target instanceof PortCost) {
+				editorPart.setActivePage(costsPage);
+				port.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+				return;
+			}
+			if (target instanceof CharterCostModel) {
+				editorPart.setActivePage(indexPage);
+				fleetCostPane.getScenarioViewer().setSelection(new StructuredSelection(target), true);
+				return;
+			}
+		}
 	}
 }
