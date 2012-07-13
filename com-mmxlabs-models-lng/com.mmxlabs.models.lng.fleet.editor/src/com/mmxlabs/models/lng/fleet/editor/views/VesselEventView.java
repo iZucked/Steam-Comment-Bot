48037874ae1778b2d22021a49c29eb079ba0b4be
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-13 09:09:33 -0100

    Link up to validation status goto interface

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/editor/views/VesselEventView.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/editor/views/VesselEventView.java
@@ -6,12 +6,16 @@ package com.mmxlabs.models.lng.fleet.editor.views;
 
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.StructuredSelection;
 
 import com.mmxlabs.models.lng.fleet.FleetModel;
 import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.VesselEvent;
 import com.mmxlabs.models.lng.fleet.ui.editorpart.VesselEventViewerPane;
 import com.mmxlabs.models.lng.ui.views.ScenarioTableViewerView;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class VesselEventView extends ScenarioTableViewerView<VesselEventViewerPane> {
 
@@ -21,11 +25,29 @@ public class VesselEventView extends ScenarioTableViewerView<VesselEventViewerPa
 	}
 
 	@Override
-	protected void initViewerPane(VesselEventViewerPane pane) {
-		pane.init(Arrays
-				.asList(new EReference[] { FleetPackage.eINSTANCE
-						.getFleetModel_VesselEvents() }), null);
-		pane.getViewer().setInput(
-				getRootObject().getSubModel(FleetModel.class));
+	protected void initViewerPane(final VesselEventViewerPane pane) {
+		pane.init(Arrays.asList(new EReference[] { FleetPackage.eINSTANCE.getFleetModel_VesselEvents() }), null);
+		pane.getViewer().setInput(getRootObject().getSubModel(FleetModel.class));
+	}
+
+	@Override
+	public void openStatus(final IStatus status) {
+		if (status.isMultiStatus()) {
+			// Try first element
+			openStatus(status.getChildren()[0]);
+		}
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			VesselEvent object = null;
+			if (dcsd.getTarget() instanceof VesselEvent) {
+				object = (VesselEvent) dcsd.getTarget();
+			}
+			if (object != null) {
+				getSite().getPage().activate(this);
+				setSelection(new StructuredSelection(object), true);
+			}
+		}
 	}
 }
