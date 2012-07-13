eff57968e9ac02cb4b9075acd2c5c1b3fb472a6f
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-13 09:04:53 -0100

    Link up to validation status goto interface

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/editor/views/PortView.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/editor/views/PortView.java
@@ -6,13 +6,18 @@ package com.mmxlabs.models.lng.port.editor.views;
 
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.StructuredSelection;
 
+import com.mmxlabs.models.lng.port.Location;
+import com.mmxlabs.models.lng.port.Port;
 import com.mmxlabs.models.lng.port.PortModel;
 import com.mmxlabs.models.lng.port.PortPackage;
 import com.mmxlabs.models.lng.port.editor.actions.MergePorts;
 import com.mmxlabs.models.lng.port.ui.editorpart.PortEditorPane;
 import com.mmxlabs.models.lng.ui.views.ScenarioTableViewerView;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class PortView extends ScenarioTableViewerView<PortEditorPane> {
 	/**
@@ -30,13 +35,36 @@ public class PortView extends ScenarioTableViewerView<PortEditorPane> {
 	}
 
 	@Override
-	protected void initViewerPane(PortEditorPane pane) {
+	protected void initViewerPane(final PortEditorPane pane) {
 		pane.init(Arrays.asList(new EReference[] { PortPackage.eINSTANCE.getPortModel_Ports() }), null);
 		pane.getViewer().setInput(getRootObject().getSubModel(PortModel.class));
 
 		// Add action to create and edit cargo groups
 		pane.getToolBarManager().appendToGroup("edit", new MergePorts(this, pane.getScenarioViewer()));
 		pane.getToolBarManager().update(true);
+	}
 
+	@Override
+	public void openStatus(final IStatus status) {
+		if (status.isMultiStatus()) {
+			// Try first element
+			openStatus(status.getChildren()[0]);
+		}
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			Port port = null;
+			if (dcsd.getTarget() instanceof Port) {
+				port = (Port) dcsd.getTarget();
+			} else if (dcsd.getTarget() instanceof Location) {
+				final Location location = (Location) dcsd.getTarget();
+				port = (Port) location.eContainer();
+			}
+			if (port != null) {
+				getSite().getPage().activate(this);
+				setSelection(new StructuredSelection(port), true);
+			}
+		}
 	}
 }
