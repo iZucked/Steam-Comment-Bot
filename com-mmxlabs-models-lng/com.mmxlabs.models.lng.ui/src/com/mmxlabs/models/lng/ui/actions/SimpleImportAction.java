732cd0dd5fdcf2eed2e249c6cc205f4b1669266f
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-16 09:36:42 -0100

    Add slf4j logging. Do not call canExecute() - let the command stack handle it

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/actions/SimpleImportAction.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/actions/SimpleImportAction.java
@@ -12,6 +12,8 @@ import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.swt.widgets.FileDialog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewer;
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
@@ -25,7 +27,10 @@ import com.mmxlabs.models.util.importer.impl.DefaultImportContext;
  * 
  */
 public class SimpleImportAction extends ImportAction {
-	private ScenarioTableViewer viewer;
+
+	private static final Logger log = LoggerFactory.getLogger(SimpleImportAction.class);
+
+	private final ScenarioTableViewer viewer;
 
 	public SimpleImportAction(final IScenarioEditingLocation part, final ScenarioTableViewer viewer) {
 		super(part);
@@ -52,14 +57,10 @@ public class SimpleImportAction extends ImportAction {
 			reader = new CSVReader(path);
 			final Collection<EObject> importedObjects = importer.importObjects(containment.getEReferenceType(), reader, context);
 			context.run();
-			Command cmd = mergeImports(container, containment, importedObjects);
-			if (cmd.canExecute()) {
-				part.getEditingDomain().getCommandStack().execute(cmd);
-			} else {
-				throw new RuntimeException("Error running import command");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			final Command cmd = mergeImports(container, containment, importedObjects);
+			part.getEditingDomain().getCommandStack().execute(cmd);
+		} catch (final IOException e) {
+			log.error(e.getMessage(), e);
 		}
 	}
 
