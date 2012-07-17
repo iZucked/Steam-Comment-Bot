ca01e030555df11f2221cde44d1dd49b9f5911bd
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-17 13:45:37 -0100

    Doc

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/IScenarioInstanceProvider.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/IScenarioInstanceProvider.java
@@ -6,6 +6,12 @@ package com.mmxlabs.models.ui;
 
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
+/**
+ * An interface to provide {@link ScenarioInstance}s.
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public interface IScenarioInstanceProvider {
 	public ScenarioInstance getScenarioInstance();
 }
