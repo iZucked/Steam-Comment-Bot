940f9704585f8a72cb7ce959e7c86a52e3cf1444
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-30 11:26:41 -0100

    Update tests for LSO & AbstractSequenceOptimiser hierarchy change.

---
+++ com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/core/impl/IntegerElement.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.optimiser.core.impl;
+
+import com.mmxlabs.optimiser.core.ISequenceElement;
+
+public class IntegerElement implements ISequenceElement, Comparable<IntegerElement> {
+
+	private final int idx;
+
+	public IntegerElement(final int idx) {
+		this.idx = idx;
+	}
+
+	@Override
+	public int getIndex() {
+		return idx;
+	}
+
+	@Override
+	public String getName() {
+		return "" + idx;
+	}
+
+	@Override
+	public int compareTo(final IntegerElement o) {
+		return idx - o.idx;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+
+		if (obj instanceof IntegerElement) {
+			return idx == ((IntegerElement) obj).idx;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return idx;
+	}
+}
---
+++ com-mmxlabs-optimiser/optimiser-core/src/test/java/com/mmxlabs/optimiser/core/impl/OptimiserTestUtil.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.optimiser.core.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.common.indexedobjects.IIndexingContext;
+import com.mmxlabs.common.indexedobjects.impl.SimpleIndexingContext;
+import com.mmxlabs.optimiser.core.IModifiableSequence;
+import com.mmxlabs.optimiser.core.IModifiableSequences;
+import com.mmxlabs.optimiser.core.IResource;
+import com.mmxlabs.optimiser.core.ISequenceElement;
+
+public class OptimiserTestUtil {
+	public static IIndexingContext index = new SimpleIndexingContext();
+
+	/**
+	 * Create a {@link IModifiableSequence} from an arbitrary list of inputs of the same type.
+	 * 
+	 * @param elements
+	 * @return
+	 */
+	public static IModifiableSequence makeSequence(final int... integers) {
+		final List<ISequenceElement> elementsList = makeList(integers);
+		return new ListModifiableSequence(elementsList);
+	}
+
+	public static List<ISequenceElement> makeList(final int... integers) {
+		final List<ISequenceElement> elementsList = new ArrayList<ISequenceElement>();
+		for (final int i : integers) {
+			elementsList.add(new IntegerElement(i));
+		}
+		return elementsList;
+	}
+
+	/**
+	 * Create a {@link IModifiableSequence} from an arbitrary list of inputs of the same type.
+	 * 
+	 * @param elements
+	 * @return
+	 */
+	public static IModifiableSequence makeSequence(final ISequenceElement... elements) {
+		final List<ISequenceElement> elementsList = CollectionsUtil.makeArrayList(elements);
+		return new ListModifiableSequence(elementsList);
+	}
+
+	public static IResource makeResource() {
+		return new Resource(index);
+	}
+
+	/**
+	 * Create a {@link IModifiableSequences} instance with only a single {@link IModifiableSequence}
+	 * 
+	 * @param
+	 * @param res
+	 * @param elements
+	 * @return
+	 */
+	public static IModifiableSequences makeSequences(final IResource res, final ISequenceElement... elements) {
+
+		final Map<IResource, IModifiableSequence> map = CollectionsUtil.makeHashMap(res, makeSequence(elements));
+
+		final IModifiableSequences sequences = new ModifiableSequences(Collections.singletonList(res), map);
+
+		return sequences;
+	}
+
+	public static IModifiableSequences makeSequences(final IResource res, final int... elements) {
+
+		final Map<IResource, IModifiableSequence> map = CollectionsUtil.makeHashMap(res, makeSequence(elements));
+
+		final IModifiableSequences sequences = new ModifiableSequences(Collections.singletonList(res), map);
+
+		return sequences;
+	}
+}
