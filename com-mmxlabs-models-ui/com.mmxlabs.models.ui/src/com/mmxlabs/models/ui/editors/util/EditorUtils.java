154ec4ede20228208fc11b0dcd6b546394a436f2
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-30 09:36:23 -0100

    Improve handling of strings such as "DES Purchase"

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/util/EditorUtils.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/util/EditorUtils.java
@@ -8,20 +8,26 @@ package com.mmxlabs.models.ui.editors.util;
  * Handy methods for editor parts
  * 
  * @author Tom Hinton
- *
+ * 
  */
 public class EditorUtils {
 	public static String unmangle(final String name) {
 		final StringBuilder sb = new StringBuilder();
-		boolean lastWasLower = true;
+		boolean lastWasLower = false;
+		boolean lastLastWasLower = false;
 		boolean firstChar = true;
 		for (final char c : name.toCharArray()) {
 			if (firstChar) {
 				sb.append(Character.toUpperCase(c));
+				lastWasLower = Character.isLowerCase(c);
 				firstChar = false;
 			} else {
-				if (lastWasLower && Character.isUpperCase(c))
+				if (lastWasLower && Character.isUpperCase(c)) {
 					sb.append(" ");
+				} else if (!lastLastWasLower && !lastWasLower && !Character.isUpperCase(c)) {
+					sb.insert(sb.length() - 1, " ");
+				}
+				lastLastWasLower = lastWasLower;
 				lastWasLower = Character.isLowerCase(c);
 				sb.append(c);
 			}
