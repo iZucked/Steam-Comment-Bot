d3a1242c6d9f8b9e84bb14d2c0d8da1513370862
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-07-31 12:16:16 -0100

    Change virtual vessel constraint a bit to also check for the expected start, end and fixed elements are present in the route.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/constraints/impl/VirtualVesselConstraintChecker.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/constraints/impl/VirtualVesselConstraintChecker.java
@@ -12,41 +12,48 @@ import com.mmxlabs.optimiser.core.IResource;
 import com.mmxlabs.optimiser.core.ISequence;
 import com.mmxlabs.optimiser.core.ISequenceElement;
 import com.mmxlabs.optimiser.core.ISequences;
-import com.mmxlabs.optimiser.core.constraints.IConstraintChecker;
+import com.mmxlabs.optimiser.core.constraints.IPairwiseConstraintChecker;
 import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
-import com.mmxlabs.scheduler.optimiser.components.IDischargeOption;
-import com.mmxlabs.scheduler.optimiser.components.IDischargeSlot;
-import com.mmxlabs.scheduler.optimiser.components.ILoadOption;
-import com.mmxlabs.scheduler.optimiser.components.ILoadSlot;
+import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
 import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
 import com.mmxlabs.scheduler.optimiser.components.IVessel;
 import com.mmxlabs.scheduler.optimiser.components.VesselInstanceType;
 import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProvider;
+import com.mmxlabs.scheduler.optimiser.providers.IPortTypeProvider;
+import com.mmxlabs.scheduler.optimiser.providers.IStartEndRequirementProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
+import com.mmxlabs.scheduler.optimiser.providers.IVirtualVesselSlotProvider;
+import com.mmxlabs.scheduler.optimiser.providers.PortType;
 
 /**
  * Check that each {@link IVessel} with {@link VesselInstanceType} of {@link VesselInstanceType#VIRTUAL VIRTUAL} has a maximum of four elements on its corresponding {@link ISequence}, and that at
- * least one of those elements is an un-shipped port slot (a FOB sale or DES purchase). Other sequencing rules will ensure that the load/discharge pairing is correct.
+ * least one of those elements is an un-shipped port slot (a FOB sale or DES purchase). Other sequencing rules will ensure that the load/discharge pairing is correct. Additionally, it also checks that
+ * virtual vessels have the correct elements. A virtual vessel always has the same start and end elements. If used it always has the same FOB or DES slot.
  * 
  * @author hinton
  * 
  */
-public class VirtualVesselConstraintChecker implements IConstraintChecker {
+public class VirtualVesselConstraintChecker implements IPairwiseConstraintChecker {
 	private final String name;
-	private final String vesselProviderKey;
-	private final String portSlotProviderKey;
 
 	@Inject
 	private IPortSlotProvider portSlotProvider;
 
 	@Inject
+	private IPortTypeProvider portTypeProvider;
+
+	@Inject
 	private IVesselProvider vesselProvider;
+
+	@Inject
+	private IStartEndRequirementProvider startEndProvider;
+
+	@Inject
+	private IVirtualVesselSlotProvider virtualVesselSlotProvider;
 
 	public VirtualVesselConstraintChecker(final String name, final String vesselProviderKey, final String portSlotProviderKey) {
 		super();
 		this.name = name;
-		this.vesselProviderKey = vesselProviderKey;
-		this.portSlotProviderKey = portSlotProviderKey;
 	}
 
 	@Override
@@ -64,7 +71,7 @@ public class VirtualVesselConstraintChecker implements IConstraintChecker {
 		for (final IResource resource : sequences.getResources()) {
 			final IVessel vessel = vesselProvider.getVessel(resource);
 			if (vessel.getVesselInstanceType() == VesselInstanceType.VIRTUAL) {
-				if (isInvalid(sequences.getSequence(resource))) {
+				if (isInvalid(resource, sequences.getSequence(resource))) {
 					return false;
 				}
 			}
@@ -75,11 +82,13 @@ public class VirtualVesselConstraintChecker implements IConstraintChecker {
 	/**
 	 * Check whether this sequence is valid for a virtual vessel
 	 * 
+	 * @param resource
+	 * 
 	 * @param sequence
 	 *            - a sequence which should be on a virtual vessel (vessel instance type is VIRTUAL)
 	 * @return true if there is a problem with this sequence, false if the sequence is OK.
 	 */
-	private boolean isInvalid(final ISequence sequence) {
+	private boolean isInvalid(final IResource resource, final ISequence sequence) {
 		if (sequence.size() == 2) {
 			return false;
 		}
@@ -87,36 +96,112 @@ public class VirtualVesselConstraintChecker implements IConstraintChecker {
 			return true;
 		}
 
-		// Check that at one of the slots on the sequence is unshipped.
-		boolean foundOne = false;
+		ISequenceElement prevElement = null;
 		for (final ISequenceElement element : sequence) {
-
-			final IPortSlot portSlot = portSlotProvider.getPortSlot(element);
-			if ((portSlot instanceof ILoadOption) && !(portSlot instanceof ILoadSlot)) {
-				// This check could be disabled if we allow multiple virtual slots on a route
-				if (foundOne) {
-					return true;
+			if (prevElement != null) {
+				if (!checkPairwiseConstraint(prevElement, element, resource)) {
+					return false;
 				}
-				foundOne = true;
-
-			}
-
-			if ((portSlot instanceof IDischargeOption) && !(portSlot instanceof IDischargeSlot)) {
-				// This check could be disabled if we allow multiple virtual slots on a route
-				if (foundOne) {
-					return true;
-				}
-				foundOne = true;
-
+				prevElement = element;
 			}
 		}
 
-		return !foundOne;
+		return true;
 	}
 
 	@Override
 	public void setOptimisationData(final IOptimisationData optimisationData) {
-		vesselProvider = optimisationData.getDataComponentProvider(vesselProviderKey, IVesselProvider.class);
-		portSlotProvider = optimisationData.getDataComponentProvider(portSlotProviderKey, IPortSlotProvider.class);
+		vesselProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_vesselProvider, IVesselProvider.class);
+		portSlotProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_portSlotsProvider, IPortSlotProvider.class);
+		portTypeProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_portTypeProvider, IPortTypeProvider.class);
+		virtualVesselSlotProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_virtualVesselSlotProvider, IVirtualVesselSlotProvider.class);
+		startEndProvider = optimisationData.getDataComponentProvider(SchedulerConstants.DCP_startEndRequirementProvider, IStartEndRequirementProvider.class);
+	}
+
+	@Override
+	public boolean checkPairwiseConstraint(final ISequenceElement first, final ISequenceElement second, final IResource resource) {
+
+		final IVessel vessel = vesselProvider.getVessel(resource);
+		if (vessel.getVesselInstanceType() != VesselInstanceType.VIRTUAL) {
+
+			if (virtualVesselSlotProvider.getVesselForElement(first) != null) {
+				return false;
+			}
+			if (virtualVesselSlotProvider.getVesselForElement(second) != null) {
+				return false;
+			}
+
+			return true;
+		}
+
+		final PortType firstType = portTypeProvider.getPortType(first);
+		final PortType secondType = portTypeProvider.getPortType(second);
+
+		final IPortSlot firstSlot = portSlotProvider.getPortSlot(first);
+		final IPortSlot secondSlot = portSlotProvider.getPortSlot(second);
+
+		final ISequenceElement startElement = startEndProvider.getStartElement(resource);
+		final ISequenceElement endElement = startEndProvider.getEndElement(resource);
+
+		final ISequenceElement elementForVessel = virtualVesselSlotProvider.getElementForVessel(vessel);
+
+		final PortType elementForVesselType = portTypeProvider.getPortType(elementForVessel);
+		if (elementForVesselType == PortType.Load) {
+			// DES Purchase
+			if (firstType == PortType.Start) {
+				if (first != startElement) {
+					return false;
+				}
+				if (second != elementForVessel) {
+					return false;
+				}
+				return true;
+			}
+			if (firstType == PortType.Load) {
+				if (first != elementForVessel) {
+					return false;
+				}
+				if (secondType != PortType.Discharge) {
+					return false;
+				}
+				return true;
+			}
+			if (firstType == PortType.Discharge) {
+				if (second != endElement) {
+					return false;
+				}
+				return true;
+			}
+		} else if (elementForVesselType == PortType.Discharge) {
+			// FOB Sale
+			if (firstType == PortType.Start) {
+				if (first != startElement) {
+					return false;
+				}
+				if (secondType != PortType.Load) {
+					return false;
+				}
+				return true;
+			}
+			if (firstType == PortType.Load) {
+				if (second != elementForVessel) {
+					return false;
+				}
+				return true;
+			}
+			if (firstType == PortType.Discharge) {
+				if (second != endElement) {
+					return false;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String explain(final ISequenceElement first, final ISequenceElement second, final IResource resource) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
