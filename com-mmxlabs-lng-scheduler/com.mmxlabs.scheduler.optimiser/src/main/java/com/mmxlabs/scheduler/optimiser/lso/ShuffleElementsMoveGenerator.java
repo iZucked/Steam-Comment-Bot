81945411851d41e1b04b677b16558810672d3c9a
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-08-06 10:05:28 -0100

    Create a new move generator to "shuffle" elements. Specifically take a single element and attempt to move it into another sequence. This may require the elements eitherside in both the source and destination sequences to be removed or new elements to be inserted.

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/lso/ShuffleElementsMoveGenerator.java
@@ -0,0 +1,438 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.lso;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.common.RandomHelper;
+import com.mmxlabs.optimiser.common.dcproviders.IOptionalElementsProvider;
+import com.mmxlabs.optimiser.common.dcproviders.IResourceAllocationConstraintDataComponentProvider;
+import com.mmxlabs.optimiser.core.IResource;
+import com.mmxlabs.optimiser.core.ISequence;
+import com.mmxlabs.optimiser.core.ISequenceElement;
+import com.mmxlabs.optimiser.core.ISequences;
+import com.mmxlabs.optimiser.lso.IMove;
+import com.mmxlabs.scheduler.optimiser.SchedulerConstants;
+import com.mmxlabs.scheduler.optimiser.components.IDischargeOption;
+import com.mmxlabs.scheduler.optimiser.components.ILoadOption;
+import com.mmxlabs.scheduler.optimiser.lso.ConstrainedMoveGenerator.Followers;
+import com.mmxlabs.scheduler.optimiser.lso.moves.ShuffleElements.ShuffleElementsBuilder;
+
+/**
+ * A module for the {@link ConstrainedMoveGenerator} handles moves on a per slot basis. This moved generator selects single slot and attempts to move it next to another preceder or follower in another
+ * {@link ISequence}. Typically slots need to be grouped as a pair of {@link ILoadOption} - {@link IDischargeOption}. This means moving a single slot will often require moving another slot or two.
+ * This move generator will attempt to remove or add a new slot on the destination side to maintain the pairs. It will also try to do the same on the source {@link ISequence}. Typically removing
+ * additional slots will either place the slot in the {@link ISequences#getUnusedElements()} list or attempt to find another unused slot and place it into another {@link ISequence}.
+ * 
+ * @author Simon Goodall.
+ * 
+ */
+public class ShuffleElementsMoveGenerator implements IConstrainedMoveGeneratorUnit {
+	private final ConstrainedMoveGenerator owner;
+	private final IOptionalElementsProvider optionalElementsProvider;
+	private final IResourceAllocationConstraintDataComponentProvider racDCP;
+
+	private final List<ISequenceElement> targetElements;
+
+	public ShuffleElementsMoveGenerator(final ConstrainedMoveGenerator owner) {
+		super();
+		this.owner = owner;
+		this.optionalElementsProvider = owner.context.getOptimisationData().getDataComponentProvider(SchedulerConstants.DCP_optionalElementsProvider, IOptionalElementsProvider.class);
+		this.racDCP = owner.context.getOptimisationData().getDataComponentProvider(SchedulerConstants.DCP_resourceAllocationProvider, IResourceAllocationConstraintDataComponentProvider.class);
+
+		targetElements = new ArrayList<ISequenceElement>(owner.context.getOptimisationData().getSequenceElements().size());
+		// Determine possible slots which can be moved.
+		for (final ISequenceElement e : owner.context.getOptimisationData().getSequenceElements()) {
+			// TODO: check new API - null might be events or start/ends?
+			// TODO: Really need port type in here
+			final Collection<IResource> resources = racDCP.getAllowedResources(e);
+			if ((resources != null && resources.size() > 1) || optionalElementsProvider.isElementOptional(e)) {
+				targetElements.add(e);
+			}
+		}
+	}
+
+	@Override
+	public void setSequences(final ISequences sequences) {
+
+	}
+
+	@Override
+	public IMove generateMove() {
+
+		final ShuffleElementsBuilder builder = new ShuffleElementsBuilder();
+
+		// Set of elements already considered in our move. They should no longer be considered available or in the follower or preceders lists
+		final Set<ISequenceElement> touchedElements = new HashSet<ISequenceElement>();
+		final ISequenceElement element = RandomHelper.chooseElementFrom(owner.getRandom(), targetElements);
+		final Pair<Integer, Integer> elementPosition = owner.reverseLookup.get(element);
+		final IResource elementResource = elementPosition.getFirst() == null ? null : owner.getSequences().getResources().get(elementPosition.getFirst());
+		final ISequence elementSequence = owner.getSequences().getSequence(elementResource);
+		touchedElements.add(element);
+
+		boolean foundMove = false;
+		// Link to a follower or preceder
+		final boolean findFollower = owner.getRandom().nextBoolean();
+		// Fix up destination part
+		if (findFollower) {
+			final Followers<ISequenceElement> followers = owner.validFollowers.get(element);
+			if (followers.size() == 0) {
+				return null;
+			}
+			for (final ISequenceElement follower : shuffleFollowers(followers)) {
+				final Pair<Integer, Integer> followerPosition = owner.reverseLookup.get(follower);
+
+				// Element is also not used!
+				if (followerPosition.getFirst() == null) {
+					continue;
+				}
+
+				// Check we can move our element to this resource
+				final IResource followerResource = owner.getSequences().getResources().get(followerPosition.getFirst());
+				if (followerResource == elementResource) {
+					continue;
+				}
+				if (!checkResource(element, followerResource)) {
+					continue;
+				}
+
+				final ISequence followerSequence = owner.getSequences().getSequence(followerResource);
+
+				final ISequenceElement followerMinus1 = followerSequence.get(followerPosition.getSecond() - 1);
+
+				if (owner.validFollowers.get(followerMinus1).contains(element)) {
+					// We can link the head and tail together directly - no need to do more.
+					builder.addFrom(elementResource, elementPosition.getSecond(), element);
+					builder.addTo(followerResource, followerPosition.getSecond(), 1);
+					foundMove = true;
+					break;
+				}
+				if (followerPosition.getSecond() > 2) {
+					final ISequenceElement followerMinus2 = followerSequence.get(followerPosition.getSecond() - 2);
+					if (owner.validFollowers.get(followerMinus2).contains(element)) {
+
+						if (removeOrBackfill(builder, followerMinus1, touchedElements)) {
+							builder.addFrom(elementResource, elementPosition.getSecond(), element);
+							builder.addTo(followerResource, followerPosition.getSecond(), 1);
+							// Yey1
+							touchedElements.add(followerMinus1);
+							foundMove = true;
+							break;
+						}
+					}
+				}
+				{
+					final Set<ISequenceElement> candidates = findPossibleUnusedElement(followerResource, owner.validFollowers.get(followerMinus1), owner.validPreceeders.get(element));
+					candidates.removeAll(touchedElements);
+					if (candidates.isEmpty()) {
+						continue;
+					}
+					final List<ISequenceElement> l = new ArrayList<ISequenceElement>(candidates);
+
+					// pick random candidate
+					{
+						final ISequenceElement candidate = l.get(owner.getRandom().nextInt(l.size()));
+						final Pair<Integer, Integer> candidatePosition = owner.reverseLookup.get(candidate);
+						// Finally! an element we can add to the sequence to make it valid.
+						builder.addFrom(null, candidatePosition.getSecond());
+						builder.addFrom(elementResource, elementPosition.getSecond(), element);
+						builder.addTo(followerResource, followerPosition.getSecond(), 2);
+
+						touchedElements.add(follower);
+						touchedElements.add(candidate);
+						foundMove = true;
+						break;
+
+					}
+				}
+			}
+
+		} else {
+			final Followers<ISequenceElement> preceders = owner.validPreceeders.get(element);
+			if (preceders.size() == 0) {
+				return null;
+			}
+			// TODO: Randomise list
+			for (final ISequenceElement preceder : shuffleFollowers(preceders)) {
+				final Pair<Integer, Integer> precederPosition = owner.reverseLookup.get(preceder);
+				// Element is also not used!
+				if (precederPosition.getFirst() == null) {
+					continue;
+				}
+
+				// Check we can move our element to this resource
+				final IResource precederResource = owner.getSequences().getResources().get(precederPosition.getFirst());
+				if (precederResource == elementResource) {
+					continue;
+				}
+				if (!checkResource(element, precederResource)) {
+					continue;
+				}
+				final ISequence precederSequence = owner.getSequences().getSequence(precederResource);
+
+				final ISequenceElement precederPlus1 = precederSequence.get(precederPosition.getSecond() + 1);
+
+				if (owner.validPreceeders.get(precederPlus1).contains(element)) {
+					builder.addFrom(elementResource, elementPosition.getSecond(), element);
+					builder.addTo(precederResource, precederPosition.getSecond() + 1, 1);
+					// We can link the head and tail together directly - no need to do more.
+					foundMove = true;
+					break;
+				}
+
+				if (precederSequence.size() > precederPosition.getSecond() + 2) {
+					final ISequenceElement precederPlus2 = precederSequence.get(precederPosition.getSecond() + 2);
+					if (owner.validPreceeders.get(precederPlus2).contains(element)) {
+						if (removeOrBackfill(builder, precederPlus1, touchedElements)) {
+							builder.addFrom(elementResource, elementPosition.getSecond(), element);
+							builder.addTo(precederResource, precederPosition.getSecond() + 1, 1);
+
+							touchedElements.add(precederPlus1);
+							foundMove = true;
+							break;
+						}
+					}
+				}
+				{
+
+					final Set<ISequenceElement> candidates = findPossibleUnusedElement(precederResource, owner.validFollowers.get(element), owner.validPreceeders.get(precederPlus1));
+					candidates.removeAll(touchedElements);
+					if (candidates.isEmpty()) {
+						continue;
+					}
+					final List<ISequenceElement> l = new ArrayList<ISequenceElement>(candidates);
+
+					// pick random candidate
+					{
+						final ISequenceElement candidate = l.get(owner.getRandom().nextInt(l.size()));
+						final Pair<Integer, Integer> candidatePosition = owner.reverseLookup.get(candidate);
+						// Finally! an element we can add to the sequence to make it valid.
+						builder.addFrom(elementResource, elementPosition.getSecond(), element);
+						builder.addFrom(null, candidatePosition.getSecond(), candidate);
+						builder.addTo(precederResource, precederPosition.getSecond() + 1, 2);
+
+						touchedElements.add(preceder);
+						touchedElements.add(candidate);
+						foundMove = true;
+						break;
+
+					}
+				}
+			}
+		}
+		if (!foundMove) {
+			return null;
+		}
+
+		// Fix up source part
+		{
+			if (elementPosition.getFirst() == null) {
+				// Element is unused, no need to do more.
+				return builder.getMove();
+			}
+			final ISequenceElement elementMinus1 = elementSequence.get(elementPosition.getSecond() - 1);
+			final ISequenceElement elementPlus1 = elementSequence.get(elementPosition.getSecond() + 1);
+
+			if (owner.validFollowers.get(elementMinus1).contains(elementPlus1)) {
+				// We can link the head and tail together directly - no need to do more.
+				return builder.getMove();
+			}
+
+			// Head and tail cannot be joined together. We have three options. Find an unused element to insert into the gap, remove element - 1 or remove element + 1. If we remove the element,
+			{
+				// Find optional element
+				final Set<ISequenceElement> candidates = findPossibleUnusedElement(elementResource, owner.validFollowers.get(elementMinus1), owner.validPreceeders.get(elementPlus1));
+				candidates.removeAll(touchedElements);
+				if (!candidates.isEmpty()) {
+					final List<ISequenceElement> l = new ArrayList<ISequenceElement>(candidates);
+
+					// pick random candidate
+					final ISequenceElement candidate = l.get(owner.getRandom().nextInt(l.size()));
+					final Pair<Integer, Integer> candidatePosition = owner.reverseLookup.get(candidate);
+					// Finally! an element we can add to the sequence to make it valid.
+					builder.addFrom(null, candidatePosition.getSecond(), candidate);
+					builder.addTo(elementResource, elementPosition.getSecond(), 1);
+
+					touchedElements.add(candidate);
+
+					return builder.getMove();
+				}
+
+			}
+			if (elementPosition.getSecond() > 2) {
+				final ISequenceElement elementMinus2 = elementSequence.get(elementPosition.getSecond() - 2);
+				if (owner.validFollowers.get(elementMinus2).contains(elementPlus1)) {
+					// We can link the head and tail together directly - no need to do more.
+					if (removeOrBackfill(builder, elementMinus1, touchedElements)) {
+						touchedElements.add(elementMinus1);
+						return builder.getMove();
+					}
+				}
+			}
+			if (elementSequence.size() > elementPosition.getSecond() + 2) {
+				final ISequenceElement elementPlus2 = elementSequence.get(elementPosition.getSecond() + 2);
+				if (owner.validFollowers.get(elementMinus1).contains(elementPlus2)) {
+					if (removeOrBackfill(builder, elementPlus1, touchedElements)) {
+						touchedElements.add(elementPlus1);
+
+						return builder.getMove();
+					}
+				}
+
+			}
+		}
+
+		return null;
+
+	}
+
+	private boolean removeOrBackfill(final ShuffleElementsBuilder builder, final ISequenceElement target, final Set<ISequenceElement> touchedElements) {
+		// We can link the smaller head and tail together directly - need to do something the the extra element
+		final Pair<Integer, Integer> targetPosition = owner.reverseLookup.get(target);
+		final IResource targetResource = owner.getSequences().getResources().get(targetPosition.getFirst());
+		if (optionalElementsProvider.isElementOptional(target) && owner.getRandom().nextBoolean()) {
+			// Can stick on unused elements list
+			builder.addFrom(targetResource, targetPosition.getSecond(), target);
+			builder.addTo(null, 0, 1);
+			return true;
+		}
+		// Else to to find a backfill option
+		if (owner.getRandom().nextBoolean()) {
+			// Find a preceder
+			final Followers<ISequenceElement> preceders = owner.validPreceeders.get(target);
+			// TODO: randomise
+			for (final ISequenceElement p : shuffleFollowers(preceders)) {
+				if (touchedElements.contains(p)) {
+					continue;
+				}
+				final Pair<Integer, Integer> position = owner.reverseLookup.get(p);
+				if (position.getFirst() == null) {
+					continue;
+				}
+				final IResource resource = owner.getSequences().getResources().get(position.getFirst());
+				final ISequence sequence = owner.getSequences().getSequence(resource);
+
+				final Set<ISequenceElement> candidates = findPossibleUnusedElement(resource, owner.validFollowers.get(target), owner.validPreceeders.get(sequence.get(position.getSecond() + 1)));
+				candidates.removeAll(touchedElements);
+				if (candidates.isEmpty()) {
+					continue;
+				}
+				final List<ISequenceElement> l = new ArrayList<ISequenceElement>(candidates);
+
+				// pick random candidate
+				{
+					final ISequenceElement candidate = l.get(owner.getRandom().nextInt(l.size()));
+					final Pair<Integer, Integer> candidatePosition = owner.reverseLookup.get(candidate);
+					// Finally! an element we can add to the sequence to make it valid.
+					builder.addFrom(targetResource, targetPosition.getSecond(), target);
+					builder.addFrom(null, candidatePosition.getSecond(), candidate);
+					builder.addTo(resource, position.getSecond(), 2);
+
+					touchedElements.add(p);
+					touchedElements.add(candidate);
+
+					return true;
+				}
+			}
+
+			return false;
+		} else {
+			// // Find a follower
+			final Followers<ISequenceElement> followers = owner.validFollowers.get(target);
+			// TODO: randomise
+			for (final ISequenceElement f : shuffleFollowers(followers)) {
+				if (touchedElements.contains(f)) {
+					continue;
+				}
+				final Pair<Integer, Integer> position = owner.reverseLookup.get(f);
+				if (position.getFirst() == null) {
+					continue;
+				}
+				final IResource resource = owner.getSequences().getResources().get(position.getFirst());
+				final ISequence sequence = owner.getSequences().getSequence(resource);
+
+				final Set<ISequenceElement> candidates = findPossibleUnusedElement(resource, owner.validFollowers.get(sequence.get(position.getSecond() - 1)), owner.validPreceeders.get(target));
+				candidates.removeAll(touchedElements);
+				if (candidates.isEmpty()) {
+					continue;
+				}
+				final List<ISequenceElement> l = new ArrayList<ISequenceElement>(candidates);
+
+				// pick random candidate
+				{
+					final ISequenceElement candidate = l.get(owner.getRandom().nextInt(l.size()));
+					final Pair<Integer, Integer> candidatePosition = owner.reverseLookup.get(candidate);
+					// Finally! an element we can add to the sequence to make it valid.
+					builder.addFrom(null, candidatePosition.getSecond(), candidate);
+					builder.addFrom(targetResource, targetPosition.getSecond(), target);
+					builder.addTo(resource, position.getSecond(), 2);
+
+					touchedElements.add(f);
+					touchedElements.add(candidate);
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+
+	private boolean checkResource(final ISequenceElement elmenet, final IResource resource) {
+
+		final Collection<IResource> allowedResources = racDCP.getAllowedResources(elmenet);
+		if (allowedResources == null) {
+			return true;
+		}
+		return allowedResources.contains(resource);
+	}
+
+	private Set<ISequenceElement> findPossibleUnusedElement(final IResource resource, final ConstrainedMoveGenerator.Followers<ISequenceElement> followers,
+			final ConstrainedMoveGenerator.Followers<ISequenceElement> preceeders) {
+		final Set<ISequenceElement> possibleFillers = new LinkedHashSet<ISequenceElement>();
+		{ // Find the set of unused elements which could be replace candidate
+			for (final ISequenceElement e : followers) {
+				if (checkResource(e, resource)) {
+					possibleFillers.add(e);
+				}
+			}
+			if (!possibleFillers.isEmpty()) {
+				final HashSet<ISequenceElement> temp = new HashSet<ISequenceElement>();
+				for (final ISequenceElement e : preceeders) {
+					if (checkResource(e, resource)) {
+						temp.add(e);
+					}
+				}
+				possibleFillers.retainAll(temp);
+			}
+			if (!possibleFillers.isEmpty()) {
+
+				final HashSet<ISequenceElement> temp = new HashSet<ISequenceElement>();
+				for (final ISequenceElement e : owner.sequences.getUnusedElements()) {
+					if (checkResource(e, resource)) {
+						temp.add(e);
+					}
+				}
+				possibleFillers.retainAll(temp);
+			}
+		}
+		return possibleFillers;
+	}
+
+	private List<ISequenceElement> shuffleFollowers(final ConstrainedMoveGenerator.Followers<ISequenceElement> followers) {
+		final List<ISequenceElement> l = Lists.newArrayList(followers);
+		Collections.shuffle(l, owner.getRandom());
+		return l;
+
+	}
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/lso/moves/ShuffleElements.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.lso.moves;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.mmxlabs.optimiser.core.IModifiableSequence;
+import com.mmxlabs.optimiser.core.IModifiableSequences;
+import com.mmxlabs.optimiser.core.IResource;
+import com.mmxlabs.optimiser.core.ISequence;
+import com.mmxlabs.optimiser.core.ISequenceElement;
+import com.mmxlabs.optimiser.core.ISequences;
+import com.mmxlabs.optimiser.lso.IMove;
+
+/**
+ * The {@link ShuffleElements} is a {@link IMove} to manipulate multiple {@link ISequenceElement}s in various places in the {@link ISequences}. Typically it will be used to combine two
+ * {@link ISequenceElement}s from different places and insert them into another part of the {@link ISequences}. A helper class, {@link ShuffleElementsBuilder} can be used to build up the individual
+ * steps. The move consists of a set of {@link From} instructions and a set of {@link To} instructions. A {@link From} instruction references a single {@link ISequenceElement}. A {@link To} element
+ * references an insertion point and an expected number of elements. The {@link From} and {@link To} lists are ordered - the {@link From} elements are consumed by the {@link To} elements in list
+ * order. For example if we have three {@link From} elements and two {@link To} elements with a size of 2 and 1 respectively, then the first {@link To} will use the first two {@link From} elements and
+ * the second {@link To} will take the third {@link From} element. The total number of {@link From} elements must equal the sum of the size attributes of all the {@link To} elements.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class ShuffleElements implements IMove {
+
+	/**
+	 * A helper class to build up the inputs for the {@link ShuffleElements} move.
+	 * 
+	 * 
+	 */
+	public static final class ShuffleElementsBuilder {
+
+		private final List<From> froms = new LinkedList<From>();
+		private final List<To> tos = new LinkedList<To>();
+
+		/**
+		 * Add a new {@link From} instruction. Use the {@link ISequenceElement} in the {@link ISequence} for the given {@link IResource} at the specified index position.
+		 * 
+		 * @param resource
+		 * @param index
+		 */
+		public void addFrom(final IResource resource, final int index) {
+			froms.add(new From(resource, index));
+		}
+
+		/**
+		 * Add a new {@link From} instruction. Use the {@link ISequenceElement} in the {@link ISequence} for the given {@link IResource} at the specified index position. This variant also stores the
+		 * expected {@link ISequenceElement} used for debugging.
+		 * 
+		 * @param resource
+		 * @param index
+		 */
+		public void addFrom(final IResource resource, final int index, final ISequenceElement element) {
+			froms.add(new From(resource, index, element));
+		}
+
+		/**
+		 * Add a new {@link To} instruction. Insert <i>size</i> elements into the {@link ISequence} for the given {@link IResource} at the given index.
+		 * 
+		 * @param resource
+		 * @param index
+		 * @param size
+		 */
+		public void addTo(final IResource resource, final int index, final int size) {
+			tos.add(new To(resource, index, size));
+		}
+
+		/**
+		 * Construct and return the new {@link ShuffleElements} move. Do not use this {@link ShuffleElementsBuilder} instance again.
+		 * 
+		 * @return
+		 */
+		public ShuffleElements getMove() {
+			return new ShuffleElements(froms.toArray(new From[0]), tos.toArray(new To[0]));
+		}
+	};
+
+	/**
+	 * The {@link From} class specifies an instruction to the {@link ShuffleElements} move as to where to find an element.
+	 * 
+	 */
+	public static final class From {
+		public final IResource resource;
+		public final int index;
+		public final ISequenceElement element;
+
+		public From(final IResource resource, final int index) {
+			this.resource = resource;
+			this.index = index;
+			this.element = null;
+		}
+
+		public From(final IResource resource, final int index, final ISequenceElement element) {
+			this.resource = resource;
+			this.index = index;
+			this.element = element;
+		}
+	}
+
+	/**
+	 * The {@link To} class specifies and instruction to the {@link ShuffleElements} move as to where to insert a sequence of elements.
+	 * 
+	 * @author sg
+	 * 
+	 */
+	public static final class To {
+
+		public final IResource resource;
+		public final int index;
+
+		/**
+		 * The number of elements to insert.
+		 */
+		public final int size;
+
+		public To(final IResource resource, final int index, final int size) {
+			this.resource = resource;
+			this.index = index;
+			this.size = size;
+		}
+	}
+
+	private final From[] froms;
+	private final To[] tos;
+
+	private final Collection<IResource> resources = new LinkedHashSet<IResource>();
+
+	public ShuffleElements(final From[] froms, final To[] tos) {
+		super();
+
+		this.froms = froms;
+		this.tos = tos;
+
+		int fromSize = 0;
+		int toSize = 0;
+		for (final From from : froms) {
+			resources.add(from.resource);
+			fromSize++;
+		}
+		for (final To to_ : tos) {
+			resources.add(to_.resource);
+			toSize += to_.size;
+		}
+		resources.remove(null);
+		if (fromSize != toSize) {
+			throw new IllegalArgumentException("Number of from elements should equal number of to elements sizes");
+		}
+	}
+
+	@Override
+	public Collection<IResource> getAffectedResources() {
+		return resources;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.mmxlabs.optimiser.lso.IMove#apply(com.mmxlabs.optimiser.core.IModifiableSequences)
+	 */
+	@Override
+	public void apply(final IModifiableSequences sequences) {
+
+		final List<ISequenceElement> spare = sequences.getModifiableUnusedElements();
+
+		// Remove the various elements first
+		final List<From> seenFroms = new ArrayList<From>(froms.length);
+		final List<ISequenceElement> insertionElements = new ArrayList<ISequenceElement>(froms.length);
+		for (final From from : froms) {
+			// The offset - if we have already interacted with this ISequence, then the indicies may need adjusting
+			int offset = 0;
+			for (final From seenFrom : seenFroms) {
+				if (from.resource == seenFrom.resource && from.index > seenFrom.index) {
+					++offset;
+				}
+			}
+			final IModifiableSequence seq = from.resource == null ? null : sequences.getModifiableSequence(from.resource);
+			final ISequenceElement e = removeElement(seq, spare, from.index - offset);
+			if (from.element != null) {
+				if (from.element != e) {
+					throw new IllegalStateException("Removing unexpeced element");
+				}
+
+			}
+			insertionElements.add(e);
+			seenFroms.add(from);
+		}
+		// Add the new elements
+
+		final List<To> seenTos = new ArrayList<To>(tos.length);
+		for (final To to_ : tos) {
+			// The offset - if we have already interacted with this ISequence, then the indicies may need adjusting
+			int offset = 0;
+
+			for (final From seenFrom : seenFroms) {
+				if (to_.resource == seenFrom.resource && to_.index > seenFrom.index) {
+					--offset;
+				}
+			}
+
+			for (final To seenTo : seenTos) {
+				if (to_.resource == seenTo.resource && to_.index > seenTo.index) {
+					offset = seenTo.size;
+				}
+			}
+			final IModifiableSequence seq = to_.resource == null ? null : sequences.getModifiableSequence(to_.resource);
+			for (int i = 0; i < to_.size; ++i) {
+				addElement(seq, spare, to_.index + offset++, insertionElements.remove(0));
+			}
+			seenTos.add(to_);
+		}
+
+	}
+
+	private void addElement(final IModifiableSequence seq, final List<ISequenceElement> spare, final int index, final ISequenceElement element) {
+		if (seq == null) {
+			spare.add(index, element);
+		} else {
+			seq.insert(index, element);
+		}
+	}
+
+	private ISequenceElement removeElement(final IModifiableSequence seq, final List<ISequenceElement> spares, final int index) {
+		if (seq == null) {
+			return spares.remove(index);
+		} else {
+			return seq.remove(index);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.mmxlabs.optimiser.lso.IMove#validate(com.mmxlabs.optimiser.core.ISequences)
+	 */
+	@Override
+	public boolean validate(final ISequences sequences) {
+
+		return true;
+	}
+}
