6df267ea088a1b813759cd04de830d5efb395787
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-08-08 08:36:56 -0100

    Fix hidden element filter

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/ScenarioServiceContentProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/ScenarioServiceContentProvider.java
@@ -94,12 +94,14 @@ public class ScenarioServiceContentProvider extends AdapterFactoryContentProvide
 			boolean mayBeShow = false;
 			if (e instanceof Container) {
 				Container container = (Container) e;
+
 				if (container.isHidden() && isShowHiddenElements()) {
 					mayBeShow = true;
+				} else if (container.isHidden()) {
+					mayBeShow = false;
 				} else if (!container.isHidden()) {
 					mayBeShow = true;
-				}
-				if (container.isArchived() || isShowArchivedElements()) {
+				} else if (container.isArchived() || isShowArchivedElements()) {
 					mayBeShow = true;
 				} else if (!container.isArchived()) {
 					mayBeShow = true;
