63ceb8bf0d0d69a04b120769fb0fb7cbed125d81
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-08-15 08:20:25 -0100

    Extract SlotInlineEditorWrapper into top level class. Process missed notifications for use directly in editors rather than just dialogs.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/SlotComponentHelper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/SlotComponentHelper.java
@@ -5,26 +5,17 @@
 package com.mmxlabs.models.lng.cargo.presentation.composites;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 
 import com.mmxlabs.models.lng.cargo.CargoPackage;
-import com.mmxlabs.models.lng.cargo.DischargeSlot;
-import com.mmxlabs.models.lng.cargo.LoadSlot;
-import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.BaseComponentHelper;
 import com.mmxlabs.models.ui.ComponentHelperUtils;
 import com.mmxlabs.models.ui.IComponentHelper;
 import com.mmxlabs.models.ui.IInlineEditorContainer;
-import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
-import com.mmxlabs.models.ui.editors.IInlineEditor;
-import com.mmxlabs.models.ui.editors.impl.IInlineEditorEnablementWrapper;
 import com.mmxlabs.models.ui.editors.impl.NumberInlineEditor;
 
 /**
@@ -34,50 +25,6 @@ import com.mmxlabs.models.ui.editors.impl.NumberInlineEditor;
  */
 public class SlotComponentHelper extends BaseComponentHelper {
 	protected List<IComponentHelper> superClassesHelpers = new ArrayList<IComponentHelper>();
-
-	/**
-	 * @generated NOT
-	 */
-	private class SlotInlineEditorWrapper extends IInlineEditorEnablementWrapper {
-
-		public SlotInlineEditorWrapper(final IInlineEditor wrapped) {
-			super(wrapped);
-		}
-
-		@Override
-		public void reallyNotifyChanged(final Notification notification) {
-			if (notification.getFeature() == CargoPackage.eINSTANCE.getLoadSlot_DESPurchase()) {
-
-				if (input instanceof LoadSlot) {
-					setEnabled(!((LoadSlot) input).isDESPurchase());
-				}
-
-			}
-			if (notification.getFeature() == CargoPackage.eINSTANCE.getDischargeSlot_FOBSale()) {
-				if (input instanceof DischargeSlot) {
-					setEnabled(!((DischargeSlot) input).isFOBSale());
-				}
-			}
-		}
-		
-		@Override
-		public void display(final IScenarioEditingLocation location, MMXRootObject scenario, EObject object, Collection<EObject> range) {
-
-			super.display(location, scenario, object, range);
-			
-			if (input instanceof LoadSlot) {
-				setEnabled(!((LoadSlot) input).isDESPurchase());
-			}
-			else if (input instanceof DischargeSlot) {
-				setEnabled(!((DischargeSlot) input).isFOBSale());
-			}
-		}
-
-		@Override
-		public EObject getEditorTarget() {
-			return wrapped.getEditorTarget();
-		}
-	};
 
 	/**
 	 * Construct a new instance, using the platform adapter manager
