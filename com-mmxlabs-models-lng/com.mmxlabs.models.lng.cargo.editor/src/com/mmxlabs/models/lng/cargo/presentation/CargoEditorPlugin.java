8f03ef629953aa171c5f215ec71be08226248730
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-08-16 08:02:44 -0100

    Use DS rather than programatic command provider service registration

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/CargoEditorPlugin.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/CargoEditorPlugin.java
@@ -8,57 +8,42 @@ import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
 import org.eclipse.emf.common.util.ResourceLocator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 
-import com.mmxlabs.models.common.commandservice.IModelCommandProvider;
-import com.mmxlabs.models.lng.cargo.ui.commands.CargoTypeUpdatingCommandProvider;
-import com.mmxlabs.models.lng.cargo.ui.commands.DateUpdatingCommandProvider;
-import com.mmxlabs.models.lng.cargo.ui.commands.PortUpdatingCommandProvider;
-import com.mmxlabs.models.lng.cargo.ui.commands.SlotNameUpdatingCommandProvider;
 import com.mmxlabs.models.lng.types.provider.LNGTypesEditPlugin;
 import com.mmxlabs.models.mmxcore.provider.MmxcoreEditPlugin;
 
 /**
- * This is the central singleton for the Cargo editor plugin.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the central singleton for the Cargo editor plugin. <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 public final class CargoEditorPlugin extends EMFPlugin {
 	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public static final CargoEditorPlugin INSTANCE = new CargoEditorPlugin();
-	
+
 	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	private static Implementation plugin;
 
 	/**
-	 * Create the instance.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public CargoEditorPlugin() {
-		super
-			(new ResourceLocator [] {
-				LNGTypesEditPlugin.INSTANCE,
-				MmxcoreEditPlugin.INSTANCE,
-			});
+		super(new ResourceLocator[] { LNGTypesEditPlugin.INSTANCE, MmxcoreEditPlugin.INSTANCE, });
 	}
 
 	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return the singleton instance.
 	 * @generated
 	 */
@@ -66,64 +51,49 @@ public final class CargoEditorPlugin extends EMFPlugin {
 	public ResourceLocator getPluginResourceLocator() {
 		return plugin;
 	}
-	
+
 	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return the singleton instance.
 	 * @generated
 	 */
 	public static Implementation getPlugin() {
 		return plugin;
 	}
-	
+
 	/**
-	 * The actual implementation of the Eclipse <b>Plugin</b>.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The actual implementation of the Eclipse <b>Plugin</b>. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated NOT services
 	 */
 	public static class Implementation extends EclipseUIPlugin {
-		private ServiceRegistration<IModelCommandProvider> dateCorrectorRegistration;
-		private ServiceRegistration<IModelCommandProvider> portCorrectorRegistration;
-		private ServiceRegistration<IModelCommandProvider> slotNameCorrectorRegistration;
-		private ServiceRegistration<IModelCommandProvider> cargoTypeUpdaterRegistration;
-
 		/**
-		 * Creates an instance.
-		 * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
+		 * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+		 * 
 		 * @generated NOT
 		 */
 		public Implementation() {
 			super();
-	
+
 			// Remember the static instance.
 			//
 			plugin = this;
 		}
 
-		
 		@Override
 		public void start(BundleContext context) throws Exception {
 			super.start(context);
-			dateCorrectorRegistration = context.registerService(IModelCommandProvider.class, new DateUpdatingCommandProvider(), null);
-			portCorrectorRegistration = context.registerService(IModelCommandProvider.class, new PortUpdatingCommandProvider(), null);
-			slotNameCorrectorRegistration = context.registerService(IModelCommandProvider.class, new SlotNameUpdatingCommandProvider(), null);
-			cargoTypeUpdaterRegistration = context.registerService(IModelCommandProvider.class, new CargoTypeUpdatingCommandProvider(), null);
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 		 */
 		@Override
 		public void stop(BundleContext context) throws Exception {
-			dateCorrectorRegistration.unregister();
-			portCorrectorRegistration.unregister();
-			slotNameCorrectorRegistration.unregister();
-			cargoTypeUpdaterRegistration.unregister();
-			
+
 			super.stop(context);
 		}
 	}
