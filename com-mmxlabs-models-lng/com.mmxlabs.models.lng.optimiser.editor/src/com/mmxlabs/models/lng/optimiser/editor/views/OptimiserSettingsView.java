db17e098c141913a780f846478aa884dc73b4ee2
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-08-29 08:54:45 -0100

    Make optimiser settings tab into a view

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.optimiser.editor/src/com/mmxlabs/models/lng/optimiser/editor/views/OptimiserSettingsView.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.optimiser.editor.views;
+
+import java.util.Arrays;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.StructuredSelection;
+
+import com.mmxlabs.models.lng.optimiser.OptimiserModel;
+import com.mmxlabs.models.lng.optimiser.OptimiserPackage;
+import com.mmxlabs.models.lng.optimiser.OptimiserSettings;
+import com.mmxlabs.models.lng.optimiser.ui.editorpart.OptimiserSettingsEditorPane;
+import com.mmxlabs.models.lng.ui.views.ScenarioTableViewerView;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+public class OptimiserSettingsView extends ScenarioTableViewerView<OptimiserSettingsEditorPane> {
+	@Override
+	protected OptimiserSettingsEditorPane createViewerPane() {
+		return new OptimiserSettingsEditorPane(getSite().getPage(), this, this, getViewSite().getActionBars());
+	}
+
+	@Override
+	protected void initViewerPane(final OptimiserSettingsEditorPane pane) {
+		pane.init(Arrays.asList(new EReference[] { OptimiserPackage.eINSTANCE.getOptimiserModel_Settings() }), null);
+		pane.getViewer().setInput(getRootObject().getSubModel(OptimiserModel.class));
+	}
+
+	@Override
+	public void openStatus(final IStatus status) {
+		if (status.isMultiStatus()) {
+			// Try first element
+			openStatus(status.getChildren()[0]);
+		}
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			OptimiserSettings object = null;
+			if (dcsd.getTarget() instanceof OptimiserSettings) {
+				object = (OptimiserSettings) dcsd.getTarget();
+			}
+			if (object != null) {
+				getSite().getPage().activate(this);
+				setSelection(new StructuredSelection(object), true);
+			}
+		}
+	}
+}
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.optimiser.editor/src/com/mmxlabs/models/lng/optimiser/ui/editorpart/OptimiserModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.optimiser.editor/src/com/mmxlabs/models/lng/optimiser/ui/editorpart/OptimiserModelEditorContribution.java
@@ -18,28 +18,28 @@ import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
  *
  */
 public class OptimiserModelEditorContribution extends BaseJointModelEditorContribution<OptimiserModel> {
-	private OptimiserSettingsEditorPane pane;
+//	private OptimiserSettingsEditorPane pane;
 
 	/* (non-Javadoc)
 	 * @see com.mmxlabs.models.ui.editorpart.IJointModelEditorContribution#addPages(org.eclipse.swt.widgets.Composite)
 	 */
 	@Override
 	public void addPages(Composite parent) {
-		pane = new OptimiserSettingsEditorPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
-		
-		pane.createControl(parent);
-		pane.init(
-				Arrays.asList(new EReference[] {OptimiserPackage.eINSTANCE.getOptimiserModel_Settings()}),
-				editorPart.getAdapterFactory());
-		pane.getViewer().setInput(modelObject);
-		
-		int pageNumber = editorPart.addPage(pane.getControl());
-		editorPart.setPageText(pageNumber, "Optimiser Settings");
+//		pane = new OptimiserSettingsEditorPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
+//		
+//		pane.createControl(parent);
+//		pane.init(
+//				Arrays.asList(new EReference[] {OptimiserPackage.eINSTANCE.getOptimiserModel_Settings()}),
+//				editorPart.getAdapterFactory());
+//		pane.getViewer().setInput(modelObject);
+//		
+//		int pageNumber = editorPart.addPage(pane.getControl());
+//		editorPart.setPageText(pageNumber, "Optimiser Settings");
 	}
 
 	@Override
 	public void setLocked(boolean locked) {
-		pane.setLocked(locked);
+//		pane.setLocked(locked);
 	}
 
 }
