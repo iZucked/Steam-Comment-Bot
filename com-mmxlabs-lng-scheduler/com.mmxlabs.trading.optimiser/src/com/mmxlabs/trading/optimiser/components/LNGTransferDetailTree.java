344901043b21a0bf788f13329a0690ceaa5f4891
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-09-03 17:35:45 -0100

    Temporary back out of changeset 74:252162f41c21

--- com-mmxlabs-lng-scheduler/com.mmxlabs.trading.optimiser/src/com/mmxlabs/trading/optimiser/components/LNGTransferDetailTree.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.trading.optimiser/src/com/mmxlabs/trading/optimiser/components/LNGTransferDetailTree.java
@@ -6,7 +6,6 @@ package com.mmxlabs.trading.optimiser.components;
 
 import com.mmxlabs.common.detailtree.DetailTree;
 import com.mmxlabs.common.detailtree.IDetailTree;
-import com.mmxlabs.common.detailtree.impl.CurrencyDetailElement;
 import com.mmxlabs.scheduler.optimiser.Calculator;
 
 /**
@@ -19,11 +18,11 @@ import com.mmxlabs.scheduler.optimiser.Calculator;
  */
 public class LNGTransferDetailTree extends DetailTree {
 	public LNGTransferDetailTree(final String key, final long lngVolume, final int lngPricePerM3, final int cvValue) {
-		super(key, new CurrencyDetailElement(Calculator.multiply(lngVolume, lngPricePerM3)));
+		super(key, Calculator.multiply(lngVolume, lngPricePerM3));
 		addChild("Volume", lngVolume);
-		addChild("Price/m3", new CurrencyDetailElement(lngPricePerM3));
+		addChild("Price/m3", lngPricePerM3);
 		addChild("CV Value", cvValue);
 		addChild("MMBTu", Calculator.multiply(cvValue, lngVolume));
-		addChild("Price/MMBTu", new CurrencyDetailElement(Calculator.divide(lngPricePerM3, cvValue)));
+		addChild("Price/MMBTu", Calculator.divide(lngPricePerM3, cvValue));
 	}
 }
