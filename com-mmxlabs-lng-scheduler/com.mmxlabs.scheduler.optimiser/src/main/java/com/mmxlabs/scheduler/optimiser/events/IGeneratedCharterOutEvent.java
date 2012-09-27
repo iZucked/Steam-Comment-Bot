3fab8a4fdfc6a9a4e4e157bd8c6429330bba76bd
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-09-27 13:15:29 -0100

    Add annotation objects to represent a generated charter out

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/events/IGeneratedCharterOutEvent.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.events;
+
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+
+/**
+ * {@link IScheduledEvent} defining a generated charter out opportunity starting and ending at a {@link IPort}
+ * 
+ * l@author Simon Goodall
+ * @since 2.0
+ * 
+ */
+public interface IGeneratedCharterOutEvent extends IScheduledEvent {
+
+	/**
+	 * Returns the {@link IPort} at which this idle event occurs.
+	 * 
+	 * @return
+	 */
+	IPort getPort();
+
+	long getCharterOutRevenue();
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/events/impl/GeneratedCharterOutEventImpl.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.events.impl;
+
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.events.IGeneratedCharterOutEvent;
+import com.mmxlabs.scheduler.optimiser.events.IIdleEvent;
+
+/**
+ * Implementation of {@link IIdleEvent}.
+ * 
+ * @author Simon Goodall
+ * @since 2.0
+ */
+public final class GeneratedCharterOutEventImpl extends AbstractScheduledEventImpl implements IGeneratedCharterOutEvent {
+
+	private IPort port;
+
+	private long charterOutRevenue;
+
+	@Override
+	public IPort getPort() {
+		return port;
+	}
+
+	public void setPort(final IPort port) {
+		this.port = port;
+	}
+
+	@Override
+	public long getCharterOutRevenue() {
+		return charterOutRevenue;
+	}
+
+	public void setCharterOutRevenue(long charterOutRevenue) {
+		this.charterOutRevenue = charterOutRevenue;
+	}
+}
