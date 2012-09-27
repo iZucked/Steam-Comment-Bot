15f9a7c0da1b586bfe8106554090acd0caead552
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-09-27 11:48:09 -0100

    Move activator to internal package

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/internal/Activator.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/internal/Activator.java
@@ -2,7 +2,7 @@
  * Copyright (C) Minimax Labs Ltd., 2010 - 2012
  * All rights reserved.
  */
-package com.mmxlabs.models.lng.transformer;
+package com.mmxlabs.models.lng.transformer.internal;
 
 /**
  * <copyright>
