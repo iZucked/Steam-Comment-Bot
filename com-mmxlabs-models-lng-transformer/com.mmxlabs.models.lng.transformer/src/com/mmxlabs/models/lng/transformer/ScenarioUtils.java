898630cbed37d0c81b5cb9d241e519687cb89749
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-03 08:25:29 -0100

    Bump fitness weighting up

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/ScenarioUtils.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/ScenarioUtils.java
@@ -105,7 +105,7 @@ public class ScenarioUtils {
 			
 			objectives.add(createObjective(of, CargoSchedulerFitnessCoreFactory.LATENESS_COMPONENT_NAME, 100000));
 			objectives.add(createObjective(of, CargoSchedulerFitnessCoreFactory.CAPACITY_COMPONENT_NAME, 100000));
-			objectives.add(createObjective(of, NonOptionalSlotFitnessCoreFactory.NAME, 100000));
+			objectives.add(createObjective(of, NonOptionalSlotFitnessCoreFactory.NAME, 3000000));
 		}
 
 		final AnnealingSettings annealingSettings = of.createAnnealingSettings();
