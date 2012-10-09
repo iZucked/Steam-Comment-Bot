8c41e91fef115fc62ac2a0ec30648278155044e2
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-09 09:46:09 -0100

    Hook into new license checker plugin
    BugzId: 407

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.app/src/com/mmxlabs/shiplingo/platform/app/intro/Application.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.app/src/com/mmxlabs/shiplingo/platform/app/intro/Application.java
@@ -4,22 +4,18 @@
  */
 package com.mmxlabs.shiplingo.platform.app.intro;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.service.datalocation.Location;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
+import com.mmxlabs.license.ssl.LicenseChecker;
 import com.mmxlabs.shiplingo.platform.app.DelayedOpenFileProcessor;
 
 /**
@@ -36,13 +32,13 @@ public class Application implements IApplication {
 	public Object start(final IApplicationContext context) {
 
 		final Display display = PlatformUI.createDisplay();
-//		if (!checkLicense()) {
-//
-//			MessageDialog.openError(display.getActiveShell(), "License Error", "Unable to validate license");
-//
-//			display.dispose();
-//			return IApplication.EXIT_OK;
-//		}
+		if (!LicenseChecker.checkLicense()) {
+
+			MessageDialog.openError(display.getActiveShell(), "License Error", "Unable to validate license");
+
+			display.dispose();
+			return IApplication.EXIT_OK;
+		}
 
 		final DelayedOpenFileProcessor processor = new DelayedOpenFileProcessor(display);
 
@@ -103,46 +99,5 @@ public class Application implements IApplication {
 				}
 			}
 		});
-	}
-
-	private boolean checkLicense() {
-
-		try {
-			// Load keystore
-			final KeyStore keyStore = KeyStore.getInstance("JKS");
-			// Hardcoded keystore password - only storing public key so not really an issue
-			final char[] password = new char[] { '1', '2', '3', '4', '5', '6' };
-			// TODO: Load from bundle resource
-			keyStore.load(new FileInputStream("C:/temp/certs/dev.jks"), password);
-
-			final Certificate rootCertificate = keyStore.getCertificate("root");
-			if (rootCertificate == null) {
-				return false;
-			}
-
-			// Load the license file
-			final Certificate licenseCertificate;
-			{
-				final File file = Platform.getLocation().append("license.pem").toFile();
-				final CertificateFactory cf = CertificateFactory.getInstance("X.509");
-				licenseCertificate = cf.generateCertificate(new FileInputStream(file));
-			}
-
-			// Verify self-signed certificate
-			rootCertificate.verify(rootCertificate.getPublicKey());
-			// Verify license is signed by the root
-			licenseCertificate.verify(rootCertificate.getPublicKey());
-
-			// Check dates are valid. We expect a X509 certificate
-			if (licenseCertificate instanceof X509Certificate) {
-				final X509Certificate x509Certificate = (X509Certificate) licenseCertificate;
-				x509Certificate.checkValidity();
-				return true;
-			}
-
-			return false;
-		} catch (final Exception e) {
-			return false;
-		}
 	}
 }
