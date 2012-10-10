7a1c1ecae4531cf9471130dde1122cc2c2a49015
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-10 10:08:43 -0100

    Remove unused imports

--- com-mmxlabs-rcp-common/com.mmxlabs.models.common/src/com/mmxlabs/models/common/commandservice/CommandProviderAwareEditingDomain.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.models.common/src/com/mmxlabs/models/common/commandservice/CommandProviderAwareEditingDomain.java
@@ -4,11 +4,7 @@
  */
 package com.mmxlabs.models.common.commandservice;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
@@ -17,15 +13,10 @@ import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.AdapterFactory;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.command.CommandParameter;
-import org.eclipse.emf.edit.command.DeleteCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
-import org.eclipse.ocl.types.TypesPackage;
 import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +24,6 @@ import org.slf4j.LoggerFactory;
 import com.mmxlabs.models.mmxcore.IMMXAdapter;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.MMXSubModel;
-import com.mmxlabs.models.mmxcore.UUIDObject;
 
 /**
  * EditingDomain implementation which is aware of {@link IModelCommandProvider}s and provides a mechanism to enable or disable their execution.
