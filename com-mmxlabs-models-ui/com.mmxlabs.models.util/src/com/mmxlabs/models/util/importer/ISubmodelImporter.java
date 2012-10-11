f109ec9106c9e8d10bd375647291d8a048097312
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-11 11:51:00 -0100

    Add method to getEClass() for the importer

--- com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/ISubmodelImporter.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/ISubmodelImporter.java
@@ -7,33 +7,50 @@ package com.mmxlabs.models.util.importer;
 import java.util.Collection;
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EClass;
+
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 
 /**
- * Each submodel being imported into a joint model should have a corresponding instance of this class.
- * It can look up IClassImporters or do any other thing it fancies to set up its submodel.
+ * Each submodel being imported into a joint model should have a corresponding instance of this class. It can look up IClassImporters or do any other thing it fancies to set up its submodel.
+ * 
  * @author hinton
- *
+ * 
  */
 public interface ISubmodelImporter {
+
+	/**
+	 * Return the {@link EClass} this {@link ISubmodelImporter} is for.
+	 * 
+	 * @return
+	 * @since 2.0
+	 */
+	EClass getEClass();
+
 	/**
 	 * Return a mapping from keys to friendly names, each being a required input file / database table / whatever
+	 * 
 	 * @return
 	 */
-	public Map<String, String> getRequiredInputs();
+	Map<String, String> getRequiredInputs();
+
 	/**
 	 * Convert the given mapping from the keys of {@link #getRequiredInputs()} to readers into a {@link UUIDObject} of the appropriate sort
+	 * 
 	 * @param inputs
 	 * @param context
 	 * @return
 	 */
-	public UUIDObject importModel(final Map<String, CSVReader> inputs, final IImportContext context);
+	UUIDObject importModel(Map<String, CSVReader> inputs, IImportContext context);
+
 	/**
 	 * Turn the given model instance into a bunch of output values, keyed like the inputs.
-	 * @param root TODO
+	 * 
+	 * @param root
+	 *            TODO
 	 * @param model
 	 * @param output
 	 */
-	public void exportModel(MMXRootObject root, final UUIDObject model, final Map<String, Collection<Map<String, String>>> output);
+	void exportModel(MMXRootObject root, UUIDObject model, Map<String, Collection<Map<String, String>>> output);
 }
