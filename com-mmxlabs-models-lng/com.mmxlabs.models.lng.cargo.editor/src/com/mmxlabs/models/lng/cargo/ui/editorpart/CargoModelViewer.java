dd6a40b460bb0febee699c346733291a0895b8a9
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-16 08:11:00 -0100

    Use Numeric manipulator for pricing

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
@@ -156,7 +156,7 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 
 		addTypicalColumn("Load Contract", new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_LoadSlot());
 
-		addTypicalColumn("Purchase Price", new BasicAttributeManipulator(pkg.getSlot_FixedPrice(), editingDomain), pkg.getCargo_LoadSlot());
+		addTypicalColumn("Purchase Price", new NumericAttributeManipulator(pkg.getSlot_FixedPrice(), editingDomain), pkg.getCargo_LoadSlot());
 
 		addTypicalColumn("Discharge Port", new SingleReferenceManipulator(pkg.getSlot_Port(), provider, editingDomain), pkg.getCargo_DischargeSlot());
 
@@ -164,7 +164,7 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 
 		addTypicalColumn("Discharge Contract", new SingleReferenceManipulator(pkg.getSlot_Contract(), provider, editingDomain), pkg.getCargo_DischargeSlot());
 
-		addTypicalColumn("Sales Price", new BasicAttributeManipulator(pkg.getSlot_FixedPrice(), editingDomain), pkg.getCargo_DischargeSlot());
+		addTypicalColumn("Sales Price", new NumericAttributeManipulator(pkg.getSlot_FixedPrice(), editingDomain), pkg.getCargo_DischargeSlot());
 
 		final InputModel input = part.getRootObject().getSubModel(InputModel.class);
 
