e4d5f8e05bc79cff95251d817162f297b14c51fe
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-22 11:33:38 -0100

    Sync test to code

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/contracts/impl/TestMarketPriceContract.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/contracts/impl/TestMarketPriceContract.java
@@ -11,7 +11,6 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.mmxlabs.common.curves.StepwiseIntegerCurve;
-import com.mmxlabs.scheduler.optimiser.OptimiserUnitConvertor;
 
 /**
  * @author Tom Hinton
@@ -26,7 +25,7 @@ public class TestMarketPriceContract {
 		for (int i = 0; i < 1000; i++) {
 			market.setValueAfter(random.nextInt(), random.nextInt());
 		}
-		final MarketPriceContract mpc = new MarketPriceContract(market, 0, OptimiserUnitConvertor.convertToInternalPrice(1));
+		final MarketPriceContract mpc = new MarketPriceContract(market, 0, 1000);
 		for (int i = 0; i < 1000; i++) {
 			final int date = random.nextInt();
 			Assert.assertEquals(mpc.calculateLoadUnitPrice(null, null, date, date, 0, 0, null, null, null), market.getValueAtPoint(date));
