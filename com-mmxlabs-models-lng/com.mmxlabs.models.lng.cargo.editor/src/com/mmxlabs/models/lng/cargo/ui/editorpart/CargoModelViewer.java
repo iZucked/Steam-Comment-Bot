e779aa95bbf8b53b4d8fdd176298b36dac144c51
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-24 12:12:04 -0100

    Extract AssignmentManipluator into top level class

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoModelViewer.java
@@ -6,19 +6,15 @@ package com.mmxlabs.models.lng.cargo.ui.editorpart;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.notify.AdapterFactory;
-import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.IElementComparer;
 import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
@@ -35,7 +31,6 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import com.mmxlabs.common.Equality;
-import com.mmxlabs.common.Pair;
 import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.CargoType;
@@ -45,7 +40,6 @@ import com.mmxlabs.models.lng.cargo.presentation.CargoEditorPlugin;
 import com.mmxlabs.models.lng.cargo.ui.actions.RotateSlotsAction;
 import com.mmxlabs.models.lng.input.ElementAssignment;
 import com.mmxlabs.models.lng.input.InputModel;
-import com.mmxlabs.models.lng.input.InputPackage;
 import com.mmxlabs.models.lng.input.editor.utils.AssignmentEditorHelper;
 import com.mmxlabs.models.lng.schedule.CargoAllocation;
 import com.mmxlabs.models.lng.schedule.Event;
@@ -53,23 +47,18 @@ import com.mmxlabs.models.lng.schedule.GeneratedCharterOut;
 import com.mmxlabs.models.lng.schedule.SlotAllocation;
 import com.mmxlabs.models.lng.schedule.SlotVisit;
 import com.mmxlabs.models.lng.schedule.VesselEventVisit;
-import com.mmxlabs.models.lng.types.AVesselSet;
 import com.mmxlabs.models.lng.ui.actions.SimpleImportAction;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewer;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.mmxcore.MMXCorePackage;
-import com.mmxlabs.models.mmxcore.UUIDObject;
 import com.mmxlabs.models.ui.dates.DateAttributeManipulator;
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 import com.mmxlabs.models.ui.editors.dialogs.DetailCompositeDialog;
 import com.mmxlabs.models.ui.tabular.BasicAttributeManipulator;
 import com.mmxlabs.models.ui.tabular.BasicOperationRenderer;
 import com.mmxlabs.models.ui.tabular.EObjectTableViewerColumnProvider;
-import com.mmxlabs.models.ui.tabular.ICellManipulator;
-import com.mmxlabs.models.ui.tabular.ICellRenderer;
 import com.mmxlabs.models.ui.tabular.NumericAttributeManipulator;
 import com.mmxlabs.models.ui.tabular.SingleReferenceManipulator;
-import com.mmxlabs.models.ui.valueproviders.IReferenceValueProvider;
 import com.mmxlabs.models.ui.valueproviders.IReferenceValueProviderProvider;
 
 public class CargoModelViewer extends ScenarioTableViewerPane {
@@ -298,93 +287,5 @@ public class CargoModelViewer extends ScenarioTableViewerPane {
 		});
 		return scenarioTableViewer;
 
-	}
-
-	class AssignmentManipulator implements ICellRenderer, ICellManipulator {
-		private final IScenarioEditingLocation location;
-		private final IReferenceValueProvider valueProvider;
-		private List<Pair<String, EObject>> allowedValues;
-		private final List<EObject> vessels = new ArrayList<EObject>();
-
-		public AssignmentManipulator(final IScenarioEditingLocation location) {
-			this.location = location;
-			this.valueProvider = location.getReferenceValueProviderCache().getReferenceValueProvider(InputPackage.eINSTANCE.getElementAssignment(),
-					InputPackage.eINSTANCE.getElementAssignment_Assignment());
-			getValues();
-		}
-
-		private void getValues() {
-			allowedValues = valueProvider.getAllowedValues(null, InputPackage.eINSTANCE.getAssignment_Vessels());
-			vessels.clear();
-			for (final Pair<String, EObject> p : allowedValues)
-				vessels.add(p.getSecond());
-		}
-
-		@Override
-		public void setValue(final Object object, final Object value) {
-			// grar.
-			final InputModel input = location.getRootObject().getSubModel(InputModel.class);
-			if (input != null) {
-				if (value == null || value.equals(-1))
-					return;
-				final AVesselSet set = (AVesselSet) vessels.get((Integer) value);
-
-				location.getEditingDomain().getCommandStack().execute(AssignmentEditorHelper.reassignElement(getEditingDomain(), input, (UUIDObject) object, set));
-			}
-		}
-
-		@Override
-		public CellEditor getCellEditor(final Composite parent, final Object object) {
-			getValues();
-			final String[] items = new String[allowedValues.size() - 1];
-			for (int i = 0; i < items.length; i++) {
-				items[i] = allowedValues.get(i).getFirst();
-			}
-			return new ComboBoxCellEditor(parent, items);
-		}
-
-		@Override
-		public Integer getValue(final Object object) {
-			if (object instanceof Cargo) {
-				final Cargo cargo = (Cargo) object;
-
-				final InputModel input = location.getRootObject().getSubModel(InputModel.class);
-				if (input != null) {
-					ElementAssignment assignment = AssignmentEditorHelper.getElementAssignment(input, cargo);
-					if (assignment != null)
-						return vessels.indexOf(assignment.getAssignment());
-				}
-			}
-
-			return null;
-		}
-
-		@Override
-		public boolean canEdit(final Object object) {
-			return object instanceof Cargo;
-		}
-
-		@Override
-		public String render(final Object object) {
-			final Integer value = getValue(object);
-			if (value == null || value == -1)
-				return "";
-			return allowedValues.get(value).getFirst();
-		}
-
-		@Override
-		public Comparable<?> getComparable(final Object object) {
-			return render(object);
-		}
-
-		@Override
-		public Object getFilterValue(final Object object) {
-			return render(object);
-		}
-
-		@Override
-		public Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(final Object object) {
-			return Collections.singleton(new Pair<Notifier, List<Object>>(location.getRootObject().getSubModel(InputModel.class), Collections.emptyList()));
-		}
 	}
 }
