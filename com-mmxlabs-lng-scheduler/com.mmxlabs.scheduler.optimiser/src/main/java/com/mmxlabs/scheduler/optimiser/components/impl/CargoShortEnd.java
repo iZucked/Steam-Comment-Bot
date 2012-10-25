071ba20a7e0bff39049457021864c4c67b13bf17
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-25 12:19:27 -0100

    Port slot type for cargo short returns

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/components/impl/CargoShortEnd.java
@@ -0,0 +1,16 @@
+package com.mmxlabs.scheduler.optimiser.components.impl;
+
+import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.providers.PortType;
+
+/**
+ * @since 2.0
+ */
+public class CargoShortEnd extends PortSlot {
+
+	public CargoShortEnd(final String id, final IPort port) {
+		setPortType(PortType.Short_Cargo_End);
+		setPort(port);
+		setId(id);
+	}
+}
