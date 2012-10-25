b41fe8fe44607f4c2e58bf31cf90c38f17a9cc88
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-25 11:56:12 -0100

    Add segment implementation with no reference to parent sequence

---
+++ com-mmxlabs-optimiser/optimiser-core/src/main/java/com/mmxlabs/optimiser/core/impl/DisconnectedSegment.java
@@ -0,0 +1,54 @@
+package com.mmxlabs.optimiser.core.impl;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.mmxlabs.optimiser.core.ISegment;
+import com.mmxlabs.optimiser.core.ISequence;
+import com.mmxlabs.optimiser.core.ISequenceElement;
+
+/**
+ * An implementation of {@link ISegment} that contains {@link ISequenceElement}s that are not contained anywhere else.
+ * 
+ * @author Simon Goodall
+ * @since 2.0
+ */
+public class DisconnectedSegment implements ISegment {
+
+	private final List<ISequenceElement> elements;
+
+	public DisconnectedSegment(final List<ISequenceElement> elements) {
+		this.elements = elements;
+	}
+
+	@Override
+	public Iterator<ISequenceElement> iterator() {
+		return elements.iterator();
+	}
+
+	@Override
+	public int size() {
+		return elements.size();
+	}
+
+	@Override
+	public ISequenceElement get(final int index) {
+		return elements.get(index);
+	}
+
+	@Override
+	public int getSequenceStart() {
+		return -1;
+	}
+
+	@Override
+	public int getSequenceEnd() {
+		return -1;
+	}
+
+	@Override
+	public ISequence getSequence() {
+		return null;
+	}
+
+}
