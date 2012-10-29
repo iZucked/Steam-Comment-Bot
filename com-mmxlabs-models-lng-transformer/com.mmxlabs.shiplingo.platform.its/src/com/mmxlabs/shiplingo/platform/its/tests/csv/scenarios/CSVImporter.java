2fdf41691e836e2be7b17a37de1823b96a1e087b
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-29 13:59:47 +0000

    Fix up list of importers

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.shiplingo.platform.its/src/com/mmxlabs/shiplingo/platform/its/tests/csv/scenarios/CSVImporter.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.shiplingo.platform.its/src/com/mmxlabs/shiplingo/platform/its/tests/csv/scenarios/CSVImporter.java
@@ -21,38 +21,46 @@ import com.mmxlabs.models.util.importer.ISubmodelImporter;
 import com.mmxlabs.models.util.importer.impl.DefaultImportContext;
 import com.mmxlabs.models.util.importer.registry.IImporterRegistry;
 import com.mmxlabs.shiplingo.platform.its.internal.Activator;
-import com.mmxlabs.shiplingo.platform.models.manifest.ManifestJointModel;
+import com.mmxlabs.shiplingo.platform.its.tests.ManifestJointModel;
 
 public class CSVImporter {
 
 	public static MMXRootObject importCSVScenario(String baseFileName) {
 
 		Map<String, String> dataMap = new HashMap<String, String>();
-		 // No analytics model importers
+
+		// No analytics model importers
+
 		dataMap.put(CargoModelImporter.CARGO_KEY, baseFileName + "/" + "Cargoes.csv");
 		dataMap.put(CargoModelImporter.CARGO_GROUP_KEY, baseFileName + "/" + "Cargo Groups.csv");
-		
+
 		dataMap.put(CommercialModelImporter.ENTITIES_KEY, baseFileName + "/" + "Entities.csv");
 		dataMap.put(CommercialModelImporter.PURCHASE_CON_KEY, baseFileName + "/" + "Purchase Contracts.csv");
 		dataMap.put(CommercialModelImporter.SALES_CON_KEY, baseFileName + "/" + "Sales Contracts.csv");
 
-		dataMap.put(FleetModelImporter.FLEET_VESSELS_KEY, baseFileName + "/" + "Vessels.csv");
+		dataMap.put(FleetModelImporter.CURVES_KEY, baseFileName + "/" + "Consumption Curves.csv");
+		dataMap.put(FleetModelImporter.FUELS_KEY, baseFileName + "/" + "Base Fuels.csv");
+		dataMap.put(FleetModelImporter.GROUPS_KEY, baseFileName + "/" + "Vessel Groups.csv");
+		dataMap.put(FleetModelImporter.VESSEL_CLASSES_KEY, baseFileName + "/" + "Vessel Classes.csv");
+		dataMap.put(FleetModelImporter.VESSELS_KEY, baseFileName + "/" + "Vessels.csv");
 		dataMap.put(FleetModelImporter.EVENTS_KEY, baseFileName + "/" + "Vessel Events.csv");
-		
+
 		dataMap.put(InputModelImporter.ASSIGNMENTS, baseFileName + "/" + "Assignments.csv");
-		
+
 		// No optimiser model importers
+
 		dataMap.put(PortModelImporter.PORT_KEY, baseFileName + "/" + "Ports.csv");
 		dataMap.put(PortModelImporter.PORT_GROUP_KEY, baseFileName + "/" + "Port Groups.csv");
 		dataMap.put(PortModelImporter.DISTANCES_KEY, baseFileName + "/" + "Distance Matrix.csv");
 		dataMap.put(PortModelImporter.SUEZ_KEY, baseFileName + "/" + "Suez Distance Matrix.csv");
-		
+
 		dataMap.put(PricingModelImporter.CHARTER_CURVE_KEY, baseFileName + "/" + "Charter Curves.csv");
 		dataMap.put(PricingModelImporter.CHARTER_PRICING_KEY, baseFileName + "/" + "Charter Rates.csv");
 		dataMap.put(PricingModelImporter.COOLDOWN_PRICING_KEY, baseFileName + "/" + "Cooldown Prices.csv");
 		dataMap.put(PricingModelImporter.PORT_COSTS_KEY, baseFileName + "/" + "Port Costs.csv");
-		dataMap.put(PricingModelImporter.ROUTE_COSTS_KEY, baseFileName + "/" + "Route Costs.csv");
+		dataMap.put(PricingModelImporter.PRICE_CURVE_KEY, baseFileName + "/" + "Commodity Curves.csv");
 		dataMap.put(PricingModelImporter.SPOT_CARGO_MARKETS_KEY, baseFileName + "/" + "Spot Cargo Markets.csv");
+
 		// No schedule importers
 
 		final DefaultImportContext context = new DefaultImportContext();
@@ -67,7 +75,7 @@ public class CSVImporter {
 			final HashMap<String, CSVReader> readers = new HashMap<String, CSVReader>();
 			for (String key : parts.keySet()) {
 				try {
-					final CSVReader r = new CSVReader(dataMap.get(key));
+					final CSVReader r = new CSVReader(baseFileName, dataMap.get(key));
 					readers.put(key, r);
 				} catch (final IOException e) {
 					Assert.fail(e.getMessage());
