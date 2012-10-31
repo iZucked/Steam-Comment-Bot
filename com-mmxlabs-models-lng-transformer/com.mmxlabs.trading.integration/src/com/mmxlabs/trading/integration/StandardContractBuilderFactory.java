4f57a8c57044c39680e48bdf52d14492af8f71c2
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-10-31 13:32:08 +0000

    Remove unused class

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/StandardContractBuilderFactory.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.trading.integration/src/com/mmxlabs/trading/integration/StandardContractBuilderFactory.java
@@ -2,7 +2,6 @@ package com.mmxlabs.trading.integration;
 
 import com.mmxlabs.models.lng.transformer.inject.IBuilderExtensionFactory;
 import com.mmxlabs.scheduler.optimiser.builder.IBuilderExtension;
-import com.mmxlabs.trading.optimiser.builder.TradingBuilderExtension;
 
 /**
  * @since 2.0
