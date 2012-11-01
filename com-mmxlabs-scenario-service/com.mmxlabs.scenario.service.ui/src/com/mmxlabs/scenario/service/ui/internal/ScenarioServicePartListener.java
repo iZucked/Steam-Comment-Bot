dc8f0a261205ef1681bcf083757caf60f8fd8d23
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-01 15:54:17 +0000

    Extract out the main part listener code to sync active editor to the scenario selection service. This permits it to be initiated before the navigator is active  and it can be active from early on in the application lifecycle. This fixes an issue where an active editor, but hidden scenario navigator would not hook up to the reports.

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/internal/ScenarioServicePartListener.java
@@ -0,0 +1,71 @@
+package com.mmxlabs.scenario.service.ui.internal;
+
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+public class ScenarioServicePartListener implements IPartListener {
+
+	private ScenarioInstance lastAutoSelection = null;
+
+	@Override
+	public void partOpened(final IWorkbenchPart part) {
+
+	}
+
+	@Override
+	public void partDeactivated(final IWorkbenchPart part) {
+
+	}
+
+	@Override
+	public void partClosed(final IWorkbenchPart part) {
+		// If the selection tracks editor, then get the scenario instance and make it the only selection.
+		if (part instanceof IEditorPart) {
+			final IEditorPart editorPart = (IEditorPart) part;
+			final IEditorInput editorInput = editorPart.getEditorInput();
+			final ScenarioInstance scenarioInstance = (ScenarioInstance) editorInput.getAdapter(ScenarioInstance.class);
+			if (scenarioInstance != null) {
+				ScenarioServiceSelectionProvider selectionProvider = Activator.getDefault().getScenarioServiceSelectionProvider();
+				if (lastAutoSelection == scenarioInstance) {
+					lastAutoSelection = null;
+					if (selectionProvider.isSelected(scenarioInstance) && scenarioInstance != selectionProvider.getPinnedInstance()) {
+						selectionProvider.deselect(scenarioInstance);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void partBroughtToTop(final IWorkbenchPart part) {
+
+	}
+
+	@Override
+	public void partActivated(final IWorkbenchPart part) {
+		// If the selection tracks editor, then get the scenario instance and make it the only selection.
+		if (part instanceof IEditorPart) {
+			final IEditorPart editorPart = (IEditorPart) part;
+			final IEditorInput editorInput = editorPart.getEditorInput();
+			final ScenarioInstance scenarioInstance = (ScenarioInstance) editorInput.getAdapter(ScenarioInstance.class);
+			if (scenarioInstance != null) {
+				ScenarioServiceSelectionProvider selectionProvider = Activator.getDefault().getScenarioServiceSelectionProvider();
+				if (lastAutoSelection != null && lastAutoSelection != scenarioInstance) {
+					if (selectionProvider.isSelected(lastAutoSelection) && selectionProvider.getPinnedInstance() != lastAutoSelection) {
+						selectionProvider.deselect(lastAutoSelection);
+					}
+					lastAutoSelection = null;
+				}
+				if (!selectionProvider.isSelected(scenarioInstance)) {
+					lastAutoSelection = scenarioInstance;
+					selectionProvider.select(scenarioInstance);
+				}
+			}
+		}
+	}
+
+}
---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/internal/ScenarioServiceStartupExtension.java
@@ -0,0 +1,34 @@
+package com.mmxlabs.scenario.service.ui.internal;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * A startup extension to register a {@link IPartListener} to track active editor selection. Note this is never de-registered. We use an {@link IStartup} rather than bundle activator as the bundle
+ * could start before the workbench is active. Potentially this class should interact with the bundle activator to co-ordinate the listener creation and disposal.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class ScenarioServiceStartupExtension implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+
+		Display.getDefault().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				final IWorkbench workbench = PlatformUI.getWorkbench();
+				final IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
+				final ScenarioServicePartListener partListener = new ScenarioServicePartListener();
+				activeWorkbenchWindow.getPartService().addPartListener(partListener);
+				partListener.partActivated(activeWorkbenchWindow.getActivePage().getActiveEditor());
+			}
+		});
+	}
+}
