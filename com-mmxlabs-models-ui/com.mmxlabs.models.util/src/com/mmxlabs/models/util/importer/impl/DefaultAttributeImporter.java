b3988fd5bf5a07b48ca328b285bea97cde27e944
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-05 14:46:42 +0000

    Add in simplistic HTML-ish encoding of strings.
    BugzId: 636

--- com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/impl/DefaultAttributeImporter.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.util/src/com/mmxlabs/models/util/importer/impl/DefaultAttributeImporter.java
@@ -14,40 +14,51 @@ import com.mmxlabs.models.util.importer.IImportContext;
 
 public class DefaultAttributeImporter implements IAttributeImporter {
 
+	private static final String HTML_AMPERSAND = "&#38;";
+	private static final String HTML_COMMA = "&#44;";
+
 	@Override
-	public void setAttribute(EObject container, final EAttribute attribute,
-			String value, IImportContext context) {
+	public void setAttribute(final EObject container, final EAttribute attribute, final String value, final IImportContext context) {
 		if (attribute.isMany()) {
 			final String[] values = value.split(",");
 			final EList eValues = (EList) container.eGet(attribute);
 			for (final String v : values) {
-				if (v.trim().isEmpty()) continue;
-				eValues.add(attributeFromString(container, attribute, v.trim(),
-						context));
+				if (v.trim().isEmpty()) {
+					continue;
+				}
+
+				// Simple (de)encoding - should expand into something more robust
+				final String decodedValue = v.trim().replaceAll(HTML_COMMA, ",").replaceAll(HTML_AMPERSAND, "&");
+
+				eValues.add(attributeFromString(container, attribute, decodedValue, context));
 			}
 		} else {
 			if (attribute.isUnsettable() && value.trim().isEmpty()) {
 				container.eUnset(attribute);
 			} else if (!value.isEmpty()) {
-				final Object oValue = attributeFromString(container, attribute, value,
-						context);
-				if (oValue != null) container.eSet(attribute, oValue);
+				final Object oValue = attributeFromString(container, attribute, value, context);
+				if (oValue != null)
+					container.eSet(attribute, oValue);
 			}
 		}
 	}
 
 	@Override
-	public String writeAttribute(EObject container, EAttribute attribute,
-			Object value) {
+	public String writeAttribute(final EObject container, final EAttribute attribute, final Object value) {
 		if (attribute.isMany()) {
 			final EList<?> eValues = (EList<?>) container.eGet(attribute);
 			final StringBuffer result = new StringBuffer();
 			boolean comma = false;
 			for (final Object o : eValues) {
-				if (comma)
+				if (comma) {
 					result.append(",");
+				}
 				comma = true;
-				result.append(stringFromAttribute(container, attribute, o));
+				final String rawValue = stringFromAttribute(container, attribute, o);
+				// Simple (de)encoding - should expand into something more robust
+				final String encodedValue = rawValue.replaceAll("&", HTML_AMPERSAND).replaceAll(",", HTML_COMMA);
+
+				result.append(encodedValue);
 			}
 			return result.toString();
 		} else {
@@ -55,21 +66,19 @@ public class DefaultAttributeImporter implements IAttributeImporter {
 		}
 	}
 
-	protected Object attributeFromString(final EObject container,
-			final EAttribute attribute, final String value,
-			final IImportContext context) {
+	protected Object attributeFromString(final EObject container, final EAttribute attribute, final String value, final IImportContext context) {
 		final EDataType dt = attribute.getEAttributeType();
 		try {
 			return dt.getEPackage().getEFactoryInstance().createFromString(dt, value);
-		} catch (Exception ex) {
+		} catch (final Exception ex) {
 			context.addProblem(context.createProblem("Error parsing value " + value + " for " + attribute.getName() + ":" + ex.toString(), true, true, true));
 		}
 		return null;
 	}
 
-	protected String stringFromAttribute(final EObject container,
-			final EAttribute attribute, final Object value) {
-		if (attribute.isUnsettable() && container.eIsSet(attribute) == false) return "";
+	protected String stringFromAttribute(final EObject container, final EAttribute attribute, final Object value) {
+		if (attribute.isUnsettable() && container.eIsSet(attribute) == false)
+			return "";
 		return attribute.getEAttributeType().getEPackage().getEFactoryInstance().convertToString(attribute.getEAttributeType(), value);
 	}
 }
