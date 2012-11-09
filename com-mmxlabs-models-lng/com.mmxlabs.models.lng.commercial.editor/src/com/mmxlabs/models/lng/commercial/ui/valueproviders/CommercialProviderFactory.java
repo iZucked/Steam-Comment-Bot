6cc5edf3b639e51c8982a1790c3d3cbda9dfa09f
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-09 13:20:19 +0000

    Use MergedMultiModelReferenceValueProvider

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/valueproviders/CommercialProviderFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/valueproviders/CommercialProviderFactory.java
@@ -7,35 +7,22 @@ package com.mmxlabs.models.lng.commercial.ui.valueproviders;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
 
-import com.mmxlabs.models.lng.commercial.CommercialModel;
-import com.mmxlabs.models.lng.commercial.CommercialPackage;
 import com.mmxlabs.models.lng.types.TypesPackage;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.valueproviders.IReferenceValueProvider;
 import com.mmxlabs.models.ui.valueproviders.IReferenceValueProviderFactory;
-import com.mmxlabs.models.ui.valueproviders.MergedReferenceValueProvider;
-import com.mmxlabs.models.ui.valueproviders.SimpleReferenceValueProvider;
+import com.mmxlabs.models.ui.valueproviders.MergedMultiModelReferenceValueProvider;
 
 public class CommercialProviderFactory implements IReferenceValueProviderFactory {
+
 	@Override
 	public IReferenceValueProvider createReferenceValueProvider(final EClass owner, final EReference reference, final MMXRootObject rootObject) {
-		final CommercialModel model = rootObject.getSubModel(CommercialModel.class);
 		final EClass referenceClass = reference.getEReferenceType();
 		final TypesPackage types = TypesPackage.eINSTANCE;
-		final CommercialPackage commercial = CommercialPackage.eINSTANCE;
 
-		if (commercial.getPurchaseContract().isSuperTypeOf(referenceClass)) {
-			return new SimpleReferenceValueProvider(model, commercial.getCommercialModel_PurchaseContracts());
-		} else if (commercial.getSalesContract().isSuperTypeOf(referenceClass)) {
-			return new SimpleReferenceValueProvider(model, commercial.getCommercialModel_SalesContracts());
-		} else if (types.getALegalEntity().isSuperTypeOf(referenceClass)) {
-			return new SimpleReferenceValueProvider(model, commercial.getCommercialModel_Entities());
-		} else if (types.getAPurchaseContract().isSuperTypeOf(referenceClass)) {
-			return new SimpleReferenceValueProvider(model, commercial.getCommercialModel_PurchaseContracts());
-		} else if (types.getASalesContract().isSuperTypeOf(referenceClass)) {
-			return new SimpleReferenceValueProvider(model, commercial.getCommercialModel_SalesContracts());
-		} else if (types.getAContract().isSuperTypeOf(referenceClass)) {
-			return new MergedReferenceValueProvider(model, commercial.getCommercialModel_PurchaseContracts(), commercial.getCommercialModel_SalesContracts());
+		if (types.getAContract().isSuperTypeOf(referenceClass) || types.getASalesContract().isSuperTypeOf(referenceClass) || types.getAPurchaseContract().isSuperTypeOf(referenceClass)
+				|| types.getALegalEntity().isSuperTypeOf(referenceClass)) {
+			return new MergedMultiModelReferenceValueProvider(rootObject, referenceClass);
 		}
 
 		return null;
