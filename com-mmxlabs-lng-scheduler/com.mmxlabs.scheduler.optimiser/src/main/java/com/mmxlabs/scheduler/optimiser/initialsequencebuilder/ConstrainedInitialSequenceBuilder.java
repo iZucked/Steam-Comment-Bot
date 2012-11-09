5e53d5b0f71533ad09f9b174a05605db951b1bab
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-09 21:32:57 +0000

    Fix typo

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/initialsequencebuilder/ConstrainedInitialSequenceBuilder.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/initialsequencebuilder/ConstrainedInitialSequenceBuilder.java
@@ -579,7 +579,7 @@ public class ConstrainedInitialSequenceBuilder implements IInitialSequenceBuilde
 
 		if (chunks.isEmpty() == false) {
 			log.error("Could not schedule the following " + chunks.size() + " elements anywhere: " + chunks);
-			throw new RuntimeException("Scenario is too hard for ConstrainedInitialSolutionBuilder. " + chunks.size() + " chunks " + "could not be scheduled anywhere: " + chunks);
+			throw new RuntimeException("Scenario is too hard for ConstrainedInitialSequenceBuilder. " + chunks.size() + " chunks " + "could not be scheduled anywhere: " + chunks);
 		}
 
 		// OK, we have done our best, now build the modifiablesequences
