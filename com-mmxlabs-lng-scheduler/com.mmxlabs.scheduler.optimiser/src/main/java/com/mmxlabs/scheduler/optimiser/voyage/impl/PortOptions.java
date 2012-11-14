a16c24cfe74bc3e44332488ac1a6bb9497450abd
Simon McGregor <unknown@kiln.example.com>
2012-11-14 16:51:43 +0000

    Refactoring change: moved portSlot and duration from PortDetails into a new PortOptions class

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/voyage/impl/PortOptions.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.voyage.impl;
+
+import com.google.common.base.Objects;
+import com.mmxlabs.common.Equality;
+import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+import com.mmxlabs.scheduler.optimiser.components.VesselState;
+
+/**
+ * Default implementation of {@link PortOptions}. This is @link {Cloneable} for use with @link{VoyagePlanOptimiser} use.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public final class PortOptions implements Cloneable {
+	private int visitDuration;
+	private IVessel vessel;
+	private IPortSlot portSlot;
+
+	private VesselState vesselState;
+
+	public PortOptions() {
+
+	}
+
+	public PortOptions(final PortOptions options) {
+		setVisitDuration(options.getVisitDuration());
+		setVessel(options.getVessel());
+		setPortSlot(options.getPortSlot());
+		setVesselState(options.getVesselState());
+	}
+
+	public final int getVisitDuration() {
+		return visitDuration;
+	}
+
+	public final IPortSlot getPortSlot() {
+		return portSlot;
+	}
+
+	public final IVessel getVessel() {
+		return vessel;
+	}
+
+	public final VesselState getVesselState() {
+		return vesselState;
+	}
+
+	public final void setVisitDuration(final int t) {
+		this.visitDuration = t;
+	}
+
+	public final void setVessel(final IVessel vessel) {
+		this.vessel = vessel;
+	}
+
+	public final void setPortSlot(final IPortSlot portSlot) {
+		this.portSlot = portSlot;
+	}
+
+	public final void setVesselState(final VesselState vesselState) {
+		this.vesselState = vesselState;
+	}
+
+	@Override
+	public final boolean equals(final Object obj) {
+
+		if (obj instanceof PortOptions) {
+			final PortOptions vo = (PortOptions) obj;
+
+			if (visitDuration != vo.visitDuration) {
+				return false;
+			}
+			if (!Equality.isEqual(vesselState, vo.vesselState)) {
+				return false;
+			}
+
+			if (!Equality.isEqual(vessel, vo.vessel)) {
+				return false;
+			}
+			if (!Equality.isEqual(portSlot, vo.portSlot)) {
+				return false;
+			}
+
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public final PortOptions clone()  {
+
+		return new PortOptions(this);
+	}
+
+	@Override
+	public String toString() {
+		return "PortOptions [availableTime=" + visitDuration + ", vessel=" + vessel + ", portSlot=" + portSlot + ", vesselState=" + vesselState + "]";
+	}
+
+	@Override
+	public final int hashCode() {
+		return Objects.hashCode(visitDuration, portSlot);
+	}
+}
---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/test/java/com/mmxlabs/scheduler/optimiser/voyage/impl/PortOptionsTest.java
@@ -0,0 +1,62 @@
+package com.mmxlabs.scheduler.optimiser.voyage.impl;
+
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
+import com.mmxlabs.scheduler.optimiser.components.IVessel;
+import com.mmxlabs.scheduler.optimiser.components.VesselState;
+import com.mmxlabs.scheduler.optimiser.voyage.FuelComponent;
+
+@RunWith(JMock.class)
+public class PortOptionsTest {
+	Mockery context = new JUnit4Mockery();
+
+	@Test
+	public void testGetSetVisitDuration() {
+
+		final int value = 100;
+		final PortOptions options = new PortOptions();		
+		Assert.assertEquals(0, options.getVisitDuration());
+		options.setVisitDuration(value);
+		Assert.assertEquals(value, options.getVisitDuration());
+	}
+	
+	@Test
+	public void testGetSetPortSlot() {
+		final IPortSlot slot = context.mock(IPortSlot.class);
+
+		final PortOptions options = new PortOptions();
+		Assert.assertNull(options.getPortSlot());
+		options.setPortSlot(slot);
+		Assert.assertSame(slot, options.getPortSlot());
+	}
+
+
+	@Test
+	public void testGetSetVessel() {
+		final IVessel vessel = context.mock(IVessel.class);
+
+		final PortOptions options = new PortOptions();
+		Assert.assertNull(options.getVessel());
+		options.setVessel(vessel);
+		Assert.assertSame(vessel, options.getVessel());
+	}
+
+
+	@Test
+	public void testGetSetVesselState() {
+		final VesselState state = context.mock(VesselState.class);
+
+		final PortOptions options = new PortOptions();
+		Assert.assertNull(options.getVesselState());
+		options.setVesselState(state);
+		Assert.assertSame(state, options.getVesselState());
+	}
+
+
+}
