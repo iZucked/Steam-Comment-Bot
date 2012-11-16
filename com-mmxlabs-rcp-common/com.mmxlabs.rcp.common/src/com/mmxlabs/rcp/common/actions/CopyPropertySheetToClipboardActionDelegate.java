0ab4ed9c80fda624bd348adbe7e467f51871eb6a
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-16 13:52:04 +0000

    Add action to copy property sheet contents to clipboard.

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/actions/CopyPropertySheetToClipboardActionDelegate.java
@@ -0,0 +1,59 @@
+package com.mmxlabs.rcp.common.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.nebula.widgets.grid.Grid;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.part.IPage;
+import org.eclipse.ui.views.properties.PropertySheet;
+
+/**
+ * @since 2.0
+ */
+public class CopyPropertySheetToClipboardActionDelegate implements IViewActionDelegate {
+
+	private PropertySheet propertySheet;;
+
+	@Override
+	public void run(final IAction action) {
+		// TODO Auto-generated method stub
+
+		if (propertySheet != null) {
+			final IPage currentPage = propertySheet.getCurrentPage();
+			if (currentPage != null) {
+				Action realAction = null;
+				final Control control = currentPage.getControl();
+				if (control instanceof Table) {
+					realAction = new CopyTableToClipboardAction((Table) control);
+				} else if (control instanceof Tree) {
+					realAction = new CopyTreeToClipboardAction((Tree) control);
+				} else if (control instanceof Grid) {
+					realAction = new CopyGridToClipboardAction((Grid) control);
+				}
+				if (realAction != null) {
+					realAction.run();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void selectionChanged(final IAction action, final ISelection selection) {
+
+	}
+
+	@Override
+	public void init(final IViewPart view) {
+
+		if (view instanceof PropertySheet) {
+			this.propertySheet = (PropertySheet) view;
+		} else {
+			this.propertySheet = null;
+		}
+	}
+}
