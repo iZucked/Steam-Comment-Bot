6430e909225f778b9a45a1bf22d6eafa5d813699
Simon Goodall <simon@simongoodall.co.uk>
2012-11-19 09:40:59 +0000

    Add initial code to look up a timezone from a lat/lon using Google maps API. TODO: Improve error message reporting

---
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/timezone/ITimezoneProvider.java
@@ -0,0 +1,16 @@
+package com.mmxlabs.common.timezone;
+
+import java.util.TimeZone;
+
+/**
+ * An interface to find a {@link TimeZone} given a lat/lon co-ordinate.
+ * 
+ * @see http://stackoverflow.com/questions/41504/timezone-lookup-from-latitude-longitude
+ * 
+ * @author Simon Goodall
+ * @since 2.1
+ */
+public interface ITimezoneProvider {
+
+	TimeZone findTimeZone(float latitude, float longitude);
+}
---
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/timezone/impl/GoogleTimezoneProvider.java
@@ -0,0 +1,111 @@
+package com.mmxlabs.common.timezone.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.TimeZone;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import com.mmxlabs.common.timezone.ITimezoneProvider;
+
+/**
+ * 
+ * A {@link ITimezoneProvider} implementation using the Google APIs.
+ * 
+ * @see https://developers.google.com/maps/documentation/timezone/
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class GoogleTimezoneProvider implements ITimezoneProvider {
+
+	private static final String dstOffset = "dstOffset";
+	/** the offset from UTC (in seconds) for the given location. This does not take into effect daylight savings. */
+	private static final String rawOffset = "rawOffset";
+
+	/**
+	 * a string containing the ID of the time zone, such as "America/Los_Angeles" or "Australia/Sydney".
+	 */
+	private static final String timeZoneId = "timeZoneId";
+	/** a string containing the long form name of the time zone. This field will be localized if the language parameter is set. eg. "Pacific Daylight Time" or "Australian Eastern Daylight Time" */
+	private static final String timeZoneName = "timeZoneName";
+
+	/** a string indicating the status of the response. */
+	private static final String status = "status";
+
+	/** indicates that the request was successful. */
+	private static final String STATUS_OK = "OK";
+	/** indicates that the request was malformed. */
+	private static final String STATUS_INVALID_REQUEST = "INVALID_REQUEST";
+	/** indicates the requestor has exceeded quota. */
+	private static final String STATUS_OVER_QUERY_LIMIT = "OVER_QUERY_LIMIT";
+	/**
+	 * indicates that the the API did not complete the request. Confirm that the request was sent over http instead of https.
+	 */
+	private static final String STATUS_REQUEST_DENIED = "REQUEST_DENIED";
+	/**
+	 * indicates an unknown error.
+	 */
+	private static final String STATUS_UNKNOWN_ERROR = "UNKNOWN_ERROR";
+	/**
+	 * indicates that no time zone data could be found for the specified position or time. Confirm that the request is for a location on land, and not over water.
+	 */
+	private static final String STATUS_ZERO_RESULTS = "ZERO_RESULTS";
+
+	@Override
+	public TimeZone findTimeZone(final float latitude, final float longitude) {
+		InputStream requestStream = null;
+
+		try {
+			final URL requestURL = new URL(buildQueryURL(latitude, longitude));
+			requestStream = requestURL.openStream();
+			final Object result = JSONValue.parse(new InputStreamReader(requestStream));
+			if (result instanceof JSONObject) {
+				final JSONObject jsonObject = (JSONObject) result;
+				if (jsonObject.containsKey(status)) {
+					final String statusString = (String) jsonObject.get(status);
+					if (STATUS_OK.equals(statusString)) {
+
+						if (jsonObject.containsKey(timeZoneId)) {
+							final String timeZoneID = (String) jsonObject.get(timeZoneId);
+
+							return TimeZone.getTimeZone(timeZoneID);
+						}
+					} else {
+						// TODO: Make this more informative
+						throw new RuntimeException(statusString);
+					}
+				}
+			}
+		} catch (final Exception e) {
+			// Wrap up and re-throw
+			throw new RuntimeException(e);
+		} finally {
+			if (requestStream != null) {
+				try {
+					requestStream.close();
+				} catch (final IOException e) {
+					// Ignore
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Construct a request {@link URL} to request timezone in json format
+	 * 
+	 * @param lat
+	 * @param lon
+	 * @return
+	 */
+	private String buildQueryURL(final float lat, final float lon) {
+		final long timestamp = System.currentTimeMillis() / 1000;
+		final String str = String.format("https://maps.googleapis.com/maps/api/timezone/json?location=%f,%f&timestamp=%d&sensor=false", lat, lon, timestamp);
+		return str;
+	}
+
+}
