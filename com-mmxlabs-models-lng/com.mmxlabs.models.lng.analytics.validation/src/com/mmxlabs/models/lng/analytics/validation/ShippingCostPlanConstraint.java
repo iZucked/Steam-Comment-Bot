1a70bd83ad08283d28454086a75915799dce43ae
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-19 15:24:58 +0000

    Update validation so creating new plans does not disable OK button due to broken rows

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingCostPlanConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingCostPlanConstraint.java
@@ -11,68 +11,71 @@ import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
 import com.mmxlabs.models.lng.analytics.DestinationType;
 import com.mmxlabs.models.lng.analytics.ShippingCostPlan;
 import com.mmxlabs.models.lng.analytics.ShippingCostRow;
+import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class ShippingCostPlanConstraint extends AbstractModelMultiConstraint {
 
 	@Override
-	protected String validate(IValidationContext ctx, List<IStatus> statuses) {
-		EObject target = ctx.getTarget();
+	protected String validate(final IValidationContext ctx, final List<IStatus> statuses) {
+		final EObject target = ctx.getTarget();
 		if (target instanceof ShippingCostPlan) {
-			ShippingCostPlan shippingCostPlan = (ShippingCostPlan) target;
+			final ShippingCostPlan shippingCostPlan = (ShippingCostPlan) target;
 
 			if (shippingCostPlan.getBaseFuelPrice() < 0.0001) {
-				DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Base fuel price must be non-zero"));
+				final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Base fuel price must be non-zero"));
 				deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_BaseFuelPrice());
 				statuses.add(deco);
 			}
 
-			DestinationType lastType = null;
-			int idx = 0;
-			for (ShippingCostRow row : shippingCostPlan.getRows()) {
-				DestinationType type = row.getDestinationType();
-				if (idx == 0) {
-					if (type != DestinationType.START) {
-						DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("First row must be a START row"));
+			if (shippingCostPlan.eContainer() != null) {
+				DestinationType lastType = null;
+				int idx = 0;
+				for (final ShippingCostRow row : shippingCostPlan.getRows()) {
+					final DestinationType type = row.getDestinationType();
+					if (idx == 0) {
+						if (type != DestinationType.START) {
+							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("First row must be a START row"));
+							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+							statuses.add(deco);
+						}
+					} else if (idx == shippingCostPlan.getRows().size() - 1) {
+						if (type != DestinationType.END) {
+							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Last row must be a END row"));
+							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+							statuses.add(deco);
+						}
+					} else {
+						if (type == DestinationType.START || type == DestinationType.END) {
+							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Waypoint rows cannot be START or END types"));
+							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+							statuses.add(deco);
+						}
+					}
+					if (lastType == DestinationType.LOAD && type != DestinationType.DISCHARGE) {
+						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
 						deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
 						deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
 						statuses.add(deco);
 					}
-				} else if (idx == shippingCostPlan.getRows().size() - 1) {
-					if (type != DestinationType.END) {
-						DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Last row must be a END row"));
+					if (lastType != DestinationType.LOAD && type == DestinationType.DISCHARGE) {
+						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
 						deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
 						deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
 						statuses.add(deco);
 					}
-				} else {
-					if (type == DestinationType.START || type == DestinationType.END) {
-						DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Waypoint rows cannot be START or END types"));
-						deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
-						deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
-						statuses.add(deco);
-					}
-				}
-				if (lastType == DestinationType.LOAD && type != DestinationType.DISCHARGE) {
-					DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
-					deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
-					deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
-					statuses.add(deco);
-				}
-				if (lastType != DestinationType.LOAD && type == DestinationType.DISCHARGE) {
-					DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
-					deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
-					deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
-					statuses.add(deco);
-				}
 
-				lastType = type;
-				idx++;
+					lastType = type;
+					idx++;
+				}
 
 			}
 		}
-		return "com.mmxlabs.models.lng.analytics.validation";
+		return Activator.PLUGIN_ID;
 	}
 
 }
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingCostRowConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingCostRowConstraint.java
@@ -10,38 +10,55 @@ import org.eclipse.emf.validation.model.IConstraintStatus;
 import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
 import com.mmxlabs.models.lng.analytics.DestinationType;
 import com.mmxlabs.models.lng.analytics.ShippingCostRow;
+import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
 public class ShippingCostRowConstraint extends AbstractModelMultiConstraint {
 
 	@Override
-	protected String validate(IValidationContext ctx, List<IStatus> statuses) {
-		EObject target = ctx.getTarget();
+	protected String validate(final IValidationContext ctx, final List<IStatus> statuses) {
+		final EObject target = ctx.getTarget();
+
+		final IExtraValidationContext extraValidationContext = Activator.getDefault().getExtraValidationContext();
+
 		if (target instanceof ShippingCostRow) {
-			ShippingCostRow shippingCostRow = (ShippingCostRow) target;
+			final ShippingCostRow shippingCostRow = (ShippingCostRow) target;
+			{
+				final EObject plan = shippingCostRow.eContainer();
+				final EObject original = extraValidationContext.getOriginal(plan);
+				if (plan != null && original != plan) {
+					return Activator.PLUGIN_ID;
+				}
+			}
 			if (shippingCostRow.getDate() == null) {
-				DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No date specified"));
+				final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No date specified"));
 				deco.addEObjectAndFeature(shippingCostRow, AnalyticsPackage.eINSTANCE.getShippingCostRow_Date());
 				statuses.add(deco);
 			}
+			if (shippingCostRow.getPort() == null) {
+				final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No port specified"));
+				deco.addEObjectAndFeature(shippingCostRow, AnalyticsPackage.eINSTANCE.getShippingCostRow_Port());
+				statuses.add(deco);
+			}
 
-			DestinationType destinationType = shippingCostRow.getDestinationType();
+			final DestinationType destinationType = shippingCostRow.getDestinationType();
 			if (destinationType == null) {
-				DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No type specified"));
+				final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No type specified"));
 				deco.addEObjectAndFeature(shippingCostRow, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
 				statuses.add(deco);
 			} else {
 				if (destinationType == DestinationType.START || destinationType == DestinationType.DISCHARGE || destinationType == DestinationType.OTHER) {
 					if (shippingCostRow.getCargoPrice() < 0.0001) {
-						DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No cargo must be non-zero"));
+						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No cargo must be non-zero"));
 						deco.addEObjectAndFeature(shippingCostRow, AnalyticsPackage.eINSTANCE.getShippingCostRow_CargoPrice());
 						statuses.add(deco);
 					}
 				}
 				if (destinationType == DestinationType.START || destinationType == DestinationType.LOAD || destinationType == DestinationType.OTHER) {
 					if (shippingCostRow.getCvValue() < 0.0001) {
-						DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No cargo CV be non-zero"));
+						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("No cargo CV be non-zero"));
 						deco.addEObjectAndFeature(shippingCostRow, AnalyticsPackage.eINSTANCE.getShippingCostRow_CargoPrice());
 						statuses.add(deco);
 					}
@@ -49,7 +66,7 @@ public class ShippingCostRowConstraint extends AbstractModelMultiConstraint {
 			}
 
 		}
-		return "com.mmxlabs.models.lng.analytics.validation";
+		return Activator.PLUGIN_ID;
 	}
 
 }
