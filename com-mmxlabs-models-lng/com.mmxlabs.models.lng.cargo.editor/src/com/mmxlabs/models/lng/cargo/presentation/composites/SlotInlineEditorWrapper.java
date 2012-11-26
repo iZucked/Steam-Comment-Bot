3ddd4daab1e504b3464cf3106575d999e9cb4b6e
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2012-11-26 16:41:28 +0000

    BugzId: 476 A few editor dialog controls disabled depending on load / discharge capability in a port. Mutually dependent on a changeset in the models.ui repository.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/SlotInlineEditorWrapper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/SlotInlineEditorWrapper.java
@@ -4,20 +4,13 @@
  */
 package com.mmxlabs.models.lng.cargo.presentation.composites;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EObject;
 
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.DischargeSlot;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
-import com.mmxlabs.models.mmxcore.MMXRootObject;
-import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
 import com.mmxlabs.models.ui.editors.impl.IInlineEditorEnablementWrapper;
-import com.mmxlabs.models.ui.editors.impl.IInlineEditorExternalNotificationListener;
 
 /**
  * @generated NOT
@@ -29,69 +22,24 @@ public class SlotInlineEditorWrapper extends IInlineEditorEnablementWrapper {
 	}
 
 	@Override
-	public void notifyChanged(final Notification notification) {
-		super.notifyChanged(notification);
+	protected boolean respondToNotification(Notification notification) {
+		final Object feature = notification.getFeature();
+		if (input instanceof LoadSlot && feature == CargoPackage.eINSTANCE.getLoadSlot_DESPurchase())
+			return true;
+		if (input instanceof DischargeSlot && feature == CargoPackage.eINSTANCE.getDischargeSlot_FOBSale())
+			return true;
+		return false;
 	}
 
 	@Override
-	protected void missedNotifications(final List<Notification> missed) {
-		for (final Notification n : missed) {
-			reallyNotifyChanged(n);
-		}
-		super.missedNotifications(missed);
-	}
-
-	@Override
-	public void reallyNotifyChanged(final Notification notification) {
-		if (notification.getFeature() == CargoPackage.eINSTANCE.getLoadSlot_DESPurchase()) {
-
-			if (input instanceof LoadSlot) {
-				setEditorEnabled(!((LoadSlot) input).isDESPurchase());
-			}
-
-		}
-		if (notification.getFeature() == CargoPackage.eINSTANCE.getDischargeSlot_FOBSale()) {
-			if (input instanceof DischargeSlot) {
-				setEditorEnabled(!((DischargeSlot) input).isFOBSale());
-			}
-		}
-	}
-
-	@Override
-	public void display(final IScenarioEditingLocation location, final MMXRootObject scenario, final EObject object, final Collection<EObject> range) {
-
-		super.display(location, scenario, object, range);
-
+	protected boolean isEnabled() {
 		if (input instanceof LoadSlot) {
-			setEditorEnabled(!((LoadSlot) input).isDESPurchase());
+			return (!((LoadSlot) input).isDESPurchase());
 		} else if (input instanceof DischargeSlot) {
-			setEditorEnabled(!((DischargeSlot) input).isFOBSale());
+			return (!((DischargeSlot) input).isFOBSale());
 		}
+		return true;
 	}
 
-	@Override
-	public EObject getEditorTarget() {
-		return wrapped.getEditorTarget();
-	}
 
-	/**
-	 * Return the wrapped editor instance.
-	 * 
-	 * @since 2.0
-	 * @return
-	 */
-	public IInlineEditor getWrapped() {
-		return wrapped;
-	}
-
-	@Override
-	public void addNotificationChangedListener(IInlineEditorExternalNotificationListener listener) {
-		wrapped.addNotificationChangedListener(listener);
-
-	}
-
-	@Override
-	public void removeNotificationChangedListener(IInlineEditorExternalNotificationListener listener) {
-		wrapped.removeNotificationChangedListener(listener);
-	}
 }
