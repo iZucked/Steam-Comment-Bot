53520ce3528768593f089fcb6e66560940d837f2
Simon Goodall <simon@simongoodall.co.uk>
2012-11-27 13:54:07 +0000

    Add new detail tree items for large and small currency numbers

---
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/detailtree/impl/TotalCostDetailElement.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.common.detailtree.impl;
+
+import com.mmxlabs.common.detailtree.IDetailTreeElement;
+
+/**
+ * Implementation of {@link IDetailTreeElement} indicating the contained object is a large cost related. We expect a large integer based number - no decimal places.
+ * 
+ * @author Simon Goodall
+ * @since 2.1
+ */
+public class TotalCostDetailElement implements IDetailTreeElement {
+
+	private final Number d;
+
+	public TotalCostDetailElement(final Number d) {
+		this.d = d;
+	}
+
+	@Override
+	public Object getObject() {
+		return d;
+	}
+}
---
+++ com-mmxlabs-common-util/com.mmxlabs.common.util/src/main/java/com/mmxlabs/common/detailtree/impl/UnitPriceDetailElement.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.common.detailtree.impl;
+
+import com.mmxlabs.common.detailtree.IDetailTreeElement;
+
+/**
+ * Implementation of {@link IDetailTreeElement} indicating the contained object is unit price related. We expect a small number with decimal place accuracy.
+ * 
+ * @author Simon Goodall
+ * @since 2.1
+ */
+public class UnitPriceDetailElement implements IDetailTreeElement {
+
+	private final Number d;
+
+	public UnitPriceDetailElement(final Number d) {
+		this.d = d;
+	}
+
+	@Override
+	public Object getObject() {
+		return d;
+	}
+}
