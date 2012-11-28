45c9c83fea32876bd80ab504617d6301f738b877
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-11-28 12:22:09 +0000

    Disallow deleting of start/end rows

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.analytics.transformer.ui/src/com/mmxlabs/models/lng/analytics/transformer/ui/views/ShippingCostRowViewerPane.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.analytics.transformer.ui/src/com/mmxlabs/models/lng/analytics/transformer/ui/views/ShippingCostRowViewerPane.java
@@ -4,21 +4,38 @@
  */
 package com.mmxlabs.models.lng.analytics.transformer.ui.views;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
 
 import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
+import com.mmxlabs.models.lng.analytics.DestinationType;
+import com.mmxlabs.models.lng.analytics.ShippingCostRow;
 import com.mmxlabs.models.lng.analytics.manipulators.DestinationTypeAttributeManipulator;
+import com.mmxlabs.models.lng.ui.actions.ScenarioModifyingAction;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.ui.dates.DateAttributeManipulator;
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 import com.mmxlabs.models.ui.tabular.NumericAttributeManipulator;
 import com.mmxlabs.models.ui.tabular.SingleReferenceManipulator;
+import com.mmxlabs.scenario.service.model.ScenarioLock;
 
 /**
  * A viewer pane for editing shipping cost rows.
@@ -50,5 +67,76 @@ public class ShippingCostRowViewerPane extends ScenarioTableViewerPane {
 		addTypicalColumn("Type", new DestinationTypeAttributeManipulator(AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType(), getEditingDomain()));
 		// Disable sorting
 		getScenarioViewer().setComparator(null);
+	}
+
+	protected Action createDeleteAction() {
+		return new ScenarioModifyingAction("Delete") {
+			{
+				setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
+				setDisabledImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_DELETE_DISABLED));
+				viewer.addSelectionChangedListener(this);
+			}
+
+			@Override
+			public void run() {
+
+				// Delete commands can be slow, so show the busy indicator while deleting.
+				final Runnable runnable = new Runnable() {
+
+					@Override
+					public void run() {
+
+						final ScenarioLock editorLock = jointModelEditorPart.getEditorLock();
+						editorLock.awaitClaim();
+						getJointModelEditorPart().setDisableUpdates(true);
+						try {
+							final ISelection sel = getLastSelection();
+							if (sel instanceof IStructuredSelection) {
+								final EditingDomain ed = jointModelEditorPart.getEditingDomain();
+								// Copy selection
+								@SuppressWarnings("unchecked")
+								final List<?> objects = new ArrayList<Object>(((IStructuredSelection) sel).toList());
+
+								// Clear current selection
+								selectionChanged(new SelectionChangedEvent(viewer, StructuredSelection.EMPTY));
+
+								// Execute command
+								final Command deleteCommand = DeleteCommand.create(ed, objects);
+								ed.getCommandStack().execute(deleteCommand);
+							}
+						} finally {
+							editorLock.release();
+							getJointModelEditorPart().setDisableUpdates(false);
+						}
+					}
+				};
+				BusyIndicator.showWhile(null, runnable);
+			}
+
+			@Override
+			protected boolean isApplicableToSelection(final ISelection selection) {
+				if (selection.isEmpty() == false && selection instanceof IStructuredSelection) {
+					final IStructuredSelection iStructuredSelection = (IStructuredSelection) selection;
+					final Iterator<?> itr = iStructuredSelection.iterator();
+
+					while (itr.hasNext()) {
+						final Object obj = itr.next();
+						if (obj instanceof ShippingCostRow) {
+							final ShippingCostRow shippingCostRow = (ShippingCostRow) obj;
+							if (shippingCostRow.getDestinationType() == DestinationType.START) {
+								return false;
+							}
+							if (shippingCostRow.getDestinationType() == DestinationType.END) {
+								return false;
+							}
+						} else {
+							return false;
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+		};
 	}
 }
