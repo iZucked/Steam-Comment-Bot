bcb3b96cb671c3e746e8c848f6624b1456d35815
Simon McGregor <unknown@kiln.example.com>
2012-12-12 11:35:47 +0000

    Added some minimal documentation to this interface

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.reports/src/com/mmxlabs/shiplingo/platform/reports/IScenarioViewerSynchronizerOutput.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.reports/src/com/mmxlabs/shiplingo/platform/reports/IScenarioViewerSynchronizerOutput.java
@@ -10,9 +10,47 @@ import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
 public interface IScenarioViewerSynchronizerOutput {
+	/**
+	 * Returns a collection of objects which are of interest to 
+	 * a particular report, for all scenarios which have been changed.
+	 * Each collected element is going to be a row in a tabular report.
+	 *  
+	 * @return
+	 */
 	public Collection<Object> getCollectedElements();
+	
+	/**
+	 * Tells you which scenario instance an object from the getCollectedElements()
+	 * method came from.
+	 * 
+	 * @param object
+	 * @return
+	 */
 	public ScenarioInstance getScenarioInstance(Object object);
+	
+	/**
+	 * Is the object from the "pinned" scenario (used as a reference scenario)?
+	 * 
+	 * @param object
+	 * @return
+	 */
 	public boolean isPinned(Object object);
+	
+	/**
+	 * Gets the MMX root object for a particular object from the 
+	 * getCollectedElements list.
+	 * 
+	 * @param object
+	 * @return
+	 */
 	public MMXRootObject getRootObject(Object object);
+	
+	/**
+	 * Returns the MMX root objects for all the currently selected scenarios
+	 * regardless of whether or not there are getCollectedElements objects coming
+	 * from them. 
+	 *  
+	 * @return
+	 */
 	public Collection<MMXRootObject> getRootObjects();
 }
