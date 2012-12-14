56b578a7dbf40e2a74d6b965a4a8a828c93c652c
Simon Goodall <simon.goodall@minimaxlabs.com>
2012-12-14 10:35:54 +0000

    Remove VirtualCargo class
    BugzId: 646

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/ScheduledSequences.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/main/java/com/mmxlabs/scheduler/optimiser/fitness/ScheduledSequences.java
@@ -6,13 +6,9 @@ package com.mmxlabs.scheduler.optimiser.fitness;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import com.mmxlabs.optimiser.core.IResource;
-import com.mmxlabs.scheduler.optimiser.components.IDischargeOption;
-import com.mmxlabs.scheduler.optimiser.components.ILoadOption;
 import com.mmxlabs.scheduler.optimiser.fitness.components.allocation.IAllocationAnnotation;
 import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlan;
 
@@ -28,8 +24,6 @@ public final class ScheduledSequences extends ArrayList<ScheduledSequence> {
 	private static final long serialVersionUID = 1L;
 
 	private Collection<IAllocationAnnotation> allocations = null;
-	@Deprecated
-	private final List<VirtualCargo> virtualCargoes = new LinkedList<VirtualCargo>();
 
 	public final Collection<IAllocationAnnotation> getAllocations() {
 		return allocations;
@@ -49,14 +43,5 @@ public final class ScheduledSequences extends ArrayList<ScheduledSequence> {
 	 */
 	public void addScheduledSequence(final IResource resource, final int startTime, final List<VoyagePlan> voyagePlans, int[] arrivalTimes) {
 		add(new ScheduledSequence(resource, startTime, voyagePlans, arrivalTimes));
-	}
-	@Deprecated
-	public List<VirtualCargo> getVirtualCargoes() {
-		return Collections.unmodifiableList(virtualCargoes);
-	}
-
-	@Deprecated
-	public void addVirtualCargo(final ILoadOption loadOption, final IDischargeOption dischargeOption, final int loadTime, final int dischargeTime) {
-		virtualCargoes.add(new VirtualCargo(loadOption, dischargeOption, loadTime, dischargeTime));
 	}
 }
