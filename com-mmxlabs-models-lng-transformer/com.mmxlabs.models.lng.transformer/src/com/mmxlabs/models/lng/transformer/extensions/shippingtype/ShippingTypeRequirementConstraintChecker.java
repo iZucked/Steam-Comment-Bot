964e580dc895d7c37376948be10d2e7450676134
Simon McGregor <unknown@kiln.example.com>
2013-01-03 13:08:13 +0000

    BugzID: 810 Unit tests for the shipping type constraint checker.

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/extensions/shippingtype/ShippingTypeRequirementConstraintChecker.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/extensions/shippingtype/ShippingTypeRequirementConstraintChecker.java
@@ -1,94 +1,33 @@
 package com.mmxlabs.models.lng.transformer.extensions.shippingtype;
 
-import java.util.List;
-
 import com.google.inject.Inject;
 import com.mmxlabs.models.lng.types.CargoDeliveryType;
 import com.mmxlabs.optimiser.core.IResource;
-import com.mmxlabs.optimiser.core.ISequence;
 import com.mmxlabs.optimiser.core.ISequenceElement;
-import com.mmxlabs.optimiser.core.ISequences;
 import com.mmxlabs.optimiser.core.constraints.IPairwiseConstraintChecker;
-import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
-import com.mmxlabs.scheduler.optimiser.components.VesselInstanceType;
-import com.mmxlabs.scheduler.optimiser.providers.IPortTypeProvider;
-import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
-import com.mmxlabs.scheduler.optimiser.providers.PortType;
+import com.mmxlabs.scheduler.optimiser.constraints.impl.AbstractPairwiseConstraintChecker;
 
 /**
  * An implementation of {@link IPairwiseConstraintChecker} to forbid certain {@link ISequenceElement} pairings
  * 
  * @since 2.0
  */
-public class ShippingTypeRequirementConstraintChecker implements IPairwiseConstraintChecker {
-
-	private final String name;
+public class ShippingTypeRequirementConstraintChecker extends AbstractPairwiseConstraintChecker {
 
 	@Inject
-	private IShippingTypeRequirementProvider desPermissionProvider;
-
-	@Inject
-	private IVesselProvider vesselProvider;
-
-	@Inject
-	private IPortTypeProvider portTypeProvider;
+	private IShippingTypeRequirementProvider shippingTypeRequirementProvider;
 
 	public ShippingTypeRequirementConstraintChecker(final String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public boolean checkConstraints(final ISequences sequences) {
-		return checkConstraints(sequences, null);
-	}
-
-	@Override
-	public boolean checkConstraints(final ISequences sequences, final List<String> messages) {
-
-		for (final IResource resource : sequences.getResources()) {
-			final ISequence sequence = sequences.getSequence(resource);
-			ISequenceElement prev = null;
-			for (final ISequenceElement current : sequence) {
-				if (prev != null) {
-					if (!checkPairwiseConstraint(prev, current, resource)) {
-						return false;
-					}
-				}
-				prev = current;
-			}
-		}
-
-		return true;
-	}
-
-	@Override
-	public void setOptimisationData(final IOptimisationData optimisationData) {
-
+		super(name);
 	}
 
 	@Override
 	public boolean checkPairwiseConstraint(final ISequenceElement first, final ISequenceElement second, final IResource resource) {
 
-		final VesselInstanceType instanceType = vesselProvider.getVessel(resource).getVesselInstanceType();
-		if (instanceType == VesselInstanceType.CARGO_SHORTS) {
-			// Cargo pairs are independent of each other, so only check real load->discharge state and ignore rest
-			final PortType t1 = portTypeProvider.getPortType(first);
-			final PortType t2 = portTypeProvider.getPortType(second);
-			// Accept, or fall through
-			if (!(t1 == PortType.Load && t2 == PortType.Discharge)) {
-				return true;
-			}
-		}
-
-		CargoDeliveryType requiredShippingType = desPermissionProvider.getSalesSlotRequiredDeliveryType(second);
+		CargoDeliveryType requiredShippingType = shippingTypeRequirementProvider.getSalesSlotRequiredDeliveryType(second);
 		
-		if (requiredShippingType != null) {
-			return requiredShippingType == desPermissionProvider.getPurchaseSlotDeliveryType(first);
+		if (requiredShippingType != null && requiredShippingType != CargoDeliveryType.ANY) {
+			return requiredShippingType == shippingTypeRequirementProvider.getPurchaseSlotDeliveryType(first);
 		}
 
 		return true;
@@ -96,6 +35,13 @@ public class ShippingTypeRequirementConstraintChecker implements IPairwiseConstr
 
 	@Override
 	public String explain(final ISequenceElement first, final ISequenceElement second, final IResource resource) {
+		CargoDeliveryType requiredShippingType = shippingTypeRequirementProvider.getSalesSlotRequiredDeliveryType(second);
+		CargoDeliveryType shippingType = shippingTypeRequirementProvider.getPurchaseSlotDeliveryType(first);
+		
+		if (requiredShippingType != null && requiredShippingType != shippingType) {
+			return String.format("Required shipping delivery type of %s does not match purchase delivery type of %s.", requiredShippingType.getName(), shippingType.getName());
+		}
+		
 		return null;
 	}
 }
