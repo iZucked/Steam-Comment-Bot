29110ce58b0c6bd546be0f036236adf5d2b16b62
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-04 17:28:23 +0000

    Pass in URIConvertor to migrate

--- com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/IMigrationUnit.java
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/IMigrationUnit.java
@@ -3,6 +3,7 @@ package com.mmxlabs.models.migration;
 import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.URIConverter;
 
 /**
  * A single migration unit to convert between two versions of a scenario version.
@@ -33,5 +34,13 @@ public interface IMigrationUnit {
 	 */
 	int getDestinationVersion();
 
-	void migrate(List<URI> uris) throws Exception;
+	/**
+	 * Perform the migration. The {@link List} or {@link URI}s specify all of the ecore model instances in the scenario. These are likely to be different to the original location. The
+	 * {@link URIConverter} will "normalize" convert between original URI and the new temporary URI. See {@link URIConverter#getURIMap()}.
+	 * 
+	 * @param uris
+	 * @param uc
+	 * @throws Exception
+	 */
+	void migrate(List<URI> uris, URIConverter uc) throws Exception;
 }
