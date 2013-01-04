8dbec0fa65fa4314f27b62ce15c1e7e32061b082
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-04 14:49:51 +0000

    Tests

---
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration.tests/src/com/mmxlabs/models/migration/MigrationTest.java
@@ -0,0 +1,130 @@
+package com.mmxlabs.models.migration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.FeatureMap.Entry;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xml.type.AnyType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mmxlabs.models.migration.utils.MetamodelLoader;
+import com.mmxlabs.models.migration.utils.MetamodelUtils;
+
+public class MigrationTest {
+
+	@Test
+	public void migrationTest() throws IOException {
+
+		// Create a V1 loader
+		final MetamodelLoader v1Loader = new MetamodelLoader();
+		final EPackage v1Pkg = v1Loader.loadEPackage(URI.createURI(getClass().getResource("/models/model-v1.ecore").toString()), "platform:/plugin/com.mmxlabs.models.migration.tests/models/model.ecore");
+		Assert.assertNotNull(v1Pkg);
+		final EPackage v1SubPkg = v1Loader.loadEPackage(URI.createURI(getClass().getResource("/models/sub-model-v1.ecore").toString()),
+				"platform:/plugin/com.mmxlabs.models.migration.tests/models/submodel.ecore");
+		Assert.assertNotNull(v1SubPkg);
+
+		// Create a V2 loader
+		final MetamodelLoader v2Loader = new MetamodelLoader();
+		final EPackage v2Pkg = v2Loader.loadEPackage(URI.createURI(getClass().getResource("/models/model-v2.ecore").toString()), "platform:/plugin/com.mmxlabs.models.migration.tests/models/model.ecore");
+		Assert.assertNotNull(v2Pkg);
+		final EPackage v2SubPkg = v2Loader.loadEPackage(URI.createURI(getClass().getResource("/models/sub-model-v2.ecore").toString()),
+				"platform:/plugin/com.mmxlabs.models.migration.tests/models/submodel.ecore");
+		Assert.assertNotNull(v2SubPkg);
+
+		// Initialise a V1 model instance
+		final EObject v1Object = createV1Model(v1SubPkg, v1Pkg, 1, 2, 3);
+
+		// Create a tmp file to serialise data to
+		final File f = File.createTempFile("migrationTest", ".xmi");
+		f.deleteOnExit();
+
+		// Save model to tmp file.
+		final URI fileURI = URI.createFileURI(f.getCanonicalPath());
+		final Resource r1 = v1Loader.getResourceSet().createResource(fileURI);
+		r1.getContents().add(v1Object);
+		r1.save(Collections.emptyMap());
+
+		// Reload model under V2 metamodels
+		final Resource r2 = v2Loader.getResourceSet().createResource(fileURI);
+		final Map<Object, Object> loadOptions = new HashMap<Object, Object>();
+		// Record features which have no meta-model equivalent so we can perform migration
+		loadOptions.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
+		r2.load(loadOptions);
+
+		// Delete tmp file
+		f.delete();
+
+		// Check model was loaded
+		Assert.assertNotNull(r2.getContents());
+		Assert.assertFalse(r2.getContents().isEmpty());
+
+		final EObject v2Object = r2.getContents().iterator().next();
+		Assert.assertNotNull(v2Object);
+
+		// This map records features with no model equiv - i.e. V1 features not in V2
+		final Map<EObject, AnyType> oldFeatures = ((XMLResource) r2).getEObjectToExtensionMap();
+
+		// Migration....
+		{
+
+			// Convert unknown features
+			if (oldFeatures.containsKey(v2Object)) {
+
+				final AnyType anyType = oldFeatures.get(v2Object);
+				for (final Entry e : anyType.getAnyAttribute()) {
+
+					final EStructuralFeature eStructuralFeature = e.getEStructuralFeature();
+					// Copy value from attributeB to attributeC
+					if (eStructuralFeature.getName().equals("attributeB")) {
+						// Need to parse string as the de-serialization does not know what type the object is.
+						final Object newValue = Integer.parseInt(e.getValue().toString());
+						// Update the new value.
+						v2Object.eSet(v2Object.eClass().getEStructuralFeature("attributeC"), newValue);
+					}
+				}
+			}
+		}
+		// Check our new attribute has required value.
+		// TODO: Should we persist and reload?
+		Assert.assertEquals(1, ((Integer) v2Object.eGet(v2Object.eClass().getEStructuralFeature("attributeA"))).intValue());
+		Assert.assertEquals(2, ((Integer) v2Object.eGet(v2Object.eClass().getEStructuralFeature("attributeC"))).intValue());
+		Assert.assertEquals(3, ((Integer) v2Object.eGet(v2Object.eClass().getEStructuralFeature("subAttributeA"))).intValue());
+
+	}
+
+	/**
+	 * Create a simple metamodel in V1 format
+	 */
+	private EObject createV1Model(final EPackage subPkg, final EPackage pkg, final int vAttribA, final int vAttribB, final int vSubAttribA) {
+
+		final EClass classA = MetamodelUtils.getEClass(pkg, "ClassA");
+		final EClass subClassA = MetamodelUtils.getEClass(subPkg, "SubClassA");
+
+		final EStructuralFeature attributeA = classA.getEStructuralFeature("attributeA");
+		final EStructuralFeature attributeB = classA.getEStructuralFeature("attributeB");
+		final EStructuralFeature subAttributeA = subClassA.getEStructuralFeature("subAttributeA");
+
+		final EFactory subFactory = subPkg.getEFactoryInstance();
+
+		final EObject obj = subFactory.create(subClassA);
+
+		obj.eSet(attributeA, vAttribA);
+		obj.eSet(attributeB, vAttribB);
+		obj.eSet(subAttributeA, vSubAttribA);
+
+		return obj;
+
+	}
+}
---
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration.tests/src/com/mmxlabs/models/migration/impl/MigrationRegistryTests.java
@@ -0,0 +1,93 @@
+package com.mmxlabs.models.migration.impl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mmxlabs.models.migration.IMigrationUnit;
+
+public class MigrationRegistryTests {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingContext1() {
+		final MigrationRegistry registry = new MigrationRegistry();
+
+		final String context = "context";
+		Assert.assertFalse(registry.isContextRegistered(context));
+		registry.getLatestContextVersion(context);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testMissingContext2() {
+		final MigrationRegistry registry = new MigrationRegistry();
+
+		final String context = "context";
+
+		Assert.assertFalse(registry.isContextRegistered(context));
+		registry.getMigrationChain(context, -1, -1);
+	}
+
+	@Test
+	public void testRegisterContext() {
+		final MigrationRegistry registry = new MigrationRegistry();
+
+		final String context = "context";
+		final int latestVersion = 10;
+
+		Assert.assertFalse(registry.isContextRegistered(context));
+		registry.registerContext(context, latestVersion);
+
+		Assert.assertTrue(registry.isContextRegistered(context));
+
+		Assert.assertEquals(latestVersion, registry.getLatestContextVersion(context));
+		Assert.assertNotNull(registry.getMigrationChain(context, -1, -1));
+	}
+
+	@Test
+	public void testGetMigrationChain1() {
+
+		final MigrationRegistry registry = new MigrationRegistry();
+
+		final String context = "context";
+		final int latestVersion = 10;
+
+		registry.registerContext(context, latestVersion);
+
+		final IMigrationUnit unit1 = mock(IMigrationUnit.class);
+		when(unit1.getContext()).thenReturn(context);
+		when(unit1.getSourceVersion()).thenReturn(1);
+		when(unit1.getDestinationVersion()).thenReturn(2);
+
+		final IMigrationUnit unit2 = mock(IMigrationUnit.class);
+		when(unit2.getContext()).thenReturn(context);
+		when(unit2.getSourceVersion()).thenReturn(2);
+		when(unit2.getDestinationVersion()).thenReturn(3);
+
+		final IMigrationUnit unit3 = mock(IMigrationUnit.class);
+		when(unit3.getContext()).thenReturn(context);
+		when(unit3.getSourceVersion()).thenReturn(3);
+		when(unit3.getDestinationVersion()).thenReturn(10);
+
+		registry.registerMigrationUnit(unit3);
+		registry.registerMigrationUnit(unit2);
+		registry.registerMigrationUnit(unit1);
+
+		final List<IMigrationUnit> chain0 = registry.getMigrationChain(context, 1, 1);
+		Assert.assertTrue(chain0.isEmpty());
+
+		final List<IMigrationUnit> chain1 = registry.getMigrationChain(context, 1, 2);
+		Assert.assertEquals(1, chain1.size());
+		Assert.assertSame(unit1, chain1.get(0));
+
+		final List<IMigrationUnit> chain2 = registry.getMigrationChain(context, 1, 10);
+		Assert.assertEquals(3, chain2.size());
+		Assert.assertSame(unit1, chain2.get(0));
+		Assert.assertSame(unit2, chain2.get(1));
+		Assert.assertSame(unit3, chain2.get(2));
+
+	}
+}
