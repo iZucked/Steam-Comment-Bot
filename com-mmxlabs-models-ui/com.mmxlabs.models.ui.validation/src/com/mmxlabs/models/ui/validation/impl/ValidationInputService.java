6dcad85be8dd636fb9bf03f060a9e1287fe3db33
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-07 13:27:38 +0000

    Update license headers

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/impl/ValidationInputService.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/impl/ValidationInputService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
  * All rights reserved.
  */
 package com.mmxlabs.models.ui.validation.impl;
--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/dialogs/MessageDialogWithCheckbox.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/dialogs/MessageDialogWithCheckbox.java
@@ -1,14 +1,7 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
 package com.mmxlabs.models.ui.editors.dialogs;
 
 import org.eclipse.jface.dialogs.MessageDialog;
