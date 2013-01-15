a94876a4e47619320bf18c10068e3d1f530391b3
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-15 14:27:44 +0000

    Define a service interface to provide migration capabilities to the scenario service

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.model/src/com/mmxlabs/scenario/service/IScenarioMigrationService.java
@@ -0,0 +1,20 @@
+package com.mmxlabs.scenario.service;
+
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+/**
+ * An interface specifying a service to handle scenario migration. Scenario services should require an instance of this service to be available to ensure scenarios can be properly loaded.
+ * 
+ * @since 3.0
+ */
+public interface IScenarioMigrationService {
+
+	/**
+	 * Attempt to migrate scenario to latest version. This method will do nothing if the scenario is already at the latest version. Any errors during a migration will an {@link Exception} of some
+	 * kind.
+	 * 
+	 * @param scenarioService
+	 * @param scenarioInstance
+	 */
+	void migrateScenario(final IScenarioService scenarioService, final ScenarioInstance scenarioInstance) throws Exception;
+}
