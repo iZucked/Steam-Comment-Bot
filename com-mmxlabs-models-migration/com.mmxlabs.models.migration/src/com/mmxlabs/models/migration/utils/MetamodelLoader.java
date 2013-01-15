5328bfb291f37461379dba11bbdda2adc9a94831
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-15 16:36:35 +0000

    Add handy EPAckage by ns UI method

--- com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/utils/MetamodelLoader.java
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/utils/MetamodelLoader.java
@@ -34,29 +34,35 @@ public class MetamodelLoader {
 	 * 
 	 * @param location
 	 *            Location to read ecore model from
-	 * @param string 
+	 * @param string
 	 * @return
 	 */
-	public EPackage loadEPackage(final URI location, String nsURI) {
+	public EPackage loadEPackage(final URI location, final String nsURI) {
 
 		final Resource rs = resourceSet.getResource(location, true);
 		final EPackage ePackage = (EPackage) rs.getContents().get(0);
-		
-		resourceSet.getPackageRegistry().put(nsURI, ePackage);
+
+		if (nsURI != null) {
+			resourceSet.getPackageRegistry().put(nsURI, ePackage);
+		}
 		resourceSet.getPackageRegistry().put(ePackage.getNsURI(), ePackage);
 		return ePackage;
 	}
 
-//	public EFactory loadEFactory(final URI location) {
-//
-//		final EPackage ePackage = loadEPackage(location);
-//		if (ePackage != null) {
-//			return ePackage.getEFactoryInstance();
-//		}
-//		return null;
-//	}
+	public EPackage getPackageByNSURI(final String nsURI) {
+		return resourceSet.getPackageRegistry().getEPackage(nsURI);
+	}
 
-	public EClassifier getClassifier(EPackage ePackage, String classifierName) {
+	// public EFactory loadEFactory(final URI location) {
+	//
+	// final EPackage ePackage = loadEPackage(location);
+	// if (ePackage != null) {
+	// return ePackage.getEFactoryInstance();
+	// }
+	// return null;
+	// }
+
+	public EClassifier getClassifier(final EPackage ePackage, final String classifierName) {
 		return ePackage.getEClassifier(classifierName);
 	}
 
