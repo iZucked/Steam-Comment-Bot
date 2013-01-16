4ad38c4bfc3b3d6e7e92978ffd227675cd5fca73
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-01-16 12:40:49 +0000

    Clean up activator

--- com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/internal/MigrationActivator.java
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/internal/MigrationActivator.java
@@ -14,40 +14,18 @@ import com.mmxlabs.models.migration.IMigrationRegistry;
 
 public class MigrationActivator implements BundleActivator {
 
-	private Injector inj;
 	@Inject
 	private Export<IMigrationRegistry> migrationRegistry;
 
-	public void start(BundleContext bc) throws Exception {
-		/* Setup Guice */
-		inj = Guice.createInjector(Peaberry.osgiModule(bc, EclipseRegistry.eclipseRegistry()), new MigrationActivationModule());
-		//
-		// /* Create bundle content */
+	public void start(final BundleContext bc) throws Exception {
+		// Bind our module together with the hooks to the eclipse registry to get plugin extensions.
+		final Injector inj = Guice.createInjector(Peaberry.osgiModule(bc, EclipseRegistry.eclipseRegistry()), new MigrationActivationModule());
 		inj.injectMembers(this);
-		//
-		// /* Activate content. Call the relevant start()/open() methods */
-		// singletonM.start();
-		// ...
-		// singletonK.start();
 	}
 
 	@Override
-	public void stop(BundleContext context) throws Exception {
-		// /* Deactivate content. Call the relevant stop()/close()/dispose() methods */
+	public void stop(final BundleContext context) throws Exception {
 		migrationRegistry.unput();
-		// ...
-		// exportN.unput();
-		// singletonM.stop();
-		// ...
-		// singletonK.stop();
-		//
-		// /* Detach dead content */
 		migrationRegistry = null;
-		// ...
-		// exportN = null;
-		// singleton1 = null;
-		// ...
-		// singletonN = null;
 	}
-
 }
