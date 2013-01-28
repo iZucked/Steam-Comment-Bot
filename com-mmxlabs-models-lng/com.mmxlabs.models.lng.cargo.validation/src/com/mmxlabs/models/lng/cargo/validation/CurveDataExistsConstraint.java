a78ac675d4e75f6a928de53267f99b9b7413f0c1
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-01-28 15:00:02 +0000

    BugzID: 813 Add start date validation to tax rate and market index curves.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/CurveDataExistsConstraint.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.cargo.validation;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
+import com.mmxlabs.models.lng.commercial.CommercialModel;
+import com.mmxlabs.models.lng.commercial.CommercialPackage;
+import com.mmxlabs.models.lng.commercial.LegalEntity;
+import com.mmxlabs.models.lng.commercial.TaxRate;
+import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.pricing.Index;
+import com.mmxlabs.models.lng.pricing.PricingModel;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
+import com.mmxlabs.models.lng.commercial.parseutils.Exposures;
+
+/**
+ * A model constraint for checking that a slot's minimum and maximum volumes are sensible (0 <= min <= max)
+ * 
+ * @author Tom Hinton
+ * 
+ */
+public class CurveDataExistsConstraint extends AbstractModelConstraint {
+	IndexStartFinder indexFinder = new IndexStartFinder();
+	TaxRateStartFinder taxFinder = new TaxRateStartFinder();
+	
+	interface CurveStartFinder<CurveType> {
+		Date getStart(CurveType curve);
+	}
+	
+	class IndexStartFinder implements CurveStartFinder<Index<?>> {
+		@Override
+		public Date getStart(Index<?> curve) {
+			Date result = null;
+			for (Date date: curve.getDates()) {
+				if (result == null || result.after(date)) {
+					result = date;
+				}
+			}
+			return result;
+		}		
+	}
+	
+	// TaxRate objects should have been implemented as Index curves, but were not
+	// until they are, this code works
+	class TaxRateStartFinder implements CurveStartFinder<EList<TaxRate>> {
+
+		@Override
+		public Date getStart(EList<TaxRate> curve) {
+			Date result = null;
+			for (TaxRate rate: curve) {
+				Date date = rate.getDate();
+				if (result == null || result.after(date)) {
+					result = date;
+				}
+			}
+			return result;
+		}
+		
+	}
+	
+	<T> Map<Object, Date> getEarliestDates(final CurveStartFinder<T> finder, final IValidationContext ctx) {
+		@SuppressWarnings("unchecked")
+		Map<Object, Date> result = (Map<Object, Date>) ctx.getCurrentConstraintData();
+		if (result == null) {
+			result = new HashMap<Object, Date>();
+			
+		}
+		return result;
+	}
+
+	<T> Date getEarliestDate(final CurveStartFinder<T> finder, final T curve, final IValidationContext ctx) {
+		Map<Object, Date> map = getEarliestDates(finder, ctx);
+		Date result = map.get(curve);
+		if (result == null) {
+			result = finder.getStart(curve);
+			map.put(curve, result);
+		}
+		return result;
+	}
+
+	/**
+	 * Checks to see if a slot has any validation problems associated with missing curve data in:
+	 * - market indices (in any associated contract or price expression)
+	 * - entity tax rates (in any associated contract)
+	 * 
+	 * @param slot
+	 * @param ctx
+	 * @param failures
+	 */
+	protected void validateSlot(final Slot slot, final IValidationContext ctx, final List<IStatus> failures) {
+		final IExtraValidationContext extraValidationContext = Activator.getDefault().getExtraValidationContext();
+
+		final MMXRootObject rootObject = extraValidationContext.getRootObject();
+		final PricingModel pricingModel = rootObject.getSubModel(PricingModel.class);
+
+		// earliest slot date 
+		final Date date = slot.getWindowStartWithSlotOrPortTime();
+		
+		// check market indices
+		for (Index<?> index: pricingModel.getCommodityIndices()) {
+			if (Exposures.getExposureCoefficient(slot, index) != 0) {
+				if (date.before(getEarliestDate(indexFinder, index, ctx))) {
+					String format = "There is no data in index '%s' for '%s' (the window start of slot '%s').";
+					final String failureMessage = String.format(format, index.getName(), slot.getWindowStart(), slot.getName());
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage), IStatus.WARNING);
+					if (slot.isSetPriceExpression()) {
+						dsd.addEObjectAndFeature(slot, CargoPackage.Literals.SLOT__PRICE_EXPRESSION);						
+					}
+					else {
+						dsd.addEObjectAndFeature(slot, CargoPackage.Literals.SLOT__CONTRACT);												
+					}
+					dsd.addEObjectAndFeature(slot, CargoPackage.Literals.SLOT__WINDOW_START);
+					failures.add(dsd);
+				}
+			}
+		}
+		
+		final LegalEntity entity = slot.getContract().getEntity();
+		
+		// check entity tax rates
+		if (date.before(getEarliestDate(taxFinder, entity.getTaxRates(), ctx))) {
+			String format = "There is no tax data in contract entity '%s' for '%s' (the window start of slot '%s').";
+			final String failureMessage = String.format(format, entity.getName(), date, slot.getName());
+			final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage), IStatus.WARNING);
+			dsd.addEObjectAndFeature(slot, CargoPackage.Literals.SLOT__WINDOW_START);
+			dsd.addEObjectAndFeature(slot, CargoPackage.Literals.SLOT__CONTRACT);
+			dsd.addEObjectAndFeature(entity, CommercialPackage.Literals.LEGAL_ENTITY__TAX_RATES);
+			failures.add(dsd);				
+		}
+		
+	}
+	
+	/**
+	 * Checks to see if a slot has any validation problems associated with missing curve data in:
+	 * - entity tax rates (for the shipping entity)
+	 * 
+	 * @param cargo
+	 * @param ctx
+	 * @param failures
+	 */
+	protected void validateCargo(final Cargo cargo, final IValidationContext ctx, final List<IStatus> failures) {		
+		final IExtraValidationContext extraValidationContext = Activator.getDefault().getExtraValidationContext();
+
+		final MMXRootObject rootObject = extraValidationContext.getRootObject();
+		final CommercialModel commercialModel = rootObject.getSubModel(CommercialModel.class);
+		
+		final Slot loadSlot = cargo.getLoadSlot();
+		
+		final Date date = loadSlot.getWindowStartWithSlotOrPortTime();
+		final LegalEntity entity = commercialModel.getShippingEntity(); // get default shipping entity
+		
+		// check entity tax rates
+		if (date.before(getEarliestDate(taxFinder, entity.getTaxRates(), ctx))) {
+			String format = "There is no tax data in shipping entity '%s' for '%s' (the load date for cargo '%s').";
+			final String failureMessage = String.format(format, entity.getName(), date, cargo.getName());
+			final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage), IStatus.WARNING);
+			dsd.addEObjectAndFeature(loadSlot, CargoPackage.Literals.SLOT__WINDOW_START);
+			dsd.addEObjectAndFeature(entity, CommercialPackage.Literals.LEGAL_ENTITY__TAX_RATES);
+			failures.add(dsd);				
+		}						
+	}
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse .emf.validation.IValidationContext)
+	 */
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject object = ctx.getTarget();
+		final List<IStatus> failures = new LinkedList<IStatus>();
+				
+		// check slots for index data (price expressions or contracts) and tax rate data (entity)
+		if (object instanceof Slot) {
+			validateSlot((Slot) object, ctx, failures);
+		}
+		// check cargoes against the shipping entity tax curve
+		else if (object instanceof Cargo) {
+			validateCargo((Cargo) object, ctx, failures);
+		}
+		// check vessel availability against charter curve data (if relevant)
+		else if (object instanceof Vessel) {
+			
+		}
+
+		if (failures.isEmpty()) {
+			return ctx.createSuccessStatus();
+		} else if (failures.size() == 1) {
+			return failures.get(0);
+		} else {
+			final MultiStatus multi = new MultiStatus(Activator.PLUGIN_ID, IStatus.ERROR, null, null);
+			for (final IStatus s : failures) {
+				multi.add(s);
+			}
+			return multi;
+		}
+	}
+
+}
