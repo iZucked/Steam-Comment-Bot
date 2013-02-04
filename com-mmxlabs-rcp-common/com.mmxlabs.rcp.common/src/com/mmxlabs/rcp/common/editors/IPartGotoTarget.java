9ec95cd2a2a00590673c6c7cc96a99141ac2d7e4
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-04 14:51:08 +0000

    Renamed

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/editors/IPartGotoTarget.java
@@ -0,0 +1,19 @@
+package com.mmxlabs.rcp.common.editors;
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewPart;
+
+/**
+ * The {@link IPartGotoTarget} interface is intended to be implemented by {@link IEditorPart}s or {@link IViewPart}s as a way of switching focus to the selected object - making in the current editing
+ * focal point. For example this could be switching tabs and selecting the object in a table. This could be performed by a {@link ISelectionListener}, but that does not guarantee the correct editor
+ * tab is selected.
+ * 
+ * @author Simon Goodall
+ * @since 2.1
+ * 
+ */
+public interface IPartGotoTarget {
+
+	void gotoTarget(Object object);
+}
