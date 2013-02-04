e9b1d155ba9397bbaf0cc8c8259f1659682e1d31
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-04 12:29:00 +0000

    For ScenarioFragments - update the open handler to use the IPartGotoTarget to open editor and select the correct data item

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/OpenScenarioCommandHandler.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/OpenScenarioCommandHandler.java
@@ -12,12 +12,15 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mmxlabs.rcp.common.editors.IPartGotoTarget;
+import com.mmxlabs.scenario.service.model.ScenarioFragment;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 import com.mmxlabs.scenario.service.ui.OpenScenarioUtils;
 
@@ -40,18 +43,34 @@ public class OpenScenarioCommandHandler extends AbstractHandler {
 				final Object element = iterator.next();
 				if (element instanceof ScenarioInstance) {
 
-					try {
-						OpenScenarioUtils.openScenarioInstance(activePage, (ScenarioInstance) element);
-					} catch (final PartInitException e) {
-
-						MessageDialog.openError(activePage.getWorkbenchWindow().getShell(), "Error opening editor", e.getMessage());
-
-						log.error(e.getMessage(), e);
+					openEditor(activePage, element);
+				} else if (element instanceof ScenarioFragment) {
+					final ScenarioFragment scenarioFragment = (ScenarioFragment) element;
+					final IEditorPart part = openEditor(activePage, scenarioFragment.getScenarioInstance());
+					if (part instanceof IPartGotoTarget) {
+						((IPartGotoTarget) part).gotoTarget(scenarioFragment.getFragment());
+					} else {
+						final Object adapter = part.getAdapter(IPartGotoTarget.class);
+						if (adapter != null) {
+							((IPartGotoTarget) adapter).gotoTarget(scenarioFragment.getFragment());
+						}
 					}
 				}
 			}
 		}
 
+		return null;
+	}
+
+	private IEditorPart openEditor(final IWorkbenchPage activePage, final Object element) {
+		try {
+			return OpenScenarioUtils.openAndReturnEditorPart(activePage, (ScenarioInstance) element);
+		} catch (final PartInitException e) {
+
+			MessageDialog.openError(activePage.getWorkbenchWindow().getShell(), "Error opening editor", e.getMessage());
+
+			log.error(e.getMessage(), e);
+		}
 		return null;
 	}
 
