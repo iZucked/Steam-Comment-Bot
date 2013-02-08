d67317f9ae68e91db6e91221853f9924ff1e0a07
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-08 21:06:02 +0000

    Hook in some constraints

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/OpportunityExpressionConstraint.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.analytics.validation;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.common.parser.IExpression;
+import com.mmxlabs.common.parser.series.ISeries;
+import com.mmxlabs.common.parser.series.SeriesParser;
+import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
+import com.mmxlabs.models.lng.analytics.BuyOpportunity;
+import com.mmxlabs.models.lng.analytics.SellOpportunity;
+import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
+import com.mmxlabs.models.lng.pricing.DataIndex;
+import com.mmxlabs.models.lng.pricing.DerivedIndex;
+import com.mmxlabs.models.lng.pricing.Index;
+import com.mmxlabs.models.lng.pricing.PricingModel;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
+
+public class OpportunityExpressionConstraint extends AbstractModelMultiConstraint {
+	@Override
+	public String validate(final IValidationContext ctx, final List<IStatus> failures) {
+		final EObject target = ctx.getTarget();
+
+		if (target instanceof BuyOpportunity) {
+			final SeriesParser parser = getParser();
+			final BuyOpportunity slot = (BuyOpportunity) target;
+				validatePriceExpression(ctx, slot, AnalyticsPackage.eINSTANCE.getBuyOpportunity_PriceExpression(), slot.getPriceExpression(), parser, failures);
+		}
+		if (target instanceof SellOpportunity) {
+			final SeriesParser parser = getParser();
+			final SellOpportunity slot = (SellOpportunity) target;
+			validatePriceExpression(ctx, slot, AnalyticsPackage.eINSTANCE.getSellOpportunity_PriceExpression(), slot.getPriceExpression(), parser, failures);
+		}
+		return Activator.PLUGIN_ID;
+	}
+
+	private void validatePriceExpression(final IValidationContext ctx, final EObject slot, final EStructuralFeature feature, final String priceExpression, final SeriesParser parser,
+			final List<IStatus> failures) {
+
+		if (priceExpression == null || priceExpression.isEmpty()) {
+			final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Price Expression is missing."));
+			dsd.addEObjectAndFeature(slot, feature);
+			failures.add(dsd);
+			return;
+		}
+
+		// Permit break even marker
+		if ("?".equals(priceExpression)) {
+			return;
+		}
+		
+		if (parser != null) {
+			ISeries parsed = null;
+			String hints = "";
+			try {
+				final IExpression<ISeries> expression = parser.parse(priceExpression);
+				parsed = expression.evaluate();
+
+			} catch (final Exception e) {
+				hints = e.getMessage();
+			}
+			if (parsed == null) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Unable to parse price expression. " + hints));
+				dsd.addEObjectAndFeature(slot, feature);
+				failures.add(dsd);
+			}
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private SeriesParser getParser() {
+		final Activator activator = Activator.getDefault();
+		if (activator == null) {
+			return null;
+		}
+		final IExtraValidationContext extraValidationContext = activator.getExtraValidationContext();
+		if (extraValidationContext != null) {
+			final MMXRootObject rootObject = extraValidationContext.getRootObject();
+
+			final SeriesParser indices = new SeriesParser();
+
+			final PricingModel pricingModel = rootObject.getSubModel(PricingModel.class);
+			for (final Index<Double> index : pricingModel.getCommodityIndices()) {
+				if (index instanceof DataIndex) {
+					// For this validation, we do not need real times or values
+					final int[] times = new int[1];
+					final Number[] nums = new Number[1];
+					indices.addSeriesData(index.getName(), times, nums);
+				} else if (index instanceof DerivedIndex) {
+					indices.addSeriesExpression(index.getName(), ((DerivedIndex) index).getExpression());
+				}
+			}
+			return indices;
+		}
+		return null;
+	}
+}
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ProvisionalCargoConstraint.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.analytics.validation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.management.timer.Timer;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
+import com.mmxlabs.models.lng.analytics.BuyOpportunity;
+import com.mmxlabs.models.lng.analytics.DestinationType;
+import com.mmxlabs.models.lng.analytics.ProvisionalCargo;
+import com.mmxlabs.models.lng.analytics.SellOpportunity;
+import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
+import com.mmxlabs.models.lng.fleet.VesselClass;
+import com.mmxlabs.models.lng.fleet.VesselClassRouteParameters;
+import com.mmxlabs.models.lng.port.Port;
+import com.mmxlabs.models.lng.port.PortModel;
+import com.mmxlabs.models.lng.port.Route;
+import com.mmxlabs.models.lng.port.RouteLine;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.scheduler.optimiser.Calculator;
+import com.mmxlabs.scheduler.optimiser.OptimiserUnitConvertor;
+
+public class ProvisionalCargoConstraint extends AbstractModelMultiConstraint {
+
+	@Override
+	protected String validate(final IValidationContext ctx, final List<IStatus> statuses) {
+		final EObject target = ctx.getTarget();
+		if (target instanceof ProvisionalCargo) {
+			final ProvisionalCargo shippingCostPlan = (ProvisionalCargo) target;
+
+//			if (shippingCostPlan.getBaseFuelPrice() < 0.0001) {
+//				final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Base fuel price must be non-zero"));
+//				deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_BaseFuelPrice());
+//				statuses.add(deco);
+//			}
+
+			if (shippingCostPlan.eContainer() != null) {
+//				DestinationType lastType = null;
+//				int idx = 0;
+//				for (final ShippingCostRow row : shippingCostPlan.getRows()) {
+//					final DestinationType type = row.getDestinationType();
+//					if (idx == 0) {
+//						if (type != DestinationType.START) {
+//							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("First row must be a START row"));
+//							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+//							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+//							statuses.add(deco);
+//						}
+//					} else if (idx == shippingCostPlan.getRows().size() - 1) {
+//						if (type != DestinationType.END) {
+//							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Last row must be a END row"));
+//							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+//							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+//							statuses.add(deco);
+//						}
+//					} else {
+//						if (type == DestinationType.START || type == DestinationType.END) {
+//							final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Waypoint rows cannot be START or END types"));
+//							deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+//							deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+//							statuses.add(deco);
+//						}
+//					}
+//					if (lastType == DestinationType.LOAD && type != DestinationType.DISCHARGE) {
+//						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
+//						deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+//						deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+//						statuses.add(deco);
+//					}
+//					if (lastType != DestinationType.LOAD && type == DestinationType.DISCHARGE) {
+//						final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("LOAD rows must be followed by DISCHARGE rows"));
+//						deco.addEObjectAndFeature(shippingCostPlan, AnalyticsPackage.eINSTANCE.getShippingCostPlan_Rows());
+//						deco.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getShippingCostRow_DestinationType());
+//						statuses.add(deco);
+//					}
+//
+//					lastType = type;
+//					idx++;
+//				}
+
+				validateSlotTravelTime(ctx, shippingCostPlan, statuses);
+			}
+		}
+		return Activator.PLUGIN_ID;
+	}
+
+	/**
+	 * Validate that the available time is enough to get from A to B, if it's not negative
+	 * 
+	 * @param ctx
+	 * @param cargo
+	 * @param availableTime
+	 * @return
+	 */
+	private void validateSlotTravelTime(final IValidationContext ctx, final ProvisionalCargo plan, final List<IStatus> statuses) {
+
+		final MMXRootObject scenario = Activator.getDefault().getExtraValidationContext().getRootObject();
+		if (scenario != null) {
+
+			final VesselClass vesselClass = plan.getVessel().getVesselClass();
+			if (plan.getVessel() == null || vesselClass == null) {
+				return;
+			}
+
+			final double maxSpeedKnots = vesselClass.getMaxSpeed();
+
+			@SuppressWarnings("unchecked")
+			Map<Pair<Port, Port>, Integer> minTimes = (Map<Pair<Port, Port>, Integer>) ctx.getCurrentConstraintData();
+			if (minTimes == null) {
+				minTimes = new HashMap<Pair<Port, Port>, Integer>();
+
+				for (final VesselClassRouteParameters parameters : vesselClass.getRouteParameters()) {
+					collectMinTimes(minTimes, parameters.getRoute(), parameters.getExtraTransitTime(), maxSpeedKnots);
+				}
+
+				for (final Route route : scenario.getSubModel(PortModel.class).getRoutes()) {
+					if (route.isCanal() == false) {
+						collectMinTimes(minTimes, route, 0, maxSpeedKnots);
+					}
+				}
+
+				ctx.putCurrentConstraintData(minTimes);
+			}
+
+//			ShippingCostRow lastRow = null;
+//			for (final ShippingCostRow row : plan.getRows()) {
+
+//				if (lastRow != null && lastRow.getPort() != null && row.getPort() != null) {
+
+	
+			BuyOpportunity lastRow = plan.getBuy();
+			SellOpportunity row = plan.getSell();
+			
+					if (row.getDate() != null && lastRow.getDate() != null) {
+
+
+						int visitDuration = 0;
+//						if (lastRow.getDestinationType() == DestinationType.LOAD ) {
+							visitDuration = lastRow.getPort().getLoadDuration();
+//						} else  if(lastRow.getDestinationType() == DestinationType.DISCHARGE) {
+//							visitDuration = lastRow.getPort().getDischargeDuration();
+//						} else  if(lastRow.getDestinationType() == DestinationType.OTHER) {
+//							visitDuration = 24;
+//						}
+						final int availableTime = (int) ((row.getDate().getTime() - lastRow.getDate().getTime()) / Timer.ONE_HOUR) - visitDuration;
+						
+						if (availableTime < 0) {
+							final String msg = String.format("Date is before the previous date.");
+							final IConstraintStatus status = (IConstraintStatus) ctx.createFailureStatus(msg);
+							final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(status);
+							dsd.addEObjectAndFeature(lastRow, AnalyticsPackage.eINSTANCE.getBuyOpportunity_Date());
+							dsd.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getSellOpportunity_Date());
+							statuses.add(dsd);
+						}
+
+						else if (!lastRow.getPort().equals(row.getPort())) {
+							final Pair<Port, Port> key = new Pair<Port, Port>(lastRow.getPort(), row.getPort());
+							final Integer time = minTimes.get(key);
+
+							if (time == null) {
+								// distance line is missing
+								final String msg = String.format("Impossible journey between %s and %s.", lastRow.getPort().getName(), row.getPort().getName());
+								final IConstraintStatus status = (IConstraintStatus) ctx.createFailureStatus(msg);
+								final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(status);
+								dsd.addEObjectAndFeature(lastRow, AnalyticsPackage.eINSTANCE.getBuyOpportunity_Date());
+								dsd.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getSellOpportunity_Date());
+								statuses.add(dsd);
+							} else {
+								if (time > availableTime) {
+
+									final String msg = String.format("Row does not have enough travel time - %s is required for the journey, but only %s is available.", formatHours(time),
+											formatHours(availableTime));
+
+									final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(msg));
+									dsd.addEObjectAndFeature(lastRow, AnalyticsPackage.eINSTANCE.getBuyOpportunity_Date());
+									dsd.addEObjectAndFeature(row, AnalyticsPackage.eINSTANCE.getSellOpportunity_Date());
+									statuses.add(dsd);
+								}
+							}
+						}
+					}
+				}
+//				lastRow = row;
+//			}
+//		}
+	}
+
+	private void collectMinTimes(final Map<Pair<Port, Port>, Integer> minTimes, final Route d, final int extraTime, final double maxSpeed) {
+		for (final RouteLine dl : d.getLines()) {
+			final Pair<Port, Port> p = new Pair<Port, Port>(dl.getFrom(), dl.getTo());
+			final int time = Calculator.getTimeFromSpeedDistance(OptimiserUnitConvertor.convertToInternalSpeed(maxSpeed), dl.getDistance()) + extraTime;
+			if (!minTimes.containsKey(p) || (minTimes.get(p) > time)) {
+				minTimes.put(p, time);
+			}
+		}
+	}
+
+	private String formatHours(final int hours) {
+		if (hours < 24) {
+			if (hours == 1) {
+				return hours + " hour";
+			} else {
+				return hours + " hours";
+			}
+		} else {
+			final int remainderHours = hours % 24;
+			final int days = hours / 24;
+			return days + " day" + (days > 1 ? "s" : "") + (remainderHours > 0 ? (", " + remainderHours + " hour" + (remainderHours > 1 ? "s" : "")) : "");
+		}
+	}
+
+}
