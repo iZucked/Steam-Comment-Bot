4900767fe1bf0ffe2f8187d939b091a4319159a7
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-11 16:51:35 +0000

    Update for model change

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.validation/src/com/mmxlabs/models/lng/commercial/validation/PriceExpressionContractConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.validation/src/com/mmxlabs/models/lng/commercial/validation/PriceExpressionContractConstraint.java
@@ -14,39 +14,30 @@ import com.mmxlabs.common.parser.series.SeriesParser;
 import com.mmxlabs.models.lng.commercial.CommercialPackage;
 import com.mmxlabs.models.lng.commercial.Contract;
 import com.mmxlabs.models.lng.commercial.ExpressionPriceParameters;
-import com.mmxlabs.models.lng.commercial.PriceExpressionContract;
 import com.mmxlabs.models.lng.commercial.validation.internal.Activator;
 import com.mmxlabs.models.lng.commercial.validation.util.ContractConstraints;
-import com.mmxlabs.models.lng.types.ALNGPriceCalculatorParameters;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 
-public class PriceExpressionContractConstraint extends
-		AbstractModelMultiConstraint {
+public class PriceExpressionContractConstraint extends AbstractModelMultiConstraint {
 
 	@Override
 	public String validate(final IValidationContext ctx, final List<IStatus> failures) {
-		final EObject target = ctx.getTarget();
+		EObject target = ctx.getTarget();
 
-		if (target instanceof PriceExpressionContract) {
+		ExpressionPriceParameters parameters = null;
+		if (target instanceof Contract) {
+			target = ((Contract) target).getPriceInfo();
+		}
+		if (target instanceof ExpressionPriceParameters) {
+			parameters = (ExpressionPriceParameters) target;
+		}
+		if (parameters != null) {
 			final SeriesParser parser = ContractConstraints.getParser();
-			final PriceExpressionContract contract = (PriceExpressionContract) target;
 
-			ContractConstraints.validatePriceExpression(ctx, contract, CommercialPackage.Literals.PRICE_EXPRESSION_CONTRACT__PRICE_EXPRESSION, contract.getPriceExpression(), parser,
-					failures);
+			ContractConstraints.validatePriceExpression(ctx, parameters, CommercialPackage.Literals.EXPRESSION_PRICE_PARAMETERS__PRICE_EXPRESSION, parameters.getPriceExpression(), parser, failures);
 
 		}
-		else if (target instanceof Contract) {
-			ALNGPriceCalculatorParameters priceInfo = ((Contract) target).getPriceInfo();
-			if (priceInfo instanceof ExpressionPriceParameters) {
-				final SeriesParser parser = ContractConstraints.getParser();
-				final ExpressionPriceParameters info = (ExpressionPriceParameters) priceInfo;
 
-				ContractConstraints.validatePriceExpression(ctx, priceInfo, CommercialPackage.Literals.EXPRESSION_PRICE_PARAMETERS__PRICE_EXPRESSION, info.getPriceExpression(), parser,
-						failures);
-				
-			}
-		}
-		
 		return Activator.PLUGIN_ID;
 	}
 }
