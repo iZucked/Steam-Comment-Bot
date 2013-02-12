51de5d8e2966bbfa012896f4471cccde317b6719
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-12 08:52:09 +0000

    Remove debugging output

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/AbstractMigrationUnit.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/AbstractMigrationUnit.java
@@ -79,17 +79,16 @@ public abstract class AbstractMigrationUnit implements IMigrationUnit {
 		for (final URI uri : baseURIs) {
 			final String nsURI = EcoreHelper.getPackageNS(uri);
 			final ModelsLNGSet_v1 type = MetamodelVersionsUtil.getTypeFromNS(nsURI);
-			if (type == null) {
-				int gg =0;
-			}
+			assert (type != null);
+
 			final XMIResource r = (XMIResource) resourceSet.createResource(uri);
 			r.setTrackingModification(true);
-			if (type != null) {
-				r.load(loadOptions);
-				EObject eObject = r.getContents().get(0);
-				assert eObject != null;
-				models.put(type, eObject);
-			}
+			r.load(loadOptions);
+
+			EObject eObject = r.getContents().get(0);
+			assert eObject != null;
+
+			models.put(type, eObject);
 		}
 
 		// Migrate!
