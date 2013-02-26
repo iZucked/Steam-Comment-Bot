5523b4ad0f693e41270874fb0ff41ee87b592e68
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-02-26 08:22:31 +0000

    Fix creation of new rows to have the OTHER type. There is still an issue where it is inserted into the wrong place

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/modelfactories/ShippingCostRowFactory.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.analytics.modelfactories;
+
+import org.eclipse.emf.ecore.EObject;
+
+import com.mmxlabs.models.lng.analytics.DestinationType;
+import com.mmxlabs.models.lng.analytics.ShippingCostRow;
+import com.mmxlabs.models.ui.modelfactories.DefaultModelFactory;
+
+public class ShippingCostRowFactory extends DefaultModelFactory {
+
+	@Override
+	protected void postprocess(final EObject top) {
+		super.postprocess(top);
+
+		if (top instanceof ShippingCostRow) {
+			final ShippingCostRow shippingCostRow = (ShippingCostRow) top;
+			shippingCostRow.setDestinationType(DestinationType.OTHER);
+		}
+	}
+}
