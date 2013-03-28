17e55315c8451a680a7477f4878649053aa00018
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-03-28 08:14:40 +0000

    Add a read-only wrapper version of a manipulator

---
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/manipulators/ReadOnlyManipulatorWrapper.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.ui.tabular.manipulators;
+
+import java.util.List;
+
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.swt.widgets.Composite;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.models.ui.tabular.ICellManipulator;
+import com.mmxlabs.models.ui.tabular.ICellRenderer;
+
+/**
+ * An {@link ICellManipulator} and {@link ICellRenderer} wrapper class to take an existing implemention of these interfaces and disable the editing capabilities.
+ * 
+ * @author Simon Goodall
+ * @since 3.0
+ * 
+ */
+public class ReadOnlyManipulatorWrapper<T extends ICellManipulator & ICellRenderer> implements ICellManipulator, ICellRenderer {
+
+	private final T wrapped;
+
+	public ReadOnlyManipulatorWrapper(final T wrapped) {
+
+		this.wrapped = wrapped;
+	}
+
+	@Override
+	public String render(final Object object) {
+		return wrapped.render(object);
+	}
+
+	@Override
+	public Comparable getComparable(final Object object) {
+		return wrapped.getComparable(object);
+	}
+
+	@Override
+	public Object getFilterValue(final Object object) {
+		return wrapped.getFilterValue(object);
+	}
+
+	@Override
+	public Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(final Object object) {
+		return wrapped.getExternalNotifiers(object);
+	}
+
+	@Override
+	public void setValue(final Object object, final Object value) {
+
+	}
+
+	@Override
+	public CellEditor getCellEditor(final Composite parent, final Object object) {
+		return null;
+	}
+
+	@Override
+	public Object getValue(final Object object) {
+		return wrapped.getValue(object);
+	}
+
+	@Override
+	public boolean canEdit(final Object object) {
+		return false;
+	}
+}
