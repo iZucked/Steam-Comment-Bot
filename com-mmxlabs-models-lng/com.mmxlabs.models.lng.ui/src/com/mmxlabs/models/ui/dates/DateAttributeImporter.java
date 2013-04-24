ce21ef48b9f58326d7f88f2296b199d4d4003566
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-04-24 09:49:45 -0100

    Bump minor version - now 3.1.0-SNAPSHOT

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/ui/dates/DateAttributeImporter.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/ui/dates/DateAttributeImporter.java
@@ -79,7 +79,7 @@ public class DateAttributeImporter extends DefaultAttributeImporter {
 	}
 
 	/**
-	 * @since 3.0
+	 * @since 3.1
 	 */
 	public String formatDate(final Date date, final TimeZone zone, boolean includeTime) {
 		final SimpleDateFormat c = (SimpleDateFormat) (includeTime ? consistentDateTime.clone() : consistentDate.clone());
