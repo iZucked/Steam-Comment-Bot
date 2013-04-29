ebac5c5618569dd125b0770b011f54c71ef72888
Simon McGregor <unknown@kiln.example.com>
2013-04-29 05:31:47 -0100

    Validation: element assignments must be attached to available vessels only

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.validation/src/com/mmxlabs/models/lng/assignment/validation/ElementAssignmentConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.validation/src/com/mmxlabs/models/lng/assignment/validation/ElementAssignmentConstraint.java
@@ -18,9 +18,16 @@ import com.mmxlabs.models.lng.assignment.AssignmentPackage;
 import com.mmxlabs.models.lng.assignment.ElementAssignment;
 import com.mmxlabs.models.lng.assignment.validation.internal.Activator;
 import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.fleet.ScenarioFleetModel;
+import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.fleet.VesselAvailability;
 import com.mmxlabs.models.lng.fleet.VesselEvent;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.types.AVesselSet;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
+
 
 public class ElementAssignmentConstraint extends AbstractModelConstraint {
 	/*
@@ -31,6 +38,8 @@ public class ElementAssignmentConstraint extends AbstractModelConstraint {
 	@Override
 	public IStatus validate(final IValidationContext ctx) {
 		final EObject object = ctx.getTarget();
+		IExtraValidationContext evc = Activator.getDefault().getExtraValidationContext();
+		
 
 		final List<IStatus> failures = new LinkedList<IStatus>();
 
@@ -51,11 +60,37 @@ public class ElementAssignmentConstraint extends AbstractModelConstraint {
 				return ctx.createSuccessStatus();
 			}
 
-			final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format(
-					"Element Assignment has unexpected assigned object of type %s. Expected Cargo or VesselEvent object type.", assignedObject.eClass().getName())));
-			failure.addEObjectAndFeature(elementAssignment, AssignmentPackage.eINSTANCE.getElementAssignment_AssignedObject());
-
-			failures.add(failure);
+			{
+				final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format(
+						"Element Assignment has unexpected assigned object of type %s. Expected Cargo or VesselEvent object type.", assignedObject.eClass().getName())));
+				failure.addEObjectAndFeature(elementAssignment, AssignmentPackage.eINSTANCE.getElementAssignment_AssignedObject());
+	
+				failures.add(failure);
+			}
+			
+			final AVesselSet<Vessel> vessel = elementAssignment.getAssignment();
+			
+			boolean found = false; 
+			
+			if (vessel instanceof Vessel) {
+				LNGScenarioModel root = (LNGScenarioModel) evc.getRootObject();
+				
+				ScenarioFleetModel fleet = root.getPortfolioModel().getScenarioFleetModel();
+				for (VesselAvailability availability: fleet.getVesselAvailabilities()) {
+					if (availability.getVessel().equals(vessel)) {
+						found = true;
+						break;
+					}
+				}
+				
+				if (!found) {
+					final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format(
+							"Element Assignment assigns an element for an unavailable vessel %s.", vessel.getName())));
+					failure.addEObjectAndFeature(elementAssignment, AssignmentPackage.Literals.ELEMENT_ASSIGNMENT__ASSIGNMENT);
+		
+					failures.add(failure);					
+				}
+			}
 		}
 		if (failures.isEmpty()) {
 			return ctx.createSuccessStatus();
