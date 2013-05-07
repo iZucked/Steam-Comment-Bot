bc49969e2516a771bceefe8fdffb542121325d7d
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-05-07 13:59:35 -0100

    @since tags

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/actions/SimpleImportAction.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.ui/src/com/mmxlabs/models/lng/ui/actions/SimpleImportAction.java
@@ -31,7 +31,7 @@ public class SimpleImportAction extends ImportAction {
 	private static final Logger log = LoggerFactory.getLogger(SimpleImportAction.class);
 	
 	/**
-	 * @since 3.0
+	 * @since 3.1
 	 */
 	public interface FieldInfoProvider {
 		EObject getContainer();
@@ -41,7 +41,7 @@ public class SimpleImportAction extends ImportAction {
 	private final FieldInfoProvider fieldInfoProvider;
 	
 	/**
-	 * @since 3.0
+	 * @since 3.1
 	 */
 	public SimpleImportAction(final ImportHooksProvider iph, final FieldInfoProvider fip) {
 		super(iph);
