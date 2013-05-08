f3e64b9f35694f345199a5224d234a5e1440456c
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-08 12:37:56 -0100

    Disable classes

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/impl/ComparingCargoAllocator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/impl/ComparingCargoAllocator.java
@@ -18,79 +18,79 @@ import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlan;
  * @author hinton
  * 
  */
-public class ComparingCargoAllocator implements IVolumeAllocator {
-	private final FastCargoAllocator fastAllocator = new FastCargoAllocator();
-
-	// private final SimplexCargoAllocator simplexAllocator = new SimplexCargoAllocator();
-
-	@Override
-	public void setTotalVolumeLimitProvider(final ITotalVolumeLimitProvider tvlp) {
-		fastAllocator.setTotalVolumeLimitProvider(tvlp);
-		// simplexAllocator.setTotalVolumeLimitProvider(tvlp);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator #init()
-	 */
-	@Override
-	public void init() {
-		fastAllocator.init();
-		// simplexAllocator.init();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator #reset()
-	 */
-	@Override
-	public void reset() {
-		fastAllocator.reset();
-		// simplexAllocator.reset();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#allocate(com.mmxlabs.scheduler.optimiser.fitness.ScheduledSequences)
-	 */
-	@Override
-	public Map<VoyagePlan, IAllocationAnnotation> allocate(final ScheduledSequences sequences) {
-		// TODO Auto-generated method stub
-		return fastAllocator.allocate(sequences);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#dispose()
-	 */
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#setVesselProvider(com.mmxlabs.scheduler.optimiser.providers.IVesselProvider)
-	 */
-	@Override
-	public void setVesselProvider(final IVesselProvider dataComponentProvider) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * @since 2.0
-	 */
-	@Override
-	public IAllocationAnnotation allocate(IVessel vessel, VoyagePlan plan, int[] arrivalTimes) {
-		// TODO Auto-generated method stub
-		return fastAllocator.allocate(vessel, plan, arrivalTimes);
-	}
+public abstract class ComparingCargoAllocator implements IVolumeAllocator {
+//	private final FastCargoAllocator fastAllocator = new FastCargoAllocator();
+//
+//	// private final SimplexCargoAllocator simplexAllocator = new SimplexCargoAllocator();
+//
+//	@Override
+//	public void setTotalVolumeLimitProvider(final ITotalVolumeLimitProvider tvlp) {
+//		fastAllocator.setTotalVolumeLimitProvider(tvlp);
+//		// simplexAllocator.setTotalVolumeLimitProvider(tvlp);
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator #init()
+//	 */
+//	@Override
+//	public void init() {
+//		fastAllocator.init();
+//		// simplexAllocator.init();
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator #reset()
+//	 */
+//	@Override
+//	public void reset() {
+//		fastAllocator.reset();
+//		// simplexAllocator.reset();
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#allocate(com.mmxlabs.scheduler.optimiser.fitness.ScheduledSequences)
+//	 */
+//	@Override
+//	public Map<VoyagePlan, IAllocationAnnotation> allocate(final ScheduledSequences sequences) {
+//		// TODO Auto-generated method stub
+//		return fastAllocator.allocate(sequences);
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#dispose()
+//	 */
+//	@Override
+//	public void dispose() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	/*
+//	 * (non-Javadoc)
+//	 * 
+//	 * @see com.mmxlabs.scheduler.optimiser.fitness.components.allocation.ICargoAllocator#setVesselProvider(com.mmxlabs.scheduler.optimiser.providers.IVesselProvider)
+//	 */
+//	@Override
+//	public void setVesselProvider(final IVesselProvider dataComponentProvider) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	/**
+//	 * @since 2.0
+//	 */
+//	@Override
+//	public IAllocationAnnotation allocate(IVessel vessel, VoyagePlan plan, int[] arrivalTimes) {
+//		// TODO Auto-generated method stub
+//		return fastAllocator.allocate(vessel, plan, arrivalTimes);
+//	}
 
 }
