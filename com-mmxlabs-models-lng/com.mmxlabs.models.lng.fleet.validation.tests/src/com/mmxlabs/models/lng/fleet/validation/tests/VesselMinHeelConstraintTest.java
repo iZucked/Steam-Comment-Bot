08b47440078270f39fde0c034dd0334dbb7d5594
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-10 12:17:31 -0100

    Check min heel vs vessel & vessel class capacities

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation.tests/src/com/mmxlabs/models/lng/fleet/validation/tests/VesselMinHeelConstraintTest.java
@@ -0,0 +1,105 @@
+package com.mmxlabs.models.lng.fleet.validation.tests;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import com.mmxlabs.models.lng.fleet.FleetFactory;
+import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.fleet.VesselClass;
+import com.mmxlabs.models.lng.fleet.validation.VesselMinHeelConstraint;
+
+public class VesselMinHeelConstraintTest {
+
+	@Test
+	public void testNoOverride_OK() {
+
+		final Vessel target = FleetFactory.eINSTANCE.createVessel();
+		final VesselClass vesselClass = FleetFactory.eINSTANCE.createVesselClass();
+		target.setVesselClass(vesselClass);
+
+		vesselClass.setMinHeel(500);
+		vesselClass.setCapacity(1000);
+		vesselClass.setFillCapacity(1.0);
+
+		checkConstraint(target, true);
+	}
+
+	@Test
+	public void testNOOverride_VC_BAD_() {
+
+		final Vessel target = FleetFactory.eINSTANCE.createVessel();
+		final VesselClass vesselClass = FleetFactory.eINSTANCE.createVesselClass();
+		target.setVesselClass(vesselClass);
+
+		vesselClass.setMinHeel(500);
+		vesselClass.setCapacity(100);
+		vesselClass.setFillCapacity(1.0);
+
+		checkConstraint(target, true);
+	}
+
+	@Test
+	public void testMinHeel_CapacityTooLow() {
+
+		final Vessel target = FleetFactory.eINSTANCE.createVessel();
+		final VesselClass vesselClass = FleetFactory.eINSTANCE.createVesselClass();
+		target.setVesselClass(vesselClass);
+
+		vesselClass.setMinHeel(500);
+		vesselClass.setCapacity(1000);
+		vesselClass.setFillCapacity(1.0);
+
+		target.setCapacity(100);
+
+		checkConstraint(target, false);
+	}
+
+	@Test
+	public void testMinHeel_FillPercentTooLow() {
+
+		final Vessel target = FleetFactory.eINSTANCE.createVessel();
+		final VesselClass vesselClass = FleetFactory.eINSTANCE.createVesselClass();
+		target.setVesselClass(vesselClass);
+
+		vesselClass.setMinHeel(500);
+		vesselClass.setCapacity(1000);
+		vesselClass.setFillCapacity(1.0);
+
+		target.setFillCapacity(0.1);
+
+		checkConstraint(target, false);
+	}
+
+	private void checkConstraint(final EObject target, boolean expectSuccess) {
+		final IConstraintStatus successStatus = mock(IConstraintStatus.class);
+		when(successStatus.isOK()).thenReturn(Boolean.TRUE);
+
+		final IConstraintStatus failureStatus = mock(IConstraintStatus.class);
+		when(failureStatus.getSeverity()).thenReturn(IStatus.ERROR);
+
+		final IValidationContext ctx = Mockito.mock(IValidationContext.class);
+		Mockito.when(ctx.getTarget()).thenReturn(target);
+		Mockito.when(ctx.createSuccessStatus()).thenReturn(successStatus);
+		Mockito.when(ctx.createFailureStatus(Matchers.anyString())).thenReturn(failureStatus);
+
+		final VesselMinHeelConstraint constraint = new VesselMinHeelConstraint();
+		final IStatus status = constraint.validate(ctx);
+
+		if (expectSuccess) {
+			Assert.assertTrue("Sucess expected", status.isOK());
+		} else {
+			Assert.assertFalse("Failure expected", status.isOK());
+
+		}
+	}
+
+}
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/VesselMinHeelConstraint.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.fleet.validation;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.fleet.VesselClass;
+import com.mmxlabs.models.lng.fleet.validation.internal.Activator;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+/**
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class VesselMinHeelConstraint extends AbstractModelMultiConstraint {
+
+	@Override
+	protected String validate(final IValidationContext ctx, final List<IStatus> statuses) {
+		final EObject target = ctx.getTarget();
+		if (target instanceof Vessel) {
+			final Vessel vessel = (Vessel) target;
+
+			if (vessel.isSetCapacity() || vessel.isSetFillCapacity()) {
+
+				final VesselClass vesselClass = vessel.getVesselClass();
+
+				double effectiveCapacity = vessel.getVesselOrVesselClassCapacity() * vessel.getVesselOrVesselClassFillCapacity();
+				if (vesselClass.getMinHeel() > effectiveCapacity) {
+					final String message = String.format("Minimum heel should be less than fill capacity");
+					final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+					if (vessel.isSetCapacity()) {
+						dcsd.addEObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_Capacity());
+					}
+					if (vessel.isSetFillCapacity()) {
+						dcsd.addEObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_FillCapacity());
+					}
+					statuses.add(dcsd);
+				}
+			}
+		}
+
+		return Activator.PLUGIN_ID;
+	}
+}
