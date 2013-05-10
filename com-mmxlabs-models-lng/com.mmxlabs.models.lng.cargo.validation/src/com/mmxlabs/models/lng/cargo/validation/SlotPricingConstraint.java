e305e7a3aa725fd95ed684b14c1c27707e4f4d03
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-10 13:04:05 -0100

    Add validation to check that at least a price expression or contract has been set.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/SlotPricingConstraint.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.cargo.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+public class SlotPricingConstraint extends AbstractModelConstraint {
+
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+		if (target instanceof Slot) {
+			final Slot slot = (Slot) target;
+
+			if (!slot.isSetPriceExpression() && !slot.isSetContract()) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("A contract or price expression must be set"));
+				dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_Contract());
+				dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PriceExpression());
+				return dsd;
+			}
+
+		}
+		return ctx.createSuccessStatus();
+	}
+}
