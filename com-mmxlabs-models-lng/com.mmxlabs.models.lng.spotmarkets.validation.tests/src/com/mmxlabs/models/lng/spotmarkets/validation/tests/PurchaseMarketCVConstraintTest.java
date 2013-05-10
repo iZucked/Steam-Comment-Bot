1316860f3b64be08e738be1583895b80a97512c6
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-10 11:27:04 -0100

    Check for sensible CV on spot purchase markets

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.validation.tests/src/com/mmxlabs/models/lng/spotmarkets/validation/tests/PurchaseMarketCVConstraintTest.java
@@ -0,0 +1,98 @@
+package com.mmxlabs.models.lng.spotmarkets.validation.tests;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import com.mmxlabs.models.lng.spotmarkets.DESPurchaseMarket;
+import com.mmxlabs.models.lng.spotmarkets.FOBPurchasesMarket;
+import com.mmxlabs.models.lng.spotmarkets.SpotMarketsFactory;
+import com.mmxlabs.models.lng.spotmarkets.validation.PurchaseMarketCVConstraint;
+
+public class PurchaseMarketCVConstraintTest {
+
+	@Test
+	public void testDESPurchaseMarket_OKCV() {
+
+		final DESPurchaseMarket target = SpotMarketsFactory.eINSTANCE.createDESPurchaseMarket();
+		target.setCv(20.0);
+
+		checkConstraint(target, true);
+	}
+
+	@Test
+	public void testDESPurchaseMarket_CV_TooLow() {
+
+		final DESPurchaseMarket target = SpotMarketsFactory.eINSTANCE.createDESPurchaseMarket();
+		target.setCv(0.01);
+
+		checkConstraint(target, false);
+	}
+
+	@Test
+	public void testDESPurchaseMarket_CV_TooHigh() {
+
+		final DESPurchaseMarket target = SpotMarketsFactory.eINSTANCE.createDESPurchaseMarket();
+		target.setCv(41);
+
+		checkConstraint(target, false);
+	}
+
+	@Test
+	public void testFOBPurchaseMarket_OKCV() {
+
+		final FOBPurchasesMarket target = SpotMarketsFactory.eINSTANCE.createFOBPurchasesMarket();
+		target.setCv(20.0);
+
+		checkConstraint(target, true);
+	}
+
+	@Test
+	public void testFOBPurchaseMarket_CV_TooLow() {
+
+		final FOBPurchasesMarket target = SpotMarketsFactory.eINSTANCE.createFOBPurchasesMarket();
+		target.setCv(0.01);
+
+		checkConstraint(target, false);
+	}
+
+	@Test
+	public void testFOBPurchaseMarket_CV_TooHigh() {
+
+		final FOBPurchasesMarket target = SpotMarketsFactory.eINSTANCE.createFOBPurchasesMarket();
+		target.setCv(41);
+
+		checkConstraint(target, false);
+	}
+
+	private void checkConstraint(final EObject target, boolean expectSuccess) {
+		final IConstraintStatus successStatus = mock(IConstraintStatus.class);
+		when(successStatus.isOK()).thenReturn(Boolean.TRUE);
+
+		final IConstraintStatus failureStatus = mock(IConstraintStatus.class);
+		when(failureStatus.getSeverity()).thenReturn(IStatus.ERROR);
+
+		final IValidationContext ctx = Mockito.mock(IValidationContext.class);
+		Mockito.when(ctx.getTarget()).thenReturn(target);
+		Mockito.when(ctx.createSuccessStatus()).thenReturn(successStatus);
+		Mockito.when(ctx.createFailureStatus(Matchers.anyString())).thenReturn(failureStatus);
+
+		final PurchaseMarketCVConstraint constraint = new PurchaseMarketCVConstraint();
+		final IStatus status = constraint.validate(ctx);
+
+		if (expectSuccess) {
+			Assert.assertTrue("Sucess expected", status.isOK());
+		} else {
+			Assert.assertFalse("Failure expected", status.isOK());
+
+		}
+	}
+}
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.validation/src/com/mmxlabs/models/lng/spotmarkets/validation/PurchaseMarketCVConstraint.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.spotmarkets.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.spotmarkets.DESPurchaseMarket;
+import com.mmxlabs.models.lng.spotmarkets.FOBPurchasesMarket;
+import com.mmxlabs.models.lng.spotmarkets.SpotMarketsPackage;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+public class PurchaseMarketCVConstraint extends AbstractModelConstraint {
+
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+		if (target instanceof DESPurchaseMarket) {
+			final DESPurchaseMarket market = (DESPurchaseMarket) target;
+
+			if (market.getCv() < 1.0) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("CV value it too low."));
+				dsd.addEObjectAndFeature(market, SpotMarketsPackage.eINSTANCE.getDESPurchaseMarket_Cv());
+				return dsd;
+			} else if (market.getCv() > 40.0) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("CV value is too high."));
+				dsd.addEObjectAndFeature(market, SpotMarketsPackage.eINSTANCE.getDESPurchaseMarket_Cv());
+				return dsd;
+			}
+
+		} else if (target instanceof FOBPurchasesMarket) {
+			final FOBPurchasesMarket market = (FOBPurchasesMarket) target;
+
+			if (market.getCv() < 1.0) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("CV value it too low."));
+				dsd.addEObjectAndFeature(market, SpotMarketsPackage.eINSTANCE.getFOBPurchasesMarket_Cv());
+				return dsd;
+			} else if (market.getCv() > 40.0) {
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("CV value is too high."));
+				dsd.addEObjectAndFeature(market, SpotMarketsPackage.eINSTANCE.getFOBPurchasesMarket_Cv());
+				return dsd;
+			}
+
+		}
+		return ctx.createSuccessStatus();
+	}
+}
