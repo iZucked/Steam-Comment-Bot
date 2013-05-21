0e4a138d054ba38bed4a9c2b51bd5c1d24e4684e
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-21 14:28:26 -0100

    Make cargo slots a "child" in the item provider. update Cargo Model name in plugin.properties

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.edit/src/com/mmxlabs/models/lng/cargo/provider/CargoItemProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.edit/src/com/mmxlabs/models/lng/cargo/provider/CargoItemProvider.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -159,6 +160,36 @@ public class CargoItemProvider
 	}
 
 	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(CargoPackage.Literals.CARGO__SLOTS);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
 	 * This returns Cargo.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -197,8 +228,10 @@ public class CargoItemProvider
 		switch (notification.getFeatureID(Cargo.class)) {
 			case CargoPackage.CARGO__NAME:
 			case CargoPackage.CARGO__ALLOW_REWIRING:
-			case CargoPackage.CARGO__SLOTS:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+			case CargoPackage.CARGO__SLOTS:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
 				return;
 		}
 		super.notifyChanged(notification);
