656b9f6e410bc685ec04694c11803095456b2963
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-05-23 12:45:12 -0100

    Backed out merge changeset: a8d823bdadc4
    Backed out merge revision to its first parent (7aaca4aff3cd)

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.util/src/com/mmxlabs/scenario/service/util/AbstractScenarioService.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.util/src/com/mmxlabs/scenario/service/util/AbstractScenarioService.java
@@ -321,10 +321,6 @@ public abstract class AbstractScenarioService extends AbstractScenarioServiceLis
 			dup.getMetadata().setCreated(cpy.getMetadata().getCreated());
 			dup.getMetadata().setLastModified(new Date());
 			dup.setName(cpy.getName());
-			
-			// Copy version context information
-			dup.setVersionContext(cpy.getVersionContext());
-			dup.setScenarioVersion(cpy.getScenarioVersion());
 
 			// Clean up
 			if (unloadScenario) {
