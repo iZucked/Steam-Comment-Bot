5bba8f3d20717395c1c6c3c31da451afc547948c
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-06-04 14:53:21 -0100

    Clear existing discharge slots when creating a new discharge.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoEditingCommands.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CargoEditingCommands.java
@@ -55,7 +55,7 @@ public class CargoEditingCommands {
 	/**
 	 * @since 4.0
 	 */
-	public CargoEditingCommands(final EditingDomain editingDomain, LNGScenarioModel rootObject, LNGPortfolioModel portfolioModel) {
+	public CargoEditingCommands(final EditingDomain editingDomain, final LNGScenarioModel rootObject, final LNGPortfolioModel portfolioModel) {
 		this.editingDomain = editingDomain;
 		this.rootObject = rootObject;
 		this.portfolioModel = portfolioModel;
@@ -246,9 +246,9 @@ public class CargoEditingCommands {
 		if (cargo != null) {
 
 			// Clear existing discharge slots
-			for (Slot slot : cargo.getSlots()) {
+			for (final Slot slot : cargo.getSlots()) {
 				if (slot instanceof DischargeSlot) {
-					DischargeSlot dischargeSlot2 = (DischargeSlot) slot;
+					final DischargeSlot dischargeSlot2 = (DischargeSlot) slot;
 					setCommands.add(SetCommand.create(editingDomain, dischargeSlot2, CargoPackage.eINSTANCE.getSlot_Cargo(), SetCommand.UNSET_VALUE));
 				}
 			}
