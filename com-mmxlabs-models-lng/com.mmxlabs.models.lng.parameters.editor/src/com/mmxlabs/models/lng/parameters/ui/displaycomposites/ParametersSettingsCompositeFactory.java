cb70e4d484f5c27cd57070fc907e01610bcf915b
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-06-06 09:08:53 -0100

    Fix settings display composite factory

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.parameters.editor/src/com/mmxlabs/models/lng/parameters/ui/displaycomposites/ParametersSettingsCompositeFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.parameters.editor/src/com/mmxlabs/models/lng/parameters/ui/displaycomposites/ParametersSettingsCompositeFactory.java
@@ -4,36 +4,19 @@
  */
 package com.mmxlabs.models.lng.parameters.ui.displaycomposites;
 
-import java.util.List;
-
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
-import com.mmxlabs.models.lng.parameters.ParametersPackage;
-import com.mmxlabs.models.mmxcore.MMXRootObject;
-import com.mmxlabs.models.ui.Activator;
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 import com.mmxlabs.models.ui.editors.IDisplayComposite;
-import com.mmxlabs.models.ui.editors.IDisplayCompositeFactory;
+import com.mmxlabs.models.ui.impl.DefaultDisplayCompositeFactory;
 
-public class ParametersSettingsCompositeFactory implements IDisplayCompositeFactory {
-
-	final IDisplayCompositeFactory delegate = Activator.getDefault().getDisplayCompositeFactoryRegistry().getDisplayCompositeFactory(ParametersPackage.eINSTANCE.getOptimiserSettings());
+public class ParametersSettingsCompositeFactory extends DefaultDisplayCompositeFactory {
 
 	@Override
 	public IDisplayComposite createToplevelComposite(final Composite composite, final EClass eClass, final IScenarioEditingLocation location) {
 		return new OptimiserSettingsToplevelComposite(composite, SWT.NONE, location);
 	}
 
-	@Override
-	public IDisplayComposite createSublevelComposite(final Composite composite, final EClass eClass, final IScenarioEditingLocation location) {
-		return delegate.createSublevelComposite(composite, eClass, location);
-	}
-
-	@Override
-	public List<EObject> getExternalEditingRange(final MMXRootObject root, final EObject value) {
-		return delegate.getExternalEditingRange(root, value);
-	}
 }
