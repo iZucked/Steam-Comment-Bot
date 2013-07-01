35cd29bc45625d73ff15eeb1dfbf7fce649b812d
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-07-01 17:11:47 -0100

    Constrain vessel event duration & windows to sensible ranges

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/EventDurationConstraint.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.fleet.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.fleet.BaseFuel;
+import com.mmxlabs.models.lng.fleet.CharterOutEvent;
+import com.mmxlabs.models.lng.fleet.DryDockEvent;
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.MaintenanceEvent;
+import com.mmxlabs.models.lng.fleet.VesselEvent;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+/**
+ * Checks that {@link BaseFuel} values are none-zero
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class EventDurationConstraint extends AbstractModelConstraint {
+	private int maxDryDockDurationInDays = 30 * 6;
+	private int maxMaintenanceDurationInDays = 30;
+	private int maxCharterOutDurationInDays = 365;
+	
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+
+		if (target instanceof VesselEvent) {
+			final VesselEvent event = (VesselEvent) target;
+			
+			int max;
+			String type;
+			if (target instanceof MaintenanceEvent) {
+				max = maxMaintenanceDurationInDays;
+				type = "Maintenance";
+			}
+			else if (target instanceof DryDockEvent) {
+				max = maxDryDockDurationInDays;
+				type = "Dry dock";
+			}
+			else if (target instanceof CharterOutEvent) {
+				max = maxCharterOutDurationInDays;
+				type = "Charter out";
+			}
+			else {
+				return (IConstraintStatus) ctx.createFailureStatus("No known maximum duration for events of type '" + event.getClass().getName() + "'.");
+			}
+			
+			final int duration = event.getDurationInDays();
+			if (duration > max) {
+				String message = String.format("%s event duration %d days (> %d days maximum)", type, duration, max);
+				final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+				dcsd.addEObjectAndFeature(event, FleetPackage.eINSTANCE.getVesselEvent_DurationInDays());
+				return dcsd;
+			}
+
+		}
+
+		return ctx.createSuccessStatus();
+	}
+}
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/EventWindowConstraint.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.fleet.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+import com.mmxlabs.models.lng.fleet.BaseFuel;
+import com.mmxlabs.models.lng.fleet.CharterOutEvent;
+import com.mmxlabs.models.lng.fleet.DryDockEvent;
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.MaintenanceEvent;
+import com.mmxlabs.models.lng.fleet.VesselEvent;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+
+/**
+ * Checks that {@link BaseFuel} values are none-zero
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class EventWindowConstraint extends AbstractModelConstraint {
+	private int maxWindowSizeInDays = 6 * 30;
+	
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+
+		if (target instanceof VesselEvent) {
+			final VesselEvent event = (VesselEvent) target;
+			
+			final long windowSizeInMs = event.getStartBy().getTime() - event.getStartAfter().getTime();
+			final long windowSizeInDays = windowSizeInMs / (1000 * 24 * 3600);
+					
+			if (windowSizeInDays > maxWindowSizeInDays) {
+				String message = String.format("Event duration %d days (> %d days maximum)", windowSizeInDays, maxWindowSizeInDays);
+				final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+				dcsd.addEObjectAndFeature(event, FleetPackage.eINSTANCE.getVesselEvent_StartAfter());
+				dcsd.addEObjectAndFeature(event, FleetPackage.eINSTANCE.getVesselEvent_StartBy());
+				return dcsd;
+			}
+
+		}
+
+		return ctx.createSuccessStatus();
+	}
+}
