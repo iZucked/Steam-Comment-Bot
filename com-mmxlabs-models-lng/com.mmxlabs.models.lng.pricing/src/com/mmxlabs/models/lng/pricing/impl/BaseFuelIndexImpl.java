513029bd7d00ac565559822088f1e6ebc4ad562f
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-07-08 07:30:50 -0100

    Files missed from previous commit

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing/src/com/mmxlabs/models/lng/pricing/impl/BaseFuelIndexImpl.java
@@ -0,0 +1,60 @@
+/**
+ */
+package com.mmxlabs.models.lng.pricing.impl;
+
+import com.mmxlabs.models.lng.pricing.BaseFuelIndex;
+import com.mmxlabs.models.lng.pricing.Index;
+import com.mmxlabs.models.lng.pricing.PricingPackage;
+
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
+import com.mmxlabs.models.mmxcore.NamedObject;
+
+import com.mmxlabs.models.mmxcore.impl.UUIDObjectImpl;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Base Fuel Index</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class BaseFuelIndexImpl extends NamedIndexContainerImpl<Double> implements BaseFuelIndex {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected BaseFuelIndexImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return PricingPackage.Literals.BASE_FUEL_INDEX;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public double getPrice() {
+		Index<Double> data = getData();
+		return data.getValueForMonth(data.getDates().get(0));
+	}
+
+} //BaseFuelIndexImpl
