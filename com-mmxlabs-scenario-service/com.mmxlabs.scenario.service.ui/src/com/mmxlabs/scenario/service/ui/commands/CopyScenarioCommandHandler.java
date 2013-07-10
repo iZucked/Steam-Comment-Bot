2d66ff570aa99661a203c64a38ed98c8b9a4d3ea
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-07-10 14:27:12 -0100

    Add busy/ icon to copy handler

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/CopyScenarioCommandHandler.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/CopyScenarioCommandHandler.java
@@ -14,6 +14,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.edit.ui.dnd.LocalTransfer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.FileTransfer;
 import org.eclipse.swt.dnd.Transfer;
@@ -31,32 +32,36 @@ public class CopyScenarioCommandHandler extends AbstractHandler {
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		final IWorkbenchPage activePage = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+		BusyIndicator.showWhile(HandlerUtil.getActiveShellChecked(event).getDisplay(), new Runnable() {
 
-		final ISelection selection = activePage.getSelection();
-		if (selection instanceof IStructuredSelection) {
-			final Clipboard clipboard = new Clipboard(HandlerUtil.getActiveWorkbenchWindow(event).getShell().getDisplay());
-			final IStructuredSelection strucSelection = (IStructuredSelection) selection;
+			@Override
+			public void run() {
+				final ISelection selection = activePage.getSelection();
+				if (selection instanceof IStructuredSelection) {
+					final Clipboard clipboard = new Clipboard(HandlerUtil.getActiveWorkbenchWindow(event).getShell().getDisplay());
+					final IStructuredSelection strucSelection = (IStructuredSelection) selection;
 
-			final ArrayList<String> tempFiles = new ArrayList<String>();
-			final ArrayList<ScenarioInstance> instances = new ArrayList<ScenarioInstance>();
-			for (final Iterator<?> iterator = strucSelection.iterator(); iterator.hasNext();) {
-				final Object element = iterator.next();
-				if (element instanceof ScenarioInstance) {
-					final ScenarioInstance instance = (ScenarioInstance) element;
-					instances.add(instance);
-					try {
-						tempFiles.add(ScenarioStorageUtil.storeToTemporaryFile(instance));
-					} catch (IOException e) {
+					final ArrayList<String> tempFiles = new ArrayList<String>();
+					final ArrayList<ScenarioInstance> instances = new ArrayList<ScenarioInstance>();
+					for (final Iterator<?> iterator = strucSelection.iterator(); iterator.hasNext();) {
+						final Object element = iterator.next();
+						if (element instanceof ScenarioInstance) {
+							final ScenarioInstance instance = (ScenarioInstance) element;
+							instances.add(instance);
+							try {
+								tempFiles.add(ScenarioStorageUtil.storeToTemporaryFile(instance));
+							} catch (final IOException e) {
+							}
+						}
 					}
+					final String[] tempFilesArray = tempFiles.toArray(new String[tempFiles.size()]);
+					if (tempFilesArray.length > 0) {
+						clipboard.setContents(new Object[] { tempFilesArray, instances }, new Transfer[] { FileTransfer.getInstance(), LocalTransfer.getInstance() });
+					}
+					clipboard.dispose();
 				}
 			}
-			final String[] tempFilesArray = tempFiles.toArray(new String[tempFiles.size()]);
-			if (tempFilesArray.length > 0) {
-				clipboard.setContents(new Object[] { tempFilesArray, instances }, new Transfer[] { FileTransfer.getInstance(), LocalTransfer.getInstance() });
-			}
-			clipboard.dispose();
-		}
-		
+		});
 		return null;
 	}
 }
