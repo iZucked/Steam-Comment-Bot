2ad4f0dcfc7f8fd783ea8b2007a96df577c1fd0a
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-07-11 11:59:49 -0100

    Changes to model factories to permit configurable override of sub-object classes

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/modelfactories/SalesContractModelFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/modelfactories/SalesContractModelFactory.java
@@ -10,8 +10,8 @@ package com.mmxlabs.models.lng.commercial.ui.modelfactories;
  */
 public class SalesContractModelFactory extends ContractModelFactory {
 	@Override
-	public void initFromExtension(final String ID, final String label, final String prototype) {
-		super.initFromExtension(ID, label, "com.mmxlabs.models.lng.commercial.SalesContract");
+	public void initFromExtension(final String ID, final String label, final String prototype, String replacementReference, String replacementClass) {
+		super.initFromExtension(ID, label, "com.mmxlabs.models.lng.commercial.SalesContract", replacementReference, replacementClass);
 		this.priceInfoClassName = prototype;
 	}
 	
