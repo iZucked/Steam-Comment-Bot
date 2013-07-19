ef531fc2f0214d12a85e9615cc4b193047efcc3e
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2013-07-19 08:23:47 -0100

    Non-zero range constraint on some vessel class fuel consumptions

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/VesselAttributesRangeConstraint.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.fleet.validation;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com.mmxlabs.models.lng.fleet.FleetPackage;
+import com.mmxlabs.models.lng.fleet.VesselStateAttributes;
+import com.mmxlabs.models.lng.fleet.validation.internal.Activator;
+import com.mmxlabs.models.ui.validation.AbstractFeatureRangeConstraint;
+
+/**
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class VesselAttributesRangeConstraint extends AbstractFeatureRangeConstraint {
+	
+	@Override
+	protected String getPluginId() {
+		return Activator.PLUGIN_ID;
+	}
+
+
+
+	@Override
+	protected boolean shouldValidateFeature(EObject object,
+			EStructuralFeature feature) {
+		return (object instanceof VesselStateAttributes);
+	}
+
+
+
+	@Override
+	protected void createConstraints() {
+		setRange(FleetPackage.Literals.VESSEL_STATE_ATTRIBUTES__NBO_RATE, 0.0, null, "NBO rate");
+		setRange(FleetPackage.Literals.VESSEL_STATE_ATTRIBUTES__IDLE_BASE_RATE, 0.0, null, "Idle base rate");
+	}
+}
