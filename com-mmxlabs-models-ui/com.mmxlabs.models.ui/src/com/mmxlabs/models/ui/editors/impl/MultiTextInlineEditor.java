eb8b153e5ea9a6d5db09f5d747d72ef3a8ccb9bb
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-07-23 11:39:29 -0100

    Fix up impl - this works on a single String value, not a List

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/impl/MultiTextInlineEditor.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/impl/MultiTextInlineEditor.java
@@ -1,8 +1,5 @@
 package com.mmxlabs.models.ui.editors.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
@@ -10,50 +7,23 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 
 /**
+ * Sub class of {@link TextInlineEditor} to display a *single* string across multiple lines. This class also enforces a minimum control height.
+ * 
  * @since 6.0
  */
 public class MultiTextInlineEditor extends TextInlineEditor {
-	final String newLine;
+	private final String newLine;
 
-	public MultiTextInlineEditor(EStructuralFeature feature) {
+	private final int minHeight;
+
+	public MultiTextInlineEditor(final EStructuralFeature feature) {
+		this(feature, 50);
+	}
+
+	public MultiTextInlineEditor(final EStructuralFeature feature, final int minHeight) {
 		super(feature, SWT.MULTI | SWT.V_SCROLL | SWT.BORDER);
 		newLine = System.getProperty("line.separator");
-	}
-	
-	@Override
-	protected synchronized void doSetValue(final Object value, final boolean forceCommandExecution) {
-		if (value == null)
-			return;
-		final String valueString = "" + value;
-		final String[] values = valueString.split(newLine);
-		final List<String> valueList = new ArrayList<String>(values.length);
-		for (final String s : values) {
-			final String s2 = s.trim();
-			if (s2.isEmpty())
-				continue;
-			valueList.add(s2);
-		}
-		super.doSetValue(valueList, forceCommandExecution);
-	}
-
-	@Override
-	protected void updateValueDisplay(final Object value) {
-		final StringBuilder sb = new StringBuilder();
-
-		if (value instanceof List) {
-			boolean c = false;
-			for (final Object o : (List) value) {
-				if (c) {
-					sb.append(newLine);
-				} else {
-					c = true;
-				}
-				sb.append(o);
-			}
-			super.updateValueDisplay(sb.toString());
-		} else {
-			super.updateValueDisplay(value);
-		}
+		this.minHeight = minHeight;
 	}
 
 	@Override
@@ -61,8 +31,6 @@ public class MultiTextInlineEditor extends TextInlineEditor {
 		// Override the Text implementation for force a minimum text height.
 		// TODO: A better way might be to store a height hint in the inline editor to pass to the layout manger
 		final Text text = new Text(parent, style) {
-
-			private static final int minHeight = 50;
 
 			@Override
 			protected void checkSubclass() {
