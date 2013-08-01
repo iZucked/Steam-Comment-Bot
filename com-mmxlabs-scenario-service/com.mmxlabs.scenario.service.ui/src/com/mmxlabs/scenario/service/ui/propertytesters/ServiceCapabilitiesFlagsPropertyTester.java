32fccc92b13d00f6b7084cdcffa558f5859a8ec8
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-08-01 14:20:04 -0100

    Add a capability property tester and hook it into commands

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/propertytesters/ServiceCapabilitiesFlagsPropertyTester.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.scenario.service.ui.propertytesters;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+import com.mmxlabs.scenario.service.model.Container;
+import com.mmxlabs.scenario.service.model.ScenarioService;
+
+public class ServiceCapabilitiesFlagsPropertyTester extends PropertyTester {
+	@Override
+	public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
+		if (receiver instanceof Container) {
+
+			Container container = (Container) receiver;
+			while (container != null && !(container instanceof ScenarioService)) {
+				container = container.getParent();
+			}
+
+			if (container instanceof ScenarioService) {
+				final ScenarioService scenarioService = (ScenarioService) container;
+				if (property.equals("forking")) {
+					return scenarioService.isSupportsForking();
+				}
+			}
+		}
+		return false;
+	}
+}
