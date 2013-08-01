5b1b68f9cd5af8cef54834573426b1d27e9ca653
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-08-01 14:21:14 -0100

    Remove obsolete validateScenario method

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/navigator/handlers/StartOptimisationHandler.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/navigator/handlers/StartOptimisationHandler.java
@@ -4,23 +4,12 @@
  */
 package com.mmxlabs.models.lng.transformer.ui.navigator.handlers;
 
-import java.util.Collections;
 import java.util.Iterator;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.validation.model.Category;
-import org.eclipse.emf.validation.model.EvaluationMode;
-import org.eclipse.emf.validation.service.IBatchValidator;
-import org.eclipse.emf.validation.service.IConstraintDescriptor;
-import org.eclipse.emf.validation.service.IConstraintFilter;
-import org.eclipse.emf.validation.service.ModelValidationService;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,10 +17,6 @@ import org.slf4j.LoggerFactory;
 import com.mmxlabs.jobmanager.eclipse.manager.IEclipseJobManager;
 import com.mmxlabs.models.lng.transformer.ui.OptimisationHelper;
 import com.mmxlabs.models.lng.transformer.ui.internal.Activator;
-import com.mmxlabs.models.mmxcore.MMXRootObject;
-import com.mmxlabs.models.ui.validation.DefaultExtraValidationContext;
-import com.mmxlabs.models.ui.validation.IValidationService;
-import com.mmxlabs.models.ui.validation.gui.ValidationStatusDialog;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 import com.mmxlabs.scenario.service.model.ScenarioLock;
 
@@ -84,54 +69,4 @@ public class StartOptimisationHandler extends AbstractOptimisationHandler {
 		return null;
 	}
 
-	public static boolean validateScenario(final MMXRootObject root) {
-		final IBatchValidator validator = (IBatchValidator) ModelValidationService.getInstance().newValidator(EvaluationMode.BATCH);
-		validator.setOption(IBatchValidator.OPTION_INCLUDE_LIVE_CONSTRAINTS, true);
-
-		validator.addConstraintFilter(new IConstraintFilter() {
-
-			@Override
-			public boolean accept(final IConstraintDescriptor constraint, final EObject target) {
-
-				for (final Category cat : constraint.getCategories()) {
-					if (cat.getId().endsWith(".base")) {
-						return true;
-					}
-				}
-
-				return false;
-			}
-		});
-
-		final IValidationService helper = Activator.getDefault().getValidationService();
-		final DefaultExtraValidationContext extraContext = new DefaultExtraValidationContext(root, false);
-
-		final IStatus status = helper.runValidation(validator, extraContext, Collections.singleton(root));
-
-		if (status == null) {
-			return false;
-		}
-
-		if (status.isOK() == false) {
-
-			// See if this command was executed in the UI thread - if so fire up the dialog box.
-			if (Display.getCurrent() != null) {
-
-				final ValidationStatusDialog dialog = new ValidationStatusDialog(Display.getCurrent().getActiveShell(), status, status.getSeverity() != IStatus.ERROR);
-
-				// Wait for use to press a button before continuing.
-				dialog.setBlockOnOpen(true);
-
-				if (dialog.open() == Window.CANCEL) {
-					return false;
-				}
-			}
-		}
-
-		if (status.getSeverity() == IStatus.ERROR) {
-			return false;
-		}
-
-		return true;
-	}
 }
