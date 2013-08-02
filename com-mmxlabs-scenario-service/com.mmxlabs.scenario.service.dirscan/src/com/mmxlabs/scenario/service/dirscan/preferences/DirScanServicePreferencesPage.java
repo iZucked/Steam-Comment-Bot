685a005b76413a0fa47b2a90cb2660e00ee9c2c6
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-08-02 10:50:08 -0100

    Remove extra refresh button

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.dirscan/src/com/mmxlabs/scenario/service/dirscan/preferences/DirScanServicePreferencesPage.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.dirscan/src/com/mmxlabs/scenario/service/dirscan/preferences/DirScanServicePreferencesPage.java
@@ -8,15 +8,6 @@ import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -56,48 +47,6 @@ public class DirScanServicePreferencesPage extends FieldEditorPreferencePage imp
 	 */
 	@Override
 	public void init(IWorkbench workbench) {
-	}
-
-	@SuppressWarnings("unused")
-	@Override
-	protected Control createContents(Composite parent) {
-		final Composite c = (Composite) super.createContents(parent);
-		final Composite sub = new Composite(c, SWT.NONE);
-		sub.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, ((GridLayout) c.getLayout()).numColumns, 1));
-
-		sub.setLayout(new GridLayout(2, false));
-
-		new Label(sub, SWT.NONE);
-		final Button refresh = new Button(sub, SWT.RIGHT);
-		refresh.setText("Reload");
-		refresh.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
-
-		refresh.addSelectionListener(new SelectionAdapter() {
-
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-//				if (!source.isConnected())
-//					source.connect();
-//				source.refresh();
-//				final ServiceStats stats = source.getStatus();
-//				lblConnected.setText(source.getConnectionStatus());
-//				lblQueries.setText(stats == null ? "unknown" : source.getDistanceQueriesRemaining() + "");
-//				lblConPerDay.setText(stats == null ? "unknown" : stats.getMaximumCalculationsPerDay() + "");
-//				lblConcur.setText(stats == null ? "unknown" : stats.getMaximumConnectionsAllowed() + "");
-//				lblCalcToday.setText(stats == null ? "unknown" : stats.getNumberOfCalculationsToday() + "");
-//				lblSubExp.setText(stats == null ? "unknown" : stats.getSubscriptionExpiryDate().getValue() + "");
-//				lblTabVer.setText(stats == null ? "unknown" : stats.getTableVersion().getValue() + "");
-//				lblTabUp.setText(stats == null ? "unknown" : stats.getTableDate().getValue() + "");
-			}
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				widgetSelected(e);
-			}
-
-		});
-
-		return c;
 	}
 
 }
