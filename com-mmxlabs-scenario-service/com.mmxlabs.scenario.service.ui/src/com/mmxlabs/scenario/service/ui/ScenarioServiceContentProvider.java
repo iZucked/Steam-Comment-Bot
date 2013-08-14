833b912620742bff5ffa9a6e0a6a9c2558381921
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-08-14 13:48:27 -0100

    Remove unused import

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/ScenarioServiceContentProvider.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/ScenarioServiceContentProvider.java
@@ -12,7 +12,6 @@ import java.util.WeakHashMap;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.ui.Saveable;
