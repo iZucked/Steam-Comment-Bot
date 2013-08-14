37f828754fe24c771cd0a89969b20a9db66e96d4
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-08-14 11:39:43 -0100

    Fix selection hook up issues

--- com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.reports/src/com/mmxlabs/shiplingo/platform/reports/views/CapacityViolationReportView.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.shiplingo.platform.reports/src/com/mmxlabs/shiplingo/platform/reports/views/CapacityViolationReportView.java
@@ -4,6 +4,7 @@
  */
 package com.mmxlabs.shiplingo.platform.reports.views;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -17,6 +18,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.swt.widgets.Composite;
 
 import com.mmxlabs.models.lng.schedule.CapacityViolationType;
 import com.mmxlabs.models.lng.schedule.CapacityViolationsHolder;
@@ -29,7 +31,8 @@ import com.mmxlabs.models.lng.schedule.SlotAllocation;
 import com.mmxlabs.models.lng.schedule.SlotVisit;
 import com.mmxlabs.models.ui.tabular.generic.GenericEMFTableDataModel;
 import com.mmxlabs.shiplingo.platform.reports.IScenarioInstanceElementCollector;
-import com.mmxlabs.shiplingo.platform.reports.ScheduledEventCollector;
+import com.mmxlabs.shiplingo.platform.reports.ScheduleElementCollector;
+import com.mmxlabs.shiplingo.platform.reports.utils.ScheduleDiffUtils;
 
 /**
  * @author Simon Goodall
@@ -66,34 +69,68 @@ public class CapacityViolationReportView extends EMFReportView {
 		addColumn("Quantity (m³)", objectFormatter, attrib_Row_Quantity);
 	}
 
-	private void createDataModel() {
-		tableDataModel = GenericEMFTableDataModel.createEPackage(NODE_OWNER);
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
 
-		final EClass rowClass = GenericEMFTableDataModel.getRowClass(tableDataModel);
-		attrib_Row_Type = GenericEMFTableDataModel.createRowAttribute(rowClass, EcorePackage.eINSTANCE.getEEnum(), ATTRIBUTE_TYPE);
-		attrib_Row_Quantity = GenericEMFTableDataModel.createRowAttribute(rowClass, EcorePackage.eINSTANCE.getELong(), ATTRIBUTE_QUANTITY);
-
-		ref_Row_Owner = (EReference) GenericEMFTableDataModel.getRowFeature(tableDataModel, NODE_OWNER);
+		viewer.setComparator(GenericEMFTableDataModel.createGroupComparator(viewer.getComparator(), tableDataModel));
 	}
 
-	private EObject createRow(final EObject dataModelInstance, final EObject owner, final CapacityViolationType type, final long qty) {
-		final EObject row = GenericEMFTableDataModel.createRow(tableDataModel, dataModelInstance, null);
-		row.eSet(ref_Row_Owner, owner);
-		row.eSet(attrib_Row_Type, type);
-		row.eSet(attrib_Row_Quantity, qty);
+	@Override
+	protected boolean handleSelections() {
+		return true;
+	}
 
-		return row;
+	@Override
+	protected void processInputs(final Object[] result) {
+
+		for (final Object obj : result) {
+			if (obj instanceof EObject) {
+				final EObject row = (EObject) obj;
+				final Event event = (Event) row.eGet(ref_Row_Owner);
+				setInputEquivalents(row, expandEvent(event));
+			}
+		}
+	}
+
+	private Collection<Object> expandEvent(final Event event) {
+
+		final Set<Object> objects = new HashSet<Object>();
+		objects.add(event);
+		if (event instanceof SlotVisit) {
+			final SlotVisit slotVisit = (SlotVisit) event;
+			final SlotAllocation slotAllocation = slotVisit.getSlotAllocation();
+			final CargoAllocation cargoAllocation = slotAllocation.getCargoAllocation();
+			if (cargoAllocation != null) {
+				for (final SlotAllocation sa : cargoAllocation.getSlotAllocations()) {
+					objects.add(sa);
+					objects.add(sa.getSlotVisit());
+				}
+				objects.add(cargoAllocation);
+				objects.add(cargoAllocation.getInputCargo());
+			} else {
+				objects.add(slotAllocation);
+			}
+		}
+
+		return objects;
+	}
+
+	@Override
+	protected boolean isElementDifferent(final EObject pinnedObject, final EObject otherObject) {
+		return ScheduleDiffUtils.isElementDifferent(pinnedObject, otherObject);
 	}
 
 	@Override
 	protected IScenarioInstanceElementCollector getElementCollector() {
-		return new ScheduledEventCollector() {
+		return new ScheduleElementCollector() {
 
 			private EObject dataModelInstance;
 
 			@Override
 			public void beginCollecting() {
 				super.beginCollecting();
+				CapacityViolationReportView.this.clearPinModeData();
 
 				dataModelInstance = GenericEMFTableDataModel.createRootInstance(tableDataModel);
 			}
@@ -114,7 +151,6 @@ public class CapacityViolationReportView extends EMFReportView {
 										final Long qty = violationMap.get(cvt);
 										if (qty != null) {
 											final EObject row = createRow(dataModelInstance, event, cvt, qty);
-											setInputEquivalents(row, expandEvent(event));
 											rows.add(row);
 										}
 									}
@@ -124,35 +160,61 @@ public class CapacityViolationReportView extends EMFReportView {
 					}
 				}
 
+				CapacityViolationReportView.this.collectPinModeElements(rows, pinned);
+
 				return rows;
-			}
-
-			private Collection<Object> expandEvent(final Event event) {
-
-				final Set<Object> objects = new HashSet<Object>();
-				objects.add(event);
-				if (event instanceof SlotVisit) {
-					final SlotVisit slotVisit = (SlotVisit) event;
-					final SlotAllocation slotAllocation = slotVisit.getSlotAllocation();
-					final CargoAllocation cargoAllocation = slotAllocation.getCargoAllocation();
-					if (cargoAllocation != null) {
-						for (final SlotAllocation sa : cargoAllocation.getSlotAllocations()) {
-							objects.add(sa);
-							objects.add(sa.getSlotVisit());
-						}
-						objects.add(cargoAllocation);
-					} else {
-						objects.add(slotAllocation);
-					}
-				}
-
-				return objects;
 			}
 		};
 	}
 
+	/**
+	 * Returns a key of some kind for the element
+	 * 
+	 * @param element
+	 * @return
+	 * @since 1.1
+	 */
 	@Override
-	protected boolean handleSelections() {
-		return true;
+	protected String getElementKey(EObject element) {
+
+		if (element.eIsSet(ref_Row_Owner)) {
+			element = (EObject) element.eGet(ref_Row_Owner);
+		}
+
+		if (element instanceof Event) {
+			return ((Event) element).name();
+		}
+		return super.getElementKey(element);
+	}
+
+	@Override
+	protected List<?> adaptSelectionFromWidget(final List<?> selection) {
+		final List<Object> adaptedSelection = new ArrayList<Object>(selection.size());
+		for (final Object obj : selection) {
+			if (obj instanceof EObject) {
+				adaptedSelection.add(((EObject) obj).eGet(ref_Row_Owner));
+			}
+		}
+
+		return adaptedSelection;
+	}
+
+	private void createDataModel() {
+		tableDataModel = GenericEMFTableDataModel.createEPackage(NODE_OWNER);
+
+		final EClass rowClass = GenericEMFTableDataModel.getRowClass(tableDataModel);
+		attrib_Row_Type = GenericEMFTableDataModel.createRowAttribute(rowClass, EcorePackage.eINSTANCE.getEEnum(), ATTRIBUTE_TYPE);
+		attrib_Row_Quantity = GenericEMFTableDataModel.createRowAttribute(rowClass, EcorePackage.eINSTANCE.getELong(), ATTRIBUTE_QUANTITY);
+
+		ref_Row_Owner = (EReference) GenericEMFTableDataModel.getRowFeature(tableDataModel, NODE_OWNER);
+	}
+
+	private EObject createRow(final EObject dataModelInstance, final EObject owner, final CapacityViolationType type, final long qty) {
+		final EObject row = GenericEMFTableDataModel.createRow(tableDataModel, dataModelInstance, null);
+		row.eSet(ref_Row_Owner, owner);
+		row.eSet(attrib_Row_Type, type);
+		row.eSet(attrib_Row_Quantity, qty);
+
+		return row;
 	}
 }
