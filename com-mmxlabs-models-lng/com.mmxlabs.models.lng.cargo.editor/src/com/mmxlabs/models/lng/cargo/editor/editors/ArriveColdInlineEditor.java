9532c385a7be4e4b7223d6db1c3b183b304ba7ef
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-09-27 14:03:33 -0100

    Flip the boolean status from the override

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/editor/editors/ArriveColdInlineEditor.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.cargo.editor.editors;
+
+import org.eclipse.emf.ecore.EAttribute;
+
+import com.mmxlabs.models.mmxcore.MMXObject;
+import com.mmxlabs.models.ui.editors.impl.YesNoInlineEditor;
+
+public class ArriveColdInlineEditor extends YesNoInlineEditor {
+
+	public ArriveColdInlineEditor(final EAttribute feature) {
+		super(feature);
+	}
+
+	@Override
+	protected Object getValue() {
+		if (input != null && (!feature.isUnsettable() || input.eIsSet(feature))) {
+			return super.getValue();
+		} else {
+			if (input instanceof MMXObject) {
+				final Object unsetValue = ((MMXObject) input).getUnsetValue(getFeature());
+				if (unsetValue instanceof Boolean) {
+					// Arrive cold override allow cooldown - the boolean inference is reversed
+					final Boolean b = (Boolean) unsetValue;
+					return !b;
+				}
+				return unsetValue;
+			} else {
+				return null;
+			}
+		}
+	}
+}
