f473e755962ef994da32e20f434b24970a47809f
proshun <proshun@minimaxlabs.com>
2013-10-28 15:36:24 +0000

    Redirection refactor begun for VoyageCostCalculator

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/extensions/redirection/RedirectionContract.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/extensions/redirection/RedirectionContract.java
@@ -7,20 +7,17 @@ package com.mmxlabs.models.lng.transformer.extensions.redirection;
 import java.util.List;
 
 import com.google.inject.Inject;
-import com.mmxlabs.common.CollectionsUtil;
 import com.mmxlabs.common.curves.ICurve;
 import com.mmxlabs.common.detailtree.IDetailTree;
 import com.mmxlabs.common.detailtree.impl.TotalCostDetailElement;
 import com.mmxlabs.common.detailtree.impl.UnitPriceDetailElement;
 import com.mmxlabs.common.indexedobjects.IIndexingContext;
 import com.mmxlabs.models.lng.transformer.extensions.redirection.providers.IOriginalDataProvider;
-import com.mmxlabs.optimiser.common.components.impl.TimeWindow;
 import com.mmxlabs.optimiser.common.dcproviders.IElementDurationProvider;
 import com.mmxlabs.optimiser.core.ISequenceElement;
 import com.mmxlabs.optimiser.core.ISequences;
 import com.mmxlabs.optimiser.core.scenario.common.IMultiMatrixProvider;
 import com.mmxlabs.scheduler.optimiser.Calculator;
-import com.mmxlabs.scheduler.optimiser.OptimiserUnitConvertor;
 import com.mmxlabs.scheduler.optimiser.components.IDischargeOption;
 import com.mmxlabs.scheduler.optimiser.components.IDischargeSlot;
 import com.mmxlabs.scheduler.optimiser.components.ILoadOption;
@@ -30,22 +27,12 @@ import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
 import com.mmxlabs.scheduler.optimiser.components.IVessel;
 import com.mmxlabs.scheduler.optimiser.components.IVesselClass;
 import com.mmxlabs.scheduler.optimiser.components.VesselInstanceType;
-import com.mmxlabs.scheduler.optimiser.components.VesselState;
-import com.mmxlabs.scheduler.optimiser.components.impl.DischargeSlot;
-import com.mmxlabs.scheduler.optimiser.components.impl.EndPortSlot;
-import com.mmxlabs.scheduler.optimiser.components.impl.LoadSlot;
-import com.mmxlabs.scheduler.optimiser.components.impl.PortSlot;
 import com.mmxlabs.scheduler.optimiser.components.impl.Vessel;
 import com.mmxlabs.scheduler.optimiser.contracts.ILoadPriceCalculator;
-import com.mmxlabs.scheduler.optimiser.contracts.impl.MarketPriceContract;
 import com.mmxlabs.scheduler.optimiser.entities.IEntityValueCalculator;
 import com.mmxlabs.scheduler.optimiser.providers.IPortSlotProvider;
 import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 import com.mmxlabs.scheduler.optimiser.voyage.ILNGVoyageCalculator;
-import com.mmxlabs.scheduler.optimiser.voyage.impl.PortDetails;
-import com.mmxlabs.scheduler.optimiser.voyage.impl.PortOptions;
-import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyageDetails;
-import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyageOptions;
 import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlan;
 
 /**
@@ -56,6 +43,8 @@ import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyagePlan;
  */
 public class RedirectionContract implements ILoadPriceCalculator {
 
+	private RedirectionVoyageCostCalculator redirVCC;
+	
 	@Inject
 	private ILNGVoyageCalculator voyageCalculator;
 
@@ -83,7 +72,7 @@ public class RedirectionContract implements ILoadPriceCalculator {
 	private Vessel vessel;
 
 	private final ICurve purchasePriceCurve;
-	private final ICurve salesPriceCurve;
+//	private final ICurve salesPriceCurve;
 	private final int notionalSpeed;
 	private final IPort baseSalesMarketPort;
 	private final IPort sourcePurchasePort;
@@ -92,7 +81,10 @@ public class RedirectionContract implements ILoadPriceCalculator {
 			final IVesselClass templateClass, final ICurve charterInCurve) {
 		super();
 		this.purchasePriceCurve = purchasePriceCurve;
-		this.salesPriceCurve = salesPriceCurve;
+//		this.salesPriceCurve = salesPriceCurve;
+		
+		redirVCC = new RedirectionVoyageCostCalculator(salesPriceCurve);
+		
 		this.notionalSpeed = notionalSpeed;
 		this.baseSalesMarketPort = baseSalesMarketPort;
 		this.sourcePurchasePort = sourcePurchasePort;
@@ -141,7 +133,7 @@ public class RedirectionContract implements ILoadPriceCalculator {
 			for (final String route : distanceProvider.getKeys()) {
 				// Notional Costs - Hammerfest -> Dragon
 
-				final VoyagePlan plan = calculateShippingCosts(loadSlot.getLoadPriceCalculator(), sourcePurchasePort, baseSalesMarketPort, originalLoadTime, loadVolumeInM3, vessel, cargoCVValue,
+				final VoyagePlan plan = redirVCC.calculateShippingCosts(loadSlot.getLoadPriceCalculator(), sourcePurchasePort, baseSalesMarketPort, originalLoadTime, loadVolumeInM3, vessel, cargoCVValue,
 						route);
 				if (plan == null) {
 					continue;
@@ -167,7 +159,7 @@ public class RedirectionContract implements ILoadPriceCalculator {
 			String notionalRoute = null;
 			for (final String route : distanceProvider.getKeys()) {
 
-				final VoyagePlan plan = calculateShippingCosts(loadSlot.getLoadPriceCalculator(), sourcePurchasePort, dischargeSlot.getPort(), originalLoadTime, loadVolumeInM3, vessel, cargoCVValue,
+				final VoyagePlan plan = redirVCC.calculateShippingCosts(loadSlot.getLoadPriceCalculator(), sourcePurchasePort, dischargeSlot.getPort(), originalLoadTime, loadVolumeInM3, vessel, cargoCVValue,
 						route);
 				if (plan == null) {
 					continue;
@@ -206,115 +198,6 @@ public class RedirectionContract implements ILoadPriceCalculator {
 		}
 	}
 
-	private VoyagePlan calculateShippingCosts(final ILoadPriceCalculator loadPriceCalculator, final IPort loadPort, final IPort dischargePort, final int loadTime, final long loadVolumeInM3,
-			final IVessel vessel, final int cargoCVValue, final String route) {
-
-		final VoyagePlan notionalPlan = new VoyagePlan();
-
-		final Integer distance = distanceProvider.get(route).get(loadPort, dischargePort);
-		if (distance == null || distance.intValue() == Integer.MAX_VALUE) {
-			return null;
-		}
-
-		// Generate a notional voyage plan
-		final int travelTime = getNotionalSpeed() == 0 ? 0 : Calculator.getTimeFromSpeedDistance(getNotionalSpeed(), distance);
-		// TODO: Extend API to contain port default
-		final int notionalLoadDuration = 24;// durationProvider.getDefaultValue();
-		final int notionalDischargeDuration = 24;// durationProvider.getDefaultValue();
-
-		// Determine notional port visit times.
-		final int notionalDischargeTime = loadTime + notionalLoadDuration + travelTime;
-		final int notionalReturnTime = notionalDischargeTime + notionalDischargeDuration + travelTime;
-		final int[] arrivalTimes = new int[] { loadTime, notionalDischargeTime, notionalReturnTime };
-
-		final LoadSlot notionalLoadSlot = new LoadSlot();
-		notionalLoadSlot.setPort(loadPort);
-		notionalLoadSlot.setTimeWindow(new TimeWindow(loadTime, loadTime));
-		notionalLoadSlot.setLoadPriceCalculator(loadPriceCalculator);
-		notionalLoadSlot.setCargoCVValue(cargoCVValue);
-		notionalLoadSlot.setCooldownForbidden(true);
-		notionalLoadSlot.setMaxLoadVolume(loadVolumeInM3);
-		notionalLoadSlot.setMinLoadVolume(loadVolumeInM3);
-
-		final DischargeSlot notionalDischargeSlot = new DischargeSlot();
-		notionalDischargeSlot.setPort(dischargePort);
-		notionalDischargeSlot.setTimeWindow(new TimeWindow(notionalDischargeTime, notionalDischargeTime));
-		notionalDischargeSlot.setDischargePriceCalculator(new MarketPriceContract(salesPriceCurve, 0, OptimiserUnitConvertor.convertToInternalConversionFactor(1)));
-
-		final PortSlot notionalReturnSlot = new EndPortSlot();
-		notionalReturnSlot.setPort(loadPort);
-		notionalReturnSlot.setTimeWindow(new TimeWindow(notionalReturnTime, notionalReturnTime));
-
-		// Calculate new voyage requirements
-		// TODO: Optimise over fuel choices using a VPO?
-		{
-			final VoyageDetails ladenDetails = new VoyageDetails();
-			{
-				final VoyageOptions ladenOptions = new VoyageOptions();
-				ladenOptions.setAvailableTime(travelTime);
-				ladenOptions.setAllowCooldown(false);
-				ladenOptions.setAvailableLNG(travelTime);
-				ladenOptions.setDistance(distance);
-				ladenOptions.setFromPortSlot(notionalLoadSlot);
-				ladenOptions.setNBOSpeed(getNotionalSpeed());
-				ladenOptions.setRoute(route);
-				ladenOptions.setShouldBeCold(true);
-				ladenOptions.setToPortSlot(notionalDischargeSlot);
-				ladenOptions.setUseFBOForSupplement(false);
-				ladenOptions.setUseNBOForIdle(true);
-				ladenOptions.setUseNBOForTravel(true);
-				ladenOptions.setVessel(vessel);
-				ladenOptions.setVesselState(VesselState.Laden);
-				ladenOptions.setWarm(false);
-
-				voyageCalculator.calculateVoyageFuelRequirements(ladenOptions, ladenDetails);
-			}
-			final VoyageDetails ballastDetails = new VoyageDetails();
-			{
-				final VoyageOptions ballastOptions = new VoyageOptions();
-				ballastOptions.setAvailableTime(travelTime);
-				ballastOptions.setAllowCooldown(false);
-				ballastOptions.setAvailableLNG(travelTime);
-				ballastOptions.setDistance(distance);
-				ballastOptions.setFromPortSlot(notionalDischargeSlot);
-				ballastOptions.setNBOSpeed(getNotionalSpeed());
-				ballastOptions.setRoute(route);
-				ballastOptions.setShouldBeCold(true);
-				ballastOptions.setToPortSlot(notionalReturnSlot);
-				ballastOptions.setUseFBOForSupplement(false);
-				ballastOptions.setUseNBOForIdle(true);
-				ballastOptions.setUseNBOForTravel(true);
-				ballastOptions.setVessel(vessel);
-				ballastOptions.setVesselState(VesselState.Ballast);
-				ballastOptions.setWarm(false);
-
-				voyageCalculator.calculateVoyageFuelRequirements(ballastOptions, ballastDetails);
-			}
-
-			final PortDetails loadDetails = new PortDetails();
-			loadDetails.setOptions(new PortOptions());
-			loadDetails.getOptions().setPortSlot(notionalLoadSlot);
-			loadDetails.getOptions().setVisitDuration(notionalLoadDuration);
-
-			final PortDetails dischargeDetails = new PortDetails();
-			dischargeDetails.setOptions(new PortOptions());
-			dischargeDetails.getOptions().setPortSlot(notionalDischargeSlot);
-			dischargeDetails.getOptions().setVisitDuration(notionalDischargeDuration);
-
-			final PortDetails returnDetails = new PortDetails();
-			returnDetails.setOptions(new PortOptions());
-			returnDetails.getOptions().setPortSlot(notionalReturnSlot);
-			returnDetails.getOptions().setVisitDuration(0);
-
-			final Object[] sequence = new Object[] { loadDetails, ladenDetails, dischargeDetails, ballastDetails, returnDetails };
-			notionalPlan.setSequence(sequence);
-			voyageCalculator.calculateVoyagePlan(notionalPlan, vessel, CollectionsUtil.toArrayList(arrivalTimes), sequence);
-
-			return notionalPlan;
-		}
-
-	}
-
 	@Override
 	public int calculateLoadPricePerMMBTu(final ILoadOption loadOption, final IDischargeOption dischargeOption, final int transferTime, final int actualSalesPricePerMMBTu, final long transferVolumeInM3,
 			final IDetailTree annotations) {
@@ -341,7 +224,7 @@ public class RedirectionContract implements ILoadPriceCalculator {
 			for (final String route : distanceProvider.getKeys()) {
 				// Notional Costs - Hammerfest -> Dragon
 
-				final VoyagePlan plan = calculateShippingCosts(loadOption.getLoadPriceCalculator(), sourcePurchasePort, baseSalesMarketPort, originalLoadTime, transferVolumeInM3, vessel,
+				final VoyagePlan plan = redirVCC.calculateShippingCosts(loadOption.getLoadPriceCalculator(), sourcePurchasePort, baseSalesMarketPort, originalLoadTime, transferVolumeInM3, vessel,
 						cargoCVValue, route);
 				if (plan == null) {
 					continue;
@@ -367,7 +250,7 @@ public class RedirectionContract implements ILoadPriceCalculator {
 			String notionalRoute = null;
 			for (final String route : distanceProvider.getKeys()) {
 
-				final VoyagePlan plan = calculateShippingCosts(loadOption.getLoadPriceCalculator(), sourcePurchasePort, dischargeOption.getPort(), originalLoadTime, transferVolumeInM3, vessel,
+				final VoyagePlan plan = redirVCC.calculateShippingCosts(loadOption.getLoadPriceCalculator(), sourcePurchasePort, dischargeOption.getPort(), originalLoadTime, transferVolumeInM3, vessel,
 						cargoCVValue, route);
 				if (plan == null) {
 					continue;
