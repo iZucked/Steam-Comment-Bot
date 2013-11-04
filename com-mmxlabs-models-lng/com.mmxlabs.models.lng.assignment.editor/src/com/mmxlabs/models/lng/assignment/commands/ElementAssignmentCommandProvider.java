37f3ccb5c54053b02c841540a73a6913dd270d27
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-11-04 13:54:41 +0000

    Incomplete - task : Vessel Assignment inline editor broken

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.editor/src/com/mmxlabs/models/lng/assignment/commands/ElementAssignmentCommandProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.editor/src/com/mmxlabs/models/lng/assignment/commands/ElementAssignmentCommandProvider.java
@@ -4,6 +4,8 @@
  */
 package com.mmxlabs.models.lng.assignment.commands;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,18 +15,16 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.CommandParameter;
 import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
-import com.mmxlabs.models.common.commandservice.BaseModelCommandProvider;
-import com.mmxlabs.models.lng.assignment.AssignmentFactory;
+import com.mmxlabs.models.common.commandservice.AbstractModelCommandProvider;
 import com.mmxlabs.models.lng.assignment.AssignmentModel;
-import com.mmxlabs.models.lng.assignment.AssignmentPackage;
 import com.mmxlabs.models.lng.assignment.ElementAssignment;
 import com.mmxlabs.models.lng.assignment.editor.utils.AssignmentEditorHelper;
 import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
-import com.mmxlabs.models.lng.cargo.CargoType;
 import com.mmxlabs.models.lng.cargo.DischargeSlot;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.Slot;
@@ -37,155 +37,116 @@ import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.mmxcore.UUIDObject;
 
-public class ElementAssignmentCommandProvider extends BaseModelCommandProvider<Object> {
+public class ElementAssignmentCommandProvider extends AbstractModelCommandProvider<Object> {
 
 	@Override
 	public Command provideAdditionalCommand(final EditingDomain editingDomain, final MMXRootObject rootObject, final Map<EObject, EObject> overrides, final Set<EObject> editSet,
 			final Class<? extends Command> commandClass, final CommandParameter parameter, final Command input) {
 
 		// These could change the cargo type!
-		if (parameter.getFeature() == CargoPackage.Literals.CARGO__SLOTS || parameter.getFeature() == CargoPackage.Literals.SLOT__CARGO) {
-			// TODO: Here we need to see what state the cargo will be in after command execution and determine whether or not to update the element assignment.
-			// TODO: However this may occur before other commands being executed so we may not be creating the correct thing...
-			// TODO: We probably need a custom command to check the current state during command execution rather than creation
-			
-//			if (parameter.getOwner() instanceof Cargo) {
-//				Cargo cargo = (Cargo) parameter.getOwner();
-//				return updateCargoType(editingDomain, rootObject, overrides, editSet, cargo);
-//			} else if (parameter.getOwner() instanceof Slot) {
-//				Slot slot = (Slot) parameter.getOwner();
-//				if (parameter.get)
-//				if (cargo.getCargoType() == CargoType.FLEET) {
-//					return objectAdded(editingDomain, rootObject, cargo, overrides, editSet);
-//				} else {
-//					return objectDeleted(editingDomain, rootObject, cargo, overrides, editSet);
-//				}
-//			}
+		if (commandClass == SetCommand.class && (parameter.getFeature() == CargoPackage.Literals.CARGO__SLOTS || parameter.getFeature() == CargoPackage.Literals.SLOT__CARGO)) {
+			if (parameter.getOwner() instanceof UUIDObject) {
+				return executeCommand(editingDomain, rootObject, (UUIDObject) parameter.getOwner());
+			}
 		}
 
 		// Check for correct owner. For example we do not want to trigger this for cargoes being added to cargo groups.
 		if (parameter.getOwner() == null || parameter.getOwner() instanceof CargoModel || parameter.getOwner() instanceof FleetModel || parameter.getOwner() instanceof ScenarioFleetModel) {
-			return super.provideAdditionalCommand(editingDomain, rootObject, overrides, editSet, commandClass, parameter, input);
-		}
-
-		else if (parameter.getOwner() instanceof LoadSlot) {
-			LoadSlot loadSlot = (LoadSlot) parameter.getOwner();
-			if (loadSlot.isDESPurchase()) {
-				return super.provideAdditionalCommand(editingDomain, rootObject, overrides, editSet, commandClass, parameter, input);
-			}
-		}
-
-		else if (parameter.getOwner() instanceof DischargeSlot) {
-			DischargeSlot dischargeSlot = (DischargeSlot) parameter.getOwner();
-			if (dischargeSlot.isFOBSale()) {
-				return super.provideAdditionalCommand(editingDomain, rootObject, overrides, editSet, commandClass, parameter, input);
-			}
-		}
-
-		return null;
-	}
-
-	public Command updateCargoType(final EditingDomain editingDomain, final MMXRootObject rootObject, final Map<EObject, EObject> overrides, final Set<EObject> editSet, Cargo cargo) {
-		if (cargo.getCargoType() == CargoType.FLEET) {
-			return objectAdded(editingDomain, rootObject, cargo, overrides, editSet);
-		} else {
-			return objectDeleted(editingDomain, rootObject, cargo, overrides, editSet);
-		}
-	}
-
-	@Override
-	protected boolean shouldHandleAddition(final Object addedObject, final Map<EObject, EObject> overrides, final Set<EObject> editSet) {
-
-		boolean checkObject = false;
-
-		checkObject = addedObject instanceof VesselEvent;
-		if (!checkObject && addedObject instanceof Cargo) {
-			Cargo cargo = (Cargo) addedObject;
-			checkObject = cargo.getCargoType() == CargoType.FLEET;
-		}
-		if (!checkObject && addedObject instanceof LoadSlot) {
-			LoadSlot loadSlot = (LoadSlot) addedObject;
-			checkObject = loadSlot.isDESPurchase();
-		}
-		if (!checkObject && addedObject instanceof DischargeSlot) {
-			DischargeSlot DischargeSlot = (DischargeSlot) addedObject;
-			checkObject = DischargeSlot.isFOBSale();
-		}
-
-		if (checkObject) {
-
-			for (final EObject entry : editSet) {
-				if (entry instanceof ElementAssignment) {
-					final ElementAssignment elementAssignment = (ElementAssignment) entry;
-					if (elementAssignment.getAssignedObject() == addedObject) {
-						// There is already an assignment under consideration.
-						return false;
+			if (commandClass == AddCommand.class || commandClass == SetCommand.class) {
+				final CompoundCommand cmd = new CompoundCommand();
+				for (final Object obj : collect(parameter)) {
+					if (obj instanceof Cargo || obj instanceof Slot || obj instanceof VesselEvent) {
+						cmd.append(executeCommand(editingDomain, rootObject, (UUIDObject) obj));
 					}
 				}
-			}
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	protected boolean shouldHandleDeletion(final Object deletedObject, final Map<EObject, EObject> overrides, final Set<EObject> editSet) {
-		boolean shouldHandleDeletion = (deletedObject instanceof Cargo || deletedObject instanceof VesselEvent || deletedObject instanceof VesselAvailability);
-
-		if (!shouldHandleDeletion && deletedObject instanceof LoadSlot) {
-			LoadSlot loadSlot = (LoadSlot) deletedObject;
-			shouldHandleDeletion = loadSlot.isDESPurchase();
-		}
-		if (!shouldHandleDeletion && deletedObject instanceof DischargeSlot) {
-			DischargeSlot DischargeSlot = (DischargeSlot) deletedObject;
-			shouldHandleDeletion = DischargeSlot.isFOBSale();
-		}
-		return shouldHandleDeletion;
-	}
-
-	@Override
-	protected Command objectAdded(final EditingDomain domain, final MMXRootObject rootObject, final Object added, final Map<EObject, EObject> overrides, final Set<EObject> editSet) {
-		if (rootObject instanceof LNGScenarioModel) {
-
-			final LNGScenarioModel lngScenarioModel = (LNGScenarioModel) rootObject;
-			final ElementAssignment ea = AssignmentFactory.eINSTANCE.createElementAssignment();
-			ea.setAssignedObject((UUIDObject) added);
-			return AddCommand.create(domain, lngScenarioModel.getPortfolioModel().getAssignmentModel(), AssignmentPackage.eINSTANCE.getAssignmentModel_ElementAssignments(), ea);
-		}
-		return null;
-	}
-
-	@Override
-	protected Command objectDeleted(final EditingDomain domain, final MMXRootObject rootObject, final Object deleted, final Map<EObject, EObject> overrides, final Set<EObject> editSet) {
-		if (rootObject instanceof LNGScenarioModel) {
-			final AssignmentModel assignmentModel = ((LNGScenarioModel) rootObject).getPortfolioModel().getAssignmentModel();
-
-			// if a vessel availability was deleted, unassign any associated element assignment
-			if (deleted instanceof VesselAvailability) {
-				final Vessel vessel = ((VesselAvailability) deleted).getVessel();
-				final CompoundCommand cmd = new CompoundCommand("Remove element assignents for " + vessel.getName());
-				for (final ElementAssignment ea : assignmentModel.getElementAssignments()) {
-					if (ea.getAssignment() == vessel) {
-						cmd.append(AssignmentEditorHelper.unassignElement(domain, ea));
-					}
-				}
-
 				if (!cmd.isEmpty()) {
 					return cmd;
 				}
-			}
-			// if a cargo or vessel event was deleted, delete any associated element assignment
-			else if (deleted instanceof Cargo || deleted instanceof VesselEvent || deleted instanceof Slot) {
-				final ElementAssignment ea = AssignmentEditorHelper.getElementAssignment(assignmentModel, (UUIDObject) deleted);
-				if (ea != null) {
-					return DeleteCommand.create(domain, ea);
+			} else if (commandClass == DeleteCommand.class) {
+				if (rootObject instanceof LNGScenarioModel) {
+
+					final LNGScenarioModel lngScenarioModel = (LNGScenarioModel) rootObject;
+					final AssignmentModel assignmentModel = lngScenarioModel.getPortfolioModel().getAssignmentModel();
+
+					final CompoundCommand cmd = new CompoundCommand();
+					for (final Object obj : collect(parameter)) {
+						if (obj instanceof Cargo || obj instanceof Slot || obj instanceof VesselEvent) {
+							deleteAssignments(cmd, editingDomain, assignmentModel, (UUIDObject) obj);
+						} else {
+							Object target = null;
+							if (obj instanceof VesselAvailability) {
+								target = ((VesselAvailability) obj).getVessel();
+							} else if (obj instanceof Vessel) {
+								target = obj;
+							}
+							if (target != null) {
+								for (final ElementAssignment ea : assignmentModel.getElementAssignments()) {
+									if (ea.getAssignment() == target) {
+										cmd.append(AssignmentEditorHelper.unassignElement(editingDomain, ea));
+									}
+								}
+							}
+						}
+					}
+					if (!cmd.isEmpty()) {
+						return cmd;
+					}
 				}
 			}
-
+		} else if (commandClass == SetCommand.class) {
+			if (parameter.getOwner() instanceof LoadSlot) {
+				final LoadSlot loadSlot = (LoadSlot) parameter.getOwner();
+				return executeCommand(editingDomain, rootObject, loadSlot);
+			} else if (parameter.getOwner() instanceof DischargeSlot) {
+				final DischargeSlot dischargeSlot = (DischargeSlot) parameter.getOwner();
+				return executeCommand(editingDomain, rootObject, dischargeSlot);
+			} else if (parameter.getOwner() instanceof Cargo) {
+				final Cargo cargo = (Cargo) parameter.getOwner();
+				return executeCommand(editingDomain, rootObject, cargo);
+			} else if (parameter.getOwner() instanceof VesselEvent) {
+				final VesselEvent vesselEvent = (VesselEvent) parameter.getOwner();
+				return executeCommand(editingDomain, rootObject, vesselEvent);
+			}
 		}
 		return null;
 	}
 
+	public Command executeCommand(final EditingDomain editingDomain, final MMXRootObject rootObject, final UUIDObject assignedObject) {
+		if (rootObject instanceof LNGScenarioModel) {
+			final AssignmentModel assignmentModel = ((LNGScenarioModel) rootObject).getPortfolioModel().getAssignmentModel();
+			return new UpdateElementAssignmentCommand(editingDomain, assignmentModel, assignedObject);
+		}
+		return null;
+	}
+
+	/**
+	 * @param parameter
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	private Collection<Object> collect(final CommandParameter parameter) {
+		if (parameter.getCollection() != null) {
+			return (Collection<Object>) parameter.getCollection();
+		} else if (parameter.getValue() != null) {
+			return Collections.singleton(parameter.getValue());
+		}
+		return Collections.emptySet();
+	}
+
+	private void deleteAssignments(final CompoundCommand cmd, final EditingDomain domain, final AssignmentModel assignmentModel, final UUIDObject uuidObject) {
+
+		for (final ElementAssignment ea : assignmentModel.getElementAssignments()) {
+			if (ea != null) {
+				// General clean up...
+				if (ea.getAssignedObject() == null && ea.getAssignment() == null) {
+					cmd.append(DeleteCommand.create(domain, ea));
+				}
+
+				// Real check
+				if (ea.getAssignedObject() == uuidObject) {
+					cmd.append(DeleteCommand.create(domain, ea));
+				}
+			}
+		}
+	}
 }
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.editor/src/com/mmxlabs/models/lng/assignment/commands/UpdateElementAssignmentCommand.java
@@ -0,0 +1,142 @@
+package com.mmxlabs.models.lng.assignment.commands;
+
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.edit.command.AddCommand;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jdt.annotation.NonNull;
+
+import com.mmxlabs.models.lng.assignment.AssignmentFactory;
+import com.mmxlabs.models.lng.assignment.AssignmentModel;
+import com.mmxlabs.models.lng.assignment.AssignmentPackage;
+import com.mmxlabs.models.lng.assignment.ElementAssignment;
+import com.mmxlabs.models.lng.assignment.editor.utils.AssignmentEditorHelper;
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoType;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.LoadSlot;
+import com.mmxlabs.models.lng.fleet.VesselEvent;
+import com.mmxlabs.models.mmxcore.UUIDObject;
+
+public class UpdateElementAssignmentCommand extends CompoundCommand {
+
+	/**
+	 * This caches the label.
+	 */
+	protected static final String LABEL = "Update Element Assignment";
+
+	/**
+	 * This caches the description.
+	 */
+	protected static final String DESCRIPTION = "Update element assignment data";
+
+	private final EditingDomain domain;
+
+	private final UUIDObject assignedObject;
+
+	private final AssignmentModel assignmentModel;
+
+	/**
+	 * This constructs a command that deletes the objects in the given collection.
+	 */
+	public UpdateElementAssignmentCommand(@NonNull final EditingDomain domain, @NonNull final AssignmentModel assignmentModel, @NonNull final UUIDObject assignedObject) {
+		super(0, LABEL, DESCRIPTION);
+		this.domain = domain;
+		this.assignmentModel = assignmentModel;
+		this.assignedObject = assignedObject;
+	}
+
+	@Override
+	protected boolean prepare() {
+		for (final Command command : commandList) {
+			if (!command.canExecute()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void execute() {
+		super.execute();
+
+		if (assignedObject instanceof LoadSlot) {
+			// If a DES Purchase, then the DES Purchase should have the element assignment, not the cargo. Otherwise it should only be on the cargo
+			final LoadSlot loadSlot = (LoadSlot) assignedObject;
+			if (loadSlot.isDESPurchase()) {
+				{
+					// Create an assignment if missing
+					createElementAssignment(assignedObject);
+				}
+				// Remove existing cargo assignment
+				final Cargo cargo = loadSlot.getCargo();
+				if (cargo != null) {
+					deleteAssignments(cargo);
+				}
+			} else {
+				// Remove non-DES Purchase assignment
+				{
+					deleteAssignments(assignedObject);
+				}
+				// Create cargo assignment if missing
+				final Cargo cargo = loadSlot.getCargo();
+				if (cargo != null) {
+					createElementAssignment(cargo);
+				}
+			}
+		} else if (assignedObject instanceof DischargeSlot) {
+			final DischargeSlot dischargeSlot = (DischargeSlot) assignedObject;
+			if (dischargeSlot.isFOBSale()) {
+				{
+					// Create an assignment if missing
+					createElementAssignment(assignedObject);
+				}
+				// Remove existing cargo assignment
+				final Cargo cargo = dischargeSlot.getCargo();
+				if (cargo != null) {
+					deleteAssignments(cargo);
+				}
+			} else {
+				// Remove non-FOB Sale assignment
+				{
+					deleteAssignments(assignedObject);
+				}
+				// Create cargo assignment if missing
+				final Cargo cargo = dischargeSlot.getCargo();
+				if (cargo != null) {
+					createElementAssignment(cargo);
+				}
+			}
+		} else if (assignedObject instanceof Cargo) {
+			final Cargo cargo = (Cargo) assignedObject;
+			if (cargo.getCargoType() == CargoType.FLEET) {
+				createElementAssignment(cargo);
+			} else {
+				deleteAssignments(cargo);
+			}
+		} else if (assignedObject instanceof VesselEvent) {
+			createElementAssignment(assignedObject);
+		}
+	}
+
+	private void createElementAssignment(final UUIDObject assignedObject) {
+		ElementAssignment elementAssignment = AssignmentEditorHelper.getElementAssignment(assignmentModel, assignedObject);
+		if (elementAssignment == null) {
+			elementAssignment = AssignmentFactory.eINSTANCE.createElementAssignment();
+			elementAssignment.setAssignedObject(assignedObject);
+			appendAndExecute(AddCommand.create(domain, assignmentModel, AssignmentPackage.eINSTANCE.getAssignmentModel_ElementAssignments(), elementAssignment));
+		}
+	}
+
+	private void deleteAssignments(final UUIDObject uuidObject) {
+
+		for (final ElementAssignment ea : new ArrayList<>(assignmentModel.getElementAssignments())) {
+			if (ea != null && ea.getAssignedObject() == uuidObject) {
+				appendAndExecute(DeleteCommand.create(domain, ea));
+			}
+		}
+	}
+}
