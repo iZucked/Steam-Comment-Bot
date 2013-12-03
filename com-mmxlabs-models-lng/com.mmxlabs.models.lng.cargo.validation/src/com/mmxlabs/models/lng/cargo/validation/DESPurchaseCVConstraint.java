36c5f51b7bf01907fc6994a8a84d3e94ae2f575b
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-12-03 10:52:27 +0000

    Fix constraint to handle a) contract CV values and b) non load ports with a CV value

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/DESPurchaseCVConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/DESPurchaseCVConstraint.java
@@ -14,11 +14,19 @@ import org.eclipse.emf.validation.model.IConstraintStatus;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
+import com.mmxlabs.models.lng.commercial.Contract;
+import com.mmxlabs.models.lng.commercial.PurchaseContract;
 import com.mmxlabs.models.lng.port.Port;
 import com.mmxlabs.models.lng.types.PortCapability;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
+/**
+ * Constraint to ensure a DES Purchase has a valid CV set.
+ * 
+ * @author Simon Goodall
+ * 
+ */
 public class DESPurchaseCVConstraint extends AbstractModelMultiConstraint {
 
 	@Override
@@ -27,9 +35,28 @@ public class DESPurchaseCVConstraint extends AbstractModelMultiConstraint {
 
 		if (target instanceof LoadSlot) {
 			final LoadSlot loadSlot = (LoadSlot) target;
+			// Is there a slot CV value?
 			if (!loadSlot.isSetCargoCV()) {
+				// No? Then check the contract for a CV
+				if (loadSlot.isSetContract()) {
+					Contract c = loadSlot.getContract();
+					if (c instanceof PurchaseContract) {
+						PurchaseContract purchaseContract = (PurchaseContract) c;
+						if (!purchaseContract.isSetCargoCV()) {
+							Port preferredPort = purchaseContract.getPreferredPort();
+							// We can actually have a CV set on a discharge port (field is not unsettable) and this will be used through the getUnsetValue methods.
+							// if (preferredPort != null && preferredPort.getCapabilities().contains(PortCapability.LOAD)) {
+							if (preferredPort != null && preferredPort.getCvValue() != 0.0) {
+								return Activator.PLUGIN_ID;
+							}
+						}
+					}
+				}
+				// No contract CV, check the port
 				final Port port = loadSlot.getPort();
-				if (port != null && !port.getCapabilities().contains(PortCapability.LOAD)) {
+				// We can actually have a CV set on a discharge port (field is not unsettable) and this will be used through the getUnsetValue methods.
+				// if (port != null && !port.getCapabilities().contains(PortCapability.LOAD)) {
+				if (port != null && port.getCvValue() == 0.0) {
 					final String format = "[DES Purchase|%s] needs a CV set or be linked to a load port.";
 					final String failureMessage = String.format(format, loadSlot.getName());
 					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage));
@@ -41,5 +68,4 @@ public class DESPurchaseCVConstraint extends AbstractModelMultiConstraint {
 
 		return Activator.PLUGIN_ID;
 	}
-
 }
