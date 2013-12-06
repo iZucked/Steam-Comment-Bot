7eae84bae4409dac894ccde48882f970a3ee6046
Simon Goodall <simon.goodall@minimaxlabs.com>
2013-12-06 12:02:06 +0000

    Run migration code when loading a scenario If "output" is specified, then save the scenario

--- com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/HeadlessApplication.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/HeadlessApplication.java
@@ -5,6 +5,9 @@
 package com.mmxlabs.lingo.app.headless;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,23 +20,46 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-
+import com.google.common.io.ByteStreams;
+import com.google.common.util.concurrent.AbstractScheduledService;
 import com.mmxlabs.lingo.app.headless.exporter.FitnessTraceExporter;
 import com.mmxlabs.lingo.app.headless.exporter.IRunExporter;
+import com.mmxlabs.lingo.app.headless.internal.Activator;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
 import com.mmxlabs.models.lng.schedule.Schedule;
+import com.mmxlabs.models.migration.IMigrationRegistry;
+import com.mmxlabs.models.migration.scenario.ScenarioMigrationService;
 import com.mmxlabs.optimiser.core.IAnnotatedSolution;
 import com.mmxlabs.optimiser.core.IOptimiser;
 import com.mmxlabs.optimiser.core.IOptimiserProgressMonitor;
+import com.mmxlabs.scenario.service.manifest.Manifest;
+import com.mmxlabs.scenario.service.manifest.ManifestFactory;
 import com.mmxlabs.scenario.service.manifest.ScenarioStorageUtil;
+import com.mmxlabs.scenario.service.model.Container;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.scenario.service.model.ScenarioService;
+import com.mmxlabs.scenario.service.util.AbstractScenarioService;
+
+/**
+ * Note duplication with various bits of ITS including ScenarioRunner and MigrationHelper
+ * 
+ * @author Simon Goodall
+ * 
+ */
 
 public class HeadlessApplication implements IApplication {
 
 	private static final String FITNESS_TRACE = "exportFitnessTrace";
 	private static final String SCENARIO = "scenario";
+	private static final String OUTPUT = "output";
 	private static final String ITERATIONS = "iterations";
 	private static final String SEED = "seed";
 
@@ -58,6 +84,8 @@ public class HeadlessApplication implements IApplication {
 			System.err.println("Unable to load scenario");
 			return IApplication.EXIT_OK;
 		}
+
+		final String outputFile = settings.getOutput();
 
 		final ScenarioRunner runner = new ScenarioRunner(rootObject, settings);
 
@@ -99,6 +127,9 @@ public class HeadlessApplication implements IApplication {
 
 		System.out.println("LNGResult(");
 		System.out.println("\tscenario='" + scenarioFile + "',");
+		if (outputFile != null) {
+			System.out.println("\toutput='" + outputFile + "',");
+		}
 		System.out.println("\titerations=" + settings.getIterations() + ",");
 		System.out.println("\tseed=" + settings.getSeed() + ",");
 
@@ -117,6 +148,12 @@ public class HeadlessApplication implements IApplication {
 		System.out.println("\truntime=" + runTime / 1000 + ",");
 
 		System.err.println("Optimised!");
+
+		if (outputFile != null) {
+			runner.updateScenario();
+			saveScenario(outputFile, runner.getScenario());
+		}
+
 		for (final IRunExporter exporter : exporters) {
 			exporter.exportData();
 		}
@@ -125,43 +162,49 @@ public class HeadlessApplication implements IApplication {
 
 	}
 
-	/**
-	 * Iterate through commandLineArgs array until we find "-scenario", then return the next argument
-	 * 
-	 * @param commandLineArgs
-	 * @return
-	 */
-	private String getScenarioArgs(final String[] commandLineArgs) {
-
-		boolean foundScenario = false;
-		for (final String arg : commandLineArgs) {
-
-			if (foundScenario) {
-				return arg;
-			}
-
-			foundScenario = false;
-			if ("-scenario".equals(arg)) {
-				foundScenario = true;
-			}
-		}
-
-		return null;
-	}
-
-	private LNGScenarioModel loadScenario(final String scenarioFile) {
+	private LNGScenarioModel loadScenario(final String scenarioFile) throws IOException {
 
 		// Create instance and preload scenario.
-		final ScenarioInstance instance = ScenarioStorageUtil.loadInstanceFromURI(URI.createFileURI(scenarioFile), true);
+		final ScenarioInstance instance = ScenarioStorageUtil.loadInstanceFromURI(URI.createFileURI(scenarioFile), false);
 
 		if (instance == null) {
 			return null;
 		}
 
+		migrateAndLoad(instance);
+
 		// Get the root object
 		final LNGScenarioModel originalScenario = (LNGScenarioModel) instance.getInstance();
 
 		return originalScenario;
+	}
+
+	private void saveScenario(final String outputFile, LNGScenarioModel scenario) throws IOException {
+		final ResourceSetImpl resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+		final Manifest manifest = ManifestFactory.eINSTANCE.createManifest();
+
+		manifest.setScenarioType("com.mmxlabs.shiplingo.platform.models.manifest.scnfile");
+
+		final IMigrationRegistry migrationRegistry = Activator.getDefault().getMigrationRegistry();
+		String versionContext = migrationRegistry.getDefaultMigrationContext();
+		manifest.setScenarioVersion(migrationRegistry.getLastReleaseVersion(versionContext));
+		manifest.setVersionContext(versionContext);
+
+		File file = new File(outputFile);
+		final URI manifestURI = URI.createURI("archive:" + URI.createFileURI(file.getAbsolutePath()) + "!/MANIFEST.xmi");
+		final Resource manifestResource = resourceSet.createResource(manifestURI);
+
+		manifestResource.getContents().add(manifest);
+
+		final URI relativeURI = URI.createURI("/rootObject.xmi");
+
+		manifest.getModelURIs().add(relativeURI.toString());
+		final URI resolved = relativeURI.resolve(manifestURI);
+		final Resource r2 = resourceSet.createResource(resolved);
+		r2.getContents().add(scenario);
+		r2.save(null);
+		manifestResource.save(null);
 	}
 
 	@Override
@@ -175,6 +218,7 @@ public class HeadlessApplication implements IApplication {
 		// create the Options
 		final Options options = new Options();
 
+		options.addOption(OptionBuilder.withLongOpt(OUTPUT).withDescription("Output scenario file").hasArg().create());
 		options.addOption(OptionBuilder.withLongOpt(SCENARIO).withDescription("Input scenario file").hasArg().isRequired().create());
 		options.addOption(ITERATIONS, true, "Number of iterations");
 		options.addOption(SEED, true, "Random seed");
@@ -195,6 +239,9 @@ public class HeadlessApplication implements IApplication {
 			if (line.hasOption(SCENARIO)) {
 				settings.setScenario(line.getOptionValue(SCENARIO));
 			}
+			if (line.hasOption(OUTPUT)) {
+				settings.setOutput(line.getOptionValue(OUTPUT));
+			}
 			if (line.hasOption(ITERATIONS)) {
 				settings.setIterations(Integer.parseInt(line.getOptionValue(ITERATIONS)));
 			}
@@ -214,5 +261,117 @@ public class HeadlessApplication implements IApplication {
 		}
 
 		return true;
+	}
+
+	public static void migrateAndLoad(final ScenarioInstance instance) throws IOException {
+		final IMigrationRegistry migrationRegistry = Activator.getDefault().getMigrationRegistry();
+
+		final ScenarioMigrationService migrationService = new ScenarioMigrationService();
+		migrationService.setMigrationRegistry(migrationRegistry);
+
+		// ScenarioInstanceMigrator migrator = new ScenarioInstanceMigrator(migrationRegistry);
+		final TestScenarioService scenarioService = new TestScenarioService("headless");
+		String context = instance.getVersionContext();
+		if (context == null || context.isEmpty()) {
+			context = migrationRegistry.getDefaultMigrationContext();
+			instance.setVersionContext(context);
+			instance.setScenarioVersion(0);
+		}
+
+		// migrator.performMigration(ss, instance);
+		scenarioService.setScenarioMigrationService(migrationService);
+
+		{
+
+			final String subModelURI = instance.getRootObjectURI();
+
+			final ExtensibleURIConverterImpl uc = new ExtensibleURIConverterImpl();
+
+			// Get original URI's as a list
+			final URI uri = scenarioService.resolveURI(subModelURI);
+			// Copy data files for manipulation
+			assert uri != null;
+			final File f = File.createTempFile("migration", ".xmi");
+			// Create a temp file and generate a URI to it to pass into migration code.
+			final URI tmpURI = URI.createFileURI(f.getCanonicalPath());
+			assert tmpURI != null;
+			copyURIData(uc, uri, tmpURI);
+
+			// Store the URI
+			// Add a mapping between the original URI and the temp URI. This should permit internal references to resolve to the new data file.
+			// TODO: Check to see whether or not the URI is the original URI or the "resolved" uri.
+			uc.getURIMap().put(uri, tmpURI);
+
+			instance.setRootObjectURI(tmpURI.toString());
+
+		}
+
+		scenarioService.load(instance);
+	}
+
+	private static class TestScenarioService extends AbstractScenarioService {
+
+		public TestScenarioService(final String name) {
+			super(name);
+		}
+
+		@Override
+		public ScenarioInstance insert(final Container container, final EObject rootObject) throws IOException {
+			return null;
+		}
+
+		@Override
+		public void delete(final Container container) {
+		}
+
+		@Override
+		protected ScenarioService initServiceModel() {
+			return null;
+		}
+
+		@Override
+		public URI resolveURI(final String uri) {
+			return URI.createURI(uri);
+		}
+
+		@Override
+		public void moveInto(List<Container> elements, Container destination) {
+
+		}
+
+		@Override
+		public void makeFolder(Container parent, String name) {
+
+		}
+
+	}
+
+	@SuppressWarnings("resource")
+	public static void copyURIData(final URIConverter uc, final URI sourceURI, final URI destURI) throws IOException {
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+
+			// Get input stream from original URI
+			is = uc.createInputStream(sourceURI);
+			os = uc.createOutputStream(destURI);
+
+			ByteStreams.copy(is, os);
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (final IOException e) {
+
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch (final IOException e) {
+
+				}
+			}
+		}
 	}
 }
