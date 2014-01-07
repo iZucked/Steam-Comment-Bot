9401fe954be9ecc234a9159c36af0aef7519a14b
proshun <proshun@minimaxlabs.com>
2014-01-07 11:50:17 +0000

    Revert SDC (WIP)

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/displaycomposites/SlotDetailComposite.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/displaycomposites/SlotDetailComposite.java
@@ -91,8 +91,7 @@ public class SlotDetailComposite extends DefaultDetailComposite implements IDisp
 
 		mainFeatures = new ArrayList<EStructuralFeature[]>();
 		mainFeatures.add(new EStructuralFeature[] { CargoFeatures.getSlot_Port() });
-		mainFeatures.add(new EStructuralFeature[] { CargoFeatures.getSlot_MinQuantity() });
-		mainFeatures.add(new EStructuralFeature[] { CargoFeatures.getSlot_MaxQuantity() });
+		mainFeatures.add(new EStructuralFeature[] { CargoFeatures.getSlot_MinQuantity(), CargoFeatures.getSlot_MaxQuantity() });
 		allFeatures.addAll(getAllFeatures(mainFeatures));
 
 		pricingFeatures = new ArrayList<EStructuralFeature[]>();
@@ -250,7 +249,7 @@ public class SlotDetailComposite extends DefaultDetailComposite implements IDisp
 		Layout l = layoutProvider.createDetailLayout(root, object);
 //		contentComposite.setLayout(new GridLayout(2, false));
 		 
-		GridLayout layout = new GridLayout(4, false);
+		GridLayout layout = new GridLayout(2, false);
 		layout.verticalSpacing = 8;
 		contentComposite.setLayout(layout);
 
