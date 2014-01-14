6e62795859708fcd8dcf12987264f2a3fc3e9fd9
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-01-14 13:10:03 +0000

    Make things final

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/dialogs/DefaultDialogEditingContext.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/dialogs/DefaultDialogEditingContext.java
@@ -3,14 +3,14 @@ package com.mmxlabs.models.ui.editors.dialogs;
 import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
 
 public class DefaultDialogEditingContext implements IDialogEditingContext {
-	private IDialogController dialogController;
-	private IScenarioEditingLocation scenarioEditingLocation;
-	
-	public DefaultDialogEditingContext(IDialogController dc, IScenarioEditingLocation sel) {
+	private final IDialogController dialogController;
+	private final IScenarioEditingLocation scenarioEditingLocation;
+
+	public DefaultDialogEditingContext(final IDialogController dc, final IScenarioEditingLocation sel) {
 		dialogController = dc;
 		scenarioEditingLocation = sel;
 	}
-	
+
 	@Override
 	public IDialogController getDialogController() {
 		return dialogController;
