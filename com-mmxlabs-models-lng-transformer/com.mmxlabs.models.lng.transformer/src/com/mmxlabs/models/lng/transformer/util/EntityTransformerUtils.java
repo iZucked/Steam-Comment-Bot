9478f03849dd6fbaa160d10de954196d37855fcc
proshun <proshun@minimaxlabs.com>
2014-02-04 11:53:33 +0000

    Added BaseLegalEntity

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/util/EntityTransformerUtils.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/util/EntityTransformerUtils.java
@@ -7,7 +7,7 @@ package com.mmxlabs.models.lng.transformer.util;
 import java.util.Date;
 
 import com.mmxlabs.common.curves.StepwiseIntegerCurve;
-import com.mmxlabs.models.lng.commercial.LegalEntity;
+import com.mmxlabs.models.lng.commercial.BaseLegalEntity;
 import com.mmxlabs.models.lng.commercial.TaxRate;
 import com.mmxlabs.scheduler.optimiser.Calculator;
 
@@ -15,7 +15,7 @@ import com.mmxlabs.scheduler.optimiser.Calculator;
  * @since 3.0
  */
 public class EntityTransformerUtils {
-	public static StepwiseIntegerCurve createTaxCurve(final LegalEntity entity, final DateAndCurveHelper dateAndCurveHelper, final Date earliestDate) {
+	public static StepwiseIntegerCurve createTaxCurve(final BaseLegalEntity entity, final DateAndCurveHelper dateAndCurveHelper, final Date earliestDate) {
 		final StepwiseIntegerCurve taxCurve = new StepwiseIntegerCurve();
 		taxCurve.setDefaultValue(0);
 		for (final TaxRate taxRate : entity.getTaxRates()) {
