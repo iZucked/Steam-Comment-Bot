589bb11e31d3a43c0c79e9a8d8da3deb89c72b64
proshun <proshun@minimaxlabs.com>
2014-02-11 20:53:37 +0000

    refactor findAnnotation method to util class

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/util/SlotContractHelper.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/util/SlotContractHelper.java
@@ -8,7 +8,10 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
+import com.mmxlabs.common.detailtree.IDetailTree;
 import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.scheduler.optimiser.annotations.IProfitAndLossAnnotation;
+import com.mmxlabs.scheduler.optimiser.annotations.IProfitAndLossEntry;
 
 /**
  * @noinstantiate This class is not intended to be instantiated by clients.
@@ -22,6 +25,22 @@ public final class SlotContractHelper {
 		for (final EObject object : slot.getExtensions()) {
 			if (cls.isInstance(object)) {
 				return cls.cast(object);
+			}
+		}
+		return null;
+	}
+	
+	public static <T> T findDetailsAnnotation(@NonNull final IProfitAndLossAnnotation annotation, String annotationKey, @NonNull final Class<T> cls) {
+		for (final IProfitAndLossEntry e : annotation.getEntries()) {
+			final IDetailTree detailTree = e.getDetails();
+			if (detailTree != null) {
+				for (final IDetailTree child : detailTree.getChildren()) {
+					if (child.getKey() == annotationKey) {						
+						if (cls.isInstance(child.getValue())) {
+							return cls.cast(child.getValue());
+						}
+					}
+				}
 			}
 		}
 		return null;
