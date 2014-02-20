e522fc3c3ee79a3d8ab5c14f08bfc5a56981221c
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2014-02-20 16:06:20 +0000

    Base class for future refactoring of NullReferenceConstraint classes

---
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/AbstractNullReferenceConstraint.java
@@ -0,0 +1,68 @@
+package com.mmxlabs.models.ui.validation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
+public abstract class AbstractNullReferenceConstraint extends AbstractModelConstraint {
+	private static final HashMap<EClass, Set<EReference>> cacheByClass = new HashMap<EClass, Set<EReference>>();
+	private final List<EReference> checkedReferences;
+	
+	public AbstractNullReferenceConstraint() {
+		checkedReferences = createCheckedReferences();
+	}
+	
+	abstract protected List<EReference> createCheckedReferences(); 
+
+	private static synchronized Set<EReference> getReferencesToCheck(final EClass targetClass, final List<EReference> checkedReferences) {
+		Set<EReference> result = cacheByClass.get(targetClass);
+		if (result == null) {
+			result = new HashSet<EReference>(targetClass.getEAllReferences());
+			result.retainAll(checkedReferences);
+			cacheByClass.put(targetClass, result);
+		}
+
+		return result;
+	}
+
+	@Override
+	public IStatus validate(final IValidationContext ctx) {
+		final EObject target = ctx.getTarget();
+
+		final LinkedList<EReference> errors = new LinkedList<EReference>();
+		final Set<EReference> targetRefs = getReferencesToCheck(target.eClass(), checkedReferences);
+		for (final EReference ref : targetRefs) {
+			if ((target.eGet(ref) == null) && !(ref.isUnsettable() && (target.eIsSet(ref) == false))) {
+				errors.add(ref);
+			}
+		}
+
+		if (errors.isEmpty()) {
+			return ctx.createSuccessStatus();
+		} else {
+			final StringBuilder sb = new StringBuilder();
+			boolean first = true;
+			for (final EReference ref : errors) {
+				if (!first) {
+					sb.append(", ");
+				}
+				sb.append(ref.getName());
+				first = false;
+			}
+			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(sb.toString()));
+			for (final EReference ref : errors) {
+				dcsd.addEObjectAndFeature(target, ref);
+			}
+			return dcsd;
+		}
+	}}
