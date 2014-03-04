87b220c25f0af12f5601d482e2be5370822d08f4
hg <unknown@kiln.example.com>
2014-03-04 10:56:11 +0000

    Cancellation fee implementation

---
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/providers/impl/HashMapCancellationFeeProviderEditor.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2014
+ * All rights reserved.
+ */
+package com.mmxlabs.scheduler.optimiser.providers.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
+import com.mmxlabs.scheduler.optimiser.providers.ICancellationFeeProviderEditor;
+
+/**
+ * @author berkan
+ *
+ */
+public class HashMapCancellationFeeProviderEditor implements ICancellationFeeProviderEditor {
+
+	private final Map<IPortSlot, Long> map = new HashMap<IPortSlot, Long>();	
+	
+	@Override
+	public long getCancellationFee(IPortSlot portSlot) {
+		if (map.containsKey(portSlot)) {
+			return map.get(portSlot);
+		}
+
+		return 0;
+	}
+	
+	@Override
+	public void setCancellationFee(IPortSlot portSlot, long fee) {
+		map.put(portSlot, fee);
+	}
+	
+	
+	@Override
+	public String getName() {
+		return "";
+	}
+
+	@Override
+	public void dispose() {
+		map.clear();
+	}
+
+}
