86f2fef2226ceb6fee83bbe55a123321aac3c08e
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-03-07 15:28:49 +0000

    Fix check

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/FOBDESCargoDatesConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/FOBDESCargoDatesConstraint.java
@@ -60,7 +60,7 @@ public class FOBDESCargoDatesConstraint extends AbstractModelConstraint {
 
 				if (loadSlot != null && dischargeSlot != null) {
 					// Check this is the correct type of DES Purchase and FOB Sale
-					if (SlotClassifier.classify(loadSlot) == SlotType.DES_Buy || SlotClassifier.classify(dischargeSlot) != SlotType.FOB_Buy) {
+					if (SlotClassifier.classify(loadSlot) == SlotType.DES_Buy || SlotClassifier.classify(dischargeSlot) == SlotType.FOB_Sale) {
 						final boolean valid;
 						if (loadSlot.getWindowStartWithSlotOrPortTime().before(dischargeSlot.getWindowStartWithSlotOrPortTime())) {
 							valid = checkDates(loadSlot.getWindowStartWithSlotOrPortTime(), loadSlot.getWindowEndWithSlotOrPortTime(), dischargeSlot.getWindowEndWithSlotOrPortTime())
