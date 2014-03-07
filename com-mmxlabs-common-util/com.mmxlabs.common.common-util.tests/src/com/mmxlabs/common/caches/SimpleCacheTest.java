41331b92a82b495b18d9725672fe036c87152f56
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-03-07 17:23:01 +0000

    Convert to tycho project

---
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util.tests/src/com/mmxlabs/common/caches/SimpleCacheTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.common.caches;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.mmxlabs.common.caches.AbstractCache.IKeyEvaluator;
+
+public class SimpleCacheTest {
+
+	@Test
+	public void testSimpleCache() {
+		final String name = "name";
+		final IKeyEvaluator<String, Object> evaluator = Mockito.mock(IKeyEvaluator.class);
+		final int binCount = 21;
+
+		final SimpleCache<String, Object> cache = new SimpleCache<String, Object>(name, evaluator, binCount);
+
+		Assert.assertSame(name, cache.getName());
+
+		Assert.assertSame(evaluator, cache.evaluator);
+
+		Assert.assertEquals(binCount, cache.entries.length);
+		Assert.assertEquals(2, cache.evictionThreshold);
+	}
+
+	@Test
+	public void testSimpleCache2() {
+		final String name = "name";
+		final IKeyEvaluator<String, Object> evaluator = Mockito.mock(IKeyEvaluator.class);
+		final int binCount = 21;
+		final int maxMisses = 30;
+
+		final SimpleCache<String, Object> cache = new SimpleCache<String, Object>(name, evaluator, binCount, maxMisses);
+
+		Assert.assertSame(name, cache.getName());
+
+		Assert.assertSame(evaluator, cache.evaluator);
+
+		Assert.assertEquals(binCount, cache.entries.length);
+		Assert.assertEquals(maxMisses, cache.evictionThreshold);
+
+	}
+
+	@Test
+	public void testSimpleCache3() {
+		final String name = "name";
+		final IKeyEvaluator<String, Object> evaluator = Mockito.mock(IKeyEvaluator.class);
+		final int binCount = 21;
+		final int maxMisses = 30;
+
+		final SimpleCache<String, Object> cache = new SimpleCache<String, Object>(name, evaluator, binCount, maxMisses);
+
+	}
+}
---
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util.tests/src/com/mmxlabs/common/delayedactions/DelayedActionsManagerTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2012
+ * All rights reserved.
+ */
+package com.mmxlabs.common.delayedactions;
+
+import javax.sound.midi.Sequence;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+public class DelayedActionsManagerTest {
+
+	@Test
+	public void testPerformActions() {
+
+		final DelayedActionsManager manager = new DelayedActionsManager();
+
+		final Runnable r1 = Mockito.mock(Runnable.class, "runnable1");
+
+		final Runnable r2 = Mockito.mock(Runnable.class, "runnable2");
+
+		manager.queue(r1);
+		manager.queue(r2);
+
+		manager.performActions();
+		manager.performActions();
+
+		InOrder inOrder = Mockito.inOrder(r1, r2);
+		inOrder.verify(r1).run();
+		inOrder.verify(r2).run();
+
+		Mockito.verifyNoMoreInteractions(r1, r2);
+	}
+
+	@Test
+	public void testDispose() {
+
+		final DelayedActionsManager manager = new DelayedActionsManager();
+
+		final Runnable r1 = Mockito.mock(Runnable.class, "runnable1");
+		final Runnable r2 = Mockito.mock(Runnable.class, "runnable2");
+
+		manager.queue(r1);
+		manager.queue(r2);
+
+		manager.dispose();
+
+		manager.performActions();
+
+		Mockito.verifyNoMoreInteractions(r1, r2);
+
+	}
+
+}
