4271c6099b094417f53eda9e48dd03bcab4d9bb0
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-03-10 13:46:07 +0000

    Doc, make things final

--- com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/io/FileDeleter.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/io/FileDeleter.java
@@ -14,13 +14,22 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 
 /**
+ * A class to *more* securely erase files over {@link File}{@link #delete(File)}. Note the underlying filesystem is outside of the control of Java and may negate anything done here.
+ * 
  * @see http://www.javafaq.nu/java-example-code-1121.html
- * @author sg
- *
+ * @author Simon Goodall
+ * 
  */
 public final class FileDeleter {
 
-	public static void delete(File file) throws IOException, FileNotFoundException {
+	/**
+	 * Overwrite the contents of the file a few times with zeros, ones and random data before deleting.
+	 * 
+	 * @param file
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	public static void delete(final File file) throws IOException, FileNotFoundException {
 
 		if (!file.exists()) {
 			throw new IllegalStateException("File must exist");
@@ -30,45 +39,31 @@ public final class FileDeleter {
 			throw new IllegalStateException("File must be a regular file");
 		}
 
-		SecureRandom random = new SecureRandom();
-		RandomAccessFile raf = new RandomAccessFile(file, "rws");
+		final SecureRandom random = new SecureRandom();
+		final RandomAccessFile raf = new RandomAccessFile(file, "rws");
 		try {
-			// // reset buffer
-			// {
-			// long length = raf.length();
-			// raf.seek(0);
-			// byte[] bytes = new byte[4096];
-			// int pos = 0;
-			// while (pos < length) {
-			// int len = (int)Math.min(length - pos, bytes.length);
-			// random.nextBytes(bytes);
-			//
-			// raf.write(bytes, pos, len);
-			// pos += bytes.length;
-			// }
-			// }
 
-			FileChannel channel = raf.getChannel();
+			final FileChannel channel = raf.getChannel();
 			try {
-				FileLock lock = channel.lock();
+				final FileLock lock = channel.lock();
 				try {
-					MappedByteBuffer buffer = channel.map(MapMode.READ_WRITE, 0, raf.length());
-					byte[] bytes = new byte[4096];
+					final MappedByteBuffer buffer = channel.map(MapMode.READ_WRITE, 0, raf.length());
+					final byte[] bytes = new byte[4096];
 
-					Arrays.fill(bytes, (byte)0x00);
+					Arrays.fill(bytes, (byte) 0x00);
 					while (buffer.hasRemaining()) {
 						buffer.put(bytes, 0, Math.min(buffer.remaining(), bytes.length));
 					}
 					buffer.force();
 					buffer.rewind();
-					Arrays.fill(bytes, (byte)0xFF);
+					Arrays.fill(bytes, (byte) 0xFF);
 					while (buffer.hasRemaining()) {
 						buffer.put(bytes, 0, Math.min(buffer.remaining(), bytes.length));
 					}
 					buffer.force();
 					buffer.rewind();
 
-					Arrays.fill(bytes, (byte)0xF0);
+					Arrays.fill(bytes, (byte) 0xF0);
 					while (buffer.hasRemaining()) {
 						buffer.put(bytes, 0, Math.min(buffer.remaining(), bytes.length));
 					}
@@ -90,16 +85,16 @@ public final class FileDeleter {
 		} finally {
 			raf.close();
 		}
-		
+
 		file.delete();
 		System.gc();
 	}
 
-	public static void main(String arg[]) throws Exception {
+	public static void main(final String arg[]) throws Exception {
 
-		File f = new File("C:/temp/test.file.txt");
+		final File f = new File("C:/temp/test.file.txt");
 
-		BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(f));
 		for (int i = 0; i < 1000; ++i) {
 			bw.write("Hello!");
 		}
