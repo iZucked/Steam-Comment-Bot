ba34ba3cef89def50f010d75de943fee2909fc8d
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-03-24 15:03:01 +0000

    Fix issue if we hit the max speed

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/components/impl/LookupTableConsumptionRateCalculator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/components/impl/LookupTableConsumptionRateCalculator.java
@@ -47,12 +47,13 @@ public final class LookupTableConsumptionRateCalculator implements IConsumptionR
 		for (int i = 0; i < table.length; i++) {
 			if (table[i] == rate) {
 				return i + minSpeed;
+			} else if (table[i] > rate) {
+				return i > 0 ? i - 1 + minSpeed : minSpeed;
 			}
-			else if (table[i] > rate) {
-				return i>0 ? i-1 + minSpeed : minSpeed;
-			}			
 		}
-		throw new IllegalStateException();		
+		// Hit max speed
+		return minSpeed + table.length;
+		// throw new IllegalStateException();
 	}
 
 	private void init(final IConsumptionRateCalculator calc) {
