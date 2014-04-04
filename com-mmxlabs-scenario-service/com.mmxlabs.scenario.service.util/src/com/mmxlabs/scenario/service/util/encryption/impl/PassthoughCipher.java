6c2d13b0869306346f6e0b20720532ceb99daf4d
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-04-04 07:39:01 -0100

    Add new classes to provide a passthrough encryption scheme (i.e. it does no encryption whatsoever)

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.util/src/com/mmxlabs/scenario/service/util/encryption/impl/PassthoughCipher.java
@@ -0,0 +1,35 @@
+package com.mmxlabs.scenario.service.util.encryption.impl;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.eclipse.emf.ecore.resource.URIConverter.Cipher;
+
+/**
+ * A {@link Cipher} which does nothing. The given {@link InputStream} and {@link OutputStream} are returned unmodified.
+ * 
+ * @author Simon Goodall
+ * 
+ */
+public class PassthoughCipher implements Cipher {
+
+	@Override
+	public void finish(final InputStream inputStream) throws Exception {
+
+	}
+
+	@Override
+	public void finish(final OutputStream outputStream) throws Exception {
+
+	}
+
+	@Override
+	public OutputStream encrypt(final OutputStream outputStream) throws Exception {
+		return outputStream;
+	}
+
+	@Override
+	public InputStream decrypt(final InputStream inputStream) throws Exception {
+		return inputStream;
+	}
+}
