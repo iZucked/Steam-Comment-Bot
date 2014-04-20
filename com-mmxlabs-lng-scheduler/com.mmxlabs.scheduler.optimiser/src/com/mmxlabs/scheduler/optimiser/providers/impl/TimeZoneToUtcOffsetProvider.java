2a92b917b5eaaf43f5c19652c2c3ec0fd8972746
berkan <beskikaya@mmxlabs.com>
2014-04-20 07:07:53 -0100

    Using better methods from joda-time

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/providers/impl/TimeZoneToUtcOffsetProvider.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/providers/impl/TimeZoneToUtcOffsetProvider.java
@@ -11,18 +11,26 @@ import com.mmxlabs.scheduler.optimiser.providers.ITimeZoneToUtcOffsetProvider;
  * 
  * Helper class that provides time offsets for translating local times to UTC.
  * 
- * It uses the Joda-Time library, which itself uses the tz database {@link http://www.iana.org/time-zones} for its 
- * calculations. The library is not released as frequently as the tz database -- so we will need to recompile the jar 
- * when the database is updated, see {@link http://www.joda.org/joda-time/tz_update.html}.
+ * There are two main functions: 
+ * 
+ *   UTC(localtime, timezoneId):long  which translates a local time in the given timezone to UTC
+ * 
+ *   getUtcOffset(timezoneId, localtime):int  which returns the localtime - UTC offset in milliseconds
+ * 
+ * 
+ * The calculations are done via the Joda-Time library, which itself uses the tz database {@link http://www.iana.org/time-zones}.
+ * 
+ * (Note that the library is not released as frequently as the tz database -- so we will need to recompile the jar when the database 
+ * is updated, see {@link http://www.joda.org/joda-time/tz_update.html})
  * 
  * At the moment, this class isn't bound in {@link com.mmxlabs.scheduler.optimiser.providers.guice.DataComponentProviderModule} 
  * like the other providers, as we only use the static methods here which call Joda-Time directly. If we need to optimise those 
- * calls, via further caching, or using code from the library internals directly etc, this can still be injected with
- * guice where needed.
+ * calls, via further caching, or using code from the library internals directly etc, this can still be initialised and injected 
+ * with guice where needed.
  * 
  * 
  * TO USE: make sure the joda-time jar/bundle is in place; enable "import org.joda.time.DateTimeZone", and alter the 
- * function getUtcOffset(String timezoneId, int localTime) below.
+ * functions UTC(int localTime, String timezoneId) and getUtcOffset(String timezoneId, int localTime) below.
  * 
  * 
  * @author berkan
@@ -38,45 +46,51 @@ public class TimeZoneToUtcOffsetProvider implements ITimeZoneToUtcOffsetProvider
 
 	/**
 	 * Convenience functions to translate local time at a port to UTC time
+	 *
+	 * The (intended) usage is to replace calls to 
 	 * 
-	 * Example 1: 
-	 * in {@link com.mmxlabs.lingo.shell.extensions.contracts.purchase.peru.PeruPurchaseContract.calculateFOBPricePerMMBTU()}
+	 *   curve.getValueAtPoint(portLocalTime) 
+	 *   
+	 *   with 
+	 *   
+	 *   curve.getValueAtPoint( UTC(portLocalTime,portTimezone) ) 
 	 * 
-	 *	 // Price at completion of loading
-	 *	 final int pricingTime = loadTime + portVisitDurationProvider.getVisitDuration(loadSlot.getPort(),PortType.Load);
-	 * 
-	 * becomes 
-	 * 
-	 *   final int pricingTime = UTC( loadTime + portVisitDurationProvider.getVisitDuration(loadSlot.getPort(),PortType.Load),
-	 *   							  loadSlot );
-	 *   calcPrice( ... );
-
-	 * Example2: 
-	 * 
-	 * Alternatively, in calcPrice ( ... ) at the point of the curve lookups:
-	 *  
-	 *   int HHPrice = HHCurve.getValueAtPoint( pricingTime );
-	 * 
-	 * becomes
-	 * 
-	 *   int HHPrice = HHCurve.getValueAtPoint( UTC(pricingTime,loadSlot) )
+	 * Q: However - these functions need to return long to represent time; curve lookup times are kept in int. What's the right way to remedy the situation? 
 	 * 
 	 * 
 	 * @param local time
 	 * @param port, or a slot at port, or the time zone id of the port
 	 * @return UTC time
 	 */
-	
-	public static int UTC(int localTime, IPort port) {
-		return (int) (localTime - getUtcOffset(port.getTimeZoneId(), localTime));
-	}		
 
-	public static int UTC(int localTime, IPortSlot portSlot) {
-		return UTC(localTime, portSlot.getPort());
+	
+	public static long UTC(int localTime, String timezoneId) {
+		// TODO-1: switch to joda-time when the jar is in place
+		Object tz = new Object();  //DateTimeZone tz = DateTimeZone.forID(timezoneId);
+		
+		// for now, re-throw any exceptions
+		try {
+			// check for illegal/wrong timezoneId
+			if (tz != null) {
+				// TODO-2: switch to using joda-time when the jar is in place
+				return localTime; // return tz.convertLocalToUTC(localTime, true);						
+			}
+			else {
+				// TODO-3: Warn about the wrong timezoneId; currently returning localTime to keep things going
+				return localTime;
+			}
+		} catch (Exception e) {
+			throw(e);
+		}
 	}
 	
-	public static int UTC(int localTime, String timezoneId) {
-		return (int) (localTime - getUtcOffset(timezoneId, localTime));
+	
+	public static long UTC(int localTime, IPort port) {
+		return UTC(localTime, port.getTimeZoneId());
+	}		
+
+	public static long UTC(int localTime, IPortSlot portSlot) {
+		return UTC(localTime, portSlot.getPort().getTimeZoneId());
 	}
 
 
@@ -92,7 +106,7 @@ public class TimeZoneToUtcOffsetProvider implements ITimeZoneToUtcOffsetProvider
 		// for now, see if we're calling this method properly; also re-throw any exceptions
 		try {			
 			//TODO:  switch to using joda-time when the jar is in place
-			return 0; // DateTimeZone.forID(timezoneId).getOffset(localTime);
+			return 0; // DateTimeZone.forID(timezoneId).getOffsetFromLocal(localTime)
 		} catch (Exception e) {
 			throw(e);
 		}		
