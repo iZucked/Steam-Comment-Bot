d9b9005e008d735f82f5885ed545847d6ddbe432
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-04-28 13:29:37 -0100

    Use recorded route cost, rather than relookup.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/RouteCostFitnessComponent.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/RouteCostFitnessComponent.java
@@ -4,29 +4,17 @@
  */
 package com.mmxlabs.scheduler.optimiser.fitness.components;
 
-import javax.inject.Inject;
-
 import com.mmxlabs.optimiser.core.IResource;
 import com.mmxlabs.scheduler.optimiser.Calculator;
-import com.mmxlabs.scheduler.optimiser.components.IVesselClass;
-import com.mmxlabs.scheduler.optimiser.components.VesselState;
 import com.mmxlabs.scheduler.optimiser.fitness.CargoSchedulerFitnessCore;
-import com.mmxlabs.scheduler.optimiser.providers.IRouteCostProvider;
-import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 import com.mmxlabs.scheduler.optimiser.voyage.impl.VoyageDetails;
 
 public class RouteCostFitnessComponent extends AbstractPerRouteSchedulerFitnessComponent {
-
-	@Inject
-	private IRouteCostProvider routeCostProvider;
-	@Inject
-	private IVesselProvider vesselProvider;
 
 	public RouteCostFitnessComponent(final String name, final CargoSchedulerFitnessCore core) {
 		super(name, core);
 	}
 
-	private IVesselClass vesselClass;
 	private long accumulator = 0;
 
 	/*
@@ -36,7 +24,6 @@ public class RouteCostFitnessComponent extends AbstractPerRouteSchedulerFitnessC
 	 */
 	@Override
 	protected boolean reallyStartSequence(final IResource resource) {
-		vesselClass = vesselProvider.getVessel(resource).getVesselClass();
 		accumulator = 0;
 		return true;
 	}
@@ -50,11 +37,7 @@ public class RouteCostFitnessComponent extends AbstractPerRouteSchedulerFitnessC
 	protected boolean reallyEvaluateObject(final Object obj, final int time) {
 		if (obj instanceof VoyageDetails) {
 			final VoyageDetails details = (VoyageDetails) obj;
-			final String route = details.getOptions().getRoute();
-			final VesselState vesselState = details.getOptions().getVesselState();
-			// addDiscountedValue(startTime,
-			// routeCostProvider.getRouteCost(route, vesselClass, vesselState));
-			accumulator += getDiscountedValue(time, routeCostProvider.getRouteCost(route, vesselClass, vesselState));
+			accumulator += getDiscountedValue(time, details.getRouteCost());
 		}
 		return true;
 	}
