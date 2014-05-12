2a15ec0957ffb333e90f4512fbdd382c21123833
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-05-12 08:05:13 -0100

    Add property tester for applicati9n license features

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.license.features/src/com/mmxlabs/license/features/properties/FeatureEnablementPropertyTester.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2013
+ * All rights reserved.
+ */
+package com.mmxlabs.license.features.properties;
+
+import org.apache.shiro.SecurityUtils;
+import org.eclipse.core.expressions.PropertyTester;
+
+public class FeatureEnablementPropertyTester extends PropertyTester {
+	@Override
+	public boolean test(final Object receiver, final String property, final Object[] args, final Object expectedValue) {
+
+		boolean permitted = args.length > 0;
+		for (final Object arg : args) {
+			permitted &= SecurityUtils.getSubject().isPermitted(arg.toString());
+		}
+		return permitted;
+	}
+}
