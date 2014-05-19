e9cf3810dac8e980ef8ad9d2cb6ba47c9aea3c51
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-05-19 13:36:59 -0100

    Set a default client context
    Card #391

--- com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/HeadlessApplication.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/HeadlessApplication.java
@@ -321,13 +321,24 @@ public class HeadlessApplication implements IApplication {
 
 		// ScenarioInstanceMigrator migrator = new ScenarioInstanceMigrator(migrationRegistry);
 		final TestScenarioService scenarioService = new TestScenarioService("headless");
-		String context = instance.getVersionContext();
-		if (context == null || context.isEmpty()) {
-			context = migrationRegistry.getDefaultMigrationContext();
-			instance.setVersionContext(context);
-			instance.setScenarioVersion(0);
+		// Scenario context
+		{
+			String context = instance.getVersionContext();
+			if (context == null || context.isEmpty()) {
+				context = migrationRegistry.getDefaultMigrationContext();
+				instance.setVersionContext(context);
+				instance.setScenarioVersion(0);
+			}
 		}
-
+		// Client context
+		{
+			String context = instance.getClientVersionContext();
+			if (context == null || context.isEmpty()) {
+				context = migrationRegistry.getDefaultClientMigrationContext();
+				instance.setClientVersionContext(context);
+				instance.setClientScenarioVersion(0);
+			}
+		}
 		// migrator.performMigration(ss, instance);
 		scenarioService.setScenarioMigrationService(migrationService);
 
