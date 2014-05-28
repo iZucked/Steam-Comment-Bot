e92f54ed6a2b6fadface66d1b7390c9bd2b01b9d
berkan <beskikaya@minimaxlabs.com>
2014-05-28 10:57:23 -0100

    com-mmxlabs-models-ui: supporting category/bin-based sorting (currently not used)

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/EObjectTableViewerSortingSupport.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.tabular/src/com/mmxlabs/models/ui/tabular/EObjectTableViewerSortingSupport.java
@@ -12,13 +12,15 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
-import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
 import org.eclipse.nebula.jface.gridviewer.GridViewerColumn;
 import org.eclipse.nebula.widgets.grid.GridColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Function;
 import com.mmxlabs.models.util.emfpath.EMFPath;
 
 /**
@@ -28,6 +30,7 @@ import com.mmxlabs.models.util.emfpath.EMFPath;
  * 
  */
 public class EObjectTableViewerSortingSupport {
+	private static final Logger log = LoggerFactory.getLogger(new Object(){}.getClass().getEnclosingClass());
 
 	/**
 	 * Overridding sort order of objects. Any change in column sort order will set this back to null.
@@ -94,6 +97,23 @@ public class EObjectTableViewerSortingSupport {
 		columnSortOrder.clear();
 	}
 
+	//BE category (ie bin) support for sorting
+	private Function<Object,Integer> categoryFunction = null;
+	
+	public void setCategoryFunction(Function<Object,Integer> categoryFunction) {
+		this.categoryFunction = categoryFunction;
+	}
+	
+	public Function<Object,Integer> getCategoryFunction() {
+		return categoryFunction;
+	}
+	
+	private int getCategory(Object object) {
+		return categoryFunction.apply(object);
+	}
+	//BE
+	
+	
 	/**
 	 * Create a {@link ViewerComparator} to install in the {@link EObjectTableViewer} to define the sort order
 	 * 
@@ -101,9 +121,22 @@ public class EObjectTableViewerSortingSupport {
 	 */
 	public ViewerComparator createViewerComparer() {
 		return new ViewerComparator() {
+			@Override //BE for completeness -- this one's not actually used, see compare() below
+			public int category(Object object) { return getCategory(object); }
+			
 			@Override
 			public int compare(final Viewer viewer, final Object leftObject, final Object rightObject) {
+				//BE take the categories (ie bins) into account while
+				if (categoryFunction != null) {
+					int cat1 = getCategory(leftObject);
+					int cat2 = getCategory(rightObject);
 
+					if (cat1 != cat2) {
+						return cat1 - cat2;
+					}
+				}
+				//BE
+				
 				// If there is a fixed sort order use that.
 				if (fixedSortOrder != null) {
 					final int idx1 = fixedSortOrder.indexOf(leftObject);
