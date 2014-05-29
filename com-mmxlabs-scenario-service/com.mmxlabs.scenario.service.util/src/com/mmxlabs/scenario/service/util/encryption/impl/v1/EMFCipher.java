354b046b6aa5cd8282dcf6230373cfdba6fbbdbf
Jenkins <unknown@kiln.example.com>
2014-05-29 11:14:58 +0000

    Update headers

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.util/src/com/mmxlabs/scenario/service/util/encryption/impl/v1/EMFCipher.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.util/src/com/mmxlabs/scenario/service/util/encryption/impl/v1/EMFCipher.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2014
+ * All rights reserved.
+ */
 package com.mmxlabs.scenario.service.util.encryption.impl.v1;
 
 import java.io.BufferedOutputStream;
