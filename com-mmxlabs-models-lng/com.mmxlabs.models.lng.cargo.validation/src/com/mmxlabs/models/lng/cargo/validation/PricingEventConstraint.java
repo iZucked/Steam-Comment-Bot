454bdafa3ad84a0ef24f7232faf0cb19c6b0cf5d
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-07-02 12:57:30 -0100

    Expanded pricing date options
    Validation
    
    BugzID: 920

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/PricingEventConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/PricingEventConstraint.java
@@ -23,6 +23,14 @@ public class PricingEventConstraint extends AbstractModelMultiConstraint {
 	@Override
 	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> failures) {
 		final EObject object = ctx.getTarget();
+		if (object instanceof Slot) {
+			Slot slot = (Slot) object;
+			if (slot.isSetPricingEvent() && slot.getPricingEvent() == null) {
+				final DetailConstraintStatusDecorator status = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Slot|" + slot.getName() + " pricing event not set."));
+				status.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PricingEvent());
+				failures.add(status);
+			}
+		}
 		if (object instanceof Cargo) {
 
 			final Cargo cargo = (Cargo) object;
@@ -36,12 +44,14 @@ public class PricingEventConstraint extends AbstractModelMultiConstraint {
 							final DetailConstraintStatusDecorator status = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Slot|" + slot.getName()
 									+ " cannot be priced on discharge event in a complex cargo."));
 							status.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PricingEvent());
+							failures.add(status);
 						}
 					} else if (slot instanceof DischargeSlot) {
 						if (pricingEvent == PricingEvent.START_LOAD || pricingEvent == PricingEvent.END_LOAD) {
 							final DetailConstraintStatusDecorator status = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Slot|" + slot.getName()
 									+ " cannot be priced on load event in a complex cargo."));
 							status.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PricingEvent());
+							failures.add(status);
 						}
 
 					}
