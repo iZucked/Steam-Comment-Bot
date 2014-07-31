83bdb365143b907b306ee0221e24ab281a745331
Simon McGregor <simon.mcgregor@minimaxlabs.com>
2014-07-31 07:30:55 -0100

    Missing class for custom report generation

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/extensions/EMFReportColumnManager.java
@@ -0,0 +1,61 @@
+package com.mmxlabs.lingo.reports.extensions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.mmxlabs.lingo.reports.components.ColumnType;
+import com.mmxlabs.lingo.reports.components.EMFReportView;
+import com.mmxlabs.lingo.reports.components.EMFReportView.EmfBlockColumnFactory;
+import com.mmxlabs.lingo.reports.components.EMFReportView.SimpleEmfBlockColumnFactory;
+import com.mmxlabs.lingo.reports.views.formatters.IFormatter;
+
+/**
+ * A class to manage columns which are shared by reports of the same report type.
+ * 
+ * Global initialisation code calls the #registerColumn method to associate columns 
+ * with particular report types. 
+ * 
+ * Report code calls the #addColumns method specifying a report type, which adds the
+ * relevant columns to the report. A convenience method allows the report to control 
+ * which columns appear in that report by default.
+ * 
+ * 
+ *   
+ * @author Simon McGregor
+ *
+ */
+public class EMFReportColumnManager {
+	private Map<String, List<EmfBlockColumnFactory>> columnsByType = new HashMap<>();
+	
+	public void registerColumn(final String reportType, final EmfBlockColumnFactory factory) {
+		if (columnsByType.containsKey(reportType) == false) {
+			columnsByType.put(reportType, new ArrayList<EmfBlockColumnFactory>());
+		}
+		columnsByType.get(reportType).add(factory);		
+	}
+	
+	/**
+	 * Adds all registered columns of the given report type to the specified report.
+	 * These columns will default to "not visible".
+	 * 
+	 * @param report
+	 * @param reportType
+	 */
+	public void addColumns(final String reportType, final EMFReportView report) {
+		// code here needs to add all the requisite columns to the specified report
+		// each column will be added to the appropriate managed block of columns on that report
+		if (columnsByType.containsKey(reportType)) {
+			for (EmfBlockColumnFactory factory: columnsByType.get(reportType)) {
+				factory.addColumn(report);
+			}
+		}
+	}
+
+	public void registerColumn(final String reportType, final String title, final ColumnType columnType, final IFormatter formatter, final Object... path) {
+		registerColumn(reportType, new SimpleEmfBlockColumnFactory(title, columnType, formatter, path));
+	}
+	
+
+}
