6335dce32c2c5dd45782ebcddca94c404b8785be
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-08-29 13:09:54 -0100

    Copy GoIntoActionProvider into scenario service app from navigator resources to avoid pulling in eclipse team deps.
    BugzId: 999

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/internal/GoIntoActionProvider.java
@@ -0,0 +1,47 @@
+package com.mmxlabs.scenario.service.ui.internal;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.internal.navigator.framelist.GoIntoAction;
+import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.navigator.CommonViewer;
+import org.eclipse.ui.navigator.ICommonActionExtensionSite;
+
+/**
+ * Copied from org.eclipse.ui.navigator.resources to avoid pulling in all of it's additional deps into the application.
+ * 
+ */
+@SuppressWarnings("restriction")
+public class GoIntoActionProvider extends CommonActionProvider {
+
+	private GoIntoAction goIntoAction;
+
+	@Override
+	public void init(ICommonActionExtensionSite anActionSite) {
+		anActionSite.getViewSite().getShell();
+		CommonViewer viewer = (CommonViewer) anActionSite.getStructuredViewer();
+		goIntoAction = new GoIntoAction(viewer.getFrameList());
+	}
+
+	@Override
+	public void dispose() {
+		goIntoAction.dispose();
+	}
+
+	@Override
+	public void fillActionBars(IActionBars actionBars) {
+		actionBars.setGlobalActionHandler(IWorkbenchActionConstants.GO_INTO, goIntoAction);
+	}
+
+	@Override
+	public void fillContextMenu(IMenuManager menu) {
+		menu.appendToGroup("group.new", goIntoAction); //$NON-NLS-1$
+	}
+
+	@Override
+	public void updateActionBars() {
+		goIntoAction.update();
+	}
+
+}
