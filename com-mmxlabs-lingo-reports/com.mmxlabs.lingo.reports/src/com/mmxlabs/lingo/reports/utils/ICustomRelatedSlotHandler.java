653cb44a1ecb4c0361e4e45a13e0e34a0c4cae5c
Jenkins <unknown@kiln.example.com>
2014-09-08 12:24:30 +0000

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/utils/ICustomRelatedSlotHandler.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/utils/ICustomRelatedSlotHandler.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2014
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.utils;
 
 import com.mmxlabs.models.lng.schedule.CargoAllocation;
