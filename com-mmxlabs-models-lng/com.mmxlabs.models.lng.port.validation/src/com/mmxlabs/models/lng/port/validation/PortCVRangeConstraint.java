eacd22277002587cac323d4b71bcb84923513db2
Alex Churchill <alex.churchill@minimaxlabs.com>
2014-10-08 14:31:57 -0100

    Added Port CV Range Constraint to Optimiser (bad method) BugzId: 940

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.validation/src/com/mmxlabs/models/lng/port/validation/PortCVRangeConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.validation/src/com/mmxlabs/models/lng/port/validation/PortCVRangeConstraint.java
@@ -1,39 +1,75 @@
 package com.mmxlabs.models.lng.port.validation;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.validation.model.IConstraintStatus;
 
 import com.mmxlabs.models.lng.port.Port;
 import com.mmxlabs.models.lng.port.PortPackage;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
-public class PortCVRangeConstraint extends AbstractModelConstraint{
+public class PortCVRangeConstraint extends AbstractModelMultiConstraint{
 
 	@Override
-	public IStatus validate(final IValidationContext ctx) {
+	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> failures) {
 		final EObject target = ctx.getTarget();
 
 		if (target instanceof Port) {
 			final Port port = (Port) target;
-			if (port.isSetMinCvValue() && port.isSetMaxCvValue()) {
-				final double minCvValue = port.getMinCvValue();
-				final double maxCvValue = port.getMaxCvValue();
-			
-				if (minCvValue > maxCvValue) {
-					final String failureMessage = String.format("Port '%s' has minimum CV %.2f greater than maximum CV %.2f", port.getName(), minCvValue, maxCvValue);
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage));
-					dsd.addEObjectAndFeature(port, PortPackage.eINSTANCE.getPort_MinCvValue());
-					dsd.addEObjectAndFeature(port, PortPackage.eINSTANCE.getPort_MaxCvValue());
-					return dsd;
-				}
-
+			DetailConstraintStatusDecorator rangeCheckDSD = checkRange(port,ctx);
+			if (rangeCheckDSD != null){
+				failures.add(rangeCheckDSD);
+			}
+			DetailConstraintStatusDecorator minCheck = checkNonNegative(port, PortPackage.Literals.PORT__MIN_CV_VALUE, ctx);
+			if (minCheck != null){
+				failures.add(minCheck);
+			}
+			DetailConstraintStatusDecorator maxCheck = checkNonNegative(port, PortPackage.Literals.PORT__MAX_CV_VALUE, ctx);
+			if (maxCheck != null){
+				failures.add(maxCheck);
 			}
 		}
 		
-		return ctx.createSuccessStatus();
+		return Activator.PLUGIN_ID;
+	}
+	
+	private DetailConstraintStatusDecorator checkRange(Port port, final IValidationContext ctx){
+		if (port.isSetMinCvValue() && port.isSetMaxCvValue()) {
+			final double minCvValue = port.getMinCvValue();
+			final double maxCvValue = port.getMaxCvValue();
+		
+			if (minCvValue > maxCvValue) {
+				final String failureMessage = String.format("Port '%s' has minimum CV %.2f greater than maximum CV %.2f", port.getName(), minCvValue, maxCvValue);
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage));
+				dsd.addEObjectAndFeature(port, PortPackage.eINSTANCE.getPort_MinCvValue());
+				dsd.addEObjectAndFeature(port, PortPackage.eINSTANCE.getPort_MaxCvValue());
+				return dsd;
+			}
+
+		}
+		return null;
+	}
+
+	/*
+	 * Check non zero field values but handled in the emf attribute bounds
+	 */
+	private DetailConstraintStatusDecorator checkNonNegative(Port port, EStructuralFeature feature, final IValidationContext ctx){
+		if (port.eIsSet(feature)) {
+			final double value = (Double) port.eGet(feature);
+			if (value < 0) {
+				final String failureMessage = String.format("Port '%s' has %s less than 0", port.getName(), feature.getName());
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage));
+				dsd.addEObjectAndFeature(port, feature);
+				return dsd;
+			}
+		}
+		return null;
 	}
 
 }
