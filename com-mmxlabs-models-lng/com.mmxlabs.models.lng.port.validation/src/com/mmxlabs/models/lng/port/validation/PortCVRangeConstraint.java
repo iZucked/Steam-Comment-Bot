ff3ba160db645c086b2c906b8be475cafb53076b
Alex Churchill <alex.churchill@minimaxlabs.com>
2014-10-21 12:48:51 -0100

    Fixed formatting

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.validation/src/com/mmxlabs/models/lng/port/validation/PortCVRangeConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.validation/src/com/mmxlabs/models/lng/port/validation/PortCVRangeConstraint.java
@@ -14,7 +14,7 @@ import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
-public class PortCVRangeConstraint extends AbstractModelMultiConstraint{
+public class PortCVRangeConstraint extends AbstractModelMultiConstraint {
 
 	@Override
 	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> failures) {
@@ -22,28 +22,28 @@ public class PortCVRangeConstraint extends AbstractModelMultiConstraint{
 
 		if (target instanceof Port) {
 			final Port port = (Port) target;
-			DetailConstraintStatusDecorator rangeCheckDSD = checkRange(port,ctx);
-			if (rangeCheckDSD != null){
+			DetailConstraintStatusDecorator rangeCheckDSD = checkRange(port, ctx);
+			if (rangeCheckDSD != null) {
 				failures.add(rangeCheckDSD);
 			}
 			DetailConstraintStatusDecorator minCheck = checkNonNegative(port, PortPackage.Literals.PORT__MIN_CV_VALUE, ctx);
-			if (minCheck != null){
+			if (minCheck != null) {
 				failures.add(minCheck);
 			}
 			DetailConstraintStatusDecorator maxCheck = checkNonNegative(port, PortPackage.Literals.PORT__MAX_CV_VALUE, ctx);
-			if (maxCheck != null){
+			if (maxCheck != null) {
 				failures.add(maxCheck);
 			}
 		}
-		
+
 		return Activator.PLUGIN_ID;
 	}
-	
-	private DetailConstraintStatusDecorator checkRange(Port port, final IValidationContext ctx){
+
+	private DetailConstraintStatusDecorator checkRange(Port port, final IValidationContext ctx) {
 		if (port.isSetMinCvValue() && port.isSetMaxCvValue()) {
 			final double minCvValue = port.getMinCvValue();
 			final double maxCvValue = port.getMaxCvValue();
-		
+
 			if (minCvValue > maxCvValue) {
 				final String failureMessage = String.format("Port '%s' has minimum CV %.2f greater than maximum CV %.2f", port.getName(), minCvValue, maxCvValue);
 				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(failureMessage));
@@ -59,7 +59,7 @@ public class PortCVRangeConstraint extends AbstractModelMultiConstraint{
 	/*
 	 * Check non zero field values but handled in the emf attribute bounds
 	 */
-	private DetailConstraintStatusDecorator checkNonNegative(Port port, EStructuralFeature feature, final IValidationContext ctx){
+	private DetailConstraintStatusDecorator checkNonNegative(Port port, EStructuralFeature feature, final IValidationContext ctx) {
 		if (port.eIsSet(feature)) {
 			final double value = (Double) port.eGet(feature);
 			if (value < 0) {
