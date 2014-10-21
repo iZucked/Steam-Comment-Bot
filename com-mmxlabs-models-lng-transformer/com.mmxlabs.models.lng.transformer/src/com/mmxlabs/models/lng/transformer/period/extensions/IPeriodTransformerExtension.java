6b73cb4d3a8612e8f45ec3b67d95d6c6f7c8cd39
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-10-21 08:23:40 -0100

    Define a service interface for period transformer extensions.
    BugzId: 922

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/period/extensions/IPeriodTransformerExtension.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.models.lng.transformer.period.extensions;
+
+import java.util.List;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.mmxlabs.models.lng.cargo.Slot;
+
+public interface IPeriodTransformerExtension {
+
+	
+	@Nullable List<Slot> getExtraDependenciesForSlot(Slot slot);
+}
