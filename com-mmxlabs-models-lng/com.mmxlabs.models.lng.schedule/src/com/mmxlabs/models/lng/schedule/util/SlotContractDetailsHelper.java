9efe41e303b19fc4ea0071f22bb6577ecb59c0d3
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-11-25 10:32:23 +0000

    Add helper class to find schedule slot contract details.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule/src/com/mmxlabs/models/lng/schedule/util/SlotContractDetailsHelper.java
@@ -0,0 +1,64 @@
+package com.mmxlabs.models.lng.schedule.util;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.schedule.CargoAllocation;
+import com.mmxlabs.models.lng.schedule.GeneralPNLDetails;
+import com.mmxlabs.models.lng.schedule.SlotAllocation;
+import com.mmxlabs.models.lng.schedule.SlotPNLDetails;
+
+public final class SlotContractDetailsHelper {
+
+	@Nullable
+	public static <T extends GeneralPNLDetails> T findSlotContractDetails(@NonNull final SlotAllocation slotAllocation, @NonNull final Class<T> detailsCls) {
+		final Slot modelSlot = slotAllocation.getSlot();
+		final CargoAllocation cargoAllocation = slotAllocation.getCargoAllocation();
+		if (modelSlot != null && cargoAllocation != null) {
+			return findSlotContractDetails(modelSlot, cargoAllocation, detailsCls);
+		}
+		return null;
+	}
+
+	@Nullable
+	public static <T extends GeneralPNLDetails> T findSlotContractDetails(@NonNull final Slot modelSlot, final @NonNull CargoAllocation cargoAllocation, @NonNull final Class<T> detailsCls) {
+
+		SlotPNLDetails slotDetails = null;
+		for (final GeneralPNLDetails generalPNLDetails : cargoAllocation.getGeneralPNLDetails()) {
+			if (generalPNLDetails instanceof SlotPNLDetails) {
+				final SlotPNLDetails slotPNLDetails = (SlotPNLDetails) generalPNLDetails;
+				if (slotPNLDetails.getSlot() == modelSlot) {
+					slotDetails = slotPNLDetails;
+				}
+			}
+		}
+		if (slotDetails == null) {
+			return null;
+		}
+
+		for (final GeneralPNLDetails details : slotDetails.getGeneralPNLDetails()) {
+			if (detailsCls.isInstance(details)) {
+				return detailsCls.cast(details);
+			}
+		}
+		return null;
+	}
+
+	@Nullable
+	public static <T extends GeneralPNLDetails> T findSlotContractDetails(final @NonNull CargoAllocation cargoAllocation, @NonNull final Class<T> detailsCls) {
+
+		for (final GeneralPNLDetails generalPNLDetails : cargoAllocation.getGeneralPNLDetails()) {
+			if (generalPNLDetails instanceof SlotPNLDetails) {
+				final SlotPNLDetails slotPNLDetails = (SlotPNLDetails) generalPNLDetails;
+				for (final GeneralPNLDetails details : slotPNLDetails.getGeneralPNLDetails()) {
+					if (detailsCls.isInstance(details)) {
+						return detailsCls.cast(details);
+					}
+				}
+			}
+		}
+
+		return null;
+	}
+}
