b6561b0055cb5607bdfbadd8d7f0d853d73bc90d
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-11-25 08:44:01 +0000

    Validation: Missing CV check for slot with inherited CV of 0.0
    Re-implement constraint to work with final CV value.
    
    BugzId: 1214

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/LoadSlotCVConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/LoadSlotCVConstraint.java
@@ -4,29 +4,51 @@
  */
 package com.mmxlabs.models.lng.cargo.validation;
 
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
 
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
-import com.mmxlabs.models.ui.validation.AbstractFeatureRangeConstraint;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
-public class LoadSlotCVConstraint extends AbstractFeatureRangeConstraint {
-
-	@Override
-	protected boolean shouldValidateFeature(EObject object,
-			EStructuralFeature feature) {
-		return object instanceof LoadSlot;
-	}
+public class LoadSlotCVConstraint extends AbstractModelMultiConstraint {
 
 	@Override
-	protected String getPluginId() {
+	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> failures) {
+		final EObject target = ctx.getTarget();
+
+		if (target instanceof LoadSlot) {
+			final LoadSlot slot = (LoadSlot) target;
+			DetailConstraintStatusDecorator rangeCheckDSD = checkCVRange(slot, ctx);
+			if (rangeCheckDSD != null) {
+				failures.add(rangeCheckDSD);
+			}
+		}
+
 		return Activator.PLUGIN_ID;
 	}
 
-	@Override
-	protected void createConstraints() {
-		setRange(CargoPackage.Literals.LOAD_SLOT__CARGO_CV, 1.0, 40.0, "CV");	
+	private DetailConstraintStatusDecorator checkCVRange(LoadSlot slot, final IValidationContext ctx) {
+		double cv = slot.getSlotOrDelegatedCV();
+		if (cv < 1.0) {
+			final String message = String.format("Slot|%s CV is %.2f (should be greater than 1.0)", slot.getName(), cv);
+			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+			dcsd.addEObjectAndFeature(slot, CargoPackage.Literals.LOAD_SLOT__CARGO_CV);
+			return dcsd;
+		} else if (cv > 40.0) {
+			final String message = String.format("Slot|%s CV is %.2f (should be less than 40.0)", slot.getName(), cv);
+			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+			dcsd.addEObjectAndFeature(slot, CargoPackage.Literals.LOAD_SLOT__CARGO_CV);
+			return dcsd;
+		}
+		return null;
 	}
+
 }
