57673e68d213e24d89d4efcaec6eadf6fb4440a9
Alex Churchill <alex.churchill@minimaxlabs.com>
2014-12-10 10:19:12 +0000

    SP1.5 Rebase model changes

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.edit/src/com/mmxlabs/models/lng/pricing/provider/PortsSplitExpressionMapItemProvider.java
@@ -0,0 +1,192 @@
+/**
+ */
+package com.mmxlabs.models.lng.pricing.provider;
+
+
+import com.mmxlabs.models.lng.pricing.PortsSplitExpressionMap;
+import com.mmxlabs.models.lng.pricing.PricingPackage;
+
+import com.mmxlabs.models.mmxcore.provider.MMXObjectItemProvider;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link com.mmxlabs.models.lng.pricing.PortsSplitExpressionMap} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PortsSplitExpressionMapItemProvider
+	extends MMXObjectItemProvider
+	implements
+		IEditingDomainItemProvider,
+		IStructuredItemContentProvider,
+		ITreeItemContentProvider,
+		IItemLabelProvider,
+		IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PortsSplitExpressionMapItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addPortsPropertyDescriptor(object);
+			addExpression1PropertyDescriptor(object);
+			addExpression2PropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Ports feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addPortsPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_PortsSplitExpressionMap_ports_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_PortsSplitExpressionMap_ports_feature", "_UI_PortsSplitExpressionMap_type"),
+				 PricingPackage.Literals.PORTS_SPLIT_EXPRESSION_MAP__PORTS,
+				 true,
+				 false,
+				 true,
+				 null,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Expression1 feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addExpression1PropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_PortsSplitExpressionMap_expression1_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_PortsSplitExpressionMap_expression1_feature", "_UI_PortsSplitExpressionMap_type"),
+				 PricingPackage.Literals.PORTS_SPLIT_EXPRESSION_MAP__EXPRESSION1,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Expression2 feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addExpression2PropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_PortsSplitExpressionMap_expression2_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_PortsSplitExpressionMap_expression2_feature", "_UI_PortsSplitExpressionMap_type"),
+				 PricingPackage.Literals.PORTS_SPLIT_EXPRESSION_MAP__EXPRESSION2,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns PortsSplitExpressionMap.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/PortsSplitExpressionMap"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((PortsSplitExpressionMap)object).getExpression1();
+		return label == null || label.length() == 0 ?
+			getString("_UI_PortsSplitExpressionMap_type") :
+			getString("_UI_PortsSplitExpressionMap_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(PortsSplitExpressionMap.class)) {
+			case PricingPackage.PORTS_SPLIT_EXPRESSION_MAP__EXPRESSION1:
+			case PricingPackage.PORTS_SPLIT_EXPRESSION_MAP__EXPRESSION2:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+}
