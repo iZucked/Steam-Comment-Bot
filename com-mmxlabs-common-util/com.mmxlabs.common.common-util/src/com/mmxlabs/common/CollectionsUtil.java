72fac748efbe45e0062e70b4fc3d2e426969072e
Simon Goodall <simon.goodall@minimaxlabs.com>
2014-12-11 12:46:37 +0000

    Various null analysis related changes.

--- com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/CollectionsUtil.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/CollectionsUtil.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.jdt.annotation.NonNull;
+
 /**
  * Utility class for to create java.util Collections objects.
  * 
@@ -30,7 +32,7 @@ public final class CollectionsUtil {
 	 * @param integers
 	 * @return
 	 */
-	public static int[] integersToIntArray(final Collection<Integer> integers) {
+	public static @NonNull int[] integersToIntArray(@NonNull final Collection<Integer> integers) {
 		final int[] result = new int[integers.size()];
 		int ix = 0;
 		for (final int x : integers) {
@@ -45,7 +47,7 @@ public final class CollectionsUtil {
 	 * @param longs
 	 * @return
 	 */
-	public static long[] longsToLongArray(final Collection<Long> longs) {
+	public static @NonNull long[] longsToLongArray(@NonNull final Collection<Long> longs) {
 		final long[] result = new long[longs.size()];
 		int ix = 0;
 		for (final long x : longs) {
@@ -60,7 +62,7 @@ public final class CollectionsUtil {
 	 * @param a
 	 * @return
 	 */
-	public static ArrayList<Integer> toArrayList(final int[] a) {
+	public static @NonNull ArrayList<Integer> toArrayList(@NonNull final int[] a) {
 		final ArrayList<Integer> list = new ArrayList<Integer>(a.length);
 		for (final int v : a) {
 			list.add(v);
@@ -74,7 +76,8 @@ public final class CollectionsUtil {
 	 * @param a
 	 * @return
 	 */
-	public static final ArrayList<Long> toArrayList(final long[] a) {
+	@NonNull
+	public static final ArrayList<Long> toArrayList(@NonNull final long[] a) {
 		final ArrayList<Long> list = new ArrayList<Long>(a.length);
 		for (final long v : a) {
 			list.add(v);
@@ -88,7 +91,7 @@ public final class CollectionsUtil {
 	 * @param a
 	 * @return
 	 */
-	public static ArrayList<Short> toArrayList(final short[] a) {
+	public static @NonNull ArrayList<Short> toArrayList(@NonNull final short[] a) {
 		final ArrayList<Short> list = new ArrayList<Short>(a.length);
 		for (final short v : a) {
 			list.add(v);
@@ -103,6 +106,7 @@ public final class CollectionsUtil {
 	 * @param sequences
 	 * @return
 	 */
+	@NonNull
 	public static <T> List<T> makeArrayList(final T... elements) {
 
 		final List<T> collection = new ArrayList<T>(elements.length);
@@ -121,7 +125,8 @@ public final class CollectionsUtil {
 	 * @param sequences
 	 * @return
 	 */
-	public static <T> List<T> makeArrayList2(final Class<T> cls, final T... elements) {
+	@NonNull
+	public static <T> List<T> makeArrayList2(@NonNull final Class<T> cls, final T... elements) {
 
 		final List<T> collection = new ArrayList<T>(elements.length);
 
@@ -185,23 +190,22 @@ public final class CollectionsUtil {
 		return defaultValue;
 	}
 
-
 	public static class Sets {
 		public static <T> Set<T> merge(Set<T>... sets) {
 			Set<T> merged = new HashSet<T>();
 			for (Set<T> set : sets) {
 				merged.addAll(set);
-			}			
-			return merged;			
+			}
+			return merged;
 		}
 	}
-	
+
 	public static class ASet {
-		public static <T> Set<T> of(T ... items) {
+		public static <T> Set<T> of(T... items) {
 			Set<T> set = new HashSet<T>();
 			set.addAll(Arrays.asList(items));
 			return set;
 		}
-	}	
+	}
 
 }
