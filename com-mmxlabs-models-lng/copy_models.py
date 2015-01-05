ca7f2da59fd04d4e189ac1c4ed5adb0fca31d5ee
Alex Churchill <alex.churchill@minimaxlabs.com>
2015-01-05 12:30:39 +0000

    Set up for migration 22

---
+++ com-mmxlabs-models-lng/copy_models.py
@@ -0,0 +1,31 @@
+import shutil, os
+#####################################
+path_to_ws = os.getcwd()
+version = 22
+#####################################
+
+root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
+files = ["port",
+		 "pricing",
+		 "fleet",
+		 "commercial",
+		 "spotmarkets",
+		 "cargo",
+		 "schedule",
+		 "analytics",
+		 "parameters",
+		 ]
+for f in files:
+	source = "{0}{1}/model/{1}.ecore".format(root,f)
+	dest = "{0}{1}/model/{1}-v{2}.ecore".format(root,f,version)
+	shutil.copyfile(source,dest)
+
+# special cases
+f = "lngtypes"
+source = "{0}types/model/{1}.ecore".format(root,f)
+dest = "{0}types/model/{1}-v{2}.ecore".format(root,f,version)
+shutil.copyfile(source,dest)
+f = "scenario"
+source = "{0}{1}.model/model/{1}.ecore".format(root,f)
+dest = "{0}{1}.model/model/{1}-v{2}.ecore".format(root,f,version)
+shutil.copyfile(source,dest)
