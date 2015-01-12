f5f50c93e9bbe615bd9bdb9d65bbf25ca1db85e4
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-01-12 12:03:45 +0000

    Check spot count. BugzId: 1112

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/CargoSpotCharterConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/CargoSpotCharterConstraint.java
@@ -9,12 +9,15 @@ import java.util.List;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+
 import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
 import com.mmxlabs.models.lng.spotmarkets.CharterInMarket;
 import com.mmxlabs.models.lng.types.VesselAssignmentType;
-import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
 /**
@@ -22,25 +25,6 @@ import com.mmxlabs.models.ui.validation.IExtraValidationContext;
  * 
  */
 public class CargoSpotCharterConstraint extends AbstractModelMultiConstraint {
-
-	private boolean isMatchingSpotMarketExist(final MMXRootObject rootObject, final CharterInMarket vc) {
-//		if (rootObject instanceof LNGScenarioModel) {
-//			final SpotMarketsModel spotModel = ((LNGScenarioModel) rootObject).getSpotMarketsModel();
-//			if (spotModel == null) {
-//				return false;
-//			}
-//			for (final CharterInMarket market : spotModel.getCharterInMarkets()) {
-//				if (!market.isEnabled()) {
-//					continue;
-//				}
-//
-//				if (market.getVesselClass().equals(vc)) {
-//					return true;
-//				}
-//			}
-//		}
-		return true;
-	}
 
 	@Override
 	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> statuses) {
@@ -50,13 +34,13 @@ public class CargoSpotCharterConstraint extends AbstractModelMultiConstraint {
 			final VesselAssignmentType vesselAssignmentType = cargo.getVesselAssignmentType();
 			if (vesselAssignmentType instanceof CharterInMarket) {
 				final CharterInMarket charterInMarket = (CharterInMarket) vesselAssignmentType;
-				// Commented out as part of BugzId:  1112 - spot market should always exist as it is part of the assignment. However we may wish to check the spot count.
-//				if (isMatchingSpotMarketExist(extraContext.getRootObject(), charterInMarket) == false) {
-//					final String message = String.format("No charter market provides %s vessels as required by cargo '%s'", charterInMarket.getName(), cargo.getName());
-//					final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-//					dcsd.addEObjectAndFeature(target, CargoPackage.Literals.ASSIGNABLE_ELEMENT__VESSEL_ASSIGNMENT_TYPE);
-//					statuses.add(dcsd);
-//				}
+				if (cargo.getSpotIndex() >= charterInMarket.getSpotCharterCount()) {
+					final String message = String.format("Cargo '%s'| Spot option is higher than the number of options available for charter in market (%s).", cargo.getName(),
+							charterInMarket.getName());
+					final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+					dcsd.addEObjectAndFeature(target, CargoPackage.Literals.ASSIGNABLE_ELEMENT__VESSEL_ASSIGNMENT_TYPE);
+					statuses.add(dcsd);
+				}
 			}
 		}
 		return Activator.PLUGIN_ID;
