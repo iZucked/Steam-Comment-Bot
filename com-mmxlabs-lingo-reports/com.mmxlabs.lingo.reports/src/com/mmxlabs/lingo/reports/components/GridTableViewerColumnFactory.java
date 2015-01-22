c85b642beb801f97b9ced6362e9c7652a6b85f40
Jenkins <unknown@kiln.example.com>
2015-01-22 17:06:32 +0000

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/components/GridTableViewerColumnFactory.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/components/GridTableViewerColumnFactory.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2014
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.components;
 
 import org.eclipse.core.databinding.observable.map.IObservableMap;
--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/schedule/diffprocessors/IDiffProcessor.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/schedule/diffprocessors/IDiffProcessor.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2014
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.views.schedule.diffprocessors;
 
 import java.util.List;
