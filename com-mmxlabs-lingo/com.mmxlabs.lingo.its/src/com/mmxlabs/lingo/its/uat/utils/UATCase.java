dc06d2dec2f3cc086b7ad64d86e32060dba2a7a4
Alex Churchill <alex.churchill@minimaxlabs.com>
2015-01-27 11:17:36 +0000

    Updated testing framework

---
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/uat/utils/UATCase.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.lingo.its.uat.utils;
+
+public class UATCase {
+
+	public String lingoFilePath;
+	public String cargoName;
+	
+	public UATCase(String lingoFilePath, String cargoName) {
+		this.lingoFilePath = lingoFilePath;
+		this.cargoName = cargoName;
+	}
+
+}
