9a581ead0faf7572f586156fb6b17b298094a889
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-01-27 16:20:42 +0000

    Create various custom exception types and adapter factories to convert to a IReasonProvider instance to display user friendlier messages on failure to load & open scenarios.
    BugzId: 1271

---
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration.scenario/src/com/mmxlabs/models/migration/scenario/FinalClientMigratedVersionMismatchException.java
@@ -0,0 +1,32 @@
+package com.mmxlabs.models.migration.scenario;
+
+public class FinalClientMigratedVersionMismatchException extends ScenarioMigrationException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final String clientContext;
+	private final int expectedVersion;
+	private final int actualVersion;
+
+	public FinalClientMigratedVersionMismatchException(final String clientContext, final int expectedVersion, final int actualVersion) {
+		super();
+		this.clientContext = clientContext;
+		this.expectedVersion = expectedVersion;
+		this.actualVersion = actualVersion;
+	}
+
+	public String getClientContext() {
+		return clientContext;
+	}
+
+	public int getExpectedVersion() {
+		return expectedVersion;
+	}
+
+	public int getActualVersion() {
+		return actualVersion;
+	}
+
+}
