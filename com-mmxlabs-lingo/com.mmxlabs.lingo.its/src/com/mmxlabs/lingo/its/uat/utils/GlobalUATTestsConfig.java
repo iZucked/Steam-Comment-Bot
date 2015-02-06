d8a5b41856c9d9d916ddd33b7a71d45a5c11385e
Alex Churchill <alex.churchill@minimaxlabs.com>
2015-02-06 14:14:00 +0000

    Turn test writing off

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/uat/utils/GlobalUATTestsConfig.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/uat/utils/GlobalUATTestsConfig.java
@@ -1,7 +1,7 @@
 package com.mmxlabs.lingo.its.uat.utils;
 
 public class GlobalUATTestsConfig {
-	public static boolean WRITE_PROPERTIES = true;
+	public static boolean WRITE_PROPERTIES = false;
 	public static boolean INCLUDE_GROUP_PROFIT_LOSS_DETAILS = true;
 	public static boolean INCLUDE_SLOT_DETAILS = true;
 	public static boolean INCLUDE_EVENT_FUEL_DETAILS = true;
