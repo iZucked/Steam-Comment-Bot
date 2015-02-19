44e1eafa306c180b9925fcd3ab86e9a91cd534fa
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-02-19 13:57:29 +0000

    Restrict cell editing to just the UserGroup
    BugzId: 1323

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DataModelCellEditingSupport.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DataModelCellEditingSupport.java
@@ -12,43 +12,34 @@ import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
-import com.mmxlabs.lingo.reports.views.schedule.model.CycleGroup;
-import com.mmxlabs.lingo.reports.views.schedule.model.Row;
 import com.mmxlabs.lingo.reports.views.schedule.model.ScheduleReportPackage;
 import com.mmxlabs.lingo.reports.views.schedule.model.UserGroup;
 
 public class DataModelCellEditingSupport extends ObservableValueEditingSupport {
-	private final ColumnViewer columnViewer;
 	private final TextCellEditor cellEditor;
-
-	// private IObservableMap[] attributeMaps;
 
 	public DataModelCellEditingSupport(final ColumnViewer viewer, final DataBindingContext dbc) {
 		super(viewer, dbc);
-		this.columnViewer = viewer;
-
 		cellEditor = new TextCellEditor((Composite) viewer.getControl());
-		//
-		// attributeMaps = new IObservableMap[3];
-		// attributeMaps[0] = EMFProperties.value(ScheduleReportPackage.Literals.USER_GROUP__COMMENT).observeDetail(cp.getKnownElements());
-		// attributeMaps[1] = EMFProperties.value(ScheduleReportPackage.Literals.CYCLE_GROUP__INDEX).observeDetail(cp.getKnownElements());
-		// attributeMaps[2] = EMFProperties.value(ScheduleReportPackage.Literals.ROW__NAME).observeDetail(cp.getKnownElements());
 	}
 
 	@Override
 	protected IObservableValue doCreateCellEditorObservable(final CellEditor cellEditor) {
 		return SWTObservables.observeText(cellEditor.getControl(), SWT.Modify);
-		// return Observables.observeSingleSelection(columnViewer);
+	}
+
+	@Override
+	protected boolean canEdit(Object element) {
+		if (element instanceof UserGroup) {
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	protected IObservableValue doCreateElementObservable(final Object element, final ViewerCell cell) {
 		if (element instanceof UserGroup) {
 			return EMFProperties.value(ScheduleReportPackage.Literals.USER_GROUP__COMMENT).observe(element);
-		} else if (element instanceof CycleGroup) {
-			return EMFProperties.value(ScheduleReportPackage.Literals.CYCLE_GROUP__INDEX).observe(element);
-		} else if (element instanceof Row) {
-			return EMFProperties.value(ScheduleReportPackage.Literals.ROW__NAME).observe(element);
 		}
 		return null;
 	}
