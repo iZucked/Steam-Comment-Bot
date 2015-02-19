ca8ce471e5b15aa07f7fdcaa083d92446304b7a4
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-02-19 13:57:45 +0000

    Improve diff view sort order.
    BugzId: 1323

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DiffGroupViewerComparator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DiffGroupViewerComparator.java
@@ -5,6 +5,7 @@ import org.eclipse.jface.viewers.ViewerComparator;
 
 import com.mmxlabs.lingo.reports.diff.utils.PNLDeltaUtils;
 import com.mmxlabs.lingo.reports.views.schedule.model.CycleGroup;
+import com.mmxlabs.lingo.reports.views.schedule.model.Row;
 import com.mmxlabs.lingo.reports.views.schedule.model.UserGroup;
 
 public final class DiffGroupViewerComparator extends ViewerComparator {
@@ -32,7 +33,31 @@ public final class DiffGroupViewerComparator extends ViewerComparator {
 		if (e1 instanceof CycleGroup && e2 instanceof CycleGroup) {
 			final CycleGroup g1 = (CycleGroup) e1;
 			final CycleGroup g2 = (CycleGroup) e2;
+
+			final int c = g2.getChangeType().ordinal() - g1.getChangeType().ordinal();
+			if (c != 0) {
+				return c;
+			}
+
 			return PNLDeltaUtils.getPNLDelta(g2) - PNLDeltaUtils.getPNLDelta(g1);
+		}
+
+		if (e1 instanceof Row && e1 instanceof Row) {
+			final Row r1 = (Row) e1;
+			final Row r2 = (Row) e2;
+
+			final int c = r1.getName().compareTo(r2.getName());
+			if (c != 0) {
+				return c;
+			}
+
+			if (r1.isReference() != r2.isReference()) {
+				if (r1.isReference()) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
 		}
 
 		return super.compare(viewer, e1, e2);
