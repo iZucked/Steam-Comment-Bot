f995971d1f61d1d99f04ba0e48eba343bd7d07e9
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-02-27 13:10:18 +0000

    Add extension to rewrite (or remove) a view ID with another one.
    BugzId: 1356

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/application/ViewPartRewriteModule.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2015
+ * All rights reserved.
+ */
+package com.mmxlabs.rcp.common.application;
+
+import static org.ops4j.peaberry.Peaberry.service;
+import static org.ops4j.peaberry.util.TypeLiterals.iterable;
+
+import org.ops4j.peaberry.Peaberry;
+import org.ops4j.peaberry.eclipse.EclipseRegistry;
+import org.osgi.framework.FrameworkUtil;
+
+import com.google.inject.AbstractModule;
+
+public class ViewPartRewriteModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+
+		install(Peaberry.osgiModule(FrameworkUtil.getBundle(ViewPartRewriteModule.class).getBundleContext(), EclipseRegistry.eclipseRegistry()));
+
+		// Extension points
+		bind(iterable(ViewPartRewriteExtension.class)).toProvider(service(ViewPartRewriteExtension.class).multiple());
+	}
+
+}
