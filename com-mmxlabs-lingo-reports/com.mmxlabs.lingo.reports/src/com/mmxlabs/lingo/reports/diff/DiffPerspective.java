30da3d417e8e3b110e085637a6ee9ae5e39b1a18
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-03-06 15:43:05 +0000

    imported patch Diff perspective update

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DiffPerspective.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/DiffPerspective.java
@@ -7,7 +7,9 @@ package com.mmxlabs.lingo.reports.diff;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IViewLayout;
 
+import com.mmxlabs.lingo.reports.views.fleet.ConfigurableFleetReportView;
 import com.mmxlabs.lingo.reports.views.schedule.ConfigurableScheduleReportView;
 import com.mmxlabs.lingo.reports.views.standard.HorizontalKPIReportView;
 
@@ -17,51 +19,35 @@ public class DiffPerspective implements IPerspectiveFactory {
 	public void createInitialLayout(final IPageLayout layout) {
 		layout.addActionSet("com.mmxlabs.lingo.reports.diff.actionset");
 		layout.setEditorAreaVisible(false);
-//		layout.setFixed();
-		
-		final IFolderLayout diffArea = layout.createFolder("diffArea", IPageLayout.LEFT, 0.32f, IPageLayout.ID_EDITOR_AREA);
+
+		final IFolderLayout diffArea = layout.createFolder("diffArea", IPageLayout.LEFT, 0.2f, IPageLayout.ID_EDITOR_AREA);
 		final IFolderLayout ganttArea = layout.createFolder("ganttArea", IPageLayout.BOTTOM, 0.25f, IPageLayout.ID_EDITOR_AREA);
 		final IFolderLayout reportsArea = layout.createFolder("reportsArea", IPageLayout.BOTTOM, 0.65f, IPageLayout.ID_EDITOR_AREA);
-//		final IFolderLayout miscFolder = layout.createFolder("miscFolder", IPageLayout.LEFT, 0.25f, "reportsFolder");
-		// final IFolderLayout KPIBannerFolder = layout.createFolder("KPIBannerFolder", IPageLayout.TOP, 0.05f, "IPageLayout.ID_EDITOR_AREA");
-		// final IFolderLayout cargoEconsFolder = layout.createFolder("cargoEconsFolder", IPageLayout.LEFT, 0.25f, "IPageLayout.ID_EDITOR_AREA");
+		final IFolderLayout fleetReportArea = layout.createFolder("fleetReportsArea", IPageLayout.BOTTOM, 0.65f, "diffArea");
 
 		diffArea.addView("com.mmxlabs.lingo.reports.diff.DiffGroupView");
 		diffArea.addView("com.mmxlabs.scenario.service.ui.navigator");
 		reportsArea.addView(ConfigurableScheduleReportView.ID);
-//		miscFolder.addPlaceholder(TotalsHierarchyView.ID);
-
-		layout.addPlaceholder(HorizontalKPIReportView.ID, IPageLayout.TOP, 0.05f, IPageLayout.ID_EDITOR_AREA);
-
-//		 KPIBannerFolder.addPlaceholder(HorizontalKPIReportView.ID);
-
-		// reportsFolder.addView(KPIReportView.ID);
+		fleetReportArea.addView(ConfigurableFleetReportView.ID);
+		
 		ganttArea.addView("com.mmxlabs.scheduleview.views.SchedulerView");
-//		reportsFolder.addPlaceholder(TotalsReportView.ID);
-//		reportsFolder.addPlaceholder(ConfigurableScheduleReportView.ID);
-//		reportsFolder.addPlaceholder(PortRotationReportView.ID);
-//		reportsFolder.addPlaceholder(LatenessReportView.ID);
-//		reportsFolder.addPlaceholder(CooldownReportView.ID);
-//		reportsFolder.addPlaceholder(FitnessReportView.ID);
-//		reportsFolder.addPlaceholder(CapacityViolationReportView.ID);
 
-		// layout.addView(CargoEconsReport.ID, IPageLayout.RIGHT, 0.85f, IPageLayout.ID_EDITOR_AREA);
+		// Horizontal KPI
+		layout.addStandaloneView(HorizontalKPIReportView.ID, false, IPageLayout.TOP, 0.1f, "reportsArea");
+		final IViewLayout viewLayout = layout.getViewLayout(HorizontalKPIReportView.ID);
+		viewLayout.setCloseable(false);
+		viewLayout.setMoveable(false);
 
-//		layout.addShowViewShortcut(KPIReportView.ID);
-//		layout.addShowViewShortcut(ConfigurableScheduleReportView.ID);
-//		layout.addShowViewShortcut(SchedulerView.ID);
-//		layout.addShowViewShortcut(TotalsReportView.ID);
-//		layout.addShowViewShortcut(TotalsHierarchyView.ID);
-//		layout.addShowViewShortcut(PortRotationReportView.ID);
-//		layout.addShowViewShortcut(LatenessReportView.ID);
-//		layout.addShowViewShortcut(CooldownReportView.ID);
-//		layout.addShowViewShortcut(FitnessReportView.ID);
-//		layout.addShowViewShortcut(CapacityViolationReportView.ID);
-//
-//		layout.addShowViewShortcut("org.eclipse.pde.runtime.LogView");
+		layout.addShowViewShortcut(HorizontalKPIReportView.ID);
+		layout.addShowViewShortcut(ConfigurableScheduleReportView.ID);
+		layout.addShowViewShortcut(ConfigurableFleetReportView.ID);
+		layout.addShowViewShortcut("com.mmxlabs.scheduleview.views.SchedulerView");
+		layout.addShowViewShortcut("com.mmxlabs.lingo.reports.diff.DiffGroupView");
+		layout.addShowViewShortcut("com.mmxlabs.scenario.service.ui.navigator");
 
 		layout.addPerspectiveShortcut("com.mmxlabs.shiplingo.platform.app.perspective.editing");
 		layout.addPerspectiveShortcut("com.mmxlabs.shiplingo.platform.app.perspective.analysis");
+
 	}
 
 }
