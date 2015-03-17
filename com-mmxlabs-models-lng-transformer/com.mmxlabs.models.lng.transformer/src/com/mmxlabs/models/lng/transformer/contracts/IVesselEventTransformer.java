e9c6f57369576407f17bf1e432025cd51639879b
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-03-17 12:09:35 +0000

    Fix potential NPE

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/IVesselEventTransformer.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer/src/com/mmxlabs/models/lng/transformer/contracts/IVesselEventTransformer.java
@@ -5,18 +5,14 @@
 package com.mmxlabs.models.lng.transformer.contracts;
 
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 
-import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.VesselEvent;
 import com.mmxlabs.models.lng.transformer.ITransformerExtension;
-import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
-import com.mmxlabs.scheduler.optimiser.components.IVesselEvent;
 import com.mmxlabs.scheduler.optimiser.components.IVesselEventPortSlot;
 
 public interface IVesselEventTransformer extends ITransformerExtension {
 
-	public void vesselEventTransformed(@Nullable VesselEvent modelEvent, @NonNull IVesselEventPortSlot optimiserEventSlot);
+	public void vesselEventTransformed(@NonNull VesselEvent modelEvent, @NonNull IVesselEventPortSlot optimiserEventSlot);
 	
 	public void vesselEventTransformed(@NonNull IVesselEventPortSlot optimiserEventSlot);
 
