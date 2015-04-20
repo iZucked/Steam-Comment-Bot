c9cda9f1a9b5779e16c6c932dc3ef553f67dec81
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-04-20 12:16:02 -0100

    Fix junit deprecated package use

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.its/src/com/mmxlabs/models/lng/transformer/its/tests/evaluation/SlotOverridesTest.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.its/src/com/mmxlabs/models/lng/transformer/its/tests/evaluation/SlotOverridesTest.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
