ef077273d604a374fe4f61dbeae3a3f92074c15c
Alex Churchill <alex.churchill@minimaxlabs.com>
2015-05-21 16:46:04 -0100

    Model changes caused by new lateness model
    BugzId: 1352

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV30.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2015
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.migration.units;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import com.mmxlabs.models.lng.migration.AbstractMigrationUnit;
+import com.mmxlabs.models.lng.migration.MetamodelVersionsUtil;
+import com.mmxlabs.models.lng.migration.ModelsLNGMigrationConstants;
+import com.mmxlabs.models.migration.PackageData;
+import com.mmxlabs.models.migration.utils.EObjectWrapper;
+import com.mmxlabs.models.migration.utils.MetamodelLoader;
+import com.mmxlabs.models.migration.utils.MetamodelUtils;
+
+public class MigrateToV30 extends AbstractMigrationUnit {
+
+	private MetamodelLoader destinationLoader;
+	private MetamodelLoader sourceLoader;
+
+	@Override
+	public String getScenarioContext() {
+		return ModelsLNGMigrationConstants.Context;
+	}
+
+	@Override
+	public int getScenarioSourceVersion() {
+		return 29;
+	}
+
+	@Override
+	public int getScenarioDestinationVersion() {
+		return 30;
+	}
+
+	@Override
+	protected MetamodelLoader getSourceMetamodelLoader(final Map<URI, PackageData> extraPackages) {
+		if (sourceLoader == null) {
+			sourceLoader = MetamodelVersionsUtil.createV28Loader(extraPackages);
+		}
+		return sourceLoader;
+	}
+
+	@Override
+	protected MetamodelLoader getDestinationMetamodelLoader(final Map<URI, PackageData> extraPackages) {
+		if (destinationLoader == null) {
+			destinationLoader = MetamodelVersionsUtil.createV29Loader(extraPackages);
+		}
+		return destinationLoader;
+	}
+
+	@Override
+	protected void doMigrationWithHelper(final MetamodelLoader metamodelLoader, final EObjectWrapper model) {
+
+	}
+
+}
