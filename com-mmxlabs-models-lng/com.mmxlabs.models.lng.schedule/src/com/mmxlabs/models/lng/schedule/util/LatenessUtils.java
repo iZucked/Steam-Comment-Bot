6070fa4edf868d9863ae80b3b9c2efb323da5075
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-06-10 10:59:22 -0100

    Fix overflow caused by multiplying a long with an int.
    BugzId: 1517

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule/src/com/mmxlabs/models/lng/schedule/util/LatenessUtils.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule/src/com/mmxlabs/models/lng/schedule/util/LatenessUtils.java
@@ -18,62 +18,17 @@ import com.mmxlabs.models.lng.schedule.SlotVisit;
 import com.mmxlabs.models.lng.schedule.VesselEventVisit;
 
 public class LatenessUtils {
-//	public static boolean isLate(Event e) {
-//		if (e instanceof SlotVisit) {
-//			final SlotVisit visit = (SlotVisit) e;
-//			final Sequence seq = visit.getSequence();
-//			if (seq.getSequenceType() == SequenceType.DES_PURCHASE || seq.getSequenceType() == SequenceType.FOB_SALE) {
-//				// ignore load slots as we don't care when the load was performed
-//				if (visit.getSlotAllocation().getSlot() instanceof LoadSlot) {
-//					return false;
-//				}
-//			}
-//			if (visit.getStart().after(visit.getSlotAllocation().getSlot().getWindowEndWithSlotOrPortTime())) {
-//				return true;
-//			}
-//
-//		} else if (e instanceof VesselEventVisit) {
-//			final VesselEventVisit vev = (VesselEventVisit) e;
-//			if (vev.getStart().after(vev.getVesselEvent().getStartBy())) {
-//				return true;
-//			}
-//		} else if (e instanceof PortVisit) {
-//			final PortVisit visit = (PortVisit) e;
-//			final Sequence seq = visit.getSequence();
-//
-//			final VesselAvailability availability = seq.getVesselAvailability();
-//			if (availability == null) {
-//				return false;
-//			}
-//			if (seq.getEvents().indexOf(visit) == 0) {
-//
-//				final Date startBy = availability.getStartBy();
-//				if (startBy != null && visit.getStart().after(startBy)) {
-//					return true;
-//				}
-//
-//			} else if (seq.getEvents().indexOf(visit) == seq.getEvents().size() - 1) {
-//				final Date endBy = availability.getEndBy();
-//				if (endBy != null && visit.getStart().after(endBy)) {
-//					return true;
-//				}
-//			}
-//			// setInputEquivalents(visit, Collections.singleton((Object) visit.getSlotAllocation().getCargoAllocation()));
-//		}
-//		return false;
-//
-//	}
 
-	public static boolean isLate(Event e) {
+	public static boolean isLate(final Event e) {
 		if (e instanceof PortVisit) {
-			PortVisitLateness portVisitLateness = ((PortVisit) e).getLateness();
+			final PortVisitLateness portVisitLateness = ((PortVisit) e).getLateness();
 			if (portVisitLateness != null) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public static Calendar getWindowStartDate(final Object object) {
 		if (object instanceof SlotVisit) {
 			final Date date = ((SlotVisit) object).getSlotAllocation().getSlot().getWindowStartWithSlotOrPortTime();
@@ -139,21 +94,15 @@ public class LatenessUtils {
 		return null;
 	}
 
-	public static String formatLateness(long diff) {
-		// Strip milliseconds
-		diff /= 1000;
-		// Strip seconds;
-		diff /= 60;
-		// Strip
-		diff /= 60;
-		if (diff / 24 == 0) {
-			return String.format("%2dh", diff % 24);
+	public static String formatLatenessHours(final int hours) {
+		if (hours / 24 == 0) {
+			return String.format("%2dh", hours % 24);
 		} else {
-			return String.format("%2dd, %2dh", diff / 24, diff % 24);
+			return String.format("%2dd, %2dh", hours / 24, hours % 24);
 		}
 	}
-	
-	public static long getLatenessInHours(PortVisit portVisit) {
+
+	public static int getLatenessInHours(final PortVisit portVisit) {
 		final PortVisit slotVisit = portVisit;
 		final Calendar localStart = slotVisit.getLocalStart();
 		final Calendar windowEndDate = LatenessUtils.getWindowEndDate(portVisit);
@@ -161,13 +110,15 @@ public class LatenessUtils {
 		long diff = localStart.getTimeInMillis() - windowEndDate.getTimeInMillis();
 
 		// Strip milliseconds
-		diff /= 1000;
+		diff /= 1000L;
 		// Strip seconds;
-		diff /= 60;
+		diff /= 60L;
 		// Strip minutes
-		diff /= 60;
+		diff /= 60L;
 
-		return diff;
+		assert diff == (int) diff;
+
+		return (int) diff;
 	}
 
 }
