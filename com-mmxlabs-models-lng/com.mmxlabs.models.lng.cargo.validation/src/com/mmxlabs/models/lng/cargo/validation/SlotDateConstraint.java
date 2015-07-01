ae9b0283392b2132d1be078d0204a3108987f86a
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-07-01 12:45:24 -0100

    Add missing class for date validation.
    BugzId: 1554

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/SlotDateConstraint.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2015
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.cargo.validation;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
+import org.joda.time.LocalDate;
+
+import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
+import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
+import com.mmxlabs.models.ui.validation.IExtraValidationContext;
+
+public class SlotDateConstraint extends AbstractModelMultiConstraint {
+
+	@Override
+	public String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> statuses) {
+		final EObject object = ctx.getTarget();
+
+		if (object instanceof Slot) {
+			final Slot slot = (Slot) object;
+			LocalDate windowStart = slot.getWindowStart();
+			if (windowStart == null) {
+				final String message;
+				message = String.format("[Slot|'%s'] needs an arrival window set", slot.getName() == null ? "(no ID)" : slot.getName());
+
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
+				dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowStart());
+				statuses.add(dsd);
+			}
+		}
+
+		return Activator.PLUGIN_ID;
+	}
+}
