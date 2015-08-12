dbd220e19756602ebb1f584a1816f2aaf97fdaca
Alex Churchill <alex.churchill@minimaxlabs.com>
2015-08-12 15:25:52 -0100

    Initial work on stochastic breakdowns. Currently mainly commented out.

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/breakdown/Difference.java
@@ -0,0 +1,27 @@
+package com.mmxlabs.models.lng.transformer.ui.breakdown;
+
+import com.mmxlabs.models.lng.transformer.ui.breakdown.ChangeChecker.DifferenceType;
+import com.mmxlabs.optimiser.core.IResource;
+import com.mmxlabs.optimiser.core.ISequenceElement;
+
+public final class Difference {
+	public DifferenceType move;
+	public ISequenceElement load;
+	public ISequenceElement discharge;
+	public IResource resource;
+	
+	public Difference(DifferenceType move, ISequenceElement load, ISequenceElement discharge, IResource resource) {
+		this.move = move;
+		this.load = load;
+		this.discharge = discharge;
+		this.resource = resource;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Difference) {
+		return this.move.equals(((Difference) obj).move) && this.load==((Difference) obj).load && this.discharge==((Difference) obj).discharge && this.resource==((Difference) obj).resource;
+		}
+		return super.equals(obj);
+	}
+}
