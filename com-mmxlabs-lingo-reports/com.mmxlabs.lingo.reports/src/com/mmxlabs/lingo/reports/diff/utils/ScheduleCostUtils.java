07e1f186fccf7eac689f080660aea8a58d5926fa
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-08-24 15:18:53 -0100

    Fix shipping cost calcs.
    BugzId: 1596

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/utils/ScheduleCostUtils.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/diff/utils/ScheduleCostUtils.java
@@ -4,12 +4,19 @@
  */
 package com.mmxlabs.lingo.reports.diff.utils;
 
+import java.util.Set;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
+import com.google.common.collect.Sets;
 import com.mmxlabs.models.lng.schedule.CargoAllocation;
 import com.mmxlabs.models.lng.schedule.Event;
 import com.mmxlabs.models.lng.schedule.EventGrouping;
+import com.mmxlabs.models.lng.schedule.Fuel;
+import com.mmxlabs.models.lng.schedule.FuelQuantity;
+import com.mmxlabs.models.lng.schedule.FuelUsage;
 import com.mmxlabs.models.lng.schedule.Idle;
 import com.mmxlabs.models.lng.schedule.Journey;
 import com.mmxlabs.models.lng.schedule.SlotAllocation;
@@ -74,33 +81,48 @@ public class ScheduleCostUtils {
 	public static Integer calculateLegCost(final EventGrouping grouping) {
 		if (grouping != null) {
 
-			boolean collecting = false;
+			// boolean collecting = false;
 			int total = 0;
 			for (final Event event : grouping.getEvents()) {
 				if (event instanceof SlotVisit) {
 					SlotVisit slotVisit = (SlotVisit) event;
-					total += slotVisit.getFuelCost();
+					// total += slotVisit.getFuelCost();
 					total += slotVisit.getCharterCost();
 					total += slotVisit.getPortCost();
 				} else if (event instanceof Journey) {
 					final Journey journey = (Journey) event;
-					if (collecting) {
-						total += journey.getFuelCost();
-						total += journey.getCharterCost();
-						total += journey.getToll();
-					}
+					// total += journey.getFuelCost();
+					total += journey.getCharterCost();
+					total += journey.getToll();
 				} else if (event instanceof Idle) {
 					final Idle idle = (Idle) event;
-					if (collecting) {
-						total += idle.getFuelCost();
-						total += idle.getCharterCost();
-					}
+					// total += idle.getFuelCost();
+					total += idle.getCharterCost();
 				}
+				if (event instanceof FuelUsage) {
+					final FuelUsage fuelUsage = (FuelUsage) event;
+					total += getFuelCost(fuelUsage, Fuel.BASE_FUEL, Fuel.PILOT_LIGHT);
+				}
+
 			}
 
 			return total;
 		}
 		return null;
+	}
+
+	protected static int getFuelCost(final FuelUsage fuelUser, final Fuel... fuels) {
+		final Set<Fuel> fuelsOfInterest = Sets.newHashSet(fuels);
+		int sum = 0;
+		if (fuelUser != null) {
+			final EList<FuelQuantity> fuelQuantities = fuelUser.getFuels();
+			for (final FuelQuantity fq : fuelQuantities) {
+				if (fuelsOfInterest.contains(fq.getFuel())) {
+					sum += fq.getCost();
+				}
+			}
+		}
+		return sum;
 	}
 
 }
