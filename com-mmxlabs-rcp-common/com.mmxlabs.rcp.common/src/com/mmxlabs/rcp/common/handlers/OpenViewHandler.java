e9970a80b67408563a93633191e33b2939fbff1f
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-09-03 14:44:36 -0100

    Action handler to open views

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/handlers/OpenViewHandler.java
@@ -0,0 +1,20 @@
+package com.mmxlabs.rcp.common.handlers;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
+
+public class OpenViewHandler {
+
+	@Inject
+	EPartService partService;
+
+	@Execute
+	public void execute(@Named("view.id") String viewId) {
+
+		partService.showPart(viewId, PartState.ACTIVATE);
+	}
+}
