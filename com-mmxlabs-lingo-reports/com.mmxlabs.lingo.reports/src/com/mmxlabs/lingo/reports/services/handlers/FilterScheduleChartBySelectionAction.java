b5eb0404cad449786d2e9757a22d4a8c9903175d
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-09-07 10:44:44 -0100

    Re-create the old action sets actions rather than an e4 handler to avoid the issues found running the ITS and some client installs.
    BugzId: 1645

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/services/handlers/FilterScheduleChartBySelectionAction.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2015
+ * All rights reserved.
+ */
+package com.mmxlabs.lingo.reports.services.handlers;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+import com.mmxlabs.lingo.reports.services.EDiffOption;
+import com.mmxlabs.lingo.reports.services.ScenarioComparisonService;
+
+public class FilterScheduleChartBySelectionAction implements IWorkbenchWindowActionDelegate {
+
+	private IWorkbenchWindow window;
+	private IAction action;
+
+	@Override
+	public void init(final IWorkbenchWindow window) {
+		this.window = window;
+		updateActionState();
+	}
+
+	@Override
+	public void dispose() {
+		window = null;
+		action = null;
+	}
+
+	@Override
+	public void run(final IAction action) {
+
+		final ScenarioComparisonService scenarioComparisonService = (ScenarioComparisonService) window.getWorkbench().getService(ScenarioComparisonService.class);
+		if (scenarioComparisonService != null) {
+			scenarioComparisonService.toggleDiffOption(EDiffOption.FILTER_SCHEDULE_CHART_BY_SELECTION);
+			setActionChecked(scenarioComparisonService.getDiffOptions().isFilterSelectedSequences());
+		}
+	}
+
+	@Override
+	public void selectionChanged(final IAction action, final ISelection selection) {
+		this.action = action;
+		updateActionState();
+	}
+
+	private void updateActionState() {
+
+		final ScenarioComparisonService scenarioComparisonService = (ScenarioComparisonService) window.getWorkbench().getService(ScenarioComparisonService.class);
+		if (scenarioComparisonService != null) {
+			setActionChecked(scenarioComparisonService.getDiffOptions().isFilterSelectedSequences());
+		}
+	}
+
+	private void setActionChecked(final boolean checked) {
+		if (action != null) {
+			action.setChecked(checked);
+		}
+	}
+
+}
