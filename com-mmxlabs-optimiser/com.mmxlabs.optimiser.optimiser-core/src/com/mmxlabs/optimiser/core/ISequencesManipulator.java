827e79915b8ef15639169542f0780e64df7bc349
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-11-19 09:01:06 +0000

    Add default method to create and return a manipulated sequences object.

--- com-mmxlabs-optimiser/com.mmxlabs.optimiser.optimiser-core/src/com/mmxlabs/optimiser/core/ISequencesManipulator.java
+++ com-mmxlabs-optimiser/com.mmxlabs.optimiser.optimiser-core/src/com/mmxlabs/optimiser/core/ISequencesManipulator.java
@@ -6,6 +6,8 @@ package com.mmxlabs.optimiser.core;
 
 import org.eclipse.jdt.annotation.NonNull;
 
+import com.mmxlabs.optimiser.core.impl.ModifiableSequences;
+
 /**
  * Interface defining an object to manipulate an {@link IModifiableSequences}. Instances of this interface are likely to replace sequence elements with alternative elements or insert new elements into
  * the sequence. Some examples cases are to change start or end elements based upon the other elements (e.g. change start or end port in a ship scheduling problem) or to replace a group of elements
@@ -22,4 +24,16 @@ public interface ISequencesManipulator {
 	 * @param sequences
 	 */
 	void manipulate(@NonNull IModifiableSequences sequences);
+
+	/**
+	 * Create and return a manipulated sequences object.
+	 * 
+	 * @param rawSequences
+	 * @return
+	 */
+	default @NonNull IModifiableSequences createManipulatedSequences(@NonNull final ISequences rawSequences) {
+		final IModifiableSequences fullSequences = new ModifiableSequences(rawSequences);
+		manipulate(fullSequences);
+		return fullSequences;
+	}
 }
