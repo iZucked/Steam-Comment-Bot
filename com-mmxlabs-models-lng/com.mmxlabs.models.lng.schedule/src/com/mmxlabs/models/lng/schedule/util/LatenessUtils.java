baba263f5bf8bc541dcfab48f3b13eaa507af2c3
Simon Goodall <simon.goodall@minimaxlabs.com>
2015-12-07 11:38:44 +0000

    Add new method to report lateness outside of flex time.
    BugzId: 1837

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule/src/com/mmxlabs/models/lng/schedule/util/LatenessUtils.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule/src/com/mmxlabs/models/lng/schedule/util/LatenessUtils.java
@@ -6,11 +6,13 @@ package com.mmxlabs.models.lng.schedule.util;
 
 import java.time.ZonedDateTime;
 
+import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.VesselAvailability;
 import com.mmxlabs.models.lng.schedule.Event;
 import com.mmxlabs.models.lng.schedule.PortVisit;
 import com.mmxlabs.models.lng.schedule.PortVisitLateness;
 import com.mmxlabs.models.lng.schedule.Sequence;
+import com.mmxlabs.models.lng.schedule.SlotAllocation;
 import com.mmxlabs.models.lng.schedule.SlotVisit;
 import com.mmxlabs.models.lng.schedule.VesselEventVisit;
 
@@ -20,6 +22,27 @@ public class LatenessUtils {
 		if (e instanceof PortVisit) {
 			final PortVisitLateness portVisitLateness = ((PortVisit) e).getLateness();
 			if (portVisitLateness != null) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean isLateWithFlex(final Event e) {
+		if (e instanceof PortVisit) {
+			final PortVisitLateness portVisitLateness = ((PortVisit) e).getLateness();
+			if (portVisitLateness != null) {
+				if (e instanceof SlotVisit) {
+					final SlotVisit slotVisit = (SlotVisit) e;
+					final SlotAllocation slotAllocation = slotVisit.getSlotAllocation();
+					if (slotAllocation != null) {
+						final Slot slot = slotAllocation.getSlot();
+						if (slot.getWindowFlex() > 0 && getLatenessInHours(slotVisit) < slot.getWindowFlex()) {
+							return false;
+						}
+					}
+					
+				}
+				
 				return true;
 			}
 		}
