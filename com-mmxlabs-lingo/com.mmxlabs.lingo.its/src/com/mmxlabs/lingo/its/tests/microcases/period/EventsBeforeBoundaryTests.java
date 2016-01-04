d45a194e77561ee19f2531dfbebd3b38fbf3990c
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-01-04 12:07:29 +0000

    More period unit tests.

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/period/EventsBeforeBoundaryTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/period/EventsBeforeBoundaryTests.java
@@ -51,7 +51,7 @@ import com.mmxlabs.models.lng.types.APortSet;
 import com.mmxlabs.optimiser.core.ISequences;
 
 @RunWith(value = ShiroRunner.class)
-public class EventsAfterBoundaryTests {
+public class EventsBeforeBoundaryTests {
 
 	@Test
 	@Category({ QuickTest.class, MicroTest.class })
@@ -85,10 +85,16 @@ public class EventsAfterBoundaryTests {
 				.build();
 
 		// Create a single charter out event
-		final CharterOutEvent charterOutEvent = cargoModelBuilder
-				.makeCharterOutEvent("charter-1", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+		cargoModelBuilder.makeCharterOutEvent("charter-1", LocalDateTime.of(2015, 2, 1, 0, 0, 0), LocalDateTime.of(2015, 2, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+				.withDurationInDays(20) //
 				.withRelocatePort(portFinder.findPort("Isle of Grain")) //
 				.withVesselAssignment(vesselAvailability_1, 1) //
+				.build(); //
+
+		cargoModelBuilder.makeCharterOutEvent("charter-2", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+				.withDurationInDays(20) //
+				.withRelocatePort(portFinder.findPort("Isle of Grain")) //
+				.withVesselAssignment(vesselAvailability_1, 2) //
 				.build(); //
 
 		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
@@ -99,8 +105,8 @@ public class EventsAfterBoundaryTests {
 		userSettings.setShippingOnly(false);
 		userSettings.setSimilarityMode(SimilarityMode.OFF);
 
-		userSettings.setPeriodStart(YearMonth.of(2015, 1));
-		userSettings.setPeriodEnd(YearMonth.of(2015, 2));
+		userSettings.setPeriodStart(YearMonth.of(2015, 4));
+		userSettings.setPeriodEnd(YearMonth.of(2015, 5));
 
 		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null);
 
@@ -114,17 +120,17 @@ public class EventsAfterBoundaryTests {
 			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
 
 			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
-			Assert.assertTrue(optimiserScenario.getCargoModel().getVesselEvents().isEmpty());
+			// Assert.assertTrue(optimiserScenario.getCargoModel().getVesselEvents().isEmpty());
 
 			final VesselAvailability vesselAvailability = optimiserScenario.getCargoModel().getVesselAvailabilities().get(0);
-			final List<APortSet<Port>> endAt = vesselAvailability.getEndAt();
-			Assert.assertEquals(1, endAt.size());
-			Assert.assertEquals("Ras Laffan", endAt.get(0).getName());
+			final List<APortSet<Port>> startAt = vesselAvailability.getStartAt();
+			Assert.assertEquals(1, startAt.size());
+			Assert.assertEquals("Ras Laffan", startAt.get(0).getName());
 			final ZoneId rasLaffanTimeZone = ZoneId.of(portFinder.findPort("Ras Laffan").getTimeZone());
 			final ZoneId utcTimeZone = ZoneId.of("UTC");
 			// Vessel availabilities always in UTC
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndAfterAsDateTime());
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndByAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartAfterAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartByAsDateTime());
 
 			// Assert initial state can be evaluted
 			final ISequences initialRawSequences = scenarioToOptimiserBridge.getDataTransformer().getInitialSequences();
@@ -167,9 +173,14 @@ public class EventsAfterBoundaryTests {
 				.build();
 
 		// Create a single charter out event
-		final DryDockEvent charterOutEvent = cargoModelBuilder
-				.makeDryDockEvent("drydock-1", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+		cargoModelBuilder.makeDryDockEvent("drydock-1", LocalDateTime.of(2015, 2, 1, 0, 0, 0), LocalDateTime.of(2015, 2, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
 				.withVesselAssignment(vesselAvailability_1, 1) //
+				.withDurationInDays(1) //
+				.build(); //
+
+		cargoModelBuilder.makeDryDockEvent("drydock-2", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+				.withVesselAssignment(vesselAvailability_1, 2) //
+				.withDurationInDays(20) //
 				.build(); //
 
 		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
@@ -180,8 +191,8 @@ public class EventsAfterBoundaryTests {
 		userSettings.setShippingOnly(false);
 		userSettings.setSimilarityMode(SimilarityMode.OFF);
 
-		userSettings.setPeriodStart(YearMonth.of(2015, 1));
-		userSettings.setPeriodEnd(YearMonth.of(2015, 2));
+		userSettings.setPeriodStart(YearMonth.of(2015, 4));
+		userSettings.setPeriodEnd(YearMonth.of(2015, 5));
 
 		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null);
 
@@ -195,17 +206,18 @@ public class EventsAfterBoundaryTests {
 			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
 
 			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
-			Assert.assertTrue(optimiserScenario.getCargoModel().getVesselEvents().isEmpty());
+			Assert.assertEquals(1, optimiserScenario.getCargoModel().getVesselEvents().size());
+			Assert.assertEquals("drydock-2", optimiserScenario.getCargoModel().getVesselEvents().get(0).getName());
 
 			final VesselAvailability vesselAvailability = optimiserScenario.getCargoModel().getVesselAvailabilities().get(0);
-			final List<APortSet<Port>> endAt = vesselAvailability.getEndAt();
-			Assert.assertEquals(1, endAt.size());
-			Assert.assertEquals("Ras Laffan", endAt.get(0).getName());
+			final List<APortSet<Port>> startAt = vesselAvailability.getStartAt();
+			Assert.assertEquals(1, startAt.size());
+			Assert.assertEquals("Ras Laffan", startAt.get(0).getName());
 			final ZoneId rasLaffanTimeZone = ZoneId.of(portFinder.findPort("Ras Laffan").getTimeZone());
 			final ZoneId utcTimeZone = ZoneId.of("UTC");
 			// Vessel availabilities always in UTC
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndAfterAsDateTime());
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndByAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartAfterAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartByAsDateTime());
 
 			// Assert initial state can be evaluted
 			final ISequences initialRawSequences = scenarioToOptimiserBridge.getDataTransformer().getInitialSequences();
@@ -247,10 +259,12 @@ public class EventsAfterBoundaryTests {
 				.withEndWindow(LocalDateTime.of(2015, 06, 01, 0, 0, 0), LocalDateTime.of(2015, 06, 01, 0, 0, 0)) //
 				.build();
 
-		// Create a single charter out event
-		final MaintenanceEvent event = cargoModelBuilder
-				.makeMaintenanceEvent("charter-1", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+		cargoModelBuilder.makeMaintenanceEvent("event-1", LocalDateTime.of(2015, 2, 1, 0, 0, 0), LocalDateTime.of(2015, 2, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
 				.withVesselAssignment(vesselAvailability_1, 1) //
+				.build(); //
+
+		cargoModelBuilder.makeMaintenanceEvent("event-2", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
+				.withVesselAssignment(vesselAvailability_1, 2) //
 				.build(); //
 
 		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
@@ -261,8 +275,8 @@ public class EventsAfterBoundaryTests {
 		userSettings.setShippingOnly(false);
 		userSettings.setSimilarityMode(SimilarityMode.OFF);
 
-		userSettings.setPeriodStart(YearMonth.of(2015, 1));
-		userSettings.setPeriodEnd(YearMonth.of(2015, 2));
+		userSettings.setPeriodStart(YearMonth.of(2015, 4));
+		userSettings.setPeriodEnd(YearMonth.of(2015, 5));
 
 		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null);
 
@@ -276,17 +290,17 @@ public class EventsAfterBoundaryTests {
 			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
 
 			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
-			Assert.assertTrue(optimiserScenario.getCargoModel().getVesselEvents().isEmpty());
+			Assert.assertEquals(1, optimiserScenario.getCargoModel().getVesselEvents().size());
 
 			final VesselAvailability vesselAvailability = optimiserScenario.getCargoModel().getVesselAvailabilities().get(0);
-			final List<APortSet<Port>> endAt = vesselAvailability.getEndAt();
-			Assert.assertEquals(1, endAt.size());
-			Assert.assertEquals("Ras Laffan", endAt.get(0).getName());
+			final List<APortSet<Port>> startAt = vesselAvailability.getStartAt();
+			Assert.assertEquals(1, startAt.size());
+			Assert.assertEquals("Ras Laffan", startAt.get(0).getName());
 			final ZoneId rasLaffanTimeZone = ZoneId.of(portFinder.findPort("Ras Laffan").getTimeZone());
 			final ZoneId utcTimeZone = ZoneId.of("UTC");
 			// Vessel availabilities always in UTC
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndAfterAsDateTime());
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndByAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartAfterAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartByAsDateTime());
 
 			// Assert initial state can be evaluted
 			final ISequences initialRawSequences = scenarioToOptimiserBridge.getDataTransformer().getInitialSequences();
@@ -330,15 +344,31 @@ public class EventsAfterBoundaryTests {
 
 		// Create cargo 1,
 		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L1", LocalDate.of(2015, 5, 1), portFinder.findPort("Ras Laffan"), null, entity, "5") //
+				.makeFOBPurchase("L1", LocalDate.of(2015, 1, 1), portFinder.findPort("Ras Laffan"), null, entity, "5") //
 				.withWindowStartTime(0) //
 				.withWindowSize(0) //
 				.build() //
-				.makeDESSale("D1", LocalDate.of(2015, 6, 1), portFinder.findPort("Mina Al Ahmadi"), null, entity, "7") //
+				.makeDESSale("D1", LocalDate.of(2015, 2, 14), portFinder.findPort("Mina Al Ahmadi"), null, entity, "7") //
 				.withWindowStartTime(0) //
 				.withWindowSize(0) //
 				.build() //
 				.withVesselAssignment(vesselAvailability_1, 0) //
+				.withAssignmentFlags(true, false) //
+				.build();
+
+		// Create cargo 2,
+		final Cargo cargo2 = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("L2", LocalDate.of(2015, 3, 1), portFinder.findPort("Ras Laffan"), null, entity, "5") //
+				.withWindowStartTime(0) //
+				.withWindowSize(0) //
+				.withVisitDuration(24) //
+				.build() //
+				.makeDESSale("D2", LocalDate.of(2015, 4, 2), portFinder.findPort("Mina Al Ahmadi"), null, entity, "7") //
+				.withWindowStartTime(0) //
+				.withWindowSize(0) //
+				.withVisitDuration(24) //
+				.build() //
+				.withVesselAssignment(vesselAvailability_1, 1) //
 				.withAssignmentFlags(true, false) //
 				.build();
 
@@ -350,8 +380,8 @@ public class EventsAfterBoundaryTests {
 		userSettings.setShippingOnly(false);
 		userSettings.setSimilarityMode(SimilarityMode.OFF);
 
-		userSettings.setPeriodStart(YearMonth.of(2015, 1));
-		userSettings.setPeriodEnd(YearMonth.of(2015, 2));
+		userSettings.setPeriodStart(YearMonth.of(2015, 4));
+		userSettings.setPeriodEnd(YearMonth.of(2015, 5));
 
 		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null);
 
@@ -365,110 +395,28 @@ public class EventsAfterBoundaryTests {
 			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
 
 			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
-			Assert.assertTrue(optimiserScenario.getCargoModel().getVesselEvents().isEmpty());
+			Assert.assertEquals(1, optimiserScenario.getCargoModel().getCargoes().size());
 
 			final VesselAvailability vesselAvailability = optimiserScenario.getCargoModel().getVesselAvailabilities().get(0);
-			final List<APortSet<Port>> endAt = vesselAvailability.getEndAt();
-			Assert.assertEquals(1, endAt.size());
-			Assert.assertEquals("Ras Laffan", endAt.get(0).getName());
+			final List<APortSet<Port>> startAt = vesselAvailability.getStartAt();
+			Assert.assertEquals(1, startAt.size());
+			Assert.assertEquals("Ras Laffan", startAt.get(0).getName());
 			final ZoneId rasLaffanTimeZone = ZoneId.of(portFinder.findPort("Ras Laffan").getTimeZone());
 			final ZoneId utcTimeZone = ZoneId.of("UTC");
 			// Vessel availabilities always in UTC
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndAfterAsDateTime());
-			Assert.assertEquals(ZonedDateTime.of(2015, 5, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getEndByAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 3, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartAfterAsDateTime());
+			Assert.assertEquals(ZonedDateTime.of(2015, 3, 1, 0, 0, 0, 0, rasLaffanTimeZone).withZoneSameInstant(utcTimeZone), vesselAvailability.getStartByAsDateTime());
 
-			// Assert initial state can be evaluted
-			final ISequences initialRawSequences = scenarioToOptimiserBridge.getDataTransformer().getInitialSequences();
-			// Validate the initial sequences are valid
-			Assert.assertTrue(MicroTestUtils.evaluateLSOSequences(scenarioToOptimiserBridge.getDataTransformer(), initialRawSequences));
-		} finally {
-			executorService.shutdownNow();
-		}
-	}
+			// Load slot is in the boundary, so make sure it is locked.
+			Cargo opt_cargo2 = optimiserScenario.getCargoModel().getCargoes().get(0);
+			Assert.assertTrue(opt_cargo2.getSortedSlots().get(0).isLocked());
+			Assert.assertEquals(1, opt_cargo2.getSortedSlots().get(0).getAllowedVessels().size());
 
-	@Test
-	@Category({ QuickTest.class, MicroTest.class })
-	public void evaluateCharterOutEventWithRelocationAsBoundaryEvent() throws Exception {
+			// Discharge should be free
+			Assert.assertFalse(opt_cargo2.getSortedSlots().get(1).isLocked());
+			Assert.assertEquals(0, opt_cargo2.getSortedSlots().get(1).getAllowedVessels().size());
 
-		// Load in the basic scenario from CSV
-
-		final @NonNull String urlRoot = getClass().getResource("/referencedata/reference-data-1/").toString();
-		final CSVImporter importer = new CSVImporter();
-		importer.importPortData(urlRoot);
-		importer.importCostData(urlRoot);
-		importer.importEntityData(urlRoot);
-		importer.importFleetData(urlRoot);
-		importer.importMarketData(urlRoot);
-		importer.importPromptData(urlRoot);
-		importer.importMarketData(urlRoot);
-
-		final LNGScenarioModel lngScenarioModel = importer.doImport();
-
-		// Create finder and builder
-		final ScenarioModelFinder scenarioModelFinder = new ScenarioModelFinder(lngScenarioModel);
-		final ScenarioModelBuilder scenarioModelBuilder = new ScenarioModelBuilder(lngScenarioModel);
-
-		final CommercialModelFinder commercialModelFinder = scenarioModelFinder.getCommercialModelFinder();
-		final FleetModelFinder fleetModelFinder = scenarioModelFinder.getFleetModelFinder();
-		final PortModelFinder portFinder = scenarioModelFinder.getPortModelFinder();
-
-		final CargoModelBuilder cargoModelBuilder = scenarioModelBuilder.getCargoModelBuilder();
-		final FleetModelBuilder fleetModelBuilder = scenarioModelBuilder.getFleetModelBuilder();
-		// final SpotMarketsModelBuilder spotMarketsModelBuilder = scenarioModelBuilder.getSpotMarketsModelBuilder();
-
-		// Create the required basic elements
-		final BaseLegalEntity entity = commercialModelFinder.findEntity("Shipping");
-
-		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
-
-		final Vessel vessel_1 = fleetModelBuilder.createVessel("Vessel-1", vesselClass);
-
-		final VesselAvailability vesselAvailability_1 = cargoModelBuilder.makeVesselAvailability(vessel_1, entity) //
-				.withStartPort(portFinder.findPort("Point Fortin")) //
-				.build();
-
-		// Construct the cargo scenario
-
-		// Create cargo 1,
-		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L1", LocalDate.of(2016, 5, 29), portFinder.findPort("Point Fortin"), null, entity, "5") //
-				.withVesselRestriction(vesselClass) //
-				.build() //
-				.makeDESSale("D1", LocalDate.of(2016, 6, 27), portFinder.findPort("Mina Al Ahmadi"), null, entity, "7") //
-				.withVesselRestriction(vesselClass) //
-				.build() //
-				.withVesselAssignment(vesselAvailability_1, 0) //
-				.withAssignmentFlags(true, false) //
-				.build();
-
-		final CharterOutEvent charterOutEvent = cargoModelBuilder
-				.makeCharterOutEvent("charter-1", LocalDateTime.of(2016, 7, 3, 0, 0, 0), LocalDateTime.of(2016, 7, 6, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
-				.withRelocatePort(portFinder.findPort("Tannguh")) //
-				.withVesselAssignment(vesselAvailability_1, 1) //
-				.build(); //
-
-		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
-		final UserSettings userSettings = ParametersFactory.eINSTANCE.createUserSettings();
-		userSettings.setBuildActionSets(false);
-		userSettings.setGenerateCharterOuts(false);
-
-		userSettings.setShippingOnly(false);
-		userSettings.setSimilarityMode(SimilarityMode.OFF);
-
-		userSettings.setPeriodStart(YearMonth.of(2016, 2));
-		userSettings.setPeriodEnd(YearMonth.of(2016, 5));
-
-		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null);
-
-		// Generate internal data
-		final ExecutorService executorService = Executors.newSingleThreadExecutor();
-		try {
-
-			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, optimiserSettings, new TransformerExtensionTestBootstrapModule(), null,
-					LNGTransformerHelper.HINT_OPTIMISE_LSO);
-			scenarioRunner.evaluateInitialState();
-			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
-
+			// Assert initial state can be evaluated
 			final ISequences initialRawSequences = scenarioToOptimiserBridge.getDataTransformer().getInitialSequences();
 			// Validate the initial sequences are valid
 			Assert.assertTrue(MicroTestUtils.evaluateLSOSequences(scenarioToOptimiserBridge.getDataTransformer(), initialRawSequences));
