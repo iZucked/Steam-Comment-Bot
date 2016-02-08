b3f8366804aae3a72b7ca35c7c54dfa70cc28b9f
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-02-08 20:19:28 +0000

    Port across some swtbot test runner stuff to try and make the build server test runs more stablw.

--- com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/CopyTreeToClipboardActionTest.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/CopyTreeToClipboardActionTest.java
@@ -4,17 +4,24 @@
  */
 package com.mmxlabs.rcp.common.tests;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,6 +29,11 @@ import com.mmxlabs.rcp.common.actions.CopyTreeToClipboardAction;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class CopyTreeToClipboardActionTest {
+	private final CyclicBarrier swtBarrier = new CyclicBarrier(2);
+	protected static Shell shell;
+	protected static SWTBot bot;
+	protected static Thread uiThread;
+	protected static Display display;
 
 	@Test
 	public void testCopyTreeToClipboardAction() throws InterruptedException {
@@ -84,5 +96,76 @@ public class CopyTreeToClipboardActionTest {
 				}
 			}
 		});
+	}
+
+	@Before
+	public void createDisplay() throws InterruptedException, BrokenBarrierException {
+
+		uiThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// Create or reuse existing display
+				display = Display.getDefault();
+				shell = new Shell(display);
+				shell.setText("Testing: " + getClass().getName());
+				shell.setLayout(new FillLayout());
+
+				bot = new SWTBot(shell);
+				shell.open();
+
+				// Pause until we near the exit of createDisplay method
+				try {
+					swtBarrier.await();
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+
+				try {
+					while (!display.isDisposed() && !shell.isDisposed()) {
+						try {
+							if (!display.readAndDispatch()) {
+								display.sleep();
+							}
+						} catch (Throwable t) {
+
+						}
+					}
+				} finally {
+					// Clean up display
+					if (!display.isDisposed()) {
+						display.dispose();
+					}
+				}
+			}
+		}, "UI Thead");
+		// Make daemon so that JVM can exit while thread is still running.
+		uiThread.setDaemon(true);
+		uiThread.start();
+
+		// Wait for display to be open before exiting this method
+		swtBarrier.await();
+
+	}
+
+	@After
+	public void disposeDisplay() throws InterruptedException {
+		// Ensure display is disposed
+		if (display != null && !display.isDisposed()) {
+			display.syncExec(new Runnable() {
+				@Override
+				public void run() {
+					if (display != null && !display.isDisposed()) {
+						display.dispose();
+					}
+				}
+			});
+		}
+		// Wait for UI thread to finish
+		uiThread.join();
+		// Reset variables
+		uiThread = null;
+		shell = null;
+		display = null;
 	}
 }
