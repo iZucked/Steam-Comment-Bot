f42e68e8914fd3d41d774e15c128e410de52a003
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-02-09 09:27:52 +0000

    Backed out changeset: 8b0f06bac178

--- com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/CopyTableToClipboardActionTest.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/CopyTableToClipboardActionTest.java
@@ -4,23 +4,16 @@
  */
 package com.mmxlabs.rcp.common.tests;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,11 +21,6 @@ import com.mmxlabs.rcp.common.actions.CopyTableToClipboardAction;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class CopyTableToClipboardActionTest {
-	private final CyclicBarrier swtBarrier = new CyclicBarrier(2);
-	protected static Shell shell;
-	protected static SWTBot bot;
-	protected static Thread uiThread;
-	protected static Display display;
 
 	@Test
 	public void testCopyTableToClipboardAction() throws InterruptedException {
@@ -82,76 +70,5 @@ public class CopyTableToClipboardActionTest {
 				}
 			}
 		});
-	}
-
-	@Before
-	public void createDisplay() throws InterruptedException, BrokenBarrierException {
-
-		uiThread = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				// Create or reuse existing display
-				display = Display.getDefault();
-				shell = new Shell(display);
-				shell.setText("Testing: " + getClass().getName());
-				shell.setLayout(new FillLayout());
-
-				bot = new SWTBot(shell);
-				shell.open();
-
-				// Pause until we near the exit of createDisplay method
-				try {
-					swtBarrier.await();
-				} catch (final Exception e) {
-					e.printStackTrace();
-				}
-
-				try {
-					while (!display.isDisposed() && !shell.isDisposed()) {
-						try {
-							if (!display.readAndDispatch()) {
-								display.sleep();
-							}
-						} catch (Throwable t) {
-
-						}
-					}
-				} finally {
-					// Clean up display
-					if (!display.isDisposed()) {
-						display.dispose();
-					}
-				}
-			}
-		}, "UI Thead");
-		// Make daemon so that JVM can exit while thread is still running.
-		uiThread.setDaemon(true);
-		uiThread.start();
-
-		// Wait for display to be open before exiting this method
-		swtBarrier.await();
-
-	}
-
-	@After
-	public void disposeDisplay() throws InterruptedException {
-		// Ensure display is disposed
-		if (display != null && !display.isDisposed()) {
-			display.syncExec(new Runnable() {
-				@Override
-				public void run() {
-					if (display != null && !display.isDisposed()) {
-						display.dispose();
-					}
-				}
-			});
-		}
-		// Wait for UI thread to finish
-		uiThread.join();
-		// Reset variables
-		uiThread = null;
-		shell = null;
-		display = null;
 	}
 }
