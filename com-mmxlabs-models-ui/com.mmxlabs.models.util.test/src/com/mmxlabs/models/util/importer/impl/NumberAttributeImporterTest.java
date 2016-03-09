2c0e855e0c9b1f7afb5be8262d1d661915017dc0
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-03-09 11:29:04 +0000

    Support new annotation property "exportFormatString" and existing "formatString" property in CSV import/export to allow data other than 3 d.p. to be stored in CSV files.
    BugzId: 1951

--- com-mmxlabs-models-ui/com.mmxlabs.models.util.test/src/com/mmxlabs/models/util/importer/impl/NumberAttributeImporterTest.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.util.test/src/com/mmxlabs/models/util/importer/impl/NumberAttributeImporterTest.java
@@ -6,6 +6,8 @@ package com.mmxlabs.models.util.importer.impl;
 
 import java.text.ParseException;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,13 +21,15 @@ public class NumberAttributeImporterTest {
 		final int[] testNumbers = new int[] { 1, 1000, 2000, -3000 };
 		final String[] expectedPatterns = new String[] { "1", "1000", "2000", "-3000" };
 
+		final EAttribute attribute = EcoreFactory.eINSTANCE.createEAttribute();
+
 		for (final char decimalSeparator : decimalSeparators) {
 			final NumberAttributeImporter importer = new NumberAttributeImporter(decimalSeparator);
 
 			for (int i = 0; i < testNumbers.length; ++i) {
-				final String str = importer.intToString(testNumbers[i]);
+				final String str = importer.intToString(testNumbers[i], attribute);
 				Assert.assertEquals(String.format(expectedPatterns[i], decimalSeparator), str);
-				final int result = importer.stringToInt(str);
+				final int result = importer.stringToInt(str, attribute);
 				Assert.assertEquals(testNumbers[i], result);
 			}
 		}
@@ -38,13 +42,14 @@ public class NumberAttributeImporterTest {
 		final String[] expectedPatterns = new String[] { "1%s000", "1000%s000", "1234%s568", "-3000%s500" };
 		final float[] expectedNumbers = new float[] { 1.0f, 1000f, 1234.568f, -3000.5f };
 
+		final EAttribute attribute = EcoreFactory.eINSTANCE.createEAttribute();
 		for (final char decimalSeparator : decimalSeparators) {
 			final NumberAttributeImporter importer = new NumberAttributeImporter(decimalSeparator);
 
 			for (int i = 0; i < testNumbers.length; ++i) {
-				final String str = importer.floatToString(testNumbers[i]);
+				final String str = importer.floatToString(testNumbers[i], attribute);
 				Assert.assertEquals(String.format(expectedPatterns[i], decimalSeparator), str);
-				final float result = importer.stringToFloat(str);
+				final float result = importer.stringToFloat(str, attribute);
 				Assert.assertEquals(expectedNumbers[i], result, 0.0f);
 			}
 		}
@@ -57,13 +62,13 @@ public class NumberAttributeImporterTest {
 		final String[] expectedPatterns = new String[] { "1%s000", "1000%s000", "1234%s568", "-3000%s500" };
 		final double[] expectedNumbers = new double[] { 1.0, 1000, 1234.568, -3000.5 };
 
+		final EAttribute attribute = EcoreFactory.eINSTANCE.createEAttribute();
 		for (final char decimalSeparator : decimalSeparators) {
 			final NumberAttributeImporter importer = new NumberAttributeImporter(decimalSeparator);
-
 			for (int i = 0; i < testNumbers.length; ++i) {
-				final String str = importer.doubleToString(testNumbers[i]);
+				final String str = importer.doubleToString(testNumbers[i], attribute);
 				Assert.assertEquals(String.format(expectedPatterns[i], decimalSeparator), str);
-				final double result = importer.stringToDouble(str);
+				final double result = importer.stringToDouble(str, attribute);
 				Assert.assertEquals(expectedNumbers[i], result, 0.0f);
 			}
 		}
