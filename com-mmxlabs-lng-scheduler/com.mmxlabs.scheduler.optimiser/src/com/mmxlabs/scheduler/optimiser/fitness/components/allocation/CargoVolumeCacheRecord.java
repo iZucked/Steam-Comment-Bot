2615d49a3bbaa1e5e0e7541b0767bac255c57fd6
Jenkins <unknown@kiln.example.com>
2016-04-08 15:58:36 -0100

    Update headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/CargoVolumeCacheRecord.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/CargoVolumeCacheRecord.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2016
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.fitness.components.allocation;
 
 import org.eclipse.jdt.annotation.NonNull;
