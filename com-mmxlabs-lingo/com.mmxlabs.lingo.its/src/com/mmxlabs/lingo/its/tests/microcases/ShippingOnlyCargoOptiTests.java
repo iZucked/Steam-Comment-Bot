94f455017477d7acadeba9c2a6edba0ca06c474c
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-04-15 10:04:10 -0100

    Shipping only optimisation micro cases.
    BugzId: 1965, 1970, 1971, 1989

---
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/ShippingOnlyCargoOptiTests.java
@@ -0,0 +1,343 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2016
+ * All rights reserved.
+ */
+package com.mmxlabs.lingo.its.tests.microcases;
+
+import java.time.LocalDate;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.mmxlabs.lingo.its.tests.AbstractOptimisationResultTester;
+import com.mmxlabs.lingo.its.tests.category.MicroTest;
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.cargo.util.CargoModelBuilder;
+import com.mmxlabs.models.lng.commercial.BaseLegalEntity;
+import com.mmxlabs.models.lng.commercial.util.CommercialModelFinder;
+import com.mmxlabs.models.lng.fleet.util.FleetModelBuilder;
+import com.mmxlabs.models.lng.fleet.util.FleetModelFinder;
+import com.mmxlabs.models.lng.parameters.OptimiserSettings;
+import com.mmxlabs.models.lng.parameters.ParametersFactory;
+import com.mmxlabs.models.lng.parameters.SimilarityMode;
+import com.mmxlabs.models.lng.parameters.UserSettings;
+import com.mmxlabs.models.lng.port.util.PortModelFinder;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelBuilder;
+import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelFinder;
+import com.mmxlabs.models.lng.transformer.inject.LNGTransformerHelper;
+import com.mmxlabs.models.lng.transformer.its.scenario.CSVImporter;
+import com.mmxlabs.models.lng.transformer.its.tests.TransformerExtensionTestBootstrapModule;
+import com.mmxlabs.models.lng.transformer.ui.LNGScenarioRunner;
+import com.mmxlabs.models.lng.transformer.ui.OptimisationHelper;
+
+public class ShippingOnlyCargoOptiTests extends AbstractOptimisationResultTester {
+
+	@Test
+	@Category(MicroTest.class)
+	public void shippingOnly_SlotSwap_Permitted() throws Exception {
+
+		// Load in the basic scenario from CSV
+
+		final @NonNull String urlRoot = getClass().getResource("/referencedata/reference-data-1/").toString();
+		final CSVImporter importer = new CSVImporter();
+		importer.importPortData(urlRoot);
+		importer.importCostData(urlRoot);
+		importer.importEntityData(urlRoot);
+		importer.importFleetData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importPromptData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importContractData(urlRoot);
+
+		final LNGScenarioModel lngScenarioModel = importer.doImport();
+
+		// Create finder and builder
+		final ScenarioModelFinder scenarioModelFinder = new ScenarioModelFinder(lngScenarioModel);
+		final ScenarioModelBuilder scenarioModelBuilder = new ScenarioModelBuilder(lngScenarioModel);
+
+		final CommercialModelFinder commercialModelFinder = scenarioModelFinder.getCommercialModelFinder();
+		final FleetModelFinder fleetModelFinder = scenarioModelFinder.getFleetModelFinder();
+		final PortModelFinder portFinder = scenarioModelFinder.getPortModelFinder();
+
+		final CargoModelBuilder cargoModelBuilder = scenarioModelBuilder.getCargoModelBuilder();
+		final FleetModelBuilder fleetModelBuilder = scenarioModelBuilder.getFleetModelBuilder();
+
+		// Create the required basic elements
+		final BaseLegalEntity entity = commercialModelFinder.findEntity("Shipping");
+
+		// Construct the cargo scenario
+
+		// Create cargo 1, cargo 2
+		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
+				.makeDESPurchase("L1", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "5", null) //
+				.withOptional(true) //
+
+				.build() //
+
+				.makeDESSale("D1", LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "7") //
+				.build() //
+
+				.build();
+
+		Slot load1 = cargo1.getSlots().get(0);
+		Slot discharge1 = cargo1.getSlots().get(1);
+
+		final Slot load2 = cargoModelBuilder.makeDESPurchase("L2", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "1", null, null) //
+				.withOptional(true) //
+				.build();
+
+		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
+		final UserSettings userSettings = ParametersFactory.eINSTANCE.createUserSettings();
+		userSettings.setBuildActionSets(false);
+		userSettings.setGenerateCharterOuts(false);
+		userSettings.setShippingOnly(false);
+		userSettings.setSimilarityMode(SimilarityMode.OFF);
+
+		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimiserSettings.getAnnealingSettings().setIterations(10_000);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, optimiserSettings, new TransformerExtensionTestBootstrapModule(), null, false,
+					LNGTransformerHelper.HINT_OPTIMISE_LSO);
+			scenarioRunner.evaluateInitialState();
+
+			scenarioRunner.run();
+
+			// Wiring should have changed
+			Assert.assertSame(load2, lngScenarioModel.getCargoModel().getCargoes().get(0).getSortedSlots().get(0));
+			Assert.assertSame(discharge1, lngScenarioModel.getCargoModel().getCargoes().get(0).getSortedSlots().get(1));
+
+		} finally {
+			executorService.shutdownNow();
+		}
+	}
+
+	@Test
+	@Category(MicroTest.class)
+	public void shippingOnly_SlotSwap_NotPermitted() throws Exception {
+
+		// Load in the basic scenario from CSV
+
+		final @NonNull String urlRoot = getClass().getResource("/referencedata/reference-data-1/").toString();
+		final CSVImporter importer = new CSVImporter();
+		importer.importPortData(urlRoot);
+		importer.importCostData(urlRoot);
+		importer.importEntityData(urlRoot);
+		importer.importFleetData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importPromptData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importContractData(urlRoot);
+
+		final LNGScenarioModel lngScenarioModel = importer.doImport();
+
+		// Create finder and builder
+		final ScenarioModelFinder scenarioModelFinder = new ScenarioModelFinder(lngScenarioModel);
+		final ScenarioModelBuilder scenarioModelBuilder = new ScenarioModelBuilder(lngScenarioModel);
+
+		final CommercialModelFinder commercialModelFinder = scenarioModelFinder.getCommercialModelFinder();
+		final FleetModelFinder fleetModelFinder = scenarioModelFinder.getFleetModelFinder();
+		final PortModelFinder portFinder = scenarioModelFinder.getPortModelFinder();
+
+		final CargoModelBuilder cargoModelBuilder = scenarioModelBuilder.getCargoModelBuilder();
+		final FleetModelBuilder fleetModelBuilder = scenarioModelBuilder.getFleetModelBuilder();
+
+		// Create the required basic elements
+		final BaseLegalEntity entity = commercialModelFinder.findEntity("Shipping");
+
+		// Construct the cargo scenario
+
+		// Create cargo 1, cargo 2
+		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
+				.makeDESPurchase("L1", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "5", null) //
+				.withOptional(true) //
+
+				.build() //
+
+				.makeDESSale("D1", LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "7") //
+				.build() //
+
+				.build();
+
+		Slot load1 = cargo1.getSlots().get(0);
+		Slot discharge1 = cargo1.getSlots().get(1);
+
+		final Slot load2 = cargoModelBuilder.makeDESPurchase("L2", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "1", null, null) //
+				.withOptional(true) //
+				.build();
+
+		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
+		final UserSettings userSettings = ParametersFactory.eINSTANCE.createUserSettings();
+		userSettings.setBuildActionSets(false);
+		userSettings.setGenerateCharterOuts(false);
+		userSettings.setShippingOnly(true);
+		userSettings.setSimilarityMode(SimilarityMode.OFF);
+
+		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimiserSettings.getAnnealingSettings().setIterations(10_000);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, optimiserSettings, new TransformerExtensionTestBootstrapModule(), null, false,
+					LNGTransformerHelper.HINT_OPTIMISE_LSO);
+			scenarioRunner.evaluateInitialState();
+
+			scenarioRunner.run();
+
+			// Wiring should have changed
+			Assert.assertSame(load1, lngScenarioModel.getCargoModel().getCargoes().get(0).getSortedSlots().get(0));
+			Assert.assertSame(discharge1, lngScenarioModel.getCargoModel().getCargoes().get(0).getSortedSlots().get(1));
+
+		} finally {
+			executorService.shutdownNow();
+		}
+	}
+
+	@Test
+	@Category(MicroTest.class)
+	public void shippingOnly_OptionalSlots_Permitted() throws Exception {
+
+		// Load in the basic scenario from CSV
+
+		final @NonNull String urlRoot = getClass().getResource("/referencedata/reference-data-1/").toString();
+		final CSVImporter importer = new CSVImporter();
+		importer.importPortData(urlRoot);
+		importer.importCostData(urlRoot);
+		importer.importEntityData(urlRoot);
+		importer.importFleetData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importPromptData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importContractData(urlRoot);
+
+		final LNGScenarioModel lngScenarioModel = importer.doImport();
+
+		// Create finder and builder
+		final ScenarioModelFinder scenarioModelFinder = new ScenarioModelFinder(lngScenarioModel);
+		final ScenarioModelBuilder scenarioModelBuilder = new ScenarioModelBuilder(lngScenarioModel);
+
+		final CommercialModelFinder commercialModelFinder = scenarioModelFinder.getCommercialModelFinder();
+		final FleetModelFinder fleetModelFinder = scenarioModelFinder.getFleetModelFinder();
+		final PortModelFinder portFinder = scenarioModelFinder.getPortModelFinder();
+
+		final CargoModelBuilder cargoModelBuilder = scenarioModelBuilder.getCargoModelBuilder();
+		final FleetModelBuilder fleetModelBuilder = scenarioModelBuilder.getFleetModelBuilder();
+
+		// Create the required basic elements
+		final BaseLegalEntity entity = commercialModelFinder.findEntity("Shipping");
+
+		// Construct the cargo scenario
+
+		// Create cargo 1, cargo 2
+		cargoModelBuilder.makeDESPurchase("L1", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "5", null, null) //
+				.build(); //
+
+		cargoModelBuilder.makeDESSale("D1", LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "7") //
+				.build(); //
+
+		// Create UserSettings, place cargo 2 load in boundary, cargo 2 discharge in period.
+		final UserSettings userSettings = ParametersFactory.eINSTANCE.createUserSettings();
+		userSettings.setBuildActionSets(false);
+		userSettings.setGenerateCharterOuts(false);
+		userSettings.setShippingOnly(false);
+		userSettings.setSimilarityMode(SimilarityMode.OFF);
+
+		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimiserSettings.getAnnealingSettings().setIterations(10_000);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, optimiserSettings, new TransformerExtensionTestBootstrapModule(), null, false,
+					LNGTransformerHelper.HINT_OPTIMISE_LSO);
+			scenarioRunner.evaluateInitialState();
+
+			scenarioRunner.run();
+
+			Assert.assertEquals(1, lngScenarioModel.getCargoModel().getCargoes().size());
+
+		} finally {
+			executorService.shutdownNow();
+		}
+	}
+
+	@Test
+	@Category(MicroTest.class)
+	public void shippingOnly_OptionalSlots_NotPermitted() throws Exception {
+
+		// Load in the basic scenario from CSV
+
+		final @NonNull String urlRoot = getClass().getResource("/referencedata/reference-data-1/").toString();
+		final CSVImporter importer = new CSVImporter();
+		importer.importPortData(urlRoot);
+		importer.importCostData(urlRoot);
+		importer.importEntityData(urlRoot);
+		importer.importFleetData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importPromptData(urlRoot);
+		importer.importMarketData(urlRoot);
+		importer.importContractData(urlRoot);
+
+		final LNGScenarioModel lngScenarioModel = importer.doImport();
+
+		// Create finder and builder
+		final ScenarioModelFinder scenarioModelFinder = new ScenarioModelFinder(lngScenarioModel);
+		final ScenarioModelBuilder scenarioModelBuilder = new ScenarioModelBuilder(lngScenarioModel);
+
+		final CommercialModelFinder commercialModelFinder = scenarioModelFinder.getCommercialModelFinder();
+		final FleetModelFinder fleetModelFinder = scenarioModelFinder.getFleetModelFinder();
+		final PortModelFinder portFinder = scenarioModelFinder.getPortModelFinder();
+
+		final CargoModelBuilder cargoModelBuilder = scenarioModelBuilder.getCargoModelBuilder();
+		final FleetModelBuilder fleetModelBuilder = scenarioModelBuilder.getFleetModelBuilder();
+
+		// Create the required basic elements
+		final BaseLegalEntity entity = commercialModelFinder.findEntity("Shipping");
+
+		// Construct the cargo scenario
+
+		// Create cargo 1, cargo 2
+		cargoModelBuilder.makeDESPurchase("L1", false, LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "5", null, null) //
+				.build(); //
+
+		cargoModelBuilder.makeDESSale("D1", LocalDate.of(2016, 2, 18), portFinder.findPort("Isle of Grain"), null, entity, "7") //
+				.build(); //
+
+		final UserSettings userSettings = ParametersFactory.eINSTANCE.createUserSettings();
+		userSettings.setBuildActionSets(false);
+		userSettings.setGenerateCharterOuts(false);
+		userSettings.setShippingOnly(true);
+		userSettings.setSimilarityMode(SimilarityMode.OFF);
+
+		final OptimiserSettings optimiserSettings = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimiserSettings.getAnnealingSettings().setIterations(10_000);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, optimiserSettings, new TransformerExtensionTestBootstrapModule(), null, false,
+					LNGTransformerHelper.HINT_OPTIMISE_LSO);
+			scenarioRunner.evaluateInitialState();
+
+			scenarioRunner.run();
+
+			Assert.assertEquals(0, lngScenarioModel.getCargoModel().getCargoes().size());
+
+		} finally {
+			executorService.shutdownNow();
+		}
+	}
+}
