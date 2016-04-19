b52843dbb499dd8d6392828480ecabb63852905e
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-04-19 11:51:31 -0100

    Fix up test runs for missing entity books.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial/src/com/mmxlabs/models/lng/commercial/util/CommercialModelBuilder.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial/src/com/mmxlabs/models/lng/commercial/util/CommercialModelBuilder.java
@@ -6,7 +6,9 @@ package com.mmxlabs.models.lng.commercial.util;
 
 import org.eclipse.jdt.annotation.NonNull;
 
+import com.mmxlabs.models.lng.commercial.CommercialFactory;
 import com.mmxlabs.models.lng.commercial.CommercialModel;
+import com.mmxlabs.models.lng.commercial.LegalEntity;
 
 public class CommercialModelBuilder {
 
@@ -14,5 +16,19 @@ public class CommercialModelBuilder {
 
 	public CommercialModelBuilder(@NonNull final CommercialModel commercialModel) {
 		this.commercialModel = commercialModel;
+	}
+
+	public @NonNull LegalEntity makeLegalEntityAndBooks(@NonNull String name) {
+		final LegalEntity entity = CommercialFactory.eINSTANCE.createLegalEntity();
+
+		entity.setShippingBook(CommercialFactory.eINSTANCE.createSimpleEntityBook());
+
+		entity.setTradingBook(CommercialFactory.eINSTANCE.createSimpleEntityBook());
+
+		entity.setUpstreamBook(CommercialFactory.eINSTANCE.createSimpleEntityBook());
+
+		commercialModel.getEntities().add(entity);
+
+		return entity;
 	}
 }
