4cf77791c004e2afaec06c0c8a5f1ca040380baf
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-05-19 09:50:50 -0100

    Change println to print

--- com-mmxlabs-models-migration/com.mmxlabs.models.migration.tests/scripts/migration.js
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration.tests/scripts/migration.js
@@ -3,12 +3,12 @@
  * All rights reserved.
  */
 		{
-					println(oldFeatures);
-					println(v2Object);
+					print(oldFeatures);
+					print(v2Object);
 
 			// Convert unknown features
 			if (oldFeatures.containsKey(v2Object)) {
-					println("Yup");
+					print("Yup");
 
 				var anyType = oldFeatures.get(v2Object);
 				var itr = anyType.getAnyAttribute().iterator();
@@ -16,7 +16,7 @@
 					var e = itr.next();
 					var eStructuralFeature = e.getEStructuralFeature();
 					// Copy value from attributeB to attributeC
-					println(eStructuralFeature.getName());
+				print(eStructuralFeature.getName());
 					if (eStructuralFeature.getName().equals("attributeB")) {
 						// Need to parse string as the de-serialization does not know what type the object is.
 						// TODO: Need scripting API to manage this better
