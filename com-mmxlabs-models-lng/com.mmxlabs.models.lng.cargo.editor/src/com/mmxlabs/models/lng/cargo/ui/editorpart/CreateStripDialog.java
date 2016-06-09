ccbdd069838464966b998e55c9d2af08c501dd58
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-06-09 07:45:26 -0100

    Fix some strip cargo issues

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CreateStripDialog.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/CreateStripDialog.java
@@ -426,7 +426,7 @@ public class CreateStripDialog extends FormDialog {
 					} else {
 						assert rt == RepeatType.Distributed;
 						label1.setText("Create");
-						((GridData) label1.getLayoutData()).widthHint = 36;
+						((GridData) label1.getLayoutData()).widthHint = 40;
 						label2.setVisible(true);
 						label3.setVisible(false);
 						intervalType.getControl().setVisible(false);
@@ -615,6 +615,9 @@ public class CreateStripDialog extends FormDialog {
 		} catch (final NumberFormatException nfe) {
 			// Ignore
 		}
+
+		int n = calSpacing;
+
 		// Min of 1 element
 		calSpacing = Math.max(1, calSpacing);
 
@@ -624,15 +627,16 @@ public class CreateStripDialog extends FormDialog {
 		// ABS as sanity check...
 		final int diffInDays = Days.between(fromDate, toDate);
 
+		final int rtIdx = repeatType.getCombo().getSelectionIndex();
+		if (rtIdx < 0) {
+			return Collections.emptyList();
+		}
+		final RepeatType rt = RepeatType.values()[rtIdx];
 		{
-			final int rtIdx = repeatType.getCombo().getSelectionIndex();
-			if (rtIdx < 0) {
-				return Collections.emptyList();
-			}
-			final RepeatType rt = RepeatType.values()[rtIdx];
 			switch (rt) {
 			case Distributed: {
 				calUnit = ChronoUnit.DAYS;
+				calSpacing = Math.max(1, calSpacing - 1);
 				calSpacing = diffInDays / calSpacing;
 				calSpacing = Math.max(1, calSpacing);
 			}
@@ -672,9 +676,17 @@ public class CreateStripDialog extends FormDialog {
 		} else {
 			sampleDate = LocalDate.now();
 		}
-		while (toDate.isAfter(sampleDate)) {
-			dates.add(sampleDate);
-			sampleDate = sampleDate.plus(calSpacing, calUnit);
+
+		if (rt == RepeatType.Distributed) {
+			for (int i = 0; i < n; ++i) {
+				dates.add(sampleDate);
+				sampleDate = sampleDate.plus(calSpacing, calUnit);
+			}
+		} else {
+			while (toDate.isAfter(sampleDate)) {
+				dates.add(sampleDate);
+				sampleDate = sampleDate.plus(calSpacing, calUnit);
+			}
 		}
 
 		// Pricing date special case - keep the difference in months constant
