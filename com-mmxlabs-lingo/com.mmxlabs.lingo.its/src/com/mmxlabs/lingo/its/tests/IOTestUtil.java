142454ddca1bda979c7566189ad67dae58255d07
berkan <beskikaya@minimaxlabs.com>
2016-06-22 07:55:19 -0100

    CSV export testing BugzId:183

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/IOTestUtil.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/IOTestUtil.java
@@ -7,6 +7,7 @@ package com.mmxlabs.lingo.its.tests;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,13 +17,46 @@ import com.mmxlabs.models.lng.parameters.OptimiserSettings;
 import com.mmxlabs.models.lng.scenario.exportWizards.ExportCSVWizard;
 import com.mmxlabs.models.lng.scenario.exportWizards.ExportCSVWizard.exportInformation;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.schedule.CargoAllocation;
 import com.mmxlabs.models.lng.schedule.Fitness;
 import com.mmxlabs.models.lng.schedule.Schedule;
+import com.mmxlabs.models.lng.schedule.SlotAllocation;
 import com.mmxlabs.models.lng.transformer.extensions.ScenarioUtils;
 import com.mmxlabs.models.lng.transformer.ui.LNGScenarioRunner;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
 public class IOTestUtil {
 	
+	
+	public static String[] ListToArray(EList<CargoAllocation> originalCargoAllocations){
+			
+			int size = originalCargoAllocations.size();
+			String[] array = new String[size];
+			
+			int i = 0;
+			for (CargoAllocation temp :  originalCargoAllocations) {
+				array[i] = (temp.getName());
+				i += 1; 
+			}
+			return array;
+			
+		}
+	
+	
+	public static ArrayList makeSlotAllocationArray(EList<CargoAllocation> originalCargoAllocations){
+		
+		ArrayList output = new ArrayList();
+		
+		for(CargoAllocation ca: originalCargoAllocations){
+			EList<SlotAllocation> allocations = ca.getSlotAllocations();
+			
+			for (SlotAllocation sa : allocations){
+				output.add(sa.getPort().getLocation());
+				//output.add(sa.getSlot().getCargo().getLoadName());
+			}
+		}
+		return output;
+	}
 	
 	/**
 	 * Converts EList<Fitness> into long[] without headers for easy comparison.
@@ -30,8 +64,9 @@ public class IOTestUtil {
 	 * @return array - Output array of fitness longs
 	 */
 	public static long[] fitnessListToArrayValues(EList<Fitness> fitnesses){
-				
-		long[] array = new long[4];
+		
+		int size = fitnesses.size();
+		long[] array = new long[size];
 		
 		int i = 0;
 		for (Fitness temp : fitnesses) {
@@ -42,7 +77,7 @@ public class IOTestUtil {
 		
 	}
 	/**
-	 *  Deletes the specified directory and all its contents
+	 *  Deletes the temporary directory and all its contents
 	 */
 	public static void tempDirectoryTeardown(){
 		
@@ -65,7 +100,7 @@ public class IOTestUtil {
 	 * @return - LNGScenarioModel object created from the target files
 	 * @throws MalformedURLException
 	 */
-	public static LNGScenarioModel URLtoScenario(URL url) throws MalformedURLException{
+	public static LNGScenarioModel URLtoScenarioCSV(URL url) throws MalformedURLException{
 		
 		CSVTestDataProvider csv_tdp = new CSVTestDataProvider(url);
 	
@@ -74,6 +109,20 @@ public class IOTestUtil {
 		return testCase;
 		
 	}
+	
+	
+	
+	public static LNGScenarioModel URLtoScenarioLiNGO(URL url) throws Exception{
+		
+		AbstractOptimisationResultTester AORT = new AbstractOptimisationResultTester();
+			
+			final ScenarioInstance instance = AORT.loadScenario(url);
+		
+			LNGScenarioModel testCase =  (LNGScenarioModel) instance.getInstance();
+	
+			return testCase;
+			
+		}
 	
 	
 	/**
@@ -93,6 +142,18 @@ public class IOTestUtil {
 		final EList<Fitness> fitnesses = schedule.getFitnesses();
 		
 		return fitnesses;
+	}
+	
+public static Schedule ScenarioModeltoSchedule(LNGScenarioModel model){
+		
+		final OptimiserSettings settings = LNGScenarioRunnerCreator.createExtendedSettings(ScenarioUtils.createDefaultSettings());
+		final ExecutorService executorService = Executors.newSingleThreadExecutor();
+		
+		LNGScenarioRunner runner = LNGScenarioRunnerCreator.createScenarioRunnerWithLSO(executorService, model, settings);
+
+		final Schedule schedule = runner.getSchedule();
+		
+		return schedule;
 	}
 	
 	/**
---
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/IOTesterLiNGO.java
@@ -0,0 +1,71 @@
+package com.mmxlabs.lingo.its.tests;
+
+import java.net.URL;
+import java.util.Arrays;
+
+import org.eclipse.emf.common.util.EList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.schedule.Fitness;
+
+@RunWith(Parameterized.class)
+public class IOTesterLiNGO {
+
+	private String name;
+	private String scenarioPath;
+
+	public IOTesterLiNGO(final String name, final String scenarioPath) throws Exception {
+
+		this.name = name;
+		this.scenarioPath = scenarioPath;
+	}
+
+	@Parameters(name = "{0}")
+	public static Iterable<Object[]> generateTests() {
+		return Arrays.asList(new Object[][] { { "testBonnyProblems_Bonny", "/scenarios/demo-cases/Bonny problems/0 Bonny.lingo" },
+				{ "testBonnyProblems_LateAndLost_DES_Backfill", "/scenarios/demo-cases/Bonny problems/1 late and lost-DES backfill -F- Bonny.lingo" },
+				{ "testBonnyProblems_O_LateAndLost_DES_Backfill", "/scenarios/demo-cases/Bonny problems/2 -O- late and lost-DES backfill -F- Bonny.lingo" },
+				{ "testDryDockIssues_base", "/scenarios/demo-cases/Dry dock issues/0 base.lingo" },
+				{ "testDryDockIssues_dryDock55_lateness_removed", "/scenarios/demo-cases/Dry dock issues/3 -O- dry dock 55 -F- lateness removed -F- base.lingo" },
+				{ "testDryDockIssues_dryDock70", "/scenarios/demo-cases/Dry dock issues/4 dry dock 70 -O- -F- -F- base.lingo" },
+				{ "testDryDockIssues_charterInGenerated", "/scenarios/demo-cases/Dry dock issues/5 charter-in generated - rewire for shorter one -O- -F- -F- base.lingo" },
+				{ "testDryDockIssues_dryDock70", "/scenarios/demo-cases/Dry dock issues/4 dry dock 70 -O- -F- -F- base.lingo" },
+				{ "testFleetCostOptimisation_fleet_demo", "/scenarios/demo-cases/Fleet cost optimisation/0 fleet demo.lingo" },
+				{ "testFleetCostOptimisation_O_fleet_demo", "/scenarios/demo-cases/Fleet cost optimisation/1 -O- fleet demo.lingo" }, { "des-cargo", "/scenarios/des-cargo.lingo" },
+				{ "sample-data", "/scenarios/sample-data.lingo" },
+
+		});
+	}
+	
+	
+	@Test
+	public void testLiNGOInOut() throws Exception {
+		
+		final URL url = getClass().getResource(scenarioPath);
+		
+		LNGScenarioModel testCase = IOTestUtil.URLtoScenarioLiNGO(url);
+
+		EList<Fitness> originalFitnesses = IOTestUtil.ScenarioModeltoFitnessList(testCase);
+
+		long[] originalArray = IOTestUtil.fitnessListToArrayValues(originalFitnesses);
+
+		URL restoredURL = IOTestUtil.exportTestCase(testCase);
+
+		LNGScenarioModel restoredCase = IOTestUtil.URLtoScenarioCSV(restoredURL);
+
+		EList<Fitness> restoredFitnesses = IOTestUtil.ScenarioModeltoFitnessList(restoredCase);
+
+		long[] restoredArray = IOTestUtil.fitnessListToArrayValues(restoredFitnesses);
+
+		// Delete temporary directory
+		IOTestUtil.tempDirectoryTeardown();
+
+		Assert.assertArrayEquals(originalArray, restoredArray);
+		
+	}
+}
