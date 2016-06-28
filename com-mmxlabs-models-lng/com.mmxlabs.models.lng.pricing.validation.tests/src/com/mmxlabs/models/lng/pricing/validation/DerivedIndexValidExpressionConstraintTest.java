21190c720a4b92c4694b67903fc01937fb04bdb5
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-06-28 08:47:14 -0100

    Add stub validation test project

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.validation.tests/src/com/mmxlabs/models/lng/pricing/validation/DerivedIndexValidExpressionConstraintTest.java
@@ -0,0 +1,15 @@
+package com.mmxlabs.models.lng.pricing.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DerivedIndexValidExpressionConstraintTest {
+
+	@Test
+	public void test() {
+		DerivedIndexValidExpressionConstraint constraint = new DerivedIndexValidExpressionConstraint();
+		
+	}
+
+}
