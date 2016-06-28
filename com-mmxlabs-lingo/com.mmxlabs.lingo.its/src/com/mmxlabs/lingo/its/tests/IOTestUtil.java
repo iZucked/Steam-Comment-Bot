0f2e71a1c81f3b4d8fcbf3ac1890fb9af01b7f78
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-06-28 08:35:09 -0100

    Formatting, final etc

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/IOTestUtil.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/IOTestUtil.java
@@ -12,6 +12,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.jdt.annotation.NonNull;
+import org.junit.Assert;
 
 import com.mmxlabs.models.lng.parameters.OptimiserSettings;
 import com.mmxlabs.models.lng.scenario.exportWizards.ExportCSVWizard;
@@ -26,160 +28,163 @@ import com.mmxlabs.models.lng.transformer.ui.LNGScenarioRunner;
 import com.mmxlabs.scenario.service.model.ScenarioInstance;
 
 public class IOTestUtil {
-	
-	
-	public static String[] ListToArray(EList<CargoAllocation> originalCargoAllocations){
-			
-			int size = originalCargoAllocations.size();
-			String[] array = new String[size];
-			
-			int i = 0;
-			for (CargoAllocation temp :  originalCargoAllocations) {
-				array[i] = (temp.getName());
-				i += 1; 
-			}
-			return array;
-			
+
+	public static String[] ListToArray(final EList<CargoAllocation> originalCargoAllocations) {
+
+		final int size = originalCargoAllocations.size();
+		final String[] array = new String[size];
+
+		int i = 0;
+		for (final CargoAllocation temp : originalCargoAllocations) {
+			array[i] = (temp.getName());
+			i += 1;
 		}
-	
-	
-	public static ArrayList makeSlotAllocationArray(EList<CargoAllocation> originalCargoAllocations){
-		
-		ArrayList output = new ArrayList();
-		
-		for(CargoAllocation ca: originalCargoAllocations){
-			EList<SlotAllocation> allocations = ca.getSlotAllocations();
-			
-			for (SlotAllocation sa : allocations){
+		return array;
+
+	}
+
+	public static ArrayList makeSlotAllocationArray(final EList<CargoAllocation> originalCargoAllocations) {
+
+		final ArrayList output = new ArrayList();
+
+		for (final CargoAllocation ca : originalCargoAllocations) {
+			final EList<SlotAllocation> allocations = ca.getSlotAllocations();
+
+			for (final SlotAllocation sa : allocations) {
 				output.add(sa.getPort().getLocation());
-				//output.add(sa.getSlot().getCargo().getLoadName());
+				// output.add(sa.getSlot().getCargo().getLoadName());
 			}
 		}
 		return output;
 	}
-	
+
 	/**
 	 * Converts EList<Fitness> into long[] without headers for easy comparison.
-	 * @param fitnesses - Input EList of fitness values
+	 * 
+	 * @param fitnesses
+	 *            - Input EList of fitness values
 	 * @return array - Output array of fitness longs
 	 */
-	public static long[] fitnessListToArrayValues(EList<Fitness> fitnesses){
-		
-		int size = fitnesses.size();
-		long[] array = new long[size];
-		
+	public static long[] fitnessListToArrayValues(final EList<Fitness> fitnesses) {
+
+		final int size = fitnesses.size();
+		final long[] array = new long[size];
+
 		int i = 0;
-		for (Fitness temp : fitnesses) {
+		for (final Fitness temp : fitnesses) {
 			array[i] = (temp.getFitnessValue());
-			i += 1; 
+			i += 1;
 		}
 		return array;
-		
+
 	}
+
 	/**
-	 *  Deletes the temporary directory and all its contents
+	 * Deletes the temporary directory and all its contents
 	 */
-	public static void tempDirectoryTeardown(){
-		
-		File tempDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old");
-		
-		String[]entries =	tempDir.list();
-		for(String s: entries){
-		    File currentFile = new File(tempDir.getPath(),s);
-		    currentFile.delete();
+	public static void tempDirectoryTeardown() {
+
+		final File tempDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old");
+
+		final String[] entries = tempDir.list();
+		for (final String s : entries) {
+			final File currentFile = new File(tempDir.getPath(), s);
+			currentFile.delete();
 		}
-		
+
 		tempDir.delete();
 	}
-	
-	
-	
+
 	/**
-	 *  Takes the URL of a scenario and returns the associated LNGScenarioModel
-	 * @param url - URL of the scenario files
+	 * Takes the URL of a scenario and returns the associated LNGScenarioModel
+	 * 
+	 * @param url
+	 *            - URL of the scenario files
 	 * @return - LNGScenarioModel object created from the target files
 	 * @throws MalformedURLException
 	 */
-	public static LNGScenarioModel URLtoScenarioCSV(URL url) throws MalformedURLException{
-		
-		CSVTestDataProvider csv_tdp = new CSVTestDataProvider(url);
-	
-		LNGScenarioModel testCase = csv_tdp.getScenarioModel();
+	public static LNGScenarioModel URLtoScenarioCSV(final URL url) throws MalformedURLException {
+
+		final CSVTestDataProvider csv_tdp = new CSVTestDataProvider(url);
+
+		final LNGScenarioModel testCase = csv_tdp.getScenarioModel();
 
 		return testCase;
-		
+
 	}
-	
-	
-	
-	public static LNGScenarioModel URLtoScenarioLiNGO(URL url) throws Exception{
-		
-		AbstractOptimisationResultTester AORT = new AbstractOptimisationResultTester();
-			
-			final ScenarioInstance instance = AORT.loadScenario(url);
-		
-			LNGScenarioModel testCase =  (LNGScenarioModel) instance.getInstance();
-	
-			return testCase;
-			
-		}
-	
-	
+
+	public static LNGScenarioModel URLtoScenarioLiNGO(final URL url) throws Exception {
+
+		final AbstractOptimisationResultTester AORT = new AbstractOptimisationResultTester();
+
+		final ScenarioInstance instance = AORT.loadScenario(url);
+
+		final LNGScenarioModel testCase = (LNGScenarioModel) instance.getInstance();
+
+		return testCase;
+
+	}
+
 	/**
-	 *  Produces an EList of the fitnesses from the passed LNGScenerioModel
-	 * @param model - LNGScenarioModel to acquire fitnesses of
+	 * Produces an EList of the fitnesses from the passed LNGScenerioModel
+	 * 
+	 * @param model
+	 *            - LNGScenarioModel to acquire fitnesses of
 	 * @return - EList<Fitness> of the associated fitnesses.
 	 */
-	public static EList<Fitness> ScenarioModeltoFitnessList(LNGScenarioModel model){
-		
+	public static EList<Fitness> ScenarioModeltoFitnessList(@NonNull final LNGScenarioModel model) {
+
 		final OptimiserSettings settings = LNGScenarioRunnerCreator.createExtendedSettings(ScenarioUtils.createDefaultSettings());
 		final ExecutorService executorService = Executors.newSingleThreadExecutor();
-		
-		LNGScenarioRunner runner = LNGScenarioRunnerCreator.createScenarioRunnerWithLSO(executorService, model, settings);
+
+		final LNGScenarioRunner runner = LNGScenarioRunnerCreator.createScenarioRunnerWithLSO(executorService, model, settings);
 
 		final Schedule schedule = runner.getSchedule();
-		
+		Assert.assertNotNull(schedule);
+
 		final EList<Fitness> fitnesses = schedule.getFitnesses();
-		
+
 		return fitnesses;
 	}
-	
-public static Schedule ScenarioModeltoSchedule(LNGScenarioModel model){
-		
+
+	public static Schedule ScenarioModeltoSchedule(final LNGScenarioModel model) {
+
 		final OptimiserSettings settings = LNGScenarioRunnerCreator.createExtendedSettings(ScenarioUtils.createDefaultSettings());
 		final ExecutorService executorService = Executors.newSingleThreadExecutor();
-		
-		LNGScenarioRunner runner = LNGScenarioRunnerCreator.createScenarioRunnerWithLSO(executorService, model, settings);
+
+		final LNGScenarioRunner runner = LNGScenarioRunnerCreator.createScenarioRunnerWithLSO(executorService, model, settings);
 
 		final Schedule schedule = runner.getSchedule();
-		
+
 		return schedule;
 	}
-	
+
 	/**
-	 *  Exports the passed LNGScenarioModel to a temporary directory 
-	 * @param model - the LNGScenarioModel to be written to the temporary directory
+	 * Exports the passed LNGScenarioModel to a temporary directory
+	 * 
+	 * @param model
+	 *            - the LNGScenarioModel to be written to the temporary directory
 	 * @return - URL by which to locate the temporary directory
 	 * @throws MalformedURLException
 	 */
-	public static URL exportTestCase(LNGScenarioModel model) throws MalformedURLException{
-		
-		ExportCSVWizard ECSVW = new ExportCSVWizard();
-		
-		exportInformation info = ECSVW.new exportInformation();
-		
-		File tempDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old");
-		
+	public static URL exportTestCase(final LNGScenarioModel model) throws MalformedURLException {
+
+		final ExportCSVWizard ECSVW = new ExportCSVWizard();
+
+		final exportInformation info = ECSVW.new exportInformation();
+
+		final File tempDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old");
+
 		info.outputDirectory = tempDir;
 		info.delimiter = ',';
 		info.decimalSeparator = '.';
-	
-		ECSVW.exportScenario(model, info,true,"holder" );
-		
-		File restoreDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old/holder");
 
-		URL restoreURL = restoreDir.toURI().toURL();
-		
+		ECSVW.exportScenario(model, info, true, "holder");
+
+		final File restoreDir = new File(System.getProperty("java.io.tmpdir", null), "tempdir-old/holder");
+
+		final URL restoreURL = restoreDir.toURI().toURL();
+
 		return restoreURL;
 	}
 
