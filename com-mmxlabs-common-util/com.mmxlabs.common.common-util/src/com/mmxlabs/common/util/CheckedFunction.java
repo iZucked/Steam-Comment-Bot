0310de2914d321a2daa5bbab7a678e7b3aba9269
Jenkins <unknown@kiln.example.com>
2016-07-11 22:19:16 -0100

    Update headers

--- com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/util/CheckedFunction.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/util/CheckedFunction.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2016
+ * All rights reserved.
+ */
 package com.mmxlabs.common.util;
 @FunctionalInterface
 	public interface CheckedFunction<T, R, E extends Exception> {
