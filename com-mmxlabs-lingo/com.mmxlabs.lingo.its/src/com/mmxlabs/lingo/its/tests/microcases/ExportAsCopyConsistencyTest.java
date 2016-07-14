dd036a707724a0279e530725763573a5086159c1
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-07-14 07:46:30 -0100

    Add new test case to catch issues with exportAsCopy.
    BugzId: 2091

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/ExportAsCopyConsistencyTest.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/ExportAsCopyConsistencyTest.java
@@ -5,6 +5,7 @@
 package com.mmxlabs.lingo.its.tests.microcases;
 
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,17 +17,15 @@ import com.mmxlabs.lingo.its.tests.category.RegressionTest;
 import com.mmxlabs.models.lng.cargo.CharterOutEvent;
 import com.mmxlabs.models.lng.cargo.VesselAvailability;
 import com.mmxlabs.models.lng.cargo.VesselEvent;
-import com.mmxlabs.models.lng.cargo.util.CargoModelBuilder;
 import com.mmxlabs.models.lng.commercial.BaseLegalEntity;
-import com.mmxlabs.models.lng.commercial.util.CommercialModelFinder;
 import com.mmxlabs.models.lng.fleet.Vessel;
 import com.mmxlabs.models.lng.fleet.VesselClass;
-import com.mmxlabs.models.lng.fleet.util.FleetModelBuilder;
-import com.mmxlabs.models.lng.fleet.util.FleetModelFinder;
-import com.mmxlabs.models.lng.port.util.PortModelFinder;
+import com.mmxlabs.models.lng.parameters.OptimisationRange;
+import com.mmxlabs.models.lng.parameters.ParametersFactory;
+import com.mmxlabs.models.lng.pricing.BaseFuelCost;
+import com.mmxlabs.models.lng.pricing.CostModel;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
-import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelBuilder;
-import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelFinder;
+import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelUtil;
 import com.mmxlabs.models.lng.transformer.its.ShiroRunner;
 import com.mmxlabs.models.lng.transformer.ui.LNGScenarioToOptimiserBridge;
 
@@ -57,6 +56,7 @@ public class ExportAsCopyConsistencyTest extends AbstractMicroTestCase {
 				.build();
 
 		// Create a single charter out event
+		@SuppressWarnings("unused")
 		final CharterOutEvent charterOutEvent = cargoModelBuilder
 				.makeCharterOutEvent("charter-1", LocalDateTime.of(2015, 5, 1, 0, 0, 0), LocalDateTime.of(2015, 5, 1, 0, 0, 0), portFinder.findPort("Ras Laffan")) //
 				.withRelocatePort(portFinder.findPort("Isle of Grain")) //
@@ -74,6 +74,60 @@ public class ExportAsCopyConsistencyTest extends AbstractMicroTestCase {
 
 			final VesselEvent event = copy.getCargoModel().getVesselEvents().get(0);
 			Assert.assertEquals(1, event.getAllowedVessels().size());
+		});
+	}
+
+	/**
+	 * See BugzId: 2091
+	 * 
+	 */
+	@Test
+	@Category({ QuickTest.class, RegressionTest.class })
+	public void testExportAsCopyExportsBaseFuelCostsCorrectlyInPeriod() throws Exception {
+
+		evaluateWithLSOTest(false, settings -> {
+			OptimisationRange range = settings.getRange();
+			if (range == null) {
+				range = ParametersFactory.eINSTANCE.createOptimisationRange();
+				settings.setRange(range);
+			}
+			range.setOptimiseAfter(YearMonth.of(2016, 5));
+			range.setOptimiseBefore(YearMonth.of(2016, 6));
+		}, null, scenarioRunner -> {
+
+			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
+
+			// FB2091 - will fail at this point
+			final LNGScenarioModel copy = scenarioToOptimiserBridge.exportAsCopy(scenarioToOptimiserBridge.getDataTransformer().getInitialSequences(), null);
+
+			final CostModel costModel = ScenarioModelUtil.getCostModel(copy);
+			for (final BaseFuelCost bfc : costModel.getBaseFuelCosts()) {
+				Assert.assertNotNull(bfc.getFuel());
+				Assert.assertNotNull(bfc.getIndex());
+			}
+		}, null);
+	}
+
+	/**
+	 * See BugzId: 2091 -- This bug was not present in a non-period optimisation
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	@Category({ QuickTest.class, RegressionTest.class })
+	public void testExportAsCopyExportsBaseFuelCostsCorrectly() throws Exception {
+
+		evaluateTest(null, null, scenarioRunner -> {
+
+			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
+
+			final LNGScenarioModel copy = scenarioToOptimiserBridge.exportAsCopy(scenarioToOptimiserBridge.getDataTransformer().getInitialSequences(), null);
+
+			final CostModel costModel = ScenarioModelUtil.getCostModel(copy);
+			for (final BaseFuelCost bfc : costModel.getBaseFuelCosts()) {
+				Assert.assertNotNull(bfc.getFuel());
+				Assert.assertNotNull(bfc.getIndex());
+			}
 		});
 	}
 }
