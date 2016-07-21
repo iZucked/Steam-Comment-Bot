78a143873eff612a73d1dcf05e98528166197cb3
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-07-21 11:03:28 -0100

    Change model factory to use a collection rather than selection object

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/modelfactories/IModelFactory.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/modelfactories/IModelFactory.java
@@ -8,7 +8,8 @@ import java.util.Collection;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 
@@ -16,36 +17,46 @@ public interface IModelFactory {
 	/**
 	 * After being instantiated from an extension point this method will be called
 	 * 
-	 * @param ID the extension point ID
-	 * @param outputEClassName the name of the output EClass
-	 * @param label a nice label for this in the UI
-	 * @param replacementEClass 
-	 * @param replacementEReference 
+	 * @param ID
+	 *            the extension point ID
+	 * @param outputEClassName
+	 *            the name of the output EClass
+	 * @param label
+	 *            a nice label for this in the UI
+	 * @param replacementEClass
+	 * @param replacementEReference
 	 */
 	public void initFromExtension(final String ID, final String label, final String prototype, String replacementEReference, String replacementEClass);
-	
+
 	/**
 	 * Get the display label for this factory.
+	 * 
 	 * @return
 	 */
 	public String getLabel();
-	
+
 	/**
 	 * Add an instance to the scenario, and return the main part of the instance (this can add other objects as well, if it wants)
 	 * 
-	 * @param rootObject the root object
-	 * @param container the object where the output should end up being contained
-	 * @param containment the reference in which the output should be contained
-	 * @param iSelection 
+	 * @param rootObject
+	 *            the root object
+	 * @param container
+	 *            the object where the output should end up being contained
+	 * @param containment
+	 *            the reference in which the output should be contained
+	 * @param iSelection
 	 * 
 	 * @return null if the operation did not happen, or the main part of whatever has been created (for example the cargo if a cargo has been created with some slots).
 	 */
-	public Collection<? extends ISetting> createInstance(final MMXRootObject rootObject, final EObject container, final EReference containment, ISelection iSelection);
+	public Collection<? extends ISetting> createInstance(final MMXRootObject rootObject, final EObject container, final EReference containment, @Nullable Collection<@NonNull EObject> selection);
 
 	public interface ISetting {
 		public EObject getInstance();
+
 		public EObject getContainer();
+
 		public EReference getContainment();
-		public ISelection getSelection();
+
+		public @Nullable Collection<@NonNull EObject> getSelection();
 	}
 }
