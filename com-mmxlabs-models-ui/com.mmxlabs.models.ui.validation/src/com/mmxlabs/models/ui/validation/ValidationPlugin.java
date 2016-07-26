dd1df74ecd47df6ae79d3b23ca464941b05c3734
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-07-26 11:43:44 -0100

    Clean up validation service use

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/ValidationPlugin.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui.validation/src/com/mmxlabs/models/ui/validation/ValidationPlugin.java
@@ -16,7 +16,6 @@ import org.osgi.util.tracker.ServiceTracker;
  */
 public abstract class ValidationPlugin extends AbstractUIPlugin {
 	private ServiceTracker<IValidationService, IValidationService> validationDataTracker;
-	private ServiceTracker<IValidationService, IValidationService> validationServiceTracker;
 
 	public IExtraValidationContext getExtraValidationContext() {
 		final IValidationService service = validationDataTracker.getService();
@@ -25,29 +24,17 @@ public abstract class ValidationPlugin extends AbstractUIPlugin {
 		return service.getExtraContext();
 	}
 
-	/**
-	 */
-	public IValidationService getValidationService() {
-		return validationServiceTracker.getService();
-	}
-
 	@Override
 	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		validationDataTracker = new ServiceTracker<IValidationService, IValidationService>(context, IValidationService.class, null);
 		validationDataTracker.open();
-
-		validationServiceTracker = new ServiceTracker<IValidationService, IValidationService>(context, IValidationService.class.getName(), null);
-		validationServiceTracker.open();
 	}
 
 	@Override
 	public void stop(final BundleContext context) throws Exception {
 		validationDataTracker.close();
 		validationDataTracker = null;
-
-		validationServiceTracker.close();
-		validationServiceTracker = null;
 		super.stop(context);
 	}
 }
