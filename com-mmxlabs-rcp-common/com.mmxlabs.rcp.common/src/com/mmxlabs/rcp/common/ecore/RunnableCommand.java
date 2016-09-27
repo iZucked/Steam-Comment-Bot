53d280615e4f0e72ff73526cddbb8969f5a4e300
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-09-27 20:08:23 +0100

    Create new RunnableCommand to allow state to be released after command execution.
    BugzId: 2219

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/ecore/RunnableCommand.java
@@ -0,0 +1,53 @@
+package com.mmxlabs.rcp.common.ecore;
+
+import java.util.function.Consumer;
+
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Sub-class of {@link CompoundCommand} taking a {@link Consumer} (taking the {@link RunnableCommand} instance) to implement the {@link #execute()} method.After execution the consumer reference is
+ * nulled allowing the GC to clean up.
+ * 
+ * @author Simon Goodall
+ *
+ */
+public class RunnableCommand extends CompoundCommand {
+
+	private Consumer<RunnableCommand> runnable;
+
+	public RunnableCommand(final String label, final @NonNull Consumer<RunnableCommand> runnable) {
+		super(label);
+		this.runnable = runnable;
+	}
+
+	public RunnableCommand(final @NonNull Consumer<RunnableCommand> runnable) {
+		this.runnable = runnable;
+	}
+
+	@Override
+	protected boolean prepare() {
+		// Always prepared
+		return true;
+	}
+
+	@Override
+	public boolean canExecute() {
+		// Always true
+		return true;
+	}
+
+	@Override
+	public void execute() {
+
+		final Consumer<RunnableCommand> pRunnable = runnable;
+		if (pRunnable == null) {
+			throw new RuntimeException("Runnable is null. Already executed?");
+		}
+		try {
+			pRunnable.accept(this);
+		} finally {
+			runnable = null;
+		}
+	}
+}
