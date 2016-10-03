2fe1129d113c893fd436e0d78f8937f9db9a01e4
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-10-03 14:03:46 +0100

    Better validation -> edit hooks.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/ui/editorpart/FleetModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.editor/src/com/mmxlabs/models/lng/fleet/ui/editorpart/FleetModelEditorContribution.java
@@ -4,11 +4,23 @@
  */
 package com.mmxlabs.models.lng.fleet.ui.editorpart;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.internal.e4.compatibility.CompatibilityView;
 
+import com.mmxlabs.models.lng.fleet.BaseFuel;
 import com.mmxlabs.models.lng.fleet.FleetModel;
+import com.mmxlabs.models.lng.fleet.editor.views.VesselAndClassView;
+import com.mmxlabs.models.lng.pricing.BaseFuelCost;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
@@ -31,6 +43,13 @@ public class FleetModelEditorContribution extends BaseJointModelEditorContributi
 		if (status instanceof DetailConstraintStatusDecorator) {
 			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
 			final EObject target = dcsd.getTarget();
+
+			if (target instanceof BaseFuelCost) {
+				return true;
+			} else if (target instanceof BaseFuel) {
+				return true;
+
+			}
 		}
 
 		return false;
@@ -40,6 +59,52 @@ public class FleetModelEditorContribution extends BaseJointModelEditorContributi
 	public void handle(final IStatus status) {
 		if (status instanceof DetailConstraintStatusDecorator) {
 			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+
+			final EObject target = dcsd.getTarget();
+
+			if (target instanceof BaseFuel || target instanceof BaseFuelCost) {
+				// @Inject
+				// private IEventBroker eventBroker;
+				//
+				// @Inject
+				// private ESelectionService selectionService;
+				//
+				// @Inject
+				// private EPartService partService;
+				//
+				// @Inject
+				// private EModelService modelService;
+				// @Inject
+				// private MApplication application;
+				// @Inject
+				// private IScenarioServiceSelectionProvider scenarioSelectionProvider;
+
+				final EModelService modelService = editorPart.getSite().getService(EModelService.class);
+				final EPartService partService = editorPart.getSite().getService(EPartService.class);
+				final MApplication application = editorPart.getSite().getService(MApplication.class);
+
+				// Switch perspective
+				final List<MPerspective> perspectives = modelService.findElements(application, null, MPerspective.class, null);
+				for (final MPerspective p : perspectives) {
+					if (p.getElementId().equals("com.mmxlabs.lingo.app.perspective.editing")) {
+						partService.switchPerspective(p);
+					}
+				}
+
+				// Activate change set view
+				final MPart part = partService.showPart("com.mmxlabs.models.lng.fleet.editor.views.VesselAndClassView", PartState.ACTIVATE);
+				if (part != null && part.getObject() != null) {
+					Object oPart = part.getObject();
+					if (oPart instanceof CompatibilityView) {
+						oPart = ((CompatibilityView) oPart).getView();
+
+					}
+					if (oPart instanceof VesselAndClassView) {
+						final VesselAndClassView vesselViewerPane_View = (VesselAndClassView) oPart;
+						vesselViewerPane_View.editObject(target);
+					}
+				}
+			}
 		}
 	}
 }
