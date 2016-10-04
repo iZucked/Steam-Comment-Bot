0bc3c1ea753339617bb8d58778c495ec45668f30
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-10-04 17:46:17 +0100

    Initial WIP break-evens / option modelling UI etc
    BugzId: 1200

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/analytics/AnalyticsScenarioEvaluator.java
@@ -0,0 +1,108 @@
+package com.mmxlabs.models.lng.transformer.ui.analytics;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.mmxlabs.models.lng.analytics.services.IAnalyticsScenarioEvaluator;
+import com.mmxlabs.models.lng.parameters.OptimisationPlan;
+import com.mmxlabs.models.lng.parameters.UserSettings;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.transformer.ui.LNGScenarioRunner;
+import com.mmxlabs.models.lng.transformer.ui.LNGScenarioRunnerUtils;
+import com.mmxlabs.models.lng.transformer.ui.OptimisationHelper;
+import com.mmxlabs.models.lng.transformer.util.LNGSchedulerJobUtils;
+import com.mmxlabs.scenario.service.IScenarioService;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+public class AnalyticsScenarioEvaluator implements IAnalyticsScenarioEvaluator {
+
+	@Override
+	public void evaluate(@org.eclipse.jdt.annotation.NonNull final LNGScenarioModel lngScenarioModel, @org.eclipse.jdt.annotation.NonNull final UserSettings userSettings,
+			@Nullable final ScenarioInstance parentForFork) {
+
+		OptimisationPlan optimisationPlan = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimisationPlan = LNGScenarioRunnerUtils.createExtendedSettings(optimisationPlan);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newFixedThreadPool(1);
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, null, optimisationPlan, LNGSchedulerJobUtils.createLocalEditingDomain(), null, null, null,
+					false);
+
+			scenarioRunner.evaluateInitialState();
+			if (parentForFork != null) {
+				final IScenarioService scenarioService = parentForFork.getScenarioService();
+
+				final ScenarioInstance dup = scenarioService.insert(parentForFork, EcoreUtil.copy(lngScenarioModel));
+				dup.setName("Base Case");
+
+				// Copy across various bits of information
+				dup.getMetadata().setContentType(parentForFork.getMetadata().getContentType());
+				dup.getMetadata().setCreated(parentForFork.getMetadata().getCreated());
+				dup.getMetadata().setLastModified(new Date());
+
+				// Copy version context information
+				dup.setVersionContext(parentForFork.getVersionContext());
+				dup.setScenarioVersion(parentForFork.getScenarioVersion());
+
+				dup.setClientVersionContext(parentForFork.getClientVersionContext());
+				dup.setClientScenarioVersion(parentForFork.getClientScenarioVersion());
+			}
+
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			executorService.shutdownNow();
+		}
+
+	}
+
+	@Override
+	public void breakEvenEvaluate(@NonNull LNGScenarioModel lngScenarioModel, @NonNull UserSettings userSettings, @Nullable ScenarioInstance parentForFork, long targetProfitAndLoss) {
+		OptimisationPlan optimisationPlan = OptimisationHelper.transformUserSettings(userSettings, null, lngScenarioModel);
+		optimisationPlan = LNGScenarioRunnerUtils.createExtendedSettings(optimisationPlan);
+
+		// Generate internal data
+		final ExecutorService executorService = Executors.newFixedThreadPool(1);
+		try {
+
+			final LNGScenarioRunner scenarioRunner = new LNGScenarioRunner(executorService, lngScenarioModel, null, optimisationPlan, LNGSchedulerJobUtils.createLocalEditingDomain(), null, null, null,
+					false);
+
+			scenarioRunner.evaluateInitialState();
+			if (parentForFork != null) {
+				final IScenarioService scenarioService = parentForFork.getScenarioService();
+
+				final ScenarioInstance dup = scenarioService.insert(parentForFork, EcoreUtil.copy(lngScenarioModel));
+				dup.setName("What if");
+
+				// Copy across various bits of information
+				dup.getMetadata().setContentType(parentForFork.getMetadata().getContentType());
+				dup.getMetadata().setCreated(parentForFork.getMetadata().getCreated());
+				dup.getMetadata().setLastModified(new Date());
+
+				// Copy version context information
+				dup.setVersionContext(parentForFork.getVersionContext());
+				dup.setScenarioVersion(parentForFork.getScenarioVersion());
+
+				dup.setClientVersionContext(parentForFork.getClientVersionContext());
+				dup.setClientScenarioVersion(parentForFork.getClientScenarioVersion());
+			}
+
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			executorService.shutdownNow();
+		}
+
+	}
+}
