b3548b7a005eda7164d8b965cca228ec43a212da
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-10-07 11:23:29 +0100

    Validation for duplicated slot use.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/PartialCaseConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/PartialCaseConstraint.java
@@ -1,15 +1,30 @@
 package com.mmxlabs.models.lng.analytics.validation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.IConstraintStatus;
 import org.eclipse.jdt.annotation.NonNull;
 
+import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
+import com.mmxlabs.models.lng.analytics.BaseCase;
+import com.mmxlabs.models.lng.analytics.BaseCaseRow;
+import com.mmxlabs.models.lng.analytics.BuyOption;
+import com.mmxlabs.models.lng.analytics.BuyReference;
 import com.mmxlabs.models.lng.analytics.PartialCase;
+import com.mmxlabs.models.lng.analytics.PartialCaseRow;
+import com.mmxlabs.models.lng.analytics.SellOption;
+import com.mmxlabs.models.lng.analytics.SellReference;
 import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 
 public class PartialCaseConstraint extends AbstractModelMultiConstraint {
@@ -21,22 +36,65 @@ public class PartialCaseConstraint extends AbstractModelMultiConstraint {
 		if (target instanceof PartialCase) {
 			final PartialCase partialCase = (PartialCase) target;
 
-//			ShippingType nonShipped = AnalyticsBuilder.isNonShipped(baseCaseRow);
-//			if (nonShipped == ShippingType.Shipped) {
-//				if (baseCaseRow.getShipping() == null) {
-//					final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Base case - no shipping option defined."));
-//					deco.addEObjectAndFeature(baseCaseRow, AnalyticsPackage.Literals.BASE_CASE_ROW__SHIPPING);
-//					statuses.add(deco);
-//				}
-//				if (!(baseCaseRow.getShipping() instanceof FleetShippingOption || baseCaseRow.getShipping() instanceof RoundTripShippingOption)) {
-//					final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("Base case - incompatible shipping option defined."));
-//					deco.addEObjectAndFeature(baseCaseRow, AnalyticsPackage.Literals.BASE_CASE_ROW__SHIPPING);
-//					statuses.add(deco);
-//				}
-//			}
+			final Set<LoadSlot> loadSlots = new HashSet<>();
+			final Set<DischargeSlot> dischargeSlots = new HashSet<>();
+
+			final Set<LoadSlot> duplicatedLoadSlots = new HashSet<>();
+			final Set<DischargeSlot> duplicatdDischargeSlots = new HashSet<>();
+
+			// First pass, find problem slots
+			processBaseCase(partialCase, (row, slot) -> {
+				if (!loadSlots.add(slot)) {
+					duplicatedLoadSlots.add(slot);
+				}
+			}, (row, slot) -> {
+				if (!dischargeSlots.add(slot)) {
+					duplicatdDischargeSlots.add(slot);
+				}
+			});
+			// Second pass, report problem slots
+			processBaseCase(partialCase, (row, slot) -> {
+				if (duplicatedLoadSlots.contains(slot)) {
+					final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("What if? - existing slot used multiple times."));
+					deco.addEObjectAndFeature(row, AnalyticsPackage.Literals.PARTIAL_CASE_ROW__BUY_OPTIONS);
+					statuses.add(deco);
+				}
+			}, (row, slot) -> {
+				if (duplicatdDischargeSlots.contains(slot)) {
+					final DetailConstraintStatusDecorator deco = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("What if? - existing slot used multiple times."));
+					deco.addEObjectAndFeature(row, AnalyticsPackage.Literals.PARTIAL_CASE_ROW__SELL_OPTIONS);
+					statuses.add(deco);
+				}
+			});
 		}
 
 		return Activator.PLUGIN_ID;
 	}
 
+	public void processBaseCase(final PartialCase partialCase, final BiConsumer<PartialCaseRow, LoadSlot> visitLoadSlot, final BiConsumer<PartialCaseRow, DischargeSlot> visitDischargeSlot) {
+		for (final PartialCaseRow row : partialCase.getPartialCase()) {
+			{
+				for (final BuyOption buy : row.getBuyOptions()) {
+					if (buy instanceof BuyReference) {
+						final BuyReference buyReference = (BuyReference) buy;
+						final LoadSlot slot = buyReference.getSlot();
+						if (slot != null) {
+							visitLoadSlot.accept(row, slot);
+						}
+					}
+				}
+			}
+			{
+				for (final SellOption sell : row.getSellOptions()) {
+					if (sell instanceof SellReference) {
+						final SellReference sellReference = (SellReference) sell;
+						final DischargeSlot slot = sellReference.getSlot();
+						if (slot != null) {
+							visitDischargeSlot.accept(row, slot);
+						}
+					}
+				}
+			}
+		}
+	}
 }
