157511ee2b4d0cc20d6696c2b2bfc7d13dc98a88
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-10-07 09:30:49 +0100

    use correct formatter for sells

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/ui/views/OptionModellerView.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/ui/views/OptionModellerView.java
@@ -855,7 +855,7 @@ public class OptionModellerView extends ScenarioInstanceView implements CommandS
 		baseCaseViewer.getGrid().setRowHeaderVisible(true);
 
 		createColumn(baseCaseViewer, "Buy", new BuyOptionDescriptionFormatter(), AnalyticsPackage.Literals.BASE_CASE_ROW__BUY_OPTION);
-		createColumn(baseCaseViewer, "Sell", new BuyOptionDescriptionFormatter(), AnalyticsPackage.Literals.BASE_CASE_ROW__SELL_OPTION);
+		createColumn(baseCaseViewer, "Sell", new SellOptionDescriptionFormatter(), AnalyticsPackage.Literals.BASE_CASE_ROW__SELL_OPTION);
 		createColumn(baseCaseViewer, "Shipping", new ShippingOptionDescriptionFormatter(), AnalyticsPackage.Literals.BASE_CASE_ROW__SHIPPING);
 
 		baseCaseViewer.getGrid().setCellSelectionEnabled(true);
