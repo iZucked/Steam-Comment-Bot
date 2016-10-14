aa8529d10157b1c42aa1dbc997f6572bd81f9ce7
hudson <hudson@minimaxlabs.com>
2016-10-14 16:17:17 +0100

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/parseutils/IExposuredExpressionProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/parseutils/IExposuredExpressionProvider.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2016
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.commercial.parseutils;
 
 import org.eclipse.jdt.annotation.NonNull;
