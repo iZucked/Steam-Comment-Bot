b6ab62cff7822648a423732cc4c46ea7f22e95ce
hudson <hudson@minimaxlabs.com>
2016-10-19 17:35:32 +0100

    Update headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser.tests/src/com/mmxlabs/scheduler/optimiser/constraints/impl/CapacityEvaluatedStateCheckerTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser.tests/src/com/mmxlabs/scheduler/optimiser/constraints/impl/CapacityEvaluatedStateCheckerTest.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2016
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.constraints.impl;
 
 import static org.junit.Assert.assertEquals;
