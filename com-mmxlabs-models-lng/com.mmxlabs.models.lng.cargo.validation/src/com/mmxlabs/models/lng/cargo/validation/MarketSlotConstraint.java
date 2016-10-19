90d31243f4f8288df981ea2c8647e4f076a10420
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-10-19 09:45:13 +0100

    Expand out cases to forbid users from changing values unexpectedly.
    BugzId: 2251

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/MarketSlotConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/MarketSlotConstraint.java
@@ -4,6 +4,7 @@
  */
 package com.mmxlabs.models.lng.cargo.validation;
 
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -16,6 +17,8 @@ import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.validation.model.IConstraintStatus;
 
+import com.mmxlabs.common.time.Hours;
+import com.mmxlabs.common.time.Months;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.SpotDischargeSlot;
@@ -29,6 +32,7 @@ import com.mmxlabs.models.lng.spotmarkets.FOBPurchasesMarket;
 import com.mmxlabs.models.lng.spotmarkets.FOBSalesMarket;
 import com.mmxlabs.models.lng.spotmarkets.SpotMarket;
 import com.mmxlabs.models.lng.types.APortSet;
+import com.mmxlabs.models.lng.types.TimePeriod;
 import com.mmxlabs.models.lng.types.util.SetUtils;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
@@ -45,8 +49,8 @@ public class MarketSlotConstraint extends AbstractModelConstraint {
 			final SpotSlot spotSlot = (SpotSlot) object;
 			final SpotMarket market = spotSlot.getMarket();
 			if (market == null) {
-				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + ((Slot) spotSlot).getName()
-						+ "] needs a market set."), IStatus.WARNING);
+				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+						(IConstraintStatus) ctx.createFailureStatus("[Market model|" + ((Slot) spotSlot).getName() + "] needs a market set."), IStatus.WARNING);
 				dsd.addEObjectAndFeature(spotSlot, CargoPackage.eINSTANCE.getSpotSlot_Market());
 				failures.add(dsd);
 			}
@@ -66,59 +70,98 @@ public class MarketSlotConstraint extends AbstractModelConstraint {
 			// Generic constraints
 			if (slot != null) {
 				if (slot.getContract() != null) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a contract set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a contract set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_Contract());
 					failures.add(dsd);
 				}
 
 				if (slot.isSetCancellationExpression()) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a cancellation fee set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a cancellation fee set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_CancellationExpression());
 					failures.add(dsd);
 				}
 
 				if (slot.getEntity() != null) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have an entity set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have an entity set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_Entity());
 					failures.add(dsd);
 				}
 				if (slot.getPriceExpression() != null) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a price expression set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a price expression set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PriceExpression());
 					failures.add(dsd);
 				}
 				if (slot.getPricingDate() != null) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a pricing date set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a pricing date set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PricingDate());
 					failures.add(dsd);
 				}
 				if (slot.isSetMinQuantity()) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a min quantity."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a min quantity."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_MinQuantity());
 					failures.add(dsd);
 				}
 				if (slot.isSetMaxQuantity()) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a max quantity."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a max quantity."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_MaxQuantity());
 					failures.add(dsd);
 				}
 				if (slot.isSetPricingEvent()) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have a pricing event."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have a pricing event."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_PricingEvent());
 					failures.add(dsd);
 				}
 				if (slot.getWindowFlex() != 0) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type
-							+ " should not have window flex."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should not have window flex."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowFlex());
+					failures.add(dsd);
+				}
+				if (slot.getWindowStart() != null && slot.getWindowStart().getDayOfMonth() != 1) {
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should start on the 1st of the month."));
+					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowStart());
+					failures.add(dsd);
+				}
+				if (!slot.isSetWindowStartTime() || slot.getWindowStartTime() != 0) {
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should have start time set to zero"));
+					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowStartTime());
+					failures.add(dsd);
+				}
+
+				boolean foundAltWindowSize = false;
+				int actual = slot.getWindowSize();
+				if (slot.isSetWindowSizeUnits() && slot.getWindowSizeUnits() == TimePeriod.HOURS || slot.getPort().getDefaultWindowSizeUnits() == TimePeriod.HOURS) {
+					if (slot.isSetWindowSize()) {
+						LocalDate windowStart = slot.getWindowStart();
+						int expected = Hours.between(windowStart, windowStart.plusMonths(1)) - 1;
+						if (expected == actual) {
+							foundAltWindowSize = true;
+						}
+					}
+				}
+
+				if (!slot.isSetWindowSizeUnits() || slot.getWindowSizeUnits() != TimePeriod.MONTHS) {
+					int eType = foundAltWindowSize ? IStatus.WARNING : IStatus.ERROR;
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should have a one month window"), eType);
+					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowSizeUnits());
+					failures.add(dsd);
+				}
+				if (!slot.isSetWindowSize() || actual != 1) {
+					int eType = foundAltWindowSize ? IStatus.WARNING : IStatus.ERROR;
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + slot.getName() + "] " + type + " should have a one month window"), eType);
+					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getSlot_WindowSize());
 					failures.add(dsd);
 				}
 			}
@@ -127,8 +170,8 @@ public class MarketSlotConstraint extends AbstractModelConstraint {
 				final SpotLoadSlot spotLoadSlot = (SpotLoadSlot) spotSlot;
 
 				if (spotLoadSlot.isSetCargoCV()) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotLoadSlot.getName() + "] " + type
-							+ " should not have CV set."));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotLoadSlot.getName() + "] " + type + " should not have CV set."));
 					dsd.addEObjectAndFeature(slot, CargoPackage.eINSTANCE.getLoadSlot_CargoCV());
 					failures.add(dsd);
 				}
@@ -140,8 +183,8 @@ public class MarketSlotConstraint extends AbstractModelConstraint {
 					final Set<Port> destinationPorts = SetUtils.getObjects(destinationPortSets);
 					if (!destinationPorts.contains(spotLoadSlot.getPort())) {
 
-						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotLoadSlot.getName()
-								+ "] DES purchase port is not a market port."), IStatus.WARNING);
+						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+								(IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotLoadSlot.getName() + "] DES purchase port is not a market port."), IStatus.WARNING);
 						dsd.addEObjectAndFeature(spotLoadSlot, CargoPackage.eINSTANCE.getSlot_Port());
 						failures.add(dsd);
 
@@ -180,8 +223,8 @@ public class MarketSlotConstraint extends AbstractModelConstraint {
 					final Set<Port> originPorts = SetUtils.getObjects(fobSalesMarket.getOriginPorts());
 					if (!originPorts.contains(spotDischargeSlot.getPort())) {
 
-						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotDischargeSlot.getName()
-								+ "] FOB sale port does not match market ports"), IStatus.WARNING);
+						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+								(IConstraintStatus) ctx.createFailureStatus("[Market model|" + spotDischargeSlot.getName() + "] FOB sale port does not match market ports"), IStatus.WARNING);
 						dsd.addEObjectAndFeature(spotDischargeSlot, CargoPackage.eINSTANCE.getSlot_Port());
 						failures.add(dsd);
 
