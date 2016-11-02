4f9e6f7dfba8363d004d4efd6f7dc38af3bd11e5
achurchill <alex.churchill@minimaxlabs.com>
2016-11-02 17:55:36 +0000

    Validation work

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingExpressionConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.validation/src/com/mmxlabs/models/lng/analytics/validation/ShippingExpressionConstraint.java
@@ -13,16 +13,12 @@ import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.validation.model.IConstraintStatus;
 
 import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
-import com.mmxlabs.models.lng.analytics.BuyOpportunity;
 import com.mmxlabs.models.lng.analytics.FleetShippingOption;
 import com.mmxlabs.models.lng.analytics.OptionalAvailabilityShippingOption;
-import com.mmxlabs.models.lng.analytics.SellOpportunity;
 import com.mmxlabs.models.lng.analytics.validation.internal.Activator;
-import com.mmxlabs.models.lng.port.Port;
 import com.mmxlabs.models.lng.pricing.util.PriceIndexUtils.PriceIndexType;
 import com.mmxlabs.models.lng.pricing.validation.utils.PriceExpressionUtils;
 import com.mmxlabs.models.lng.pricing.validation.utils.PriceExpressionUtils.ValidationResult;
-import com.mmxlabs.models.lng.types.PortCapability;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
@@ -82,65 +78,6 @@ public class ShippingExpressionConstraint extends AbstractModelMultiConstraint {
 					dsd.addEObjectAndFeature(shippingOption, AnalyticsPackage.Literals.OPTIONAL_AVAILABILITY_SHIPPING_OPTION__REPOSITIONING_FEE);
 					failures.add(dsd);
 				}
-			}
-		}
-		if (target instanceof SellOpportunity) {
-			final SellOpportunity slot = (SellOpportunity) target;
-			if (!"?".equals(slot.getPriceExpression())) {
-
-				final ValidationResult result = PriceExpressionUtils.validatePriceExpression(ctx, slot, AnalyticsPackage.eINSTANCE.getSellOpportunity_PriceExpression(), slot.getPriceExpression(),
-						PriceIndexType.COMMODITY);
-				if (!result.isOk()) {
-					final String message = String.format("%s", result.getErrorDetails());
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-
-					dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__PRICE_EXPRESSION);
-					failures.add(dsd);
-				}
-			}
-
-			final Port port = slot.getPort();
-			if (port == null) {
-				final String message = String.format("Sell has no port");
-				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-				dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__PORT);
-				failures.add(dsd);
-			} else {
-				if (slot.isFobSale()) {
-					if (!port.getCapabilities().contains(PortCapability.LOAD)) {
-						final String message = String.format("Sell port should be a load port");
-						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-						dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__PORT);
-						failures.add(dsd);
-					}
-				} else {
-					if (!port.getCapabilities().contains(PortCapability.DISCHARGE)) {
-						final String message = String.format("Sell port should be a discharge port");
-						final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-						dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__PORT);
-						failures.add(dsd);
-					}
-				}
-			}
-			if (slot.getDate() == null) {
-				final String message = String.format("Sell has no date");
-				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-				dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__DATE);
-				failures.add(dsd);
-			} else {
-				LocalDate date = slot.getDate();
-				if (date.getYear() < 1900) {
-					final String message = String.format("Sell has invalid year");
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-					dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__DATE);
-					failures.add(dsd);
-				}
-			}
-			if (slot.getEntity() == null && slot.getContract() == null) {
-				final String message = String.format("Sell has no entity");
-				final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message));
-				dsd.addEObjectAndFeature(slot, AnalyticsPackage.Literals.SELL_OPPORTUNITY__ENTITY);
-				failures.add(dsd);
 			}
 		}
 		return Activator.PLUGIN_ID;
