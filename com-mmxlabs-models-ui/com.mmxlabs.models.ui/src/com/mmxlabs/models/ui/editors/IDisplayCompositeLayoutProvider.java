2e54868efe4c9145e73bb6b8c556c9f789f982a7
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-11-09 11:21:01 +0000

    Tidy up  interface

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/IDisplayCompositeLayoutProvider.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/editors/IDisplayCompositeLayoutProvider.java
@@ -18,34 +18,36 @@ import com.mmxlabs.models.mmxcore.MMXRootObject;
  *
  */
 public interface IDisplayCompositeLayoutProvider {
-	public boolean showLabelFor(final MMXRootObject root, final EObject value, final IInlineEditor editor);
-	
+	boolean showLabelFor(MMXRootObject root, EObject value, IInlineEditor editor);
+
 	/**
-	 * Returns a "top level" editor layout which may include a number of specific "detail" layouts.
-	 * These "detail" layouts correspond to data sub-components which themselves require a
-	 * separate GUI editing sub-component
+	 * Returns a "top level" editor layout which may include a number of specific "detail" layouts. These "detail" layouts correspond to data sub-components which themselves require a separate GUI
+	 * editing sub-component
 	 * 
-	 * @param root The root object for the entire data model
-	 * @param object The object being edited
-	 * @param numDetailLayouts The number of GUI sub-components involved in the editor
+	 * @param root
+	 *            The root object for the entire data model
+	 * @param object
+	 *            The object being edited
+	 * @param numDetailLayouts
+	 *            The number of GUI sub-components involved in the editor
 	 * 
 	 * @return A Swing Layout object for the top GUI level in the editor component
 	 */
-	public Layout createTopLevelLayout(final MMXRootObject root, final EObject object, int numDetailLayouts);
-	
+	Layout createTopLevelLayout(MMXRootObject root, final EObject object, int numDetailLayouts);
+
 	/**
-	 * Returns a "detail" editor layout specifying how to lay out fields added to a specific
-	 * GUI sub-component
+	 * Returns a "detail" editor layout specifying how to lay out fields added to a specific GUI sub-component
 	 * 
-	 * @param root The root object for the entire data model
-	 * @param subObject The data sub-component which has been marked for editing in its own GUI sub-component 
+	 * @param root
+	 *            The root object for the entire data model
+	 * @param subObject
+	 *            The data sub-component which has been marked for editing in its own GUI sub-component
 	 * @return
 	 */
-	public Layout createDetailLayout(final MMXRootObject root, final EObject subObject);
-	
+	Layout createDetailLayout(MMXRootObject root, EObject subObject);
+
 	/**
-	 * Returns SWT layout data for the GUI control which edits a particular field of a particular object, 
-	 * in a GUI sub-component whose layout is controlled by createDetailLayout.   
+	 * Returns SWT layout data for the GUI control which edits a particular field of a particular object, in a GUI sub-component whose layout is controlled by createDetailLayout.
 	 * 
 	 * @param root
 	 * @param value
@@ -53,23 +55,10 @@ public interface IDisplayCompositeLayoutProvider {
 	 * @param control
 	 * @return
 	 */
-	public Object createEditorLayoutData(final MMXRootObject root, final EObject value, final IInlineEditor editor, final Control control);
+	Object createEditorLayoutData(MMXRootObject root, EObject value, IInlineEditor editor, Control control);
 
 	/**
 	 * Returns SWT layout data for the GUI label attached to a particular field editor.
-	 *  
-	 * @param root
-	 * @param value
-	 * @param editor
-	 * @param control
-	 * @param label
-	 * @return
-	 */
-	public Object createLabelLayoutData(final MMXRootObject root, final EObject value, final IInlineEditor editor, final Control control, Label label);
-
-	/**
-	 * Returns SWT layout data for the editor GUI sub-component which represents an entire 
-	 * data sub-component, within a top-level component whose layout is controlled by createTopLevelLayout. 
 	 * 
 	 * @param root
 	 * @param value
@@ -78,5 +67,17 @@ public interface IDisplayCompositeLayoutProvider {
 	 * @param label
 	 * @return
 	 */
-	public Object createTopLayoutData(final MMXRootObject root, final EObject value, final EObject detail);
+	Object createLabelLayoutData(MMXRootObject root, EObject value, final IInlineEditor editor, Control control, Label label);
+
+	/**
+	 * Returns SWT layout data for the editor GUI sub-component which represents an entire data sub-component, within a top-level component whose layout is controlled by createTopLevelLayout.
+	 * 
+	 * @param root
+	 * @param value
+	 * @param editor
+	 * @param control
+	 * @param label
+	 * @return
+	 */
+	Object createTopLayoutData(MMXRootObject root, EObject value, EObject detail);
 }
