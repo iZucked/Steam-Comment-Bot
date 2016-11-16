dbdf231f426930a08ffeb8641008f5cf90f5c099
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-11-16 14:02:15 +0000

    Add buy, sell and shipping entity columns to Schedule Summary.
    BugzId: 2275

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/formatters/Formatters.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/formatters/Formatters.java
@@ -8,13 +8,26 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
+import com.mmxlabs.models.mmxcore.NamedObject;
 import com.mmxlabs.models.ui.tabular.BaseFormatter;
 import com.mmxlabs.models.ui.tabular.ICellRenderer;
 
 public final class Formatters {
 
 	public static final ICellRenderer objectFormatter = new BaseFormatter();
+
+	public static final ICellRenderer namedObjectFormatter = new BaseFormatter() {
+		@Override
+		public @Nullable String render(final Object object) {
+			if (object instanceof NamedObject) {
+				final NamedObject namedObject = (NamedObject) object;
+				return namedObject.getName();
+			}
+			return super.render(object);
+		}
+	};
 
 	public static final ICellRenderer asLocalDateFormatter = new AsLocalDateFormatter(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 	public static final ICellRenderer asLocalTimeFormatter = new AsLocalDateFormatter(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT));
@@ -33,12 +46,12 @@ public final class Formatters {
 	public static DurationMode getDurationMode() {
 		return DURATION_MODE;
 	}
-	
-	public static void setDurationMode(@NonNull DurationMode mode) {
+
+	public static void setDurationMode(@NonNull final DurationMode mode) {
 		DURATION_MODE = mode;
 	}
 
-	public static @NonNull String formatAsDays(int hours) {
+	public static @NonNull String formatAsDays(final int hours) {
 		switch (DURATION_MODE) {
 		case DAYS_HOURS:
 			return String.format("%02d:%02d", hours / 24, hours % 24);
