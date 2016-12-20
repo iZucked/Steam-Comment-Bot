0a58055fecf0d6cdd22b860f15b86cce3b405039
Simon Goodall <simon.goodall@minimaxlabs.com>
2016-12-20 12:47:02 +0000

    Fix up multiple availability overlap checks

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/MultipleVesselAvailabilitiesConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/MultipleVesselAvailabilitiesConstraint.java
@@ -4,6 +4,8 @@
  */
 package com.mmxlabs.models.lng.cargo.validation;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -15,7 +17,9 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.validation.model.IConstraintStatus;
 
+import com.mmxlabs.common.NonNullPair;
 import com.mmxlabs.common.Pair;
+import com.mmxlabs.common.time.TimeUtils;
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.VesselAvailability;
@@ -70,7 +74,7 @@ public class MultipleVesselAvailabilitiesConstraint extends AbstractModelMultiCo
 	@Override
 	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> statuses) {
 		final EObject target = ctx.getTarget();
-			
+
 		if (target instanceof VesselAvailability) {
 			CargoModel cargoModel;
 			if (extraContext.isValidatingClone()) {
@@ -79,57 +83,75 @@ public class MultipleVesselAvailabilitiesConstraint extends AbstractModelMultiCo
 				cargoModel = (CargoModel) extraContext.getContainer(target);
 			}
 
-			HashMap<Vessel, List<VesselAvailability>> currentConstraintData = getCurrentConstraintData(ctx, extraContext);
+			final HashMap<Vessel, List<VesselAvailability>> currentConstraintData = getCurrentConstraintData(ctx, extraContext);
 			validateAvailability(ctx, statuses, currentConstraintData, (VesselAvailability) target);
 		}
 
 		return Activator.PLUGIN_ID;
 	}
 
-	private void validateAvailability(final IValidationContext ctx, final List<IStatus> statuses, HashMap<Vessel, List<VesselAvailability>> currentConstraintData, VesselAvailability availability) {
-		final List<VesselAvailability> conflicts = currentConstraintData.get(availability.getVessel());
+	private void validateAvailability(final IValidationContext ctx, final List<IStatus> statuses, final HashMap<Vessel, List<VesselAvailability>> currentConstraintData,
+			final VesselAvailability availability) {
+		final Vessel vessel = availability.getVessel();
+		final List<VesselAvailability> conflicts = currentConstraintData.get(vessel);
 		// conflicts.remove(availability);
-		Collections.sort(conflicts, (a, b) -> a.getStartBy() == null ? - 1 : b.getStartBy() == null ? 1 : a.getStartBy().compareTo(b.getStartBy()));
+		Collections.sort(conflicts, (a, b) -> {
+			if (a.getStartAfter() == null) {
+				return -1;
+			}
+			if (a.getEndBy() == null) {
+				return 1;
+			}
+			if (b.getStartAfter() == null) {
+				return 1;
+			}
+			if (b.getEndBy() == null) {
+				return -1;
+			}
+			return a.getStartAfter().compareTo(b.getStartAfter());
+		});
+
 		// sort out nulls
-		boolean nulls = false;
-		if (conflicts.stream().filter(c -> c.getStartBy() == null).count() > 1) {
+		final boolean nulls = false;
+		final String vesselName = vessel == null ? "<Unspecified vessel>" : vessel.getName();
+		if (conflicts.stream().filter(c -> c.getStartAfter() == null).count() > 1) {
 			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator(
-					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only one availability with the same vessel can have an empty Start By date", availability.getVessel().getName())));
-			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_BY);
+					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only one availability with the same vessel can have an empty Start After date", vesselName)));
+			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_AFTER);
 			statuses.add(dcsd);
 			return;
 		}
-		if (conflicts.indexOf(availability) != 0 && availability.getStartBy() == null) {
+		if (conflicts.indexOf(availability) != 0 && availability.getStartAfter() == null) {
 			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator(
-					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only the earliest multiple availability can have an empty Start By date.", availability.getVessel().getName())));
-			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_BY);
+					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only the earliest multiple availability can have an empty Start After date.", vesselName)));
+			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_AFTER);
 			statuses.add(dcsd);
 			return;
 		}
 		if (conflicts.stream().filter(c -> c.getEndBy() == null).count() > 1) {
 			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator(
-					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only one availability with the same vessel can have an empty End By date", availability.getVessel().getName())));
+					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only one availability with the same vessel can have an empty End By date", vesselName)));
 			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__END_BY);
 			statuses.add(dcsd);
 			return;
 		}
 		if (conflicts.indexOf(availability) != conflicts.size() - 1 && availability.getEndBy() == null) {
 			final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator(
-					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only the earliest availability with the same vessel can have an empty End By date.", availability.getVessel().getName())));
+					(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Only the earliest availability with the same vessel can have an empty End By date.", vesselName)));
 			dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__END_BY);
 			statuses.add(dcsd);
 			return;
 		}
 		if (nulls == false && conflicts != null && (conflicts.size() > 1)) {
-			for (VesselAvailability sibling : conflicts) {
+			for (final VesselAvailability sibling : conflicts) {
 				if (sibling == availability) {
 					continue;
 				}
-				if (!(isBefore(sibling, availability) || isAfter(sibling, availability))) {
-					final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx
-							.createFailureStatus(String.format("%s - Multiple availabilities assigned to the same vessel must not overlap.", availability.getVessel().getName())));
+				if (overlaps(sibling, availability)) {
+					final DetailConstraintStatusDecorator dcsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus(String.format("%s - Multiple availabilities assigned to the same vessel must not overlap.", vesselName)));
 					dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__VESSEL);
-					dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_BY);
+					dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__START_AFTER);
 					dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.VESSEL_AVAILABILITY__END_BY);
 					dcsd.addEObjectAndFeature(availability, CargoPackage.Literals.CARGO_MODEL__VESSEL_AVAILABILITIES);
 					statuses.add(dcsd);
@@ -139,31 +161,24 @@ public class MultipleVesselAvailabilitiesConstraint extends AbstractModelMultiCo
 		}
 	}
 
-	private boolean isAfter(VesselAvailability sibling, VesselAvailability availability) {
-		if (sibling.getStartBy() == null) {
-			return false;
-		}
-		if (availability.getEndBy() == null) {
-			return false;
-		}
-		if ((availability.getStartBy() == null || sibling.getStartBy().isAfter(availability.getStartBy())) && (sibling.getEndBy() == null || sibling.getEndBy().isAfter(availability.getEndBy())) && !availability.getEndBy().isBefore(sibling.getStartBy())) {
-			return true;
-		}
-		return false;
+	private boolean overlaps(final VesselAvailability a, final VesselAvailability b) {
+
+		final NonNullPair<ZonedDateTime, ZonedDateTime> aRange = getRange(a);
+		final NonNullPair<ZonedDateTime, ZonedDateTime> bRange = getRange(b);
+
+		return TimeUtils.overlaps(aRange, bRange);
 	}
-	
-	private boolean isBefore(VesselAvailability sibling, VesselAvailability availability) {
-		if (availability.getStartBy() == null) {
-			return false;
+
+	private NonNullPair<ZonedDateTime, ZonedDateTime> getRange(final VesselAvailability availabilty) {
+		ZonedDateTime start = availabilty.getStartAfterAsDateTime();
+		if (start == null) {
+			start = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC"));
 		}
-		if (sibling.getEndBy() == null) {
-			return false;
+		ZonedDateTime end = availabilty.getEndByAsDateTime();
+		if (end == null) {
+			end = ZonedDateTime.of(2050, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC"));
 		}
-		if (((sibling.getStartBy() == null || sibling.getStartBy().isBefore(availability.getStartBy())) && (availability.getEndBy() == null || sibling.getEndBy().isBefore(availability.getEndBy()))
-				&& !sibling.getEndBy().isAfter(availability.getStartBy()))) {
-			return true;
-		}
-		return false;
+		return new NonNullPair<ZonedDateTime, ZonedDateTime>(start, end);
 	}
 
 }
