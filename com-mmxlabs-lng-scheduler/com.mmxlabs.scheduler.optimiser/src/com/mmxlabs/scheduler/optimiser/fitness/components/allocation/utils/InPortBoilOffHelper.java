b69a48d00a94e7edb5322eedacdf1ae02e0c6df0
hudson <hudson@minimaxlabs.com>
2017-01-09 19:00:55 +0000

    Update headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/utils/InPortBoilOffHelper.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/fitness/components/allocation/utils/InPortBoilOffHelper.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.fitness.components.allocation.utils;
 
 import com.mmxlabs.scheduler.optimiser.Calculator;
