15c939fa3560731a1dfd8decfb132d10625bc6f0
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-01-26 14:24:07 +0000

    Changed for v65 migration

--- com-mmxlabs-models-lng/copy_models.py
+++ com-mmxlabs-models-lng/copy_models.py
@@ -6,7 +6,7 @@
 import shutil, os
 #####################################
 path_to_ws = os.getcwd()
-version = 64
+version = 65
 #####################################
 
 root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
