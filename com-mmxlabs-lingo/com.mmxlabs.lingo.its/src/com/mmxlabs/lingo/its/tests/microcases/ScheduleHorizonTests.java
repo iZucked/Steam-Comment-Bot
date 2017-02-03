06c724fd024baf0f06a88e58cdb3742ee58167e0
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-02-03 11:24:36 +0000

    Fix: Issue with schedule horizon and period optimisation
    BugzId: 2372

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/ScheduleHorizonTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/ScheduleHorizonTests.java
@@ -95,7 +95,7 @@ public class ScheduleHorizonTests extends AbstractMicroTestCase {
 	}
 
 	/**
-	 * Optional unued time charter, should not be evaluated
+	 * Optional unused time charter, should not be evaluated
 	 */
 	@Test
 	public void testUnusedOptionalTC() {
@@ -180,23 +180,24 @@ public class ScheduleHorizonTests extends AbstractMicroTestCase {
 			Assert.assertFalse(LocalDateTime.of(2016, 1, 1, 0, 0, 0).isAfter(lastEvent.getEnd().plusHours(1).withZoneSameInstant(ZoneId.of("UTC")).toLocalDateTime()));
 		});
 	}
+
 	/**
 	 * Horizon is before vessel dates, so adjust end
 	 */
 	@Test
 	public void testVesselWithDatesAfterHorizon() {
-		
+
 		@NonNull
 		LocalDate horizon = LocalDate.of(2016, 3, 1);
 		scenarioModelBuilder.setScheduleHorizon(horizon);
-		
+
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
 		final Vessel vessel = fleetModelBuilder.createVessel("My Vessel", vesselClass);
-		
+
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
 				.withEndWindow(LocalDateTime.of(2016, 4, 1, 0, 0, 0), null) //
 				.build();
-		
+
 		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
 				.makeFOBPurchase("L1", LocalDate.of(2015, 12, 5), portFinder.findPort("Point Fortin"), null, entity, "5") //
 				.build() //
@@ -205,32 +206,32 @@ public class ScheduleHorizonTests extends AbstractMicroTestCase {
 				.withVesselAssignment(vesselAvailability, 1) //
 				.withAssignmentFlags(false, false) //
 				.build();
-		
+
 		evaluateWithLSOTest(scenarioRunner -> {
-			
+
 			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
-			
+
 			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
 			Assert.assertEquals(1, optimiserScenario.getCargoModel().getCargoes().size());
-			
+
 			final Cargo optCargo1 = optimiserScenario.getCargoModel().getCargoes().get(0);
-			
+
 			@Nullable
 			final Schedule schedule = ScenarioModelUtil.findSchedule(lngScenarioModel);
 			Assert.assertNotNull(schedule);
-			
+
 			@Nullable
 			final CargoAllocation cargoAllocation = ScheduleTools.findCargoAllocation(optCargo1.getLoadName(), schedule);
-			
+
 			Assert.assertNotNull(cargoAllocation);
-			
+
 			final EList<Event> events = cargoAllocation.getSequence().getEvents();
 			final Event lastEvent = events.get(events.size() - 1);
-			
+
 			// End near early window bound
 			// As we can finish any time, there is no need to keep going until schedule end date
-//			Assert.assertTrue(lastEvent.getEnd().plusHours(1).withZoneSameInstant(ZoneId.of("UTC")).toLocalDate().isBefore(horizon));
-			
+			// Assert.assertTrue(lastEvent.getEnd().plusHours(1).withZoneSameInstant(ZoneId.of("UTC")).toLocalDate().isBefore(horizon));
+
 			// TODO: Not sure if we need +1 here...
 			Assert.assertEquals(LocalDateTime.of(2016, 3, 1, 0, 0, 0), lastEvent.getEnd().withZoneSameInstant(ZoneId.of("UTC")).toLocalDateTime());
 		});
@@ -712,6 +713,84 @@ public class ScheduleHorizonTests extends AbstractMicroTestCase {
 			Assert.assertEquals(LocalDateTime.of(2016, 1, 1, 0, 0, 0), period_vesselAvailability.getEndAfter());
 			Assert.assertEquals(LocalDateTime.of(2016, 5, 16, 0, 0, 0), period_vesselAvailability.getEndBy());
 
+			Assert.assertNull(optimiserScenario.getSchedulingEndDate());
+		}, null);
+	}
+
+	/**
+	 * Real test case. V 4.1.3 - vessel with end date in 2021, horizon end 1 month after opt period end. Period transformer did not create correctly set end date or set the force end rules.
+	 */
+	@Test
+	public void test_Period_FB_2372() {
+		@NonNull
+		LocalDate horizon = LocalDate.of(2017, 6, 1);
+		scenarioModelBuilder.setScheduleHorizon(horizon);
+
+		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
+		final Vessel vessel = fleetModelBuilder.createVessel("My Vessel", vesselClass);
+
+		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
+				.withCharterRate("80000") //
+				.withStartWindow(LocalDateTime.of(2016, 12, 1, 0, 0, 0)) //
+				.withEndWindow(LocalDateTime.of(2021, 9, 1, 0, 0, 0)) //
+				.build();
+
+		// Get rid of timezone to make testing easier
+		@NonNull
+		Port loadPort = portFinder.findPort("Point Fortin");
+		@NonNull
+		Port dischargePort = portFinder.findPort("Dominion Cove Point LNG");
+
+		loadPort.setTimeZone("UTC");
+		loadPort.setDefaultStartTime(0);
+		dischargePort.setTimeZone("UTC");
+		dischargePort.setDefaultStartTime(0);
+
+		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("L1", LocalDate.of(2017, 2, 9), loadPort, null, entity, "5") //
+				.withWindowSize(1, TimePeriod.DAYS) //
+				.build() //
+				.makeDESSale("D1", LocalDate.of(2017, 2, 27), dischargePort, null, entity, "7") //
+				.withWindowSize(1, TimePeriod.DAYS) //
+				.build() //
+				.withVesselAssignment(vesselAvailability, 1) //
+				.withAssignmentFlags(false, false) //
+				.build();
+
+		evaluateWithLSOTest(false, optimisationPlan -> {
+			optimisationPlan.getUserSettings().setPeriodStart(YearMonth.of(2017, 2));
+			optimisationPlan.getUserSettings().setPeriodEnd(YearMonth.of(2017, 5));
+		}, null, scenarioRunner -> {
+
+			final LNGScenarioToOptimiserBridge scenarioToOptimiserBridge = scenarioRunner.getScenarioToOptimiserBridge();
+
+			final LNGScenarioModel optimiserScenario = scenarioToOptimiserBridge.getOptimiserScenario();
+
+			Assert.assertEquals(1, optimiserScenario.getCargoModel().getCargoes().size());
+
+			final Cargo optCargo1 = optimiserScenario.getCargoModel().getCargoes().get(0);
+
+			@Nullable
+			final Schedule schedule = ScenarioModelUtil.findSchedule(optimiserScenario);
+			Assert.assertNotNull(schedule);
+
+			@Nullable
+			final CargoAllocation cargoAllocation = ScheduleTools.findCargoAllocation(cargo1.getLoadName(), schedule);
+
+			Assert.assertNotNull(cargoAllocation);
+
+			Sequence sequence = cargoAllocation.getSequence();
+			final List<Event> events = sequence.getEvents();
+			final Event lastEvent = events.get(events.size() - 1);
+
+			// Expect to end in time for cargo 2 loading which has been stripped out
+			// Assert.assertEquals(LocalDate.of(2016, 4, 16), lastEvent.getEnd().plusHours(1).withZoneSameInstant(ZoneId.of("UTC")).toLocalDate());
+
+			// No end date flex, fully costed
+			VesselAvailability period_vesselAvailability = optimiserScenario.getCargoModel().getVesselAvailabilities().get(0);
+			Assert.assertTrue(period_vesselAvailability.isForceHireCostOnlyEndRule());
+			Assert.assertEquals(LocalDateTime.of(2017, 6, 1, 0, 0, 0), period_vesselAvailability.getEndAfter());
+			Assert.assertEquals(LocalDateTime.of(2021, 9, 1, 0, 0, 0), period_vesselAvailability.getEndBy());
 
 			Assert.assertNull(optimiserScenario.getSchedulingEndDate());
 		}, null);
