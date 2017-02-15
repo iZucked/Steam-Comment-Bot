74670f247b53f221054ec7f9f5377417c7cffc4b
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-02-15 16:01:32 +0000

    Add basic pieces to dynamically create a new ChangeSetView instance.

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/ui/utils/AnalyticsSolution.java
@@ -0,0 +1,40 @@
+package com.mmxlabs.models.lng.analytics.ui.utils;
+
+import org.eclipse.emf.ecore.EObject;
+
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+
+public class AnalyticsSolution {
+	private String title;
+	private String id;
+	private ScenarioInstance scenarioInstance;
+	private EObject solution;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public ScenarioInstance getScenarioInstance() {
+		return scenarioInstance;
+	}
+
+	public void setScenarioInstance(ScenarioInstance scenarioInstance) {
+		this.scenarioInstance = scenarioInstance;
+	}
+
+	public EObject getSolution() {
+		return solution;
+	}
+
+	public void setSolution(EObject solution) {
+		this.solution = solution;
+	}
+
+	public String getID() {
+		return null;
+	}
+}
