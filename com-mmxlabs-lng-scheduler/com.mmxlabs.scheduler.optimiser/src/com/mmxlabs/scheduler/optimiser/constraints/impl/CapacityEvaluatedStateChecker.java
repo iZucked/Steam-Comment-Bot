7393e5390c2afa78960eca43bd0d0802ee37d84d
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-02-24 20:14:17 +0000

    Rename fields

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/constraints/impl/CapacityEvaluatedStateChecker.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/constraints/impl/CapacityEvaluatedStateChecker.java
@@ -31,21 +31,88 @@ import com.mmxlabs.scheduler.optimiser.voyage.impl.CapacityViolationType;
  */
 public final class CapacityEvaluatedStateChecker implements IEvaluatedStateConstraintChecker {
 
-	private long initialViolations = 0;
+	private long initialTotalViolations = 0;
 	private List<CapacityViolationType> initialTriggeredViolations;
-	public List<CapacityViolationType> getInitialTriggeredViolations() {
-		return initialTriggeredViolations;
-	}
+
 	public List<CapacityViolationType> allViolations;
-	public List<CapacityViolationType> allViolationsOriginal;
-	
+	public List<CapacityViolationType> initialAllViolations;
+
 	private long totalViolations = 0;
 	private final @NonNull String name;
 	private boolean initialised;
-	private Set<IPortSlot> violatedSlots = null;
+	private Set<IPortSlot> initialViolatedSlots = null;
 	private Set<IPortSlot> currentViolatedSlots;
 	private List<CapacityViolationType> triggeredViolations;
-	
+
+	private final Set<CapacityViolationType> targetViolations = EnumSet.of(//
+			CapacityViolationType.MIN_DISCHARGE, //
+			CapacityViolationType.MAX_DISCHARGE, //
+			CapacityViolationType.MIN_LOAD, //
+			CapacityViolationType.MAX_LOAD, //
+			CapacityViolationType.VESSEL_CAPACITY);
+
+	public CapacityEvaluatedStateChecker(@NonNull final String name) {
+		this.name = name;
+	}
+
+	@Override
+	public boolean checkConstraints(@NonNull final ISequences rawSequences, @NonNull final ISequences fullSequences, @NonNull final IEvaluationState evaluationState) {
+
+		final VolumeAllocatedSequences volumeAllocatedSequences = evaluationState.getData(SchedulerEvaluationProcess.VOLUME_ALLOCATED_SEQUENCES, VolumeAllocatedSequences.class);
+
+		if (volumeAllocatedSequences == null) {
+			return true;
+		}
+
+		totalViolations = volumeAllocatedSequences.stream()//
+				.mapToLong(seq -> seq.getSequenceSlots().stream() //
+						.mapToLong(slot -> seq.getCapacityViolationCount(slot)).sum())
+				.sum();
+
+		currentViolatedSlots = new HashSet<IPortSlot>();
+		triggeredViolations = new ArrayList<CapacityViolationType>();
+		allViolations = new ArrayList<CapacityViolationType>();
+
+		for (final VolumeAllocatedSequence volumeAllocatedSequence : volumeAllocatedSequences) {
+			final List<@NonNull IPortSlot> slots = volumeAllocatedSequence.getSequenceSlots();
+
+			for (final IPortSlot slot : slots) {
+				final List<@NonNull CapacityViolationType> violations = volumeAllocatedSequence.getCapacityViolations(slot);
+
+				for (@NonNull
+				final CapacityViolationType violation : violations) {
+					allViolations.add(violation);
+					if (targetViolations.contains(violation)) {
+						triggeredViolations.add(violation);
+						currentViolatedSlots.add(slot);
+					}
+				}
+
+			}
+		}
+
+		if (initialViolatedSlots != null) {
+			currentViolatedSlots.removeAll(initialViolatedSlots);
+			if (totalViolations > initialTotalViolations || currentViolatedSlots.size() > 0) {
+				return false;
+			}
+		} // If this is the first run, then set the initial state
+		else {
+			initialViolatedSlots = currentViolatedSlots;
+			initialTotalViolations = totalViolations;
+			initialAllViolations = allViolations;
+			initialised = true;
+			return true;
+		}
+
+		return true;
+	}
+
+	@Override
+	public @NonNull String getName() {
+		return name;
+	}
+
 	public List<CapacityViolationType> getTriggeredViolations() {
 		return triggeredViolations;
 	}
@@ -57,86 +124,21 @@ public final class CapacityEvaluatedStateChecker implements IEvaluatedStateConst
 	public long getTotalViolations() {
 		return totalViolations;
 	}
-	
-	public long getInitialViolations() {
-		return initialViolations;
-	}
 
+	public long getInitialViolations() {
+		return initialTotalViolations;
+	}
 
 	public boolean isInitialised() {
 		return initialised;
 	}
 
-
 	public Set<IPortSlot> getViolatedSlots() {
-		return violatedSlots;
+		return initialViolatedSlots;
 	}
 
-	private Set<CapacityViolationType> targetViolations = EnumSet.of(CapacityViolationType.MIN_DISCHARGE,
-			CapacityViolationType.MIN_LOAD,CapacityViolationType.MAX_LOAD,CapacityViolationType.MAX_DISCHARGE,
-			CapacityViolationType.VESSEL_CAPACITY);
-
-	public CapacityEvaluatedStateChecker(@NonNull final String name) {
-		this.name = name;
-	}
-
-
-	@Override
-	public boolean checkConstraints(@NonNull final ISequences rawSequences, @NonNull final ISequences fullSequences, @NonNull final IEvaluationState evaluationState) {
-
-		final VolumeAllocatedSequences volumeAllocatedSequences = evaluationState.getData(SchedulerEvaluationProcess.VOLUME_ALLOCATED_SEQUENCES, VolumeAllocatedSequences.class);
-
-		if (volumeAllocatedSequences == null) {
-			return true;
-		}
-		
-		totalViolations = volumeAllocatedSequences.stream()//
-				.mapToLong(seq -> seq.getSequenceSlots().stream() //
-						.mapToLong(slot -> seq.getCapacityViolationCount(slot)).sum())
-				.sum();
-		
-		currentViolatedSlots = new HashSet<IPortSlot>();
-		triggeredViolations = new ArrayList<CapacityViolationType>();
-		
-		allViolations = new ArrayList<CapacityViolationType>();
-		
-		for (VolumeAllocatedSequence volumeAllocatedSequence : volumeAllocatedSequences) {
-			List<@NonNull IPortSlot> slots = volumeAllocatedSequence.getSequenceSlots();
-			
-			for( IPortSlot slot : slots){
-				List<@NonNull CapacityViolationType> violations = volumeAllocatedSequence.getCapacityViolations(slot);
-				
-				for(@NonNull CapacityViolationType violation: violations){
-					allViolations.add(violation);
-					if(targetViolations.contains(violation)){
-						triggeredViolations.add(violation);
-						currentViolatedSlots.add(slot);
-					}
-				}
-			
-			}
-		}
-
-	if (violatedSlots != null) {
-		currentViolatedSlots.removeAll(violatedSlots);
-		if (totalViolations > initialViolations || currentViolatedSlots.size() > 0) {
-			return false;
-		}
-	}// If this is the first run, then set the initial state
-	else {
-		violatedSlots = currentViolatedSlots;	
-		initialViolations = totalViolations;
-		allViolationsOriginal = allViolations;
-		initialised = true;
-		return true;
-	}
-	
-	return true;
-	}
-
-	@Override
-	public @NonNull String getName() {
-		return name;
+	public List<CapacityViolationType> getInitialTriggeredViolations() {
+		return initialTriggeredViolations;
 	}
 
 }
