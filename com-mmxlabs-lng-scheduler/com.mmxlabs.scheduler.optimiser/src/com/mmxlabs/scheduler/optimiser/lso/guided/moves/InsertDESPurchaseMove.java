a58d1d0f5d7f7affd4e0d2f1c3083594296fe4eb
Jenkins <hudson@minimaxlabs.com>
2017-03-17 12:14:10 +0000

    Update headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/lso/guided/moves/InsertDESPurchaseMove.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/lso/guided/moves/InsertDESPurchaseMove.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.lso.guided.moves;
 
 import java.util.Collection;
--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/moves/util/MoveGeneratorModule.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/moves/util/MoveGeneratorModule.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.moves.util;
 
 import java.util.HashMap;
