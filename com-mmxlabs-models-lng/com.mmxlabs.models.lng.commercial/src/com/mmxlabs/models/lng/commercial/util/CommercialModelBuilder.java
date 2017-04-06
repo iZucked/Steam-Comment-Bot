24c37f0147b39e0c1f3fb4443b629ac95370fa2c
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-04-06 12:01:36 +0100

    API for ballast bonus contracts (from ITS test case)

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial/src/com/mmxlabs/models/lng/commercial/util/CommercialModelBuilder.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial/src/com/mmxlabs/models/lng/commercial/util/CommercialModelBuilder.java
@@ -5,6 +5,7 @@
 package com.mmxlabs.models.lng.commercial.util;
 
 import java.time.LocalDate;
+import java.util.Collection;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jdt.annotation.NonNull;
@@ -15,9 +16,14 @@ import com.mmxlabs.models.lng.commercial.CommercialFactory;
 import com.mmxlabs.models.lng.commercial.CommercialModel;
 import com.mmxlabs.models.lng.commercial.ExpressionPriceParameters;
 import com.mmxlabs.models.lng.commercial.LegalEntity;
+import com.mmxlabs.models.lng.commercial.LumpSumBallastBonusContractLine;
+import com.mmxlabs.models.lng.commercial.NotionalJourneyBallastBonusContractLine;
 import com.mmxlabs.models.lng.commercial.PurchaseContract;
+import com.mmxlabs.models.lng.commercial.RuleBasedBallastBonusContract;
 import com.mmxlabs.models.lng.commercial.SalesContract;
 import com.mmxlabs.models.lng.commercial.TaxRate;
+import com.mmxlabs.models.lng.port.Port;
+import com.mmxlabs.models.lng.types.APortSet;
 
 public class CommercialModelBuilder {
 
@@ -27,7 +33,7 @@ public class CommercialModelBuilder {
 		this.commercialModel = commercialModel;
 	}
 
-	public @NonNull LegalEntity makeLegalEntityAndBooks(@NonNull String name) {
+	public @NonNull LegalEntity makeLegalEntityAndBooks(@NonNull final String name) {
 		final LegalEntity entity = CommercialFactory.eINSTANCE.createLegalEntity();
 
 		entity.setShippingBook(CommercialFactory.eINSTANCE.createSimpleEntityBook());
@@ -41,7 +47,7 @@ public class CommercialModelBuilder {
 		return entity;
 	}
 
-	public @NonNull SalesContract makeExpressionSalesContract(@NonNull String name, @NonNull BaseLegalEntity entity, @NonNull String expression) {
+	public @NonNull SalesContract makeExpressionSalesContract(@NonNull final String name, @NonNull final BaseLegalEntity entity, @NonNull final String expression) {
 		final SalesContract contract = CommercialFactory.eINSTANCE.createSalesContract();
 		final ExpressionPriceParameters params = CommercialFactory.eINSTANCE.createExpressionPriceParameters();
 		contract.setName(name);
@@ -55,7 +61,7 @@ public class CommercialModelBuilder {
 		return contract;
 	}
 
-	public @NonNull PurchaseContract makeExpressionPurchaseContract(@NonNull String name, @NonNull BaseLegalEntity entity, @NonNull String expression) {
+	public @NonNull PurchaseContract makeExpressionPurchaseContract(@NonNull final String name, @NonNull final BaseLegalEntity entity, @NonNull final String expression) {
 		final PurchaseContract contract = CommercialFactory.eINSTANCE.createPurchaseContract();
 		final ExpressionPriceParameters params = CommercialFactory.eINSTANCE.createExpressionPriceParameters();
 		contract.setName(name);
@@ -69,13 +75,13 @@ public class CommercialModelBuilder {
 		return contract;
 	}
 
-	public @NonNull TaxRate createTaxRate(@NonNull LocalDate date, float taxRate) {
+	public @NonNull TaxRate createTaxRate(@NonNull final LocalDate date, final float taxRate) {
 
 		// Check sensible bounds.
 		assert taxRate >= 0.0f;
 		assert taxRate <= 1.0f;
 
-		TaxRate rate = CommercialFactory.eINSTANCE.createTaxRate();
+		final TaxRate rate = CommercialFactory.eINSTANCE.createTaxRate();
 
 		rate.setDate(date);
 		rate.setValue(taxRate);
@@ -89,7 +95,7 @@ public class CommercialModelBuilder {
 	 * @param book
 	 * @param rate
 	 */
-	public void setTaxRate(BaseEntityBook book, @NonNull TaxRate rate) {
+	public void setTaxRate(final BaseEntityBook book, @NonNull final TaxRate rate) {
 		if (book == null) {
 			throw new NullPointerException();
 		}
@@ -103,9 +109,38 @@ public class CommercialModelBuilder {
 	 * @param book
 	 * @param rate
 	 */
-	public void setTaxRates(@NonNull BaseLegalEntity entity, @NonNull TaxRate rate) {
+	public void setTaxRates(@NonNull final BaseLegalEntity entity, @NonNull final TaxRate rate) {
 		setTaxRate(entity.getTradingBook(), EcoreUtil.copy(rate));
 		setTaxRate(entity.getShippingBook(), EcoreUtil.copy(rate));
 		setTaxRate(entity.getUpstreamBook(), EcoreUtil.copy(rate));
+	}
+
+	public @NonNull RuleBasedBallastBonusContract createSimpleLumpSumBallastBonusContract(@NonNull final Port redeliveryPort, @NonNull final String priceExpression) {
+
+		final LumpSumBallastBonusContractLine lumpSumBallastBonusContractLine = CommercialFactory.eINSTANCE.createLumpSumBallastBonusContractLine();
+		lumpSumBallastBonusContractLine.getRedeliveryPorts().add(redeliveryPort);
+		lumpSumBallastBonusContractLine.setPriceExpression(priceExpression);
+
+		final RuleBasedBallastBonusContract ballastBonusContract = CommercialFactory.eINSTANCE.createRuleBasedBallastBonusContract();
+		ballastBonusContract.getRules().add(lumpSumBallastBonusContractLine);
+
+		return ballastBonusContract;
+	}
+
+	public @NonNull RuleBasedBallastBonusContract createSimpleNotionalJourneyBallastBonusContract(final @NonNull Collection<@NonNull APortSet<Port>> redeliveryPorts, final double speed,
+			final @NonNull String hireExpression, final @NonNull String fuelExpression, final boolean includeCanal, final @NonNull Collection<@NonNull APortSet<Port>> returnPorts) {
+
+		final NotionalJourneyBallastBonusContractLine notionalJourneyBallastBonusContractLine = CommercialFactory.eINSTANCE.createNotionalJourneyBallastBonusContractLine();
+		notionalJourneyBallastBonusContractLine.getRedeliveryPorts().addAll(redeliveryPorts);
+		notionalJourneyBallastBonusContractLine.getReturnPorts().addAll(returnPorts);
+		notionalJourneyBallastBonusContractLine.setFuelPriceExpression(fuelExpression);
+		notionalJourneyBallastBonusContractLine.setHirePriceExpression(hireExpression);
+		notionalJourneyBallastBonusContractLine.setIncludeCanal(includeCanal);
+		notionalJourneyBallastBonusContractLine.setSpeed(speed);
+
+		final RuleBasedBallastBonusContract ballastBonusContract = CommercialFactory.eINSTANCE.createRuleBasedBallastBonusContract();
+		ballastBonusContract.getRules().add(notionalJourneyBallastBonusContractLine);
+
+		return ballastBonusContract;
 	}
 }
