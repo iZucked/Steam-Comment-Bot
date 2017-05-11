8796625102d4108589c36fdb1d4b8f1e87612ccb
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-05-11 10:47:37 +0100

    Add option to deselect all scenarios.

---
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.ui/src/com/mmxlabs/scenario/service/ui/commands/DeselectAllCommandHandler.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
+package com.mmxlabs.scenario.service.ui.commands;
+
+import java.util.function.Consumer;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.mmxlabs.rcp.common.ServiceHelper;
+import com.mmxlabs.scenario.service.ui.IScenarioServiceSelectionProvider;
+
+public class DeselectAllCommandHandler extends AbstractHandler {
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		BusyIndicator.showWhile(HandlerUtil.getActiveShellChecked(event).getDisplay(),
+				() -> ServiceHelper.withService(IScenarioServiceSelectionProvider.class, (Consumer<IScenarioServiceSelectionProvider>) p -> p.deselectAll()));
+
+		return null;
+	}
+}
