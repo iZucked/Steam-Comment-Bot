e238e56c11ad118004128719c36fb278bc696c83
Jenkins <hudson@minimaxlabs.com>
2017-05-22 15:26:23 +0100

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/changeset/handlers/SwitchSlotEvent.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/changeset/handlers/SwitchSlotEvent.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.views.changeset.handlers;
 
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
