60d60cca9f24372855510c914231dcde503e741d
Robert Erdin <robert.erdin@minimaxlabs.com>
2017-06-13 09:41:33 +0100

    whitlisting solutions that are not worse than the original one. Bugfix in scheduler

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.extensions/src/com/mmxlabs/models/lng/transformer/extensions/panamaslots/PanamaSlotsConstraintChecker.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.extensions/src/com/mmxlabs/models/lng/transformer/extensions/panamaslots/PanamaSlotsConstraintChecker.java
@@ -4,10 +4,14 @@
  */
 package com.mmxlabs.models.lng.transformer.extensions.panamaslots;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -22,11 +26,16 @@ import com.mmxlabs.optimiser.core.ISequences;
 import com.mmxlabs.optimiser.core.constraints.IConstraintChecker;
 import com.mmxlabs.optimiser.core.scenario.IOptimisationData;
 import com.mmxlabs.scheduler.optimiser.components.IPort;
+import com.mmxlabs.scheduler.optimiser.components.IPortSlot;
 import com.mmxlabs.scheduler.optimiser.components.IRouteOptionSlot;
+import com.mmxlabs.scheduler.optimiser.components.IVesselAvailability;
+import com.mmxlabs.scheduler.optimiser.components.VesselInstanceType;
 import com.mmxlabs.scheduler.optimiser.fitness.impl.enumerator.EnumeratingSequenceScheduler;
 import com.mmxlabs.scheduler.optimiser.fitness.impl.enumerator.NonSchedulingScheduler;
 import com.mmxlabs.scheduler.optimiser.fitness.impl.enumerator.PanamaPriceBasedSequenceScheduler;
+import com.mmxlabs.scheduler.optimiser.fitness.util.SequenceEvaluationUtils;
 import com.mmxlabs.scheduler.optimiser.providers.IPanamaSlotsProvider;
+import com.mmxlabs.scheduler.optimiser.providers.IVesselProvider;
 
 /**
  * An implementation of {@link IConstraintChecker} to forbid certain {@link ISequenceElement} pairings
@@ -41,6 +50,11 @@ public class PanamaSlotsConstraintChecker implements IConstraintChecker {
 
 	@Inject
 	private NonSchedulingScheduler scheduler;
+	
+	@Inject
+	private IVesselProvider vesselProvider;
+	
+	private Set<ISequenceElement> unbookedSlots; 
 	
 	public PanamaSlotsConstraintChecker(final String name) {
 		this.name = name;
@@ -64,7 +78,9 @@ public class PanamaSlotsConstraintChecker implements IConstraintChecker {
 		
 		int strictBoundary = panamaSlotsProvider.getStrictBoundary();
 		int relaxedBoundary = panamaSlotsProvider.getRelaxedBoundary();
-		int relaxedCount = panamaSlotsProvider.getRelaxedSlotCount();
+		
+		Set<ISequenceElement> currentUnbookedSlots = new HashSet<>();
+		Set<ISequenceElement> currentUnbookedSlotsInRelaxed = new HashSet<ISequenceElement>();
 				
 		//TODO: what to do with potential vessel return when end time is not specified?
 		//TODO: nominal vessel
@@ -74,7 +90,21 @@ public class PanamaSlotsConstraintChecker implements IConstraintChecker {
 			IResource resource = sequences.getResources().get(r);
 			ISequence sequence = sequences.getSequence(resource);
 			
+			
+			// skip resources that are not scheduled
+			final IVesselAvailability vesselAvailability = vesselProvider.getVesselAvailability(resource);
+			if (vesselAvailability.getVesselInstanceType() == VesselInstanceType.DES_PURCHASE || vesselAvailability.getVesselInstanceType() == VesselInstanceType.FOB_SALE) {
+				// TODO: Implement something here rather than rely on VoyagePlanner
+				continue;
+			}
+
+			// filters out solutions with less than 2 elements (i.e. spot charters, etc.)
+			if (sequence.size() < 2) {
+				continue;
+			}
+			
 			for (int index = 0; index < sequence.size(); index++){
+				
 				if (!throughPanama[r][index]){
 					// not going through Panama, ignore
 					continue;
@@ -87,12 +117,37 @@ public class PanamaSlotsConstraintChecker implements IConstraintChecker {
 					continue;
 				}
 				
-				if (schedule[r][index] >= strictBoundary && schedule[r][index] < relaxedBoundary && relaxedCount > 0){
-					relaxedCount--;
-					continue;
+				if (schedule[r][index] >= strictBoundary && schedule[r][index] < relaxedBoundary){
+					currentUnbookedSlotsInRelaxed.add(sequence.get(index));
 				}
+				currentUnbookedSlots.add(sequence.get(index));
+			}
+		}
+		
+		if (unbookedSlots != null){
+			// strict constraint
+			currentUnbookedSlots.removeAll(unbookedSlots);
+			currentUnbookedSlots.removeAll(currentUnbookedSlotsInRelaxed);
+			if (!currentUnbookedSlots.isEmpty()){
 				return false;
 			}
+			
+			// relaxed constraint
+			int countBefore = currentUnbookedSlots.size();						// 6
+			currentUnbookedSlotsInRelaxed.removeAll(unbookedSlots);
+			int countAfter = currentUnbookedSlotsInRelaxed.size();				// 0
+			int whitelistedSlotCount = (countBefore - countAfter);				// 6
+			
+			int relaxedSlotCount = panamaSlotsProvider.getRelaxedSlotCount(); 	// 5
+			int newCount = relaxedSlotCount - whitelistedSlotCount;				// -1
+			
+			if (countAfter == 0 || countAfter <= newCount){
+				return true;
+			}else{
+				return false;
+			}
+		}else {
+			unbookedSlots = currentUnbookedSlots;
 		}
 		return true;
 	}
