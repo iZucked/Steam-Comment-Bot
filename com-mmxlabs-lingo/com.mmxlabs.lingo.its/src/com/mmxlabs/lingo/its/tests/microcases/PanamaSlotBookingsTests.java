a9a14e27b51300e6cff541d32ad9715040cacf7e
Robert Erdin <robert.erdin@minimaxlabs.com>
2017-06-15 10:52:19 +0100

    Cleaning up. Renamed remaining occurances of slot to booking (in regard to canal bookings). BugzId: 2445

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/PanamaSlotBookingsTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/PanamaSlotBookingsTests.java
@@ -39,7 +39,7 @@ import com.mmxlabs.scheduler.optimiser.fitness.impl.enumerator.NonSchedulingSche
 import com.mmxlabs.scheduler.optimiser.manipulators.SequencesManipulatorModule;
 
 @RunWith(value = ShiroRunner.class)
-public class PanamaSlotsTests extends AbstractMicroTestCase {
+public class PanamaSlotBookingsTests extends AbstractMicroTestCase {
 
 	@Override
 	public LNGScenarioModel importReferenceData() throws MalformedURLException {
@@ -67,39 +67,30 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 			}
 		});
 		
-		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
-
-//		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
-//		d1.setRoute(panama);
-//		d1.setEntryPoint(colon);
-//		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
-//		model.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
-		
-//		CanalBookingSlot d2 = CargoFactory.eINSTANCE.createCanalBookingSlot();
-//		d2.setRoute(panama);
-//		d2.setEntryPoint(colon);
-//		d2.setSlotDate(LocalDate.of(2017, Month.JUNE, 20));
-//		model.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d2);
-//		
-//		CanalBookingSlot d3 = CargoFactory.eINSTANCE.createCanalBookingSlot();
-//		d3.setRoute(panama);
-//		d3.setEntryPoint(balboa);
-//		d3.setSlotDate(LocalDate.of(2017, Month.JUNE, 6));
-//		model.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d3);
-		
-//		CanalBookingSlot d4 = CargoFactory.eINSTANCE.createCanalBookingSlot();
-//		d4.setRoute(panama);
-//		d4.setEntryPoint(balboa);
-//		d4.setSlotDate(LocalDate.of(2017, Month.JULY, 20));
-//		model.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d4);
-		
 		model.getCargoModel().getCanalBookings().setStrictBoundaryOffsetDays(30);
 		model.getCargoModel().getCanalBookings().setRelaxedBoundaryOffsetDays(90);
-		model.getCargoModel().getCanalBookings().setFlexibleSlotAmount(0);
-		
+		model.getCargoModel().getCanalBookings().setFlexibleBookingAmount(0);
 		
 		return model;
+	}
+	
+	private Cargo createFobDesCargo(final VesselAvailability vesselAvailability, final Port loadPort, final Port dischargePort, final LocalDateTime loadDate, final LocalDateTime dischargeDate) {
+		final Cargo cargo = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("L", loadDate.toLocalDate(), loadPort, null, entity, "5") //
+				.withWindowStartTime(0) //
+				.withVisitDuration(24) //
+				.withWindowSize(0, TimePeriod.HOURS) //
+				.build() //
+				//
+				.makeDESSale("D", dischargeDate.toLocalDate(), dischargePort, null, entity, "7") //
+				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
+				.withVisitDuration(24) //
+				.withWindowSize(0, TimePeriod.HOURS) //
+				.build() //
+				//
+				.withVesselAssignment(vesselAvailability, 1) //
+				.build();
+		return cargo;
 	}
 
 	@Test
@@ -110,12 +101,11 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
 
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 
@@ -142,21 +132,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 
 		evaluateWithLSOTest(scenarioRunner -> {
 
@@ -171,6 +147,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 			assertTrue(checker.checkConstraints(manipulatedSequences, null));
 		});
 	}
+
 	
 	@Test
 	@Category({ MicroTest.class })
@@ -182,7 +159,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
@@ -212,37 +189,9 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 		
-		final Cargo cargo2 = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability2, 1) //
-				.build();
+		final Cargo cargo2 = createFobDesCargo(vesselAvailability2, port1, port2, loadDate, dischargeDate);
 		
 		evaluateWithLSOTest(scenarioRunner -> {
 
@@ -274,12 +223,11 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
 
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
@@ -305,21 +253,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.AUGUST, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(10);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 
 		evaluateWithLSOTest(scenarioRunner -> {
 
@@ -343,12 +277,11 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
 
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
@@ -356,10 +289,6 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
 				.build();
 		
-		final Vessel vessel2 = fleetModelBuilder.createVessel("vessel2", vesselClass);
-		final VesselAvailability vesselAvailability2 = cargoModelBuilder.makeVesselAvailability(vessel2, entity) //
-				.build();
-
 		@NonNull
 		final Port port1 = portFinder.findPort("Sabine Pass");
 
@@ -378,21 +307,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 		
 		evaluateWithLSOTest(scenarioRunner -> {
 
@@ -421,12 +336,11 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
-
+		
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 10));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 10));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
@@ -434,10 +348,6 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
 				.build();
 		
-		final Vessel vessel2 = fleetModelBuilder.createVessel("vessel2", vesselClass);
-		final VesselAvailability vesselAvailability2 = cargoModelBuilder.makeVesselAvailability(vessel2, entity) //
-				.build();
-
 		@NonNull
 		final Port port1 = portFinder.findPort("Sabine Pass");
 
@@ -456,21 +366,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 		
 		evaluateWithLSOTest(scenarioRunner -> {
 
@@ -501,17 +397,12 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
 
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
 		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
 				.build();
 		
-		final Vessel vessel2 = fleetModelBuilder.createVessel("vessel2", vesselClass);
-		final VesselAvailability vesselAvailability2 = cargoModelBuilder.makeVesselAvailability(vessel2, entity) //
-				.build();
-
 		@NonNull
 		final Port port1 = portFinder.findPort("Sabine Pass");
 
@@ -530,26 +421,12 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 		
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		d1.setSlot(cargo.getSortedSlots().get(0));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
@@ -580,7 +457,6 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		Route panama = potentialPanama.get();
 		
 		EntryPoint colon = panama.getEntryA();
-		EntryPoint balboa = panama.getEntryB();
 
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
 		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
@@ -609,43 +485,15 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(13);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 		
 		
-		final Cargo cargo2 = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability2, 1) //
-				.build();
+		final Cargo cargo2 = createFobDesCargo(vesselAvailability2, port1, port2, loadDate, dischargeDate);
 		
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		d1.setSlot(cargo2.getSortedSlots().get(0));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
@@ -680,7 +528,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		CanalBookingSlot d1 = CargoFactory.eINSTANCE.createCanalBookingSlot();
 		d1.setRoute(panama);
 		d1.setEntryPoint(colon);
-		d1.setSlotDate(LocalDate.of(2017, Month.JUNE, 7));
+		d1.setBookingDate(LocalDate.of(2017, Month.JUNE, 7));
 		lngScenarioModel.getCargoModel().getCanalBookings().getCanalBookingSlots().add(d1);
 		
 
@@ -707,21 +555,7 @@ public class PanamaSlotsTests extends AbstractMicroTestCase {
 		final LocalDateTime loadDate = LocalDateTime.of(2017, Month.JUNE, 1, 0, 0, 0);
 		final LocalDateTime dischargeDate = loadDate.plusDays(25);
 
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", loadDate.toLocalDate(), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
+		final Cargo cargo = createFobDesCargo(vesselAvailability, port1, port2, loadDate, dischargeDate);
 
 		evaluateWithLSOTest(scenarioRunner -> {
 
