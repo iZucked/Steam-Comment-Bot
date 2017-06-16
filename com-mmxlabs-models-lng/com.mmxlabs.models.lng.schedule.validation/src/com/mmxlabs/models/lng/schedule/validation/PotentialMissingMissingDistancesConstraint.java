5cb62ffc0f8325a562d50c2be94f179c40c8353e
Robert Erdin <robert.erdin@minimaxlabs.com>
2017-06-16 18:31:01 +0100

    Accidently commented constraint checker in previous commit

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule.validation/src/com/mmxlabs/models/lng/schedule/validation/PotentialMissingMissingDistancesConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.schedule.validation/src/com/mmxlabs/models/lng/schedule/validation/PotentialMissingMissingDistancesConstraint.java
@@ -57,134 +57,134 @@ public class PotentialMissingMissingDistancesConstraint extends AbstractModelMul
 	@Override
 	protected String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> statuses) {
 
-//		if (true) {
-//			// return Activator.PLUGIN_ID;
-//		}
-//
-//		final EObject target = ctx.getTarget();
-//
-//		final MMXRootObject rootObject = extraContext.getRootObject();
-//		if (target instanceof PortModel) {
-//			final LNGScenarioModel scenarioModel = (LNGScenarioModel) rootObject;
-//			final LNGReferenceModel referenceModel = scenarioModel.getReferenceModel();
-//			final SpotMarketsModel spotMarketsModel = referenceModel.getSpotMarketsModel();
-//			final CargoModel cargoModel = scenarioModel.getCargoModel();
-//			final PortModel portModel = ScenarioModelUtil.getPortModel(scenarioModel);
-//
-//			final Set<Port> startPorts = new HashSet<>();
-//			final Set<Port> endPorts = new HashSet<>();
-//			final Set<Port> eventPorts = new HashSet<>();
-//			final Set<Port> loadPorts = new HashSet<>();
-//			final Set<Port> dischargePorts = new HashSet<>();
-//
-//			cargoModel.getVesselAvailabilities().forEach(va -> startPorts.addAll(SetUtils.getObjects(va.getStartAt())));
-//			cargoModel.getVesselAvailabilities().forEach(va -> endPorts.addAll(SetUtils.getObjects(va.getEndAt())));
-//
-//			cargoModel.getLoadSlots().stream() //
-//					.filter(s -> !s.isDESPurchase() || s.isDivertible()) //
-//					.forEach(s -> loadPorts.add(s.getPort()));
-//
-//			cargoModel.getDischargeSlots().stream() //
-//					.filter(s -> !s.isFOBSale() || s.isDivertible()) //
-//					.forEach(s -> dischargePorts.add(s.getPort()));
-//
-//			// TODO: Complex cargoes will also have load -> load and discharge -> discharge distance requirements
-//
-//			cargoModel.getVesselEvents().forEach(e -> eventPorts.add(e.getPort()));
-//
-//			cargoModel.getVesselEvents().stream() //
-//					.filter(e -> e instanceof CharterOutEvent) //
-//					.forEach(e -> eventPorts.add(((CharterOutEvent) e).getRelocateTo()));
-//
-//			spotMarketsModel.getCharterOutMarkets().forEach(m -> eventPorts.addAll(SetUtils.getObjects(m.getAvailablePorts())));
-//
-//			spotMarketsModel.getFobPurchasesSpotMarket().getMarkets().forEach(m -> loadPorts.add(((FOBPurchasesMarket) m).getNotionalPort()));
-//			spotMarketsModel.getDesSalesSpotMarket().getMarkets().forEach(m -> dischargePorts.add(((DESSalesMarket) m).getNotionalPort()));
-//
-//			Map<RouteOption, RouteDistanceLineCache> caches = new EnumMap<>(RouteOption.class);
-//			for (final Route route : portModel.getRoutes()) {
-//				caches.put(route.getRouteOption(), (RouteDistanceLineCache) Platform.getAdapterManager().loadAdapter(route, RouteDistanceLineCache.class.getName()));
-//			}
-//			final RouteDistanceLineCache fCache = caches.get(RouteOption.DIRECT);
-//			if (fCache == null) {
-//				// No direct matrix?
-//				return Activator.PLUGIN_ID;
-//			}
-//
-//			// Lambda function to check two sets of distances
-//			final BiFunction<Set<Port>, Set<Port>, Set<Pair<Port, Port>>> distanceChecker = (froms, tos) -> {
-//				return froms.stream()//
-//						.filter(from -> from != null) //
-//						.flatMap(from -> tos.stream() //
-//								.filter(to -> to != null) //
-//								// Skip identity distance
-//								.filter(to -> !from.equals(to)) //
-//								.filter(to -> !fCache.hasDistance(from, to)) //
-//								.map(to -> new Pair<Port, Port>(from, to))) //
-//						.collect(Collectors.toSet());
-//			};
-//
-//			// Use lamba function against our different possible from/tos
-//			final Set<Pair<Port, Port>> missingDistances = new HashSet<>();
-//			missingDistances.addAll(distanceChecker.apply(startPorts, loadPorts));
-//			missingDistances.addAll(distanceChecker.apply(loadPorts, dischargePorts));
-//			missingDistances.addAll(distanceChecker.apply(dischargePorts, loadPorts));
-//			missingDistances.addAll(distanceChecker.apply(dischargePorts, endPorts));
-//
-//			missingDistances.addAll(distanceChecker.apply(startPorts, eventPorts));
-//			missingDistances.addAll(distanceChecker.apply(eventPorts, loadPorts));
-//			missingDistances.addAll(distanceChecker.apply(dischargePorts, eventPorts));
-//			missingDistances.addAll(distanceChecker.apply(eventPorts, endPorts));
-//
-//			missingDistances.addAll(distanceChecker.apply(eventPorts, eventPorts));
-//
-//			missingDistances.addAll(distanceChecker.apply(startPorts, endPorts));
-//
-//			// Complex cargoes
-//			for (final Cargo c : cargoModel.getCargoes()) {
-//				if (c.getSlots().size() > 2) {
-//					Port prev = null;
-//					for (final Slot slot : c.getSortedSlots()) {
-//						final Port thisPort = slot.getPort();
-//						if (prev != null) {
-//							missingDistances.addAll(distanceChecker.apply(Collections.singleton(prev), Collections.singleton(thisPort)));
-//						}
-//						prev = thisPort;
-//					}
-//				}
-//			}
-//
-//			for (final Pair<Port, Port> p : missingDistances) {
-//				final String msg = String.format("Missing distance between %s and %s.", getPortName(p.getFirst()), getPortName(p.getSecond()));
-//				final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(msg));
-//				failure.addEObjectAndFeature(p.getFirst(), MMXCorePackage.Literals.NAMED_OBJECT__NAME);
-//				failure.addEObjectAndFeature(p.getSecond(), MMXCorePackage.Literals.NAMED_OBJECT__NAME);
-//
-//				statuses.add(failure);
-//			}
-//
-//			// Check load -> discharge and back again - typically needed for notional voyages
-//			for (Port from : loadPorts) {
-//				for (Port to : dischargePorts) {
-//					if (from != to) {
-//						for (RouteOption route : RouteOption.values()) {
-//							final RouteDistanceLineCache cache = caches.get(route);
-//							if (cache != null && cache.hasDistance(from, to)) {
-//								if (!cache.hasDistance(to, from)) {
-//									final String msg = String.format("Missing distance between %s and %s for %s route.", getPortName(to), getPortName(from), route);
-//									final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(msg));
-//									failure.addEObjectAndFeature(from, MMXCorePackage.Literals.NAMED_OBJECT__NAME);
-//									failure.addEObjectAndFeature(to, MMXCorePackage.Literals.NAMED_OBJECT__NAME);
-//
-//									statuses.add(failure);
-//								}
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//
+		if (true) {
+			// return Activator.PLUGIN_ID;
+		}
+
+		final EObject target = ctx.getTarget();
+
+		final MMXRootObject rootObject = extraContext.getRootObject();
+		if (target instanceof PortModel) {
+			final LNGScenarioModel scenarioModel = (LNGScenarioModel) rootObject;
+			final LNGReferenceModel referenceModel = scenarioModel.getReferenceModel();
+			final SpotMarketsModel spotMarketsModel = referenceModel.getSpotMarketsModel();
+			final CargoModel cargoModel = scenarioModel.getCargoModel();
+			final PortModel portModel = ScenarioModelUtil.getPortModel(scenarioModel);
+
+			final Set<Port> startPorts = new HashSet<>();
+			final Set<Port> endPorts = new HashSet<>();
+			final Set<Port> eventPorts = new HashSet<>();
+			final Set<Port> loadPorts = new HashSet<>();
+			final Set<Port> dischargePorts = new HashSet<>();
+
+			cargoModel.getVesselAvailabilities().forEach(va -> startPorts.addAll(SetUtils.getObjects(va.getStartAt())));
+			cargoModel.getVesselAvailabilities().forEach(va -> endPorts.addAll(SetUtils.getObjects(va.getEndAt())));
+
+			cargoModel.getLoadSlots().stream() //
+					.filter(s -> !s.isDESPurchase() || s.isDivertible()) //
+					.forEach(s -> loadPorts.add(s.getPort()));
+
+			cargoModel.getDischargeSlots().stream() //
+					.filter(s -> !s.isFOBSale() || s.isDivertible()) //
+					.forEach(s -> dischargePorts.add(s.getPort()));
+
+			// TODO: Complex cargoes will also have load -> load and discharge -> discharge distance requirements
+
+			cargoModel.getVesselEvents().forEach(e -> eventPorts.add(e.getPort()));
+
+			cargoModel.getVesselEvents().stream() //
+					.filter(e -> e instanceof CharterOutEvent) //
+					.forEach(e -> eventPorts.add(((CharterOutEvent) e).getRelocateTo()));
+
+			spotMarketsModel.getCharterOutMarkets().forEach(m -> eventPorts.addAll(SetUtils.getObjects(m.getAvailablePorts())));
+
+			spotMarketsModel.getFobPurchasesSpotMarket().getMarkets().forEach(m -> loadPorts.add(((FOBPurchasesMarket) m).getNotionalPort()));
+			spotMarketsModel.getDesSalesSpotMarket().getMarkets().forEach(m -> dischargePorts.add(((DESSalesMarket) m).getNotionalPort()));
+
+			Map<RouteOption, RouteDistanceLineCache> caches = new EnumMap<>(RouteOption.class);
+			for (final Route route : portModel.getRoutes()) {
+				caches.put(route.getRouteOption(), (RouteDistanceLineCache) Platform.getAdapterManager().loadAdapter(route, RouteDistanceLineCache.class.getName()));
+			}
+			final RouteDistanceLineCache fCache = caches.get(RouteOption.DIRECT);
+			if (fCache == null) {
+				// No direct matrix?
+				return Activator.PLUGIN_ID;
+			}
+
+			// Lambda function to check two sets of distances
+			final BiFunction<Set<Port>, Set<Port>, Set<Pair<Port, Port>>> distanceChecker = (froms, tos) -> {
+				return froms.stream()//
+						.filter(from -> from != null) //
+						.flatMap(from -> tos.stream() //
+								.filter(to -> to != null) //
+								// Skip identity distance
+								.filter(to -> !from.equals(to)) //
+								.filter(to -> !fCache.hasDistance(from, to)) //
+								.map(to -> new Pair<Port, Port>(from, to))) //
+						.collect(Collectors.toSet());
+			};
+
+			// Use lamba function against our different possible from/tos
+			final Set<Pair<Port, Port>> missingDistances = new HashSet<>();
+			missingDistances.addAll(distanceChecker.apply(startPorts, loadPorts));
+			missingDistances.addAll(distanceChecker.apply(loadPorts, dischargePorts));
+			missingDistances.addAll(distanceChecker.apply(dischargePorts, loadPorts));
+			missingDistances.addAll(distanceChecker.apply(dischargePorts, endPorts));
+
+			missingDistances.addAll(distanceChecker.apply(startPorts, eventPorts));
+			missingDistances.addAll(distanceChecker.apply(eventPorts, loadPorts));
+			missingDistances.addAll(distanceChecker.apply(dischargePorts, eventPorts));
+			missingDistances.addAll(distanceChecker.apply(eventPorts, endPorts));
+
+			missingDistances.addAll(distanceChecker.apply(eventPorts, eventPorts));
+
+			missingDistances.addAll(distanceChecker.apply(startPorts, endPorts));
+
+			// Complex cargoes
+			for (final Cargo c : cargoModel.getCargoes()) {
+				if (c.getSlots().size() > 2) {
+					Port prev = null;
+					for (final Slot slot : c.getSortedSlots()) {
+						final Port thisPort = slot.getPort();
+						if (prev != null) {
+							missingDistances.addAll(distanceChecker.apply(Collections.singleton(prev), Collections.singleton(thisPort)));
+						}
+						prev = thisPort;
+					}
+				}
+			}
+
+			for (final Pair<Port, Port> p : missingDistances) {
+				final String msg = String.format("Missing distance between %s and %s.", getPortName(p.getFirst()), getPortName(p.getSecond()));
+				final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(msg));
+				failure.addEObjectAndFeature(p.getFirst(), MMXCorePackage.Literals.NAMED_OBJECT__NAME);
+				failure.addEObjectAndFeature(p.getSecond(), MMXCorePackage.Literals.NAMED_OBJECT__NAME);
+
+				statuses.add(failure);
+			}
+
+			// Check load -> discharge and back again - typically needed for notional voyages
+			for (Port from : loadPorts) {
+				for (Port to : dischargePorts) {
+					if (from != to) {
+						for (RouteOption route : RouteOption.values()) {
+							final RouteDistanceLineCache cache = caches.get(route);
+							if (cache != null && cache.hasDistance(from, to)) {
+								if (!cache.hasDistance(to, from)) {
+									final String msg = String.format("Missing distance between %s and %s for %s route.", getPortName(to), getPortName(from), route);
+									final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(msg));
+									failure.addEObjectAndFeature(from, MMXCorePackage.Literals.NAMED_OBJECT__NAME);
+									failure.addEObjectAndFeature(to, MMXCorePackage.Literals.NAMED_OBJECT__NAME);
+
+									statuses.add(failure);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
 		return Activator.PLUGIN_ID;
 	}
 
