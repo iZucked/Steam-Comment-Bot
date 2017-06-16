f42717dd138d29365b13826947d969c4bdf3423c
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-06-16 10:19:22 +0100

    Fix entry point editors

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/RouteTopLevelComposite.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/RouteTopLevelComposite.java
@@ -5,24 +5,19 @@
 package com.mmxlabs.models.lng.port.ui.editors;
 
 import java.util.Collection;
-import java.util.Iterator;
 
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.databinding.EMFDataBindingContext;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
-import com.mmxlabs.models.lng.cargo.VesselAvailability;
-import com.mmxlabs.models.lng.commercial.BallastBonusContract;
 import com.mmxlabs.models.lng.port.PortPackage;
 import com.mmxlabs.models.lng.port.Route;
 import com.mmxlabs.models.lng.port.ui.distanceeditor.DistanceEditorComposite;
@@ -33,20 +28,13 @@ import com.mmxlabs.models.ui.editors.util.EditorUtils;
 import com.mmxlabs.models.ui.impl.DefaultTopLevelComposite;
 
 /**
- * A display composite for the {@link VesselAvailability} editor to keep the start and end heel options in one column. Note this assumes start and end heel are the only contained children.
  * 
  * @author Simon Goodall
  * 
  */
 public class RouteTopLevelComposite extends DefaultTopLevelComposite {
 
-	/**
-	 * {@link Composite} to contain the heel editors
-	 */
-	private Composite right;
-	// private IDisplayComposite ballastBonusComposite;
-
-	public RouteTopLevelComposite(final Composite parent, final int style, final IDialogEditingContext dialogContext, FormToolkit toolkit) {
+	public RouteTopLevelComposite(final Composite parent, final int style, final IDialogEditingContext dialogContext, final FormToolkit toolkit) {
 		super(parent, style, dialogContext, toolkit);
 	}
 
@@ -58,80 +46,40 @@ public class RouteTopLevelComposite extends DefaultTopLevelComposite {
 
 		if (object instanceof Route) {
 
-			Route route = (Route) object;
+			final Route route = (Route) object;
 			final EClass eClass = object.eClass();
-			Composite containerComposite = toolkit.createComposite(this, SWT.NONE);
+			final Composite containerComposite = toolkit.createComposite(this, SWT.NONE);
 			containerComposite.setLayout(new GridLayout(2, true));
 
-			IDisplayComposite entryAComposite = createChildArea(root, route, containerComposite, PortPackage.Literals.ROUTE__ENTRY_A, route.getEntryA());
-			IDisplayComposite entryBComposite = createChildArea(root, route, containerComposite, PortPackage.Literals.ROUTE__ENTRY_B, route.getEntryB());
+			IDisplayComposite entryAComposite = null;
+			IDisplayComposite entryBComposite = null;
+			if (route.isCanal()) {
+				entryAComposite = createChildArea(root, route, containerComposite, PortPackage.Literals.ROUTE__ENTRY_A, route.getEntryA());
+				entryBComposite = createChildArea(root, route, containerComposite, PortPackage.Literals.ROUTE__ENTRY_B, route.getEntryB());
+			}
 
-			// // START CUSTOM SECTION
-			//// // Initialise right composite
-			//// right = toolkit.createComposite(containerComposite);
-			//// createChildComposites(root, object, eClass, right);
-			// // Single column
-			// final GridLayout layout = GridLayoutFactory.swtDefaults() //
-			// .numColumns(1) //
-			// .equalWidth(true) //
-			// .margins(0, 0) //
-			// .extendedMargins(0, 0, 0, 0) //
-			// .create();
-			// right.setLayout(layout);
-			// right.setLayoutData(new GridData(GridData.FILL_BOTH));
-			// right.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
-
-
-			final Iterator<IDisplayComposite> children = childComposites.iterator();
-			final Iterator<EObject> childObjectsItr = childObjects.iterator();
-
-			while (childObjectsItr.hasNext()) {
-				EObject next = childObjectsItr.next();
-				if (!(next instanceof BallastBonusContract)) {
-					children.next().display(dialogContext, root, next, range, dbc);
+			if (route.isCanal()) {
+				if (entryAComposite != null) {
+					entryAComposite.display(dialogContext, root, route.getEntryA(), range, dbc);
+				}
+				if (entryBComposite != null) {
+					entryBComposite.display(dialogContext, root, route.getEntryB(), range, dbc);
 				}
 			}
-			if (entryAComposite != null) {
-				entryAComposite.display(dialogContext, root, route.getEntryA(), range, dbc);
-			}
-			if (entryBComposite != null) {
-				entryAComposite.display(dialogContext, root, route.getEntryB(), range, dbc);
-			}
-			// createChildArea(root, route, containerComposite, ref, (EObject) o);
-			//
-			 final Group g = new Group(containerComposite, SWT.NONE);
-			 toolkit.adapt(g);
-			//
-			 g.setText(EditorUtils.unmangle(eClass.getName()));
-			 g.setLayout(new FillLayout());
-			 g.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).create());
-			 g.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
-			
-			 // topLevel = Activator.getDefault().getDisplayCompositeFactoryRegistry().getDisplayCompositeFactory(eClass).createSublevelComposite(g, eClass, dialogContext, toolkit);
-			 topLevel = new DistanceEditorComposite(g, SWT.NONE, toolkit);
-			 topLevel.setCommandHandler(commandHandler);
-			 topLevel.setEditorWrapper(editorWrapper);
-			// Overrides default layout factory so we get two columns columns
-//			containerComposite.setLayout(new GridLayout(2, true));
-//
-			 topLevel.display(dialogContext, root, object, range, dbc);
-//			Composite bottomComposite = toolkit.createComposite(this, SWT.NONE);
-//			bottomComposite.setLayout(new FillLayout());
-//			bottomComposite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-//
-//			final Group g2 = new Group(bottomComposite, SWT.NONE);
-//			toolkit.adapt(g2);
-//			g2.setText("Distances");
-//			g2.setLayout(new GridLayout());
-//			g2.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
-//			//
-			// ballastBonusComposite = new BallastBonusContractDetailComposite(g2, SWT.NONE, toolkit, () -> {
-			// if (!RouteTopLevelComposite.this.isDisposed()) {
-			// RouteTopLevelComposite.this.layout(true, true);
-			// }
-			// });
-			// ballastBonusComposite.setCommandHandler(commandHandler);
-			// ballastBonusComposite.display(dialogContext, root, object, range, dbc);
+
+			final Group g = new Group(containerComposite, SWT.NONE);
+			toolkit.adapt(g);
+
+			g.setText(EditorUtils.unmangle(eClass.getName()));
+			g.setLayout(new FillLayout());
+			g.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).create());
+			g.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
+
+			topLevel = new DistanceEditorComposite(g, SWT.NONE, toolkit);
+			topLevel.setCommandHandler(commandHandler);
+			topLevel.setEditorWrapper(editorWrapper);
+
+			topLevel.display(dialogContext, root, object, range, dbc);
 
 			this.setLayout(new GridLayout(1, true));
 		} else {
@@ -139,16 +87,4 @@ public class RouteTopLevelComposite extends DefaultTopLevelComposite {
 			super.display(dialogContext, root, object, range, dbc);
 		}
 	}
-
-	@Override
-	public void displayValidationStatus(IStatus status) {
-		super.displayValidationStatus(status);
-		// ballastBonusComposite.displayValidationStatus(status);
-	}
-
-	// @Override
-	// protected boolean shouldDisplay(final EReference ref) {
-	//// return ref.isContainment() && !ref.isMany() && ref != CargoPackage.eINSTANCE.getVesselAvailability_BallastBonusContract();
-	// }
-
 }
