abae5ae44aa1ee98b229d640a96b5488c03d9f58
Jenkins <hudson@minimaxlabs.com>
2017-06-20 17:57:03 +0100

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/presentation/wizard/ADPModelUtil.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/presentation/wizard/ADPModelUtil.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.adp.presentation.wizard;
 
 import java.time.Month;
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp/src/com/mmxlabs/models/lng/adp/ext/impl/CargoIntervalFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp/src/com/mmxlabs/models/lng/adp/ext/impl/CargoIntervalFactory.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.adp.ext.impl;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
