2989f3353c0be1e260d39fea7c2479c4beb403cf
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-07-18 17:27:49 +0100

    Move ITS property to RCP common

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/Constants.java
@@ -0,0 +1,16 @@
+package com.mmxlabs.rcp.common;
+
+/**
+ * Shared constants. Try not to use this.
+ * 
+ * @author Simon Goodall
+ *
+ */
+public class Constants {
+	/**
+	 * Property set by ITS runs to trigger repeatable test behaviour. E.g. reports which use "today" should use a fixed value instead. Property used in combination with unit tests so copy/paste to
+	 * clipboard returns a constant date rather than the current date.
+	 */
+	public static final String PROPERTY_RUNNING_ITS = "com.mmxlabs.running.its";
+
+}
