7e7a6d6f1f1735a182def1756c3aa884274c0a79
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-09-18 11:32:39 +0100

    Add BooleanSupplier impl

---
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/util/BooleanSupplier.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
+
+package com.mmxlabs.common.util;
+
+@FunctionalInterface
+public interface BooleanSupplier {
+
+	boolean get();
+}
