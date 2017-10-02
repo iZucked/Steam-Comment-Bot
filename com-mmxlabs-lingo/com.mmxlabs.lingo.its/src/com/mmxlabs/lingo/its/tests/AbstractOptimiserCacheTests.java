e1843fb035f56179a57dd731155216e1f54880d8
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-10-02 14:19:35 +0100

    Create an explicit cache test group

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/AbstractOptimiserCacheTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/AbstractOptimiserCacheTests.java
@@ -12,7 +12,7 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import com.mmxlabs.lingo.its.tests.category.OptimisationTest;
+import com.mmxlabs.lingo.its.tests.category.CacheTest;
 import com.mmxlabs.models.lng.parameters.SimilarityMode;
 import com.mmxlabs.models.lng.transformer.inject.LNGTransformerHelper;
 import com.mmxlabs.scheduler.optimiser.cache.CacheMode;
@@ -32,70 +32,70 @@ public abstract class AbstractOptimiserCacheTests extends AbstractAdvancedOptimi
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_VPO_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Verify, CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_TimeWindow_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Verify, CacheMode.Off, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_VolumeAllocator_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Verify, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_PNL_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Verify, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_VolumeAllocatedSequence_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Verify),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_GCO_VPO_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Verify, CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_GCO_TimeWindow_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Verify, CacheMode.Off, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_GCO_VolumeAllocator_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Verify, CacheMode.Off, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_GCO_PNL_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Verify, CacheMode.Off),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
 	}
 
 	@Test
-	@Category(OptimisationTest.class)
+	@Category(CacheTest.class)
 	public void advancedOptimisation_Limited_GCO_VolumeAllocatedSequence_Verify() throws Exception {
 		runAdvancedOptimisationTestCase(true, SimilarityMode.OFF, false, false, withCacheSettings(CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Off, CacheMode.Verify),
 				LNGTransformerHelper.HINT_TESTING_IGNORE_CACHE_SETTINGS);
