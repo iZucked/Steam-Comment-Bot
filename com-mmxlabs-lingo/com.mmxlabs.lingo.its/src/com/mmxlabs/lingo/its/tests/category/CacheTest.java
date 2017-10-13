6ad2d5a0b2563b1275bc05f966ca4a43ab44c28e
Jenkins <hudson@minimaxlabs.com>
2017-10-13 16:49:48 +0100

    Update headers

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/category/CacheTest.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/category/CacheTest.java
@@ -1,4 +1,8 @@
 /**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
+/**
 RegressionTest.java * Copyright (C) Minimax Labs Ltd., 2010 - 2017
  * All rights reserved.
  */
