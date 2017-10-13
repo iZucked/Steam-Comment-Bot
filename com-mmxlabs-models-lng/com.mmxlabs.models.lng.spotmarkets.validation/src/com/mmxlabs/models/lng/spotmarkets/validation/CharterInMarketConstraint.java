4293791b2994e61eded9f7bb27439002cc4d2025
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-10-13 15:04:55 +0100

    Charter in markets with nominals enabled and min or max duration should not be permitted

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.validation/src/com/mmxlabs/models/lng/spotmarkets/validation/CharterInMarketConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.validation/src/com/mmxlabs/models/lng/spotmarkets/validation/CharterInMarketConstraint.java
@@ -44,31 +44,43 @@ public class CharterInMarketConstraint extends AbstractModelConstraint {
 			if (spotMarket.getCharterContract() != null && spotMarket.getCharterContract() instanceof BallastBonusCharterContract) {
 				BallastBonusCharterContract ballastBonusCharterContract = (BallastBonusCharterContract) spotMarket.getCharterContract();
 				if (spotMarket.isNominal()) {
-					//nominals
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format("[Charter in market vessel | %s] has nominals enabled and a ballast bonus chartering contract.", spotMarket.getName())));
+					// nominals
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx
+							.createFailureStatus(String.format("[Charter in market vessel | %s] has nominals enabled and a ballast bonus chartering contract.", spotMarket.getName())));
 					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getSpotMarketsModel_CharterInMarkets());
 					failures.add(dsd);
 				}
 				if (ballastBonusCharterContract.getEntity() == null) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format("[Charter in market vessel | %s] uses a ballast bonus chartering contract without an entity set.", spotMarket.getName())));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx
+							.createFailureStatus(String.format("[Charter in market vessel | %s] uses a ballast bonus chartering contract without an entity set.", spotMarket.getName())));
 					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getSpotMarketsModel_CharterInMarkets());
 					failures.add(dsd);
 				}
 			}
-			
+
 			int minDurationInHours = spotMarket.getMarketOrContractMinDuration();
 			int maxDurationInHours = spotMarket.getMarketOrContractMaxDuration();
-			
+
+			if (minDurationInHours != 0 || maxDurationInHours != 0) {
+				if (spotMarket.isNominal()) {
+					// nominals
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus(String.format("[Charter in market vessel | %s] has nominals enabled and a min or max duration.", spotMarket.getName())));
+					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getSpotMarketsModel_CharterInMarkets());
+					failures.add(dsd);
+				}
+			}
 			if (minDurationInHours != 0 && maxDurationInHours != 0) {
 				if (minDurationInHours > maxDurationInHours) {
-					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(String.format("[Charter in market vessel | %s] has min duration superior to the max duration.", spotMarket.getName())));
+					final DetailConstraintStatusDecorator dsd = new DetailConstraintStatusDecorator(
+							(IConstraintStatus) ctx.createFailureStatus(String.format("[Charter in market vessel | %s] has min duration superior to the max duration.", spotMarket.getName())));
 					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getSpotMarketsModel_CharterInMarkets());
 					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getCharterInMarket_MinDuration());
 					dsd.addEObjectAndFeature(spotMarket, SpotMarketsPackage.eINSTANCE.getCharterInMarket_MaxDuration());
 					failures.add(dsd);
 				}
 			}
-			
+
 		}
 		final MultiStatus multi = new MultiStatus(Activator.PLUGIN_ID, IStatus.ERROR, null, null);
 		for (final IStatus s : failures) {
