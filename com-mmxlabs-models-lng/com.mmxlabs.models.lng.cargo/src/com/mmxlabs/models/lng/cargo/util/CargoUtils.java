abd3defaf4c3db31855d3b4957b6718a24158757
alex <alex.churchill@minimaxlabs.com>
2017-10-22 12:53:15 +0100

    Profit shared sale constraint BugzId:2578

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo/src/com/mmxlabs/models/lng/cargo/util/CargoUtils.java
@@ -0,0 +1,29 @@
+package com.mmxlabs.models.lng.cargo.util;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.mmxlabs.common.CollectionsUtil;
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.LoadSlot;
+import com.mmxlabs.models.lng.cargo.Slot;
+
+public class CargoUtils {
+	public static @Nullable LoadSlot getLoadSlot(Cargo cargo) {
+		Slot firstElement = CollectionsUtil.getFirstElement(cargo.getSortedSlots());
+		LoadSlot load = null;
+		if (firstElement != null && firstElement instanceof LoadSlot) {
+			load = (LoadSlot) firstElement;
+		}
+		return load;
+	}
+
+	public static @Nullable DischargeSlot getDischargeSlot(Cargo cargo) {
+		Slot lastElement = CollectionsUtil.getLastElement(cargo.getSortedSlots());
+		DischargeSlot discharge = null;
+		if (lastElement != null && lastElement instanceof DischargeSlot) {
+			discharge = (DischargeSlot) lastElement;
+		}
+		return discharge;
+	}
+}
