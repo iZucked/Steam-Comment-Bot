2944464c2a04ab2a3f44f52757519c07b3083af5
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-10-27 14:11:05 +0100

    Fix creation of expression contracts.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/modelfactories/ContractModelFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.commercial.editor/src/com/mmxlabs/models/lng/commercial/ui/modelfactories/ContractModelFactory.java
@@ -8,28 +8,50 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 
+import com.mmxlabs.models.lng.commercial.CommercialFactory;
 import com.mmxlabs.models.lng.commercial.CommercialPackage;
+import com.mmxlabs.models.lng.commercial.Contract;
+import com.mmxlabs.models.lng.commercial.LNGPriceCalculatorParameters;
 import com.mmxlabs.models.ui.modelfactories.DefaultModelFactory;
 
 /**
  */
 public class ContractModelFactory extends DefaultModelFactory {
 	protected String priceInfoClassName;
-	
+
+	@Override
+	protected EObject constructInstance(final EClass eClass) {
+		final EObject instance = super.constructInstance(eClass);
+
+		if (instance instanceof Contract) {
+			final Contract contract = (Contract) instance;
+			if (contract.getPriceInfo() == null) {
+				contract.setPriceInfo(CommercialFactory.eINSTANCE.createExpressionPriceParameters());
+				if (priceInfoClassName != null) {
+					final EClass priceInfoClass = getEClassFromName(priceInfoClassName, null);
+					// return a price info object of the specified type
+					if (priceInfoClass != null) {
+						contract.setPriceInfo((LNGPriceCalculatorParameters) priceInfoClass.getEPackage().getEFactoryInstance().create(priceInfoClass));
+					}
+				}
+			}
+		}
+		return instance;
+	}
+
 	@Override
 	protected EObject createSubInstance(final EObject top, final EReference reference) {
 		if (reference.equals(CommercialPackage.Literals.CONTRACT__PRICE_INFO)) {
-			EClass priceInfoClass = getEClassFromName(priceInfoClassName, null);
+			final EClass priceInfoClass = getEClassFromName(priceInfoClassName, null);
 			// return a price info object of the specified type
 			if (priceInfoClass != null) {
 				return priceInfoClass.getEPackage().getEFactoryInstance().create(priceInfoClass);
 			}
-			
+
 			return CommercialPackage.eINSTANCE.getEFactoryInstance().create(CommercialPackage.Literals.EXPRESSION_PRICE_PARAMETERS);
-			
+
 		}
 		return super.createSubInstance(top, reference);
 	}
-	
 
 }
