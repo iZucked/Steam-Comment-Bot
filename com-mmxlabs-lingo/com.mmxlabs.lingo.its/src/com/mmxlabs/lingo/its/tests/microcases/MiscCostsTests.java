d7b0e408abd260f7ceb1397f1679f97c37f809ee
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-11-24 11:43:15 +0000

    Fix up assertion statements (ordering, bad messages)

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/MiscCostsTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/MiscCostsTests.java
@@ -41,29 +41,28 @@ public class MiscCostsTests extends AbstractMicroTestCase {
 	@Test
 	@Category({ MicroTest.class })
 	public void testCargo_NoMiscCosts() throws Exception {
-		
+
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
 		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
-				.withOptionality(true)
-				.build();
-		
+				.withOptionality(true).build();
+
 		@NonNull
 		final Port port1 = portFinder.findPort("Point Fortin");
-		
+
 		@NonNull
 		final Port port2 = portFinder.findPort("Dominion Cove Point LNG");
-		
+
 		// map into same timezone to make expectations easier
 		port1.setTimeZone("UTC");
 		port2.setTimeZone("UTC");
-		
+
 		// Set distance and speed to exact multiple -- quickest travel time is 100 hours
 		scenarioModelBuilder.getPortModelBuilder().setPortToPortDistance(port1, port2, 1500, 2000, 2000, true);
 		vesselClass.setMaxSpeed(15.0);
-		
+
 		final LocalDateTime dischargeDate = LocalDateTime.of(2015, 12, 1, 0, 0, 0).plusHours(24 + 100);
-		
+
 		final Cargo cargo = cargoModelBuilder.makeCargo() //
 				.makeFOBPurchase("L", LocalDate.of(2015, 12, 1), port1, null, entity, "5") //
 				.withWindowStartTime(0) //
@@ -79,60 +78,58 @@ public class MiscCostsTests extends AbstractMicroTestCase {
 				//
 				.withVesselAssignment(vesselAvailability, 1) //
 				.build();
-		
+
 		evaluateTest(null, null, scenarioRunner -> {
-			
+
 			final SlotVisit visit1 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(0), lngScenarioModel);
 			final PortVisitLateness lateness1 = visit1.getLateness();
 			Assert.assertNull(lateness1);
-			
+
 			final SlotVisit visit2 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(1), lngScenarioModel);
 			final PortVisitLateness lateness2 = visit2.getLateness();
 			Assert.assertNull(lateness2);
-			
-			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream()
-					.filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst().get();
+
+			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream().filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst()
+					.get();
 			assert cargoAllocation != null;
-			Assert.assertEquals(cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss(), 6042139L);
+			Assert.assertEquals(6042139L, cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss());
 			StartEvent start = getStartEvent(vesselAvailability);
-			Assert.assertEquals(start.getGroupProfitAndLoss().getProfitAndLoss(), 0);
-			Assert.assertEquals(ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()), 6042139L);
+			Assert.assertEquals(0, start.getGroupProfitAndLoss().getProfitAndLoss());
+			Assert.assertEquals(6042139L, ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()));
 		});
 	}
 
 	@Test
 	@Category({ MicroTest.class })
 	public void testCargo_WithMiscCostsLoad() throws Exception {
-		
+
 		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
 		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
 		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
-				.withOptionality(true)
-				.build();
-		
+				.withOptionality(true).build();
+
 		@NonNull
 		final Port port1 = portFinder.findPort("Point Fortin");
-		
+
 		@NonNull
 		final Port port2 = portFinder.findPort("Dominion Cove Point LNG");
-		
+
 		// map into same timezone to make expectations easier
 		port1.setTimeZone("UTC");
 		port2.setTimeZone("UTC");
-		
+
 		// Set distance and speed to exact multiple -- quickest travel time is 100 hours
 		scenarioModelBuilder.getPortModelBuilder().setPortToPortDistance(port1, port2, 1500, 2000, 2000, true);
 		vesselClass.setMaxSpeed(15.0);
-		
+
 		final LocalDateTime dischargeDate = LocalDateTime.of(2015, 12, 1, 0, 0, 0).plusHours(24 + 100);
-		
+
 		final Cargo cargo = cargoModelBuilder.makeCargo() //
 				.makeFOBPurchase("L", LocalDate.of(2015, 12, 1), port1, null, entity, "5") //
 				.withWindowStartTime(0) //
 				.withVisitDuration(24) //
 				.withWindowSize(0, TimePeriod.HOURS) //
-				.withMiscCosts(1_000_000)
-				.build() //
+				.withMiscCosts(1_000_000).build() //
 				//
 				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
 				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
@@ -142,86 +139,86 @@ public class MiscCostsTests extends AbstractMicroTestCase {
 				//
 				.withVesselAssignment(vesselAvailability, 1) //
 				.build();
-		
+
 		evaluateTest(null, null, scenarioRunner -> {
-			
+
 			final SlotVisit visit1 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(0), lngScenarioModel);
 			final PortVisitLateness lateness1 = visit1.getLateness();
 			Assert.assertNull(lateness1);
-			
+
 			final SlotVisit visit2 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(1), lngScenarioModel);
 			final PortVisitLateness lateness2 = visit2.getLateness();
 			Assert.assertNull(lateness2);
-			
-			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream()
-					.filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst().get();
+
+			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream().filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst()
+					.get();
 			assert cargoAllocation != null;
-			Assert.assertEquals(String.format("Expected %s was %s", 6042139, cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss()), cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss(), 6_042_139L - 1_000_000L);
-			Assert.assertEquals(ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()), 6_042_139L - 1_000_000L);
-		});
-	}
-	
-	@Test
-	@Category({ MicroTest.class })
-	public void testCargo_WithMiscCostsDischarge() throws Exception {
-		
-		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
-		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
-		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
-				.withOptionality(true)
-				.build();
-		
-		@NonNull
-		final Port port1 = portFinder.findPort("Point Fortin");
-		
-		@NonNull
-		final Port port2 = portFinder.findPort("Dominion Cove Point LNG");
-		
-		// map into same timezone to make expectations easier
-		port1.setTimeZone("UTC");
-		port2.setTimeZone("UTC");
-		
-		// Set distance and speed to exact multiple -- quickest travel time is 100 hours
-		scenarioModelBuilder.getPortModelBuilder().setPortToPortDistance(port1, port2, 1500, 2000, 2000, true);
-		vesselClass.setMaxSpeed(15.0);
-		
-		final LocalDateTime dischargeDate = LocalDateTime.of(2015, 12, 1, 0, 0, 0).plusHours(24 + 100);
-		
-		final Cargo cargo = cargoModelBuilder.makeCargo() //
-				.makeFOBPurchase("L", LocalDate.of(2015, 12, 1), port1, null, entity, "5") //
-				.withWindowStartTime(0) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.build() //
-				//
-				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
-				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
-				.withVisitDuration(24) //
-				.withWindowSize(0, TimePeriod.HOURS) //
-				.withMiscCosts(1_000_000)
-				.build() //
-				//
-				.withVesselAssignment(vesselAvailability, 1) //
-				.build();
-		
-		evaluateTest(null, null, scenarioRunner -> {
-			
-			final SlotVisit visit1 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(0), lngScenarioModel);
-			final PortVisitLateness lateness1 = visit1.getLateness();
-			Assert.assertNull(lateness1);
-			
-			final SlotVisit visit2 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(1), lngScenarioModel);
-			final PortVisitLateness lateness2 = visit2.getLateness();
-			Assert.assertNull(lateness2);
-			
-			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream()
-					.filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst().get();
-			assert cargoAllocation != null;
-			Assert.assertEquals(String.format("Expected %s was %s", 6042139, cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss()), cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss(), 6_042_139L - 1_000_000L);
-			Assert.assertEquals(ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()), 6_042_139L - 1_000_000L);
+			Assert.assertEquals(String.format("Expected %s was %s", 6042139 - 1_000_000L, 6_042_139L - 1_000_000L, cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss()),
+					cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss());
+			Assert.assertEquals(6_042_139L - 1_000_000L, ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()));
 		});
 	}
 
+	@Test
+	@Category({ MicroTest.class })
+	public void testCargo_WithMiscCostsDischarge() throws Exception {
+
+		final VesselClass vesselClass = fleetModelFinder.findVesselClass("STEAM-145");
+		final Vessel vessel = fleetModelBuilder.createVessel("vessel", vesselClass);
+		final VesselAvailability vesselAvailability = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
+				.withOptionality(true).build();
+
+		@NonNull
+		final Port port1 = portFinder.findPort("Point Fortin");
+
+		@NonNull
+		final Port port2 = portFinder.findPort("Dominion Cove Point LNG");
+
+		// map into same timezone to make expectations easier
+		port1.setTimeZone("UTC");
+		port2.setTimeZone("UTC");
+
+		// Set distance and speed to exact multiple -- quickest travel time is 100 hours
+		scenarioModelBuilder.getPortModelBuilder().setPortToPortDistance(port1, port2, 1500, 2000, 2000, true);
+		vesselClass.setMaxSpeed(15.0);
+
+		final LocalDateTime dischargeDate = LocalDateTime.of(2015, 12, 1, 0, 0, 0).plusHours(24 + 100);
+
+		final Cargo cargo = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("L", LocalDate.of(2015, 12, 1), port1, null, entity, "5") //
+				.withWindowStartTime(0) //
+				.withVisitDuration(24) //
+				.withWindowSize(0, TimePeriod.HOURS) //
+				.build() //
+				//
+				.makeDESSale("D", dischargeDate.toLocalDate(), port2, null, entity, "7") //
+				.withWindowStartTime(dischargeDate.toLocalTime().getHour()) //
+				.withVisitDuration(24) //
+				.withWindowSize(0, TimePeriod.HOURS) //
+				.withMiscCosts(1_000_000) //
+				.build() //
+				//
+				.withVesselAssignment(vesselAvailability, 1) //
+				.build();
+
+		evaluateTest(null, null, scenarioRunner -> {
+
+			final SlotVisit visit1 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(0), lngScenarioModel);
+			final PortVisitLateness lateness1 = visit1.getLateness();
+			Assert.assertNull(lateness1);
+
+			final SlotVisit visit2 = MicroTestUtils.findSlotVisit(cargo.getSlots().get(1), lngScenarioModel);
+			final PortVisitLateness lateness2 = visit2.getLateness();
+			Assert.assertNull(lateness2);
+
+			CargoAllocation cargoAllocation = lngScenarioModel.getScheduleModel().getSchedule().getCargoAllocations().stream().filter(c -> ScheduleModelUtils.matchingSlots(cargo, c)).findFirst()
+					.get();
+			assert cargoAllocation != null;
+			Assert.assertEquals(String.format("Expected %s was %s", 6_042_139L - 1_000_000L, 6_042_139L - 1_000_000L, cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss()),
+					cargoAllocation.getGroupProfitAndLoss().getProfitAndLoss());
+			Assert.assertEquals(6_042_139L - 1_000_000L, ScheduleModelKPIUtils.getScheduleProfitAndLoss(lngScenarioModel.getScheduleModel().getSchedule()));
+		});
+	}
 
 	private StartEvent getStartEvent(final VesselAvailability vesselAvailability) {
 		Sequence sequence = lngScenarioModel.getScheduleModel().getSchedule().getSequences().stream().filter(s -> s.getVesselAvailability().equals(vesselAvailability)).findFirst().get();
