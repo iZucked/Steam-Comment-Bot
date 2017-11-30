1d4416eed0a3fce12401b4c9236b7de61191ed76
joseph pallamidessi <jpallamidessi@minimaxlabs.com>
2017-11-30 16:27:06 +0000

    [Cleaning] Remove useless overload (same code) [K431940]

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/formatters/CostFormatter.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/formatters/CostFormatter.java
@@ -4,28 +4,21 @@
  */
 package com.mmxlabs.lingo.reports.views.formatters;
 
-import java.util.List;
-
-import org.eclipse.emf.common.notify.Notifier;
-
-import com.mmxlabs.common.Pair;
-import com.mmxlabs.models.ui.tabular.ICellRenderer;
-
 /**
  */
 public class CostFormatter extends IntegerFormatter {
 
 	public enum Type {
-	    COST, REVENUE, UNSET 
+		COST, REVENUE, UNSET
 	}
-	
+
 	private final boolean includeUnits;
 	private Type type = Type.UNSET;
-	
+
 	public CostFormatter(final boolean includeUnits) {
 		this.includeUnits = includeUnits;
 	}
-	
+
 	public CostFormatter(final boolean includeUnits, Type type) {
 		this.includeUnits = includeUnits;
 		this.type = type;
@@ -35,18 +28,11 @@ public class CostFormatter extends IntegerFormatter {
 		this.type = type;
 		this.includeUnits = false;
 	}
-	
-	public Integer getIntValue(final Object object) {
-		if (object == null) {
-			return null;
-		}
-		return ((Number) object).intValue();
-	}
-	
+
 	public Type getType() {
 		return type;
 	}
-	
+
 	@Override
 	public String render(final Object object) {
 		if (object == null) {
@@ -57,29 +43,5 @@ public class CostFormatter extends IntegerFormatter {
 			return "";
 		}
 		return String.format(includeUnits ? "$%,d" : "%,d", x);
-	}
-
-	@Override
-	public boolean isValueUnset(Object object) {
-		return false;
-	}
-
-	@Override
-	public Comparable getComparable(final Object object) {
-		final Integer x = getIntValue(object);
-		if (x == null) {
-			return -Integer.MAX_VALUE;
-		}
-		return x;
-	}
-
-	@Override
-	public Object getFilterValue(final Object object) {
-		return getComparable(object);
-	}
-	
-	@Override
-	public Iterable<Pair<Notifier, List<Object>>> getExternalNotifiers(Object object) {
-		return null;
 	}
 }
