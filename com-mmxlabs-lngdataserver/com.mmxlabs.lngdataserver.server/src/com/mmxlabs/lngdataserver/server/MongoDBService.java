b0bc283cfbf80a3ff3a3be76c297c134c6db8e20
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-12-06 14:04:12 +0000

    Terminate existing mongo on startup

--- com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.server/src/com/mmxlabs/lngdataserver/server/MongoDBService.java
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.server/src/com/mmxlabs/lngdataserver/server/MongoDBService.java
@@ -1,10 +1,19 @@
 package com.mmxlabs.lngdataserver.server;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.util.UUID;
 
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.mongodb.MongoClient;
 
 import de.flapdoodle.embed.mongo.Command;
 import de.flapdoodle.embed.mongo.MongodExecutable;
@@ -18,9 +27,9 @@ import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Storage;
 import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.runtime.Mongod;
 import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import de.flapdoodle.embed.process.extract.ITempNaming;
-import de.flapdoodle.embed.process.extract.UUIDTempNaming;
 import de.flapdoodle.embed.process.io.directories.FixedPath;
 import de.flapdoodle.embed.process.io.directories.IDirectory;
 import de.flapdoodle.embed.process.runtime.Network;
@@ -32,46 +41,66 @@ public class MongoDBService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MongoDBService.class);
 
-	// @Value("${db.embedded:true}")
-	// private boolean embedded;
-	// @Value("${db.embeddedBinaries:#{null}}")
 	private String embeddedBinariesLocation;
-	// @Value("${db.host:localhost}")
-	// private String host;
-	// @Value("${db.port:27017}")
-	// private int providedPort; // this is only a hint in the embedded setup
-	// @Value("${db.user:#{null}}")
-	// private String dbUser;
-	// @Value("${db.pw:#{null}}")
-	// private String dbPw;
 
-	// // only needed for embedded mode
-	// @Value("${db.embeddedDataLocation:mongo_data}")
 	private String embeddedDataLocation;
 	//
 	private static MongodStarter MONGOD_STARTER;
 	private MongodExecutable mongodExecutable;
 	private MongodProcess mongodProcess;
-	// private int randomPort;
 
 	public int start() throws IOException, ClassNotFoundException {
+
+		final IPath workspaceLocation = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+		final IPath mongoPath = workspaceLocation.append("mongo");
+		mongoPath.toFile().mkdirs();
+
+		final String mongoPortFile = mongoPath.append("mongodb.port").toOSString();
+		try {
+			final int port = readInt(mongoPortFile);
+			if (port > 0) {
+				if (false) {
+					// Attempt to reuse the existing connection. Note, this means we have no control over the process
+					final MongoClient mongoClient = new MongoClient("127.0.0.1", port);
+					mongoClient.close();
+					LOGGER.info("Connected to existing MongoDB on port " + port);
+					return port;
+				} else {
+					// Terminate existing server
+					Mongod.sendShutdown(InetAddress.getByName("127.0.0.1"), port);
+					// Sleep a bit to let it shutdown
+					Thread.sleep(500);
+				}
+			}
+		} catch (final Exception e) {
+			// Ignore
+		}
+
 		if (MONGOD_STARTER == null) {
 			LOGGER.debug("Starting embedded Mongo DB");// " + getPort());
 			if (embeddedBinariesLocation != null) {
-				IDirectory artifactStorePath = new FixedPath(embeddedBinariesLocation);
-				ITempNaming executableNaming = new UUIDTempNaming();
+				final IDirectory artifactStorePath = new FixedPath(embeddedBinariesLocation);
+				final ITempNaming executableNaming = new ITempNaming() {
+					@Override
+					public String nameFor(final String arg0, final String arg1) {
+						return mongoPath.append(String.format("%s-%s-%s", arg0, UUID.randomUUID().toString(), arg1)).toOSString();
+					}
+				};
 
-				Command command = Command.MongoD;
+				final Command command = Command.MongoD;
 
-				IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder()
+				final IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder()//
 						.defaults(command)
-						.artifactStore(new ExtractedArtifactStoreBuilder()
-								.defaults(command)
-						.download(new DownloadConfigBuilder()
-								.defaultsForCommand(command)
-								.artifactStorePath(artifactStorePath)
-								.build())
-						.executableNaming(executableNaming))
+
+						.artifactStore(new ExtractedArtifactStoreBuilder()//
+								.defaults(command)//
+								.download(new DownloadConfigBuilder() //
+										.defaultsForCommand(command)//
+										.artifactStorePath(artifactStorePath)//
+										.build())//
+								.executableNaming(executableNaming))
+						.daemonProcess(true)
+
 						.build();
 
 				MONGOD_STARTER = MongodStarter.getInstance(runtimeConfig);
@@ -81,81 +110,51 @@ public class MongoDBService {
 
 		}
 
-		File file = new File(embeddedDataLocation);
+		final File file = new File(embeddedDataLocation);
 		file.mkdirs();
 		// Net n = new Net(bindIp, port, ipv6)
-		mongodExecutable = MONGOD_STARTER.prepare(new MongodConfigBuilder().version(Version.Main.PRODUCTION).replication(new Storage(embeddedDataLocation, null, 0))
-				// .net(new Net(host, getPort(), false))
+		mongodExecutable = MONGOD_STARTER.prepare(new MongodConfigBuilder() //
+				.version(Version.Main.PRODUCTION) //
+				.replication(new Storage(embeddedDataLocation, null, 0)) //
 				// .net(new Net(host, getPort(), false))
 				.net(new Net("127.0.0.1", Network.getFreeServerPort(), false))
 				// .setParameter("bind_ip","127.0.0.1")
-
 				.cmdOptions(new MongoCmdOptionsBuilder().syncDelay(10).build()).build());
 
 		mongodProcess = mongodExecutable.start();
-
-		int port = mongodProcess.getConfig().net().getPort();
-		System.out.println(port);
-		int ii = 0;
+		final int port = mongodProcess.getConfig().net().getPort();
+		try (PrintWriter pw = new PrintWriter(mongoPortFile)) {
+			pw.println(port);
+		}
 		LOGGER.debug("Started embedded Mongo DB on port " + port);// " + getPort());
 
 		return port;
 	}
-	//
-	// public int getPort() throws IOException {
-	// if (randomPort == 0 && portAvailable(providedPort)) {
-	// // try to set the random port to the one provided by the user
-	// // this is slightly dangerous since it could be taken in the meantime
-	// randomPort = providedPort;
-	// return randomPort;
-	// }
-	// if (randomPort == 0) {
-	// try {
-	// randomPort = Network.getFreeServerPort();
-	// } catch (IOException e) {
-	// LOGGER.warn("Error getting free port for embedded mongodb", e);
-	// throw e;
-	// }
-	// }
-	// return randomPort;
-	// }
 
-	// private static boolean portAvailable(int port) {
-	// LOGGER.info("--------------Testing port " + port);
-	// Socket s = null;
-	// try {
-	// s = new Socket("localhost", port);
-	//
-	// // If the code makes it this far without an exception it means
-	// // something is using the port and has responded.
-	// LOGGER.info("--------------Port " + port + " is not available");
-	// return false;
-	// } catch (IOException e) {
-	// LOGGER.info("--------------Port " + port + " is available");
-	// return true;
-	// } finally {
-	// if (s != null) {
-	// try {
-	// s.close();
-	// } catch (IOException e) {
-	// throw new RuntimeException("Error closing probing socket", e);
-	// }
-	// }
-	// }
-	// }
+	private int readInt(final String file) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			final String line = reader.readLine();
+			return Integer.parseInt(line);
+		} catch (final Exception e) {
+			// Ignore
+		}
+
+		return 0;
+	}
 
 	public void stop() {
 		LOGGER.info("shutting down embedded database...");
 		mongodProcess.stop();
 		mongodExecutable.stop();
 		LOGGER.info("... embedded mongodb gracefully shut down");
+		MONGOD_STARTER = null;
 	}
 
 	public String getEmbeddedBinariesLocation() {
 		return embeddedBinariesLocation;
 	}
 
-	public void setEmbeddedBinariesLocation(String embeddedBinariesLocation) {
+	public void setEmbeddedBinariesLocation(final String embeddedBinariesLocation) {
 		this.embeddedBinariesLocation = embeddedBinariesLocation;
 	}
 
@@ -163,7 +162,7 @@ public class MongoDBService {
 		return embeddedDataLocation;
 	}
 
-	public void setEmbeddedDataLocation(String embeddedDataLocation) {
+	public void setEmbeddedDataLocation(final String embeddedDataLocation) {
 		this.embeddedDataLocation = embeddedDataLocation;
 	}
 }
