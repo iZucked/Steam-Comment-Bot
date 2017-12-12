5f0ed2188c0ee3164e174381c16b9a769b718d97
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-12-12 09:39:17 +0000

    Use a plugin startup extension to start plugin during ITS

---
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration.scenario/src/com/mmxlabs/models/migration/scenario/internal/PluginStartup.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.models.migration.scenario.internal;
+
+import org.eclipse.ui.IStartup;
+
+public class PluginStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// Do nothing except trigger plugin startup
+
+	}
+
+}
---
+++ com-mmxlabs-models-migration/com.mmxlabs.models.migration/src/com/mmxlabs/models/migration/internal/PluginStartup.java
@@ -0,0 +1,12 @@
+package com.mmxlabs.models.migration.internal;
+
+import org.eclipse.ui.IStartup;
+
+public class PluginStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// Do nothing except trigger plugin startup 
+	}
+
+}
