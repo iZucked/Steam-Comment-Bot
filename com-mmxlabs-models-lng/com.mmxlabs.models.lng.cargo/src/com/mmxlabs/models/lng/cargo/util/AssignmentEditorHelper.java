3151bea52395c4ff39fdd1e031d4de4587b2fe4d
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-12-18 14:37:25 +0000

    Fix issue where we created an entry for nominal cargoes even if the market is not for nominal cargoes

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo/src/com/mmxlabs/models/lng/cargo/util/AssignmentEditorHelper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo/src/com/mmxlabs/models/lng/cargo/util/AssignmentEditorHelper.java
@@ -181,7 +181,8 @@ public class AssignmentEditorHelper {
 		final List<Pair<CharterInMarket, Integer>> charterInMarketKeysOrder = new ArrayList<>();
 		final Map<Pair<CharterInMarket, Integer>, List<AssignableElement>> spotGrouping = new HashMap<>();
 		for (final CharterInMarket charterInMarket : spotMarketsModel.getCharterInMarkets()) {
-			for (int i = -1; i < charterInMarket.getSpotCharterCount(); ++i) {
+			int start = charterInMarket.isNominal() ? -1 : 0;
+			for (int i = start; i < charterInMarket.getSpotCharterCount(); ++i) {
 				final Pair<CharterInMarket, Integer> key = new Pair<>(charterInMarket, i);
 				charterInMarketKeysOrder.add(key);
 				// Pre-create map values
