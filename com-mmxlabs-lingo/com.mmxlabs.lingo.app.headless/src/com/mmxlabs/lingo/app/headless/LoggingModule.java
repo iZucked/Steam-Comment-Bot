e700394a43f66dee8ade08b367fe9bbf705a7125
Simon Goodall <simon.goodall@minimaxlabs.com>
2017-12-22 11:31:34 +0000

    Move action plan code to LNG Scheduler

--- com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/LoggingModule.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.app.headless/src/com/mmxlabs/lingo/app/headless/LoggingModule.java
@@ -13,11 +13,12 @@ import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Provides;
 import com.google.inject.name.Named;
+import com.mmxlabs.models.lng.transformer.actionplan.ActionSetLogger;
 import com.mmxlabs.models.lng.transformer.ui.AbstractRunnerHook;
-import com.mmxlabs.models.lng.transformer.ui.ActionSetLogger;
 import com.mmxlabs.optimiser.common.logging.ILoggingDataStore;
 import com.mmxlabs.optimiser.lso.logging.ILoggingProvider;
 import com.mmxlabs.optimiser.lso.logging.LSOLogger;
+import com.mmxlabs.scheduler.optimiser.actionplan.BagOptimiser;
 
 /**
  * A {@link Module} providing the data from {@link ILoggingDataStore} to the {@link Injector} framework.
@@ -31,9 +32,7 @@ public class LoggingModule extends AbstractModule {
 	private final AbstractRunnerHook runnerHook;
 	private final int reportingInterval;
 	private final ActionSetLogger actionSetLogger;
-	
-	private final static String ACTION_PLAN_LOGGER = "ACTION_PLAN_LOGGER";
-	
+
 	public LoggingModule(final Map<String, LSOLogger> phaseToLoggerMap, final ActionSetLogger actionSetLogger, final AbstractRunnerHook runnerHook, final int reportingInterval) {
 		this.phaseToLoggerMap = phaseToLoggerMap;
 		this.runnerHook = runnerHook;
@@ -56,48 +55,48 @@ public class LoggingModule extends AbstractModule {
 	private Map<String, LSOLogger> providePhaseToLoggerMap() {
 		return phaseToLoggerMap;
 	}
-	
+
 	@Provides
 	@Named("RUNNER_HOOK")
 	private AbstractRunnerHook provideRunnerHook() {
 		return runnerHook;
 	}
-	
+
 	@Provides
 	@Named("REPORTING_INTERVAL")
 	private int provideReportingInterval() {
 		return reportingInterval;
 	}
-	
-//	@Provides
-//	private LSOLogger providerLSOLogger(@NonNull final Injector injector) {
-//		final String phase = runnerHook.getPhase();
-//		assert phase != null && !phase.isEmpty();
-//		if (phaseToLoggerMap.containsKey(phase)) {
-//			return phaseToLoggerMap.get(phase);
-//		}
-//
-//		final LSOLogger logger = new LSOLogger(reportingInterval);
-//		injector.injectMembers(logger);
-//
-//		phaseToLoggerMap.put(phase, logger);
-//		return logger;
-//	}
-	
+
+	// @Provides
+	// private LSOLogger providerLSOLogger(@NonNull final Injector injector) {
+	// final String phase = runnerHook.getPhase();
+	// assert phase != null && !phase.isEmpty();
+	// if (phaseToLoggerMap.containsKey(phase)) {
+	// return phaseToLoggerMap.get(phase);
+	// }
+	//
+	// final LSOLogger logger = new LSOLogger(reportingInterval);
+	// injector.injectMembers(logger);
+	//
+	// phaseToLoggerMap.put(phase, logger);
+	// return logger;
+	// }
+
 	@Provides
 	private LSOLogger providerLSOLogger(@NonNull final Injector injector) {
 		return null;
 	}
-	
+
 	@Provides
 	private ILoggingProvider provideLoggingProvider(@NonNull final Injector injector) {
 		final LoggingProvider logger = new LoggingProvider();
 		injector.injectMembers(logger);
 		return logger;
 	}
-	
+
 	@Provides
-	@Named(ACTION_PLAN_LOGGER)
+	@Named(BagOptimiser.ACTION_PLAN__LOGGER)
 	private ActionSetLogger providerActionSetLogger(@NonNull final Injector injector) {
 		return actionSetLogger;
 	}
