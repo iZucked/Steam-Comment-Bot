db8f53248e30df5062c1d5ff78b3ca908ad263d8
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-01-08 13:24:22 +0000

    Add plugin to pre-load db with existing port & distance data.

---
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.data.distances/src/com/mmxlabs/lngdataserver/data/distances/Activator.java
@@ -0,0 +1,19 @@
+package com.mmxlabs.lngdataserver.data.distances;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.mmxlabs.lngdataserver.server.BackEndUrlProvider;
+
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		BackEndUrlProvider.INSTANCE.addAvailableListener(() -> new DataImporter().importData());
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+
+	}
+}
---
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.data.distances/src/com/mmxlabs/lngdataserver/data/distances/PluginStartup.java
@@ -0,0 +1,13 @@
+package com.mmxlabs.lngdataserver.data.distances;
+
+import org.eclipse.ui.IStartup;
+
+public class PluginStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// Do nothing except trigger start up the plugin
+
+	}
+
+}
