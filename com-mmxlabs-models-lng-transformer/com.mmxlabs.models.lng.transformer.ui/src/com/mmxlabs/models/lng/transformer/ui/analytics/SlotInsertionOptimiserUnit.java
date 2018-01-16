88e4e9b878161c461b3528ecc8f2092035e1082d
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-01-16 11:35:14 +0000

    Update lookup after modifying sequences.

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/analytics/SlotInsertionOptimiserUnit.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/analytics/SlotInsertionOptimiserUnit.java
@@ -242,6 +242,7 @@ public class SlotInsertionOptimiserUnit {
 										++state.initialMetrics[MetricType.COMPULSARY_SLOT.ordinal()];
 									}
 								}
+								lookupManager.updateLookup(tmpRawSequences, lookup.getFirst(), null);
 
 							}
 						}
