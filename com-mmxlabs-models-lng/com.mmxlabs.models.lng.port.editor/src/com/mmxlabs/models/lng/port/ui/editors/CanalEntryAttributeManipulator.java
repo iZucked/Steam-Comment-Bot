4bd9987225cd3033a77148f66a5715b58b724514
alex <alex.churchill@minimaxlabs.com>
2018-01-24 14:25:00 +0000

    Panama name issue
    BugzID:2631

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/CanalEntryAttributeManipulator.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/CanalEntryAttributeManipulator.java
@@ -9,15 +9,23 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.swt.widgets.Composite;
 
 import com.mmxlabs.common.Pair;
+import com.mmxlabs.models.lng.cargo.CanalBookingSlot;
 import com.mmxlabs.models.lng.port.CanalEntry;
+import com.mmxlabs.models.lng.port.PortModel;
+import com.mmxlabs.models.lng.port.util.ModelDistanceProvider;
 import com.mmxlabs.models.lng.port.util.PortModelLabeller;
 import com.mmxlabs.models.ui.tabular.manipulators.ValueListAttributeManipulator;
 
 public class CanalEntryAttributeManipulator extends ValueListAttributeManipulator {
-	public CanalEntryAttributeManipulator(final EAttribute field, final EditingDomain editingDomain) {
+	PortModel portModel;
+	public CanalEntryAttributeManipulator(PortModel portModel, final EAttribute field, final EditingDomain editingDomain) {
 		super(field, editingDomain, getValues());
+		this.portModel = portModel;
 	}
 
 	private static List<Pair<String, Object>> getValues() {
@@ -27,4 +35,42 @@ public class CanalEntryAttributeManipulator extends ValueListAttributeManipulato
 		}
 		return values;
 	}
+	
+	@Override
+	public CellEditor createCellEditor(final Composite c, final Object object) {
+		// where we populate list
+		// need port model to populate
+		CanalBookingSlot canalBookingSlot = null;
+		if (object instanceof CanalBookingSlot) {
+			canalBookingSlot = (CanalBookingSlot) object;
+			String[] names = new String[CanalEntry.values().length];
+			int n = 0;
+			for (final CanalEntry literal : CanalEntry.values()) {
+				names[n] = ModelDistanceProvider.getCanalEntranceName(portModel, canalBookingSlot.getRouteOption(), literal);
+				n++;
+			}
+			return new ComboBoxCellEditor(c, names);
+		}
+		return null;
+	}
+	
+	@Override
+	public String render(final Object object) {
+		Object value = super.getValue(object);
+		CanalBookingSlot canalBookingSlot = null;
+		if (object instanceof CanalBookingSlot) {
+			canalBookingSlot = (CanalBookingSlot) object;
+			String[] names = new String[CanalEntry.values().length];
+			int n = 0;
+			for (final CanalEntry literal : CanalEntry.values()) {
+				names[n] = ModelDistanceProvider.getCanalEntranceName(portModel, canalBookingSlot.getRouteOption(), literal);
+				n++;
+			}
+			return names[(int) value];
+		} else {
+			return super.render(object);
+		}
+	}
+
+
 }
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/CanalEntryInlineEditorFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/editors/CanalEntryInlineEditorFactory.java
@@ -10,8 +10,13 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
+import com.mmxlabs.models.lng.cargo.CanalBookingSlot;
+import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.port.CanalEntry;
+import com.mmxlabs.models.lng.port.PortModel;
+import com.mmxlabs.models.lng.port.util.ModelDistanceProvider;
 import com.mmxlabs.models.lng.port.util.PortModelLabeller;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
 import com.mmxlabs.models.ui.editors.IInlineEditorFactory;
 import com.mmxlabs.models.ui.editors.impl.EENumInlineEditor;
@@ -27,7 +32,37 @@ public class CanalEntryInlineEditorFactory implements IInlineEditorFactory {
 			objectsList.add(name);
 			objectsList.add(canalEntry);
 		}
-		return new EENumInlineEditor((EAttribute) feature, objectsList.toArray());
+		return new EENumInlineEditor((EAttribute) feature, objectsList.toArray()) {
+			@Override
+			protected boolean updateOnChangeToFeature(Object changedFeature) {
+				// update names
+				if (changedFeature == CargoPackage.Literals.CANAL_BOOKING_SLOT__ROUTE_OPTION) {
+					updateNames();
+					return true;
+				}
+				return super.updateOnChangeToFeature(changedFeature);
+			}
+			
+			@Override
+			protected void updateControl() {
+				super.updateControl();
+				updateNames();
+			}
+
+			private void updateNames() {
+				final LNGScenarioModel scenarioModel = (LNGScenarioModel) rootObject;
+				PortModel portModel = scenarioModel.getReferenceModel().getPortModel();
+
+				if (input instanceof CanalBookingSlot) {
+					CanalBookingSlot canalBookingSlot = (CanalBookingSlot) input;
+					names.clear();
+					for (final CanalEntry literal : CanalEntry.values()) {
+						names.add(ModelDistanceProvider.getCanalEntranceName(portModel, canalBookingSlot.getRouteOption(), literal));
+					}
+				}
+				combo.setItems(names.toArray(new String[names.size()]));
+			}
+		};
 	}
 
 }
