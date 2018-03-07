70128072e1326995006e655d290486fdc81e63d3
Jenkins <hudson@minimaxlabs.com>
2018-03-07 22:00:39 +0000

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/changeset/extensions/ChangeSetColumnValueExtenderExtensionPoint.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/changeset/extensions/ChangeSetColumnValueExtenderExtensionPoint.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2018
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.views.changeset.extensions;
 
 import org.ops4j.peaberry.eclipse.ExtensionBean;
