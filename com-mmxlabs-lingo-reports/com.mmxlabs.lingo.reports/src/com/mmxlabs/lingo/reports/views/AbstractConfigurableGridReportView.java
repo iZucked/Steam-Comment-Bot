11327d1146d6d0294a64bbda7cd7657c420b79ab
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-04-17 10:10:36 +0100

    Speed up selection changes by only refreshing when diff filters are active.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/AbstractConfigurableGridReportView.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/AbstractConfigurableGridReportView.java
@@ -167,21 +167,20 @@ public abstract class AbstractConfigurableGridReportView extends ViewPart implem
 				public int compare(final Viewer viewer, Object e1, Object e2) {
 					RowGroup g1 = null;
 					RowGroup g2 = null;
-					
+
 					Boolean firstIsComposite = false;
 					Boolean secondIsComposite = false;
-					
+
 					if (e1 instanceof Row) {
 						g1 = ((Row) e1).getRowGroup();
 					}
 					if (e2 instanceof Row) {
 						g2 = ((Row) e2).getRowGroup();
 					}
-					
 
 					if (e1 instanceof CompositeRow) {
 						CompositeRow tmp = ((CompositeRow) e1);
-						
+
 						if (tmp.getPreviousRow() != null) {
 							g1 = tmp.getPreviousRow().getRowGroup();
 							e1 = tmp.getPreviousRow();
@@ -195,7 +194,7 @@ public abstract class AbstractConfigurableGridReportView extends ViewPart implem
 
 					if (e2 instanceof CompositeRow) {
 						CompositeRow tmp = ((CompositeRow) e2);
-						
+
 						if (tmp.getPreviousRow() != null) {
 							g2 = tmp.getPreviousRow().getRowGroup();
 							e2 = tmp.getPreviousRow();
@@ -209,11 +208,11 @@ public abstract class AbstractConfigurableGridReportView extends ViewPart implem
 					if (e1 instanceof List) {
 						return Integer.MAX_VALUE;
 					}
-					
+
 					if (e2 instanceof List) {
 						return Integer.MIN_VALUE;
 					}
-					
+
 					if (g1 == g2) {
 						int res = vc.compare(viewer, e1, e2);
 						return res;
@@ -760,9 +759,18 @@ public abstract class AbstractConfigurableGridReportView extends ViewPart implem
 				return;
 			}
 		}
+
 		final ISelection selection = SelectionHelper.adaptSelection(selectionObject);
+
 		viewer.setSelection(selection, true);
-		ViewerHelper.refresh(viewer, true);
+
+		if (refreshOnSelectionChange()) {
+			ViewerHelper.refresh(viewer, true);
+		}
+	}
+
+	protected boolean refreshOnSelectionChange() {
+		return false;
 	}
 
 	protected boolean handleSelections() {
