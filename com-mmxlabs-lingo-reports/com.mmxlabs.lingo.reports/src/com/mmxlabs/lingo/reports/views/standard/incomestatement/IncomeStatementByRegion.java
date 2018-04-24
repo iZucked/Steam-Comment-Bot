47827a9a61e5935ce9f82b4f465d3bf4618ebb99
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-04-24 12:22:24 +0100

    Add Income Statement reports

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/standard/incomestatement/IncomeStatementByRegion.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
+package com.mmxlabs.lingo.reports.views.standard.incomestatement;
+
+import java.util.Collection;
+
+import com.google.common.collect.Lists;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.port.Port;
+
+public class IncomeStatementByRegion extends AbstractIncomeStatement<Regions> {
+
+	public IncomeStatementByRegion() {
+		super("com.mmxlabs.lingo.reports.Reports_IncomeStatementByRegion");
+	}
+
+	@Override
+	protected Regions getSubType(final DischargeSlot dischargeSlot) {
+
+		final Port port = dischargeSlot.getPort();
+		if (port != null) {
+			final String country = port.getLocation().getCountry().toLowerCase();
+			switch (country) {
+			case "china":
+			case "china, peoples rep":
+			case "taiwan":
+			case "south korea":
+			case "japan":
+			case "australia":
+			case "united arab emirates":
+			case "u.a.e.":
+			case "kuwait":
+			case "pakistan":
+			case "india":
+				return Regions.FAR_EAST_AND_MIDDLE_EAST;
+			case "spain":
+			case "portugal":
+			case "norway":
+			case "united kingdom":
+			case "turkey":
+			case "greece":
+			case "cyprus":
+				return Regions.EUROPE;
+			case "canada":
+			case "chile":
+			case "u.k.":
+			case "u.s.a.":
+			case "argentina":
+			case "brazil":
+				return Regions.AMERICAS;
+			default:
+				return Regions.OTHER;
+			}
+		}
+		return Regions.OTHER;
+
+	}
+
+	@Override
+	protected Collection<Regions> getSubTypes() {
+		return Lists.newArrayList(Regions.values());
+	}
+
+	@Override
+	protected int doCompare(final IncomeStatementData o1, final IncomeStatementData o2) {
+		return ((Enum) o1.key).ordinal() - ((Enum) o2.key).ordinal();
+	}
+
+	@Override
+	protected void resetData() {
+
+	}
+
+	@Override
+	protected String doGetColumnText(IncomeStatementData data) {
+		return data.key.toString();
+	}
+}
---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/standard/incomestatement/IncomeStatementData.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2017
+ * All rights reserved.
+ */
+package com.mmxlabs.lingo.reports.views.standard.incomestatement;
+
+import java.time.YearMonth;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+import com.mmxlabs.models.lng.schedule.Schedule;
+import com.mmxlabs.scenario.service.ui.ScenarioResult;
+
+public class IncomeStatementData {
+	public final ScenarioResult scenarioResult;
+	public final Object key;
+	public final Map<YearMonth, Double> valuesByMonth;
+	public IncomeStatementData parent;
+	public final List<IncomeStatementData> children = new LinkedList<>();
+	public Schedule schedule;
+
+	public IncomeStatementData(@NonNull ScenarioResult scenarioResult, @NonNull Schedule schedule, final Object key, final Map<YearMonth, Double> exposuresByMonth) {
+		super();
+		this.scenarioResult = scenarioResult;
+		this.schedule = schedule;
+		this.key = key;
+		this.valuesByMonth = exposuresByMonth;
+	}
+}
