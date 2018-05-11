f895d10d46fe5148b2e74159785dc4299c3b7424
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-05-11 10:01:32 +0100

    Add some synchronization to serialise setURL requests to avoid crashing the JVM with rapid changes in URL.
    TODO: Ideally we should use java script navigation for a smoother user experience.

--- com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.ui/src/com/mmxlabs/lngdataserver/integration/ui/WebNavigatorView.java
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.ui/src/com/mmxlabs/lngdataserver/integration/ui/WebNavigatorView.java
@@ -14,6 +14,8 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISelectionListener;
@@ -29,76 +31,97 @@ public class WebNavigatorView extends ViewPart {
 	public static final String ID = "com.mmxlabs.lngdataserver.integration.ui.WebNavigatorView";
 
 	private Browser browser;
-
 	private ISelectionListener selectionListener;
 
+	private final Object lockObject = new Object(); // lock object for changing the browser base URL
+	private boolean ready = true; // Can we load the URL immediately or do we need to delay it?
+	private String nextUrl = null; // The next URL to load in.
+	private final ProgressListener listener = new ProgressListener() {
+
+		@Override
+		public void completed(final ProgressEvent event) {
+			// URL has finished loading. Check to see if another URL has been requested.
+			ready = true;
+			synchronized (lockObject) {
+				if (nextUrl != null) {
+					// Clear the nextURL value...
+					final String u = nextUrl;
+					nextUrl = null;
+					// .. and trigger an update for the URL
+					updateURL(u);
+				}
+			}
+		}
+
+		@Override
+		public void changed(final ProgressEvent event) {
+
+		}
+	};
+
 	@Override
-	public void createPartControl(Composite parent) {
+	public void createPartControl(final Composite parent) {
 		parent.setLayout(new FillLayout());
 		browser = new Browser(parent, SWT.NONE);
 		System.out.println("Browser: " + browser.getBrowserType());
 
 		browser.setBounds(0, 0, 600, 800);
 
-		// String encodedBackend;
-		// try {
-		// encodedBackend = URLEncoder.encode(BackEndUrlProvider.INSTANCE.getUrl(), "UTF-8");
-		// } catch (UnsupportedEncodingException e) {
-		// throw new RuntimeException(e);
-		// }
-		// System.out.println("Opening: " + ServerUrlProvider.INSTANCE.getBaseUrl() + Activator.URL_PREFIX + "?apiBaseUrl=" + encodedBackend);
+		browser.addProgressListener(listener);
+
 		selectionListener = new ISelectionListener() {
 
 			@Override
-			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+			public void selectionChanged(final IWorkbenchPart part, final ISelection selection) {
 
 				if (!(selection instanceof TreeSelection)) {
 					return;
 				}
 
-				TreeSelection treeSelection = (TreeSelection) selection;
+				final TreeSelection treeSelection = (TreeSelection) selection;
 
 				if (treeSelection.getFirstElement() == null) {
 					return;
 				}
 
-				Node node = (Node) treeSelection.getFirstElement();
+				final Node node = (Node) treeSelection.getFirstElement();
 
 				// check if distances node
 				if (node.getParent() != null) {
 					if (node.getParent().getDisplayName().equals("Distances")) {
 						System.out.println("update received: " + node.getDisplayName());
-						browser.setUrl(getUrl("distances", node.getVersionIdentifier()));
+						updateURL(getUrl("distances", node.getVersionIdentifier()));
 					}
 					if (node.getParent().getDisplayName().equals("Pricing")) {
 						System.out.println("update received: " + node.getDisplayName());
-						browser.setUrl(getUrl("pricing", node.getVersionIdentifier()));
+						updateURL(getUrl("pricing", node.getVersionIdentifier()));
 					}
 					if (node.getParent().getDisplayName().equals("Vessels")) {
 						System.out.println("update received: " + node.getDisplayName());
-						browser.setUrl(getUrl("vessels", node.getVersionIdentifier()));
+						updateURL(getUrl("vessels", node.getVersionIdentifier()));
 					}
 					if (node.getParent().getDisplayName().equals("Ports")) {
 						System.out.println("update received: " + node.getDisplayName());
-						browser.setUrl(getUrl("ports", node.getVersionIdentifier()));
+						updateURL(getUrl("ports", node.getVersionIdentifier()));
 					}
 				}
 			}
+
 		};
 
 		try {
 			// Icon from https://commons.wikimedia.org/wiki/Category:Throbbers#/media/File:Ajax-loader.gif
 			// Trigger extraction of throbber.gif to file system so relative urls work in builds
-			URL _gif_resource = FileLocator.toFileURL(getClass().getResource("/throbber.gif"));
-			URL resource = FileLocator.toFileURL(getClass().getResource("/wait.html"));
+			final URL _gif_resource = FileLocator.toFileURL(getClass().getResource("/throbber.gif"));
+			final URL resource = FileLocator.toFileURL(getClass().getResource("/wait.html"));
 			browser.setUrl(resource.toExternalForm());
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 
 		BackEndUrlProvider.INSTANCE.addAvailableListener(() -> RunnerHelper.asyncExec(() -> {
 			if (!browser.isDisposed()) {
-				browser.setUrl(getUrl("pricing", "latest"));
+				updateURL(getUrl("pricing", "latest"));
 				getViewSite().getWorkbenchWindow().getSelectionService().addSelectionListener("com.mmxlabs.lngdataserver.browser.ui.DataBrowser", selectionListener);
 			}
 		}));
@@ -111,11 +134,11 @@ public class WebNavigatorView extends ViewPart {
 		}
 	}
 
-	private String getUrl(String type, String version) {
+	private String getUrl(final String type, final String version) {
 		String encodedBackend;
 		try {
 			encodedBackend = URLEncoder.encode(BackEndUrlProvider.INSTANCE.getUrl(), "UTF-8");
-		} catch (UnsupportedEncodingException e) {
+		} catch (final UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
 		String url = BackEndUrlProvider.INSTANCE.getUrl() + Activator.URL_PREFIX + "#/" + type + "?apiBaseUrl=" + encodedBackend;
@@ -129,5 +152,22 @@ public class WebNavigatorView extends ViewPart {
 	@Override
 	public void setFocus() {
 		browser.setFocus();
+	}
+
+	/**
+	 * Set the new URL for the browser OR store the URL for delayed loading.
+	 * 
+	 * @param url
+	 */
+	private void updateURL(final String url) {
+
+		synchronized (lockObject) {
+			if (ready) {
+				ready = false;
+				browser.setUrl(url);
+			} else {
+				nextUrl = url;
+			}
+		}
 	}
 }
