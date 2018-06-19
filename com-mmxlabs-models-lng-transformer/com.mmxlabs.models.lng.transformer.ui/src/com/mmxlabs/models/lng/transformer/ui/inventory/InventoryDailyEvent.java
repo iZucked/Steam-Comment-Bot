ce52aabffaa80b582258770c24031593925728f0
Alex Churchill <alex.churchill@minimaxlabs.com>
2018-06-19 11:53:50 +0000

    Feature/load trigger

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/inventory/InventoryDailyEvent.java
@@ -0,0 +1,24 @@
+package com.mmxlabs.models.lng.transformer.ui.inventory;
+
+import java.time.LocalDate;
+
+class InventoryDailyEvent {
+	LocalDate date;
+	int netVolumeIn = 0;
+	int minVolume = 0;
+	int maxVolume = 0;
+	
+	public InventoryDailyEvent() {
+	}
+	
+	public InventoryDailyEvent(LocalDate date, int netVolumeIn, int minVolume, int maxVolume) {
+		this.date = date;
+		this.netVolumeIn = netVolumeIn;
+		this.minVolume = minVolume;
+		this.maxVolume = maxVolume;
+	}
+	
+	public void addVolume(int volume) {
+		netVolumeIn += volume;
+	}
+}
