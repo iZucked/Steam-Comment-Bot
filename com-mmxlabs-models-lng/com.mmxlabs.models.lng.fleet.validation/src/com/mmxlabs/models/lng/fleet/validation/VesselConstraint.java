852baf4ec40ce1166c1d422402f8eb189351866c
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-10-04 10:02:28 +0100

    Add validation for missing route parameter obejcts.
    BugzId: 2721

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/VesselConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.fleet.validation/src/com/mmxlabs/models/lng/fleet/validation/VesselConstraint.java
@@ -7,12 +7,15 @@ package com.mmxlabs.models.lng.fleet.validation;
 import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
 
 import com.mmxlabs.models.lng.fleet.FleetPackage;
 import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.fleet.VesselClassRouteParameters;
 import com.mmxlabs.models.lng.fleet.validation.internal.Activator;
+import com.mmxlabs.models.lng.port.RouteOption;
 import com.mmxlabs.models.lng.scenario.model.util.ScenarioElementNameHelper;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusFactory;
@@ -31,10 +34,10 @@ public class VesselConstraint extends AbstractModelMultiConstraint {
 		if (target instanceof Vessel) {
 			final Vessel vessel = (Vessel) target;
 
-			DetailConstraintStatusFactory baseFactory = DetailConstraintStatusFactory.makeStatus() //
+			final DetailConstraintStatusFactory baseFactory = DetailConstraintStatusFactory.makeStatus() //
 					.withTypedName(ScenarioElementNameHelper.getTypeName(vessel), ScenarioElementNameHelper.getNonNullString(vessel.getName()));
 
-			double effectiveCapacity = vessel.getVesselOrDelegateCapacity() * vessel.getVesselOrDelegateFillCapacity();
+			final double effectiveCapacity = vessel.getVesselOrDelegateCapacity() * vessel.getVesselOrDelegateFillCapacity();
 			if (vessel.getVesselOrDelegateSafetyHeel() > effectiveCapacity) {
 				final String message = String.format("Minimum heel should be less than fill capacity");
 
@@ -45,58 +48,71 @@ public class VesselConstraint extends AbstractModelMultiConstraint {
 			}
 
 			if (vessel.getVesselOrDelegateBaseFuel() == null) {
-				String message = "Vessel has no travel base fuel";
+				final String message = "Vessel has no travel base fuel";
 				baseFactory.copyName() //
 						.withMessage(message) //
 						.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_BaseFuel()) //
 						.make(ctx, statuses);
 			}
 			if (vessel.getVesselOrDelegateIdleBaseFuel() == null) {
-				String message = "Vessel has no idle base fuel";
+				final String message = "Vessel has no idle base fuel";
 				baseFactory.copyName() //
-				.withMessage(message) //
-				.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_IdleBaseFuel()) //
-				.make(ctx, statuses);
+						.withMessage(message) //
+						.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_IdleBaseFuel()) //
+						.make(ctx, statuses);
 			}
 			if (vessel.getVesselOrDelegatePilotLightBaseFuel() == null) {
-				String message = "Vessel has no pilot light base fuel";
+				final String message = "Vessel has no pilot light base fuel";
 				baseFactory.copyName() //
-				.withMessage(message) //
-				.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_PilotLightBaseFuel()) //
-				.make(ctx, statuses);
+						.withMessage(message) //
+						.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_PilotLightBaseFuel()) //
+						.make(ctx, statuses);
 			}
 			if (vessel.getVesselOrDelegateInPortBaseFuel() == null) {
-				String message = "Vessel has no in port base fuel";
+				final String message = "Vessel has no in port base fuel";
 				baseFactory.copyName() //
-				.withMessage(message) //
-				.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_InPortBaseFuel()) //
-				.make(ctx, statuses);
+						.withMessage(message) //
+						.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_InPortBaseFuel()) //
+						.make(ctx, statuses);
 			}
 			if (vessel.getReference() != null) {
 				if (vessel.getReference().getReference() != null) {
-					String message = "Reference vessel can not have a reference vessel";
+					final String message = "Reference vessel can not have a reference vessel";
 					baseFactory.copyName() //
 							.withMessage(message) //
 							.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_Reference()) //
 							.make(ctx, statuses);
 				}
 			}
-			// if (vessel.isPrototype()) {
-			// String message = "Prototype vessels cannot inherit attributes";
-			// DetailConstraintStatusFactory factory = baseFactory.copyName() //
-			// .withMessage(message);
-			//
-			// boolean foundFault = false;
-			//
-			// if (vessel.getBasedOn() != null) {
-			// foundFault = true;
-			// factory.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_Reference());
-			// }
-			//
-			// if (foundFault) {
-			// factory.make(ctx, statuses);
-			// }
-			// }
+			final EList<VesselClassRouteParameters> routeParameters = vessel.getVesselOrDelegateRouteParameters();
+			if (routeParameters == null || routeParameters.isEmpty()) {
+				final String message = "Vessel has no canal parameters";
+				baseFactory.copyName() //
+						.withMessage(message) //
+						.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_RouteParameters()) //
+						.make(ctx, statuses);
+			} else {
+				boolean foundSuez = false;
+				boolean foundPanama = false;
+				for (final VesselClassRouteParameters rp : routeParameters) {
+					foundSuez |= rp.getRouteOption() == RouteOption.SUEZ;
+					foundPanama |= rp.getRouteOption() == RouteOption.PANAMA;
+				}
+				if (!foundSuez) {
+					final String message = "Vessel has no Suez canal parameters";
+					baseFactory.copyName() //
+							.withMessage(message) //
+							.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_RouteParameters()) //
+							.make(ctx, statuses);
+				}
+				if (!foundPanama) {
+					final String message = "Vessel has no Panama canal parameters";
+					baseFactory.copyName() //
+							.withMessage(message) //
+							.withObjectAndFeature(vessel, FleetPackage.eINSTANCE.getVessel_RouteParameters()) //
+							.make(ctx, statuses);
+				}
+			}
 		}
 
 		return Activator.PLUGIN_ID;
