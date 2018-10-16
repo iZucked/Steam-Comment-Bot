e463769d3f2adbbd299235993b2d25691e7209e5
Jenkins <hudson@minimaxlabs.com>
2018-10-16 13:28:47 +0100

    Update headers

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/optimiser/lightweightscheduler/fitnessfunctions/CopyDefaultPNLLightWeightFitnessFunction.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/optimiser/lightweightscheduler/fitnessfunctions/CopyDefaultPNLLightWeightFitnessFunction.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2018
+ * All rights reserved.
+ */
 ///**
 // * Copyright (C) Minimax Labs Ltd., 2010 - 2018
 // * All rights reserved.
--- com-mmxlabs-optimiser/com.mmxlabs.optimiser.optimiser-lso/src/com/mmxlabs/optimiser/lso/multiobjective/modules/MultiObjectiveOptimiserModule.java
+++ com-mmxlabs-optimiser/com.mmxlabs.optimiser.optimiser-lso/src/com/mmxlabs/optimiser/lso/multiobjective/modules/MultiObjectiveOptimiserModule.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2018
+ * All rights reserved.
+ */
 package com.mmxlabs.optimiser.lso.multiobjective.modules;
 
 import java.util.List;
