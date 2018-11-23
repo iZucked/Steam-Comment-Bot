1f5009ea9823d1a1e3de1c71448b9d0c764c4d74
Simon Goodall <simon.goodall@minimaxlabs.com>
2018-11-23 13:02:29 +0000

    Add toggle button for charter out generation.
    Resolves #31

---
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/navigator/handlers/editor/ToggleCharterOutEditorActionDelegate.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2018
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.transformer.ui.navigator.handlers.editor;
+
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.actions.ActionDelegate;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.mmxlabs.models.lng.parameters.ParametersPackage;
+import com.mmxlabs.models.lng.parameters.UserSettings;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioPackage;
+import com.mmxlabs.models.lng.transformer.extensions.ScenarioUtils;
+import com.mmxlabs.scenario.service.model.ScenarioInstance;
+import com.mmxlabs.scenario.service.model.manager.IScenarioDataProvider;
+import com.mmxlabs.scenario.service.model.manager.ModelReference;
+import com.mmxlabs.scenario.service.model.manager.SSDataManager;
+import com.mmxlabs.scenario.service.model.manager.ScenarioModelRecord;
+import com.mmxlabs.scenario.service.ui.editing.IScenarioServiceEditorInput;
+
+public class ToggleCharterOutEditorActionDelegate extends ActionDelegate implements IEditorActionDelegate {
+	protected IEditorPart targetEditor;
+	protected IAction action;
+	protected LNGScenarioModel currentModel;
+	protected UserSettings currentSettings;
+	protected EditingDomain editingDomain;
+
+	protected AdapterImpl notificationAdapter = new AdapterImpl() {
+		@Override
+		public void notifyChanged(final org.eclipse.emf.common.notify.Notification msg) {
+			super.notifyChanged(msg);
+			if (msg.isTouch()) {
+				return;
+			}
+			if (msg.getFeature() == LNGScenarioPackage.eINSTANCE.getLNGScenarioModel_UserSettings()) {
+				updateState();
+			}
+			if (msg.getFeature() == ParametersPackage.eINSTANCE.getUserSettings_GenerateCharterOuts()) {
+				updateState();
+			}
+		};
+	};
+
+	@Override
+	public synchronized void setActiveEditor(final IAction action, final IEditorPart targetEditor) {
+
+		if (this.targetEditor != null) {
+			// Remove notifications
+		}
+		if (currentModel != null) {
+			currentModel.eAdapters().remove(notificationAdapter);
+		}
+		currentModel = null;
+		if (currentSettings != null) {
+			currentSettings.eAdapters().remove(notificationAdapter);
+		}
+		currentSettings = null;
+
+		this.editingDomain = null;
+		this.currentModel = null;
+		this.targetEditor = targetEditor;
+		this.action = action;
+
+		if (this.targetEditor != null) {
+			final IEditorInput editorInput = targetEditor.getEditorInput();
+			if (editorInput instanceof IScenarioServiceEditorInput) {
+				final IScenarioServiceEditorInput iScenarioServiceEditorInput = (IScenarioServiceEditorInput) editorInput;
+				final ScenarioInstance instance = iScenarioServiceEditorInput.getScenarioInstance();
+				@NonNull
+				final ScenarioModelRecord modelRecord = SSDataManager.Instance.getModelRecord(instance);
+
+				try (final IScenarioDataProvider sdp = modelRecord.aquireScenarioDataProvider("ToggleCharterOutEditorActionDelegate")) {
+					this.currentModel = sdp.getTypedScenario(LNGScenarioModel.class);
+					this.currentSettings = currentModel.getUserSettings();
+					this.editingDomain = sdp.getEditingDomain();
+				}
+			}
+		}
+		updateState();
+	}
+
+	private void updateState() {
+		if (currentSettings != null) {
+			currentSettings.eAdapters().remove(notificationAdapter);
+		}
+		if (currentModel != null) {
+			currentSettings = currentModel.getUserSettings();
+		}
+		if (currentSettings != null) {
+			currentSettings.eAdapters().add(notificationAdapter);
+			action.setEnabled(true);
+			action.setChecked(currentSettings.isGenerateCharterOuts());
+		} else {
+			action.setEnabled(false);
+			action.setChecked(false);
+		}
+	}
+
+	@Override
+	public void dispose() {
+		action = null;
+		targetEditor = null;
+		currentModel = null;
+	}
+
+	@Override
+	public synchronized void run(final IAction action) {
+		if (editingDomain == null || currentModel == null) {
+			return;
+		}
+		final CompoundCommand cmd = new CompoundCommand("Toggle charter out generation");
+
+		UserSettings settings = currentModel.getUserSettings();
+		if (settings == null) {
+			settings = ScenarioUtils.createDefaultUserSettings();
+			cmd.append(SetCommand.create(editingDomain, currentModel, LNGScenarioPackage.eINSTANCE.getLNGScenarioModel_UserSettings(), settings));
+		}
+		cmd.append(SetCommand.create(editingDomain, settings, ParametersPackage.eINSTANCE.getUserSettings_GenerateCharterOuts(), !settings.isGenerateCharterOuts()));
+
+		editingDomain.getCommandStack().execute(cmd);
+	}
+
+}
