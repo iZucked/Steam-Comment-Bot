28e8f96cd218d7cdd6a982829dbd23c9df6f0886
Simon Goodall <simon.goodall@minimaxlabs.com>
2019-01-25 09:44:49 +0000

    [DataHub] Move extension into exported package

--- com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.integration.ports/src/com/mmxlabs/lngdataserver/integration/ports/PortsDataExtension.java
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.integration.ports/src/com/mmxlabs/lngdataserver/integration/ports/PortsDataExtension.java
@@ -2,13 +2,14 @@
  * Copyright (C) Minimax Labs Ltd., 2010 - 2018
  * All rights reserved.
  */
-package com.mmxlabs.lngdataserver.integration.ports.internal;
+package com.mmxlabs.lngdataserver.integration.ports;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mmxlabs.lngdataserver.browser.CompositeNode;
 import com.mmxlabs.lngdataserver.browser.ui.DataExtension;
+import com.mmxlabs.lngdataserver.integration.ports.internal.Activator;
 
 public class PortsDataExtension implements DataExtension {
 
