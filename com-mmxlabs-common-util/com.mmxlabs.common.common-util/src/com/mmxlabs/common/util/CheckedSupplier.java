aaf22a9cef616817f08d5be54abcc7cca645ea03
Simon Goodall <simon.goodall@minimaxlabs.com>
2019-01-25 09:45:28 +0000

    Create a CheckedSupplier functional interface

---
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/util/CheckedSupplier.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2018
+ * All rights reserved.
+ */
+package com.mmxlabs.common.util;
+
+@FunctionalInterface
+public interface CheckedSupplier<T, E extends Exception> {
+
+	/**
+	 *
+	 * @return the function result
+	 */
+	T get() throws E;
+}
