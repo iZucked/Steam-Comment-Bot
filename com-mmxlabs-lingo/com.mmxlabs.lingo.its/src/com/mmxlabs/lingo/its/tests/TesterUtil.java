d4af00961054f73b2e95ed226f71709c91bf1a92
Simon Goodall <simon.goodall@minimaxlabs.com>
2019-03-29 12:33:35 +0000

    Fix sorting of test case properties

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/TesterUtil.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/TesterUtil.java
@@ -4,13 +4,16 @@
  */
 package com.mmxlabs.lingo.its.tests;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -51,6 +54,10 @@ import com.mmxlabs.scenario.service.model.util.encryption.IScenarioCipherProvide
 
 public class TesterUtil {
 
+	private TesterUtil() {
+
+	}
+
 	@NonNull
 	public static List<Fitness> getFitnessFromExtraAnnotations(@NonNull final Map<String, Object> extraAnnotations) {
 		final List<Fitness> outputFitnesses = new LinkedList<>();
@@ -81,12 +88,158 @@ public class TesterUtil {
 		if (create) {
 			/**
 			 * Extend to save properties in a sorted order for ease of reading
+			 * Copy and paste so much due to private methods. :(
 			 */
 			return new Properties() {
+
+				private void writeComments(BufferedWriter bw, String comments) throws IOException {
+					bw.write("#");
+					int len = comments.length();
+					int current = 0;
+					int last = 0;
+					char[] uu = new char[6];
+					uu[0] = '\\';
+					uu[1] = 'u';
+					while (current < len) {
+						char c = comments.charAt(current);
+						if (c > '\u00ff' || c == '\n' || c == '\r') {
+							if (last != current)
+								bw.write(comments.substring(last, current));
+							if (c > '\u00ff') {
+								uu[2] = toHex((c >> 12) & 0xf);
+								uu[3] = toHex((c >> 8) & 0xf);
+								uu[4] = toHex((c >> 4) & 0xf);
+								uu[5] = toHex(c & 0xf);
+								bw.write(new String(uu));
+							} else {
+								bw.newLine();
+								if (c == '\r' && current != len - 1 && comments.charAt(current + 1) == '\n') {
+									current++;
+								}
+								if (current == len - 1 || (comments.charAt(current + 1) != '#' && comments.charAt(current + 1) != '!'))
+									bw.write("#");
+							}
+							last = current + 1;
+						}
+						current++;
+					}
+					if (last != current)
+						bw.write(comments.substring(last, current));
+					bw.newLine();
+				}
+
+				private void store0(BufferedWriter bw, String comments, boolean escUnicode) throws IOException {
+					if (comments != null) {
+						writeComments(bw, comments);
+					}
+					bw.write("#" + new Date().toString());
+					bw.newLine();
+					synchronized (this) {
+						for (Object e : new TreeSet<Object>(super.keySet())) {
+							// for (Map.Entry<Object, Object> e : entrySet()) {
+							String key = (String) e;
+							String val = getProperty(key);
+							key = saveConvert(key, true, escUnicode);
+							/*
+							 * No need to escape embedded and trailing spaces for value, hence pass false to flag.
+							 */
+							val = saveConvert(val, false, escUnicode);
+							bw.write(key + "=" + val);
+							bw.newLine();
+						}
+					}
+					bw.flush();
+				}
+
+				/*
+				 * Converts unicodes to encoded &#92;uxxxx and escapes special characters with a preceding slash
+				 */
+				private String saveConvert(String theString, boolean escapeSpace, boolean escapeUnicode) {
+					int len = theString.length();
+					int bufLen = len * 2;
+					if (bufLen < 0) {
+						bufLen = Integer.MAX_VALUE;
+					}
+					StringBuilder outBuffer = new StringBuilder(bufLen);
+
+					for (int x = 0; x < len; x++) {
+						char aChar = theString.charAt(x);
+						// Handle common case first, selecting largest block that
+						// avoids the specials below
+						if ((aChar > 61) && (aChar < 127)) {
+							if (aChar == '\\') {
+								outBuffer.append('\\');
+								outBuffer.append('\\');
+								continue;
+							}
+							outBuffer.append(aChar);
+							continue;
+						}
+						switch (aChar) {
+						case ' ':
+							if (x == 0 || escapeSpace)
+								outBuffer.append('\\');
+							outBuffer.append(' ');
+							break;
+						case '\t':
+							outBuffer.append('\\');
+							outBuffer.append('t');
+							break;
+						case '\n':
+							outBuffer.append('\\');
+							outBuffer.append('n');
+							break;
+						case '\r':
+							outBuffer.append('\\');
+							outBuffer.append('r');
+							break;
+						case '\f':
+							outBuffer.append('\\');
+							outBuffer.append('f');
+							break;
+						case '=': // Fall through
+						case ':': // Fall through
+						case '#': // Fall through
+						case '!':
+							outBuffer.append('\\');
+							outBuffer.append(aChar);
+							break;
+						default:
+							if (((aChar < 0x0020) || (aChar > 0x007e)) & escapeUnicode) {
+								outBuffer.append('\\');
+								outBuffer.append('u');
+								outBuffer.append(toHex((aChar >> 12) & 0xF));
+								outBuffer.append(toHex((aChar >> 8) & 0xF));
+								outBuffer.append(toHex((aChar >> 4) & 0xF));
+								outBuffer.append(toHex(aChar & 0xF));
+							} else {
+								outBuffer.append(aChar);
+							}
+						}
+					}
+					return outBuffer.toString();
+				}
+
+				/**
+				 * Convert a nibble to a hex character
+				 * 
+				 * @param nibble
+				 *            the nibble to convert.
+				 */
+				private char toHex(int nibble) {
+					return hexDigit[(nibble & 0xF)];
+				}
+
+				/** A table of hex digits */
+				private final char[] hexDigit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 				/**
 				 * 
 				 */
 				private static final long serialVersionUID = 1L;
+
+				public void store(OutputStream out, String comments) throws IOException {
+					store0(new BufferedWriter(new OutputStreamWriter(out, "8859_1")), comments, true);
+				}
 
 				@Override
 				public Set<Object> keySet() {
