7f52bd1424ae65d3d0b3e16ec377f06718fb0fbf
smcgregor@minimaxlabs.com <simonmcgregor@minimaxs-mbp>
2019-05-17 15:00:08 +0100

    Added a getOpenSlotsIterable(CargoModel cargoModel) method to CargoUtils

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/editing/CargoEditingTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/editing/CargoEditingTests.java
@@ -6,6 +6,10 @@ package com.mmxlabs.lingo.its.editing;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.junit.jupiter.api.Assertions;
@@ -14,12 +18,17 @@ import org.junit.jupiter.api.Test;
 
 import com.mmxlabs.lingo.its.tests.category.TestCategories;
 import com.mmxlabs.lingo.its.tests.microcases.AbstractMicroTestCase;
+import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.DischargeSlot;
 import com.mmxlabs.models.lng.cargo.DryDockEvent;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
+import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.VesselAvailability;
 import com.mmxlabs.models.lng.cargo.ui.editorpart.actions.CargoEditingHelper;
+import com.mmxlabs.models.lng.cargo.util.CargoUtils;
 import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.spotmarkets.CharterInMarket;
 import com.mmxlabs.models.lng.transformer.util.LNGSchedulerJobUtils;
 
 public class CargoEditingTests extends AbstractMicroTestCase {
@@ -128,5 +137,94 @@ public class CargoEditingTests extends AbstractMicroTestCase {
 		helper.assignNominatedVessel("Unassign slot", discharge, null);
 
 		Assertions.assertNull(discharge.getNominatedVessel());
+	}
+	
+	/**
+	 * Tests CargoUtils.getOpenSlotsIterable() to check that it iterates correctly over
+	 * a list of all open slots, and no other slots, in the cargo model. 
+	 */
+	private void testGetOpenSlotsIterable(CargoModel cargoModel) {
+				
+		Set<Slot<?>> allSlots = new HashSet<Slot<?>>();
+		Set<Slot<?>> openSlots = new HashSet<Slot<?>>();
+		Set<Slot<?>> cargoSlots = new HashSet<Slot<?>>();
+		
+		allSlots.addAll(cargoModel.getLoadSlots());
+		allSlots.addAll(cargoModel.getDischargeSlots());
+		
+		// build a set of all slots that have no cargo associated with them
+		for (Slot<?> slot: allSlots) {
+			Assertions.assertNotNull(slot);
+			if (slot.getCargo() == null) {
+				openSlots.add(slot);
+			}
+		}
+		
+		// build a set of all slots that are attached to a cargo in the list of cargoes
+		for (Cargo cargo: cargoModel.getCargoes()) {
+			for (Slot<?> slot: cargo.getSlots()) {
+				Assertions.assertNotNull(slot);
+				cargoSlots.add(slot);
+			}
+		}
+		
+		// sanity check that there is no overlap between the sets		
+		HashSet<Slot<?>> intersection = new HashSet<Slot<?>>(openSlots);
+		intersection.retainAll(cargoSlots);
+		Assertions.assertTrue(intersection.isEmpty(), "Cargo model inconsistency: slots with null cargo in cargo.");
+		
+		// sanity check that the lists are exhaustive
+		HashSet<Slot<?>> union = new HashSet<Slot<?>>(openSlots);
+		union.addAll(cargoSlots);
+		Assertions.assertTrue(union.equals(allSlots), "Cargo model inconsistency: load & discharge slot lists do not match up with open and closed slot lists.");
+				
+		// build a set of all slots returned by getOpenSlotsIterable()
+		HashSet<Slot<?>> testSet = new HashSet<Slot<?>>();		
+		for (Slot<?> slot: CargoUtils.getOpenSlotsIterable(cargoModel)) {
+			testSet.add(slot);
+		}
+		
+		// check that it is the same as the set of slots with no cargo attached to them
+		Assertions.assertTrue(testSet.equals(openSlots));				
+	}
+	
+	@Test
+	/** Tests CargoUtils.getOpenSlotsIterator() against a small toy cargo model.
+	 * 
+	 */
+	public void testGetOpenSlotsIterator() {
+		// test structure: create a scenario with load slots, discharge slots and cargoes
+		// ensure that there are some open slots (i.e. not attached to cargoes)
+		// and some slots that are attached to cargoes
+		// 
+		// then call getOpenSlotsIterable(), iterate over it, and ensure that it returns the open slots 
+
+		// Setup code copy-pasted from NominalMarketTest
+		
+		final Vessel vessel = fleetModelFinder.findVessel("STEAM-145");
+		vessel.setSafetyHeel(500);
+
+		final CharterInMarket charterInMarket_1 = spotMarketsModelBuilder.createCharterInMarket("CharterIn 1", vessel, "50000", 0);
+		final Cargo cargo1 = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("L1", LocalDate.of(2015, 12, 5), portFinder.findPort("Point Fortin"), null, entity, "5") //
+				.build() //
+				.makeDESSale("D1", LocalDate.of(2015, 12, 11), portFinder.findPort("Dominion Cove Point LNG"), null, entity, "7") //
+				.build() //
+				.withVesselAssignment(charterInMarket_1, -1, 1) // -1 is nominal
+				.withAssignmentFlags(true, false) //
+				.build();
+		
+		// end copy-paste
+		
+		// add some open slots to the model
+		
+		cargoModelBuilder.createFOBPurchase("L2", LocalDate.of(2015, 12, 3), portFinder.findPort("Point Fortin"), null, entity, "5", null);
+		cargoModelBuilder.createFOBPurchase("L3", LocalDate.of(2015, 12, 1), portFinder.findPort("Point Fortin"), null, entity, "5", null);
+		cargoModelBuilder.createDESSale("D2", LocalDate.of(2015, 12, 11), portFinder.findPort("Dominion Cove Point LNG"), null, entity, "7");
+
+		// run a test on the model
+
+		testGetOpenSlotsIterable(lngScenarioModel.getCargoModel());
+		
 	}
 }
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo/src/com/mmxlabs/models/lng/cargo/util/CargoUtils.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo/src/com/mmxlabs/models/lng/cargo/util/CargoUtils.java
@@ -4,10 +4,14 @@
  */
 package com.mmxlabs.models.lng.cargo.util;
 
+import java.util.Iterator;
+
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jdt.annotation.NonNull;
 
 import com.mmxlabs.common.CollectionsUtil;
 import com.mmxlabs.models.lng.cargo.Cargo;
+import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.DischargeSlot;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
 import com.mmxlabs.models.lng.cargo.Slot;
@@ -29,5 +33,102 @@ public class CargoUtils {
 			discharge = (DischargeSlot) lastElement;
 		}
 		return discharge;
+	}
+	
+	/**
+	 * Returns an iterable over all the open slots in a particular cargo model, for use in a for loop.
+	 * <p>
+	 * Example use:
+	 * <pre>{@code
+	 * for ( Slot<?> slot: CargoUtils.getOpenSlotsIterable(cargoModel) ) {
+	 *   ...
+	 * }
+	 * }</pre>
+	 * @param cargoModel
+	 * @return An iterable over all the load and discharge slots that do not belong to a Cargo.
+	 */
+	public static @NonNull Iterable<Slot<?>> getOpenSlotsIterable(CargoModel cargoModel) {
+		// annoyingly, Java requires for-loops to be over instances of Iterable, not Iterator
+		return new Iterable<Slot<?>>() {
+
+			@Override
+			public Iterator<Slot<?>> iterator() {
+				return getOpenSlotsIterator(cargoModel);
+			}
+			
+		};
+	}
+	
+	/**
+	 * Returns an iterator over all the open slots in a particular cargo model.
+	 * <p>
+	 * The code is written to provide efficient iteration without copying any data.
+	 * 
+	 * @param cargoModel
+	 * @return An iterator over all the load and discharge slots that do not belong to a Cargo.
+	 */
+	public static @NonNull Iterator<Slot<?>> getOpenSlotsIterator(CargoModel cargoModel) {
+		return new Iterator<Slot<?>>() {
+			// we will traverse iterators over the load slots and discharge slots
+			private Iterator<?> iterators [] = {
+					cargoModel.getLoadSlots().iterator(),
+					cargoModel.getDischargeSlots().iterator()				
+			};
+
+			// we need to keep track of where we are in our sub-iterator collection 
+			private int index = 0;
+			
+			// we store look-ahead results for hasNext() and next()
+			private boolean _hasNext = true;
+			private Slot<?> _next = null;
+
+			// initialisation code
+			{
+				traverse();
+			}
+			
+			/**
+			 * Look ahead to find the next open slot, if any, in the cargo model.
+			 * Store it as _next, if there is one; otherwise, set _hasNext to false. 
+			 */
+			private void traverse() {
+				do {
+					// check if we hit the end of our sub-iterator
+					while (!iterators[index].hasNext()) {
+						// continue with the next sub-iterator
+						index++;
+						// unless we have run out of sub-iterators
+						if (index >= iterators.length) {
+							// then we have no more slots to return
+							_hasNext = false;
+							return;
+						}
+					}
+					
+					_next = (Slot<?>) iterators[index].next();
+				}
+				// keep clicking forward until we find an open slot
+				while (_next == null || _next.getCargo() != null); 
+					
+			}
+
+			@Override
+			public boolean hasNext() {
+				// return the cached computation
+				return _hasNext;
+			}
+
+			@Override
+			public Slot<?> next() {
+				// store the cached iteration result
+				Slot<?> nextResult = _next;
+				// cache the next iteration
+				traverse();
+				// return this iteration's result
+				return nextResult;
+			}
+			
+		};
+
 	}
 }
