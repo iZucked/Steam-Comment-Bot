7ca9cfebfd05f1b245a6acb493fe7fe9f1049325
smcgregor@minimaxlabs.com <simonmcgregor@minimaxs-mbp>
2019-05-23 09:36:12 +0100

    Roll forward dialog now includes Horizon date and better documentation

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario.ui/src/com/mmxlabs/models/lng/scenario/actions/RollForwardDialog.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.scenario.ui/src/com/mmxlabs/models/lng/scenario/actions/RollForwardDialog.java
@@ -28,7 +28,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -55,10 +54,6 @@ import com.mmxlabs.scenario.service.model.manager.IScenarioDataProvider;
 public class RollForwardDialog extends FormDialog {
 
 	private static final String DIALOG_SECTION = "com.mmxlabs.models.lng.scenario.action.RollForwardDialog";
-
-	private static final String DIALOG_REMOVE_SLOT_EVENT_ENABLE = "remove_slot_event_enable";
-	private static final String DIALOG_UPDATE_VESSELS_ENABLED = "update_vessels_enabled";
-	private static final String DIALOG_REMOVE_VESSELS_ENABLED = "remove_vessels_enabled";
 	
 	// Following fields are used to persist checkbox settings
 	private static final String DIALOG_RELATIVE_CHECKBOX_SUFFIX = "_relative_checkbox_checked"; 
@@ -66,10 +61,7 @@ public class RollForwardDialog extends FormDialog {
 	private static final String DIALOG_CHARTER_START_NAME = "charter_start"; 
 	private static final String DIALOG_FREEZE_DATE_NAME = "freeze_date";
 
-	private static final String TOOLTIP_REMOVE_DATE = "Slots and Events before this date will be removed from the scenario.";
 	private static final String TOOLTIP_FREEZE_DATE = "Cargoes and Events before or over this date will be frozen. Vessel assignment will be fixed, pairings will be fixed and arrival window will be set to currently scheduled arrival time.";
-	private static final String TOOLTIP_UPDATE_VESSELS = "Vessels start date, location and heel will be updated where possible based on the first cargo or event still in the scenario assigned to the vessel.";
-	private static final String TOOLTIP_REMOVE_VESSELS = "Vessels whose End By date is before the remove date will be removed from the scenaruio.";
 
 
 	private final EditingDomain domain;
@@ -80,21 +72,14 @@ public class RollForwardDialog extends FormDialog {
 	
 	private DateTime promptStart;
 	private DateTime promptEnd;
+	private DateTime horizon = null;
 	private DateTime charterOutStart = null;
 	private DateTime charterOutEnd = null;
-	/*
-	private Button removeDateCheckBox;
-	private DateTime removeDate;
-	private Button removeVesselsCheckBox;
-	private Button updateVesselsCheckBox;
-	*/
+
 	private List<IRollForwardChange> changes;
 	
 	private Map<String, Button> persistenceStringsForCheckboxes = new HashMap<>();
-	private Map<DateTime, Period> relativeTimeFieldPeriods = new HashMap<>();
-
-
-	
+	private Map<DateTime, Period> relativeTimeFieldPeriods = new HashMap<>();	
 
 	public RollForwardDialog(final Shell shell, final IScenarioDataProvider scenarioDataProvider) {
 		super(shell);
@@ -193,6 +178,12 @@ public class RollForwardDialog extends FormDialog {
 		
 	}
 
+	/**
+	 * If the specified checkbox is set, updates the specified DateTime control to be a certain period of time after the prompt start date.
+	 * @param checkbox
+	 * @param dateField
+	 * @param amount
+	 */
 	protected void updateFieldIfCheckboxSet(Button checkbox, DateTime dateField, Period amount) 
 	{
 		boolean selected = checkbox.getSelection();
@@ -208,6 +199,11 @@ public class RollForwardDialog extends FormDialog {
 		
 	}
 	
+	/**
+	 * Creates a form label, a matching date control, and a checkbox that determines whether the date control is derived from the prompt start.
+	 * 
+	 * @return A {@link List} of the controls created.
+	 */
 	protected List<Control> createRelativeDateControls(IManagedForm mform, Composite parent, String label, String id, LocalDate date, Period amount)
 	{
 		List<Control> result = createLabelAndDateControl(mform, parent, label, date);
@@ -276,6 +272,9 @@ public class RollForwardDialog extends FormDialog {
 		return result;
 	}
 
+	/**
+	 * Creates the controls for the roll forward dialog.
+	 */
 	@Override
 	protected void createFormContent(final IManagedForm mform) {
 
@@ -292,23 +291,15 @@ public class RollForwardDialog extends FormDialog {
 			mform.getToolkit().adapt(datesGroup);
 
 			promptStart = (DateTime) createLabelAndDateControl(mform, datesGroup, "Prompt Start", null).get(1);
-			/*
-			promptStart.addSelectionListener(new SelectionAdapter() {
-				@Override
-				public void widgetSelected(final SelectionEvent e) {
-					recalculateRelativeDateFields();
-					refreshPreview();
-				}
-				
-			});
-			*/
 			
 			mform.getToolkit().createLabel(datesGroup, ""); // empty label for empty grid cell
 
 			promptEnd = (DateTime) createRelativeDateControls(mform, datesGroup, "Prompt End", DIALOG_PROMPT_END_NAME, LocalDate.now().plusDays(90), Period.of(0, 0, 90)).get(1);
 
-			final DateTime horizon = (DateTime) createLabelAndDateControl(mform, datesGroup, "Horizon", scenarioModel.getSchedulingEndDate()).get(1);
-			mform.getToolkit().createLabel(datesGroup, ""); // empty label for empty grid cell
+			if (scenarioModel.getSchedulingEndDate() != null) {
+				horizon = (DateTime) createLabelAndDateControl(mform, datesGroup, "Horizon", scenarioModel.getSchedulingEndDate()).get(1);
+				mform.getToolkit().createLabel(datesGroup, ""); // empty label for empty grid cell
+			}			
 			
 			CharterOutMarketParameters charterDates = ScenarioModelUtil.getSpotMarketsModel(scenarioModel).getCharterOutMarketParameters();
 			
@@ -333,106 +324,14 @@ public class RollForwardDialog extends FormDialog {
 
 			// Freeze Date
 			{
-				freezeDate = (DateTime) createRelativeDateControls(mform, inputGroup, "Freeze before", DIALOG_FREEZE_DATE_NAME, LocalDate.now().plusMonths(1), Period.of(0,  1,  0) ).get(1);
+				List<Control> controls = createRelativeDateControls(mform, inputGroup, "Freeze before", DIALOG_FREEZE_DATE_NAME, LocalDate.now().plusMonths(1), Period.of(0,  1,  0) );
+				freezeDate = (DateTime) controls.get(1);
+				Control lbl = controls.get(0);
 				
-				// lbl.setToolTipText(TOOLTIP_FREEZE_DATE);
+				lbl.setToolTipText(TOOLTIP_FREEZE_DATE);
 				freezeDate.setToolTipText(TOOLTIP_FREEZE_DATE);
 			}
 
-			/*
-			// Remove Date
-			{
-				final Label lbl = mform.getToolkit().createLabel(inputGroup, "Remove before");
-
-				final Composite c = mform.getToolkit().createComposite(inputGroup);
-				c.setLayout(new GridLayout(2, false));
-				removeDateCheckBox = mform.getToolkit().createButton(c, "", SWT.CHECK);
-				getBooleanValue(DIALOG_REMOVE_SLOT_EVENT_ENABLE, removeDateCheckBox);
-
-				removeDateCheckBox.addSelectionListener(new SelectionAdapter() {
-
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-
-						removeDate.setEnabled(removeDateCheckBox.getSelection());
-						removeVesselsCheckBox.setEnabled(removeDateCheckBox.getSelection() && updateVesselsCheckBox.getSelection());
-						refreshPreview();
-					}
-				});
-
-				removeDate = new DateTime(c, SWT.DATE);
-				mform.getToolkit().adapt(removeDate, true, true);
-				removeDate.addSelectionListener(new SelectionAdapter() {
-
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-						refreshPreview();
-					}
-				});
-				removeDate.setEnabled(removeDateCheckBox.getSelection());
-
-				{
-					// Always set to zero!
-					removeDate.setSeconds(0);
-					removeDate.setMinutes(0);
-					removeDate.setHours(0);
-
-					final Calendar cal = Calendar.getInstance();
-					cal.add(Calendar.MONTH, -2);
-					removeDate.setYear(cal.get(Calendar.YEAR));
-					removeDate.setMonth(cal.get(Calendar.MONTH));
-					removeDate.setDay(cal.get(Calendar.DAY_OF_MONTH));
-				}
-
-				lbl.setToolTipText(TOOLTIP_REMOVE_DATE);
-				removeDateCheckBox.setToolTipText(TOOLTIP_REMOVE_DATE);
-				removeDate.setToolTipText(TOOLTIP_REMOVE_DATE);
-				c.setToolTipText(TOOLTIP_REMOVE_DATE);
-			}
-			*/
-			
-			/*
-			// Update Vessels
-			{
-				final Label lbl = mform.getToolkit().createLabel(inputGroup, "Update Vessel Start");
-
-				updateVesselsCheckBox = mform.getToolkit().createButton(inputGroup, "", SWT.CHECK);
-				updateVesselsCheckBox.addSelectionListener(new SelectionAdapter() {
-
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-
-						removeVesselsCheckBox.setEnabled(removeDateCheckBox.getSelection() && updateVesselsCheckBox.getSelection());
-						refreshPreview();
-					}
-				});
-				getBooleanValue(DIALOG_UPDATE_VESSELS_ENABLED, updateVesselsCheckBox);
-
-				lbl.setToolTipText(TOOLTIP_UPDATE_VESSELS);
-				updateVesselsCheckBox.setToolTipText(TOOLTIP_UPDATE_VESSELS);
-			}
-
-			// Remove Vessels
-			{
-				final Label lbl = mform.getToolkit().createLabel(inputGroup, "Remove old vessels");
-
-				removeVesselsCheckBox = mform.getToolkit().createButton(inputGroup, "", SWT.CHECK);
-				removeVesselsCheckBox.addSelectionListener(new SelectionAdapter() {
-
-					@Override
-					public void widgetSelected(final SelectionEvent e) {
-
-						refreshPreview();
-					}
-				});
-				removeVesselsCheckBox.setEnabled(removeDateCheckBox.getSelection() && updateVesselsCheckBox.getSelection());
-				getBooleanValue(DIALOG_REMOVE_VESSELS_ENABLED, removeVesselsCheckBox);
-
-				lbl.setToolTipText(TOOLTIP_REMOVE_VESSELS);
-				removeVesselsCheckBox.setToolTipText(TOOLTIP_REMOVE_VESSELS);
-
-			}
-			*/
 		}
 		// Preview Section
 		{
@@ -468,6 +367,10 @@ public class RollForwardDialog extends FormDialog {
 		refreshPreview();
 	}
 
+	/**
+	 * Creates a list of {@link IRollForwardChange} objects representing the changes to the relevant date fields in the data model.
+	 * @return A list of {@link IRollForwardChange} objects (that will all be {@link UpdateDateChange} instances).
+	 */
 	private List<IRollForwardChange> generateDateChanges() {
 		List<IRollForwardChange> result = new LinkedList<>();
 		
@@ -477,6 +380,10 @@ public class RollForwardDialog extends FormDialog {
 		result.add(new UpdateDateChange("Prompt Start", scenarioModel, promptStartFeature, getLocalDateFromDateTimeField(promptStart), domain));
 		result.add(new UpdateDateChange("Prompt End", scenarioModel, promptEndFeature, getLocalDateFromDateTimeField(promptEnd), domain));
 	
+		if (horizon != null) {			
+			result.add(new UpdateDateChange("Horizon", scenarioModel, LNGScenarioPackage.eINSTANCE.getLNGScenarioModel_SchedulingEndDate(), getLocalDateFromDateTimeField(horizon), domain));
+		}
+
 		
 		CharterOutMarketParameters charterDates = ScenarioModelUtil.getSpotMarketsModel(scenarioModel).getCharterOutMarketParameters();
 
@@ -487,8 +394,6 @@ public class RollForwardDialog extends FormDialog {
 		if (charterOutEnd != null) {			
 			result.add(new UpdateDateChange("Charter Out End", charterDates, SpotMarketsPackage.Literals.CHARTER_OUT_MARKET_PARAMETERS__CHARTER_OUT_END_DATE, getLocalDateFromDateTimeField(charterOutEnd), domain));
 		}
-
-		//final DateTime horizon = (DateTime) createLabelAndDateControl(mform, datesGroup, "Horizon", scenarioModel.getSchedulingEndDate()).get(1);
 
 		return result ;
 	}
@@ -502,14 +407,7 @@ public class RollForwardDialog extends FormDialog {
 			final LocalDate dt = LocalDate.of(freezeDate.getYear(), 1 + freezeDate.getMonth(), freezeDate.getDay());
 			descriptor.put(RollForwardDescriptor.FreezeDate, dt);
 		}
-		/*
-		if (removeDateCheckBox.getSelection()) {
-			final LocalDate dt = LocalDate.of(removeDate.getYear(), 1 + removeDate.getMonth(), removeDate.getDay());
-			descriptor.put(RollForwardDescriptor.RemoveDate, dt);
-		}
-		descriptor.put(RollForwardDescriptor.UpdateVessels, updateVesselsCheckBox.getSelection());
-		descriptor.put(RollForwardDescriptor.RemoveVessels, updateVesselsCheckBox.getSelection() && removeVesselsCheckBox.getSelection());
-		*/
+
 		final Button button = getButton(IDialogConstants.OK_ID);
 		try {
 			final LocalDate dt = LocalDate.of(freezeDate.getYear(), 1 + freezeDate.getMonth(), freezeDate.getDay());
