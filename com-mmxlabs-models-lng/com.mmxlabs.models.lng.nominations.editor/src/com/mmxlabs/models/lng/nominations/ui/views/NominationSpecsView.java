18b532933ff6023182a97b4a85d5979588e3be34
Patrick Mills <pmills@minimaxlabs.com>
2019-06-04 18:03:03 +0100

    Add openStatus methods so that if item is selected in validation view, will get selected in nominations view or nominations specs view.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/ui/views/NominationSpecsView.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/ui/views/NominationSpecsView.java
@@ -6,13 +6,17 @@ package com.mmxlabs.models.lng.nominations.ui.views;
 
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.viewers.StructuredSelection;
 
+import com.mmxlabs.models.lng.nominations.AbstractNominationSpec;
 import com.mmxlabs.models.lng.nominations.NominationsPackage;
 import com.mmxlabs.models.lng.nominations.ui.editorpart.NominationSpecsViewerPane;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioPackage;
 import com.mmxlabs.models.lng.ui.views.ScenarioTableViewerView;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class NominationSpecsView extends ScenarioTableViewerView<NominationSpecsViewerPane> {
 	public static final String ID = "com.mmxlabs.models.lng.nominations.editor.NominationSpecsView";
@@ -34,4 +38,22 @@ public class NominationSpecsView extends ScenarioTableViewerView<NominationSpecs
 			pane.getViewer().setInput(getRootObject());
 		}	
 	}
+	
+	@Override
+	public void openStatus(final IStatus status) {
+		if (status.isMultiStatus()) {
+			// Try first element
+			openStatus(status.getChildren()[0]);
+		}
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			if (dcsd.getTarget() instanceof AbstractNominationSpec) {
+				final AbstractNominationSpec ns = (AbstractNominationSpec) dcsd.getTarget();
+				getSite().getPage().activate(this);
+				setSelection(new StructuredSelection(ns), true);
+			}
+		}
+	}
+
 }
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/ui/views/NominationsView.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/ui/views/NominationsView.java
@@ -4,51 +4,27 @@
  */
 package com.mmxlabs.models.lng.nominations.ui.views;
 
-import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jface.viewers.StructuredSelection;
 
+import com.mmxlabs.models.lng.nominations.AbstractNomination;
 import com.mmxlabs.models.lng.nominations.NominationsPackage;
 import com.mmxlabs.models.lng.nominations.ui.editorpart.RelativeDateRangeNominationsViewerPane;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioPackage;
 import com.mmxlabs.models.lng.ui.tabular.ScenarioTableViewerPane;
 import com.mmxlabs.models.lng.ui.views.ScenarioTableViewerView;
-import com.mmxlabs.models.ui.editorpart.IScenarioEditingLocation;
+import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 
 public class NominationsView extends ScenarioTableViewerView<ScenarioTableViewerPane> {
-
-	private static Logger logger = LoggerFactory.getLogger(NominationsView.class);
-
+	
 	public static final String ID = "com.mmxlabs.models.lng.nominations.editor.NominationsView";
 	
 	@Override
 	protected ScenarioTableViewerPane createViewerPane() {
-		try {
-			String viewerPaneClassName = this.getConfigurationElement().getAttribute("viewerPane");
-			if (viewerPaneClassName != null) {
-				Class<?> cls = Class.forName(viewerPaneClassName);
-				Constructor<?> cons = cls.getConstructor(IWorkbenchPage.class, IWorkbenchPart.class, IScenarioEditingLocation.class, IActionBars.class);
-				Object viewerPaneObject = cons.newInstance(getSite().getPage(), this, this, getViewSite().getActionBars());
-				if (viewerPaneObject instanceof ScenarioTableViewerPane) {
-					return (ScenarioTableViewerPane)viewerPaneObject;
-				}
-				else {
-					logger.error("Invalid viewerPane specified in plugin.xml: "+viewerPaneClassName+" is not derived from ScenarioTableViewerPane.");
-				}
-			}
-		}
-		catch (Exception ex) {
-			logger.error("Error creating specified viewerPane object in NominationsView: ", ex);
-		}
-
-		//Default view pane in case none specified in plugin.xml.
 		return new RelativeDateRangeNominationsViewerPane(getSite().getPage(), this, this, getViewSite().getActionBars());
 	}
 
@@ -61,6 +37,23 @@ public class NominationsView extends ScenarioTableViewerView<ScenarioTableViewer
 					NominationsPackage.eINSTANCE.getNominationsModel_SlotNominations() }),
 					getAdapterFactory(), getModelReference());
 			pane.getViewer().setInput(getRootObject());
+		}
+	}
+	
+	@Override
+	public void openStatus(final IStatus status) {
+		if (status.isMultiStatus()) {
+			// Try first element
+			openStatus(status.getChildren()[0]);
+		}
+
+		if (status instanceof DetailConstraintStatusDecorator) {
+			final DetailConstraintStatusDecorator dcsd = (DetailConstraintStatusDecorator) status;
+			if (dcsd.getTarget() instanceof AbstractNomination) {
+				final AbstractNomination n = (AbstractNomination) dcsd.getTarget();
+				getSite().getPage().activate(this);
+				setSelection(new StructuredSelection(n), true);
+			}
 		}
 	}
 }
