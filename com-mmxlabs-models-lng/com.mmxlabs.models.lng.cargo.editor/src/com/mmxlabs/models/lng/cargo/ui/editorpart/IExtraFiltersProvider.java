a9ecd0ae9f286cb372ac88f77f01796c3bd2ba32
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2019-08-02 14:21:40 +0000

    Adds IExtraFiltersProvider for TradesWiringViewer
    Addresses #116

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/IExtraFiltersProvider.java
@@ -0,0 +1,17 @@
+package com.mmxlabs.models.lng.cargo.ui.editorpart;
+
+import java.util.List;
+
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+import com.mmxlabs.models.lng.cargo.ui.editorpart.actions.DefaultMenuCreatorAction;
+
+public interface IExtraFiltersProvider {
+	
+	List<ViewerFilter> getExtraFilters();
+	
+	List<DefaultMenuCreatorAction> getExtraMenuActions(final StructuredViewer viewer);
+	
+	void clear();
+}
