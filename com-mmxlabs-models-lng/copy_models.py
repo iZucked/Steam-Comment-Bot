02252289e1a294f9ec267ef6ebbd30a8ed709bc0
Patrick Mills <pmills@minimaxlabs.com>
2019-10-08 15:50:17 +0100

    Migration to v116 (add include canal travel time flag + bb lump sum).

--- com-mmxlabs-models-lng/copy_models.py
+++ com-mmxlabs-models-lng/copy_models.py
@@ -6,7 +6,7 @@
 import shutil, os
 #####################################
 path_to_ws = os.getcwd()
-version = 115
+version = 116
 #####################################
 
 root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
