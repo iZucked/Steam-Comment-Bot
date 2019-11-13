283ec45baf406cd4ea2a2de9edacc8f03946e3fd
Patrick Mills <pmills@minimaxlabs.com>
2019-11-13 14:39:23 +0000

    ix: Update Due Date in nominations editor when period changes.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/presentation/composites/DueDateInlineEditor.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/presentation/composites/DueDateInlineEditor.java
@@ -7,8 +7,11 @@ package com.mmxlabs.models.lng.nominations.presentation.composites;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 import com.mmxlabs.models.lng.nominations.AbstractNomination;
+import com.mmxlabs.models.lng.nominations.NominationsPackage;
 import com.mmxlabs.models.lng.nominations.utils.NominationsModelUtils;
 import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.mmxcore.MMXObject;
+import com.mmxlabs.models.mmxcore.MMXObject.DelegateInformation;
 import com.mmxlabs.models.ui.editors.impl.LocalDateInlineEditor;
 
 public class DueDateInlineEditor extends LocalDateInlineEditor {
@@ -25,5 +28,13 @@ public class DueDateInlineEditor extends LocalDateInlineEditor {
 		else {
 			return super.getValue();
 		}
+	}
+	
+	@Override
+	protected boolean updateOnChangeToFeature(final Object changedFeature) {
+		if (changedFeature == NominationsPackage.eINSTANCE.getAbstractNominationSpec_Size()) {
+			return true;
+		}
+		return super.updateOnChangeToFeature(changedFeature);
 	}
 }
