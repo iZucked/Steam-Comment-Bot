2462bf6d050da5d7ea9f3044729f06c8ba29e2e5
Patrick Mills <pmills@minimaxlabs.com>
2019-11-19 16:09:02 +0000

    Add javadoc.

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/contracts/ICharterCostCalculator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/contracts/ICharterCostCalculator.java
@@ -1,5 +1,17 @@
 package com.mmxlabs.scheduler.optimiser.contracts;
 
+/**
+ * For calculating the cost of chartering in a vessel.
+ */
 public interface ICharterCostCalculator extends ICalculator {
+
+	/**
+	 * Get the total charter cost for the duration. 
+	 * @param vesselStartTime
+	 * @param voyagePlanStartTime
+	 * @param eventStartTime 
+	 * @param duration in hours
+	 * @return the total charter cost.
+	 */
 	long getCharterCost(int vesselStartTime, int voyagePlanStartTime, int eventStartTime, int duration);
 }
