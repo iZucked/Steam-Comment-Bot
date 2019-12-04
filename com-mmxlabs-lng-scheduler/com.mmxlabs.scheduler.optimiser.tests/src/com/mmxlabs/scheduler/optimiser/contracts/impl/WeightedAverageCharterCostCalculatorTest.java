2743657e38d0be69b078336ef67c9a49f4a680be
Jenkins <hudson@minimaxlabs.com>
2019-12-04 15:44:42 +0000

    Update headers

--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser.tests/src/com/mmxlabs/scheduler/optimiser/contracts/impl/WeightedAverageCharterCostCalculatorTest.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser.tests/src/com/mmxlabs/scheduler/optimiser/contracts/impl/WeightedAverageCharterCostCalculatorTest.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2019
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.contracts.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/contracts/impl/FixedCharterInRateCharterCostCalculator.java
+++ com-mmxlabs-lng-scheduler/com.mmxlabs.scheduler.optimiser/src/com/mmxlabs/scheduler/optimiser/contracts/impl/FixedCharterInRateCharterCostCalculator.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2019
+ * All rights reserved.
+ */
 package com.mmxlabs.scheduler.optimiser.contracts.impl;
 
 import com.mmxlabs.scheduler.optimiser.contracts.ICharterCostCalculator;
