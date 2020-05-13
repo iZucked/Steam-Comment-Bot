8d31882cf82eca1889b650a9bc90369c6e44f7cf
Patrick Mills <pmills@minimaxlabs.com>
2020-05-13 17:04:29 +0100

    Update tool-tips for CAP and FLOOR as per Proshun's suggested text.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/autocomplete/PriceExpressionProposalProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.pricing.editor/src/com/mmxlabs/models/lng/pricing/ui/autocomplete/PriceExpressionProposalProvider.java
@@ -148,14 +148,14 @@ public class PriceExpressionProposalProvider implements IMMXContentProposalProvi
 			final String proposal = "CAP(";
 			if (proposal.length() >= contents.length() && proposal.substring(0, contents.length()).equalsIgnoreCase(contents)) {
 				final String c = proposal.substring(contents.length());
-				list.add(new ContentProposal(c, proposal + "a,b)", "Function returning the lower value of a or b.", c.length()));
+				list.add(new ContentProposal(c, proposal + "a,b)", "Use this to put an upper limit (a or b) on a price expression, e.g. \"CAP(1.1, 115%HH)\" gives 115%HH but no higher than $1.10", c.length()));
 			}
 		}
 		{
 			final String proposal = "FLOOR(";
 			if (proposal.length() >= contents.length() && proposal.substring(0, contents.length()).equalsIgnoreCase(contents)) {
 				final String c = proposal.substring(contents.length());
-				list.add(new ContentProposal(c, proposal + "a,b)", "Function returning the higher value of a or b.", c.length()));
+				list.add(new ContentProposal(c, proposal + "a,b)", "Use this to put a lower limit (a or b) on a price expression, e.g. \"FLOOR(1.1, 115%HH)\" gives 115%HH but no lower than $1.10", c.length()));
 			}
 		}
 		{
