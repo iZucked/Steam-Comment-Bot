7c9a36e8fc9d5ffa1adcc31762a3faf5c3308cdb
Jenkins <hudson@minimaxlabs.com>
2020-06-04 11:54:52 +0000

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/schedule/CustomReportsDefaults.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/schedule/CustomReportsDefaults.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2020
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.views.schedule;
 
 import static org.ops4j.peaberry.eclipse.EclipseRegistry.eclipseRegistry;
