1675a8697ac83c0ec5604f4e3446c9f7188b48da
Patrick Mills <pmills@minimaxlabs.com>
2020-06-29 15:46:04 +0100

    Organise imports.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/customizable/CustomReportsManagerDialog.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/customizable/CustomReportsManagerDialog.java
@@ -11,7 +11,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Supplier;
