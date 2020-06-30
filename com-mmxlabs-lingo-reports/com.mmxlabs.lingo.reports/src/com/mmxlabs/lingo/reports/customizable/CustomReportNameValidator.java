6ca7c2d380d35ed341a9f332146065520f21597f
Patrick Mills <pmills@minimaxlabs.com>
2020-06-30 16:29:41 +0100

    Exclude system view names from allow custom report names.

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/customizable/CustomReportNameValidator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/customizable/CustomReportNameValidator.java
@@ -4,9 +4,12 @@
  */
 package com.mmxlabs.lingo.reports.customizable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.IViewDescriptor;
 
 public class CustomReportNameValidator implements IInputValidator {
 	
@@ -25,12 +28,46 @@ public class CustomReportNameValidator implements IInputValidator {
 		if (this.existingNames.contains(name.toLowerCase())) {
 			return "Cannot use this name as a report already exists with it";
 		}
+		
 		if (name == null || name.isEmpty()) {
 			return "A name must be set";
 		}
+		
 		if ("Schedule Summary".equalsIgnoreCase(name.trim())) {
 			return "Cannot use this name";
 		}
+		
+		String registeredViewName = isRegisteredViewName(name);
+		if (registeredViewName != null) {	
+			return "Cannot use this name, as there is already a System view named "+registeredViewName;
+		}
+		
 		return null;
+	}
+	
+	private String isRegisteredViewName(String name) {
+		List<String> registeredViewNames = getExistingRegisteredViewNames();
+		for (String viewName : registeredViewNames) {
+			if (viewName.equalsIgnoreCase(name)) {
+				return viewName;
+			}
+		}
+		return null;
+	}
+	
+	private List<String> getExistingRegisteredViewNames() {
+		final IViewDescriptor[] views = PlatformUI.getWorkbench().getViewRegistry().getViews();
+		List<String> registeredViewNames = new ArrayList<>();
+		
+		for (IViewDescriptor view : views) {
+			if (view.getCategoryPath() != null && 
+				view.getCategoryPath().length == 1 &&
+				!"[User Reports]".equalsIgnoreCase(view.getCategoryPath()[0]) &&
+				!"[Team Reports]".equalsIgnoreCase(view.getCategoryPath()[0])) {
+				registeredViewNames.add(view.getLabel());
+			}
+		}
+		
+		return registeredViewNames;
 	}
 }
