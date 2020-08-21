36f5715f54d048fe61bac747c745477ffd0da404
Simon Goodall <simon.goodall@minimaxlabs.com>
2020-08-21 13:01:04 +0100

    [Sandbox] Add new spot index drop down for existing charter.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/presentation/composites/ExistingCharterMarketOptionComponentHelper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/presentation/composites/ExistingCharterMarketOptionComponentHelper.java
@@ -6,23 +6,20 @@
  */
 package com.mmxlabs.models.lng.analytics.presentation.composites;
 
-import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
-
-import com.mmxlabs.models.mmxcore.MMXCorePackage;
-import com.mmxlabs.models.ui.BaseComponentHelper;
-import com.mmxlabs.models.ui.ComponentHelperUtils;
-import com.mmxlabs.models.ui.IComponentHelper;
-import com.mmxlabs.models.ui.IInlineEditorContainer;
-
-import com.mmxlabs.models.ui.registries.IComponentHelperRegistry;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.core.runtime.Platform;
-
 import org.eclipse.emf.ecore.EClass;
+
+import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
+import com.mmxlabs.models.mmxcore.MMXCorePackage;
+import com.mmxlabs.models.ui.BaseComponentHelper;
+import com.mmxlabs.models.ui.ComponentHelperUtils;
+import com.mmxlabs.models.ui.IComponentHelper;
+import com.mmxlabs.models.ui.IInlineEditorContainer;
+import com.mmxlabs.models.ui.registries.IComponentHelperRegistry;
 
 /**
  * A component helper for ExistingCharterMarketOption instances
@@ -84,9 +81,9 @@ public class ExistingCharterMarketOptionComponentHelper extends BaseComponentHel
 	/**
 	 * Create the editor for the spotIndex feature on ExistingCharterMarketOption
 	 *
-	 * @generated
+	 * @generated NOT
 	 */
 	protected void add_spotIndexEditor(final IInlineEditorContainer detailComposite, final EClass topClass) {
-		detailComposite.addInlineEditor(ComponentHelperUtils.createDefaultEditor(topClass, AnalyticsPackage.Literals.EXISTING_CHARTER_MARKET_OPTION__SPOT_INDEX));
+		detailComposite.addInlineEditor(new ExistingMarketSpotIndexInlineEditor(AnalyticsPackage.Literals.EXISTING_CHARTER_MARKET_OPTION__SPOT_INDEX));
 	}
 }
---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/presentation/composites/ExistingMarketSpotIndexInlineEditor.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2020
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.analytics.presentation.composites;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jdt.annotation.NonNull;
+
+import com.mmxlabs.common.Pair;
+import com.mmxlabs.license.features.LicenseFeatures;
+import com.mmxlabs.models.lng.analytics.ExistingCharterMarketOption;
+import com.mmxlabs.models.lng.cargo.AssignableElement;
+import com.mmxlabs.models.lng.spotmarkets.CharterInMarket;
+import com.mmxlabs.models.lng.types.VesselAssignmentType;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
+import com.mmxlabs.models.ui.editors.dialogs.IDialogEditingContext;
+import com.mmxlabs.models.ui.editors.impl.ValueListInlineEditor;
+
+/**
+ */
+public class ExistingMarketSpotIndexInlineEditor extends ValueListInlineEditor {
+	private static @NonNull List<Pair<String, Object>> defaultValues = getDefaultValues();
+
+	public ExistingMarketSpotIndexInlineEditor(final EAttribute feature) {
+		super(feature, defaultValues);
+	}
+
+	@Override
+	public void display(final IDialogEditingContext dialogContext, final MMXRootObject rootObject, final EObject input, final Collection<EObject> range) {
+
+		List<Pair<String, Object>> values = getDefaultValues();
+
+		if (input instanceof ExistingCharterMarketOption) {
+			final ExistingCharterMarketOption option = (ExistingCharterMarketOption) input;
+			final CharterInMarket charterInMarket = option.getCharterInMarket();
+			values = getValues(charterInMarket, option.getSpotIndex());
+		}
+		updateCombo(values);
+
+		super.display(dialogContext, rootObject, input, range);
+	}
+
+	private static @NonNull List<Pair<String, Object>> getDefaultValues() {
+		final ArrayList<Pair<String, Object>> result = new ArrayList<>();
+		result.add(new Pair<String, Object>("Unknown", Integer.valueOf(0)));
+		return result;
+	}
+
+	private static @NonNull List<Pair<String, Object>> getValues(final CharterInMarket market, final int currentIndex) {
+		final ArrayList<Pair<String, Object>> result = new ArrayList<>();
+		// Always show nominal if current index, even if not licensed otherwise we may get null pointer or array index exceptions.
+		if (currentIndex == -1 || (LicenseFeatures.isPermitted("features:nominals") && market.isNominal())) {
+			result.add(new Pair<String, Object>("Nominal", Integer.valueOf(-1)));
+		}
+		if (market.isEnabled()) {
+			for (int i = 0; i < market.getSpotCharterCount(); ++i) {
+				result.add(new Pair<String, Object>(String.format("%d", 1 + i), Integer.valueOf(i)));
+			}
+		}
+		if (currentIndex != -1 && (!market.isEnabled() || currentIndex >= market.getSpotCharterCount())) {
+			result.add(new Pair<String, Object>(String.format("%d", 1 + currentIndex), Integer.valueOf(currentIndex)));
+		}
+		return result;
+	}
+
+}
