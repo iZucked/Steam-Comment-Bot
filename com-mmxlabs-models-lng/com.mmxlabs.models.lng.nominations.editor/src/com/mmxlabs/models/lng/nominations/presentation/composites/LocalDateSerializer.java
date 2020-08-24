19ed3e88c3481ccd6b3ff034866797809c4ede24
Jenkins <hudson@minimaxlabs.com>
2020-08-24 13:51:21 +0000

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/presentation/composites/LocalDateSerializer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.nominations.editor/src/com/mmxlabs/models/lng/nominations/presentation/composites/LocalDateSerializer.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2020
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.nominations.presentation.composites;
 
 import java.io.IOException;
