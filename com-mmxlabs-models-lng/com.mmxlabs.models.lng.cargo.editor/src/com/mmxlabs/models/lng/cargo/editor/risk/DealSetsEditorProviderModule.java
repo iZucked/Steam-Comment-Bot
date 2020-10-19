2e4d9054976c9de7d568fd57c26b9f93fbaade79
Jenkins <hudson@minimaxlabs.com>
2020-10-19 14:20:53 +0000

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/editor/risk/DealSetsEditorProviderModule.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/editor/risk/DealSetsEditorProviderModule.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2020
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.cargo.editor.risk;
 
 import static org.ops4j.peaberry.Peaberry.service;
