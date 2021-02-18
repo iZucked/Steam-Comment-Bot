50f6eb9e29b02b1783e08ad84b9e312851b0eaf3
Jenkins <hudson@minimaxlabs.com>
2021-02-18 13:16:04 +0000

    Update headers

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/services/SelectionServiceUtils.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/services/SelectionServiceUtils.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2021
+ * All rights reserved.
+ */
 package com.mmxlabs.lingo.reports.services;
 
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
