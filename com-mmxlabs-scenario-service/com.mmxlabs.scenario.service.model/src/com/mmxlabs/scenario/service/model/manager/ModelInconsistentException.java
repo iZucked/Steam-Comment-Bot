4b9641ec4d9a927bd82f58d48cccff644933b67d
Jenkins <hudson@minimaxlabs.com>
2021-06-09 14:39:36 +0000

    Update headers

--- com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.model/src/com/mmxlabs/scenario/service/model/manager/ModelInconsistentException.java
+++ com-mmxlabs-scenario-service/com.mmxlabs.scenario.service.model/src/com/mmxlabs/scenario/service/model/manager/ModelInconsistentException.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2021
+ * All rights reserved.
+ */
 package com.mmxlabs.scenario.service.model.manager;
 
 public class ModelInconsistentException extends RuntimeException {
