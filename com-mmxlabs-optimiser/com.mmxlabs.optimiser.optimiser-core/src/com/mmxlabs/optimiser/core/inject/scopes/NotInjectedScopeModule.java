e052a2270f129bab36dda577f2f7e27ed8720d63
Simon Goodall <simon.goodall@minimaxlabs.com>
2021-10-08 12:16:19 +0100

    Added scope to throw error if a class is used in injector.

---
+++ com-mmxlabs-optimiser/com.mmxlabs.optimiser.optimiser-core/src/com/mmxlabs/optimiser/core/inject/scopes/NotInjectedScopeModule.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2021
+ * All rights reserved.
+ */
+package com.mmxlabs.optimiser.core.inject.scopes;
+
+import com.google.inject.AbstractModule;
+
+public class NotInjectedScopeModule extends AbstractModule {
+	@Override
+	public void configure() {
+		NotInjectedScopeImpl impl = new NotInjectedScopeImpl();
+
+		// tell Guice about the scope
+		bindScope(NotInjectedScope.class, impl);
+
+		// make our scope instance injectable
+		bind(NotInjectedScopeImpl.class).toInstance(impl);
+	}
+}
