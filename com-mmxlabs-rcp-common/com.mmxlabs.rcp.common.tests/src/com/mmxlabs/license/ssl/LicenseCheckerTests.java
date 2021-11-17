42020e650d2132b3890b1ce47eb89b3d6210609f
Philippe Partarrieu <ppartarrieu@minimaxlabs.com>
2021-11-17 12:23:58 +0100

    Squashed commit of the following:
    commit 7e98ac1e7b98c12f2c2ddf184d85f9ad635d72cc
    Author: Philippe Partarrieu <ppartarrieu@minimaxlabs.com>
    Date:   Mon Nov 15 17:14:33 2021 +0100
    
        add feature for license mgmt
    
    commit e692beac3d0f007438d32b10a73da4f7bd478504
    Author: Simon Goodall <simon.goodall@minimaxlabs.com>
    Date:   Thu Jul 8 12:12:21 2021 +0100
    
        Re-jig Application logic flow to ensure workspace is locked early.
    
    commit 4bc37665d981b92dc070eebf2a40e748a23097b3
    Author: Philippe Partarrieu <ppartarrieu@minimaxlabs.com>
    Date:   Mon Sep 20 18:03:31 2021 +0200
    
        add license checker unit tests
    
    commit 2dc171dcf55e36c49d8a3a2ecaf7329fba7cbb42
    Author: Philippe Partarrieu <ppartarrieu@minimaxlabs.com>
    Date:   Wed May 26 08:27:12 2021 +0000
    
        Get license from DataHub

--- com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/license/ssl/LicenseCheckerTests.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/license/ssl/LicenseCheckerTests.java
@@ -5,16 +5,30 @@
 package com.mmxlabs.license.ssl;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class LicenseCheckerTests {
+import com.mmxlabs.rcp.common.tests.internal.Activator;
+
+class LicenseCheckerTests {
+
+	private static final Logger log = LoggerFactory.getLogger(LicenseCheckerTests.class);
 
 	@Test
-	public void testLoadingFromURIWithSpaces() throws Exception {
+	void loadFromURIWithSpaces() throws Exception {
 
 		Path tempDir = Files.createTempDirectory("lingo test spaces");
 		try {
@@ -26,7 +40,71 @@ public class LicenseCheckerTests {
 		} finally {
 			Files.delete(tempDir);
 		}
+	}
 
-		// LicenseChecker.
+	@Test
+	void importExtraCertsFromHome() throws CertificateException, KeyStoreException {
+		// TODO use mock keystore instead
+		final File file = new File("license.p12");
+		if (file.exists()) {
+			try (InputStream inStream = new FileInputStream(file)) {
+				final KeyStore keystore = KeyStore.getInstance("PKCS12");
+				keystore.load(inStream, "helloworld".toCharArray());
+
+				Assertions.assertNull(keystore.getCertificate("lingo"));
+				LicenseChecker.importExtraCertsFromHome(keystore);
+
+				Assertions.assertNotNull(keystore.getCertificate("cert.pem"));
+			} catch (NoSuchAlgorithmException | IOException e) {
+				log.error("failed to load test license: " + e.getMessage());
+			}
+		}
+	}
+
+	@Test
+	void licenseFromSystemProperty() throws CertificateException {
+		KeyStore keystore = LicenseChecker.getLicenseFromSystemProperty();
+		Assertions.assertNotNull(keystore);
+	}
+
+	@Test
+	void licenseFromEclipseHome() throws CertificateException {
+		KeyStore keystore = LicenseChecker.getEclipseHomeLicense();
+		Assertions.assertNotNull(keystore);
+	}
+
+	@Test
+	void licenseFromUserData() {
+		KeyStore keystore = LicenseChecker.getUserDataLicense();
+		Assertions.assertNotNull(keystore);
+	}
+
+	@Test
+	void createKeystoreCopies() {
+		// TODO use mock keystore instead
+		Mockito.mock(KeyStore.class);
+		final File licenseFile = new File("license.p12");
+		final File keystoreFile = new File("keystore.p12");
+		if (licenseFile.exists() && keystoreFile.exists()) {
+			try (InputStream inStreamLic = new FileInputStream(licenseFile); InputStream inStreamKey = new FileInputStream(keystoreFile)) {
+				final KeyStore license = KeyStore.getInstance("PKCS12");
+				license.load(inStreamLic, "helloworld".toCharArray());
+
+				final KeyStore keystore = KeyStore.getInstance("PKCS12");
+				keystore.load(inStreamKey, "helloworld".toCharArray());
+
+				final File keyStoreFile = Activator.getDefault().getBundle().getDataFile("local-keystore.jks");
+				final File trustStoreFile = Activator.getDefault().getBundle().getDataFile("local-truststore.jks");
+				Assertions.assertNull(keyStoreFile);
+				Assertions.assertNull(trustStoreFile);
+
+				LicenseChecker.createKeystoreCopies(keystore, license);
+
+				Assertions.assertNotNull(keyStoreFile);
+				Assertions.assertNotNull(trustStoreFile);
+			} catch (NoSuchAlgorithmException | IOException | CertificateException | KeyStoreException e) {
+				log.error("failed to load test license: " + e.getMessage());
+			}
+		}
 	}
 }
