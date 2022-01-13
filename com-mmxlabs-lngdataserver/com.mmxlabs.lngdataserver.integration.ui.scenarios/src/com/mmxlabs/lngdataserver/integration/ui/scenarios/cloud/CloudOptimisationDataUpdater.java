f942d3418080d54e7e0c1ea8575223eebce55db6
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-01-13 09:54:41 +0000

    Persist task state after submittion

--- com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.integration.ui.scenarios/src/com/mmxlabs/lngdataserver/integration/ui/scenarios/cloud/CloudOptimisationDataUpdater.java
+++ com-mmxlabs-lngdataserver/com.mmxlabs.lngdataserver.integration.ui.scenarios/src/com/mmxlabs/lngdataserver/integration/ui/scenarios/cloud/CloudOptimisationDataUpdater.java
@@ -348,6 +348,9 @@ public class CloudOptimisationDataUpdater {
 				.addAll(currentRecords) //
 				.add(r) //
 				.build();
+		
+		saveAndUpdateCurrentRecords(currentRecords);
+		
 		readyCallback.accept(r);
 
 	}
