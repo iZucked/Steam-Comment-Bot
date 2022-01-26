a4f04c92585cf3364cdb1fcba427d1e033fa9086
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-01-26 18:41:57 +0000

    Sonar clean up

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/inlineeditors/GroupedSlotsDialog.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/inlineeditors/GroupedSlotsDialog.java
@@ -2,7 +2,6 @@ package com.mmxlabs.models.lng.cargo.ui.inlineeditors;
 
 import org.eclipse.core.databinding.ObservablesManager;
 import org.eclipse.emf.common.command.CompoundCommand;
-import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.databinding.EMFDataBindingContext;
 import org.eclipse.emf.ecore.EObject;
@@ -40,20 +39,17 @@ import com.mmxlabs.models.ui.tabular.manipulators.NumericAttributeManipulator;
 import com.mmxlabs.models.ui.valueproviders.IReferenceValueProvider;
 import com.mmxlabs.rcp.common.CommonImages;
 import com.mmxlabs.rcp.common.CommonImages.IconPaths;
-import com.mmxlabs.scenario.service.model.manager.ModelReference;
 
 public class GroupedSlotsDialog extends FormDialog {
 
 	private EObjectTableViewer tableViewer;
-	private AdapterFactory adapterFactory;
-	private ModelReference modelReference;
 	private EObject container;
 	private EReference containment;
-	
+
 	private final IScenarioEditingLocation scenarioEditingLocation;
 	private ObservablesManager mgr;
 	private EMFDataBindingContext dbc;
-	
+
 	public GroupedSlotsDialog(@NonNull final IShellProvider parentShell, @NonNull final IScenarioEditingLocation originalScenarioEditingLocation) {
 		super(parentShell);
 		this.scenarioEditingLocation = originalScenarioEditingLocation;
@@ -66,84 +62,86 @@ public class GroupedSlotsDialog extends FormDialog {
 
 	private void createFormArea(final IManagedForm mform) {
 		final FormToolkit toolkit = mform.getToolkit();
-		
+
 		final Composite body = mform.getForm().getBody();
 		body.setLayout(new GridLayout(1, false));
 		final Composite mainPane = toolkit.createComposite(body);
 		mainPane.setLayout(new GridLayout(1, false));
 		mainPane.setLayoutData(new GridData(GridData.FILL_BOTH));
-		{
-			final Group template = new Group(mainPane, SWT.NONE);
-			template.setLayout(new GridLayout(2, false));
-			template.setLayoutData(new GridData(GridData.FILL_BOTH));
-			
-			template.setText("Constraints");
-			final DetailToolbarManager buttonManager = new DetailToolbarManager(template, SWT.TOP);
 
-			tableViewer = new EObjectTableViewer(template, SWT.FULL_SELECTION | SWT.BORDER);
-			tableViewer.init(scenarioEditingLocation.getAdapterFactory(), scenarioEditingLocation.getModelReference(), CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS);
-				
-			final Grid table = tableViewer.getGrid();
-			
-			table.setHeaderVisible(true);
-			table.setLinesVisible(true);
-			table.setRowHeaderVisible(true);
+		final Group template = new Group(mainPane, SWT.NONE);
+		template.setLayout(new GridLayout(2, false));
+		template.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-			final Action addConstraintGroup = new Action("Add") {
-				@Override
-				public void run() {
-					final CompoundCommand cmd = new CompoundCommand("Add constraint row");
-					final GroupedDischargeSlotsConstraint newConstraint = CargoFactory.eINSTANCE.createGroupedDischargeSlotsConstraint();
-					cmd.append(AddCommand.create(scenarioEditingLocation.getEditingDomain(), ((LNGScenarioModel) scenarioEditingLocation.getRootObject()).getCargoModel(), CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS, newConstraint));
-					if (!cmd.isEmpty()) {
+		template.setText("Constraints");
+		final DetailToolbarManager buttonManager = new DetailToolbarManager(template, SWT.TOP);
+
+		tableViewer = new EObjectTableViewer(template, SWT.FULL_SELECTION | SWT.BORDER);
+		tableViewer.init(scenarioEditingLocation.getAdapterFactory(), scenarioEditingLocation.getModelReference(), CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS);
+
+		final Grid table = tableViewer.getGrid();
+
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		table.setRowHeaderVisible(true);
+
+		final Action addConstraintGroup = new Action("Add") {
+			@Override
+			public void run() {
+				final CompoundCommand cmd = new CompoundCommand("Add constraint row");
+				final GroupedDischargeSlotsConstraint newConstraint = CargoFactory.eINSTANCE.createGroupedDischargeSlotsConstraint();
+				cmd.append(AddCommand.create(scenarioEditingLocation.getEditingDomain(), ((LNGScenarioModel) scenarioEditingLocation.getRootObject()).getCargoModel(),
+						CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS, newConstraint));
+				if (!cmd.isEmpty()) {
+					final EditingDomain ed = scenarioEditingLocation.getEditingDomain();
+					ed.getCommandStack().execute(cmd);
+				}
+			}
+		};
+		CommonImages.setImageDescriptors(addConstraintGroup, IconPaths.Plus);
+		buttonManager.getToolbarManager().add(addConstraintGroup);
+
+		final Action deleteRowsAction = new Action("Delete group constraints") {
+			@Override
+			public void run() {
+				if (tableViewer != null) {
+					final ISelection selection = tableViewer.getSelection();
+					if (selection.isEmpty()) {
+						return;
+					}
+					if (selection instanceof IStructuredSelection iStructuredSelection) {
+						final CompoundCommand cc = new CompoundCommand("Delete group constraints");
 						final EditingDomain ed = scenarioEditingLocation.getEditingDomain();
-						ed.getCommandStack().execute(cmd);
+						final EObject firstLine = (EObject) iStructuredSelection.getFirstElement();
+						cc.append(RemoveCommand.create(ed, firstLine.eContainer(), firstLine.eContainingFeature(), iStructuredSelection.toList()));
+						ed.getCommandStack().execute(cc);
 					}
 				}
-			};
-			CommonImages.setImageDescriptors(addConstraintGroup, IconPaths.Plus);
-			buttonManager.getToolbarManager().add(addConstraintGroup);
+			}
+		};
+		CommonImages.setImageDescriptors(deleteRowsAction, IconPaths.Delete);
+		deleteRowsAction.setEnabled(false);
+		buttonManager.getToolbarManager().add(deleteRowsAction);
 
-			final Action deleteRowsAction = new Action("Delete group constraints") {
-				@Override
-				public void run() {
-					if (tableViewer != null) {
-						final ISelection selection = tableViewer.getSelection();
-						if (selection.isEmpty()) {
-							return;
-						}
-						if (selection instanceof IStructuredSelection iStructuredSelection) {
-							final CompoundCommand cc = new CompoundCommand("Delete group constraints");
-							final EditingDomain ed = scenarioEditingLocation.getEditingDomain();
-							final EObject firstLine = (EObject) iStructuredSelection.getFirstElement();
-							cc.append(RemoveCommand.create(ed, firstLine.eContainer(), firstLine.eContainingFeature(), iStructuredSelection.toList()));
-							ed.getCommandStack().execute(cc);
-						}
-					}
-				}
-			};
-			CommonImages.setImageDescriptors(deleteRowsAction, IconPaths.Delete);
-			deleteRowsAction.setEnabled(false);
-			buttonManager.getToolbarManager().add(deleteRowsAction);
+		final ICommandHandler commandHandler = scenarioEditingLocation.getDefaultCommandHandler();
 
-			final ICommandHandler commandHandler = scenarioEditingLocation.getDefaultCommandHandler();
+		// Use specific (GroupedDischargeSlotsConstraint) reference value provider
+		final IReferenceValueProvider dischargeSlotProvider = commandHandler.getReferenceValueProviderProvider().getReferenceValueProvider(CargoPackage.eINSTANCE.getGroupedDischargeSlotsConstraint(),
+				CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots());
+		final MultipleReferenceManipulator slotsManipulator = new MultipleReferenceManipulator(CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots(), commandHandler, dischargeSlotProvider,
+				MMXCorePackage.eINSTANCE.getNamedObject_Name());
+		tableViewer.addTypicalColumn("Slots", slotsManipulator);
+		tableViewer.addTypicalColumn("Minimum Required", new NumericAttributeManipulator(CargoPackage.eINSTANCE.getGroupedSlotsConstraint_MinimumBound(), commandHandler));
 
-			// Use specific (GroupedDischargeSlotsConstraint) reference value provider
-			final IReferenceValueProvider dischargeSlotProvider = commandHandler.getReferenceValueProviderProvider().getReferenceValueProvider(CargoPackage.eINSTANCE.getGroupedDischargeSlotsConstraint(), CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots());
-			final MultipleReferenceManipulator slotsManipulator = new MultipleReferenceManipulator(CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots(), commandHandler, dischargeSlotProvider, MMXCorePackage.eINSTANCE.getNamedObject_Name());
-			tableViewer.addTypicalColumn("Slots", slotsManipulator);
-			tableViewer.addTypicalColumn("Minimum Required", new NumericAttributeManipulator(CargoPackage.eINSTANCE.getGroupedSlotsConstraint_MinimumBound(), commandHandler));
-			
-			tableViewer.setInput(((LNGScenarioModel) scenarioEditingLocation.getRootObject()).getCargoModel());
-			tableViewer.refresh();
-			table.setLayoutData(new GridData(GridData.FILL_BOTH));
+		tableViewer.setInput(((LNGScenarioModel) scenarioEditingLocation.getRootObject()).getCargoModel());
+		tableViewer.refresh();
+		table.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-			buttonManager.getToolbarManager().update(true);
-			tableViewer.addSelectionChangedListener(event -> {
-				final ISelection iSelection = event.getSelection();
-				deleteRowsAction.setEnabled(!iSelection.isEmpty());
-			});
-		}
+		buttonManager.getToolbarManager().update(true);
+		tableViewer.addSelectionChangedListener(event -> {
+			final ISelection iSelection = event.getSelection();
+			deleteRowsAction.setEnabled(!iSelection.isEmpty());
+		});
 	}
 
 	public EList<GroupedDischargeSlotsConstraint> getResult() {
@@ -166,16 +164,11 @@ public class GroupedSlotsDialog extends FormDialog {
 		String title = "Edit grouped discharge slot constraints";
 		mForm.getForm().setText(title);
 		mForm.getToolkit().decorateFormHeading(mForm.getForm().getForm());
-		
+
 		mgr = new ObservablesManager();
 		dbc = new EMFDataBindingContext();
-		
-		mgr.runAndCollect(new Runnable() {
-			@Override
-			public void run() {
-				createFormArea(mForm);
-			}
-		});
+
+		mgr.runAndCollect(() -> createFormArea(mForm));
 	}
 
 	@Override
