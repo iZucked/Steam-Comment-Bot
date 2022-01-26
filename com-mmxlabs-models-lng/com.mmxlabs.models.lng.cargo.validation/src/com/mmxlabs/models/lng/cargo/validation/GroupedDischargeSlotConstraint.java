239b657276822667deb51ed6d7aa7eb6453a76d7
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-01-26 15:46:16 +0000

    Switch to doValidate

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/GroupedDischargeSlotConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.validation/src/com/mmxlabs/models/lng/cargo/validation/GroupedDischargeSlotConstraint.java
@@ -9,12 +9,12 @@ import java.util.List;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.jdt.annotation.NonNull;
 
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.GroupedSlotsConstraint;
 import com.mmxlabs.models.lng.cargo.Slot;
-import com.mmxlabs.models.lng.cargo.validation.internal.Activator;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusFactory;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
@@ -22,47 +22,44 @@ import com.mmxlabs.models.ui.validation.IExtraValidationContext;
 public class GroupedDischargeSlotConstraint extends AbstractModelMultiConstraint {
 
 	@Override
-	public String validate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> statuses) {
+	protected void doValidate(@NonNull IValidationContext ctx, @NonNull IExtraValidationContext extraContext, @NonNull List<IStatus> statuses) {
 		final EObject object = ctx.getTarget();
-
 		if (!(extraContext.getContainer(object) instanceof CargoModel)) {
-			return Activator.PLUGIN_ID;
+			return;
 		}
-		if (object instanceof GroupedSlotsConstraint) {
-			final GroupedSlotsConstraint groupConstraint = (GroupedSlotsConstraint) object;
+		if (object instanceof GroupedSlotsConstraint groupedSlotsConstraint) {
 			final DetailConstraintStatusFactory factory = DetailConstraintStatusFactory.makeStatus().withName("Grouped Constraint");
 
-			final List<Slot> dischargeSlotGroup = groupConstraint.getSlots();
-			final int bound = groupConstraint.getMinimumBound();
+			final List<Slot> dischargeSlotGroup = groupedSlotsConstraint.getSlots();
+			final int bound = groupedSlotsConstraint.getMinimumBound();
 			if (dischargeSlotGroup.isEmpty()) {
-				factory.copyName().withMessage("Slot group must be non-empty.").withObjectAndFeature(groupConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx,
+				factory.copyName().withMessage("Slot group must be non-empty.").withObjectAndFeature(groupedSlotsConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx,
 						statuses);
 			} else {
 				final boolean dischargeSlotGroupBoundValid = dischargeSlotGroup.size() >= bound;
 				if (!dischargeSlotGroupBoundValid) {
 					factory.copyName().withMessage("Discharge slot group must have at least minimum number.")
-							.withObjectAndFeature(groupConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
+							.withObjectAndFeature(groupedSlotsConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
 				}
 				final boolean dischargeSlotGroupAllOptional = dischargeSlotGroup.stream().allMatch(Slot::isOptional);
 				if (!dischargeSlotGroupAllOptional) {
 					factory.copyName().withMessage("All discharge slots in group must be optional.")
-							.withObjectAndFeature(groupConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
+							.withObjectAndFeature(groupedSlotsConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
 				}
 				if (dischargeSlotGroupBoundValid && dischargeSlotGroupAllOptional) {
 					final long boundTruncatedDischargeHasCargoCount = dischargeSlotGroup.stream().unordered().filter(s -> s.getCargo() != null).limit(bound).count();
+					// Check if less than bound. Greater or equal means constraint is satisfied
 					if (boundTruncatedDischargeHasCargoCount < bound) {
 						factory.copyName().withMessage("Bound constraint should be satisfied by basecase")
-								.withObjectAndFeature(groupConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
+								.withObjectAndFeature(groupedSlotsConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx, statuses);
 					}
 				}
 			}
 
 			if (bound == 0) {
-				factory.copyName().withMessage("Minimum bound must be positive.").withObjectAndFeature(groupConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx,
+				factory.copyName().withMessage("Minimum bound must be positive.").withObjectAndFeature(groupedSlotsConstraint, CargoPackage.eINSTANCE.getGroupedSlotsConstraint_Slots()).make(ctx,
 						statuses);
 			}
 		}
-
-		return Activator.PLUGIN_ID;
 	}
 }
