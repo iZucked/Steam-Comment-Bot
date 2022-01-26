4b1018402179d1c900f75fea08150e782428db1e
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-01-26 17:57:56 +0000

    Add existing elements to filtered list.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/valueproviders/GroupedDischargeSlotsConstraintsSlotsValueProviderFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/valueproviders/GroupedDischargeSlotsConstraintsSlotsValueProviderFactory.java
@@ -1,7 +1,9 @@
 package com.mmxlabs.models.lng.cargo.ui.valueproviders;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EClass;
@@ -13,6 +15,7 @@ import org.eclipse.emf.ecore.EcorePackage;
 import com.mmxlabs.common.Pair;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.DischargeSlot;
+import com.mmxlabs.models.lng.cargo.GroupedDischargeSlotsConstraint;
 import com.mmxlabs.models.lng.cargo.SpotSlot;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.Activator;
@@ -54,14 +57,18 @@ public class GroupedDischargeSlotsConstraintsSlotsValueProviderFactory implement
 				@Override
 				public List<Pair<String, EObject>> getAllowedValues(EObject target, EStructuralFeature field) {
 					final List<Pair<String, EObject>> delegateValue = delegateFactory.getAllowedValues(target, field);
-					final ArrayList<Pair<String, EObject>> filteredList = new ArrayList<>();
-					for (final Pair<String, EObject> value : delegateValue) {
-						final EObject valueEObject = value.getSecond();
-						if (valueEObject instanceof DischargeSlot && !(valueEObject instanceof SpotSlot)) {
-							filteredList.add(value);
+					if (target instanceof GroupedDischargeSlotsConstraint groupedDischageSlotsConstraint) {
+						final Set<DischargeSlot> selectedSlots = new HashSet<>(groupedDischageSlotsConstraint.getSlots());
+						final ArrayList<Pair<String, EObject>> filteredList = new ArrayList<>();
+						for (final Pair<String, EObject> value : delegateValue) {
+							final EObject valueEObject = value.getSecond();
+							if (valueEObject instanceof DischargeSlot dischargeSlot && (!(dischargeSlot instanceof SpotSlot) || selectedSlots.contains(dischargeSlot))) {
+								filteredList.add(value);
+							}
 						}
+						return filteredList;
 					}
-					return filteredList;
+					return delegateValue;
 				}
 
 				@Override
