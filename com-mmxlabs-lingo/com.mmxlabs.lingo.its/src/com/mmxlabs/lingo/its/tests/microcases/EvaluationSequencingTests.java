629d94c7ce4393a4a2265069a0b9739c006ec089
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-02-04 16:18:33 +0000

    Added sequence hint test that forces ordering.

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/EvaluationSequencingTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/tests/microcases/EvaluationSequencingTests.java
@@ -108,13 +108,13 @@ public class EvaluationSequencingTests extends AbstractMicroTestCase {
 			if (event instanceof StartEvent || event instanceof Idle || event instanceof Journey || event instanceof EndEvent) {
 				continue;
 			}
-			if (event instanceof VesselEventVisit vev) {
+			if (event instanceof final VesselEventVisit vev) {
 				Assertions.assertEquals(maintenanceEvent, vev.getVesselEvent());
 				Assertions.assertFalse(seenLoad);
 				Assertions.assertFalse(seenDischarge);
 				Assertions.assertFalse(seenMaintenance);
 				seenMaintenance = true;
-			} else if (event instanceof SlotVisit sv) {
+			} else if (event instanceof final SlotVisit sv) {
 				final Slot<?> slot = sv.getSlotAllocation().getSlot();
 				if (slot instanceof final LoadSlot loadSlot) {
 					Assertions.assertEquals(cargo.getSlots().get(0), loadSlot);
@@ -140,4 +140,104 @@ public class EvaluationSequencingTests extends AbstractMicroTestCase {
 		Assertions.assertTrue(seenDischarge);
 	}
 
+	/**
+	 * Based on FB6402
+	 * 
+	 * This test checks that the evaluator respects provided sequence hints. testOverlappingCargoAndEventOrderingCustomCargoDuration() should prove (when passing) that the maintenance event should
+	 * precede the cargo. This test forces the maintenance event to follow the cargo by setting sequence hints.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	@Tag(TestCategories.MICRO_TEST)
+	public void testOverlappingCargoAndEventOrderingCustomCargoDurationForceOppositeSequenceHint() {
+		final Port loadPort = portFinder.findPortById(InternalDataConstants.PORT_BARROW_ISLAND);
+		final Port dischargePort = portFinder.findPortById(InternalDataConstants.PORT_TOKYO_BAY);
+		final Port maintenancePort = portFinder.findPortById(InternalDataConstants.PORT_GWANGYANG);
+
+		final Vessel vessel = fleetModelFinder.findVessel(InternalDataConstants.REF_VESSEL_TFDE_160);
+
+		final VesselAvailability va = cargoModelBuilder.makeVesselAvailability(vessel, entity) //
+				.withCharterRate("10000") //
+				.withStartWindow(LocalDateTime.of(2022, 4, 24, 0, 0)) //
+				.withEndWindow(LocalDateTime.of(2022, 7, 31, 15, 0)) //
+				.withStartHeel(2_000, 7_000, 23, "") //
+				.withEndHeel(500, 500, EVesselTankState.MUST_BE_COLD, false) //
+				.build();
+
+		final LocalDateTime mStartAfter = LocalDateTime.of(2022, 5, 1, 0, 0);
+		final LocalDateTime mStartBy = LocalDateTime.of(2022, 6, 15, 0, 0);
+		final MaintenanceEvent maintenanceEvent = cargoModelBuilder.makeMaintenanceEvent("MaintenanceEvent", mStartAfter, mStartBy, maintenancePort) //
+				.withDurationInDays(5) //
+				.withVesselAssignment(va, 2) //
+				.build();
+
+		final Cargo cargo = cargoModelBuilder.makeCargo() //
+				.makeFOBPurchase("Load", LocalDate.of(2022, 5, 29), loadPort, null, entity, "9.5") //
+				.withVolumeLimits(125_000, 174_000, VolumeUnits.M3) //
+				.withWindowStartTime(18) //
+				.withWindowSize(0, TimePeriod.DAYS) //
+				.withVisitDuration(30) //
+				.build() //
+				.makeDESSale("Discharge", LocalDate.of(2022, 6, 1), dischargePort, null, entity, "10.26") //
+				.withVolumeLimits(130_000, 180_000, VolumeUnits.M3) //
+				.withWindowStartTime(0).withWindowSize(1, TimePeriod.MONTHS) //
+				.withVisitDuration(144) //
+				.build() //
+				.withVesselAssignment(va, 1) //
+				.build();
+
+		Assertions.assertEquals(2, maintenanceEvent.getSequenceHint());
+		Assertions.assertEquals(1, cargo.getSequenceHint());
+
+		evaluateTest();
+		final Schedule schedule = ScenarioModelUtil.findSchedule(scenarioDataProvider);
+
+		Assertions.assertNotNull(schedule);
+
+		// Maintenance should come before cargo.
+		Assertions.assertEquals(2, maintenanceEvent.getSequenceHint());
+		Assertions.assertEquals(1, cargo.getSequenceHint());
+
+		// Double check event sequence agrees with sequence hint
+		boolean seenMaintenance = false;
+		boolean seenLoad = false;
+		boolean seenDischarge = false;
+
+		Assertions.assertEquals(1, schedule.getSequences().size());
+		for (final Event event : schedule.getSequences().get(0).getEvents()) {
+			if (event instanceof StartEvent || event instanceof Idle || event instanceof Journey || event instanceof EndEvent) {
+				continue;
+			}
+			if (event instanceof final VesselEventVisit vev) {
+				Assertions.assertEquals(maintenanceEvent, vev.getVesselEvent());
+				Assertions.assertTrue(seenLoad);
+				Assertions.assertTrue(seenDischarge);
+				Assertions.assertFalse(seenMaintenance);
+				seenMaintenance = true;
+			} else if (event instanceof final SlotVisit sv) {
+				final Slot<?> slot = sv.getSlotAllocation().getSlot();
+				if (slot instanceof final LoadSlot loadSlot) {
+					Assertions.assertEquals(cargo.getSlots().get(0), loadSlot);
+					Assertions.assertFalse(seenMaintenance);
+					Assertions.assertFalse(seenLoad);
+					Assertions.assertFalse(seenDischarge);
+					seenLoad = true;
+				} else {
+					final DischargeSlot dischargeSlot = (DischargeSlot) slot;
+					Assertions.assertEquals(cargo.getSlots().get(1), dischargeSlot);
+					Assertions.assertFalse(seenMaintenance);
+					Assertions.assertTrue(seenLoad);
+					Assertions.assertFalse(seenDischarge);
+					seenDischarge = true;
+				}
+			} else {
+				// Unexpected event
+				Assertions.fail();
+			}
+		}
+		Assertions.assertTrue(seenMaintenance);
+		Assertions.assertTrue(seenLoad);
+		Assertions.assertTrue(seenDischarge);
+	}
 }
