e9904a401a156a2462c7e48d56757a3cce584245
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-02-04 11:31:29 +0000

    Implement default methods on interface to avoid need for default methods on subclass.
    TODO: Remove need for BaseComponentHelper.

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/IComponentHelper.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/IComponentHelper.java
@@ -4,6 +4,7 @@
  */
 package com.mmxlabs.models.ui;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
@@ -21,12 +22,12 @@ public interface IComponentHelper {
 	/**
 	 * The default method
 	 */
-	public void addEditorsToComposite(IInlineEditorContainer detailComposite);
+	void addEditorsToComposite(IInlineEditorContainer detailComposite);
 	
 	/**
 	 * Called when a composite is being filled with editors for a particular type.
 	 */
-	public void addEditorsToComposite(IInlineEditorContainer detailComposite, final EClass displayedClass);
+	 void addEditorsToComposite(IInlineEditorContainer detailComposite, final EClass displayedClass);
 	
 	/**
 	 * Get the external editing range for things produced by this CH.
@@ -38,7 +39,11 @@ public interface IComponentHelper {
 	 * @param value
 	 * @return
 	 */
-	public List<EObject> getExternalEditingRange(final MMXRootObject root, final EObject value);
+	default List<EObject> getExternalEditingRange(final MMXRootObject root, final EObject value) {
+		return Collections.emptyList();
+	}
 	
-	public int getDisplayPriority();
+	default int getDisplayPriority() {
+		return 0;
+	}
 }
