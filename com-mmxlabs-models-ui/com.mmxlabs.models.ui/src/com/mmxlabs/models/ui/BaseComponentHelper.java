12a4d8b3876fa6e883493034a142c7adabfbbaf7
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-02-04 11:31:29 +0000

    Implement default methods on interface to avoid need for default methods on subclass.
    TODO: Remove need for BaseComponentHelper.

--- com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/BaseComponentHelper.java
+++ com-mmxlabs-models-ui/com.mmxlabs.models.ui/src/com/mmxlabs/models/ui/BaseComponentHelper.java
@@ -4,23 +4,6 @@
  */
 package com.mmxlabs.models.ui;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.eclipse.emf.ecore.EObject;
-
-import com.mmxlabs.models.mmxcore.MMXRootObject;
-
 public abstract class BaseComponentHelper implements IComponentHelper {
 
-	@Override
-	public List<EObject> getExternalEditingRange(MMXRootObject root,
-			EObject value) {
-		return Collections.emptyList();
-	}
-
-	@Override
-	public int getDisplayPriority() {
-		return 0;
-	}
 }
