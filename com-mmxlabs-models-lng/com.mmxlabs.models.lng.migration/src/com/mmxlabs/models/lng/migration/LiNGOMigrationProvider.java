757b8e85d02ce1d1244ecf9152c8bf12e0b38eb5
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-02-09 13:22:20 +0000

    Add missing 153 migration enablement

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
@@ -24,7 +24,7 @@ public class LiNGOMigrationProvider implements IMigrationProvider {
 	 * reflective class loader to work.
 	 */
 
-	private static final int CURRENT_VERSION = 152;
+	private static final int CURRENT_VERSION = 153;
 
 	@Override
 	public int priority() {
