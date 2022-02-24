78371bbdf8a1a22cf22d5c09cda1f91e045d0c37
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-02-24 14:52:13 +0000

    Layout factory cleanup

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/presentation/composites/DistributionModelTopLevelCompositeFactory.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/presentation/composites/DistributionModelTopLevelCompositeFactory.java
@@ -6,19 +6,14 @@ package com.mmxlabs.models.lng.adp.presentation.composites;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Layout;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
-import com.mmxlabs.models.lng.adp.ADPPackage;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.editors.IDisplayComposite;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
@@ -38,54 +33,9 @@ public class DistributionModelTopLevelCompositeFactory extends DefaultDisplayCom
 			public Layout createDetailLayout(@NonNull MMXRootObject root, EObject value) {
 				return GridLayoutFactory.swtDefaults().numColumns(3).create();
 			}
-//			@Override
-//			public boolean showLabelFor(@NonNull MMXRootObject root, EObject value, @NonNull IInlineEditor editor) {
-//				
-//				final EStructuralFeature feature = editor.getFeature();
-//				if (feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_UNIT) {
-//					return false;
-//				}
-//				return super.showLabelFor(root, value, editor);
-//			}
-//			@Override
-//			public Object createLabelLayoutData(@NonNull MMXRootObject root, EObject value, @NonNull IInlineEditor editor, Control control, Label label) {
-//
-//				final EStructuralFeature feature = editor.getFeature();
-//				if (feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_UNIT) {
-//					return GridDataFactory.swtDefaults().exclude(true).create();
-//				}
-//				return super.createLabelLayoutData(root, value, editor, control, label);
-//			}
 
 			@Override
 			public Object createEditorLayoutData(final MMXRootObject root, final EObject value, final IInlineEditor editor, final Control control) {
-
-//				final EStructuralFeature feature = editor.getFeature();
-//				if (feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_PER_CARGO || feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_PER_CARGO_MIN) { // || feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_UNIT) {
-//					final GridData gd = GridDataFactory.swtDefaults().hint(80, SWT.DEFAULT).create();
-//					// 64 - magic constant from MultiDetailDialog
-//					// gd.widthHint = 80;
-//
-//					// FIXME: Hack pending proper APi to manipulate labels
-//					final Label label = editor.getLabel();
-//					if (feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_PER_CARGO_MIN) {
-//						if (label != null) {
-//							label.setText("Volume");
-//						}
-//						// Disables text overwrite later
-//						editor.setLabel(null);
-//					} else if (feature == ADPPackage.Literals.DISTRIBUTION_MODEL__VOLUME_PER_CARGO){
-//						editor.setLabel(null);
-//					}
-//					else {
-//						if (label != null) {
-//							// Exclude from layout
-////							label.setLayoutData(GridDataFactory.swtDefaults().exclude(true).create());
-//						}
-//						editor.setLabel(null);
-//					}
-//					return gd;
-//				}
 				return GridDataFactory.swtDefaults().span(2, 1).grab(true, false).create();
 			}
 		});
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.editor/src/com/mmxlabs/models/lng/spotmarkets/editor/displaycomposites/SpotMarketsDetailComposite.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.spotmarkets.editor/src/com/mmxlabs/models/lng/spotmarkets/editor/displaycomposites/SpotMarketsDetailComposite.java
@@ -25,6 +25,7 @@ import com.mmxlabs.models.ui.editors.IDisplayCompositeLayoutProvider;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
 import com.mmxlabs.models.ui.impl.DefaultDetailComposite;
 import com.mmxlabs.models.ui.impl.DefaultDisplayCompositeLayoutProvider;
+import com.mmxlabs.models.ui.impl.RowGroupDisplayCompositeLayoutProviderBuilder;
 
 /**
  * @author Simon Goodall
@@ -40,8 +41,8 @@ public class SpotMarketsDetailComposite extends DefaultDetailComposite {
 
 	public SpotMarketsDetailComposite(final Composite parent, final int style, final DetailGroup contractDetailGroup, final FormToolkit toolkit) {
 		super(parent, style, toolkit);
-
 		this.contractDetailGroup = contractDetailGroup;
+		layoutProvider = createLayoutProvider();
 	}
 
 	@Override
@@ -52,7 +53,8 @@ public class SpotMarketsDetailComposite extends DefaultDetailComposite {
 		// By default all elements are in the main tab
 		DetailGroup cdg = DetailGroup.GENERAL;
 
-		// Here the exceptions are listed for the elements which should go into the bottom
+		// Here the exceptions are listed for the elements which should go into the
+		// bottom
 		if (editor.getFeature() == SpotMarketsPackage.eINSTANCE.getSpotMarket_RestrictedContractsArePermissive() //
 				|| editor.getFeature() == SpotMarketsPackage.eINSTANCE.getSpotMarket_RestrictedPortsArePermissive() //
 				|| editor.getFeature() == SpotMarketsPackage.eINSTANCE.getSpotMarket_RestrictedVesselsArePermissive() //
@@ -78,103 +80,42 @@ public class SpotMarketsDetailComposite extends DefaultDetailComposite {
 
 	@Override
 	protected IDisplayCompositeLayoutProvider createLayoutProvider() {
-		return new DefaultDisplayCompositeLayoutProvider() {
+		if (contractDetailGroup == DetailGroup.GENERAL) {
 
-			@Override
-			public Layout createDetailLayout(final MMXRootObject root, final EObject value) {
-				return new GridLayout(6, false);
-			}
+			return new RowGroupDisplayCompositeLayoutProviderBuilder() //
+					.withRow() //
+					.withLabel("Volume") //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__MIN_QUANTITY, 80) //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__MAX_QUANTITY, 80) //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__VOLUME_LIMITS_UNIT) //
+					.makeRow() //
+					//
+					.make();
+		}
+		if (contractDetailGroup == DetailGroup.RESTRICTIONS) {
 
-			@Override
-			public Object createEditorLayoutData(final MMXRootObject root, final EObject value, final IInlineEditor editor, final Control control) {
-
-				// Special case for min/max volumes - ensure text box has enough width for around 7 digits.
-				// Note: Should really render the font to get width - this is ok on my system, but other systems (default font & size, resolution, dpi etc) could make this wrong
-				final EStructuralFeature feature = editor.getFeature();
-				if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__MAX_QUANTITY || feature == SpotMarketsPackage.Literals.SPOT_MARKET__MIN_QUANTITY
-						|| feature == SpotMarketsPackage.Literals.SPOT_MARKET__VOLUME_LIMITS_UNIT
-				// || feature == SpotMarketsPackage.Literals.SPOT_MARKET__OPERATIONAL_TOLERANCE
-				) {
-					final GridData gd = (GridData) super.createEditorLayoutData(root, value, editor, control);
-					// 64 - magic constant from MultiDetailDialog
-					if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__MAX_QUANTITY || feature == SpotMarketsPackage.Literals.SPOT_MARKET__MIN_QUANTITY) {
-						gd.widthHint = 80;
-					}
-
-					// FIXME: Hack pending proper APi to manipulate labels
-					if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__MIN_QUANTITY) {
-						final Label label = editor.getLabel();
-						if (label != null) {
-							label.setText("Volume");
-						}
-						editor.setLabel(null);
-					} else {
-						editor.setLabel(null);
-					}
-
-					return gd;
-				}
-				
-				if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_CONTRACTS 
-						|| feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_CONTRACTS_ARE_PERMISSIVE) {
-					final GridData gd = (GridData) super.createEditorLayoutData(root, value, editor, control);
-					// 64 - magic constant from MultiDetailDialog
-					gd.horizontalSpan = 2;
-					// FIXME: Hack pending proper APi to manipulate labels
-					if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_CONTRACTS) {
-						
-						final Label label = editor.getLabel();
-						if (label != null) {
-							label.setText("Contracts");
-						}
-						editor.setLabel(null);
-					} else {
-						editor.setLabel(null);
-					}
-
-					return gd;
-				}
-				if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_PORTS 
-						|| feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_PORTS_ARE_PERMISSIVE) {
-					final GridData gd = (GridData) super.createEditorLayoutData(root, value, editor, control);
-					// 64 - magic constant from MultiDetailDialog
-					gd.horizontalSpan = 2;
-					// FIXME: Hack pending proper APi to manipulate labels
-					if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_PORTS) {
-						final Label label = editor.getLabel();
-						if (label != null) {
-							label.setText("Ports");
-						}
-						editor.setLabel(null);
-					} else {
-						editor.setLabel(null);
-					}
-
-					return gd;
-				}
-				if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_VESSELS 
-						|| feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_VESSELS_ARE_PERMISSIVE) {
-					final GridData gd = (GridData) super.createEditorLayoutData(root, value, editor, control);
-					// 64 - magic constant from MultiDetailDialog
-					gd.horizontalSpan = 2;
-					// FIXME: Hack pending proper APi to manipulate labels
-					if (feature == SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_VESSELS) {
-						final Label label = editor.getLabel();
-						if (label != null) {
-							label.setText("Vessels");
-						}
-						editor.setLabel(null);
-					} else {
-						editor.setLabel(null);
-					}
-
-					return gd;
-				}
-
-				GridData gd = (GridData) super.createEditorLayoutData(root, value, editor, control);
-				gd.horizontalSpan = 5;
-				return gd;
-			}
-		};
+			return new RowGroupDisplayCompositeLayoutProviderBuilder() //
+					.withRow() //
+					.withLabel("Contracts") //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_CONTRACTS) //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_CONTRACTS_ARE_PERMISSIVE) //
+					.makeRow() //
+					//
+					.withRow() //
+					.withLabel("Ports") //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_PORTS) //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_PORTS_ARE_PERMISSIVE) //
+					.makeRow() //
+					//
+					//
+					.withRow() //
+					.withLabel("Vessels") //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_VESSELS) //
+					.withFeature(SpotMarketsPackage.Literals.SPOT_MARKET__RESTRICTED_VESSELS_ARE_PERMISSIVE) //
+					.makeRow() //
+					//
+					.make();
+		}
+		return super.createLayoutProvider();
 	}
 }
