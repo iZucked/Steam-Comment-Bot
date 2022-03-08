d1a3745ca9bda5a6853aaacb6a0e56e89c9eec56
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-03-08 10:37:31 +0000

    Fix genmodel for cargoesForExposures and cargoesForHedging

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.edit/src/com/mmxlabs/models/lng/cargo/provider/CargoModelItemProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.edit/src/com/mmxlabs/models/lng/cargo/provider/CargoModelItemProvider.java
@@ -146,6 +146,8 @@ public class CargoModelItemProvider
 			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__CHARTER_IN_MARKET_OVERRIDES);
 			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__PAPER_DEALS);
 			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__DEAL_SETS);
+			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_EXPOSURES);
+			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_HEDGING);
 			childrenFeatures.add(CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS);
 		}
 		return childrenFeatures;
@@ -213,6 +215,8 @@ public class CargoModelItemProvider
 			case CargoPackage.CARGO_MODEL__CHARTER_IN_MARKET_OVERRIDES:
 			case CargoPackage.CARGO_MODEL__PAPER_DEALS:
 			case CargoPackage.CARGO_MODEL__DEAL_SETS:
+			case CargoPackage.CARGO_MODEL__CARGOES_FOR_EXPOSURES:
+			case CargoPackage.CARGO_MODEL__CARGOES_FOR_HEDGING:
 			case CargoPackage.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
 				return;
@@ -318,8 +322,42 @@ public class CargoModelItemProvider
 
 		newChildDescriptors.add
 			(createChildParameter
+				(CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_EXPOSURES,
+				 CargoFactory.eINSTANCE.createCargo()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_HEDGING,
+				 CargoFactory.eINSTANCE.createCargo()));
+
+		newChildDescriptors.add
+			(createChildParameter
 				(CargoPackage.Literals.CARGO_MODEL__GROUPED_DISCHARGE_SLOTS,
 				 CargoFactory.eINSTANCE.createGroupedDischargeSlotsConstraint()));
+	}
+
+	/**
+	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getCreateChildText(Object owner, Object feature, Object child, Collection<?> selection) {
+		Object childFeature = feature;
+		Object childObject = child;
+
+		boolean qualify =
+			childFeature == CargoPackage.Literals.CARGO_MODEL__CARGOES ||
+			childFeature == CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_EXPOSURES ||
+			childFeature == CargoPackage.Literals.CARGO_MODEL__CARGOES_FOR_HEDGING;
+
+		if (qualify) {
+			return getString
+				("_UI_CreateChild_text2",
+				 new Object[] { getTypeText(childObject), getFeatureText(childFeature), getTypeText(owner) });
+		}
+		return super.getCreateChildText(owner, feature, child, selection);
 	}
 
 }
