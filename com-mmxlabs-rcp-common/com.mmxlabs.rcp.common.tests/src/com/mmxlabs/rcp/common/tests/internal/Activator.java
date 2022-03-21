b81e136ebb4bf1c02cab2f626729114721292255
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-03-21 15:01:59 +0000

    Try setting a bundle start level

--- com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/internal/Activator.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common.tests/src/com/mmxlabs/rcp/common/tests/internal/Activator.java
@@ -20,31 +20,15 @@ public class Activator extends AbstractUIPlugin {
 	// The shared instance
 	private static Activator plugin;
 
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		
+
 		// Trigger plugin startup
 		var x = CommonImages.class;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
