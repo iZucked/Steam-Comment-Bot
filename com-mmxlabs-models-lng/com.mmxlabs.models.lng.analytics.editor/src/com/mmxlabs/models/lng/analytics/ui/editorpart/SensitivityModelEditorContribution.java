aac89260454e50f3b7109b739d8dca2a784cddb5
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-04-08 10:10:26 +0100

    Hid price sensitivity pane and report behind feature.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/ui/editorpart/SensitivityModelEditorContribution.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.analytics.editor/src/com/mmxlabs/models/lng/analytics/ui/editorpart/SensitivityModelEditorContribution.java
@@ -4,7 +4,8 @@ import java.util.Collections;
 
 import org.eclipse.swt.widgets.Composite;
 
-import com.mmxlabs.common.Pair;
+import com.mmxlabs.license.features.KnownFeatures;
+import com.mmxlabs.license.features.LicenseFeatures;
 import com.mmxlabs.models.lng.analytics.AnalyticsPackage;
 import com.mmxlabs.models.lng.analytics.SensitivityModel;
 import com.mmxlabs.models.lng.pricing.PricingModel;
@@ -13,13 +14,13 @@ import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelUtil;
 import com.mmxlabs.models.ui.editorpart.BaseJointModelEditorContribution;
 
 public class SensitivityModelEditorContribution extends BaseJointModelEditorContribution<SensitivityModel> {
-	private SensitivityCurvesPane curvesPane;
+	private SensitivityCurvesPane curvesPane = null;
 
 	private int curvesPage = -1;
 
 	@Override
 	public void addPages(Composite parent) {
-		{
+		if (LicenseFeatures.isPermitted(KnownFeatures.FEATURE_PRICE_SENSITIVITY)) {
 			curvesPane = new SensitivityCurvesPane(editorPart.getSite().getPage(), editorPart, editorPart, editorPart.getEditorSite().getActionBars());
 			curvesPane.createControl(parent);
 			curvesPane.init(Collections.singletonList(AnalyticsPackage.eINSTANCE.getSensitivityModel_SensitivityModel()), editorPart.getAdapterFactory(), editorPart.getModelReference());
@@ -28,8 +29,7 @@ public class SensitivityModelEditorContribution extends BaseJointModelEditorCont
 				final PricingModel pricingModel = ScenarioModelUtil.getPricingModel(model);
 				curvesPane.setInput(modelObject, pricingModel);
 			}
-			
-			
+
 			curvesPage = editorPart.addPage(curvesPane.getControl());
 			editorPart.setPageText(curvesPage, "Sensitivity");
 		}
@@ -40,6 +40,6 @@ public class SensitivityModelEditorContribution extends BaseJointModelEditorCont
 		if (curvesPane != null) {
 			curvesPane.setLocked(locked);
 		}
-		
+
 	}
 }
