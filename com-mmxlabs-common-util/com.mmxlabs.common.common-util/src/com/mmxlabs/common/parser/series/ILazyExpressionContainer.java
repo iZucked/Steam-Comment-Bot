3e50a4006c843a8604c8c07b83f17cf010d8d81c
Jenkins <hudson@minimaxlabs.com>
2022-04-27 15:18:22 +0000

    Update headers

--- com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/parser/series/ILazyExpressionContainer.java
+++ com-mmxlabs-common-util/com.mmxlabs.common.common-util/src/com/mmxlabs/common/parser/series/ILazyExpressionContainer.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
 package com.mmxlabs.common.parser.series;
 
 public interface ILazyExpressionContainer extends IExpressionContainer {
