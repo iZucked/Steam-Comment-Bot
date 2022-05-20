aa2c33bacc1743935b67ca7d7892006f3e3d89b3
Jenkins <Jenkins intserv@minimaxlabs.com>
2022-05-20 16:08:28 +0000

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/mull/IInventoryBasedGenerationSolver.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/mull/IInventoryBasedGenerationSolver.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.adp.mull;
 
 import org.eclipse.emf.common.command.Command;
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/mull/algorithm/DefaultShippedStrategyContainer.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.adp.editor/src/com/mmxlabs/models/lng/adp/mull/algorithm/DefaultShippedStrategyContainer.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.adp.mull.algorithm;
 
 import org.eclipse.jdt.annotation.NonNull;
