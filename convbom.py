9292c94c740ba4b0bdbf227309f46e6ef6c441b5
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-07-22 10:52:36 +0100

    Add in more mappings

--- convbom.py
+++ convbom.py
@@ -8,7 +8,17 @@ mappings = {
   'com.fasterxml.jackson.core.jackson-core' : { 'name':'jackson-core','group':'com.fasterxml.jackson.core' },
   'com.fasterxml.jackson.core.jackson-annotations' : { 'name':'jackson-annotations','group':'com.fasterxml.jackson.core' },
   'org.apache.log4j' : { 'name':'log4j','group':'log4j', 'stripQualifier': true },
-  'wrapped.com.squareup.okhttp3.okhttp' : { 'name':'okhttp','group':'com.squareup.okhttp3' }
+  'wrapped.com.squareup.okhttp3.okhttp' : { 'name':'okhttp','group':'com.squareup.okhttp3' },
+  'wrapped.com.squareup.okhttp3.okio' : { 'name':'okio','group':'com.squareup.okio' },
+  
+  'wrapped.org.testcontainers.testcontainers' : { 'name':'testcontainers','group':'org.testcontainers' },
+  'wrapped.org.testcontainers.junit-jupiter' : { 'name':'junit-jupiter','group':'org.testcontainers' },
+  
+  'wrapped.com.google.errorprone.error_prone_annotations' : { 'name':'error_prone_annotations','group':'com.google.errorprone' },
+  
+  'ch.qos.logback.classic' : { 'name':'logback-classic','group':'ch.qos.logback' }
+  'ch.qos.logback.core' : { 'name':'logback-core','group':'ch.qos.logback' }
+  'ch.qos.logback.slf4j' : { 'name':'logback-classic','group':'ch.qos.logback' } ## SLF4j is really part of the main classic jar file
 }
 
 infile = sys.argv[1]
