4e2fcfdc7a7311ad03cab08c72c11ab75788a4ac
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-07-22 07:27:39 +0100

    Add a python script to try and fix the DepCheck BOM

---
+++ convbom.py
@@ -0,0 +1,35 @@
+import json
+import sys
+
+mappings = {
+  'com.fasterxml.jackson.core.jackson-databind' : { 'name':'jackson-databind','group':'com.fasterxml.jackson.core' },
+  'com.fasterxml.jackson.datatype.jackson-datatype-jdk8' : { 'name':'jackson-datatype-jdk8','group':'com.fasterxml.jackson.datatype' },
+  'com.fasterxml.jackson.datatype.jackson-datatype-jsr310' : { 'name':'jackson-datatype-jsr310','group':'com.fasterxml.jackson.datatype' },
+  'com.fasterxml.jackson.core.jackson-core' : { 'name':'jackson-core','group':'com.fasterxml.jackson.core' },
+  'com.fasterxml.jackson.core.jackson-annotations' : { 'name':'jackson-annotations','group':'com.fasterxml.jackson.core' },
+  'org.apache.log4j' : { 'name':'log4j','group':'log4j', 'version':'1.2.15' },
+  'wrapped.com.squareup.okhttp3.okhttp' : { 'name':'okhttp','group':'com.squareup.okhttp3' }
+}
+
+infile = sys.argv[2]
+outfile = sys.argv[3]
+
+bom = None
+with open(infile) as json_data:
+    bom = json.load(json_data)
+
+
+for component in bom['components']:
+    if component['name'] in mappings:
+        v = mappings[component['name']]
+        #component['name'] = v['name']
+        #component['group'] = v['group']
+        #if 'version' in v:
+        #    component['version'] = v['version']
+        #    
+        u = { **component , **v}
+        component['bom-ref'] = 'pkg:maven/{group}/{name}@{version}?type=jar'.format(**u)
+        component['purl'] = 'pkg:maven/{group}/{name}@{version}?type=jar'.format(**u)
+
+with open(outfile, 'w') as f:
+    json.dump(bom, f, indent=4)
