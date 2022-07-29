59e4282701cdf7d5e15a2f52a41f77a58a1b90e6
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2022-07-29 13:39:44 +0100

    Migration to v163

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
@@ -24,7 +24,7 @@ public class LiNGOMigrationProvider implements IMigrationProvider {
 	 * reflective class loader to work.
 	 */
 
-	private static final int CURRENT_VERSION = 162;
+	private static final int CURRENT_VERSION = 163;
 
 	@Override
 	public int priority() {
--- com-mmxlabs-models-lng/copy_models.py
+++ com-mmxlabs-models-lng/copy_models.py
@@ -6,7 +6,7 @@
 import shutil, os
 #####################################
 path_to_ws = os.getcwd()
-version = 162
+version = 163
 #####################################
 
 root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
