4444b10eedc38e23d47f78a6eac6b27674e08a76
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-08-02 14:25:38 +0100

    Added basic general preferences page

---
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/generalprefs/LingoGeneralPreferencesPage.java
@@ -0,0 +1,39 @@
+package com.mmxlabs.rcp.common.generalprefs;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+
+public class LingoGeneralPreferencesPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public LingoGeneralPreferencesPage() {
+		setPreferenceStore(WorkbenchPlugin.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		final IntegerFieldEditor maxOpenedEditors = new IntegerFieldEditor("REUSE_OPEN_EDITORS", "&Number of opened editors before closing:", getFieldEditorParent());
+		addField(maxOpenedEditors);
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		final Composite c = (Composite) super.createContents(parent);
+		final Composite sub = new Composite(c, SWT.NONE);
+		sub.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, ((GridLayout) c.getLayout()).numColumns, 1));
+		sub.setLayout(new GridLayout(2, false));
+		return c;
+	}
+
+}
