503cbf0701702b34f9102b688773023d73a379ed
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-08-03 09:48:46 +0100

    Use a new ScopedPreferenceStore instead of looking up internal workbench implementation

--- com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/generalprefs/LingoGeneralPreferencesPage.java
+++ com-mmxlabs-rcp-common/com.mmxlabs.rcp.common/src/com/mmxlabs/rcp/common/generalprefs/LingoGeneralPreferencesPage.java
@@ -1,5 +1,7 @@
 package com.mmxlabs.rcp.common.generalprefs;
 
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.swt.SWT;
@@ -9,16 +11,20 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 public class LingoGeneralPreferencesPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
+	private @NonNull ScopedPreferenceStore scopedPreferenceStore;
+
 	public LingoGeneralPreferencesPage() {
-		setPreferenceStore(WorkbenchPlugin.getDefault().getPreferenceStore());
+		scopedPreferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.eclipse.ui.workbench");
+		setPreferenceStore(scopedPreferenceStore);
 	}
 
 	@Override
 	public void init(IWorkbench workbench) {
+		// Nothing to do
 	}
 
 	@Override
@@ -35,5 +41,4 @@ public class LingoGeneralPreferencesPage extends FieldEditorPreferencePage imple
 		sub.setLayout(new GridLayout(2, false));
 		return c;
 	}
-
 }
