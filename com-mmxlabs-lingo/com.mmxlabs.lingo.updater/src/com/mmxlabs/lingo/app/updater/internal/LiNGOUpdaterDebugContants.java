4efaf29797e4d0d9c483639668f8afcef6c1bb64
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-08-11 09:29:18 +0100

    Add in stub LiNGO updater debug logging options

---
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.updater/src/com/mmxlabs/lingo/app/updater/internal/LiNGOUpdaterDebugContants.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
+package com.mmxlabs.lingo.app.updater.internal;
+
+/**
+ * Constants for the lingo updater logging.
+ * 
+ * Remember to update the .options file in the project root for Eclipse
+ * run/debug dialog integration
+ * 
+ * @author Simon Goodall
+ *
+ */
+public class LiNGOUpdaterDebugContants {
+
+	private LiNGOUpdaterDebugContants() {
+
+	}
+
+	public static final String ID_BASE = "com.mmxlabs.lingo.updater/debug/updater/";
+
+	public static final String DEBUG_DOWNLOAD = ID_BASE + "download";
+}
