6b1b2bfa667f77fd9d818754930a76e8b5886514
Simon Goodall <simon.goodall@minimaxlabs.com>
2022-09-26 15:57:05 +0100

    [HubTests] Use new test tenant

--- com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/datahub/OAuthTests.java
+++ com-mmxlabs-lingo/com.mmxlabs.lingo.its/src/com/mmxlabs/lingo/its/datahub/OAuthTests.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -59,26 +58,11 @@ public class OAuthTests {
 
 	static final String CONTAINER = HubTestHelper.getContainerString(VersionHelper.getInstance().getClientCode());
 
-	// @formatter:off
 	@Container
-	// we need a fixed port for this test because we cannot programmatically modify the redirection-url on Azure
+	// we need a fixed port for this test because we cannot programmatically modify
+	// the redirection-url on Azure
 	// AD has port 8090 - 8099 set for testing purposes
-	public static GenericContainer datahubContainer = new FixedHostPortGenericContainer(CONTAINER)
-	.withFixedExposedPort(availablePort, DATAHUB_PORT)
-	.withExposedPorts(DATAHUB_PORT)
-	.withEnv("PORT", Integer.toString(DATAHUB_PORT))
-	.withEnv("PROTOCOL", "http")
-	.withEnv("HOSTNAME", "localhost")
-	.withEnv("AUTHENTICATION_SCHEME", "oauth")
-	.withEnv("INSTANCE_TAG", "docker")
-	.withEnv("DB_HOST", "0.0.0.0")
-	.withEnv("DB_PORT", "27000")
-	.withEnv("AZURE_CLIENT_ID", "e52d83a9-40c0-42ae-aae3-d5054ef24919")
-	.withEnv("AZURE_TENANT_ID", "dceff11f-6e74-436e-b9a0-65f9697b8472")
-	.withEnv("AZURE_CLIENT_SECRET", "n-LV4_3vPJ3s1v.9MWwLy1.ZO-1oz17u54")
-	.withEnv("AZURE_GROUPS", "MinimaxUsers, MinimaxLingo, MinimaxBasecase, MinimaxAdmin")
-	.waitingFor(Wait.forLogMessage(".*Started ServerConnector.*", 1));
-	// @formatter:on
+	public static GenericContainer datahubContainer = HubTestHelper.createDataHubContainer(CONTAINER, availablePort, DATAHUB_PORT, true);
 
 	@BeforeAll
 	private static void beforeAll() {
@@ -101,7 +85,9 @@ public class OAuthTests {
 	}
 
 	/*
-	 * Got this setup & teardown example for the creator of SWTBot himself in his PhD thesis: https://www.theseus.fi/bitstream/handle/10024/7470/Mazurkiewicz_Milosz.pdf
+	 * Got this setup & teardown example for the creator of SWTBot himself in his
+	 * PhD thesis:
+	 * https://www.theseus.fi/bitstream/handle/10024/7470/Mazurkiewicz_Milosz.pdf
 	 */
 	@AfterAll
 	public static void afterAll() {
@@ -145,7 +131,8 @@ public class OAuthTests {
 	}
 
 	/**
-	 * If this test fails all other tests will fail too because the hub is not online
+	 * If this test fails all other tests will fail too because the hub is not
+	 * online
 	 */
 	@Test
 	public void checkDatahubConnection() {
@@ -154,12 +141,15 @@ public class OAuthTests {
 	}
 
 	/**
-	 * Test OAuth login by executing JavaScript in the browser. Because bot.browser() returns void type we cannot use bot.waitUntil() conditions to check the contents of the page.
-	 * bot.waitForPageLoaded() doesn't work for this use case because the url doesn't change during the authentication process
+	 * Test OAuth login by executing JavaScript in the browser. Because
+	 * bot.browser() returns void type we cannot use bot.waitUntil() conditions to
+	 * check the contents of the page. bot.waitForPageLoaded() doesn't work for this
+	 * use case because the url doesn't change during the authentication process
 	 * <p>
 	 * </p>
-	 * If the tests fail to often try increasing the sleep time. If it keeps failing, make sure that test@minimaxlabs.com is able to login to {@link https://portal.azure.com} without any additional
-	 * prompts
+	 * If the tests fail to often try increasing the sleep time. If it keeps
+	 * failing, make sure that test@minimaxlabs.com is able to login to
+	 * {@link https://portal.azure.com} without any additional prompts
 	 */
 	@Test
 	public void oauthLogin() throws InterruptedException {
@@ -173,7 +163,8 @@ public class OAuthTests {
 
 		logger.info(Boolean.toString(datahubServiceProvider.isLoggedIn()));
 
-		// if user was previously authenticated, clikcing on Login will automatically log them back in
+		// if user was previously authenticated, clikcing on Login will automatically
+		// log them back in
 		if (!datahubServiceProvider.isLoggedIn()) {
 			bot.buttonWithId("login").click();
 			SWTBotShell[] swtshells = bot.shells();
