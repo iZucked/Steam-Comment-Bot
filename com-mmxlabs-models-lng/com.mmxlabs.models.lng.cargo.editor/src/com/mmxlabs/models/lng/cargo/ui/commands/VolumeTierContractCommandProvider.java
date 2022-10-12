87ac80859283db3cff718a3f03124aa77e333043
Jenkins <Jenkins intserv@minimaxlabs.com>
2022-10-12 16:26:52 +0000

    Update headers

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/commands/VolumeTierContractCommandProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/commands/VolumeTierContractCommandProvider.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.cargo.ui.commands;
 
 import com.mmxlabs.models.lng.commercial.CommercialFactory;
