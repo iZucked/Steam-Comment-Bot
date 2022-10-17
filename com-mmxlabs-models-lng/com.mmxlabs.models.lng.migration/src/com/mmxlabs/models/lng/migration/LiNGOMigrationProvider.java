4bc73e2b6409a09944dda9d3af813127f46e797f
Miten Mistry <miten.mistry@minimaxlabs.com>
2022-10-17 22:00:45 +0100

    Migrate to V167. Value matrix.

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
@@ -24,7 +24,7 @@ public class LiNGOMigrationProvider implements IMigrationProvider {
 	 * reflective class loader to work.
 	 */
 
-	private static final int CURRENT_VERSION = 166;
+	private static final int CURRENT_VERSION = 167;
 
 	@Override
 	public int priority() {
