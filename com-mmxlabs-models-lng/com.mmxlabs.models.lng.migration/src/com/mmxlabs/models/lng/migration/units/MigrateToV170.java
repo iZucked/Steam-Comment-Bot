d6243cac1a8e14babb156cf4313f1f5ef25830bd
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2022-12-18 11:01:27 +0000

    Migration v170

---
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV170.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2022
+ * All rights reserved.
+ */
+package com.mmxlabs.models.lng.migration.units;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+import com.mmxlabs.models.lng.migration.AbstractMigrationUnit;
+import com.mmxlabs.models.lng.migration.ModelsLNGMigrationConstants;
+import com.mmxlabs.models.migration.MigrationModelRecord;
+
+public class MigrateToV170 extends AbstractMigrationUnit {
+
+	@Override
+	public String getScenarioContext() {
+		return ModelsLNGMigrationConstants.Context;
+	}
+
+	@Override
+	public int getScenarioSourceVersion() {
+		return 169;
+	}
+
+	@Override
+	public int getScenarioDestinationVersion() {
+		return 170;
+	}
+
+	@Override
+	protected void doMigration(@NonNull final MigrationModelRecord modelRecord) {
+		// Do nothing
+
+		// Add support harmony stuff - Business units and preferred pricing basis
+	}
+}
--- com-mmxlabs-models-lng/copy_models.py
+++ com-mmxlabs-models-lng/copy_models.py
@@ -6,7 +6,7 @@
 import shutil, os
 #####################################
 path_to_ws = os.getcwd()
-version = 169
+version = 170
 #####################################
 
 root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
