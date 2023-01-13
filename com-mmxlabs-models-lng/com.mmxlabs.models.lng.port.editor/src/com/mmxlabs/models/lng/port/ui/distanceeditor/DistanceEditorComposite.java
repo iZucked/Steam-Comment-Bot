e11a2eb89362f5c6f84b236583af403d98efd9cd
Simon Goodall <simon.goodall@minimaxlabs.com>
2023-01-13 14:57:51 +0000

    Fix opening of distance editor

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/distanceeditor/DistanceEditorComposite.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.port.editor/src/com/mmxlabs/models/lng/port/ui/distanceeditor/DistanceEditorComposite.java
@@ -255,6 +255,9 @@ public class DistanceEditorComposite extends DefaultDetailComposite {
 	public void display(IDialogEditingContext dialogContext, MMXRootObject root, EObject object, Collection<EObject> range, EMFDataBindingContext dbc) {
 		final EClass eClass = object.eClass();
 		this.object = object;
+		if (layoutProvider == null) {
+			layoutProvider = createLayoutProvider(eClass);
+		}
 		setLayout(layoutProvider.createDetailLayout(root, object));
 		if (eClass != displayedClass) {
 			clear();
