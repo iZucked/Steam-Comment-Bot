3836744fafefe12264368c17e2cd8e1f5ea2fd09
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2023-01-23 12:14:00 +0000

    Migration v172

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV172.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV172.java
@@ -10,7 +10,7 @@ import com.mmxlabs.models.lng.migration.AbstractMigrationUnit;
 import com.mmxlabs.models.lng.migration.ModelsLNGMigrationConstants;
 import com.mmxlabs.models.migration.MigrationModelRecord;
 
-public class MigrateToV170 extends AbstractMigrationUnit {
+public class MigrateToV172 extends AbstractMigrationUnit {
 
 	@Override
 	public String getScenarioContext() {
@@ -19,12 +19,12 @@ public class MigrateToV170 extends AbstractMigrationUnit {
 
 	@Override
 	public int getScenarioSourceVersion() {
-		return 169;
+		return 171;
 	}
 
 	@Override
 	public int getScenarioDestinationVersion() {
-		return 170;
+		return 172;
 	}
 
 	@Override
