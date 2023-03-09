2d5e18bfa6b4968a1ed354e4302ab5c13c266638
Miten Mistry <miten.mistry@minimaxlabs.com>
2023-03-09 11:19:28 +0000

    LDD support for cloud metrics.

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/headless/common/ScenarioMetaUtils.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/headless/common/ScenarioMetaUtils.java
@@ -11,8 +11,7 @@ import java.util.Map;
 
 import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoModel;
-import com.mmxlabs.models.lng.cargo.DischargeSlot;
-import com.mmxlabs.models.lng.cargo.LoadSlot;
+import com.mmxlabs.models.lng.cargo.CargoType;
 import com.mmxlabs.models.lng.cargo.SchedulingTimeWindow;
 import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.VesselCharter;
@@ -29,110 +28,93 @@ import com.mmxlabs.models.lng.transformer.ui.headless.HeadlessGenericJSON.Scenar
 import com.mmxlabs.scenario.service.model.manager.IScenarioDataProvider;
 
 public class ScenarioMetaUtils {
-	
+
 	private static final int HOURS_IN_DAY = 24;
 	private static final int DAYS_IN_WEEK = 7;
 	private static final int WEEKS_IN_MONTH = 4;
 
-
 	public static Map<String, Integer> calculateActiveSpotMarkets(SpotMarketsModel spotMarketsModel) {
 		List<SpotMarketGroup> spotMarkets = getSpotMarkets(spotMarketsModel);
-		
+
 		Map<String, Integer> spotMarketsActive = new HashMap<>();
 		for (SpotMarketGroup marketGroup : spotMarkets) {
-			//get markets from market group
+			// get markets from market group
 			List<SpotMarket> spotsPerGroup = marketGroup.getMarkets();
-			//keep counter of available spots
+			// keep counter of available spots
 			int numAvailable = 0;
-			
+
 			for (SpotMarket market : spotsPerGroup) {
 				numAvailable += market.isEnabled() ? market.getAvailability().getConstant() : 0;
 			}
-				
-			//add number of availabilities and type of market to map
+
+			// add number of availabilities and type of market to map
 			spotMarketsActive.put(marketGroup.getType().getName(), numAvailable);
 		}
-	
+
 		return spotMarketsActive;
 	}
-	
+
 	public static int calculateShippedCargoes(List<Cargo> cargoes) {
-		int numShippedCargoes = 0;
-		for (Cargo cargo : cargoes) {
-			List<Slot<?>> slots = cargo.getSortedSlots();
-			// Assume: there is exactly one load and discharge each
-			assert slots.size() == 2;
-			
-			LoadSlot loadSlot = (LoadSlot) slots.get(0);
-			DischargeSlot dischargeSlot = (DischargeSlot) slots.get(1);
-			
-			if (!loadSlot.isDESPurchase() && !dischargeSlot.isFOBSale()) {
-				numShippedCargoes++;
-			}
-		}
-		
-		return numShippedCargoes;
+		return (int) cargoes.stream() //
+				.map(Cargo::getCargoType) //
+				.filter(CargoType.FLEET::equals) //
+				.count();
 	}
-	
+
 	public static Map<String, Integer> calculateVesselEvents(List<VesselEvent> vesselEvents) {
-		Map<String, Integer> vesselEventCounts = new HashMap<>(); 
+		Map<String, Integer> vesselEventCounts = new HashMap<>();
 		vesselEvents.forEach(event -> vesselEventCounts.merge(event.getClass().getSimpleName(), 1, Integer::sum));
-		
+
 		return vesselEventCounts;
 	}
-	
+
 	public static int calculateCharterOutSpots(SpotMarketsModel spotMarketsModel) {
 		int charterOutSpots = 0;
 		List<CharterOutMarket> charterOutMarkets = spotMarketsModel.getCharterOutMarkets();
-		
+
 		for (CharterOutMarket market : charterOutMarkets) {
 			charterOutSpots += market.isEnabled() ? 1 : 0;
 		}
-			
+
 		return charterOutSpots;
 	}
-	
+
 	public static int calculateCharterInSpots(SpotMarketsModel spotMarketsModel) {
 		int charterInSpots = 0;
 		List<CharterInMarket> charterInMarkets = spotMarketsModel.getCharterInMarkets();
-				
+
 		for (CharterInMarket market : charterInMarkets) {
 			charterInSpots += market.isEnabled() ? 1 : 0;
 		}
-			
+
 		return charterInSpots;
 	}
-	
+
 	private static Map<Integer, Integer> calculateSlotDurations(List<Slot<?>> slots) {
-		Map<Integer, Integer> durations = new HashMap<>(); 
-		slots.forEach(slot -> durations.merge(slot.getSchedulingTimeWindow().getDuration(),
-				1, Integer::sum));
-		
+		Map<Integer, Integer> durations = new HashMap<>();
+		slots.forEach(slot -> durations.merge(slot.getSchedulingTimeWindow().getDuration(), 1, Integer::sum));
+
 		return durations;
 	}
-	
+
 	private static Map<String, Integer> calculateSlotWindows(List<Slot<?>> slots) {
-		List<Integer> times = slots.stream().
-				map(Slot::getSchedulingTimeWindow).
-				map(SchedulingTimeWindow::getSizeInHours).
-				toList();
-		
+		List<Integer> times = slots.stream().map(Slot::getSchedulingTimeWindow).map(SchedulingTimeWindow::getSizeInHours).toList();
+
 		return bucketByHours(times);
 	}
-	
+
 	private static List<Integer> calculateMinTimeWindows(List<VesselCharter> availabilities) {
 		return availabilities.stream().map(VesselCharter::getCharterOrDelegateMinDuration).toList();
 	}
-	
+
 	private static List<Integer> calculateMaxTimeWindows(List<VesselCharter> availabilities) {
 		return availabilities.stream().map(VesselCharter::getCharterOrDelegateMaxDuration).toList();
 	}
-	
-	public static ScenarioMeta writeOptimisationMetrics(IScenarioDataProvider scenarioDataProvider, 
-			UserSettings userSettings) {
+
+	public static ScenarioMeta writeOptimisationMetrics(IScenarioDataProvider scenarioDataProvider, UserSettings userSettings) {
 		ScenarioMeta scenarioMeta = new ScenarioMeta();
-		
-		final CargoModel cargoModel = ScenarioModelUtil.getCargoModel(scenarioDataProvider); 
+
+		final CargoModel cargoModel = ScenarioModelUtil.getCargoModel(scenarioDataProvider);
 		final SpotMarketsModel spotMarketsModel = ScenarioModelUtil.getSpotMarketsModel(scenarioDataProvider);
 		final CommercialModel commercialModel = ScenarioModelUtil.getCommercialModel(scenarioDataProvider);
 
@@ -140,7 +122,7 @@ public class ScenarioMetaUtils {
 		List<VesselCharter> availabilities = cargoModel.getVesselCharters();
 		List<Slot<?>> loadSlots = new ArrayList<>(cargoModel.getLoadSlots());
 		List<Slot<?>> dischargeSlots = new ArrayList<>(cargoModel.getDischargeSlots());
-		
+
 		scenarioMeta.setNumCargoes(cargoes.size());
 		scenarioMeta.setNumVessels(availabilities.size());
 		scenarioMeta.setNumLoads(loadSlots.size());
@@ -148,65 +130,65 @@ public class ScenarioMetaUtils {
 		scenarioMeta.setNumSalesContracts(commercialModel.getSalesContracts().size());
 		scenarioMeta.setNumPurchaseContracts(commercialModel.getPurchaseContracts().size());
 		scenarioMeta.setShippedCargoes(calculateShippedCargoes(cargoes));
-		scenarioMeta.setVesselEvents(calculateVesselEvents(cargoModel.getVesselEvents()));	
+		scenarioMeta.setVesselEvents(calculateVesselEvents(cargoModel.getVesselEvents()));
 		scenarioMeta.setCharterInSpotsCount(calculateCharterInSpots(spotMarketsModel));
-		
+
 		if (userSettings != null) {
-			if(userSettings.isWithSpotCargoMarkets()) {
+			if (userSettings.isWithSpotCargoMarkets()) {
 				scenarioMeta.setSpotMarketOptions(calculateActiveSpotMarkets(spotMarketsModel));
 			}
-			
-			if(userSettings.isGenerateCharterOuts()) {
+
+			if (userSettings.isGenerateCharterOuts()) {
 				scenarioMeta.setCharterOutSpotsCount(calculateCharterOutSpots(spotMarketsModel));
 			}
 		}
-		
+
 		scenarioMeta.setMinTimeWindows(calculateMinTimeWindows(availabilities));
 		scenarioMeta.setMaxTimeWindows(calculateMaxTimeWindows(availabilities));
-		
+
 		scenarioMeta.setLoadTimeWindow(calculateSlotWindows(loadSlots));
 		scenarioMeta.setDischargeTimeWindow(calculateSlotWindows(dischargeSlots));
 		scenarioMeta.setLoadSlotDuration(calculateSlotDurations(loadSlots));
 		scenarioMeta.setDischargeSlotDuration(calculateSlotDurations(dischargeSlots));
-		
+
 		return scenarioMeta;
 	}
-	
+
 	private static List<SpotMarketGroup> getSpotMarkets(SpotMarketsModel spotMarketsModel) {
 		List<SpotMarketGroup> spotMarkets = new ArrayList<>();
 		spotMarkets.add(spotMarketsModel.getDesPurchaseSpotMarket());
 		spotMarkets.add(spotMarketsModel.getDesSalesSpotMarket());
 		spotMarkets.add(spotMarketsModel.getFobPurchasesSpotMarket());
 		spotMarkets.add(spotMarketsModel.getFobSalesSpotMarket());
-		
+
 		return spotMarkets;
 	}
-	
+
 	private static Map<String, Integer> bucketByHours(List<Integer> times) {
 		Map<String, Integer> timeBuckets = new HashMap<>();
-		
+
 		for (Integer hours : times) {
 			if (hours < HOURS_IN_DAY) {
 				timeBuckets.merge(hours + " hours", 1, Integer::sum);
 				continue;
 			}
-			
+
 			double days = (double) hours / HOURS_IN_DAY;
 			if (days < DAYS_IN_WEEK) {
-				timeBuckets.merge((int)(Math.ceil(days)) + " days", 1, Integer::sum);
+				timeBuckets.merge((int) (Math.ceil(days)) + " days", 1, Integer::sum);
 				continue;
 			}
-			
+
 			double weeks = days / DAYS_IN_WEEK;
 			if (weeks < WEEKS_IN_MONTH) {
-				timeBuckets.merge((int)(Math.ceil(weeks)) + " weeks", 1, Integer::sum);
+				timeBuckets.merge((int) (Math.ceil(weeks)) + " weeks", 1, Integer::sum);
 				continue;
 			}
-			
+
 			double months = weeks / WEEKS_IN_MONTH;
-			timeBuckets.merge((int)(Math.ceil(months)) + " months", 1, Integer::sum);
+			timeBuckets.merge((int) (Math.ceil(months)) + " months", 1, Integer::sum);
 		}
-		
+
 		return timeBuckets;
 	}
 }
