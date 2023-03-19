2bbd2122f8a7268ef924abfe7d092bfec393df8d
Jenkins <Jenkins intserv@minimaxlabs.com>
2023-03-19 16:24:17 +0000

    Update headers

--- com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/analytics/SwapValueMatrixElementProvider.java
+++ com-mmxlabs-models-lng-transformer/com.mmxlabs.models.lng.transformer.ui/src/com/mmxlabs/models/lng/transformer/ui/analytics/SwapValueMatrixElementProvider.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Minimax Labs Ltd., 2010 - 2023
+ * All rights reserved.
+ */
 package com.mmxlabs.models.lng.transformer.ui.analytics;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
