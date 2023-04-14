fee7a38e7b00f2808a679b80cf75905b62544308
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2023-04-14 10:59:56 +0100

    Migration v180

--- com-mmxlabs-models-lng/copy_models.py
+++ com-mmxlabs-models-lng/copy_models.py
@@ -6,7 +6,7 @@
 import shutil, os
 #####################################
 path_to_ws = os.getcwd()
-version = 179
+version = 180
 #####################################
 
 root = "{0}/com.mmxlabs.models.lng.".format(path_to_ws)
