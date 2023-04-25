bde4c8ab3b49c668f479a40e68dc7c58a1edc3e3
Andrey Popov <apopov@minimaxlabs.com>
2023-04-25 10:55:55 +0100

    Troubleshooted the script

--- scripts/pricing-calendar-update.py
+++ scripts/pricing-calendar-update.py
@@ -33,7 +33,7 @@ calendars = [
     },
 ]
 
-print('Searching Pricing Calendar CSV in the project...')
+print('Searching Pricing Calendar CSV in the project...', flush=True)
 
 filename = 'Pricing Calendars.csv'
 data_dir_path = os.path.join(
@@ -46,7 +46,7 @@ data_dir_path = os.path.join(
 
 final_path = os.path.normpath(os.path.join(data_dir_path, filename))
 
-print('Path initialized correctly! Reading file...')
+print('Path initialized correctly! Reading file...', flush=True)
 
 rows = []
 
@@ -59,32 +59,38 @@ with open(final_path) as f:
             row = line.strip().split(',')
             rows.append(row)
 
-print('File was read successfully! Adding new data...')
+print('File was read successfully! Adding new data...', flush=True)
 
-for calendar in calendars:
+for exchange_calendar in calendars:
 
-    exchange_name = calendar['name']
+    exchange_name = exchange_calendar['name']
 
-    rows.append([exchange_name, calendar['title'], '', '', ''])
+    # rows.append([exchange_name, exchange_calendar['title'], '', '', ''])
 
     for year in years_to_append:
         for month in range(1, 13):
 
             month_name = datetime.date(year, month, 1).strftime("%B").lower().capitalize()
 
-            start_day, start_month= tuple(map(eval, calendar['start'].split()))
-            end_day, end_month = tuple(map(eval, calendar['end'].split()))
+            start_day, start_month= tuple(map(eval, exchange_calendar['start'].split()))
+            end_day, end_month = tuple(map(eval, exchange_calendar['end'].split()))
+            start_year = year
+            end_year = year
 
             if start_month < 1:
                 start_month += 12
+                start_year -= 1
             if end_month < 1:
                 end_month += 12
+                end_year -= 1
 
             if end_day == LAST_DAY_OF_THE_MONTH:
                 _, end_day = calendar.monthrange(year, end_month)
+            if start_day == LAST_DAY_OF_THE_MONTH:
+                _, start_day = calendar.monthrange(year, end_month)
 
-            start_date = f'{"%02d" % (start_day,)}/{"%02d" % (start_month,)}/{year}'
-            end_date = f'{"%02d" % (end_day,)}/{"%02d" % (end_month,)}/{year}'
+            start_date = f'{"%02d" % (start_day,)}/{"%02d" % (start_month,)}/{start_year}'
+            end_date = f'{"%02d" % (end_day,)}/{"%02d" % (end_month,)}/{end_year}'
 
             month_numeric_notation = f'{year}-{"%02d" % (month,)}'
 
@@ -92,11 +98,11 @@ for calendar in calendars:
 
 rows = sorted(rows, key=row_comarator_key)
 
-print('Data sorted! Writing it back...')
+print('Data sorted! Writing it back...', flush=True)
 
 with open(final_path, 'w') as f:
     f.write(header)
     for row in rows:
         f.write(','.join(row) + '\n')
 
-print('OK!')
+print('OK!', flush=True)
