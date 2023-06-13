3128595a19f3949b7429c052d70b4aa3230d4d02
Simon Goodall <simon.goodall@minimaxlabs.com>
2023-06-13 09:46:26 +0100

    New Migration

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
@@ -24,7 +24,7 @@ public class LiNGOMigrationProvider implements IMigrationProvider {
 	 * reflective class loader to work.
 	 */
 
-	private static final int CURRENT_VERSION = 185;
+	private static final int CURRENT_VERSION = 186;
 
 	@Override
 	public int priority() {
