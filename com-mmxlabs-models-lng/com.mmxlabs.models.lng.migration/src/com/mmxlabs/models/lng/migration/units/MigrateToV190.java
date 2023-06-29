f8fa049f6fcce783fa74b1e0e7b7477bfce17673
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2023-06-29 14:23:38 +0100

    Fix: migration overlooked the generated papers in schedule

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV190.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/units/MigrateToV190.java
@@ -7,6 +7,7 @@ package com.mmxlabs.models.lng.migration.units;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EEnum;
@@ -43,47 +44,59 @@ public class MigrateToV190 extends AbstractMigrationUnit {
 
 		EObjectWrapper scenarioModel = modelRecord.getModelRoot();
 		EObjectWrapper referenceModel = scenarioModel.getRef("referenceModel");
-		EObjectWrapper cargoModel = scenarioModel.getRef("cargoModel");
-		
-		final List<EObjectWrapper> paperDeals = cargoModel.getRefAsList("paperDeals");
-		paperDeals.forEach( pd -> {
-			pd.unsetFeature("startDate");
-			pd.unsetFeature("endDate");
-			
-			if (pd.getAttrib("pricingMonth") != null) {
-				final YearMonth contractMonth = (YearMonth) pd.getAttrib("pricingMonth");
-				
-				final YearMonth pricingMonth = contractMonth.minusMonths(2);
-				final LocalDate pricingPeriodStart = pricingMonth.atDay(1);
-				final LocalDate pricingPeriodEnd = pricingMonth.atEndOfMonth();
-				
-				final YearMonth hedgingMonth = contractMonth.minusMonths(1);
-				final LocalDate hedgingPeriodStart = hedgingMonth.atDay(1);
-				final LocalDate hedgingPeriodEnd = hedgingMonth.atEndOfMonth();
-				
-				pd.setAttrib("pricingPeriodStart", pricingPeriodStart);
-				pd.setAttrib("pricingPeriodEnd", pricingPeriodEnd);
-				pd.setAttrib("hedgingPeriodStart", hedgingPeriodStart);
-				pd.setAttrib("hedgingPeriodEnd", hedgingPeriodEnd);
+
+		Consumer<List<EObjectWrapper>> processPaperDeals = paperDeals -> {
+			if (paperDeals != null) {
+				paperDeals.forEach( pd -> {
+					if (pd.getAttrib("startDate") != null)
+						pd.unsetFeature("startDate");
+					if (pd.getAttrib("endDate") != null)
+						pd.unsetFeature("endDate");
+
+					if (pd.getAttrib("pricingMonth") != null) {
+						final YearMonth contractMonth = (YearMonth) pd.getAttrib("pricingMonth");
+
+						final YearMonth pricingMonth = contractMonth.minusMonths(2);
+						final LocalDate pricingPeriodStart = pricingMonth.atDay(1);
+						final LocalDate pricingPeriodEnd = pricingMonth.atEndOfMonth();
+
+						final YearMonth hedgingMonth = contractMonth.minusMonths(1);
+						final LocalDate hedgingPeriodStart = hedgingMonth.atDay(1);
+						final LocalDate hedgingPeriodEnd = hedgingMonth.atEndOfMonth();
+
+						pd.setAttrib("pricingPeriodStart", pricingPeriodStart);
+						pd.setAttrib("pricingPeriodEnd", pricingPeriodEnd);
+						pd.setAttrib("hedgingPeriodStart", hedgingPeriodStart);
+						pd.setAttrib("hedgingPeriodEnd", hedgingPeriodEnd);
+					}
+
+				});
 			}
-			
-		});
-		
+		};
+		EObjectWrapper cargoModel = scenarioModel.getRef("cargoModel");
+		processPaperDeals.accept(cargoModel.getRefAsList("paperDeals"));
+
+		EObjectWrapper scheduleModel = scenarioModel.getRef("scheduleModel");
+		if (scheduleModel != null) {
+			EObjectWrapper schedule = scheduleModel.getRef("schedule");
+			processPaperDeals.accept(schedule.getRefAsList("generatedPaperDeals"));
+		}
+
 		final EPackage lngTypesPackage = modelRecord.getMetamodelLoader().getPackageByNSURI(ModelsLNGMigrationConstants.NSURI_LNGTypes);
 		final EEnum pricingPeriodENum = MetamodelUtils.getEEnum(lngTypesPackage, "PricingPeriod");
 		final EEnumLiteral monthsLiteral = MetamodelUtils.getEEnum_Literal(pricingPeriodENum, "MONTHS");
-		
+
 		EObjectWrapper pricingModel = referenceModel.getRef("pricingModel");
 		final EPackage pricingPackage = modelRecord.getMetamodelLoader().getPackageByNSURI(ModelsLNGMigrationConstants.NSURI_PricingModel);
 		final EClass eClassInstrumentPeriod = MetamodelUtils.getEClass(pricingPackage, "InstrumentPeriod");
 		final EFactory pricingFactory = pricingPackage.getEFactoryInstance();
-		
+
 		final List<EObjectWrapper> settleStrategies = pricingModel.getRefAsList("settleStrategies");
 		settleStrategies.forEach( ss -> {
 			ss.unsetFeature("settlePeriod");
 			ss.unsetFeature("settlePeriodUnit");
 			ss.unsetFeature("settleStartMonthsPrior");
-			
+
 			final EObjectWrapper pricingPeriod = (EObjectWrapper) pricingFactory.create(eClassInstrumentPeriod);
 			pricingPeriod.setAttrib("periodSize", 1);
 			pricingPeriod.setAttrib("periodSizeUnit", monthsLiteral);
@@ -92,7 +105,7 @@ public class MigrateToV190 extends AbstractMigrationUnit {
 			hedgingPeriod.setAttrib("periodSize", 1);
 			hedgingPeriod.setAttrib("periodSizeUnit", monthsLiteral);
 			hedgingPeriod.setAttrib("periodOffset", 0);
-			
+
 			ss.setRef("pricingPeriod", pricingPeriod);
 			ss.setRef("hedgingPeriod", hedgingPeriod);
 		});
