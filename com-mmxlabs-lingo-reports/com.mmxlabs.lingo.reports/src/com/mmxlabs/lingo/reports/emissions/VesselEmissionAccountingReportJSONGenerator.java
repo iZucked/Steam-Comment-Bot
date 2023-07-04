84a3ffe7682e164d5d535b09db237eac1ca3179e
Andrey Popov <apopov@minimaxlabs.com>
2023-07-04 14:09:31 +0100

    Refactoring. Breaking big method into smallet ones

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/emissions/VesselEmissionAccountingReportJSONGenerator.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/emissions/VesselEmissionAccountingReportJSONGenerator.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mmxlabs.models.lng.cargo.LoadSlot;
@@ -70,82 +71,7 @@ public class VesselEmissionAccountingReportJSONGenerator {
 					
 					model.eventID = vesselName;
 
-					if (e instanceof final SlotVisit sv) {
-						model.eventType = "Slot Visit";
-						final SlotAllocation sa = sv.getSlotAllocation();
-						model.otherID = sa.getName();
-						if (sa != null) {
-							final Slot<?> slot = sa.getSlot();
-							model.eventID = slot.getName();
-							if (slot != null) {
-								model.equivalents.add(slot);
-								if (slot.getCargo() != null) {
-									model.otherID = slot.getCargo().getUuid();
-								}
-							}
-							if (slot instanceof LoadSlot) {
-								model.methaneSlip += Math.round(sa.getPhysicalEnergyTransferred() * model.methaneSlipRate);
-							}
-						}
-					} else if (e instanceof final StartEvent se) {
-						model.eventType = "Vessel start";
-						final SlotAllocation slotAllocation = se.getSlotAllocation();
-						if (slotAllocation != null) {
-							model.otherID = slotAllocation.getName();
-							final Slot<?> slot = slotAllocation.getSlot();
-							if (slot != null) {
-								model.eventID = slot.getName();
-							}
-						}
-					} else if (e instanceof final EndEvent endEvent) {
-						model.eventType = "Vessel end";
-						if (endEvent.getSlotAllocation() != null) {
-							model.otherID = endEvent.getSlotAllocation().getName();
-						}
-						final SlotAllocation slotAllocation = endEvent.getSlotAllocation();
-						if (slotAllocation != null) {
-							model.otherID = slotAllocation.getName();
-							final Slot<?> slot = slotAllocation.getSlot();
-							if (slot != null) {
-								model.eventID = slot.getName();
-							}
-						}
-					} else if (e instanceof final Journey j) {
-						if (j.isLaden()) {
-							model.eventType = "Laden Leg";
-						} else {
-							model.eventType = "Ballast Leg";
-						}
-						if (j.getPreviousEvent() instanceof final SlotVisit sv && sv.getSlotAllocation() != null) {
-							final SlotAllocation slotAllocation = sv.getSlotAllocation();
-							if (slotAllocation != null) {
-								model.otherID = slotAllocation.getName();
-								//								final Slot<?> slot = slotAllocation.getSlot();
-								//								if (slot != null) {
-								//									model.eventID = slot.getName();
-								//								}
-							}
-							model.otherID = sv.getSlotAllocation().getName();
-						}
-						journeyDistance = j.getDistance();
-					} else if (e instanceof final Idle idleEvent) {
-						if (idleEvent.isLaden()) {
-							model.eventType = "Laden Idle";
-						} else {
-							model.eventType = "Ballast Idle";
-						}
-						if (idleEvent.getNextEvent() instanceof final SlotVisit sv && sv.getSlotAllocation() != null) {
-							final SlotAllocation slotAllocation = sv.getSlotAllocation();
-							if (slotAllocation != null) {
-								model.otherID = slotAllocation.getName();
-								//								final Slot<?> slot = slotAllocation.getSlot();
-								//								if (slot != null) {
-								//									model.eventID = slot.getName();
-								//								}
-							}
-							model.otherID = sv.getSlotAllocation().getName();
-						}
-					}
+					journeyDistance = processEvent(e, model, journeyDistance);
 					if (model.eventType == null) {
 						continue;
 					}
@@ -161,47 +87,7 @@ public class VesselEmissionAccountingReportJSONGenerator {
 					model.baseFuelConsumed = 0L;
 
 					if (e instanceof FuelUsage fuelUsageEvent) {	
-
-						double emissionsLNGAccumulator = 0.0;
-						
-						for (final FuelQuantity fuelQuantity : fuelUsageEvent.getFuels()) {
-
-							final Fuel fuel = fuelQuantity.getFuel();
-							final BaseFuel baseFuel = fuelQuantity.getBaseFuel();
-							final FuelAmount fuelAmount = fuelQuantity.getAmounts().get(0);
-
-
-							switch (fuel) {
-							case BASE_FUEL:
-								if (fuelQuantity.getAmounts().get(0).getUnit() != FuelUnit.MT) {
-									throw new IllegalStateException();
-								}
-								model.baseFuelType = baseFuel.getName();
-								model.baseFuelConsumed = Math.round(fuelAmount.getQuantity());
-								model.baseFuelEmissions = Math.round(model.baseFuelConsumed * baseFuel.getEmissionRate());
-								break;
-							case PILOT_LIGHT:
-								if (fuelQuantity.getAmounts().get(0).getUnit() != FuelUnit.MT) {
-									throw new IllegalStateException();
-								}
-								model.pilotLightFuelType = baseFuel.getName();
-								model.pilotLightFuelConsumption = Math.round(fuelAmount.getQuantity());
-								model.pilotLightEmission = Math.round(model.pilotLightFuelConsumption * baseFuel.getEmissionRate());
-								break;
-							case FBO:
-								model.consumedFBO += EmissionsUtils.consumedQuantityLNG(fuelQuantity);
-								emissionsLNGAccumulator += model.consumedFBO * EmissionsUtils.LNG_EMISSION_RATE_TON_CO2_PER_TON_FUEL;
-								break;
-							case NBO:
-								model.consumedNBO += EmissionsUtils.consumedQuantityLNG(fuelQuantity);
-								emissionsLNGAccumulator += model.consumedNBO * EmissionsUtils.LNG_EMISSION_RATE_TON_CO2_PER_TON_FUEL;
-								break;
-							default:
-							}
-						}
-
-						model.emissionsLNG = Math.round(emissionsLNGAccumulator);
-						model.totalEmission = model.baseFuelEmissions + model.pilotLightEmission + model.emissionsLNG;
+						processFuelUsageEvent(model, fuelUsageEvent);
 					}
 
 					model.totalEmission += 25 * model.methaneSlip;
@@ -217,6 +103,143 @@ public class VesselEmissionAccountingReportJSONGenerator {
 		}
 
 		return models;
+	}
+
+	private static void processFuelUsageEvent(final VesselEmissionAccountingReportModelV1 model, FuelUsage fuelUsageEvent) {
+		double emissionsLNGAccumulator = 0.0;
+		
+		for (final FuelQuantity fuelQuantity : fuelUsageEvent.getFuels()) {
+
+			final Fuel fuel = fuelQuantity.getFuel();
+			final BaseFuel baseFuel = fuelQuantity.getBaseFuel();
+			final FuelAmount fuelAmount = fuelQuantity.getAmounts().get(0);
+
+
+			switch (fuel) {
+			case BASE_FUEL:
+				if (fuelQuantity.getAmounts().get(0).getUnit() != FuelUnit.MT) {
+					throw new IllegalStateException();
+				}
+				model.baseFuelType = baseFuel.getName();
+				model.baseFuelConsumed = Math.round(fuelAmount.getQuantity());
+				model.baseFuelEmissions = Math.round(model.baseFuelConsumed * baseFuel.getEmissionRate());
+				break;
+			case PILOT_LIGHT:
+				if (fuelQuantity.getAmounts().get(0).getUnit() != FuelUnit.MT) {
+					throw new IllegalStateException();
+				}
+				model.pilotLightFuelType = baseFuel.getName();
+				model.pilotLightFuelConsumption = Math.round(fuelAmount.getQuantity());
+				model.pilotLightEmission = Math.round(model.pilotLightFuelConsumption * baseFuel.getEmissionRate());
+				break;
+			case FBO:
+				model.consumedFBO += EmissionsUtils.consumedQuantityLNG(fuelQuantity);
+				emissionsLNGAccumulator += model.consumedFBO * EmissionsUtils.LNG_EMISSION_RATE_TON_CO2_PER_TON_FUEL;
+				break;
+			case NBO:
+				model.consumedNBO += EmissionsUtils.consumedQuantityLNG(fuelQuantity);
+				emissionsLNGAccumulator += model.consumedNBO * EmissionsUtils.LNG_EMISSION_RATE_TON_CO2_PER_TON_FUEL;
+				break;
+			default:
+			}
+		}
+
+		model.emissionsLNG = Math.round(emissionsLNGAccumulator);
+		model.totalEmission = model.baseFuelEmissions + model.pilotLightEmission + model.emissionsLNG;
+	}
+
+	private static int processEvent(final Event event, final VesselEmissionAccountingReportModelV1 model, int journeyDistance) {
+		if (event instanceof final SlotVisit slotVisit) {
+			processSlotVisit(model, slotVisit);
+		} else if (event instanceof final StartEvent startEvent) {
+			processStartEvent(model, startEvent);
+		} else if (event instanceof final EndEvent endEvent) {
+			processEndEvent(model, endEvent);
+		} else if (event instanceof final Journey journeyEvent) {
+			journeyDistance = processJourneyEvent(model, journeyEvent);
+		} else if (event instanceof final Idle idleEvent) {
+			processIdleEvent(model, idleEvent);
+		}
+		return journeyDistance;
+	}
+
+	private static void processIdleEvent(final VesselEmissionAccountingReportModelV1 model, Idle idleEvent) {
+		if (idleEvent.isLaden()) {
+			model.eventType = "Laden Idle";
+		} else {
+			model.eventType = "Ballast Idle";
+		}
+		if (idleEvent.getNextEvent() instanceof final SlotVisit sv && sv.getSlotAllocation() != null) {
+			final SlotAllocation slotAllocation = sv.getSlotAllocation();
+			if (slotAllocation != null) {
+				model.otherID = slotAllocation.getName();
+			}
+			model.otherID = sv.getSlotAllocation().getName();
+		}
+	}
+
+	private static int processJourneyEvent(final VesselEmissionAccountingReportModelV1 model, Journey journeyEvent) {
+		int journeyDistance;
+		if (journeyEvent.isLaden()) {
+			model.eventType = "Laden Leg";
+		} else {
+			model.eventType = "Ballast Leg";
+		}
+		if (journeyEvent.getPreviousEvent() instanceof final SlotVisit sv && sv.getSlotAllocation() != null) {
+			final SlotAllocation slotAllocation = sv.getSlotAllocation();
+			if (slotAllocation != null) {
+				model.otherID = slotAllocation.getName();
+			}
+			model.otherID = sv.getSlotAllocation().getName();
+		}
+		journeyDistance = journeyEvent.getDistance();
+		return journeyDistance;
+	}
+
+	private static void processEndEvent(final VesselEmissionAccountingReportModelV1 model, EndEvent endEvent) {
+		model.eventType = "Vessel end";
+		if (endEvent.getSlotAllocation() != null) {
+			model.otherID = endEvent.getSlotAllocation().getName();
+		}
+		final SlotAllocation slotAllocation = endEvent.getSlotAllocation();
+		if (slotAllocation != null) {
+			model.otherID = slotAllocation.getName();
+			final Slot<?> slot = slotAllocation.getSlot();
+			if (slot != null) {
+				model.eventID = slot.getName();
+			}
+		}
+	}
+
+	private static void processStartEvent(final VesselEmissionAccountingReportModelV1 model, StartEvent startEvent) {
+		model.eventType = "Vessel start";
+		final SlotAllocation slotAllocation = startEvent.getSlotAllocation();
+		if (slotAllocation != null) {
+			model.otherID = slotAllocation.getName();
+			final Slot<?> slot = slotAllocation.getSlot();
+			if (slot != null) {
+				model.eventID = slot.getName();
+			}
+		}
+	}
+
+	private static void processSlotVisit(final VesselEmissionAccountingReportModelV1 model, SlotVisit slotVisit) {
+		model.eventType = "Slot Visit";
+		final @Nullable SlotAllocation slotAllocation = slotVisit.getSlotAllocation();
+		if (slotAllocation != null) {
+			model.otherID = slotAllocation.getName();
+			final Slot<?> slot = slotAllocation.getSlot();
+			if (slot != null) {
+				model.eventID = slot.getName();
+				model.equivalents.add(slot);
+				if (slot.getCargo() != null) {
+					model.otherID = slot.getCargo().getUuid();
+				}
+			}
+			if (slot instanceof LoadSlot) {
+				model.methaneSlip += Math.round(slotAllocation.getPhysicalEnergyTransferred() * model.methaneSlipRate);
+			}
+		}
 	}
 
 	public static File jsonOutput(final List<VesselEmissionAccountingReportModelV1> models) {
