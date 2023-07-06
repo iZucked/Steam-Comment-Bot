50b04966465b0507c24b2660b859a7e5a858b618
Andrey Popov <apopov@minimaxlabs.com>
2023-07-06 13:28:53 +0100

    WIP CII utils

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/emissions/UtilsCII.java
@@ -0,0 +1,90 @@
+package com.mmxlabs.lingo.reports.emissions;
+
+import java.time.Year;
+import java.util.Map;
+
+import com.mmxlabs.models.lng.fleet.Vessel;
+
+public class UtilsCII {
+	
+	private static final double POWER_NON_LARGE_DWT = 2.673;
+	private static final double MIN_CAPACITY = 65_000.0;
+	private static final double LARGE_DWT_THESHOLD = 100_000.0;
+	private static final double MULTIPLIER_COEFF_LARGE_DWT = 9.827;
+	private static final double MULTIPLIER_COEFF_NON_LARGE_DWT = 14_479E+10;
+
+	private static final Map<Year, Double> RELATIVE_REDUCTION_FACTOR_TABLE =
+			Map.of(
+					Year.of(2023), 0.05,
+					Year.of(2024), 0.07,
+					Year.of(2025), 0.09
+					// hard code new values here
+			);
+	
+	private static final int DWT_GREATER_OR_EQUAL_100K = 0;
+	private static final int DWT_LESS_THAN_100K = 1;
+	private static final double[][] GRADES_EXPONENT_BOUNDARY_TABLES = {
+			{ 0.89, 0.98, 1.06, 1.13 },
+			{ 0.78, 0.92, 1.10, 1.17 }
+	};
+	
+	enum Grade {
+		A(0), 
+		B(1), 
+		C(2), 
+		D(3);
+		
+		public final int index;
+		
+		Grade(int indexValue) {
+			this.index = indexValue;
+		}
+	}
+	
+	private UtilsCII() {}
+	
+	public static double[][] getLetterGradeBoundaries(final Vessel vessel) {
+		
+		final double dwt = vessel.getVesselOrDelegateDeadWeight();
+		
+		//
+		// Reference CII 2019
+		final double capacity = Math.max(dwt, MIN_CAPACITY);
+		final double a = dwt < LARGE_DWT_THESHOLD ? MULTIPLIER_COEFF_NON_LARGE_DWT : MULTIPLIER_COEFF_LARGE_DWT;
+		final double c = dwt < LARGE_DWT_THESHOLD ? POWER_NON_LARGE_DWT : 0;
+		//
+		final double referenceValueCII = a * Math.pow(capacity, -c);
+		
+		//
+		// Required CII current year
+		final double z = RELATIVE_REDUCTION_FACTOR_TABLE.get(Year.now());
+		final double requiredCII = (1 - z / 100) *  referenceValueCII;
+		
+		//
+		// Rating boundaries
+		final double[][] letterGradeBoundaries = new double[4][2];
+		//
+		for (final Grade grade : Grade.values()) {
+			final double lowerExponentBoundary = GRADES_EXPONENT_BOUNDARY_TABLES[DWT_LESS_THAN_100K][grade.index];
+			final double lowerGradeBoundary = Math.exp(lowerExponentBoundary) * requiredCII;
+			letterGradeBoundaries[DWT_LESS_THAN_100K][grade.index] = lowerGradeBoundary;
+			
+			final double upperExponentBoundary = GRADES_EXPONENT_BOUNDARY_TABLES[DWT_GREATER_OR_EQUAL_100K][grade.index];
+			final double upperGradeBoundary = Math.exp(upperExponentBoundary) * requiredCII;
+			letterGradeBoundaries[DWT_GREATER_OR_EQUAL_100K][grade.index] = upperGradeBoundary;
+		}
+		return letterGradeBoundaries;
+	}
+	
+	public static String getLetterGrade(final Vessel vessel, final double ciiValue) {
+//		final int deadweightIndex = vessel.getVesselOrDelegateDeadWeight() >= LARGE_DWT_THESHOLD ? 1 : 0;
+//		final double[][] gradesTableForThatVessel = getLetterGradeBoundaries(vessel);
+//		Grade
+//		Grade result = null;
+//		for (final Grade grade : Grade.values()) {
+//			final double boundary = gradesTableForThatVessel[deadweightIndex][grade.index];
+//			if (ciiValue < boundary)
+//		}
+		return null;
+	}
+}
