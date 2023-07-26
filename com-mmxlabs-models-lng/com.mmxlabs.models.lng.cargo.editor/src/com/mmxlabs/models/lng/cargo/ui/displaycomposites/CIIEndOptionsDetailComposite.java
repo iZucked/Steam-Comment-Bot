6e325a572498133b2a014d02581ce4fa50d5e867
Andrey Popov <apopov@minimaxlabs.com>
2023-07-26 13:39:31 +0100

    Added Drop down list for CII grades

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/displaycomposites/CIIEndOptionsDetailComposite.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/displaycomposites/CIIEndOptionsDetailComposite.java
@@ -1,11 +1,23 @@
 package com.mmxlabs.models.lng.cargo.ui.displaycomposites;
 
+import java.util.List;
+
+import org.eclipse.emf.common.notify.impl.BasicNotifierImpl.EScannableAdapterList;
+import org.eclipse.emf.databinding.EMFDataBindingContext;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 import com.mmxlabs.models.lng.cargo.CargoPackage;
+import com.mmxlabs.models.lng.commercial.EndHeelOptions;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.editors.IDisplayCompositeLayoutProvider;
+import com.mmxlabs.models.ui.editors.IInlineEditor;
+import com.mmxlabs.models.ui.editors.dialogs.IDialogEditingContext;
+import com.mmxlabs.models.ui.editors.impl.AbstractComboInlineEditor;
 import com.mmxlabs.models.ui.impl.DefaultDetailComposite;
 import com.mmxlabs.models.ui.impl.RowGroupDisplayCompositeLayoutProviderBuilder;
 
@@ -22,5 +34,47 @@ public class CIIEndOptionsDetailComposite extends DefaultDetailComposite {
 				.withFeature(CargoPackage.Literals.CII_END_OPTIONS__DESIRED_CII_GRADE, "Rating", 75) //
 				.makeRow() //
 				.make();
+	}
+	
+	@Override
+	public @Nullable IInlineEditor addInlineEditor(IInlineEditor editor) {
+		if (editor.getFeature().equals(CargoPackage.Literals.CII_END_OPTIONS__DESIRED_CII_GRADE)) {
+			return super.addInlineEditor(new CIIEndOptionsInlineEditor(editor.getEditorTarget()));
+		}
+		return super.addInlineEditor(editor);
+	}
+	
+	static class CIIEndOptionsInlineEditor extends AbstractComboInlineEditor {
+		
+		public CIIEndOptionsInlineEditor(EObject editorTarget) {
+			super(CargoPackage.Literals.CII_END_OPTIONS__DESIRED_CII_GRADE);
+		}
+
+		@Override
+		protected void doSelectionChange(IStructuredSelection sel) {
+			if (sel.getFirstElement() instanceof String s) {
+				doSetValue(s, true);
+			}
+		}
+
+		@Override
+		protected void setProposalHelper() {
+			// //
+		}
+
+		@Override
+		protected void setInitialValues() {
+			addValues(List.of("A", "B", "C", "D", "E"), false);
+		}
+
+		@Override
+		protected void updateValueDisplay(Object value) {
+			if (combo == null || ccombo.isDisposed()) {
+				return;
+			}
+			combo.getCombo().setText(String.valueOf(value));
+		}
+		
+		
 	}
 }
