194147bb3db406abc57d9cc15a7a4c40969331e2
Farukh Mukhamedov <fmukhamedov@minimaxlabs.com>
2023-08-02 11:15:04 +0100

    Access the VesselCharter in CII YtD Grade wrapper

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIStartOptionsComponentHelper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIStartOptionsComponentHelper.java
@@ -1,20 +1,12 @@
 package com.mmxlabs.models.lng.cargo.presentation.composites;
 
-import java.time.Year;
-
-import com.mmxlabs.models.lng.cargo.CIIStartOptions;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
-import com.mmxlabs.models.lng.cargo.VesselCharter;
-import com.mmxlabs.models.lng.fleet.Vessel;
-import com.mmxlabs.models.lng.schedule.cii.UtilsCII;
 import com.mmxlabs.models.ui.ComponentHelperUtils;
-import com.mmxlabs.models.ui.editors.IInlineEditor;
-import com.mmxlabs.models.ui.editors.impl.SimpleOperationInlineEditor;
 import com.mmxlabs.models.ui.impl.DefaultComponentHelper;
 
 public class CIIStartOptionsComponentHelper extends DefaultComponentHelper {
 	
-	private final static CargoPackage cp = CargoPackage.eINSTANCE;
+	private static final CargoPackage cp = CargoPackage.eINSTANCE;
 
 	public CIIStartOptionsComponentHelper() {
 		super(cp.getCIIStartOptions());
@@ -29,32 +21,7 @@ public class CIIStartOptionsComponentHelper extends DefaultComponentHelper {
 		});
 		
 		addEditor(cp.getCIIStartOptions__YearToDatePartialCII(), topClass -> {
-			final IInlineEditor editor = new SimpleOperationInlineEditor("YTD Rating", cp.getCIIStartOptions__YearToDatePartialCII()) {
-				
-				@Override
-				protected void updateDisplay(Object value) {
-					if (text == null || text.isDisposed()) {
-						return;
-					}
-					//					if (value instanceof final VesselCharter vesselCharter) {
-					//						final Vessel vessel = vesselCharter.getVessel();
-					//						final CIIStartOptions ciiStartOptions = vesselCharter.getCiiStartOptions();
-					//						if (vessel != null && ciiStartOptions != null) {
-					//							final double ciiValue = UtilsCII.findCII(vessel, ciiStartOptions.getYearToDateEmissions(), ciiStartOptions.getYearToDateDistance());
-					//							final String grade = UtilsCII.getLetterGrade(vessel, ciiValue, Year.from(vesselCharter.getStartBy()));
-					//							text.setText(grade);
-					//						}
-					//					}
-					text.setText(String.valueOf(value));
-				}
-				
-				@Override
-				protected boolean updateOnChangeToFeature(final Object changedFeature) {
-					return cp.getCIIStartOptions_YearToDateEmissions().equals(changedFeature)
-							|| cp.getCIIStartOptions_YearToDateDistance().equals(changedFeature);
-				}
-			};
-			return new CIIYearToDateGradeInlineEditorWrapper(editor);
+			return new CIIYearToDateGradeInlineEditorWrapper(new CIIYearToDateGradeInlineEditor("YTD Rating", cp.getCIIStartOptions__YearToDatePartialCII())); 
 		});
 	}
 
--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIYearToDateGradeInlineEditorWrapper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIYearToDateGradeInlineEditorWrapper.java
@@ -5,19 +5,43 @@ import java.util.Collection;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.annotation.NonNull;
 
+import com.mmxlabs.models.lng.cargo.CIIStartOptions;
+import com.mmxlabs.models.lng.cargo.VesselCharter;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
 import com.mmxlabs.models.ui.editors.dialogs.IDialogEditingContext;
 import com.mmxlabs.models.ui.editors.impl.ReadOnlyInlineEditorWrapper;
 
 public class CIIYearToDateGradeInlineEditorWrapper extends ReadOnlyInlineEditorWrapper {
+	
+	private boolean enabled = false;
+	private IDialogEditingContext dialogContext = null;
+	private MMXRootObject scenario;
+	private Collection<EObject> range = null;
 
 	protected CIIYearToDateGradeInlineEditorWrapper(final @NonNull IInlineEditor wrapped) {
 		super(wrapped);
 	}
 	
 	@Override
+	protected boolean isEnabled() {
+		return enabled;
+	}
+	
+	@Override
 	public void display(IDialogEditingContext dialogContext, MMXRootObject scenario, EObject object, Collection<EObject> range) {
+		
+		this.dialogContext = dialogContext;
+		this.scenario = scenario;
+		this.range = range;
+
+		enabled = true;
+		if (object instanceof final CIIStartOptions options && options.eContainer() instanceof final VesselCharter vesselCharter) {
+			
+			if (wrapped instanceof CIIYearToDateGradeInlineEditor editor) {
+				editor.setGrade("Hello Andrey! The vessel charter is now available for your disposal");
+			}
+		}
 		super.display(dialogContext, scenario, object, range);
 	}
 }
