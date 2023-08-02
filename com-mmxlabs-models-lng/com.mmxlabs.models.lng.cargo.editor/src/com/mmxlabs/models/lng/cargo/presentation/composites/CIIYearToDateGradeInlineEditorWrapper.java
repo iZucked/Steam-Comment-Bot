f6f963123fd51f362941260caf06cadabd25dacf
Andrey Popov <apopov@minimaxlabs.com>
2023-08-02 12:20:13 +0100

    Set grade in inline editor wrapper

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIYearToDateGradeInlineEditorWrapper.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/presentation/composites/CIIYearToDateGradeInlineEditorWrapper.java
@@ -1,5 +1,6 @@
 package com.mmxlabs.models.lng.cargo.presentation.composites;
 
+import java.time.Year;
 import java.util.Collection;
 
 import org.eclipse.emf.common.notify.Notification;
@@ -10,12 +11,16 @@ import org.eclipse.swt.widgets.Label;
 import com.mmxlabs.models.lng.cargo.CIIStartOptions;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
 import com.mmxlabs.models.lng.cargo.VesselCharter;
+import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.schedule.cii.UtilsCII;
 import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.editors.IInlineEditor;
 import com.mmxlabs.models.ui.editors.dialogs.IDialogEditingContext;
 import com.mmxlabs.models.ui.editors.impl.ReadOnlyInlineEditorWrapper;
 
 public class CIIYearToDateGradeInlineEditorWrapper extends ReadOnlyInlineEditorWrapper {
+	
+	private static final String UNDEFINED_CII_GRADE_LABEL_TEXT = "-";
 	
 	private boolean enabled = false;
 	private IDialogEditingContext dialogContext = null;
@@ -42,12 +47,24 @@ public class CIIYearToDateGradeInlineEditorWrapper extends ReadOnlyInlineEditorW
 		if (object instanceof final CIIStartOptions options && options.eContainer() instanceof final VesselCharter vesselCharter) {
 			
 			if (wrapped instanceof CIIYearToDateGradeInlineEditor editor) {
-				editor.setGrade("Hello Andrey! The vessel charter is now available for your disposal");
+				updateCIIGrade(vesselCharter, editor);
 			}
 		}
 		super.display(dialogContext, scenario, object, range);
 	}
 	
+	private void updateCIIGrade(final VesselCharter vesselCharter, final CIIYearToDateGradeInlineEditor editor) {
+		String resultingCIIGradeLabelTest = UNDEFINED_CII_GRADE_LABEL_TEXT;
+		
+		final Vessel vessel = vesselCharter.getVessel();
+		final CIIStartOptions ciiStartOptions = vesselCharter.getCiiStartOptions();
+		if (vessel != null && ciiStartOptions != null) {
+			final double ciiValue = UtilsCII.findCII(vessel, ciiStartOptions.getYearToDateEmissions(), ciiStartOptions.getYearToDateDistance());
+			resultingCIIGradeLabelTest = UtilsCII.getLetterGrade(vessel, ciiValue, Year.from(vesselCharter.getStartBy()));
+		}
+		editor.setGrade(resultingCIIGradeLabelTest);
+	}
+
 	@Override
 	protected boolean respondToNotification(final Notification notification) {
 		final EObject object = (EObject) notification.getNotifier();
@@ -55,7 +72,8 @@ public class CIIYearToDateGradeInlineEditorWrapper extends ReadOnlyInlineEditorW
 				notification.getFeature() == CargoPackage.eINSTANCE.getCIIStartOptions_YearToDateEmissions()) {
 			if (object instanceof final CIIStartOptions options && options.eContainer() instanceof final VesselCharter vesselCharter) {
 				if (wrapped instanceof CIIYearToDateGradeInlineEditor editor) {
-					editor.setGrade("Hello Andrey! The vessel charter is now available for your disposal");
+
+					updateCIIGrade(vesselCharter, editor);
 					
 					dialogContext.getDialogController().setEditorVisibility(object, getFeature(), true);
 					dialogContext.getDialogController().updateEditorVisibility();
