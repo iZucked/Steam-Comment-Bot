38d54bf6a73c308ad68fc13e76bde866941498d8
Andrey Popov <apopov@minimaxlabs.com>
2023-08-03 10:50:53 +0100

    Attempt to add warning to vessel charter

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.validation/src/com/mmxlabs/models/lng/assignment/validation/VesselCharterConstraint.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.assignment.validation/src/com/mmxlabs/models/lng/assignment/validation/VesselCharterConstraint.java
@@ -4,6 +4,7 @@
  */
 package com.mmxlabs.models.lng.assignment.validation;
 
+import java.time.Year;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -13,6 +14,7 @@ import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.validation.model.IConstraintStatus;
 
 import com.mmxlabs.models.lng.cargo.AssignableElement;
+import com.mmxlabs.models.lng.cargo.CIIEndOptions;
 import com.mmxlabs.models.lng.cargo.Cargo;
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
@@ -20,7 +22,13 @@ import com.mmxlabs.models.lng.cargo.Slot;
 import com.mmxlabs.models.lng.cargo.VesselCharter;
 import com.mmxlabs.models.lng.cargo.VesselEvent;
 import com.mmxlabs.models.lng.fleet.Vessel;
+import com.mmxlabs.models.lng.scenario.model.LNGScenarioModel;
+import com.mmxlabs.models.lng.scenario.model.util.ScenarioModelUtil;
+import com.mmxlabs.models.lng.schedule.ScheduleModel;
+import com.mmxlabs.models.lng.schedule.cii.CIIComparator;
+import com.mmxlabs.models.lng.schedule.cii.CIIGradeFinder;
 import com.mmxlabs.models.lng.types.VesselAssignmentType;
+import com.mmxlabs.models.mmxcore.MMXRootObject;
 import com.mmxlabs.models.ui.validation.AbstractModelMultiConstraint;
 import com.mmxlabs.models.ui.validation.DetailConstraintStatusDecorator;
 import com.mmxlabs.models.ui.validation.IExtraValidationContext;
@@ -35,8 +43,25 @@ public class VesselCharterConstraint extends AbstractModelMultiConstraint {
 	@Override
 	public void doValidate(final IValidationContext ctx, final IExtraValidationContext extraContext, final List<IStatus> failures) {
 		final EObject object = ctx.getTarget();
+		
+		//		if (object instanceof final CIIEndOptions ciiEndOptions) {
+		//			final VesselCharter vesselCharter = (VesselCharter) ciiEndOptions.eContainer();
+		//			final Year vesselCharterEndYear = Year.from(vesselCharter.getEndBy());
+		//			final Vessel vessel = vesselCharter.getVessel();
+		//			final MMXRootObject rootObject = extraContext.getRootObject();
+		//			if (vessel != null && rootObject instanceof final LNGScenarioModel lngScenarioModel) {
+		//				final ScheduleModel scheduleModel = ScenarioModelUtil.getScheduleModel(lngScenarioModel);
+		//				final String ciiGrade = CIIGradeFinder.findCIIGradeForScheduleVesselYear(scheduleModel, vessel, vesselCharterEndYear);
+		//				if (CIIComparator.compareGrades(ciiGrade, ciiEndOptions.getDesiredCIIGrade()) < 0) {
+		//					final String message = "The desired CII rating is not achieved";
+		//					final DetailConstraintStatusDecorator failure = new DetailConstraintStatusDecorator((IConstraintStatus) ctx.createFailureStatus(message, IStatus.WARNING));
+		//					failure.addEObjectAndFeature(ciiEndOptions, CargoPackage.eINSTANCE.getCIIEndOptions_DesiredCIIGrade());
+		//					failures.add(failure);
+		//				}
+		//			}
+		//		}
 
-		if (object instanceof AssignableElement assignment) {
+		if (object instanceof final AssignableElement assignment) {
 
 			final VesselAssignmentType vesselAssignmentType = assignment.getVesselAssignmentType();
 			if (!(vesselAssignmentType instanceof VesselCharter)) {
