b1acc1fc7ca053a7f146049faa7fe34d9f03cf5e
Andrey Popov <apopov@minimaxlabs.com>
2023-08-03 10:10:51 +0100

    CII Grade is now colored red when grade is E or D

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/emissions/cii/managers/CIIYearColumnManager.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/emissions/cii/managers/CIIYearColumnManager.java
@@ -2,6 +2,11 @@ package com.mmxlabs.lingo.reports.emissions.cii.managers;
 
 import java.time.Year;
 
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+
 import com.mmxlabs.lingo.reports.emissions.cii.CIIGradesData;
 
 public class CIIYearColumnManager extends AbstractCIIColumnManager {
@@ -20,5 +25,14 @@ public class CIIYearColumnManager extends AbstractCIIColumnManager {
 			return "-";
 		}
 		return grade;
+	}
+	
+	@Override
+	public @Nullable Color getForeground(final CIIGradesData data) {
+		final String grade = data.getGrades().get(year);
+		if (grade != null && (grade.equals("D") || grade.equals("E"))) {
+			return Display.getDefault().getSystemColor(SWT.COLOR_RED);
+		}
+		return null;
 	}
 }
