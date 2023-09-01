f6888a693bea7503daaa24424b7c09ff56851361
Simon Goodall <simon.goodall@minimaxlabs.com>
2023-09-01 11:22:13 +0100

    Fix for spot fob purchase export errors

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.migration/src/com/mmxlabs/models/lng/migration/LiNGOMigrationProvider.java
@@ -24,7 +24,7 @@ public class LiNGOMigrationProvider implements IMigrationProvider {
 	 * reflective class loader to work.
 	 */
 
-	private static final int CURRENT_VERSION = 196;
+	private static final int CURRENT_VERSION = 197;
 
 	@Override
 	public int priority() {
