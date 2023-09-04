c09d2d48cf5ec706b29932091e7d5e92fc6c0eaf
Tobi Adelana <tadelana@minimaxlabs.com>
2023-09-04 05:20:58 +0100

    Save CV show state in inventory report

---
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/standard/inventory/InventoryReportConstants.java
@@ -0,0 +1,7 @@
+package com.mmxlabs.lingo.reports.views.standard.inventory;
+
+public final class InventoryReportConstants {
+	private InventoryReportConstants() {}
+	
+	public static final String Show_CV = "Inventory.ShowCV";
+}
