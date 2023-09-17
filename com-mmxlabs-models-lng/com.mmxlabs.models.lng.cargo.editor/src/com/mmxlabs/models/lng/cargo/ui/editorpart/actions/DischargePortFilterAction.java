6c4ffc72ef96fef5cb29af459d04a5a321460476
Miten Mistry <miten.mistry@minimaxlabs.com>
2023-09-17 17:07:09 +0100

    Fixed NPE in trades table port filters

--- com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/actions/DischargePortFilterAction.java
+++ com-mmxlabs-models-lng/com.mmxlabs.models.lng.cargo.editor/src/com/mmxlabs/models/lng/cargo/ui/editorpart/actions/DischargePortFilterAction.java
@@ -5,14 +5,17 @@
 package com.mmxlabs.models.lng.cargo.ui.editorpart.actions;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.mmxlabs.models.lng.cargo.CargoModel;
 import com.mmxlabs.models.lng.cargo.CargoPackage;
-import com.mmxlabs.models.lng.cargo.Slot;
+import com.mmxlabs.models.lng.cargo.DischargeSlot;
 import com.mmxlabs.models.lng.cargo.ui.editorpart.StructuredViewerFilterManager;
 import com.mmxlabs.models.lng.cargo.ui.editorpart.TradesRowEMFPath;
+import com.mmxlabs.models.lng.port.Port;
 import com.mmxlabs.models.lng.port.PortPackage;
 import com.mmxlabs.models.mmxcore.NamedObject;
 
@@ -24,15 +27,17 @@ public class DischargePortFilterAction extends EMFPathFilterAction {
 
 	@Override
 	protected List<NamedObject> getValues() {
-		CargoModel cargoModel = (CargoModel) getSourceObject();
-		List<NamedObject> actualDischarges = new LinkedList<>();
-		for (Slot s : cargoModel.getDischargeSlots()) {
-			if (!actualDischarges.contains(s.getPort())) {
-				actualDischarges.add(s.getPort());
+		final CargoModel cargoModel = (CargoModel) getSourceObject();
+		final Set<Port> seenPorts = new HashSet<>();
+		final List<NamedObject> usedDischargePorts = new LinkedList<>();
+		for (final DischargeSlot dischargeSlot : cargoModel.getDischargeSlots()) {
+			final Port port = dischargeSlot.getPort();
+			if (port != null && seenPorts.add(port)) {
+				usedDischargePorts.add(port);
 			}
 		}
 		
-		Collections.sort(actualDischarges, (a, b) -> a.getName().compareTo(b.getName()));
-		return actualDischarges;
+		Collections.sort(usedDischargePorts, (a, b) -> a.getName().compareTo(b.getName()));
+		return usedDischargePorts;
 	}
 }
