85811cf0384eedd5774da1e9066f3a3e5c18e14e
Miten Mistry <miten.mistry@minimaxlabs.com>
2023-10-12 01:38:04 +0100

    Ninety day schedule: benchmarked schedule canvas.

---
+++ com-mmxlabs-widgets/com.mmxlabs.widgets.schedulechart/src/com/mmxlabs/widgets/schedulechart/BenchmarkedScheduleCanvas.java
@@ -0,0 +1,37 @@
+package com.mmxlabs.widgets.schedulechart;
+
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mmxlabs.widgets.schedulechart.providers.ScheduleChartProviders;
+
+public class BenchmarkedScheduleCanvas extends ScheduleCanvas {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(BenchmarkedScheduleCanvas.class);
+	
+	private long totalTime = 0;
+	private int numRepaints = 0;
+
+	public BenchmarkedScheduleCanvas(Composite parent, ScheduleChartProviders providers) {
+		super(parent, providers);
+	}
+	
+	public BenchmarkedScheduleCanvas(Composite parent, ScheduleChartProviders providers, IScheduleChartSettings settings) {
+		super(parent, providers, settings);
+	}
+
+	@Override
+	protected void repaint(GC gc) {
+		long startTime = System.nanoTime();
+		super.repaint(gc);
+		long endTime = System.nanoTime();
+		totalTime += (endTime - startTime);
+		numRepaints++;
+		System.out.println("Num repaints: " + numRepaints + ", Avg repaint time: " + (totalTime / numRepaints) + "ns");
+	}
+	
+
+}
