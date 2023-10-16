a5a44b079e6fc007b12a9584ec5b5d3a395e8257
Simon Goodall <simon.goodall@minimaxlabs.com>
2023-10-16 13:40:07 +0100

    Cleanup

--- com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/scenariodiff/Matcher.java
+++ com-mmxlabs-lingo-reports/com.mmxlabs.lingo.reports/src/com/mmxlabs/lingo/reports/views/scenariodiff/Matcher.java
@@ -5,8 +5,10 @@
 package com.mmxlabs.lingo.reports.views.scenariodiff;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 
+@NonNullByDefault
 public interface Matcher<@NonNull T> {
 
-	public boolean matches(T o1, T o2);
+	boolean matches(T o1, T o2);
 }
